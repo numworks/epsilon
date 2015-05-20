ee69038e48da70f5529b28df4426141f20c39ca6
Romain Goyet <romain@goyet.com>
2015-05-20 13:03:27 +0200

    Cleaner types for _bss and _data tags

--- platform/stm32f429/boot/crt0.c
+++ platform/stm32f429/boot/crt0.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
 #include <string.h>
 
-extern const void * _data_section_start_flash;
-extern const void * _data_section_start_ram;
-extern const void * _data_section_end_ram;
-extern const void * _bss_section_start_ram;
-extern const void * _bss_section_end_ram;
+extern char _data_section_start_flash;
+extern char _data_section_start_ram;
+extern char _data_section_end_ram;
+extern char _bss_section_start_ram;
+extern char _bss_section_end_ram;
 
 void init();
 
@@ -24,12 +24,12 @@ void _start(void) {
    * in Flash, but linked as if it were in RAM. Now's our opportunity to copy
    * it. Note that until then the data section (e.g. global variables) contains
    * garbage values and should not be used. */
-  size_t dataSectionLength = (char *)&_data_section_end_ram - (char *)&_data_section_start_ram;
+  size_t dataSectionLength = (&_data_section_end_ram - &_data_section_start_ram);
   memcpy(&_data_section_start_ram, &_data_section_start_flash, dataSectionLength);
 
   /* Zero-out the bss section in RAM
    * Until we do, any uninitialized global variable will be unusable. */
-  size_t bssSectionLength = (char *)&_bss_section_end_ram - (char *)&_bss_section_start_ram;
+  size_t bssSectionLength = (&_bss_section_end_ram - &_bss_section_start_ram);
   memset(&_bss_section_start_ram, 0, bssSectionLength);
 
   init();

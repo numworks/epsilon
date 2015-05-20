c37520e535dd106e0c8cc83e3ffa2a08ab552089
Romain Goyet <romain@goyet.com>
2015-05-20 13:03:52 +0200

    Heap initialization

---
+++ platform/stm32f429/init_heap.c
@@ -0,0 +1,19 @@
+#include "init_lcd.h"
+#include <lib/private/mem5config.h>
+
+extern char _ccm_heap_start;
+extern char _ccm_heap_end;
+
+heap_config_t HeapConfig;
+
+int memsys5Init(void *NotUsed);
+
+void init_heap() {
+  HeapConfig.nHeap = (&_ccm_heap_end - &_ccm_heap_start);
+  HeapConfig.pHeap = &_ccm_heap_start;
+  HeapConfig.mnReq = 1;
+  HeapConfig.bMemstat = 0;
+  HeapConfig.xLog = 0;
+
+  memsys5Init(0);
+}

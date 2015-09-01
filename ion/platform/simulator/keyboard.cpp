0f0e1b3d98a7d598e5a59cfa853b72dfd268de63
Romain Goyet <romain@goyet.com>
2015-09-01 22:48:47 +0200

    [ION] Simulator keyboard uses C++

---
+++ ion/platform/simulator/keyboard.cpp
@@ -0,0 +1,19 @@
+extern "C" {
+#include <ion.h>
+}
+#include "platform.h"
+
+bool ion_scankey(ion_key_t key) {
+  return Platform.keyboard->scankey(key);
+}
+
+char ion_getchar() {
+  printf("GETCHAR\n");
+  for (int i=0; i<100; i++) {
+    ion_sleep();
+  }
+  //ion_sleep();
+  //char c = getchar();
+  //printf("Returning %c\n", c);
+  return '6';
+}

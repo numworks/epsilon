11bc675ac2042b1ef698b82992ccb5ebbfa96980
Romain Goyet <romain@goyet.com>
2015-09-01 22:57:19 +0200

    [ION] Use FltkKBD on simulator

--- ion/platform/simulator/init.cpp
+++ ion/platform/simulator/init.cpp
@@ -7,24 +7,25 @@ extern "C" {
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
 #include <ion/drivers/fltklcd/fltklcd.h>
+#include <ion/drivers/fltkkbd/fltkkbd.h>
 
 void ion_init() {
   Fl::visual(FL_RGB);
   int margin = 10;
-  Fl_Window * window = new Fl_Window(ION_FRAMEBUFFER_WIDTH+2*margin, ION_FRAMEBUFFER_HEIGHT+2*margin);
-#if 1
+  Fl_Window * window = new Fl_Window(ION_FRAMEBUFFER_WIDTH+2*margin, ION_FRAMEBUFFER_HEIGHT+2*margin+40+2*margin);
+
   FltkLCD * lcd = new FltkLCD(margin, margin, ION_FRAMEBUFFER_WIDTH, ION_FRAMEBUFFER_HEIGHT);
   assert(ION_FRAMEBUFFER_BITS_PER_PIXEL == 8);
   Platform.display = lcd;
   PlatformFramebuffer = lcd->m_framebuffer;
-#else
-  Fl_Box * box = new Fl_Box(20,20,320,240,"Hello, World!");
-  box->box(FL_UP_BOX);
-  box->labelfont(FL_BOLD+FL_ITALIC);
-  box->labelsize(36);
-  box->labeltype(FL_SHADOW_LABEL);
-#endif
+
+  FltkKBD * kbd = new FltkKBD(0,0,100,100);
+  Platform.keyboard = kbd;
+
+  Fl_Button *button = new Fl_Button(margin, ION_FRAMEBUFFER_HEIGHT+2*margin+margin, 40, 40, "A");
+
   window->end();
   window->show(NULL, NULL);
   //Fl::run();

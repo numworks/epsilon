8de6ed970353cb644be8996baae06667836ddc55
Romain Goyet <romain@goyet.com>
2015-09-14 19:54:41 +0200

    [Poincare] Add more tests for the Integer class

--- poincare/test/integer.cpp
+++ poincare/test/integer.cpp
@@ -7,5 +7,7 @@ QUIZ_CASE(poincare_integer) {
 }
 
 QUIZ_CASE(poincare_integer_add) {
-  assert(Integer(123)+Integer(456) == Integer(579));
+  assert(Integer((uint32_t)0) + Integer((uint32_t)0) == Integer((uint32_t)0));
+  assert(Integer(123) + Integer(456) == Integer(579));
+  assert(Integer(123) + Integer(123456789) == Integer(123456912));
 }

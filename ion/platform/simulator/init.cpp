523522bc0ba0f4fb0cf4c4211a00c3fa94663849
Romain Goyet <romain@goyet.com>
2015-09-14 19:55:06 +0200

    [ion] Fix the simulator

--- ion/platform/simulator/init.cpp
+++ ion/platform/simulator/init.cpp
@@ -5,6 +5,7 @@ extern "C" {
 #include <unistd.h>
 }
 #include "platform.h"
+#include "framebuffer.h"
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
@@ -15,14 +16,14 @@ void ion_init() {
   Fl::visual(FL_RGB);
   int margin = 10;
 
-  Fl_Window * window = new Fl_Window(ION_FRAMEBUFFER_WIDTH+2*margin, ION_FRAMEBUFFER_HEIGHT+2*margin+120+2*margin);
+  Fl_Window * window = new Fl_Window(FRAMEBUFFER_WIDTH+2*margin, FRAMEBUFFER_HEIGHT+2*margin+120+2*margin);
 
-  FltkLCD * lcd = new FltkLCD(margin, margin, ION_FRAMEBUFFER_WIDTH, ION_FRAMEBUFFER_HEIGHT);
-  assert(ION_FRAMEBUFFER_BITS_PER_PIXEL == 8);
+  FltkLCD * lcd = new FltkLCD(margin, margin, FRAMEBUFFER_WIDTH, FRAMEBUFFER_HEIGHT);
+  assert(FRAMEBUFFER_BITS_PER_PIXEL == 8);
   Platform.display = lcd;
   PlatformFramebuffer = lcd->m_framebuffer;
 
-  FltkKbd * kbd = new FltkKbd(margin,ION_FRAMEBUFFER_HEIGHT+3*margin,ION_FRAMEBUFFER_WIDTH,120);
+  FltkKbd * kbd = new FltkKbd(margin,FRAMEBUFFER_HEIGHT+3*margin,FRAMEBUFFER_WIDTH,120);
   Platform.keyboard = kbd;
   ion_key_states = kbd->m_keyStates;
 

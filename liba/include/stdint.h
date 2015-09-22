3e8ba2312d9033cd55196d217313a7bd105cf1e0
Romain Goyet <romain@goyet.com>
2015-09-22 15:52:01 +0200

    [liba] Proper signed-ness of intxx_t

--- liba/include/stdint.h
+++ liba/include/stdint.h
@@ -6,8 +6,8 @@ typedef unsigned int uint16_t;
 typedef unsigned long uint32_t;
 typedef unsigned long long uint64_t;
 
-typedef char int8_t;
-typedef long int32_t;
-typedef long long int64_t;
+typedef signed char int8_t;
+typedef signed long int32_t;
+typedef signed long long int64_t;
 
 #endif

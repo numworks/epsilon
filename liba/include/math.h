fd0c6eaba36331641eeb2d61314f712f63826db5
Romain Goyet <romain.goyet@numworks.com>
2016-03-18 16:52:02 +0100

    Add a (fake) implementation of powf in liba
    Change-Id: I14af6937fecfbe90ab053594e1269fcbc3681b1c

---
+++ liba/include/math.h
@@ -0,0 +1,12 @@
+#ifndef LIBA_ASSERT_H
+#define LIBA_ASSERT_H
+
+#include "private/macros.h"
+
+LIBA_BEGIN_DECLS
+
+float powf(float x, float y);
+
+LIBA_END_DECLS
+
+#endif
---
+++ liba/src/powf.c
@@ -0,0 +1,3 @@
+float powf(float x, float y) {
+  return 1.0f; // TODO: do this for real
+}

fcca8e77f17097f9328ab786d28d223043ac07e5
Felix Raimundo <felix.raimundo@numworks.com>
2016-03-21 17:47:52 +0100

    Add a test for products.
    Change-Id: I479f4fa49bca62dd1d558c745a3b43f9ab55a72a

---
+++ poincare/test/product.cpp
@@ -0,0 +1,9 @@
+#include <quiz.h>
+#include <poincare.h>
+#include <assert.h>
+
+QUIZ_CASE(poincare_multiply_approximate) {
+  //Context context;
+  //Integer a(1), b(2);
+  //assert(Product(&a, &b).approximate(context) == 2);
+}

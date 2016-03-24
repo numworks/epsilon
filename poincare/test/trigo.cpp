46ffb55ec371146a17eecfb80ba9a296c65c496f
Felix Raimundo <felix.raimundo@numworks.com>
2016-03-24 18:30:44 +0100

    Add parsing test for trigo functions.
    Change-Id: Ibcf1dd1759cd106521023f0c2adadc1f993f3eaa

---
+++ poincare/test/trigo.cpp
@@ -0,0 +1,21 @@
+#include <quiz.h>
+#include <poincare.h>
+#include <assert.h>
+
+QUIZ_CASE(poincare_parse_trigo) {
+  {
+    Expression * e = Expression::parse("sin(0)");
+    Expression * e2 = e->simplify();
+    assert(e2->type() == Expression::Type::Sinus);
+  }
+  {
+    Expression * e = Expression::parse("cos(0)");
+    Expression * e2 = e->simplify();
+    assert(e2->type() == Expression::Type::Cosinus);
+  }
+  {
+    Expression * e = Expression::parse("tan(0)");
+    Expression * e2 = e->simplify();
+    assert(e2->type() == Expression::Type::Tangent);
+  }
+}

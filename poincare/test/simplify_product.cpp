3f8c05ad7e2b8f2d96c477b9a20fa894edd18f92
Felix Raimundo <felix.raimundo@numworks.com>
2016-04-11 16:38:58 +0200

    Add multiplication rules.
    Change-Id: I5e91414e8c6ad45e0ff2d19fc7c5dc3914fa247a

--- poincare/test/simplify_product.cpp
+++ poincare/test/simplify_product.cpp
@@ -5,6 +5,12 @@
 QUIZ_CASE(poincare_simplify_product_by_zero) {
   assert(simplifies_to("3*0", "0"));
   assert(simplifies_to("foo*0", "0"));
+
   assert(simplifies_to("0*3", "0"));
   assert(simplifies_to("0*foo", "0"));
+
+  assert(simplifies_to("3*5", "15"));
+  assert(simplifies_to("8*6", "48"));
+
+  assert(simplifies_to("3*(5+4)", "27"));
 }

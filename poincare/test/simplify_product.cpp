c9e90299af822c310854174a27500613a3c34b40
Felix Raimundo <felix.raimundo@numworks.com>
2016-04-11 16:47:03 +0200

    Distributivity for addition rules.
    Change-Id: I86eb9133e2a83e7bdefea9facaa84e475f480426

--- poincare/test/simplify_product.cpp
+++ poincare/test/simplify_product.cpp
@@ -14,3 +14,9 @@ QUIZ_CASE(poincare_simplify_product_by_zero) {
 
   assert(simplifies_to("3*(5+4)", "27"));
 }
+
+QUIZ_CASE(poincare_simplify_distributive) {
+  assert(equivalent_to("3*(x+y)", "3*x+3*y"));
+  assert(equivalent_to("3*(x+y+z)", "3*x+3*y+3*z"));
+  assert(equivalent_to("3*(x+y+z+w)", "3*x+3*y+3*z+3*w"));
+}

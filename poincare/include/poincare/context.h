f044bb4691e1a4c4b51b8ebb1f58f8b7463d9ef0
Felix Raimundo <felix.raimundo@numworks.com>
2016-04-18 14:34:25 +0200

    Remove dead code in context.h
    Change-Id: I2392c1025e212282ad4801a65b71b62a5c793948

--- poincare/include/poincare/context.h
+++ poincare/include/poincare/context.h
@@ -11,10 +11,6 @@ class Context {
     ~Context();
     Expression * operator[](const char * symbol_name) const;
     void setExpressionForSymbolName(Expression * expression, const char * symbol_name);
-    /*
-    Expression const& operator[](const char * symbol_name) const;
-    Expression& operator[](const char * symbol_name);
-    */
   private:
     struct ContextPair {
       const char * name;

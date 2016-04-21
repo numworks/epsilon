f35a93198d61854b4b285535f2834e680cecf6d4
Romain Goyet <romain.goyet@numworks.com>
2016-04-21 15:50:22 +0200

    Escher: Improve the View logic and add some comments
    Change-Id: I9d6f0b1b8652ef10ea4395dccb45785f3e1a0737

--- escher/include/escher/view.h
+++ escher/include/escher/view.h
@@ -6,26 +6,36 @@ extern "C" {
 #include <kandinsky.h>
 }
 
+/* Key concepts
+ * - A View always clips: you cannot draw outside its frame (TODO!)
+ * - A View can redraw its whole hierarchy, but a very important optimization is
+ *   for it not to do this all the time. So a View will try to track which parts
+ *   of it really need to be redrawn.
+ * - A view can be offscreen. Until it's attached to the screen, it shouldn't
+ *   send any display command.
+ */
+
 class View {
 public:
   View(KDRect frame);
   ~View();
-  void draw();
-  virtual void drawRect(KDRect rect);
+
+  virtual void drawRect(KDRect rect); // To be implemented. Draw ourself.
+
   void addSubview(View * subview);
   void removeFromSuperview();
   void setFrame(KDRect frame);
 protected:
   KDRect bounds();
 private:
-  void setOriginAndDrawRect(KDRect rect);
+  void redraw();
+  void redraw(KDRect rect);
   KDPoint absoluteOrigin();
   //TODO: We may want a dynamic size at some point
   static constexpr uint8_t k_maxNumberOfSubviews = 4;
   KDRect m_frame;
   View * m_superview;
   View * m_subviews[k_maxNumberOfSubviews];
-  uint8_t m_numberOfSubviews;
 };
 
 #endif
--- escher/src/view.cpp
+++ escher/src/view.cpp
@@ -5,49 +5,79 @@ extern "C" {
 
 View::View(KDRect frame) :
   m_frame(frame),
-  m_superview(nullptr),
-  m_numberOfSubviews(0)
+  m_superview(nullptr)
 {
+  for (uint8_t i=0; i<k_maxNumberOfSubviews; i++) {
+    m_subviews[i] = nullptr;
+  }
 }
 
 View::~View() {
 }
 
 void View::drawRect(KDRect rect) {
+  // By default, a view doesn't do anything
+  // It's transparent!
 }
 
-void View::draw() {
-  setOriginAndDrawRect(m_frame);
-  for (uint8_t i=0; i<m_numberOfSubviews; i++) {
+void View::redraw() {
+  redraw(bounds());
+}
+
+void View::redraw(KDRect rect) {
+  // Fisrt, let's draw our own content by calling drawRect
+  KDSetOrigin(absoluteOrigin());
+  this->drawRect(rect);
+  // Then, let's recursively draw our children over ourself
+  for (uint8_t i=0; i<k_maxNumberOfSubviews; i++) {
     View * subview = m_subviews[i];
-    subview->draw();
+    if (subview == nullptr) {
+      continue;
+    }
+    KDRect intersection = KDRectIntersect(rect, subview->m_frame);
+    if (intersection.width > 0 || intersection.height > 0) {
+      // Let's express intersection in subview's coordinates
+      intersection.x -= subview->m_frame.x;
+      intersection.y -= subview->m_frame.y;
+      subview->redraw(intersection);
+    }
   }
 }
 
 void View::addSubview(View * subview) {
-  assert(m_numberOfSubviews < k_maxNumberOfSubviews);
+  // Let's find a spot for that subview
+  uint8_t i = 0;
+  while (m_subviews[i] != nullptr) {
+    i++;
+    assert(i<k_maxNumberOfSubviews); // No room left!
+  }
+
   subview->m_superview = this;
-  m_subviews[m_numberOfSubviews++] = subview;
+  m_subviews[i] = subview;
+
+  // That subview needs to be drawn
+  subview->redraw();
 }
 
 void View::removeFromSuperview() {
   assert(m_superview != nullptr);
+  // First, remove the view from its parent hierarchy
   assert(false); // FIXME: Unimplemented
+  // Then, redraw the parent
+  m_superview->drawRect(m_frame);
 }
 
 void View::setFrame(KDRect frame) {
  // TODO: Return if frame is equal to m_frame
- if (m_superview != nullptr) {
-   // We're moving this view
-   // So let's ask its superview to redraw itself where that view previously was
-   m_superview->setOriginAndDrawRect(m_frame);
- }
+ KDRect previousFrame = m_frame;
  m_frame = frame;
  if (m_superview != nullptr) {
-   // We've been moved
-   // Draw ourself at the new location
-   setOriginAndDrawRect(bounds());
+   /* We have moved this view. This left a blank spot in its superview were it
+    * previously was. So let's redraw that part of the superview. */
+   m_superview->redraw(previousFrame);
  }
+ // The view now needs to redraw itself entirely
+ redraw();
 }
 
 KDRect View::bounds() {
@@ -55,11 +85,6 @@ KDRect View::bounds() {
   bounds.x = 0;
   bounds.y = 0;
   return bounds;
-}
-
-void View::setOriginAndDrawRect(KDRect rect) {
-  KDSetOrigin(absoluteOrigin());
-  this->drawRect(rect);
 }
 
 KDPoint View::absoluteOrigin() {

4f3a062bb8cd9c32645886305f51f37d563be931
Romain Goyet <romain.goyet@numworks.com>
2016-04-25 16:53:34 +0200

    libaxx: remove a warning
    Change-Id: I0d59ee19258e63d92adc870bb068f733adf39376

--- libaxx/src/new.cpp
+++ libaxx/src/new.cpp
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 
+// See the C++ standard, section 3.7.4 for those definitions
+
 void * operator new (unsigned int size) {
   return malloc(size);
 }
 
-void operator delete (void * ptr) {
+void operator delete (void * ptr) noexcept {
   free(ptr);
 }

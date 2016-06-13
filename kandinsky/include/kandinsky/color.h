2bf599b7865f86ade6a24f594aa9804bca29f4d5
Romain Goyet <romain.goyet@numworks.com>
2016-06-13 14:04:16 +0200

    Kandinsky: Introduce KDColorRGB
    Change-Id: Ic64f2ff9a441580940d37151e190fd0a3d954d8a

--- kandinsky/include/kandinsky/color.h
+++ kandinsky/include/kandinsky/color.h
@@ -3,8 +3,21 @@
 
 #include <stdint.h>
 
+// Kandinsky uses RGB565
+
 typedef uint16_t KDColor;
 
-#define KDColorRGB(r,g,b) ((KDColor)(((r&0x1F)<<11)&((g&0x3F)<<5)&(b&0x1F)))
+/* KDColorRGB takes a 32-bits RGB color and builds a KDColor from it.
+ * Given KDColor is RGB565 and that we take 8-bit values for R, G, and B,
+ * some shifting has to happen. */
+
+#define KDColorRGB(r,g,b) ((KDColor)((((((uint16_t)(r))>>3)&0x1F)<<11)|(((((uint16_t)(g))>>2)&0x3F)<<5)|((((uint16_t)(b))>>3)&0x1F)))
+
+#define KDColorBlack KDColorRGB(0x00, 0x00, 0x00)
+#define KDColorWhite KDColorRGB(0xFF, 0xFF, 0xFF)
+
+#define KDColorRed KDColorRGB(0xFF, 0x00, 0x00)
+#define KDColorGreen KDColorRGB(0x00, 0xFF, 0x00)
+#define KDColorBlue KDColorRGB(0x00, 0x00, 0xFF)
 
 #endif

7d49decd0318d89e324ebbe1b8e9e524f07d052e
Romain Goyet <romain.goyet@numworks.com>
2016-08-29 13:30:34 +0200

    [kandinsky] optimize KDFrameBuffer::pushRectUniform
    Change-Id: Ie1217a8c70817110b52e505e1cf60eab08180cac

--- kandinsky/src/framebuffer.cpp
+++ kandinsky/src/framebuffer.cpp
@@ -27,10 +27,18 @@ void KDFrameBuffer::pushRect(KDRect rect, const KDColor * pixels) {
 }
 
 void KDFrameBuffer::pushRectUniform(KDRect rect, KDColor color) {
-  for (KDCoordinate i=0; i<rect.width(); i++) {
-    for (KDCoordinate j=0; j<rect.height(); j++) {
-      *pixelAddress(rect.origin().translatedBy(KDPoint(i,j))) = color;
+  // Caution: this code is used very frequently
+  // It's worth optimizing!
+  KDColor * pixel = pixelAddress(rect.origin());
+  int lineDelta = m_size.width() - rect.width();
+  for (KDCoordinate j=0; j<rect.height(); j++) {
+    //KDColor * topLeftPixelAddress = pixelAddress(rect.origin().translatedBy(KDPoint(0,j)));
+    //*pixelAddress(rect.origin().translatedBy(KDPoint(i,j))) = color;
+    for (KDCoordinate i=0; i<rect.width(); i++) {
+      *pixel++ = color;
+      //*pixelAddress(rect.origin().translatedBy(KDPoint(i,j))) = color;
     }
+    pixel += lineDelta;
   }
 }
 

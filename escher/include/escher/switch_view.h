b54c9929385ba48799ea20a207687d0b94cf1254
Émilie Feral <emilie.feral@numworks.com>
2016-09-21 15:18:30 +0200

    [escher] implement redraw in switch view
    Change-Id: I9a917f2788e918acb68b139cb55132497954ca3b

--- escher/include/escher/switch_view.h
+++ escher/include/escher/switch_view.h
@@ -2,13 +2,15 @@
 #define ESCHER_SWITCH_VIEW_H
 
 #include <escher/childless_view.h>
-#include <escher/label_view.h>
-#include <escher/palette.h>
 
 class SwitchView : public ChildlessView {
 public:
   SwitchView();
   void drawRect(KDContext * ctx, KDRect rect) const override;
+private:
+  constexpr static KDCoordinate k_switchHeight = 10;
+  constexpr static KDCoordinate k_switchWidth = 20;
+  constexpr static KDCoordinate k_separatorThickness = 1;
 };
 
 #endif
--- escher/src/switch_view.cpp
+++ escher/src/switch_view.cpp
@@ -1,4 +1,12 @@
 #include <escher/switch_view.h>
+#include <escher/palette.h>
+
+/* k_switchHeight and k_switchWidth are the dimensions of the switch (including
+* the outline of the switch). The outline thickness is k_separatorThickness. */
+constexpr KDCoordinate SwitchView::k_switchHeight;
+constexpr KDCoordinate SwitchView::k_switchWidth;
+constexpr KDCoordinate SwitchView::k_separatorThickness;
+
 
 SwitchView::SwitchView() :
 ChildlessView()
@@ -6,13 +14,21 @@ ChildlessView()
 }
 
 void SwitchView::drawRect(KDContext * ctx, KDRect rect) const {
-  KDCoordinate width = bounds().width();
-  KDCoordinate height = bounds().height();
+  /* Draw the switch in the center of the view. The widthCenter and heightCenter
+  * are the coordinates of the middle of the view. That way, for instance,
+  * (widthCenter - switchHalfWidth) and (heightCenter-switchHalfHeight) indicate
+  * the top left corner of the view. */
+  KDCoordinate widthCenter = bounds().width()/2;
+  KDCoordinate heightCenter =  bounds().height()/2;
+  KDCoordinate switchHalfWidth = k_switchWidth/2;
+  KDCoordinate switchHalfHeight = k_switchHeight/2;
 
-  ctx->fillRect(KDRect(0, 0, width, 1), Palette::LineColor);
-  ctx->fillRect(KDRect(0, 1, 1, height-1), Palette::LineColor);
-  ctx->fillRect(KDRect(1, height-1, width-1, 1), Palette::LineColor);
-  ctx->fillRect(KDRect(width-1, 1, 1, height-1), Palette::LineColor);
-  ctx->fillRect(KDRect(1, 1, width/3, height-2), KDColorBlack);
-  ctx->fillRect(KDRect(width/3+1, 1, width-2-width/3, height-2), KDColorGreen);
+  // These 4 lines draw the outline of the switch.
+  ctx->fillRect(KDRect(widthCenter - switchHalfWidth, heightCenter-switchHalfHeight, k_switchWidth, k_separatorThickness), Palette::LineColor);
+  ctx->fillRect(KDRect(widthCenter - switchHalfWidth, heightCenter-switchHalfHeight+k_separatorThickness, k_separatorThickness, k_switchHeight-k_separatorThickness), Palette::LineColor);
+  ctx->fillRect(KDRect(widthCenter - switchHalfWidth+k_separatorThickness, heightCenter+switchHalfHeight-k_separatorThickness, k_switchWidth-k_separatorThickness, k_separatorThickness), Palette::LineColor);
+  ctx->fillRect(KDRect(widthCenter + switchHalfWidth, heightCenter-switchHalfHeight, k_separatorThickness, k_switchHeight), Palette::LineColor);
+  // These 2 lines fill the switch with black and green.
+  ctx->fillRect(KDRect(widthCenter - switchHalfWidth+k_separatorThickness, heightCenter-switchHalfHeight+k_separatorThickness, switchHalfWidth, k_switchHeight-2*k_separatorThickness), KDColorBlack);
+  ctx->fillRect(KDRect(widthCenter+k_separatorThickness, heightCenter-switchHalfHeight+k_separatorThickness, switchHalfWidth-k_separatorThickness, k_switchHeight-2*k_separatorThickness), KDColorGreen);
 }

9c92fa4604216070df079dde43171fad4c33211f
Émilie Feral <emilie.feral@numworks.com>
2016-10-12 18:03:46 +0200

    [apps/graph/values] Clean tab spaces
    Change-Id: I5646fde32d78caf7a6fd66bb71e235a9af259ff3

--- apps/graph/values/interval.cpp
+++ apps/graph/values/interval.cpp
@@ -2,21 +2,21 @@
 #include <assert.h>
 
 Graph::Interval::Interval(float start, float end, float step) :
-	m_start(start),
-	m_end(end),
-	m_step(step)
+  m_start(start),
+  m_end(end),
+  m_step(step)
 {
 }
 
 int Graph::Interval::numberOfElements() {
-	if (m_start > m_end) {
-		return 0;
-	} else {
-		return 1 + (m_end - m_start)/m_step;
-	}
+  if (m_start > m_end) {
+    return 0;
+  } else {
+    return 1 + (m_end - m_start)/m_step;
+  }
 }
 
 float Graph::Interval::element(int i) {
-	assert(i >= 0 && i < numberOfElements());
-	return m_start + i*m_step;
+  assert(i >= 0 && i < numberOfElements());
+  return m_start + i*m_step;
 }

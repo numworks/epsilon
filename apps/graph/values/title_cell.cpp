9eeb0abca14ccf5d7b29b4b43a3a8113d2197a32
Émilie Feral <emilie.feral@numworks.com>
2016-10-14 17:41:00 +0200

    [apps/graph/values] use pointer text view in title cell
    Change-Id: I4a9430da99f91eb220c9915179a2589d56242195

--- apps/graph/values/title_cell.cpp
+++ apps/graph/values/title_cell.cpp
@@ -1,25 +1,43 @@
 #include "title_cell.h"
-#include "../function_store.h"
+#include "../function.h"
+#include <assert.h>
 
 namespace Graph {
 
 TitleCell::TitleCell() :
   EvenOddCell(),
-  m_text(nullptr)
+  m_pointerTextView(nullptr, 0.5f, 0.5f)
 {
 }
 
+void TitleCell::reloadCell() {
+  EvenOddCell::reloadCell();
+  m_pointerTextView.setBackgroundColor(backgroundColor());
+}
+
 void TitleCell::setText(const char * title) {
-  m_text = (char *)title;
+  m_pointerTextView.setText(title);
+}
+
+int TitleCell::numberOfSubviews() const {
+  return 1;
+}
+
+View * TitleCell::subviewAtIndex(int index) {
+  assert(index == 0);
+  return &m_pointerTextView;
+}
+
+void TitleCell::layoutSubviews() {
+  m_pointerTextView.setFrame(bounds());
 }
 
 void TitleCell::drawRect(KDContext * ctx, KDRect rect) const {
   EvenOddCell::drawRect(ctx, rect);
-  // Write the content of the cell
+  // Write the "(x)"
   KDColor background = backgroundColor();
-  KDSize textSize = KDText::stringSize(m_text);
+  KDSize textSize = KDText::stringSize("f");
   KDPoint origin(0.5f*(m_frame.width() - textSize.width()), 0.5f*(m_frame.height() - textSize.height()));
-  ctx->drawString(m_text, origin, KDColorBlack, background);
   ctx->drawString(Function::Parameter, origin.translatedBy(KDPoint(textSize.width(), 0)), KDColorBlack, background);
 }
 
--- apps/graph/values/title_cell.h
+++ apps/graph/values/title_cell.h
@@ -8,11 +8,15 @@ namespace Graph {
 class TitleCell : public EvenOddCell {
 public:
   TitleCell();
-  void setText(const char * title);
+  void reloadCell() override;
+  void setText(const char * textContent);
+  int numberOfSubviews() const override;
+  View * subviewAtIndex(int index) override;
+  void layoutSubviews() override;
   void drawRect(KDContext * ctx, KDRect rect) const override;
 
 protected:
-  char * m_text;
+  PointerTextView m_pointerTextView;
 };
 
 }

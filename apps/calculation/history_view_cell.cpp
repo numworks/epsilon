6869df126a9ed58e982294dc0eda4b73fad5092e
Émilie Feral <emilie.feral@numworks.com>
2016-10-26 17:46:03 +0200

    [apps/calculation] Use pretty print view in history view cell
    Change-Id: I272e03bad1ca99240285316beed8fe9ab7a270b7

--- apps/calculation/history_view_cell.cpp
+++ apps/calculation/history_view_cell.cpp
@@ -5,53 +5,52 @@
 namespace Calculation {
 
 HistoryViewCell::HistoryViewCell() :
-  m_calculation(nullptr),
+  Responder(nullptr),
+  m_prettyPrint(PrettyPrintView(this)),
   m_result(BufferTextView(1.0f, 0.5f))
 {
 }
 
 int HistoryViewCell::numberOfSubviews() const {
-  return 1;
+  return 2;
 }
 
 View * HistoryViewCell::subviewAtIndex(int index) {
-  assert(index == 0);
-  return &m_result;
+  switch (index) {
+    case 0:
+      return &m_prettyPrint;
+    case 1:
+      return &m_result;
+    default:
+      assert(false);
+      return nullptr;
+  }
 }
 
 void HistoryViewCell::layoutSubviews() {
   KDCoordinate width = bounds().width();
   KDCoordinate height = bounds().height();
   // Position the result
-  KDSize prettyPrintSize = KDSize(0,0);
-  if (m_calculation && m_calculation->layout() != nullptr) {
-    prettyPrintSize = m_calculation->layout()->size();
-  }
-  KDRect resultFrame(prettyPrintSize.width(), 0, width - prettyPrintSize.width(), height);
+  KDSize prettyPrintSize = m_prettyPrint.minimalSizeForOptimalDisplay();
+  KDRect prettyPrintFrame(0, 0, width, prettyPrintSize.height());
+  m_prettyPrint.setFrame(prettyPrintFrame);
+  KDRect resultFrame(0, prettyPrintSize.height(), width, height - prettyPrintSize.height());
   m_result.setFrame(resultFrame);
 }
 
 void HistoryViewCell::setCalculation(Calculation * calculation) {
-  m_calculation = calculation;
+  m_prettyPrint.setExpression(calculation->layout());
   char buffer[7];
-  Float(m_calculation->evaluation()).convertFloatToText(buffer, 14, 7);
+  Float(calculation->evaluation()).convertFloatToText(buffer, 14, 7);
   m_result.setText(buffer);
 }
 
 void HistoryViewCell::reloadCell() {
   KDColor backgroundColor = isHighlighted() ? Palette::FocusCellBackgroundColor : Palette::CellBackgroundColor;
   m_result.setBackgroundColor(backgroundColor);
+  m_prettyPrint.setBackgroundColor(backgroundColor);
   TableViewCell::reloadCell();
-}
-
-void HistoryViewCell::drawRect(KDContext * ctx, KDRect rect) const {
-  // Select background color
-  KDColor backgroundColor = isHighlighted() ? Palette::FocusCellBackgroundColor : Palette::CellBackgroundColor;
-  ctx->fillRect(rect, backgroundColor);
-  // Draw the pretty print
-  if (m_calculation && m_calculation->layout() != nullptr) {
-    m_calculation->layout()->draw(ctx, KDPointZero, KDColorBlack, backgroundColor);
-  }
+  layoutSubviews();
 }
 
 }

26f6441e38d5b639a2db12976a4c56b00f383700
Émilie Feral <emilie.feral@numworks.com>
2016-11-02 10:09:37 +0100

    [poincare] Correct tests in float to round instead of truncate
    Change-Id: Ic960206a56b1dc117466e9d9e28d06da2fd140b6

--- poincare/test/float.cpp
+++ poincare/test/float.cpp
@@ -8,19 +8,21 @@ QUIZ_CASE(poincare_float_to_text) {
   Float(123.456).convertFloatToText(buffer, 14, 7);
   assert(strcmp(buffer, "1.23456e2") == 0);
   Float(1.234567891011).convertFloatToText(buffer, 14, 7);
-  assert(strcmp(buffer, "1.234567e0") == 0);
+  assert(strcmp(buffer, "1.234568e0") == 0);
   Float(2).convertFloatToText(buffer, 14, 7);
   assert(strcmp(buffer, "2.e0") == 0);
   Float(123456789).convertFloatToText(buffer, 14, 7);
-  assert(strcmp(buffer, "1.234567e8") == 0);
+  assert(strcmp(buffer, "1.234568e8") == 0);
   Float(0.00000123456789).convertFloatToText(buffer, 14, 7);
-  assert(strcmp(buffer, "1.234567e-6") == 0);
+  assert(strcmp(buffer, "1.234568e-6") == 0);
   Float(0.99).convertFloatToText(buffer, 14, 7);
   assert(strcmp(buffer, "9.9e-1") == 0);
   Float(-123.456789).convertFloatToText(buffer, 14, 7);
-  assert(strcmp(buffer, "-1.234567e2") == 0);
+  assert(strcmp(buffer, "-1.234568e2") == 0);
   Float(-0.000123456789).convertFloatToText(buffer, 14, 7);
-  assert(strcmp(buffer, "-1.234567e-4") == 0);
+  assert(strcmp(buffer, "-1.234568e-4") == 0);
   Float(0).convertFloatToText(buffer, 14, 7);
   assert(strcmp(buffer, "0.e0") == 0);
+  Float(10000000000000000000000000000.0f).convertFloatToText(buffer, 14, 7);
+  assert(strcmp(buffer, "1.e28") == 0);
 }

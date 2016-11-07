33eb1601beb2c7db3a24f1452cb256844a259ca7
Émilie Feral <emilie.feral@numworks.com>
2016-11-07 16:37:33 +0100

    [apps] Display the tool box when clicking on F5
    Change-Id: I529a455c2c2cc0933575d334b8f592c458ac242c

--- apps/expression_text_field_delegate.cpp
+++ apps/expression_text_field_delegate.cpp
@@ -1,4 +1,5 @@
 #include "expression_text_field_delegate.h"
+#include "apps_container.h"
 #include <math.h>
 
 bool ExpressionTextFieldDelegate::textFieldDidReceiveEvent(TextField * textField, Ion::Events::Event event) {
@@ -13,6 +14,14 @@ bool ExpressionTextFieldDelegate::textFieldDidReceiveEvent(TextField * textField
     isnan(Expression::parse(textField->text())->approximate(*evaluateContext()))) {
       textField->app()->displayWarning("Relis ton cours de maths, veux tu?");
       return true;
+  }
+  //if (event == Ion::Events::Event::TOOL_BOX) {
+  if (event == Ion::Events::Event::F5) {
+    AppsContainer * appsContainer = (AppsContainer *)textField->app()->container();
+    ToolBoxController * toolBoxController = appsContainer->toolBoxController();
+    toolBoxController->setTextFieldCaller(textField);
+    textField->app()->displayModalViewController(toolBoxController, 0.f, 0.f, 50, 50, 0, 50);
+    return true;
   }
   return false;
 }

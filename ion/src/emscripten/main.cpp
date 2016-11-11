5dc34dd38795a9212a32303a67c7abea8aa4702b
Romain Goyet <romain.goyet@numworks.com>
2016-11-11 22:03:46 +0100

    [ion] Emscripten backend handles text input
    Change-Id: I615845e2b75b99b2a666cf956ed6cc219c95b204

--- ion/src/emscripten/main.cpp
+++ ion/src/emscripten/main.cpp
@@ -18,6 +18,7 @@ void IonEmscriptenPushEvent(int eventNumber) {
 }
 
 int main(int argc, char * argv[]) {
+  SDL_EnableUNICODE(1); // We're using Unicode values from Keyboard input
   SDL_Init(SDL_INIT_VIDEO);
   screen = SDL_SetVideoMode(Ion::Display::Width, Ion::Display::Height, 32, SDL_HWSURFACE);
   ion_app();
@@ -51,6 +52,23 @@ void Ion::Display::pushRectUniform(KDRect r, KDColor c) {
 void Ion::Display::pullRect(KDRect r, KDColor * pixels) {
 }
 
+using namespace Ion::Events;
+
+static constexpr Event sEventForASCIICharAbove32[95] = {
+  Space, Exclamation, DoubleQuotes, None, None, None, None, None,
+  LeftParenthesis, RightParenthesis, Multiplication, Plus, Comma, Minus, Dot, Division,
+  Zero, One, Two, Three, Four, Five, Six, Seven,
+  Eight, Nine, Colon, SemiColon, Lower, Equal, Greater, Question,
+  None, UpperA, UpperB, UpperC, UpperD, UpperE, UpperF, UpperG,
+  UpperH, UpperI, UpperJ, UpperK, UpperL, UpperM, UpperN, UpperO,
+  UpperP, UpperQ, UpperR, UpperS, UpperT, UpperU, UpperV, UpperW,
+  UpperX, UpperY, UpperZ, LeftBracket, None, RightBracket, Power, Underscore,
+  None, LowerA, LowerB, LowerC, LowerD, LowerE, LowerF, LowerG,
+  LowerH, LowerI, LowerJ, LowerK, LowerL, LowerM, LowerN, LowerO,
+  LowerP, LowerQ, LowerR, LowerS, LowerT, LowerU, LowerV, LowerW,
+  LowerX, LowerY, LowerZ, LeftBrace, None, RightBrace, None
+};
+
 Ion::Events::Event Ion::Events::getEvent() {
   if (sEvent != Ion::Events::None) {
     Ion::Events::Event event = sEvent;
@@ -73,6 +91,11 @@ Ion::Events::Event Ion::Events::getEvent() {
           return Ion::Events::OK;
         case SDLK_ESCAPE:
           return Ion::Events::Back;
+        case SDLK_BACKSPACE:
+          return Ion::Events::Backspace;
+      }
+      if (event.key.keysym.unicode >= 32 && event.key.keysym.unicode < 127) {
+        return sEventForASCIICharAbove32[event.key.keysym.unicode-32];
       }
     }
   }

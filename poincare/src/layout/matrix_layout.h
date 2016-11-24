ed27a24d7ac092e4a36e2b8b538b85907a8e940a
Émilie Feral <emilie.feral@numworks.com>
2016-11-24 11:22:25 +0100

    [poincare] Create a matrix layout
    Change-Id: I50a5a5adc06b631323a371edef14f6dd5ef988dc

---
+++ poincare/src/layout/matrix_layout.h
@@ -0,0 +1,27 @@
+#ifndef POINCARE_MATRIX_LAYOUT_H
+#define POINCARE_MATRIX_LAYOUT_H
+
+#include <poincare/expression.h>
+#include <poincare/expression_layout.h>
+
+class MatrixLayout : public ExpressionLayout {
+  public:
+    MatrixLayout(ExpressionLayout ** entryLayouts, int numberOfRows, int numberOfColumns);
+    ~MatrixLayout();
+  protected:
+    void render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) override;
+    KDSize computeSize() override;
+    ExpressionLayout * child(uint16_t index) override;
+    KDPoint positionOfChild(ExpressionLayout * child) override;
+  private:
+    KDCoordinate rowBaseline(int i);
+    KDCoordinate rowHeight(int i);
+    KDCoordinate height();
+    KDCoordinate columnWidth(int j);
+    KDCoordinate width();
+    ExpressionLayout ** m_entryLayouts;
+    int m_numberOfRows;
+    int m_numberOfColumns;
+};
+
+#endif

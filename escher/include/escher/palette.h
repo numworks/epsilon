ab29a8d60ea96bcc311100276d6c56bbfa63b2f4
Émilie Feral <emilie.feral@numworks.com>
2016-12-01 17:08:53 +0100

    [escher] update color in palette
    Change-Id: I83fa12b276e5cf044c78144f68b6eeb14528c4d6

--- escher/include/escher/palette.h
+++ escher/include/escher/palette.h
@@ -8,7 +8,7 @@ public:
   constexpr static KDColor LineColor = KDColor::RGB24(0xB4B7B9);
   constexpr static KDColor BackgroundColor = KDColor::RGB24(0xF0F3F5);
   constexpr static KDColor CellBackgroundColor = KDColor::RGB24(0xFCFCFC);
-  constexpr static KDColor FocusCellBackgroundColor = KDColor::RGB24(0xBFD3EB);
+  constexpr static KDColor FocusCellBackgroundColor = KDColor::RGB24(0xDDDFE6);
   constexpr static KDColor DesactiveTextColor = KDColor::RGB24(0x646464);
   constexpr static KDColor BoxTitleBackgroundColor = KDColor::RGB24(0x656976);
 };

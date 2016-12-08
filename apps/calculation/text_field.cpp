41ed0bf8daad0ea0ceaceb1cadea7c02273e7c10
Émilie Feral <emilie.feral@numworks.com>
2016-12-08 17:03:36 +0100

    [apps/calculation] delete useless dirty marker in the calculation text field
    Change-Id: I81507ded019eb9791776854b300b7f70a6d7f596

--- apps/calculation/text_field.cpp
+++ apps/calculation/text_field.cpp
@@ -9,12 +9,8 @@ TextField::TextField(Responder * parentResponder, char * textBuffer, size_t text
 
 bool TextField::handleEvent(Ion::Events::Event event) {
   if (event == Ion::Events::Ans) {
-    KDSize sizePreviousText = KDText::stringSize(m_textBuffer);
     insertTextAtLocation("ans", cursorLocation());
     setCursorLocation(cursorLocation() + strlen("ans"));
-    KDSize sizeText = KDText::stringSize(m_textBuffer);
-    KDRect dirtyZone(sizePreviousText.width(), 0, sizeText.width()-sizePreviousText.width(), sizeText.height());
-    markRectAsDirty(dirtyZone);
     return true;
   }
   if (m_currentTextLength == 0 &&
@@ -23,12 +19,8 @@ bool TextField::handleEvent(Ion::Events::Event event) {
       event == Ion::Events::Minus ||
       event == Ion::Events::Dot ||
       event == Ion::Events::Division)) {
-    KDSize sizePreviousText = KDText::stringSize(m_textBuffer);
     insertTextAtLocation("ans", cursorLocation());
     setCursorLocation(cursorLocation() + strlen("ans"));
-    KDSize sizeText = KDText::stringSize(m_textBuffer);
-    KDRect dirtyZone(sizePreviousText.width(), 0, sizeText.width()-sizePreviousText.width(), sizeText.height());
-    markRectAsDirty(dirtyZone);
   }
   return(::TextField::handleEvent(event));
 }

6f5a3cf48fbee4c74f71a068404f41d12a0c21c3
Romain Goyet <romain.goyet@numworks.com>
2016-12-08 14:48:47 +0100

    [libaxx] Explicitely define __cxa_pure_virtual
    Change-Id: I07315f1bf84771a2784788e0d0eea4360245701d

---
+++ libaxx/src/cxxabi/pure_virtual.cpp
@@ -0,0 +1,10 @@
+extern "C" {
+#include <stdlib.h>
+}
+
+/* C++ code calls __cxa_pure_virtual when a pure-virtual method is called.
+ * This is an error case, so we just redirect it to abort. */
+
+extern "C" void __cxa_pure_virtual() {
+  abort();
+}

8db4cad6f1c3bd283e77f2f884b1b3b7f6f40d34
Émilie Feral <emilie.feral@numworks.com>
2017-01-13 15:22:41 +0100

    [poincare] Create a class sum
    Change-Id: Ida82d33d8389376abd2b6d6874acb338639a5e37

--- poincare/include/poincare.h
+++ poincare/include/poincare.h
@@ -26,6 +26,7 @@
 #include <poincare/product.h>
 #include <poincare/sine.h>
 #include <poincare/subtraction.h>
+#include <poincare/sum.h>
 #include <poincare/symbol.h>
 #include <poincare/tangent.h>
 #include <poincare/x_context.h>

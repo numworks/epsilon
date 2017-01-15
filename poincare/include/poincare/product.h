4fa5fef8ef0850869b51675632d6eba2a83b363d
Émilie Feral <emilie.feral@numworks.com>
2017-01-15 20:02:54 +0100

    [poincare] Create a class product
    Change-Id: I7069b55c7ec17ede42375a83e7f122ff1ed24f88

---
+++ poincare/include/poincare/product.h
@@ -0,0 +1,17 @@
+#ifndef POINCARE_PRODUCT_H
+#define POINCARE_PRODUCT_H
+
+#include <poincare/function.h>
+#include <poincare/n_context.h>
+
+class Product : public Function {
+public:
+  Product();
+  float approximate(Context & context) const override;
+  Type type() const override;
+  Expression * cloneWithDifferentOperands(Expression ** newOperands,
+      int numberOfOperands, bool cloneOperands = true) const override;
+  ExpressionLayout * createLayout() const override;
+};
+
+#endif
---
+++ poincare/src/product.cpp
@@ -0,0 +1,49 @@
+#include <poincare/product.h>
+#include <poincare/symbol.h>
+#include <poincare/float.h>
+#include "layout/string_layout.h"
+#include "layout/horizontal_layout.h"
+#include "layout/product_layout.h"
+extern "C" {
+#include <assert.h>
+#include <stdlib.h>
+}
+
+Product::Product() :
+  Function("product")
+{
+}
+
+Expression::Type Product::type() const {
+  return Type::Product;
+}
+
+Expression * Product::cloneWithDifferentOperands(Expression** newOperands,
+        int numberOfOperands, bool cloneOperands) const {
+  assert(numberOfOperands == 2);
+  assert(newOperands != nullptr);
+  Product * p = new Product();
+  p->setArgument(newOperands, numberOfOperands, cloneOperands);
+  return p;
+}
+
+float Product::approximate(Context& context) const {
+  NContext nContext = NContext(&context);
+  Symbol nSymbol = Symbol('n');
+  int start = m_args[1]->approximate(context);
+  int end = m_args[2]->approximate(context);
+  float result = 1.0f;
+  for (int i = start; i <= end; i++) {
+    Float iExpression = Float(i);
+    nContext.setExpressionForSymbolName(&iExpression, &nSymbol);
+    result = result*m_args[0]->approximate(nContext);
+  }
+  return result;
+}
+
+ExpressionLayout * Product::createLayout() const {
+  ExpressionLayout ** childrenLayouts = (ExpressionLayout **)malloc(2*sizeof(ExpressionLayout *));
+  childrenLayouts[0] = new StringLayout("n=", 2);
+  childrenLayouts[1] = m_args[1]->createLayout();
+  return new ProductLayout(new HorizontalLayout(childrenLayouts, 2), m_args[2]->createLayout(), m_args[0]->createLayout());
+}

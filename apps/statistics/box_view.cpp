e72af8f61ca945c8e48c2dc22fb20fe8576eb953
Émilie Feral <emilie.feral@numworks.com>
2017-01-26 11:25:43 +0100

    [apps/statistics] Improve box drawing
    Change-Id: I401df6524f923d2f8b2ae0b7752083c92352d4c7

--- apps/statistics/box_view.cpp
+++ apps/statistics/box_view.cpp
@@ -20,7 +20,7 @@ void BoxView::reload() {
   float pixelUpperBound = floatToPixel(Axis::Vertical, 0.2f)+1;
   float pixelLowerBound = floatToPixel(Axis::Vertical, 0.8)-1;
   float selectedValueInPixels = floatToPixel(Axis::Horizontal, calculation);
-  KDRect dirtyZone(KDRect(selectedValueInPixels-1, pixelLowerBound, 2, pixelUpperBound - pixelLowerBound));
+  KDRect dirtyZone(KDRect(selectedValueInPixels, pixelLowerBound, 2, pixelUpperBound - pixelLowerBound));
   markRectAsDirty(dirtyZone);
 }
 
@@ -44,24 +44,36 @@ void BoxView::drawRect(KDContext * ctx, KDRect rect) const {
   ctx->fillRect(rect, KDColorWhite);
   drawAxes(ctx, rect, Axis::Horizontal);
   drawLabels(ctx, rect, Axis::Horizontal, false);
+  float lowBound = 0.35f;
+  float upBound = 0.65f;
+  // Draw the main box
+  KDCoordinate firstQuartilePixels = floatToPixel(Axis::Horizontal, m_store->firstQuartile());
+  KDCoordinate thirdQuartilePixels = floatToPixel(Axis::Horizontal, m_store->thirdQuartile());
+  KDCoordinate lowBoundPixel = floatToPixel(Axis::Vertical, upBound);
+  KDCoordinate upBoundPixel = floatToPixel(Axis::Vertical, lowBound);
+  ctx->fillRect(KDRect(firstQuartilePixels, lowBoundPixel, thirdQuartilePixels - firstQuartilePixels,
+    upBoundPixel-lowBoundPixel), Palette::GreyWhite);
+  // Add 'shadows' to the box
+  ctx->fillRect(KDRect(firstQuartilePixels+3, lowBoundPixel, 1, upBoundPixel-lowBoundPixel), Palette::GreyMiddle);
+  ctx->fillRect(KDRect(firstQuartilePixels+2, lowBoundPixel, 1, upBoundPixel-lowBoundPixel-1), Palette::GreyBright);
+  ctx->fillRect(KDRect(firstQuartilePixels+2, upBoundPixel-1, thirdQuartilePixels-firstQuartilePixels-2, 1), Palette::GreyBright);
+  ctx->fillRect(KDRect(firstQuartilePixels+3, upBoundPixel, thirdQuartilePixels-firstQuartilePixels-3, 1), Palette::GreyMiddle);
+
   float calculations[5] = {m_store->minValue(), m_store->firstQuartile(), m_store->median(), m_store->thirdQuartile(), m_store->maxValue()};
-  float lowBounds[5] = {0.4f, 0.2f, 0.2f, 0.2f, 0.4f};
-  float upBounds[5] = {0.6f, 0.8f, 0.8f, 0.8f, 0.6f};
-  /* We first draw all the vertical lines of the box and then recolor the
+  /* We then draw all the vertical lines of the box and then recolor the
    * the selected quantile (if there is one). As two quantiles can have the same
    * value, we cannot choose line colors and then color only once the vertical
    * lines. This solution could hide the highlighed line by coloring the next
    * quantile if it has the same value. */
   for (int k = 0; k < 5; k++) {
-    drawSegment(ctx, rect, Axis::Vertical, calculations[k], lowBounds[k], upBounds[k], KDColorBlack);
+    drawSegment(ctx, rect, Axis::Vertical, calculations[k], lowBound, upBound, Palette::Select, 2);
   }
   if (isMainViewSelected()) {
-    drawSegment(ctx, rect, Axis::Vertical, calculations[(int)m_selectedQuantile], lowBounds[(int)m_selectedQuantile], upBounds[(int)m_selectedQuantile], KDColorRed);
+    drawSegment(ctx, rect, Axis::Vertical, calculations[(int)m_selectedQuantile], lowBound, upBound, Palette::YellowOne, 2);
   }
-  drawSegment(ctx, rect, Axis::Horizontal, 0.5f, m_store->minValue(), m_store->firstQuartile(), KDColorBlack);
-  drawSegment(ctx, rect, Axis::Horizontal, 0.5f, m_store->thirdQuartile(), m_store->maxValue(), KDColorBlack);
-  drawSegment(ctx, rect, Axis::Horizontal, 0.2f, m_store->firstQuartile(), m_store->thirdQuartile(), KDColorBlack);
-  drawSegment(ctx, rect, Axis::Horizontal, 0.8f, m_store->firstQuartile(), m_store->thirdQuartile(), KDColorBlack);
+  // Draw the horizontal lines linking the box to the extreme bounds
+  drawSegment(ctx, rect, Axis::Horizontal, 0.5f, m_store->minValue(), m_store->firstQuartile(), Palette::GreyDark);
+  drawSegment(ctx, rect, Axis::Horizontal, 0.5f, m_store->thirdQuartile(), m_store->maxValue(), Palette::GreyDark);
 }
 
 char * BoxView::label(Axis axis, int index) const {

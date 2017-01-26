740278ccadfeaaf8a6232a49f485f7cd35eff12d
Émilie Feral <emilie.feral@numworks.com>
2017-01-26 16:41:13 +0100

    [poincare] Make product layout inherits from symbol layout
    Change-Id: I5a34ab7e9b0d05762d9bb31c2cc5d19f48a07296

--- poincare/src/layout/product_layout.h
+++ poincare/src/layout/product_layout.h
@@ -1,27 +1,15 @@
 #ifndef POINCARE_PRODUCT_LAYOUT_H
 #define POINCARE_PRODUCT_LAYOUT_H
 
-#include <poincare/expression.h>
-#include <poincare/expression_layout.h>
+#include "symbol_layout.h"
 
-class ProductLayout : public ExpressionLayout {
+class ProductLayout : public SymbolLayout {
 public:
-  ProductLayout(ExpressionLayout * lowerBoundLayout, ExpressionLayout * upperBoundLayout, ExpressionLayout * argumentLayout);
-  ~ProductLayout();
+  using SymbolLayout::SymbolLayout;
 protected:
   void render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) override;
-  KDSize computeSize() override;
-  ExpressionLayout * child(uint16_t index) override;
-  KDPoint positionOfChild(ExpressionLayout * child) override;
 private:
-  constexpr static KDCoordinate k_symbolHeight = 15;
-  constexpr static KDCoordinate k_symbolWidth = 9;
-  constexpr static KDCoordinate k_boundHeightMargin = 2;
-  constexpr static KDCoordinate k_argumentWidthMargin = 2;
   constexpr static KDCoordinate k_lineThickness = 1;
-  ExpressionLayout * m_lowerBoundLayout;
-  ExpressionLayout * m_upperBoundLayout;
-  ExpressionLayout * m_argumentLayout;
 };
 
 #endif

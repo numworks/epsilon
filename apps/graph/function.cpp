59553e69e0e7426d171e76b584a97b1ea4b0c946
Émilie Feral <emilie.feral@numworks.com>
2017-02-06 15:42:55 +0100

    [apps/shared] Move function model in shared to be used by sequences
    Change-Id: I2fbd4136b8352497da30c8c959cd0f0b25d36642

--- apps/graph/function.cpp
+++ apps/graph/function.cpp
@@ -1,95 +1,31 @@
 #include "function.h"
-#include <string.h>
-#include <math.h>
-
-using namespace Poincare;
 
 namespace Graph {
 
 Function::Function(const char * text, KDColor color) :
-  m_text{0},
-  m_name(text),
-  m_color(color),
-  m_expression(nullptr),
-  m_layout(nullptr),
-  m_active(true),
+  Shared::Function(text, color),
   m_displayDerivative(false)
 {
-}
-
-void Function::setContent(const char * c) {
-  strlcpy(m_text, c, sizeof(m_text));
-  if (m_expression != nullptr) {
-    delete m_expression;
-  }
-  m_expression = Expression::parse(m_text);
-  if (m_layout != nullptr) {
-    delete m_layout;
-  }
-  m_layout = nullptr;
-  if (m_expression) {
-    m_layout = expression()->createLayout();
-  }
-}
-
-void Function::setColor(KDColor color) {
-  m_color = color;
-}
-
-Function::~Function() {
-  if (m_layout != nullptr) {
-    delete m_layout;
-  }
-  if (m_expression != nullptr) {
-    delete m_expression;
-  }
-}
-
-const char * Function::text() {
-  return m_text;
-}
-
-const char * Function::name() {
-  return m_name;
-}
-
-Expression * Function::expression() {
-  return m_expression;
-}
-
-ExpressionLayout * Function::layout() {
-  return m_layout;
-}
-
-bool Function::isActive() {
-  return m_active;
 }
 
 bool Function::displayDerivative() {
   return m_displayDerivative;
 }
 
-void Function::setActive(bool active) {
-  m_active = active;
-}
-
 void Function::setDisplayDerivative(bool display) {
   m_displayDerivative = display;
 }
 
-float Function::evaluateAtAbscissa(float x, Context * context, Expression::AngleUnit angleUnit) const {
-  Symbol xSymbol = Symbol('x');
-  Complex e = Complex(x);
-  context->setExpressionForSymbolName(&e, &xSymbol);
-  return m_expression->approximate(*context, angleUnit);
-}
-
-float Function::approximateDerivative(float x, Context * context, Expression::AngleUnit angleUnit) const {
-  Complex abscissa = Complex(x);
-  Expression * args[2] = {m_expression, &abscissa};
-  Derivative derivative = Derivative();
+float Function::approximateDerivative(float x, Poincare::Context * context, Poincare::Expression::AngleUnit angleUnit) const {
+  Poincare::Complex abscissa = Poincare::Complex(x);
+  Poincare::Expression * args[2] = {m_expression, &abscissa};
+  Poincare::Derivative derivative = Poincare::Derivative();
   derivative.setArgument(args, 2, true);
   return derivative.approximate(*context, angleUnit);
+}
+
+char Function::symbol() const {
+  return 'x';
 }
 
 }
--- apps/graph/function.h
+++ apps/graph/function.h
@@ -1,38 +1,20 @@
 #ifndef GRAPH_FUNCTION_H
 #define GRAPH_FUNCTION_H
 
-#include <poincare.h>
-#include <kandinsky.h>
+#include "../shared/function.h"
 
 namespace Graph {
 
-class Function {
+class Function : public Shared::Function {
 public:
   static constexpr const char * Parameter = "(x)";
-
+  using Shared::Function::Function;
   Function(const char * text = nullptr, KDColor color = KDColorBlack);
-  ~Function(); // Delete expression and layout, if needed
-  const char * text();
-  const char * name();
-  KDColor color() const { return m_color; }
-  Poincare::Expression * expression();
-  Poincare::ExpressionLayout * layout();
-  bool isActive();
-  void setActive(bool active);
   bool displayDerivative();
   void setDisplayDerivative(bool display);
-  void setContent(const char * c);
-  void setColor(KDColor m_color);
-  float evaluateAtAbscissa(float x, Poincare::Context * context, Poincare::Expression::AngleUnit angleUnit) const;
   float approximateDerivative(float x, Poincare::Context * context, Poincare::Expression::AngleUnit angleUnit) const;
 private:
-  constexpr static int k_bodyLength = 255;
-  char m_text[k_bodyLength];
-  const char * m_name;
-  KDColor m_color;
-  Poincare::Expression * m_expression;
-  Poincare::ExpressionLayout * m_layout;
-  bool m_active;
+  char symbol() const override;
   bool m_displayDerivative;
 };
 

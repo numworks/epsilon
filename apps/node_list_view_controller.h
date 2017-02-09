82f29ea2e41c1fc20f13a2300339950238ba9ec8
Émilie Feral <emilie.feral@numworks.com>
2017-02-09 15:29:42 +0100

    [apps] Merge node navigation controller and box controller
    Change-Id: Ie8163568dc4cc30932a93b133c433f62be2cff43

--- apps/node_list_view_controller.h
+++ apps/node_list_view_controller.h
@@ -3,6 +3,7 @@
 
 #include <escher.h>
 #include "node.h"
+#include "toolbox_leaf_cell.h"
 
 /* m_nodeModel points at the node of the tree (describing the whole model)
  * where we are located. It enables to know which rows are leaves and which are
@@ -12,7 +13,7 @@ class NodeNavigationController;
 
 class NodeListViewController : public ViewController, public ListViewDataSource {
 public:
-  NodeListViewController(NodeNavigationController * parent);
+  NodeListViewController(Responder * parentResponder);
   View * view() override;
   const char * title() const override;
   void didBecomeFirstResponder() override;
@@ -33,10 +34,12 @@ public:
   void setVerticalScroll(KDCoordinate verticalScroll);
   KDCoordinate verticalScroll();
   void deselectTable();
-  constexpr static int k_maxNumberOfDisplayedRows = 6; //240/40
 private:
   constexpr static KDCoordinate k_nodeRowHeight = 40;
-  NodeNavigationController * m_nodeNavigationController;
+  constexpr static KDCoordinate k_leafRowHeight = 40;
+  constexpr static int k_maxNumberOfDisplayedRows = 6; //240/40
+  ToolboxLeafCell m_leafCells[k_maxNumberOfDisplayedRows];
+  ChevronMenuListCell m_nodeCells[k_maxNumberOfDisplayedRows];
   SelectableTableView m_selectableTableView;
   Node * m_nodeModel;
   int m_firstSelectedRow;

cf6956530518db6e5bd13198202a6b865501a5c0
Émilie Feral <emilie.feral@numworks.com>
2017-02-22 09:54:51 +0100

    [apps/sequence/graph] Create a class banner view
    Change-Id: Ie619eab80a150e8a2c42a2861d42b95357dd848c

---
+++ apps/sequence/graph/banner_view.cpp
@@ -0,0 +1,20 @@
+#include "banner_view.h"
+
+namespace Sequence {
+
+BannerView::BannerView() :
+  m_abscissaView(KDText::FontSize::Small, 0.5f, 0.5f, KDColorBlack, Palette::GreyMiddle),
+  m_sequenceView(KDText::FontSize::Small, 0.5f, 0.5f, KDColorBlack, Palette::GreyMiddle)
+{
+}
+
+int BannerView::numberOfSubviews() const {
+  return 2;
+}
+
+TextView * BannerView::textViewAtIndex(int i) {
+  TextView * views[2] = {&m_abscissaView, &m_sequenceView};
+  return views[i];
+}
+
+}

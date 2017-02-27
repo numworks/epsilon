8f0e4408382902b4a7d0388b740765c83b05bad5
Émilie Feral <emilie.feral@numworks.com>
2017-02-27 10:54:19 +0100

    [escher] Create a class key view
    Change-Id: I7a8e02996ceac8bbdb9d9d214f2b3b70b1fc6cf0

---
+++ escher/include/escher/key_view.h
@@ -0,0 +1,26 @@
+#ifndef ESCHER_KEY_VIEW_H
+#define ESCHER_KEY_VIEW_H
+
+#include <escher/view.h>
+
+class KeyView : public View {
+public:
+  enum class Type {
+    Up,
+    Down,
+    Left,
+    Right,
+    Plus,
+    Minus
+  };
+  KeyView(Type type);
+  void drawRect(KDContext * ctx, KDRect rect) const override;
+  KDSize minimalSizeForOptimalDisplay() override;
+  constexpr static KDCoordinate k_keySize = 8;
+private:
+  const uint8_t * mask() const;
+  Type m_type;
+};
+
+#endif
+

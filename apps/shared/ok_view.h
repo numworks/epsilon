3252ff90785458e093e464814abbf2b92e608c3e
Émilie Feral <emilie.feral@numworks.com>
2017-02-27 12:59:12 +0100

    [apps/shared] Create an OK view
    Change-Id: I516e4df9ef6f37a356ee42c2c9d34582905ab4cb

---
+++ apps/shared/ok_view.h
@@ -0,0 +1,19 @@
+#ifndef SHARED_OK_VIEW_H
+#define SHARED_OK_VIEW_H
+
+#include <escher.h>
+
+namespace Shared {
+
+class OkView : public View {
+public:
+  using View::View;
+  void drawRect(KDContext * ctx, KDRect rect) const override;
+  KDSize minimalSizeForOptimalDisplay() override;
+  constexpr static KDCoordinate k_okSize = 20;
+};
+
+}
+
+#endif
+

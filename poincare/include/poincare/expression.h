7a030b11744f7053450d015d34e17df8c4bb454f
Émilie Feral <emilie.feral@numworks.com>
2017-03-09 10:38:33 +0100

    [poincare] Parse permute(n,k)
    Change-Id: I0c435d9cebaf3f2b9a7b4e5b4944071aecda7a8d

--- poincare/include/poincare/expression.h
+++ poincare/include/poincare/expression.h
@@ -36,6 +36,7 @@ public:
     Opposite,
     Fraction,
     Parenthesis,
+    PermuteCoefficient,
     Power,
     Product,
     ReelPart,
---
+++ poincare/src/permute_coefficient.cpp
@@ -0,0 +1,44 @@
+#include <poincare/permute_coefficient.h>
+#include <poincare/complex.h>
+
+extern "C" {
+#include <assert.h>
+#include <math.h>
+}
+
+namespace Poincare {
+
+PermuteCoefficient::PermuteCoefficient() :
+  Function("permute")
+{
+}
+
+Expression::Type PermuteCoefficient::type() const {
+  return Type::PermuteCoefficient;
+}
+
+Expression * PermuteCoefficient::cloneWithDifferentOperands(Expression** newOperands,
+        int numberOfOperands, bool cloneOperands) const {
+  assert(numberOfOperands == 1);
+  assert(newOperands != nullptr);
+  PermuteCoefficient * pc = new PermuteCoefficient();
+  pc->setArgument(newOperands, numberOfOperands, cloneOperands);
+  return pc;
+}
+
+float PermuteCoefficient::privateApproximate(Context& context, AngleUnit angleUnit) const {
+  assert(angleUnit != AngleUnit::Default);
+  float n = m_args[0]->approximate(context, angleUnit);
+  float k = m_args[1]->approximate(context, angleUnit);
+  if (isnan(n) || isnan(k) || n != (int)n || k != (int)k) {
+    return NAN;
+  }
+  float result = 1.0f;
+  for (int i = (int)n-(int)k+1; i <= (int)n; i++) {
+    result *= i;
+  }
+  return roundf(result);
+}
+
+}
+

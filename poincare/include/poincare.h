db4dab9731786dc55e130dc1c965c3bb4572116a
Émilie Feral <emilie.feral@numworks.com>
2017-03-09 16:55:56 +0100

    [poincare] Parse acosh, asinh, atanh
    Change-Id: I4c0f965e9bb1738c017964b8664c7867e41c5339

--- poincare/include/poincare.h
+++ poincare/include/poincare.h
@@ -24,6 +24,9 @@
 #include <poincare/function.h>
 #include <poincare/global_context.h>
 #include <poincare/great_common_divisor.h>
+#include <poincare/hyperbolic_arc_cosine.h>
+#include <poincare/hyperbolic_arc_sine.h>
+#include <poincare/hyperbolic_arc_tangent.h>
 #include <poincare/hyperbolic_cosine.h>
 #include <poincare/hyperbolic_sine.h>
 #include <poincare/hyperbolic_tangent.h>
---
+++ poincare/src/hyperbolic_arc_sine.cpp
@@ -0,0 +1,33 @@
+#include <poincare/hyperbolic_arc_sine.h>
+extern "C" {
+#include <assert.h>
+#include <math.h>
+}
+
+namespace Poincare {
+
+HyperbolicArcSine::HyperbolicArcSine() :
+  Function("asinh")
+{
+}
+
+Expression::Type HyperbolicArcSine::type() const {
+  return Type::HyperbolicArcSine;
+}
+
+Expression * HyperbolicArcSine::cloneWithDifferentOperands(Expression** newOperands,
+        int numberOfOperands, bool cloneOperands) const {
+  assert(numberOfOperands == 1);
+  assert(newOperands != nullptr);
+  HyperbolicArcSine * s = new HyperbolicArcSine();
+  s->setArgument(newOperands, numberOfOperands, cloneOperands);
+  return s;
+}
+
+float HyperbolicArcSine::privateApproximate(Context& context, AngleUnit angleUnit) const {
+  assert(angleUnit != AngleUnit::Default);
+  float x = m_args[0]->approximate(context, angleUnit);
+  return logf(x+sqrtf(x*x+1.0f));
+}
+
+}

d3ad0ddbdd9f11547d05dfeb8e62f53c93c2b9fc
Romain Goyet <romain.goyet@numworks.com>
2017-03-17 15:31:42 +0100

    [ion] Events from stdin
    Change-Id: I3ad9900b44eb1907f02c619227894ffcdc59f459

---
+++ ion/src/shared/events_stdin.cpp
@@ -0,0 +1,17 @@
+#include <ion/events.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Use this implementation of Ion::Events::getEvent to simulate pre-recorded key
+ * presses. It streams event recorded to a file and played back from standard
+ * input. */
+
+Ion::Events::Event Ion::Events::getEvent(int * timeout) {
+  int c = getchar();
+  msleep(100);
+  if (c == EOF) {
+    exit(0);
+  } else {
+    return Ion::Events::Event(c);
+  }
+}

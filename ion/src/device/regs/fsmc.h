fad88b5da7b59a70bc8d73c5ba7e99086fb20d5c
Émilie Feral <emilie.feral@numworks.com>
2017-03-31 15:14:24 +0200

    [ion] In fsmc regs, enable different reading and writing timings
    Change-Id: I3c387889d6716fd591e0455b44683ef579a4824a

--- ion/src/device/regs/fsmc.h
+++ ion/src/device/regs/fsmc.h
@@ -15,9 +15,21 @@ public:
     REGS_BOOL_FIELD(MUXEN, 1);
     REGS_TYPE_FIELD(MWID, 5, 4);
     REGS_BOOL_FIELD(WREN, 12);
+    REGS_BOOL_FIELD(EXTMOD, 14);
   };
 
   class BTR : Register32 {
+  public:
+    REGS_FIELD(ADDSET, uint8_t, 3, 0);
+    REGS_FIELD(ADDHLD, uint8_t, 7, 4);
+    REGS_FIELD(DATAST, uint8_t, 15, 8);
+    REGS_FIELD(BUSTURN, uint8_t, 19, 16);
+    REGS_FIELD(CLKDIV, uint8_t, 23, 20);
+    REGS_FIELD(DATLAT, uint8_t, 27, 24);
+    REGS_FIELD(ACCMOD, uint8_t, 29, 28);
+  };
+
+  class BWTR : Register32 {
   public:
     REGS_FIELD(ADDSET, uint8_t, 3, 0);
     REGS_FIELD(ADDHLD, uint8_t, 7, 4);
@@ -34,6 +46,9 @@ public:
   }
   volatile BTR * BTR(int index) const {
     return (class BTR *)(Base() + 4 + 8*(index-1));
+  }
+  volatile BWTR * BWTR(int index) const {
+    return (class BWTR *)(Base() + 0x104 + 8*(index-1));
   }
 private:
   constexpr uint32_t Base() const {

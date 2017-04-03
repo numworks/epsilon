26b8e1490147ca4ca6cd68abdd4bee92e8d47f3d
Émilie Feral <emilie.feral@numworks.com>
2017-04-03 14:04:13 +0200

    [apps/hardware_test] Add LED test
    Change-Id: I998074b74357f76a624962bfa274ebf7a10ac0fc

--- apps/hardware_test/keyboard_controller.cpp
+++ apps/hardware_test/keyboard_controller.cpp
@@ -7,7 +7,8 @@ namespace HardwareTest {
 
 KeyboardController::KeyboardController(Responder * parentResponder) :
   ViewController(parentResponder),
-  m_view(KeyboardView())
+  m_view(KeyboardView()),
+  m_color(KDColorBlack)
 {
 }
 
@@ -16,6 +17,8 @@ View * KeyboardController::view() {
 }
 
 bool KeyboardController::handleEvent(Ion::Events::Event event) {
+  Ion::LED::setColor(m_color);
+  m_color = nextColor(m_color);
   m_view.updateBatteryState(Ion::Battery::voltage(), Ion::Battery::isCharging());
   if (event != Ion::Events::Event::PlainKey(m_view.testedKey()) && event != Ion::Events::Event::ShiftKey(m_view.testedKey()) && event != Ion::Events::Event::AlphaKey(m_view.testedKey()) && event != Ion::Events::Event::ShiftAlphaKey(m_view.testedKey())) {
     m_view.setDefectiveKey(m_view.testedKey());
@@ -25,8 +28,28 @@ bool KeyboardController::handleEvent(Ion::Events::Event event) {
 }
 
 void KeyboardController::viewWillAppear() {
+  m_color = KDColorBlack;
   m_view.resetTestedKey();
   m_view.updateBatteryState(Ion::Battery::voltage(), Ion::Battery::isCharging());
+}
+
+KDColor KeyboardController::nextColor(KDColor color) {
+  if (color == KDColorBlack) {
+    return KDColorWhite;
+  }
+  if (color == KDColorWhite) {
+    return KDColorRed;
+  }
+  if (color == KDColorRed) {
+    return KDColorBlue;
+  }
+  if (color == KDColorBlue) {
+    return KDColorGreen;
+  }
+  if (color == KDColorGreen) {
+    return KDColorBlack;
+  }
+  return KDColorBlack;
 }
 
 }

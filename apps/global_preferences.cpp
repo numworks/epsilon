dadeb98d99d63e40dd3e09c6f1353802476ce6c5
Émilie Feral <emilie.feral@numworks.com>
2017-04-07 11:37:28 +0200

    [apps] Add exam mode in global preferences
    Change-Id: I70cd1585c07f73f29212aaf11f82005653c11f67

--- apps/global_preferences.cpp
+++ apps/global_preferences.cpp
@@ -3,7 +3,8 @@
 static GlobalPreferences s_globalPreferences;
 
 GlobalPreferences::GlobalPreferences() :
-  m_language(I18n::Language::French)
+  m_language(I18n::Language::French),
+  m_examMode(ExamMode::Desactivate)
 {
 }
 
@@ -18,5 +19,15 @@ I18n::Language GlobalPreferences::language() const {
 void GlobalPreferences::setLanguage(I18n::Language language) {
   if (language != m_language) {
     m_language = language;
+  }
+}
+
+GlobalPreferences::ExamMode GlobalPreferences::examMode() const {
+  return m_examMode;
+}
+
+void GlobalPreferences::setExamMode(ExamMode examMode) {
+  if (examMode != m_examMode) {
+    m_examMode = examMode;
   }
 }

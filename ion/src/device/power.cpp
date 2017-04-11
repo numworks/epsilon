ea2c3bdb32b2fb032a37c74648f5cf09c6cd35e0
Émilie Feral <emilie.feral@numworks.com>
2017-04-11 16:33:15 +0200

    [ion] Enable LED to switch on during suspend
    Change-Id: I9c028803e21fad0f07500be482597f5bfcb0fc99

--- ion/src/device/power.cpp
+++ ion/src/device/power.cpp
@@ -1,18 +1,18 @@
 #include <ion.h>
 #include "regs/regs.h"
+#include "battery.h"
 #include "device.h"
 #include "keyboard.h"
 #include "led.h"
 #include "display.h"
+#include "usb.h"
 
 void Ion::Power::suspend() {
-  Device::shutdownPeripherals(false);
+  Device::shutdownPeripherals(USB::isPlugged());
 
   PWR.CR()->setLPDS(true); // Turn the regulator off. Takes longer to wake up.
   PWR.CR()->setFPDS(true); // Put the flash to sleep. Takes longer to wake up.
   CM4.SCR()->setSLEEPDEEP(true);
-
-  Keyboard::Device::generateWakeUpEventForPowerKey();
 
   Device::shutdownClocks();
 
@@ -23,9 +23,48 @@ void Ion::Power::suspend() {
    * already be on. So the safest way to make sure we actually wait for a new
    * event is to force the event flag to on (SEV instruction), use a first WFE
    * to clear it, and then a second WFE to wait for a _new_ event. */
-  asm("sev");
-  asm("wfe");
-  asm("wfe");
+
+  Device::initStandbyClock();
+
+  //TODO: optimize stand by consumption
+  while (1) {
+    Keyboard::Device::generateWakeUpEventForPowerKey();
+    Battery::Device::generateWakeUpEventForChargingState();
+    USB::Device::generateWakeUpEventForUSBPlug();
+
+    asm("sev");
+    asm("wfe");
+    msleep(1);
+    asm("wfe");
+
+    Keyboard::Device::init();
+    Keyboard::State scan = Keyboard::scan();
+    Keyboard::Device::shutdown();
+
+    // Update LEDS
+    KDColor LEDColor = KDColorBlack;
+    if (Battery::isCharging()) {
+      LEDColor = KDColorBlue;
+    } else if (Ion::USB::isPlugged()) {
+      LEDColor = KDColorGreen;
+    } else {
+      LEDColor = KDColorBlack;
+    }
+    if (LEDColor == KDColorBlack) {
+      LED::setColor(LEDColor);
+      //TODO: understand why only shutdown the LED is inefficient
+      Device::shutdownPeripherals(false);
+    } else {
+      LED::Device::init();
+      LED::setColor(LEDColor);
+    }
+
+    Ion::Keyboard::State OnlyPowerKeyDown = (1 << (uint8_t)Keyboard::Key::B2);
+    if (scan == OnlyPowerKeyDown) {
+      // Wake up
+      break;
+    }
+  }
 
   Device::initClocks();
 

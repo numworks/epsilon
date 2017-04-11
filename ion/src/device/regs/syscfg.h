73e9c1832b0aabc1cb050c8eb63cee1746009a39
Émilie Feral <emilie.feral@numworks.com>
2017-04-11 16:24:07 +0200

    [ion] In regs, create registers in EXTI and SYSCFG
    Change-Id: I571e8f87c8996300f3ac2f60c20a50b2128749b2

--- ion/src/device/regs/syscfg.h
+++ ion/src/device/regs/syscfg.h
@@ -11,9 +11,13 @@ public:
   public:
     void setEXTI(int index, GPIO gpio) volatile { setBitRange(4*index+3, 4*index, (uint32_t)gpio); }
   };
-
+  class EXTICR3 : Register32 {
+  public:
+    void setEXTI(int index, GPIO gpio) volatile { setBitRange(4*(index-8)+3, 4*(index-8), (uint32_t)gpio); }
+  };
   constexpr SYSCFG() {};
   REGS_REGISTER_AT(EXTICR1, 0x08);
+  REGS_REGISTER_AT(EXTICR3, 0x10);
 private:
   constexpr uint32_t Base() const {
     return 0x40013800;

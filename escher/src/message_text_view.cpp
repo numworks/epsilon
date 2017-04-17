647ca22eed3b9b1e2db20818a94f29f86cb542d8
Émilie Feral <emilie.feral@numworks.com>
2017-04-17 15:37:17 +0200

    [escher] Fix bug, do not forget that one language is "default"
    Change-Id: Iade90393d3256b1a1d4202faf2cf10f86650828f

--- escher/src/message_text_view.cpp
+++ escher/src/message_text_view.cpp
@@ -25,7 +25,7 @@ void MessageTextView::setMessage(I18n::Message message) {
 
 KDSize MessageTextView::minimalSizeForOptimalDisplay() const  {
   KDCoordinate width = 0;
-  for (int l = 0; l < I18n::numberOfLanguages(); l++) {
+  for (int l = 0; l < I18n::numberOfLanguages()+1; l++) {
     KDCoordinate newWidth = KDText::stringSize(I18n::translate(m_message, (I18n::Language)l), m_fontSize).width();
     width = width > newWidth ?  width : newWidth;
   }

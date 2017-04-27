351006b80539935cc01ee23723758c8e11cca3ca
Émilie Feral <emilie.feral@numworks.com>
2017-04-27 16:57:23 +0200

    [poincare] Factorize product and sum and cap their evaluation time
    Change-Id: Iea355d2239bd2f7e332fe42591119ad573d93286

---
+++ poincare/include/poincare/sequence.h
@@ -0,0 +1,24 @@
+#ifndef POINCARE_SEQUENCE_H
+#define POINCARE_SEQUENCE_H
+
+#include <poincare/function.h>
+
+namespace Poincare {
+
+class Sequence : public Function {
+public:
+  Sequence(const char * name);
+private:
+  constexpr static int k_maxNumberOfSteps = 10000;
+  float privateApproximate(Context & context, AngleUnit angleUnit) const override;
+  ExpressionLayout * privateCreateLayout(FloatDisplayMode floatDisplayMode, ComplexFormat complexFormat) const override;
+  Expression * privateEvaluate(Context& context, AngleUnit angleUnit) const override;
+  virtual float emptySequenceValue() const = 0;
+  virtual float approximateWithNextTerm(float sequence, float newTerm) const = 0;
+  virtual ExpressionLayout * createSequenceLayoutWithArgumentLayouts(ExpressionLayout * subscriptLayout, ExpressionLayout * superscriptLayout, ExpressionLayout * argumentLayout) const = 0;
+  virtual Expression * evaluateWithNextTerm(Expression ** args, Context& context, AngleUnit angleUnit) const = 0;
+};
+
+}
+
+#endif
--- poincare/src/product.cpp
+++ poincare/src/product.cpp
@@ -1,10 +1,5 @@
 #include <poincare/product.h>
-#include <poincare/symbol.h>
-#include <poincare/complex.h>
 #include <poincare/multiplication.h>
-#include <poincare/variable_context.h>
-#include "layout/string_layout.h"
-#include "layout/horizontal_layout.h"
 #include "layout/product_layout.h"
 extern "C" {
 #include <assert.h>
@@ -15,7 +10,7 @@ extern "C" {
 namespace Poincare {
 
 Product::Product() :
-  Function("product", 3)
+  Sequence("product")
 {
 }
 
@@ -31,57 +26,22 @@ Expression * Product::cloneWithDifferentOperands(Expression** newOperands,
   return p;
 }
 
-float Product::privateApproximate(Context& context, AngleUnit angleUnit) const {
-  assert(angleUnit != AngleUnit::Default);
-  VariableContext nContext = VariableContext('n', &context);
-  Symbol nSymbol = Symbol('n');
-  float start = m_args[1]->approximate(context, angleUnit);
-  float end = m_args[2]->approximate(context, angleUnit);
-  if (start != (int)start || end != (int)end) {
-    return NAN;
-  }
-  float result = 1.0f;
-  for (int i = start; i <= end; i++) {
-    Complex iExpression = Complex::Float(i);
-    nContext.setExpressionForSymbolName(&iExpression, &nSymbol);
-    result = result*m_args[0]->approximate(nContext, angleUnit);
-  }
-  return result;
+float Product::emptySequenceValue() const {
+  return 1.0f;
 }
 
-ExpressionLayout * Product::privateCreateLayout(FloatDisplayMode floatDisplayMode, ComplexFormat complexFormat) const {
-  assert(floatDisplayMode != FloatDisplayMode::Default);
-  assert(complexFormat != ComplexFormat::Default);
-  ExpressionLayout * childrenLayouts[2];
-  childrenLayouts[0] = new StringLayout("n=", 2);
-  childrenLayouts[1] = m_args[1]->createLayout(floatDisplayMode, complexFormat);
-  return new ProductLayout(new HorizontalLayout(childrenLayouts, 2), m_args[2]->createLayout(floatDisplayMode, complexFormat), m_args[0]->createLayout(floatDisplayMode, complexFormat));
+float Product::approximateWithNextTerm(float sequence, float nextTerm) const {
+  return sequence * nextTerm;
 }
 
-Expression * Product::privateEvaluate(Context& context, AngleUnit angleUnit) const {
-  assert(angleUnit != AngleUnit::Default);
-  float start = m_args[1]->approximate(context, angleUnit);
-  float end = m_args[2]->approximate(context, angleUnit);
-  if (isnan(start) || isnan(end)) {
-    return new Complex(Complex::Float(NAN));
-  }
-  VariableContext nContext = VariableContext('n', &context);
-  Symbol nSymbol = Symbol('n');
-  Expression * result = new Complex(Complex::Float(1.0f));
-  for (int i = (int)start; i <= (int)end; i++) {
-    Complex * iExpression = new Complex(Complex::Float(i));
-    nContext.setExpressionForSymbolName(iExpression, &nSymbol);
-    delete iExpression;
-    Expression * operands[2];
-    operands[0] = result;
-    operands[1] = m_args[0]->evaluate(nContext, angleUnit);
-    Expression * multiplication = new Multiplication(operands, true);
-    Expression * newResult = multiplication->evaluate(context, angleUnit);
-    delete result;
-    delete operands[1];
-    result = newResult;
-    delete multiplication;
-  }
+ExpressionLayout * Product::createSequenceLayoutWithArgumentLayouts(ExpressionLayout * subscriptLayout, ExpressionLayout * superscriptLayout, ExpressionLayout * argumentLayout) const {
+  return new ProductLayout(subscriptLayout, superscriptLayout, argumentLayout);
+}
+
+Expression * Product::evaluateWithNextTerm(Expression ** args, Context& context, AngleUnit angleUnit) const {
+  Expression * multiplication = new Multiplication(args, true);
+  Expression * result = multiplication->evaluate(context, angleUnit);
+  delete multiplication;
   return result;
 }
 

6c8fb277b06a84706fe7d3a9f65c89bf0bcb3d98
Romain Goyet <romain.goyet@numworks.com>
2017-05-01 18:31:05 +0200

    [ion] Add an event printer tool
    Change-Id: Ib87737f30fe9818cd5d0a61e9bdaa8ee009052ae

---
+++ ion/src/shared/tools/event_printer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cassert>
+#include <ion/events.h>
+
+int main(int argc, char * argv[]) {
+  unsigned char c = 0;
+  while (std::cin >> c) {
+    Ion::Events::Event event(c);
+    if (event.isValid()) {
+      if (event.name() == nullptr) {
+        std::cerr << "*** UNNAMED(" << (int)c << ") ***" << std::endl;
+      } else {
+        std::cout << event.name() << std::endl;
+      }
+    } else {
+      std::cerr << "*** INVALID(" << (int)c << ") ***" << std::endl;
+    }
+  }
+}

2474d3c73d4535381f38ad3b6b6ca053a6eb247c
Romain Goyet <romain.goyet@numworks.com>
2017-05-01 19:01:15 +0200

    [ion] Add a test for the events
    Change-Id: Ia15b137b151831a7b4140c176f41621dad1f48ef

--- ion/include/ion/keyboard.h
+++ ion/include/ion/keyboard.h
@@ -20,6 +20,15 @@ enum class Key : uint8_t {
   I1=48, I2=49, I3=50, I4=51, I5=52, // I6=53,
 };
 
+constexpr Key ValidKeys[] = {
+  Key::A1, Key::A2, Key::A3, Key::A4, Key::A5, Key::A6, Key::B1, Key::B2,
+  Key::C1, Key::C2, Key::C3, Key::C4, Key::C5, Key::C6, Key::D1, Key::D2,
+  Key::D3, Key::D4, Key::D5, Key::D6, Key::E1, Key::E2, Key::E3, Key::E4,
+  Key::E5, Key::E6, Key::F1, Key::F2, Key::F3, Key::F4, Key::F5, Key::G1,
+  Key::G2, Key::G3, Key::G4, Key::G5, Key::H1, Key::H2, Key::H3, Key::H4,
+  Key::H5, Key::I1, Key::I2, Key::I3, Key::I4, Key::I5,
+};
+
 constexpr int NumberOfKeys  = 54;
 typedef uint64_t State;
 

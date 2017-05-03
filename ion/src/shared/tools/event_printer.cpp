d3a9d8b3cdfcbd5bea54b29b181ae9e7e774f2e4
Romain Goyet <romain.goyet@numworks.com>
2017-05-03 17:06:12 +0200

    [ion] Fix tools
    Change-Id: I97b3770993adfd1b98b2eee24f9d3005842d8093

--- ion/src/shared/tools/event_printer.cpp
+++ ion/src/shared/tools/event_printer.cpp
@@ -6,7 +6,7 @@ int main(int argc, char * argv[]) {
   unsigned char c = 0;
   while (std::cin >> c) {
     Ion::Events::Event event(c);
-    if (event.isValid()) {
+    if (event.isDefined()) {
       if (event.name() == nullptr) {
         std::cerr << "*** UNNAMED(" << (int)c << ") ***" << std::endl;
       } else {

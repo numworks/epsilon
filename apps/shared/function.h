e5dd0d8d7f8d806d6950ca3fc0979c1e5bab781a
Émilie Feral <emilie.feral@numworks.com>
2017-05-10 11:33:25 +0200

    [apps/sequence] Fix error in copy assignment of sequence
    Change-Id: If27924c6001a8d7011371336ffbaf012f9d8bbc9

--- apps/shared/function.h
+++ apps/shared/function.h
@@ -15,7 +15,7 @@ public:
   Function& operator=(Function&& other) = delete;
   Function(const Function& other) = delete;
   Function(Function&& other) = delete;
-  const char * text();
+  const char * text() const;
   const char * name() const;
   KDColor color() const { return m_color; }
   Poincare::Expression * expression();

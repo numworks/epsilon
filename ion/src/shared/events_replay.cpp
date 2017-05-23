20b1845fb93fbd2fd5b8e268e38a7ffc88f8fa1b
Romain Goyet <romain.goyet@numworks.com>
2017-05-23 16:54:39 +0200

    [ion] Fix the events_replay generator
    Change-Id: I8a638f31c64ca74f277bada4c94323b4d4dd2b29

--- ion/src/shared/events_replay.cpp
+++ ion/src/shared/events_replay.cpp
@@ -4,16 +4,20 @@
  * presses. This implementation doesn't wait at all, which can be useful when
  * profiling code execution. */
 
-const int loopLength = 2;
+const int loopLength = 12;
 
-const Ion::Events::Event sequence[] = {
-  Ion::Events::Event::F1,
-  Ion::Events::Event::DOWN_ARROW,
-  Ion::Events::Event::DOWN_ARROW,
-  Ion::Events::Event::UP_ARROW,
+using namespace Ion::Events;
+
+const Event sequence[] = {
+  Right,
+  Right,
+  Right,
+  OK,
+  Down, Down, Down, Down, Down, Down,
+  Up, Up, Up, Up, Up, Up
 };
 
-Ion::Events::Event Ion::Events::getEvent() {
+Event Ion::Events::getEvent(int * timeout) {
   static int i = 0;
   int sequenceLength = sizeof(sequence)/sizeof(sequence[0]);
   if (i == sequenceLength) {

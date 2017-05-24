53f4fd0b0680d4412c92fb7b964a6756c82f8322
Émilie Feral <emilie.feral@numworks.com>
2017-05-24 12:02:05 +0200

    [escher] Reset superview when a modal view is dismissed
    Change-Id: I2d3ee97f7ade486f5cf8ba16851866d60dd6cdb6

--- escher/include/escher/view.h
+++ escher/include/escher/view.h
@@ -30,6 +30,7 @@ public:
   View(View&& other) = delete;
   View& operator=(const View& other) = delete;
   View& operator=(View&& other) = delete;
+  void resetSuperview();
   /* The drawRect method should be implemented by each View subclass. In a
    * typical drawRect implementation, a subclass will make drawing calls to the
    * Kandinsky library using the provided context. */

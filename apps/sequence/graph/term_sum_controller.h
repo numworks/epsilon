9ea68ed938d115a9b572a22535d47a2a573e1fd9
Émilie Feral <emilie.feral@numworks.com>
2017-06-01 14:36:59 +0200

    [apps/sequence] Fix bug in term sum controller
    Change-Id: Ia304aa891d4056a271258361ac867f0490c6cf87

--- apps/sequence/graph/term_sum_controller.h
+++ apps/sequence/graph/term_sum_controller.h
@@ -21,7 +21,7 @@ public:
 private:
   constexpr static float k_cursorTopMarginRatio = 0.07f;   // (cursorHeight/2)/graphViewHeight
   constexpr static float k_cursorRightMarginRatio = 0.04f; // (cursorWidth/2)/graphViewWidth
-  constexpr static float k_cursorBottomMarginRatio = 0.15f; // (cursorHeight/2+bannerHeigh)/graphViewHeight
+  constexpr static float k_cursorBottomMarginRatio = 0.28f; // (cursorHeight/2+bannerHeigh)/graphViewHeight
   constexpr static float k_cursorLeftMarginRatio = 0.04f;  // (cursorWidth/2)/graphViewWidth
   class ContentView : public View {
   public:

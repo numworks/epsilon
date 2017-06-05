a6cbd1cb937101170522e64da7f15036d34d9839
Émilie Feral <emilie.feral@numworks.com>
2017-06-05 15:44:30 +0200

    [apps/regression] Handle edge cases in regression
    Change-Id: Id4a88e19a44ba054db35025173ba0344a989d15e

--- apps/regression/store.h
+++ apps/regression/store.h
@@ -37,9 +37,9 @@ public:
   float correlationCoefficient();
   float squaredCorrelationCoefficient();
 private:
-  constexpr static float k_displayTopMarginRatio = 0.11f;
+  constexpr static float k_displayTopMarginRatio = 0.12f;
   constexpr static float k_displayRightMarginRatio = 0.05f;
-  constexpr static float k_displayBottomMarginRatio = 0.4f;
+  constexpr static float k_displayBottomMarginRatio = 0.5f;
   constexpr static float k_displayLeftMarginRatio = 0.05f;
   float maxValueOfColumn(int i);
   float minValueOfColumn(int i);

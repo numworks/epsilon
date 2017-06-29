e97e2607adb2c749a39c580d1a557f8da7165f83
Émilie Feral <emilie.feral@numworks.com>
2017-06-29 15:12:28 +0200

    [apps/hardware] Add schematic instruction for USB plugging
    Change-Id: I77a04242128d8f3d1aa322f468b6bea30fead870

--- apps/hardware_test/usb_test_controller.h
+++ apps/hardware_test/usb_test_controller.h
@@ -3,6 +3,7 @@
 
 #include <escher.h>
 #include "reset_controller.h"
+#include "arrow_view.h"
 
 namespace HardwareTest {
 
@@ -12,16 +13,28 @@ public:
   View * view() override;
   bool handleEvent(Ion::Events::Event event) override;
   void viewWillAppear() override;
+  void didBecomeFirstResponder() override;
 private:
   class ContentView : public SolidColorView {
   public:
     ContentView();
     BufferTextView * USBTextView();
+    ArrowView * arrowView();
   private:
+    class USBView : public View {
+    public:
+      void drawRect(KDContext * ctx, KDRect rect) const override;
+      constexpr static KDCoordinate k_USBHoleWidth = 40;
+      constexpr static KDCoordinate k_USBHoleHeight = 20;
+      constexpr static KDCoordinate k_marginHeight = 50;
+    };
+    constexpr static KDCoordinate k_textHeight = 50;
     void layoutSubviews() override;
     int numberOfSubviews() const override;
     View * subviewAtIndex(int index) override;
     BufferTextView m_USBTextView;
+    USBView m_USBView;
+    ArrowView m_arrowView;
   };
   constexpr static const char * k_USBPlugText = "PLUG USB";
   constexpr static const char * k_USBUnplugText = "OK, UNPLUG USB";

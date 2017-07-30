82c6cfa5b12802a86e5f9db014b28e333bb9282c
Romain Goyet <romain@goyet.com>
2017-07-30 22:13:13 +0200

    [liba] Add strchr to string.h
    Change-Id: I6651daa030ad6dc5e567bb78f2f22d48f07524aa

--- liba/include/string.h
+++ liba/include/string.h
@@ -10,9 +10,11 @@ int memcmp(const void * s1, const void * s2, size_t n);
 void * memcpy(void * dst, const void * src, size_t n);
 void * memmove(void * dst, const void * src, size_t n);
 void * memset(void * b, int c, size_t len);
-size_t strlen(const char * s);
-int strcmp(const char *s1, const char *s2);
+
+char * strchr(const char * s, int c);
+int strcmp(const char * s1, const char * s2);
 size_t strlcpy(char * dst, const char * src, size_t len);
+size_t strlen(const char * s);
 
 LIBA_END_DECLS
 

3ef709502e47d7f350c021dd33bbeac83c5e5f5c
Romain Goyet <romain@goyet.com>
2017-07-30 22:12:42 +0200

    [liba] Add memcmp to strings.h
    Change-Id: Ied4281e588d423e2b00dced10018a690455544d5

--- liba/include/string.h
+++ liba/include/string.h
@@ -6,6 +6,7 @@
 
 LIBA_BEGIN_DECLS
 
+int memcmp(const void * s1, const void * s2, size_t n);
 void * memcpy(void * dst, const void * src, size_t n);
 void * memmove(void * dst, const void * src, size_t n);
 void * memset(void * b, int c, size_t len);

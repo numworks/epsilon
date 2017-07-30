e2839cb8087ff6f7bb416a70545b8fd0085defc6
Romain Goyet <romain@goyet.com>
2017-07-30 12:36:01 +0200

    [liba] Define ssize_t and offsetof
    Change-Id: Icbf546cd09f4e3c3e849f830bb3c60ffa3c13b6c

--- liba/include/stddef.h
+++ liba/include/stddef.h
@@ -3,10 +3,9 @@
 
 #define NULL 0
 
-/* C99 says that size_t is an unsigned integer type of at least 16 bit.
- * Moreover, size_t is the return type of the "sizeof" operator, which is
- * defined by the compiler. That's why both GCC and clang define a __SIZE_TYPE__
- * macro to let the libc header know about the type used by the compiler. */
-typedef __SIZE_TYPE__ size_t;
+typedef int ssize_t;
+typedef unsigned int size_t;
+
+#define offsetof(st, m) __builtin_offsetof(st, m)
 
 #endif

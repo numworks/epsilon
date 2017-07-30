1972b30a7703ad776617004ee089cbd750829ca2
Romain Goyet <romain@goyet.com>
2017-07-30 15:20:55 +0200

    [liba] Support for variadic functions (va_arg)
    Change-Id: Ic4ea2bce521ad8149198433afe74214018998afc

---
+++ liba/include/stdarg.h
@@ -0,0 +1,9 @@
+#ifndef LIBA_STDARG_H
+#define LIBA_STDARG_H
+
+#define va_arg __builtin_va_arg
+#define va_end __builtin_va_end
+#define va_list __builtin_va_list
+#define va_start __builtin_va_start
+
+#endif

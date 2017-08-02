c0040d9aaea52bd1b6f7ede2f4f5d9cb7ab0ef35
Romain Goyet <romain.goyet@numworks.com>
2017-08-02 09:56:19 +0200

    [libaxx] Add operator new[] and delete[]
    Change-Id: Id20e803461a6e8c2b3484d094a5483763c06ed0d

--- libaxx/src/new.cpp
+++ libaxx/src/new.cpp
@@ -2,10 +2,18 @@
 
 // See the C++ standard, section 3.7.4 for those definitions
 
-void * operator new (unsigned int size) {
+void * operator new(size_t size) {
   return malloc(size);
 }
 
-void operator delete (void * ptr) noexcept {
+void operator delete(void * ptr) noexcept {
   free(ptr);
+}
+
+void * operator new[](size_t size) {
+    return ::operator new(size);
+}
+
+void operator delete[](void * ptr) noexcept {
+    ::operator delete(ptr);
 }

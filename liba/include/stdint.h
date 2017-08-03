569425b20f5e12555cea901760e6253896ff3a37
Romain Goyet <romain.goyet@numworks.com>
2017-08-03 10:48:19 +0200

    [liba] Add fast/least integer type definitions
    Change-Id: Id360c019659e64a7f0a2137df206047c19be5906

--- liba/include/stdint.h
+++ liba/include/stdint.h
@@ -11,4 +11,18 @@ typedef signed short int16_t;
 typedef signed int int32_t;
 typedef signed long long int64_t;
 
+typedef uint8_t uint_fast8_t;
+typedef uint16_t uint_fast16_t;
+typedef uint32_t uint_fast32_t;
+typedef uint64_t uint_fast64_t;
+
+typedef int8_t int_fast8_t;
+typedef int16_t int_fast16_t;
+typedef int32_t int_fast32_t;
+typedef int64_t int_fast64_t;
+
+typedef uint8_t uint_least8_t;
+
+#define UINT64_C(c) c ## ULL
+
 #endif

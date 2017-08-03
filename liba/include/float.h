58db17b6315fa7fb378d86c9a1f5df5603b5fd9f
Romain Goyet <romain.goyet@numworks.com>
2017-08-03 14:52:20 +0200

    [liba] Add a double-precision libm
    Change-Id: I3d0b5393eaaec3e01529e739535512c07f41a088

--- liba/include/float.h
+++ liba/include/float.h
@@ -1,8 +1,12 @@
 #ifndef LIBA_FLOAT_H
 #define LIBA_FLOAT_H
 
+#define FLT_EVAL_METHOD __FLT_EVAL_METHOD__
+
 #define FLT_MAX 1E+37f
 #define FLT_MIN 1E-37f
 #define FLT_EPSILON 1E-5f
+
+#define LDBL_MANT_DIG (-1)
 
 #endif

49ecebcadb7ce94e01e1416fe4a68da7b6228145
Émilie Feral <emilie.feral@numworks.com>
2017-08-08 14:38:51 +0200

    [ion] Fix dummy events modifier
    Change-Id: If79f8e61c275189bc72ad2cbe035609e4c31d694

--- ion/src/shared/dummy/events_modifier.cpp
+++ ion/src/shared/dummy/events_modifier.cpp
@@ -3,19 +3,15 @@
 namespace Ion {
 namespace Events {
 
+ShiftAlphaStatus shiftAlphaStatus() {
+  return ShiftAlphaStatus::Default;
+}
+
 bool isShiftActive() {
   return false;
 }
 
 bool isAlphaActive() {
-  return false;
-}
-
-bool isAlphaLocked() {
-  return false;
-}
-
-bool isShiftAlphaLocked() {
   return false;
 }
 

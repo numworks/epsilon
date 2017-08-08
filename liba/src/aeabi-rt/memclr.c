3ac050a076c08acac9f6f087ed3fd870edc3e231
Émilie Feral <emilie.feral@numworks.com>
2017-08-08 15:43:28 +0200

    [liba] Implement memclr8
    Change-Id: Icc1ad9aed4911c5990e4f2e51e68c596e99e8534

--- liba/src/aeabi-rt/memclr.c
+++ liba/src/aeabi-rt/memclr.c
@@ -10,3 +10,7 @@ void __aeabi_memclr(void * dest, size_t n) {
 void __aeabi_memclr4(void * dest, size_t n) {
   memset(dest, 0, n);
 }
+
+void __aeabi_memclr8(void * dest, size_t n) {
+  memset(dest, 0, n);
+}

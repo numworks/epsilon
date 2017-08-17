3184bc2276fd8ad70d27b382bc921bf67036efba
Émilie Feral <emilie.feral@numworks.com>
2017-08-17 15:32:26 +0200

    [kandinsky] Enable to draw string with \n or \t characters
    Change-Id: I66a1953dd1eb00d7f397b32a69edb56a1d83f4ac

--- kandinsky/src/text.cpp
+++ kandinsky/src/text.cpp
@@ -7,8 +7,20 @@ KDSize KDText::stringSize(const char * text, FontSize size) {
   if (text == nullptr) {
     return KDSizeZero;
   }
-  KDSize cSize = charSize(size);
-  return KDSize(cSize.width()*strlen(text), cSize.height());
+  KDSize commonCharSize = charSize(size);
+  KDSize stringSize = KDSize(0, commonCharSize.height());
+  while (*text != 0) {
+    KDSize cSize = KDSize(commonCharSize.width(), 0);
+    if (*text == '\t') {
+      cSize = KDSize(k_tabCharacterWidth*commonCharSize.width(), 0);
+    }
+    if (*text == '\n') {
+      cSize = KDSize(0, commonCharSize.height());
+    }
+    stringSize = KDSize(stringSize.width()+cSize.width(), stringSize.height()+cSize.height());
+    text++;
+  }
+  return stringSize;
 }
 
 KDSize KDText::charSize(FontSize size) {

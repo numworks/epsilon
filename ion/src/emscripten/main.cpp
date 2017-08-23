e7534b55acf0a2cf0f6fa3612bf34a689a229436
Romain Goyet <romain.goyet@numworks.com>
2017-08-23 10:45:57 +0200

    emscripten: set the language from command-line arguments
    Change-Id: I8bac1670c4c01e974f6303c9aa392a12c5d2fac0

--- ion/src/emscripten/main.cpp
+++ ion/src/emscripten/main.cpp
@@ -1,10 +1,23 @@
 #include <ion.h>
 #include "display.h"
 #include "events_keyboard.h"
+#include "../../../apps/global_preferences.h"
 
 int main(int argc, char * argv[]) {
   Ion::Display::Emscripten::init();
   Ion::Events::Emscripten::init();
+
+  if (argc == 2) {
+    const char * languageIdentifiers[] = {"none", "en", "fr", "es", "de", "pt"};
+    const char * requestedLanguageId = argv[1];
+    for (int i=0; i<sizeof(languageIdentifiers)/sizeof(languageIdentifiers[0]); i++) {
+      if (strcmp(requestedLanguageId, languageIdentifiers[i]) == 0) {
+        GlobalPreferences::sharedGlobalPreferences()->setLanguage((I18n::Language)i);
+        break;
+      }
+    }
+  }
+
   ion_app();
   return 0;
 }

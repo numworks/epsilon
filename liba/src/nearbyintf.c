77e92ceaf250383f805eebef278a65ab4f44fe44
Émilie Feral <emilie.feral@numworks.com>
2017-08-28 11:17:22 +0200

    [liba] Add comment regarding nearbyintf
    Change-Id: I6c02f2fe19cccce60516ad36062bc736e803991a

--- liba/src/nearbyintf.c
+++ liba/src/nearbyintf.c
@@ -1,5 +1,17 @@
 #include <math.h>
 
+/* nearbyintf is not supposed to be the same as roundf according to openBSD
+ * documentation. Indeed:
+ * - they round halfway cases to the nearest integer instead of away from zero
+ *   (nearbyint(4.5) = 4 while round(4.5) = 5 for example).
+ * - nearbyint is guaranteed never to set floating point flags or trigger
+ *   floating point exceptions (which are disabled by default)
+ *   accroding to openBSD open manual page.
+ *
+ * However, as nearbyintf is required by micropython only, in order to save
+ * space and time (nearbyintf openBSD 6.0 implementation required some other
+ * function implementation), we used roundf. */
+
 float nearbyintf(float x) {
   return roundf(x);
 }

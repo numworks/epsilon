198c26f99dc7a927328684781e55724425fb5522
Jacob Young <jacobly0@users.noreply.github.com>
2017-08-31 18:09:00 -0400

    Fix grammar.

--- poincare/test/helper.h
+++ poincare/test/helper.h
@@ -4,8 +4,8 @@ constexpr Poincare::Expression::AngleUnit Degree = Poincare::Expression::AngleUn
 constexpr Poincare::Expression::AngleUnit Radian = Poincare::Expression::AngleUnit::Radian;
 
 template<typename T>
-void assert_parsed_expression_evaluate_to(const char * expression, Poincare::Complex<T> * results, int numberOfRows, int numberOfColumns = 1, Poincare::Expression::AngleUnit angleUnit = Degree);
+void assert_parsed_expression_evaluates_to(const char * expression, Poincare::Complex<T> * results, int numberOfRows, int numberOfColumns = 1, Poincare::Expression::AngleUnit angleUnit = Degree);
 template<typename T>
-void assert_parsed_expression_evaluate_to(const char * expression, Poincare::Complex<T> * results, Poincare::Expression::AngleUnit angleUnit = Degree) {
-  assert_parsed_expression_evaluate_to(expression, results, 1, 1, angleUnit);
+void assert_parsed_expression_evaluates_to(const char * expression, Poincare::Complex<T> * results, Poincare::Expression::AngleUnit angleUnit = Degree) {
+  assert_parsed_expression_evaluates_to(expression, results, 1, 1, angleUnit);
 }

10c42e276a19d18124fceae6b8bf89e77315d0b6
Émilie Feral <emilie.feral@numworks.com>
2017-09-06 11:51:08 +0200

    [escher] Add a gauge view and a message table cell with gauge

---
+++ escher/include/escher/gauge_view.h
@@ -0,0 +1,21 @@
+#ifndef ESCHER_GAUGE_VIEW_H
+#define ESCHER_GAUGE_VIEW_H
+
+#include <escher/view.h>
+
+class GaugeView : public View {
+public:
+  GaugeView();
+  float level();
+  void setLevel(float level);
+  void setBackgroundColor(KDColor color);
+  void drawRect(KDContext * ctx, KDRect rect) const override;
+  KDSize minimalSizeForOptimalDisplay() const override;
+  constexpr static KDCoordinate k_indicatorDiameter = 10;
+private:
+  constexpr static KDCoordinate k_thickness = 2;
+  float m_level;
+  KDColor m_backgroundColor;
+};
+
+#endif
---
+++ escher/include/escher/message_table_cell_with_gauge.h
@@ -0,0 +1,16 @@
+#ifndef ESCHER_MESSAGE_TABLE_CELL_WITH_GAUGE_H
+#define ESCHER_MESSAGE_TABLE_CELL_WITH_GAUGE_H
+
+#include <escher/message_table_cell.h>
+#include <escher/gauge_view.h>
+
+class MessageTableCellWithGauge : public MessageTableCell {
+public:
+  MessageTableCellWithGauge(I18n::Message message = (I18n::Message)0, KDText::FontSize size = KDText::FontSize::Small);
+  View * accessoryView() const override;
+  void setHighlighted(bool highlight) override;
+private:
+  GaugeView m_accessoryView;
+};
+
+#endif

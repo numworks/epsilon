70cebb82e094370867fac6e86d3aee23153a210b
Émilie Feral <emilie.feral@numworks.com>
2017-09-06 11:51:08 +0200

    [escher] Add a gauge view and a message table cell with gauge

--- escher/include/escher.h
+++ escher/include/escher.h
@@ -20,6 +20,7 @@
 #include <escher/expression_table_cell.h>
 #include <escher/expression_table_cell_with_pointer.h>
 #include <escher/expression_view.h>
+#include <escher/gauge_view.h>
 #include <escher/highlight_cell.h>
 #include <escher/image.h>
 #include <escher/image_view.h>
@@ -34,6 +35,7 @@
 #include <escher/message_table_cell_with_chevron_and_message.h>
 #include <escher/message_table_cell_with_chevron_and_expression.h>
 #include <escher/message_table_cell_with_editable_text.h>
+#include <escher/message_table_cell_with_gauge.h>
 #include <escher/message_table_cell_with_message.h>
 #include <escher/message_table_cell_with_switch.h>
 #include <escher/message_text_view.h>
---
+++ escher/src/gauge_view.cpp
@@ -0,0 +1,60 @@
+#include <escher/gauge_view.h>
+#include <escher/palette.h>
+
+const uint8_t gaugeIndicatorMask[GaugeView::k_indicatorDiameter][GaugeView::k_indicatorDiameter] = {
+  {0xFF, 0xFF, 0xE1, 0x0C, 0x00, 0x00, 0x0C, 0xE1, 0xFF, 0xFF},
+  {0xFF, 0x45, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x45, 0xFF},
+  {0xE1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE1},
+  {0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C},
+  {0xE1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xE1},
+  {0xFF, 0x45, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x45, 0xFF},
+  {0xFF, 0xFF, 0xE1, 0x0C, 0x00, 0x00, 0x0C, 0xE1, 0xFF, 0xFF},
+};
+
+GaugeView::GaugeView() :
+  View(),
+  m_level(1),
+  m_backgroundColor(KDColorWhite)
+{
+}
+
+float GaugeView::level() {
+  return m_level;
+}
+
+void GaugeView::setLevel(float level) {
+  if (m_level != level) {
+    level = level < 0 ? 0 : level;
+    level = level > 1 ? 1 : level;
+    m_level = level;
+    markRectAsDirty(bounds());
+  }
+}
+
+void GaugeView::setBackgroundColor(KDColor color) {
+  if (m_backgroundColor != color) {
+    m_backgroundColor = color;
+    markRectAsDirty(bounds());
+  }
+}
+
+KDColor s_gaugeIndicatorWorkingBuffer[GaugeView::k_indicatorDiameter*GaugeView::k_indicatorDiameter];
+
+void GaugeView::drawRect(KDContext * ctx, KDRect rect) const {
+  ctx->fillRect(bounds(), m_backgroundColor);
+  /* Draw the gauge centered vertically on all the width available */
+  KDCoordinate width = bounds().width()-k_indicatorDiameter;
+  KDCoordinate height =  bounds().height();
+
+  ctx->fillRect(KDRect(k_indicatorDiameter/2, (height-k_thickness)/2, width*m_level, k_thickness), Palette::YellowDark);
+  ctx->fillRect(KDRect(k_indicatorDiameter/2+width*m_level, (height-k_thickness)/2, width*(1.0f-m_level), k_thickness), Palette::GreyDark);
+  KDRect frame(width*m_level, (height-k_indicatorDiameter)/2, k_indicatorDiameter, k_indicatorDiameter);
+  ctx->blendRectWithMask(frame, Palette::YellowDark, (const uint8_t *)gaugeIndicatorMask, s_gaugeIndicatorWorkingBuffer);
+}
+
+KDSize GaugeView::minimalSizeForOptimalDisplay() const {
+  return KDSize(12*k_indicatorDiameter, k_indicatorDiameter);
+}

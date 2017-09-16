c9ca43e257ba2d300219a2052b775f20fa259c33
Jacob Young <jacobly0@users.noreply.github.com>
2017-09-16 08:00:13 -0400

    Reuse bracket layout logic for absolute value, saving 184 bytes on my device build.

--- poincare/src/layout/absolute_value_layout.h
+++ poincare/src/layout/absolute_value_layout.h
@@ -1,28 +1,22 @@
 #ifndef POINCARE_ABSOLUTE_VALUE_LAYOUT_H
 #define POINCARE_ABSOLUTE_VALUE_LAYOUT_H
 
-#include <poincare/expression.h>
-#include <poincare/expression_layout.h>
+#include "bracket_layout.h"
 
 namespace Poincare {
 
-class AbsoluteValueLayout : public ExpressionLayout {
+class AbsoluteValueLayout : public BracketLayout {
 public:
-  AbsoluteValueLayout(ExpressionLayout * operandLayout);
-  ~AbsoluteValueLayout();
+  AbsoluteValueLayout(ExpressionLayout * operandLayout) : BracketLayout(operandLayout) {}
+  ~AbsoluteValueLayout() {}
   AbsoluteValueLayout(const AbsoluteValueLayout& other) = delete;
   AbsoluteValueLayout(AbsoluteValueLayout&& other) = delete;
   AbsoluteValueLayout& operator=(const AbsoluteValueLayout& other) = delete;
   AbsoluteValueLayout& operator=(AbsoluteValueLayout&& other) = delete;
 protected:
-  void render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) override;
-  KDSize computeSize() override;
-  ExpressionLayout * child(uint16_t index) override;
-  KDPoint positionOfChild(ExpressionLayout * child) override;
-private:
-  constexpr static KDCoordinate k_straightBracketWidth = 1;
-  constexpr static KDCoordinate k_straightBracketMargin = 2;
-  ExpressionLayout * m_operandLayout;
+  KDCoordinate widthMargin() const { return 2; }
+  bool renderTopBar() const { return false; }
+  bool renderBottomBar() const { return false; }
 };
 
 }

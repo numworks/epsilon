341651ba8b022044a33ae6b4714a052c25410694
Romain Goyet <romain.goyet@numworks.com>
2017-09-25 11:15:02 +0200

    [poincare] Clean the Integer class
    Change-Id: I010f571b521a5c45f6131040d396dc0241517728

--- poincare/test/integer.cpp
+++ poincare/test/integer.cpp
@@ -5,51 +5,53 @@
 using namespace Poincare;
 
 QUIZ_CASE(poincare_integer) {
-  assert(Integer(123) == Integer(123));
-  assert(Integer("123") == Integer(123));
-  assert(!(Integer("-123") == Integer(123)));
-  assert(Integer("-123") == Integer(-123));
+  assert(Integer(123).isEqualTo(Integer(123)));
+  assert(Integer("123").isEqualTo(Integer(123)));
+  assert(!Integer("-123").isEqualTo(Integer(123)));
+  assert(Integer("-123").isEqualTo(Integer(-123)));
   //assert(Integer("0123") == Integer(123));
   //FIXME: assert(Integer("0x2BABE") == Integer(178878));
   //FIXME: assert(Integer("0b1011") == Integer(11));
 }
 
 QUIZ_CASE(poincare_integer_compare) {
-  assert(Integer(123) < Integer(456));
-  assert(!(Integer(123) < Integer(123)));
-  assert(!(Integer(-123) < Integer(-123)));
-  assert(Integer(-100) < Integer(2));
-  assert(Integer(-200) < Integer(-100));
-  assert(Integer(123) < Integer("123456789123456789"));
-  assert(Integer("123456789123456788") < Integer("123456789123456789"));
-  assert(!(Integer("123456789123456789") < Integer("123456789123456788")));
+  assert(Integer(123).isLowerThan(Integer(456)));
+  assert(!Integer(123).isLowerThan(Integer(123)));
+  assert(!Integer(-123).isLowerThan(Integer(-123)));
+  assert(Integer(-100).isLowerThan(Integer(2)));
+  assert(Integer(-200).isLowerThan(Integer(-100)));
+  assert(Integer(123).isLowerThan(Integer("123456789123456789")));
+  assert(Integer(-123).isLowerThan(Integer("123456789123456789")));
+  assert(Integer("123456789123456788").isLowerThan(Integer("123456789123456789")));
+  assert(Integer("-1234567891234567892109209109").isLowerThan(Integer("123456789123456789")));
+  assert(!Integer("123456789123456789").isLowerThan(Integer("123456789123456788")));
 }
 
-QUIZ_CASE(poincare_integer_add) {
-  assert(Integer("0").add(Integer("0")) == Integer("0"));
-  assert(Integer(123).add(Integer(456)) == Integer(579));
-  assert(Integer("123456789123456789").add(Integer(1)) == Integer("123456789123456790"));
+QUIZ_CASE(poincare_integer_addition) {
+  assert(Integer::Addition(Integer("0"), Integer("0")).isEqualTo(Integer(0)));
+  assert(Integer::Addition(Integer(123), Integer(456)).isEqualTo(Integer(579)));
+  assert(Integer::Addition(Integer("123456789123456789"), Integer(1)).isEqualTo(Integer("123456789123456790")));
 }
 
-QUIZ_CASE(poincare_integer_subtract) {
-  assert(Integer(123).subtract(Integer(23)) == Integer(100));
-  assert(Integer("123456789123456789").subtract(Integer("9999999999")) == Integer("123456779123456790"));
-  assert(Integer(23).subtract(Integer(100)) == Integer(-77));
+QUIZ_CASE(poincare_integer_subtraction) {
+  assert(Integer::Subtraction(Integer(123), Integer(23)).isEqualTo(Integer(100)));
+  assert(Integer::Subtraction(Integer("123456789123456789"), Integer("9999999999")).isEqualTo(Integer("123456779123456790")));
+  assert(Integer::Subtraction(Integer(23), Integer(100)).isEqualTo(Integer(-77)));
 }
 
-QUIZ_CASE(poincare_integer_multiply) {
-  assert(Integer(12).multiply_by(Integer(34)) == Integer(408));
-  assert(Integer(56).multiply_by(Integer("0")) == Integer("0"));
-  assert(Integer(-12).multiply_by(Integer(34)) == Integer(-408));
-  assert(Integer(-12).multiply_by(Integer(-34)) == Integer(408));
-  assert(Integer(999999).multiply_by(Integer(999999)) == Integer("999998000001"));
-  assert(Integer("9999999999").multiply_by(Integer("9999999999")) == Integer("99999999980000000001"));
+QUIZ_CASE(poincare_integer_multiplication) {
+  assert(Integer::Multiplication(Integer(12), Integer(34)).isEqualTo(Integer(408)));
+  assert(Integer::Multiplication(Integer(56), Integer(0)).isEqualTo(Integer(0)));
+  assert(Integer::Multiplication(Integer(-12), Integer(34)).isEqualTo(Integer(-408)));
+  assert(Integer::Multiplication(Integer(-12), Integer(-34)).isEqualTo(Integer(408)));
+  assert(Integer::Multiplication(Integer(123456), Integer(654321)).isEqualTo(Integer("80779853376")));
+  assert(Integer::Multiplication(Integer("9999999999"), Integer("9999999999")).isEqualTo(Integer("99999999980000000001")));
 }
 
 QUIZ_CASE(poincare_integer_divide) {
-  assert(Integer(8).divide_by(Integer(4)) == Integer(2));
-  assert(Integer("3293920983029832").divide_by(Integer("38928")) == Integer("84615726033"));
-  assert(Integer("3293920983029832").divide_by(Integer("389090928")) == Integer("8465684"));
+//  assert(Integer(8).divide_by(Integer(4)) == Integer(2));
+//  assert(Integer("3293920983029832").divide_by(Integer("38928")) == Integer("84615726033"));
+//  assert(Integer("3293920983029832").divide_by(Integer("389090928")) == Integer("8465684"));
 }
 
 template<typename T>

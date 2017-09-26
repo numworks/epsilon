00f45f4be25d2381a305acf438f2a21637faeb92
Émilie Feral <emilie.feral@numworks.com>
2017-09-26 16:38:22 +0200

    [poincare] Repair tests
    Change-Id: Ie14d3c1afc0c8e66d4b0eddb2919958391c5c959

--- poincare/test/identity.cpp
+++ poincare/test/identity.cpp
@@ -29,8 +29,8 @@ QUIZ_CASE(poincare_identity_simple_term) {
 }
 
 QUIZ_CASE(poincare_identity_commutativity) {
-  assert(identical_to("1+2", "2+1"));
+  assert(equivalent_to("1+2", "2+1"));
   //assert(identical_to("1*2", "2*1"));
-  assert(!identical_to("1-2", "2-1"));
-  assert(!identical_to("1/2", "2/1"));
+  assert(!equivalent_to("1-2", "2-1"));
+  assert(!equivalent_to("1/2", "2/1"));
 }

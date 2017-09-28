43e0f530d93167303ec233d0d270ea399407bb70
Émilie Feral <emilie.feral@numworks.com>
2017-09-28 16:54:11 +0200

    [poincare] Simplification: factorize implementation of int addition and multiplication transform
    Change-Id: Ie03d534693b1cfc9a6e4a66919be5f6b368aff41

---
+++ poincare/src/simplification/transform/integer_dynamic_hierarchy_transform.cpp
@@ -0,0 +1,40 @@
+#include "transform.h"
+#include <assert.h>
+#include <poincare/integer.h>
+#include <poincare/dynamic_hierarchy.h>
+#include <poincare/addition.h>
+#include <poincare/multiplication.h>
+#include <utility>
+
+namespace Poincare {
+
+typedef Integer (*Operation)(const Integer&, const Integer &);
+
+static bool IntegerDynamicHierarchyTransform(Expression * captures[], Operation operation) {
+  DynamicHierarchy * a = static_cast<DynamicHierarchy *>(captures[0]);
+  Integer * i1 = static_cast<Integer *>(captures[1]);
+  Integer * i2 = static_cast<Integer *>(captures[2]);
+
+  Integer resultOnStack = operation(*i1, *i2);
+  Integer * r = new Integer(std::move(resultOnStack));
+
+  if (a->numberOfOperands() == 2) {
+    static_cast<Hierarchy *>(a->parent())->replaceOperand(a, r);
+  } else {
+    assert(a->numberOfOperands() > 2);
+    a->replaceOperand(i1, r);
+    a->removeOperand(i2);
+  }
+
+  return true;
+}
+
+bool Simplification::IntegerAdditionTransform(Expression * captures[]) {
+  return IntegerDynamicHierarchyTransform(captures, Integer::Addition);
+}
+
+bool Simplification::IntegerMultiplicationTransform(Expression * captures[]) {
+  return IntegerDynamicHierarchyTransform(captures, Integer::Multiplication);
+}
+
+}

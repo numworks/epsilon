c0b67fb63597352497c07d5112a4b1d3923e28d2
Romain Goyet <romain.goyet@numworks.com>
2017-09-29 21:17:12 +0200

    [poincare] Actually implement AnySelector

--- poincare/src/simplification/ruleset.h
+++ poincare/src/simplification/ruleset.h
@@ -2,6 +2,7 @@
 #define POINCARE_SIMPLIFICATION_RULESET_H
 
 #include "rule.h"
+#include "selector/any_selector.h"
 #include "selector/type_selector.h"
 #include "selector/type_and_identifier_selector.h"
 

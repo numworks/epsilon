0a4845abde16097988cd6957514fb3f7471f0396
Émilie Feral <emilie.feral@numworks.com>
2017-10-13 17:44:21 +0200

    [poincare] Sine::immediateSimplify
    Change-Id: Iab48a0e939386861c3ede2507dc48998e2bc6e9d

--- poincare/test/simplify_easy.cpp
+++ poincare/test/simplify_easy.cpp
@@ -153,6 +153,16 @@ QUIZ_CASE(poincare_simplify_easy) {
   assert_parsed_expression_simplify_to("cos(-P/12)", "(R(6)+R(2))/4");
   assert_parsed_expression_simplify_to("cos(-P*R(2))", "cos(P*R(2))");
 
+  assert_parsed_expression_simplify_to("sin(0)", "0");
+  assert_parsed_expression_simplify_to("sin(P)", "0");
+  assert_parsed_expression_simplify_to("sin(P*35/29)", "sin(P*6/29)");
+  assert_parsed_expression_simplify_to("sin(-P*35/29)", "-sin(P*6/29)");
+  assert_parsed_expression_simplify_to("sin(P*340000)", "0");
+  assert_parsed_expression_simplify_to("sin(P*340001)", "0");
+  assert_parsed_expression_simplify_to("sin(-P*340001)", "0");
+  assert_parsed_expression_simplify_to("sin(P/12)", "(R(6)-R(2))/4");
+  assert_parsed_expression_simplify_to("sin(-P/12)", "(R(2)-R(6))/4");
+  assert_parsed_expression_simplify_to("sin(-P*R(2))", "-sin(P*R(2))");
   /* This does not work but should not as it is above k_primorial32 = 1*3*5*7*11*... (product of first 32 primes. */
   //assert_parsed_expression_simplify_to("1881676377434183981909562699940347954480361860897069^(1/3)", "123456789123456789");
 

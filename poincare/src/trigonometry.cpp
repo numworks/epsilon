70818b7bc4a902b32d5cf67b37276259f3bb13bb
Émilie Feral <emilie.feral@numworks.com>
2017-10-16 10:13:06 +0200

    [poincare] Fix bug in trigonometry
    Change-Id: I02c6ab9057d9e50ef6a355c86a890c9f0f684a16

--- poincare/src/trigonometry.cpp
+++ poincare/src/trigonometry.cpp
@@ -6,7 +6,7 @@ namespace Poincare {
 
 static_assert('\x89' == Ion::Charset::SmallPi, "Incorrect");
 constexpr const char * cheatTable[Trigonometry::k_numberOfEntries][3] =
-{{"\x89",            "\x89*3^(-1)",       "-1"},
+{{"\x89",            "\x89*(-2)^(-1)",       "-1"},
  {"\x89*11*12^(-1)", "\x89*(-5)*12^(-1)", "(-1)*6^(1/2)*4^(-1)-2^(1/2)*4^(-1)"},
  {"\x89*7*8^(-1)",   "\x89*(-3)*8^(-1)",  "-(2+2^(1/2))^(1/2)*2^(-1)"},
  {"\x89*5*6^(-1)",   "\x89*(-3)^(-1)",    "-3^(1/2)*2^(-1)"},
@@ -23,7 +23,7 @@ constexpr const char * cheatTable[Trigonometry::k_numberOfEntries][3] =
  {"\x89*6^(-1)",     "\x89*3^(-1)",       "3^(1/2)*2^(-1)"},
  {"\x89*8^(-1)",     "\x89*3*8^(-1)",     "(2+2^(1/2))^(1/2)*2^(-1)"},
  {"\x89*12^(-1)",    "\x89*5*12^(-1)",    "6^(1/2)*4^(-1)+2^(1/2)*4^(-1)"},
- {"0",               "\x89*5*12^(-1)",    "1"}
+ {"0",               "\x89*2^(-1)",    "1"}
 };
 
 Expression * Trigonometry::table(const Expression * e, Function f, bool inverse) {

0b79be058fad68163986ecc216aa7a2a4c38529a
Émilie Feral <emilie.feral@numworks.com>
2017-10-16 13:05:59 +0200

    [poincare] Tangent::immediateSimplify()
    Change-Id: I9ecdf64640693679c9e797f1d7013886a23a3b9d

--- poincare/src/tangent.cpp
+++ poincare/src/tangent.cpp
@@ -21,6 +21,18 @@ Expression * Tangent::clone() const {
   return a;
 }
 
+Expression * Tangent::immediateSimplify() {
+  const Expression * op[1] = {operand(0)};
+  Sine * s = new Sine(op, true);
+  Cosine * c = new Cosine(op, true);
+  const Expression * divisionOperands[2] = {s, c};
+  Division * d = new Division(divisionOperands, false);
+  c->immediateSimplify();
+  s->immediateSimplify();
+  Expression * newExpression = replaceWith(d, true);
+  return newExpression->simplify();
+}
+
 template<typename T>
 Complex<T> Tangent::computeOnComplex(const Complex<T> c, AngleUnit angleUnit) {
   Complex<T> result = Division::compute(Sine::computeOnComplex(c, angleUnit), Cosine::computeOnComplex(c, angleUnit));

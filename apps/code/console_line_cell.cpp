18bd9b43b24b5d88e896c12e113cdd66d9118d8a
Léa Saviot <lea.saviot@numworks.com>
2017-11-07 16:40:32 +0100

    [code] Left/right navigation in the console history.
    Change-Id: Ibbd534aa80f2f8dcb41d10e38f4abfcc7dbfd4a7

--- apps/code/console_line_cell.cpp
+++ apps/code/console_line_cell.cpp
@@ -3,30 +3,98 @@
 #include <kandinsky/point.h>
 #include <kandinsky/coordinate.h>
 #include <apps/i18n.h>
+#include <escher/palette.h>
 
 namespace Code {
 
-ConsoleLineCell::ConsoleLineCell() :
+ConsoleLineCell::ScrollableConsoleLineView::ConsoleLineView::ConsoleLineView() :
   HighlightCell(),
+  m_line(nullptr)
+{
+}
+
+void ConsoleLineCell::ScrollableConsoleLineView::ConsoleLineView::setLine(ConsoleLine * line) {
+  m_line = line;
+}
+
+void ConsoleLineCell::ScrollableConsoleLineView::ConsoleLineView::drawRect(KDContext * ctx, KDRect rect) const {
+  ctx->fillRect(bounds(), KDColorWhite);
+  ctx->drawString(m_line->text(), KDPointZero, ConsoleController::k_fontSize, KDColorBlack, isHighlighted()? Palette::Select : KDColorWhite);
+}
+
+KDSize ConsoleLineCell::ScrollableConsoleLineView::ConsoleLineView::minimalSizeForOptimalDisplay() const {
+  return KDText::stringSize(m_line->text(), ConsoleController::k_fontSize);
+}
+
+ConsoleLineCell::ScrollableConsoleLineView::ScrollableConsoleLineView(Responder * parentResponder) :
+  ScrollableView(parentResponder, &m_consoleLineView, this),
+  m_consoleLineView()
+{
+}
+
+KDSize ConsoleLineCell::ScrollableConsoleLineView::minimalSizeForOptimalDisplay() const {
+  return m_consoleLineView.minimalSizeForOptimalDisplay();
+}
+
+ConsoleLineCell::ConsoleLineCell(Responder * parentResponder) :
+  HighlightCell(),
+  Responder(parentResponder),
+  m_promptView(ConsoleController::k_fontSize, I18n::Message::ConsolePrompt, 0, 0.5),
+  m_scrollableView(this),
   m_line()
 {
 }
 
-void ConsoleLineCell::drawRect(KDContext * ctx, KDRect rect) const {
-  KDColor textBackgroundColor = isHighlighted() ? Palette::Select : KDColorWhite;
-  ctx->fillRect(bounds(), KDColorWhite);
-  if (m_line.type() == ConsoleLine::Type::Command) {
-    ctx->drawString(I18n::translate(I18n::Message::ConsolePrompt), KDPointZero, ConsoleController::k_fontSize);
-    KDCoordinate chevronsWidth = KDText::stringSize(I18n::translate(I18n::Message::ConsolePrompt), ConsoleController::k_fontSize).width();
-    ctx->drawString(m_line.text(), KDPoint(chevronsWidth, KDCoordinate(0)), ConsoleController::k_fontSize, KDColorBlack, textBackgroundColor);
-  } else {
-    ctx->drawString(m_line.text(), KDPointZero, ConsoleController::k_fontSize, KDColorBlack, textBackgroundColor);
-  }
-}
-
 void ConsoleLineCell::setLine(ConsoleLine line) {
   m_line = line;
+  m_scrollableView.consoleLineView()->setLine(&m_line);
   reloadCell();
+}
+
+void ConsoleLineCell::setHighlighted(bool highlight) {
+  HighlightCell::setHighlighted(highlight);
+  m_scrollableView.consoleLineView()->setHighlighted(highlight);
+}
+
+void ConsoleLineCell::reloadCell() {
+  layoutSubviews();
+  HighlightCell::reloadCell();
+  m_scrollableView.reloadScroll();
+}
+
+int ConsoleLineCell::numberOfSubviews() const {
+  if (m_line.type() == ConsoleLine::Type::Command) {
+    return 2;
+  }
+  assert(m_line.type() == ConsoleLine::Type::Result);
+  return 1;
+}
+
+View * ConsoleLineCell::subviewAtIndex(int index) {
+  if (m_line.type() == ConsoleLine::Type::Command) {
+    assert(index >= 0 && index < 2);
+    View * views[] = {&m_promptView, &m_scrollableView};
+    return views[index];
+  }
+  assert(m_line.type() == ConsoleLine::Type::Result);
+  assert(index == 0);
+  return &m_scrollableView;
+}
+
+void ConsoleLineCell::layoutSubviews() {
+  if (m_line.type() == ConsoleLine::Type::Command) {
+    KDSize promptSize = KDText::stringSize(I18n::translate(I18n::Message::ConsolePrompt), ConsoleController::k_fontSize);
+    m_promptView.setFrame(KDRect(KDPointZero, promptSize.width(), bounds().height()));
+    m_scrollableView.setFrame(KDRect(KDPoint(promptSize.width(), 0), bounds().width() - promptSize.width(), bounds().height()));
+    return;
+  }
+  assert(m_line.type() == ConsoleLine::Type::Result);
+  m_promptView.setFrame(KDRectZero);
+  m_scrollableView.setFrame(bounds());
+}
+
+void ConsoleLineCell::didBecomeFirstResponder() {
+  app()->setFirstResponder(&m_scrollableView);
 }
 
 }
--- apps/code/console_line_cell.h
+++ apps/code/console_line_cell.h
@@ -2,16 +2,53 @@
 #define CODE_CONSOLE_LINE_CELL_H
 
 #include <escher/highlight_cell.h>
+#include <escher/message_text_view.h>
+#include <escher/responder.h>
+#include <escher/scrollable_view.h>
+#include <escher/scroll_view_data_source.h>
+#include <assert.h>
+
 #include "console_line.h"
 
 namespace Code {
 
-class ConsoleLineCell : public HighlightCell {
+class ConsoleLineCell : public HighlightCell, public Responder {
 public:
-  ConsoleLineCell();
-  void drawRect(KDContext * ctx, KDRect rect) const override;
+  ConsoleLineCell(Responder * parentResponder = nullptr);
   void setLine(ConsoleLine line);
+
+  /* HighlightCell */
+  void setHighlighted(bool highlight) override;
+  void reloadCell() override;
+
+  /* View */
+  int numberOfSubviews() const override;
+  View * subviewAtIndex(int index) override;
+  void layoutSubviews() override;
+
+  /* Responder */
+  void didBecomeFirstResponder() override;
 private:
+  class ScrollableConsoleLineView : public ScrollableView, public ScrollViewDataSource {
+  public:
+    class ConsoleLineView : public HighlightCell {
+    public:
+      ConsoleLineView();
+      void setLine(ConsoleLine * line);
+      void drawRect(KDContext * ctx, KDRect rect) const override;
+      KDSize minimalSizeForOptimalDisplay() const override;
+    private:
+      ConsoleLine * m_line;
+    };
+
+    ScrollableConsoleLineView(Responder * parentResponder);
+    KDSize minimalSizeForOptimalDisplay() const override;
+    ConsoleLineView * consoleLineView() { return &m_consoleLineView; }
+  private:
+    ConsoleLineView m_consoleLineView;
+  };
+  MessageTextView m_promptView;
+  ScrollableConsoleLineView m_scrollableView;
   ConsoleLine m_line;
 };
 

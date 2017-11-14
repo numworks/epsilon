51c2ee34df6b7afc389902a51c993f2e4f8b1d13
Émilie Feral <emilie.feral@numworks.com>
2017-11-14 10:52:05 +0100

    [poincare] Add Pi/10 in Trigonometry cheatTable
    Change-Id: I193e50d2cc4dd4641bf76ba6ce6583e3d5dfbefd

--- poincare/include/poincare/trigonometry.h
+++ poincare/include/poincare/trigonometry.h
@@ -14,7 +14,7 @@ public:
   static Expression * shallowReduceDirectFunction(Expression * e, Context& context, Expression::AngleUnit angleUnit);
   static Expression * shallowReduceInverseFunction(Expression * e, Context& context, Expression::AngleUnit angleUnit);
   static bool ExpressionIsEquivalentToTangent(const Expression * e);
-  constexpr static int k_numberOfEntries = 31;
+  constexpr static int k_numberOfEntries = 37;
   static Expression * table(const Expression * e, Expression::Type type, Context & context, Expression::AngleUnit angleUnit); // , Function f, bool inverse
 };
 

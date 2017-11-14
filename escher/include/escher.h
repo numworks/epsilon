3d76741829cf3f8fb8ec711097c228903e6bb9c4
Léa Saviot <lea.saviot@numworks.com>
2017-11-14 11:13:29 +0100

    [escher/code] Move Accordion from escher/ to apps/code/.
    Change-Id: Id90ef9791f8c18bcdce497b8e20f060314f7b58b

--- escher/include/escher.h
+++ escher/include/escher.h
@@ -1,7 +1,6 @@
 #ifndef ESCHER_H
 #define ESCHER_H
 
-#include <escher/accordion.h>
 #include <escher/alternate_empty_view_controller.h>
 #include <escher/alternate_empty_view_delegate.h>
 #include <escher/app.h>

845337a0423c25795ed3351f47e69713a8765769
Damien Nicolet <zardam@gmail.com>
2017-11-16 22:21:55 +0100

    Enable instruction and data cache

--- ion/src/device/regs/flash.h
+++ ion/src/device/regs/flash.h
@@ -8,6 +8,9 @@ public:
   class ACR : public Register32 {
   public:
     REGS_FIELD(LATENCY, uint8_t, 3, 0);
+    REGS_BOOL_FIELD(PRFTEN, 8);
+    REGS_BOOL_FIELD(ICEN, 9);
+    REGS_BOOL_FIELD(DCEN, 10);
   };
 
   constexpr FLASH() {};

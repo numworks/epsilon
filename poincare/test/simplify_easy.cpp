4c01e6a8ce97adcff18820bc40c401fdb6c8772f
Émilie Feral <emilie.feral@numworks.com>
2017-11-17 16:47:34 +0100

    [poincare] Fix infinite loop in logarithm
    Change-Id: Ic70f2d236af9895a468a57953a446db496a54f2f

--- poincare/test/simplify_easy.cpp
+++ poincare/test/simplify_easy.cpp
@@ -477,6 +477,7 @@ QUIZ_CASE(poincare_simplify_easy) {
   assert_parsed_expression_simplify_to("P^log(R(3),P)", "R(3)");
   assert_parsed_expression_simplify_to("10^log(P)", "P");
   assert_parsed_expression_simplify_to("log(1/R(2))", "-log(2)/2");
+  assert_parsed_expression_simplify_to("log(-I)", "log(-I)");
 
   /* This does not work but should not as it is above k_primorial32 = 1*3*5*7*11*... (product of first 32 primes. */
   //assert_parsed_expression_simplify_to("1881676377434183981909562699940347954480361860897069^(1/3)", "123456789123456789");

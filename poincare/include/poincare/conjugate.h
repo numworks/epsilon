ff0a72177c9999c224d05a701c7409c6397ea650
Émilie Feral <emilie.feral@numworks.com>
2017-11-22 13:57:02 +0100

    [poincare] Fix error in write in buffer: conjugate(2)->conj(2)
    Change-Id: I68056b192afc76f50117fd4157d85d5f9fa029db

--- poincare/include/poincare/conjugate.h
+++ poincare/include/poincare/conjugate.h
@@ -16,7 +16,7 @@ private:
   /* Layout */
   ExpressionLayout * privateCreateLayout(FloatDisplayMode floatDisplayMode, ComplexFormat complexFormat) const override;
   int writeTextInBuffer(char * buffer, int bufferSize) const override {
-    return LayoutEngine::writePrefixExpressionTextInBuffer(this, buffer, bufferSize, "conjugate");
+    return LayoutEngine::writePrefixExpressionTextInBuffer(this, buffer, bufferSize, "conj");
   }
   /* Simplification */
   Expression * shallowReduce(Context& context, AngleUnit angleUnit) override;

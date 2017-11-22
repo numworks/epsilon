8f4c49d8c14b3c74d836e567b53a18e46e7cbfa3
Émilie Feral <emilie.feral@numworks.com>
2017-11-22 14:38:53 +0100

    [apps] In calculation, fix the almostEqual aligmnent
    Change-Id: I26eca4c9c2b5a3b1ad05567e8d5daf76922f2cb7

--- apps/calculation/output_expressions_view.cpp
+++ apps/calculation/output_expressions_view.cpp
@@ -57,8 +57,11 @@ KDSize OutputExpressionsView::minimalSizeForOptimalDisplay() const {
     return approximateExpressionSize;
   }
   KDSize exactExpressionSize = m_exactExpressionView.minimalSizeForOptimalDisplay();
+  KDCoordinate exactBaseline = m_exactExpressionView.expressionLayout()->baseline();
+  KDCoordinate approximateBaseline = m_approximateExpressionView.expressionLayout()->baseline();
+  KDCoordinate height = max(exactBaseline, approximateBaseline) + max(exactExpressionSize.height()-exactBaseline, approximateExpressionSize.height()-approximateBaseline);
   KDSize approximateSignSize = m_approximateSign.minimalSizeForOptimalDisplay();
-  return KDSize(exactExpressionSize.width()+approximateSignSize.width()+approximateExpressionSize.width()+2*k_digitHorizontalMargin, exactExpressionSize.height() > approximateExpressionSize.height() ? exactExpressionSize.height() : approximateExpressionSize.height());
+  return KDSize(exactExpressionSize.width()+approximateSignSize.width()+approximateExpressionSize.width()+2*k_digitHorizontalMargin, height);
 }
 
 void OutputExpressionsView::didBecomeFirstResponder() {
@@ -115,11 +118,14 @@ void OutputExpressionsView::layoutSubviews() {
     m_approximateExpressionView.setFrame(KDRect(0, 0, approximateExpressionSize.width(), height));
     return;
   }
+  KDCoordinate exactBaseline = m_exactExpressionView.expressionLayout()->baseline();
+  KDCoordinate approximateBaseline = m_approximateExpressionView.expressionLayout()->baseline();
+  KDCoordinate baseline = max(exactBaseline, approximateBaseline);
   KDSize exactExpressionSize = m_exactExpressionView.minimalSizeForOptimalDisplay();
-  m_exactExpressionView.setFrame(KDRect(0, 0, exactExpressionSize.width(), height));
   KDSize approximateSignSize = m_approximateSign.minimalSizeForOptimalDisplay();
-  m_approximateSign.setFrame(KDRect(k_digitHorizontalMargin+exactExpressionSize.width(), 0, approximateSignSize.width(), height));
-  m_approximateExpressionView.setFrame(KDRect(2*k_digitHorizontalMargin+exactExpressionSize.width()+approximateSignSize.width(), 0, approximateExpressionSize.width(), height));
+  m_exactExpressionView.setFrame(KDRect(0, baseline-exactBaseline, exactExpressionSize));
+  m_approximateExpressionView.setFrame(KDRect(2*k_digitHorizontalMargin+exactExpressionSize.width()+approximateSignSize.width(), baseline-approximateBaseline, approximateExpressionSize));
+  m_approximateSign.setFrame(KDRect(k_digitHorizontalMargin+exactExpressionSize.width(), baseline-approximateSignSize.height()/2, approximateSignSize));
 }
 
 }

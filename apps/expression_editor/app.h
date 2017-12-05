6733cf2d627e4adc9bbf33f752ea21ec63075395
Léa Saviot <lea.saviot@numworks.com>
2017-12-05 15:32:25 +0100

    [expression_editor] Base app for development, to remove later.
    Change-Id: I1d5b59f67fd146d2e2917546a7d8b9419a0a7036

---
+++ apps/expression_editor/app.h
@@ -0,0 +1,35 @@
+#ifndef EXPRESSION_EDITOR_APP_H
+#define EXPRESSION_EDITOR_APP_H
+
+#include <escher.h>
+#include "controller.h"
+#include "expression_and_layout.h"
+
+namespace ExpressionEditor {
+
+/* TODO This app is used for creating ExpressionLayout edition. It should be
+ * removed when the development is finished. */
+
+class App : public ::App {
+public:
+  class Descriptor : public ::App::Descriptor {
+  public:
+    I18n::Message name() override;
+    I18n::Message upperName() override;
+  };
+  class Snapshot : public ::App::Snapshot, public SelectableTableViewDataSource {
+  public:
+    App * unpack(Container * container) override;
+    Descriptor * descriptor() override;
+    ExpressionAndLayout * expressionAndLayout() { return &m_expressionAndLayout; }
+  private:
+    ExpressionAndLayout m_expressionAndLayout;
+  };
+private:
+  App(Container * container, Snapshot * snapshot);
+  Controller m_controller;
+};
+
+}
+
+#endif

090cdc39137f5752799497cdac6d082944159635
Léa Saviot <lea.saviot@numworks.com>
2017-12-08 10:54:05 +0100

    [python] Fix min/max functions.

--- python/port/mpconfigport.h
+++ python/port/mpconfigport.h
@@ -40,7 +40,6 @@
 #define MICROPY_PY_BUILTINS_SET     (0)
 #define MICROPY_PY_BUILTINS_SLICE   (0)
 #define MICROPY_PY_BUILTINS_PROPERTY (0)
-#define MICROPY_PY_BUILTINS_MIN_MAX (0)
 #define MICROPY_PY___FILE__         (0)
 #define MICROPY_PY_GC               (0)
 #define MICROPY_PY_ARRAY            (0)

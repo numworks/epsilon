73810bbd9a84f7e0a3bd13e5addaf5637597f4f8
Léa Saviot <lea.saviot@numworks.com>
2017-12-14 12:08:42 +0100

    [poincare] ExpressionLayout cursor.
    Change-Id: I2367d20754189608a7925f0d8e8dfe361fed0fd0

---
+++ poincare/src/expression_layout_cursor.cpp
@@ -0,0 +1,22 @@
+#include <poincare/expression_layout_cursor.h>
+
+namespace Poincare {
+
+bool ExpressionLayoutCursor::moveLeft() {
+  return false; //TODO
+}
+
+bool ExpressionLayoutCursor::moveRight() {
+  return false; //TODO
+}
+
+bool ExpressionLayoutCursor::moveUp() {
+  return false; //TODO
+}
+
+bool ExpressionLayoutCursor::moveDown() {
+  return false; //TODO
+}
+
+}
+

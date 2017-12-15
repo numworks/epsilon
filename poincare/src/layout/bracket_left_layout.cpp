ab81f74bbf12da53d1e36620f9affaa3d2a88a46
Léa Saviot <lea.saviot@numworks.com>
2017-12-15 12:04:19 +0100

    [poincare] Bracket Left and Right layouts.
    Change-Id: I05987e837324628b5141b31d01f59c88ae5ff3ce

---
+++ poincare/src/layout/bracket_left_layout.cpp
@@ -0,0 +1,12 @@
+#include "bracket_left_layout.h"
+
+namespace Poincare {
+
+void BracketLeftLayout::render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) {
+  //TODO Make sure m_operandHeight is up-to-date.
+  ctx->fillRect(KDRect(p.x()+k_externWidthMargin, p.y(), k_lineThickness, m_operandHeight), expressionColor);
+  ctx->fillRect(KDRect(p.x()+k_externWidthMargin, p.y(), k_bracketWidth, k_lineThickness), expressionColor);
+  ctx->fillRect(KDRect(p.x()+k_externWidthMargin, p.y() + m_operandHeight, k_bracketWidth, k_lineThickness), expressionColor);
+}
+
+}

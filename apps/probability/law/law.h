48835ac16fadccde9eb5ba52fcc1ee67625ccc30
Émilie Feral <emilie.feral@numworks.com>
2017-12-19 13:50:08 +0100

    [apps] Probability: display 7 significative numbers in calculation results

--- apps/probability/law/law.h
+++ apps/probability/law/law.h
@@ -3,6 +3,7 @@
 
 #include <poincare.h>
 #include <escher.h>
+#include "../../constant.h"
 #include "../../shared/curve_view_range.h"
 #include "../../i18n.h"
 
@@ -38,7 +39,8 @@ public:
   virtual double evaluateAtDiscreteAbscissa(int k) const;
 protected:
   constexpr static int k_maxNumberOfOperations = 1000000;
-  constexpr static double k_maxProbability = 0.9995;
+  static_assert(Constant::LargeNumberOfSignificantDigits == 7, "k_maxProbability is ill-defined compared to LargeNumberOfSignificantDigits");
+  constexpr static double k_maxProbability = 0.9999995;
   constexpr static float k_displayTopMarginRatio = 0.05f;
   constexpr static float k_displayBottomMarginRatio = 0.2f;
   constexpr static float k_displayLeftMarginRatio = 0.05f;

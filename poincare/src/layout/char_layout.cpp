1c54da21c4038deebd9f58e0381a20ed207baa53
Léa Saviot <lea.saviot@numworks.com>
2017-12-19 15:23:22 +0100

    [poincare] Char layout.
    Change-Id: I1fc7180b724b7a9faee0bc0e6e7762a1e883f925

---
+++ poincare/src/layout/char_layout.cpp
@@ -0,0 +1,68 @@
+#include "char_layout.h"
+#include <poincare/expression_layout_cursor.h>
+#include <assert.h>
+#include <stdlib.h>
+
+namespace Poincare {
+
+CharLayout::CharLayout(char c, KDText::FontSize fontSize) :
+  StaticLayoutHierarchy<0>(),
+  m_char(c),
+  m_fontSize(fontSize)
+{
+  // Half height of the font.
+  m_baseline = (KDText::charSize(m_fontSize).height()+1)/2;
+}
+
+ExpressionLayout * CharLayout::clone() const {
+  CharLayout * layout = new CharLayout(m_char, m_fontSize);
+  return layout;
+}
+
+bool CharLayout::moveLeft(ExpressionLayoutCursor * cursor) {
+  assert(cursor->pointedExpressionLayout() == this);
+  // Case: Right.
+  // Go Left.
+  if (cursor->position() == ExpressionLayoutCursor::Position::Right) {
+    cursor->setPosition(ExpressionLayoutCursor::Position::Left);
+    return true;
+  }
+  // Case: Left.
+  // Ask the parent.
+  if (m_parent) {
+    return m_parent->moveLeft(cursor);
+  }
+  return false;
+}
+
+bool CharLayout::moveRight(ExpressionLayoutCursor * cursor) {
+  assert(cursor->pointedExpressionLayout() == this);
+  // Case: Left.
+  // Go Right.
+  if (cursor->position() == ExpressionLayoutCursor::Position::Left) {
+    cursor->setPosition(ExpressionLayoutCursor::Position::Right);
+    return true;
+  }
+  // Case: Right.
+  // Ask the parent.
+  if (m_parent) {
+    return m_parent->moveRight(cursor);
+  }
+  return false;
+}
+
+void CharLayout::render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) {
+  char string[2] = {m_char, 0};
+  ctx->drawString(string, p, m_fontSize, expressionColor, backgroundColor);
+}
+
+KDPoint CharLayout::positionOfChild(ExpressionLayout * child) {
+  assert(false);
+  return KDPointZero;
+}
+
+KDSize CharLayout::computeSize() {
+  return KDText::charSize(m_fontSize);
+}
+
+}
---
+++ poincare/src/layout/char_layout.h
@@ -0,0 +1,27 @@
+#ifndef POINCARE_CHAR_LAYOUT_H
+#define POINCARE_CHAR_LAYOUT_H
+
+#include <poincare/static_layout_hierarchy.h>
+#include <string.h>
+
+namespace Poincare {
+
+class CharLayout : public StaticLayoutHierarchy<0> {
+public:
+  CharLayout(char c, KDText::FontSize fontSize = KDText::FontSize::Large);
+  ExpressionLayout * clone() const override;
+
+  char character() { return m_char; }
+  bool moveLeft(ExpressionLayoutCursor * cursor) override;
+  bool moveRight(ExpressionLayoutCursor * cursor) override;
+protected:
+  void render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) override;
+  KDPoint positionOfChild(ExpressionLayout * child) override;
+  KDSize computeSize() override;
+  char m_char;
+  KDText::FontSize m_fontSize;
+};
+
+}
+
+#endif

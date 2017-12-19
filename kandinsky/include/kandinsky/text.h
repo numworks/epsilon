57866ce788718a70ada3c9f06a1071350a6f274e
Émilie Feral <emilie.feral@numworks.com>
2017-12-19 13:22:53 +0100

    [kandinsky] Make charSize methods constexpr

--- kandinsky/include/kandinsky/text.h
+++ kandinsky/include/kandinsky/text.h
@@ -2,6 +2,8 @@
 #define KANDINSKY_TEXT_H
 
 #include <kandinsky/size.h>
+#include "../src/large_font.h"
+#include "../src/small_font.h"
 
 class KDText {
 public:
@@ -11,7 +13,9 @@ public:
   };
   constexpr static int k_tabCharacterWidth = 4;
   static KDSize stringSize(const char * text, FontSize size = FontSize::Large);
-  static KDSize charSize(FontSize size = FontSize::Large);
+  constexpr static KDSize charSize(FontSize size = FontSize::Large) {
+    return size == FontSize::Large ? KDSize(BITMAP_LargeFont_CHARACTER_WIDTH, BITMAP_LargeFont_CHARACTER_HEIGHT) : KDSize(BITMAP_SmallFont_CHARACTER_WIDTH, BITMAP_SmallFont_CHARACTER_HEIGHT);
+  }
 };
 
 #endif

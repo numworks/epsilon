6520b6037c3a5086bc42ac149373310508dcdcd7
Léa Saviot <lea.saviot@numworks.com>
2017-12-20 09:55:37 +0100

    [poincare] Empty and EmptyVisible layouts.
    Change-Id: Id3234f406e1bfe7addb704c873bde357546ddf67

---
+++ poincare/src/layout/empty_layout.cpp
@@ -0,0 +1,38 @@
+#include "empty_layout.h"
+#include <poincare/expression_layout_cursor.h>
+#include <assert.h>
+
+namespace Poincare {
+
+EmptyLayout::EmptyLayout() :
+  StaticLayoutHierarchy<0>()
+{
+  m_baseline = 0;
+}
+
+ExpressionLayout * EmptyLayout::clone() const {
+  EmptyLayout * layout = new EmptyLayout();
+  return layout;
+}
+
+bool EmptyLayout::moveLeft(ExpressionLayoutCursor * cursor) {
+  assert(cursor->pointedExpressionLayout() == this);
+  // Ask the parent.
+  cursor->setPosition(ExpressionLayoutCursor::Position::Left);
+  if (m_parent) {
+    return m_parent->moveLeft(cursor);
+  }
+  return false;
+}
+
+bool EmptyLayout::moveRight(ExpressionLayoutCursor * cursor) {
+  assert(cursor->pointedExpressionLayout() == this);
+  // Ask the parent.
+  cursor->setPosition(ExpressionLayoutCursor::Position::Right);
+  if (m_parent) {
+    return m_parent->moveRight(cursor);
+  }
+  return false;
+}
+
+}

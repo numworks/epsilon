3bd04bae0d2a2a3dd567a782da3c7b83402a8b9b
Émilie Feral <emilie.feral@numworks.com>
2017-12-21 17:34:15 +0100

    [apps] Redesign Probability Calculation page

--- apps/probability/calculation_controller.cpp
+++ apps/probability/calculation_controller.cpp
@@ -6,6 +6,14 @@
 #include "calculation/left_integral_calculation.h"
 #include "calculation/right_integral_calculation.h"
 #include "calculation/finite_integral_calculation.h"
+#include "images/calcul1_icon.h"
+#include "images/calcul2_icon.h"
+#include "images/calcul3_icon.h"
+#include "images/calcul4_icon.h"
+#include "images/focused_calcul1_icon.h"
+#include "images/focused_calcul2_icon.h"
+#include "images/focused_calcul3_icon.h"
+#include "images/focused_calcul4_icon.h"
 #include <assert.h>
 #include <cmath>
 
@@ -14,9 +22,9 @@ using namespace Shared;
 
 namespace Probability {
 
-CalculationController::ContentView::ContentView(Responder * parentResponder, CalculationController * calculationController, Calculation * calculation, Law * law) :
+CalculationController::ContentView::ContentView(SelectableTableView * selectableTableView, Law * law, Calculation * calculation) :
   m_titleView(KDText::FontSize::Small, I18n::Message::ComputeProbability, 0.5f, 0.5f, Palette::GreyDark, Palette::WallScreen),
-  m_calculationView(parentResponder, calculationController, calculation, law),
+  m_selectableTableView(selectableTableView),
   m_lawCurveView(law, calculation)
 {
 }
@@ -33,25 +41,45 @@ View * CalculationController::ContentView::subviewAtIndex(int index) {
   if (index == 1) {
     return &m_lawCurveView;
   }
-  return &m_calculationView;
+  return m_selectableTableView;
 }
 
 void CalculationController::ContentView::layoutSubviews() {
   KDCoordinate titleHeight = KDText::charSize(KDText::FontSize::Small).height()+k_titleHeightMargin;
   m_titleView.setFrame(KDRect(0, 0, bounds().width(), titleHeight));
-  KDCoordinate calculationHeight = m_calculationView.minimalSizeForOptimalDisplay().height();
-  m_calculationView.setFrame(KDRect(0,  titleHeight, bounds().width(), bounds().height()-titleHeight));
+  KDCoordinate calculationHeight = ResponderImageCell::k_oneCellHeight;
+  m_selectableTableView->setFrame(KDRect(0,  titleHeight, bounds().width(), calculationHeight));
   m_lawCurveView.setFrame(KDRect(0,  titleHeight+calculationHeight, bounds().width(), bounds().height() - calculationHeight - titleHeight));
 }
 
 CalculationController::CalculationController(Responder * parentResponder, Law * law, Calculation * calculation) :
   ViewController(parentResponder),
-  m_contentView(this, this, calculation, law),
+  m_contentView(&m_selectableTableView, law, calculation),
+  m_selectableTableView(this, this, 0, 0, 0, 0, 0, 0, this, nullptr, false, true, KDColorWhite),
+  m_imageCell(&m_selectableTableView, law, calculation, this),
+  m_draftTextBuffer{},
   m_calculation(calculation),
   m_law(law)
 {
   assert(law != nullptr);
   assert(calculation != nullptr);
+  for (int i = 0; i < k_numberOfCalculationCells; i++) {
+    m_calculationCells[i].editableTextCell()->setParentResponder(&m_selectableTableView);
+    m_calculationCells[i].editableTextCell()->textField()->setDelegate(this);
+    m_calculationCells[i].editableTextCell()->textField()->setDraftTextBuffer(m_draftTextBuffer);
+  }
+}
+
+void CalculationController::didEnterResponderChain(Responder * previousResponder) {
+  App::Snapshot * snapshot = (App::Snapshot *)app()->snapshot();
+  snapshot->setActivePage(App::Snapshot::Page::Calculations);
+  updateTitle();
+  reloadLawCurveView();
+  m_selectableTableView.reloadData();
+}
+
+void CalculationController::didBecomeFirstResponder() {
+  app()->setFirstResponder(&m_selectableTableView);
 }
 
 View * CalculationController::view() {
@@ -62,12 +90,157 @@ const char * CalculationController::title() {
   return m_titleBuffer;
 }
 
+void CalculationController::viewWillAppear() {
+  ViewController::viewWillAppear();
+  selectCellAtLocation(1, 0);
+}
+
+void CalculationController::viewDidDisappear() {
+  m_selectableTableView.deselectTable();
+  ViewController::viewDidDisappear();
+}
+
+int CalculationController::numberOfRows() {
+  return 1;
+}
+
+int CalculationController::numberOfColumns() {
+  return m_calculation->numberOfParameters()+1;
+}
+
+KDCoordinate CalculationController::columnWidth(int i) {
+  if (i == 0) {
+    return m_imageCell.minimalSizeForOptimalDisplay().width();
+  }
+  return m_calculationCells[i-1].minimalSizeForOptimalDisplay().width();
+}
+
+KDCoordinate CalculationController::rowHeight(int j) {
+  return ResponderImageCell::k_oneCellHeight;
+}
+
+KDCoordinate CalculationController::cumulatedWidthFromIndex(int j) {
+  int result = 0;
+  for (int k = 0; k < j; k++) {
+    result += columnWidth(k);
+  }
+  return result;
+}
+
+int CalculationController::indexFromCumulatedWidth(KDCoordinate offsetX) {
+  int result = 0;
+  int i = 0;
+  while (result < offsetX && i < numberOfColumns()) {
+    result += columnWidth(i++);
+  }
+  return (result < offsetX || offsetX == 0) ? i : i - 1;
+}
+
+KDCoordinate CalculationController::cumulatedHeightFromIndex(int j) {
+  return rowHeight(0) * j;
+}
+
+int CalculationController::indexFromCumulatedHeight(KDCoordinate offsetY) {
+  KDCoordinate height = rowHeight(0);
+  if (height == 0) {
+    return 0;
+  }
+  return (offsetY - 1) / height;
+}
+
+HighlightCell * CalculationController::reusableCell(int index, int type) {
+  if (type == 0) {
+    assert(index == 0);
+    return &m_imageCell;
+  }
+  assert(index >= 0 && index < k_numberOfCalculationCells);
+  return &m_calculationCells[index];
+}
+
+int CalculationController::reusableCellCount(int type) {
+  if (type == 0) {
+    return 1;
+  }
+  return k_numberOfCalculationCells;
+}
+
+int CalculationController::typeAtLocation(int i, int j) {
+  if (i == 0 && j == 0) {
+    return 0;
+  }
+  return 1;
+}
+
+void CalculationController::willDisplayCellAtLocation(HighlightCell * cell, int i, int j) {
+  if (i == 0) {
+    ResponderImageCell * myCell = static_cast<ResponderImageCell *>(cell);
+    const Image *  images[CalculationTypeController::k_numberOfImages] = {ImageStore::Calcul1Icon, ImageStore::Calcul2Icon, ImageStore::Calcul3Icon, ImageStore::Calcul4Icon};
+    const Image * focusedImages[CalculationTypeController::k_numberOfImages] = {ImageStore::FocusedCalcul1Icon, ImageStore::FocusedCalcul2Icon, ImageStore::FocusedCalcul3Icon, ImageStore::FocusedCalcul4Icon};
+    myCell->setImage(images[(int)m_calculation->type()], focusedImages[(int)m_calculation->type()]);
+  } else {
+    CalculationCell * myCell = static_cast<CalculationCell *>(cell);
+    myCell->messageTextView()->setMessage(m_calculation->legendForParameterAtIndex(i-1));
+    bool calculationCellIsResponder = true;
+    if ((m_law->type() != Law::Type::Normal && i == 3) || (m_calculation->type() == Calculation::Type::Discrete && i == 2)) {
+      calculationCellIsResponder = false;
+    }
+    myCell->setResponder(calculationCellIsResponder);
+    TextField * field = static_cast<CalculationCell *>(cell)->editableTextCell()->textField();
+    if (field->isEditing()) {
+      return;
+    }
+    char buffer[PrintFloat::bufferSizeForFloatsWithPrecision(Constant::LargeNumberOfSignificantDigits)];
+    Complex<double>::convertFloatToText(m_calculation->parameterAtIndex(i-1), buffer, PrintFloat::bufferSizeForFloatsWithPrecision(Constant::LargeNumberOfSignificantDigits), Constant::LargeNumberOfSignificantDigits, Expression::FloatDisplayMode::Decimal);
+    field->setText(buffer);
+  }
+}
+
+bool CalculationController::textFieldDidHandleEvent(::TextField * textField, Ion::Events::Event event, bool returnValue, bool textHasChanged) {
+  if (returnValue && textHasChanged) {
+    m_selectableTableView.reloadData(); //TODO: optimize with reloadCell at index?
+  }
+  return returnValue;
+}
+
+bool CalculationController::textFieldShouldFinishEditing(TextField * textField, Ion::Events::Event event) {
+  return TextFieldDelegate::textFieldShouldFinishEditing(textField, event)
+       || (event == Ion::Events::Right && textField->cursorLocation() == textField->draftTextLength() && selectedColumn() < m_calculation->numberOfParameters())
+       || (event == Ion::Events::Left && textField->cursorLocation() == 0);
+}
+
+bool CalculationController::textFieldDidFinishEditing(TextField * textField, const char * text, Ion::Events::Event event) {
+  App * probaApp = (App *)app();
+  Context * globalContext = probaApp->container()->globalContext();
+  double floatBody = Expression::approximateToScalar<double>(text, *globalContext);
+  if (std::isnan(floatBody) || std::isinf(floatBody)) {
+    app()->displayWarning(I18n::Message::UndefinedValue);
+    return false;
+  }
+  if (m_calculation->type() != Calculation::Type::FiniteIntegral && selectedColumn() == 2) {
+    if (floatBody < 0.0) {
+      floatBody = 0.0;
+    }
+    if (floatBody > 1.0) {
+      floatBody = 1.0;
+    }
+  }
+  if (!m_law->isContinuous() && (selectedColumn() == 1 || m_calculation->type() == Calculation::Type::FiniteIntegral)) {
+    floatBody = std::round(floatBody);
+  }
+  m_calculation->setParameterAtIndex(floatBody, selectedColumn()-1);
+  if (event == Ion::Events::Right || event == Ion::Events::Left) {
+    m_selectableTableView.handleEvent(event);
+  }
+  reload();
+  return true;
+}
+
 void CalculationController::reloadLawCurveView() {
   m_contentView.lawCurveView()->reload();
 }
 
 void CalculationController::reload() {
-  m_contentView.calculationView()->reload();
+  m_selectableTableView.reloadData();
   reloadLawCurveView();
 }
 
@@ -93,22 +266,10 @@ void CalculationController::setCalculationAccordingToIndex(int index, bool force
      return;
   }
   m_calculation->setLaw(m_law);
-  reload();
 }
 
-void CalculationController::viewWillAppear() {
-  reloadLawCurveView();
-  m_contentView.calculationView()->selectSubview(1);
-}
-
-void CalculationController::didEnterResponderChain(Responder * previousResponder) {
-  App::Snapshot * snapshot = (App::Snapshot *)app()->snapshot();
-  snapshot->setActivePage(App::Snapshot::Page::Calculations);
-  updateTitle();
-}
-
-void CalculationController::didBecomeFirstResponder() {
-  app()->setFirstResponder(m_contentView.calculationView());
+TextFieldDelegateApp * CalculationController::textFieldDelegateApp() {
+  return (App *)app();
 }
 
 void CalculationController::updateTitle() {
--- apps/probability/calculation_controller.h
+++ apps/probability/calculation_controller.h
@@ -4,33 +4,57 @@
 #include <escher.h>
 #include "law/law.h"
 #include "law_curve_view.h"
-#include "calculation_view.h"
+#include "calculation_cell.h"
+#include "responder_image_cell.h"
 #include "calculation/calculation.h"
 #include "../shared/parameter_text_field_delegate.h"
 
 namespace Probability {
 
-class CalculationController : public ViewController {
+class CalculationController : public ViewController, public TableViewDataSource, public SelectableTableViewDataSource, public Shared::ParameterTextFieldDelegate {
 public:
   CalculationController(Responder * parentResponder, Law * law, Calculation * calculation);
-  View * view() override;
-  const char * title() override;
-  void reload();
-  void reloadLawCurveView();
-  void setCalculationAccordingToIndex(int index, bool forceReinitialisation = false);
-  void viewWillAppear() override;
+  /* Responder */
   void didEnterResponderChain(Responder * previousResponder) override;
   void didBecomeFirstResponder() override;
+
+  /* ViewController */
+  View * view() override;
+  const char * title() override;
+  void viewWillAppear() override;
+  void viewDidDisappear() override;
+
+  /* TableViewDataSource */
+  int numberOfRows() override;
+  int numberOfColumns() override;
+  KDCoordinate columnWidth(int i) override;
+  KDCoordinate rowHeight(int j) override;
+  KDCoordinate cumulatedWidthFromIndex(int i) override;
+  KDCoordinate cumulatedHeightFromIndex(int j) override;
+  int indexFromCumulatedWidth(KDCoordinate offsetX) override;
+  int indexFromCumulatedHeight(KDCoordinate offsetY) override;
+  HighlightCell * reusableCell(int index, int type) override;
+  int reusableCellCount(int type) override;
+  int typeAtLocation(int i, int j) override;
+  void willDisplayCellAtLocation(HighlightCell * cell, int i, int j) override;
+
+  /* TextField delegate */
+  bool textFieldDidHandleEvent(TextField * textField, Ion::Events::Event event, bool returnValue, bool textHasChanged) override;
+  bool textFieldShouldFinishEditing(TextField * textField, Ion::Events::Event event) override;
+  bool textFieldDidFinishEditing(TextField * textField, const char * text, Ion::Events::Event event) override;
+
+  void reloadLawCurveView();
+  void reload();
+  void setCalculationAccordingToIndex(int index, bool forceReinitialisation = false);
 private:
+  constexpr static int k_numberOfCalculationCells = 3;
+  Shared::TextFieldDelegateApp * textFieldDelegateApp() override;
   void updateTitle();
   class ContentView : public View {
   public:
-    ContentView(Responder * parentResponder, CalculationController * calculationController, Calculation * Calculation, Law * law);
+    ContentView(SelectableTableView * selectableTableView, Law * law, Calculation * calculation);
     LawCurveView * lawCurveView() {
       return &m_lawCurveView;
-    }
-    CalculationView * calculationView() {
-      return &m_calculationView;
     }
   private:
     constexpr static KDCoordinate k_titleHeightMargin = 5;
@@ -38,10 +62,14 @@ private:
     View * subviewAtIndex(int index) override;
     void layoutSubviews() override;
     MessageTextView m_titleView;
-    CalculationView m_calculationView;
+    SelectableTableView * m_selectableTableView;
     LawCurveView m_lawCurveView;
   };
   ContentView m_contentView;
+  SelectableTableView m_selectableTableView;
+  ResponderImageCell m_imageCell;
+  char m_draftTextBuffer[TextField::maxBufferSize()];
+  CalculationCell m_calculationCells[k_numberOfCalculationCells];
   Calculation * m_calculation;
   Law * m_law;
   constexpr static int k_maxNumberOfTitleCharacters = 30;

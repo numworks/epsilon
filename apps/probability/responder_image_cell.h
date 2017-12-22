f95b961463e93bbb929f8676ea68d5d27b17f90d
Émilie Feral <emilie.feral@numworks.com>
2017-12-22 14:16:49 +0100

    [apps] Probability: clean table margins (replace weird cell margins by table margins)

--- apps/probability/responder_image_cell.h
+++ apps/probability/responder_image_cell.h
@@ -20,10 +20,8 @@ public:
   void setHighlighted(bool highlight) override;
   void setImage(const Image * image, const Image * focusedImage);
   constexpr static KDCoordinate k_outline = 1;
-  constexpr static KDCoordinate k_margin = 3;
-  constexpr static KDCoordinate k_totalMargin = k_outline+k_margin;
-  constexpr static KDCoordinate k_oneCellWidth = 2*k_totalMargin+ImageCell::k_width;
-  constexpr static KDCoordinate k_oneCellHeight = 2*k_totalMargin+ImageCell::k_height;
+  constexpr static KDCoordinate k_oneCellWidth = 2*k_outline+ImageCell::k_width;
+  constexpr static KDCoordinate k_oneCellHeight = 2*k_outline+ImageCell::k_height;
 private:
   int numberOfSubviews() const override;
   View * subviewAtIndex(int index) override;

b8ac854be23d507a9e7fd0d0dfd50b0a852ccf46
Romain Goyet <romain.goyet@numworks.com>
2018-01-08 12:11:00 +0100

    Configure the USB GPIO's alternate functions
    Change-Id: Iee2c685645778accad88cd2008c4faac0f119224

--- ion/src/device/usb.cpp
+++ ion/src/device/usb.cpp
@@ -26,11 +26,20 @@ void initGPIO() {
    * plugged, the pin must be pulled down. */
   VbusPin.group().MODER()->setMode(VbusPin.pin(), GPIO::MODER::Mode::Input);
   VbusPin.group().PUPDR()->setPull(VbusPin.pin(), GPIO::PUPDR::Pull::Down);
+
+  DmPin.group().MODER()->setMode(DmPin.pin(), GPIO::MODER::Mode::AlternateFunction);
+  DmPin.group().AFR()->setAlternateFunction(DmPin.pin(), GPIO::AFR::AlternateFunction::AF10);
+
+  DpPin.group().MODER()->setMode(DpPin.pin(), GPIO::MODER::Mode::AlternateFunction);
+  DpPin.group().AFR()->setAlternateFunction(DpPin.pin(), GPIO::AFR::AlternateFunction::AF10);
 }
 
 void shutdown() {
-  VbusPin.group().MODER()->setMode(VbusPin.pin(), GPIO::MODER::Mode::Analog);
-  VbusPin.group().PUPDR()->setPull(VbusPin.pin(), GPIO::PUPDR::Pull::None);
+  constexpr static GPIOPin USBPins[] = {DpPin, DmPin, VbusPin};
+  for (const GPIOPin & g : USBPins) {
+    g.group().MODER()->setMode(g.pin(), GPIO::MODER::Mode::Analog);
+    g.group().PUPDR()->setPull(g.pin(), GPIO::PUPDR::Pull::None);
+  }
 }
 
 }

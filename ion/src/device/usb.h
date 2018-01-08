74e0175bf9462b709c5108ccb95165c061632cd6
Romain Goyet <romain.goyet@numworks.com>
2018-01-08 12:11:00 +0100

    Configure the USB GPIO's alternate functions
    Change-Id: Iee2c685645778accad88cd2008c4faac0f119224

--- ion/src/device/usb.h
+++ ion/src/device/usb.h
@@ -10,6 +10,8 @@ namespace Device {
 /*  Pin | Role              | Mode                  | Function
  * -----+-------------------+-----------------------+----------
  *  PA9 | VBUS              | Input, pulled down    | Low = unplugged, high = plugged
+ * PA11 | USB D-            | Alternate Function 10 |
+ * PA12 | USB D+            | Alternate Function 10 |
  */
 
 void init();
@@ -17,6 +19,8 @@ void initGPIO();
 void shutdown();
 
 constexpr static GPIOPin VbusPin = GPIOPin(GPIOA, 9);
+constexpr static GPIOPin DmPin = GPIOPin(GPIOA, 11);
+constexpr static GPIOPin DpPin = GPIOPin(GPIOA, 12);
 
 }
 }

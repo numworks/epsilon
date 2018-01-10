bbfa04abc8b4acb4d46381cd7c5fe345f97ea9c1
Émilie Feral <emilie.feral@numworks.com>
2018-01-10 15:40:20 +0100

    [apps] Graph: implement coloring area under curve in graph view

--- apps/graph/graph/graph_view.cpp
+++ apps/graph/graph/graph_view.cpp
@@ -24,11 +24,23 @@ void GraphView::drawRect(KDContext * ctx, KDRect rect) const {
   FunctionGraphView::drawRect(ctx, rect);
   for (int i = 0; i < m_functionStore->numberOfActiveFunctions(); i++) {
     CartesianFunction * f = m_functionStore->activeFunctionAtIndex(i);
-    drawCurve(ctx, rect, [](float t, void * model, void * context) {
+
+    /* Draw function (color the area under curve of the selected function) */
+    if (f == m_selectedFunction) {
+      drawCurve(ctx, rect, [](float t, void * model, void * context) {
+        CartesianFunction * f = (CartesianFunction *)model;
+        Poincare::Context * c = (Poincare::Context *)context;
+        return f->evaluateAtAbscissa(t, c);
+      }, f, context(), f->color(), true, m_highlightedStart, m_highlightedEnd);
+    } else {
+      drawCurve(ctx, rect, [](float t, void * model, void * context) {
         CartesianFunction * f = (CartesianFunction *)model;
         Poincare::Context * c = (Poincare::Context *)context;
         return f->evaluateAtAbscissa(t, c);
       }, f, context(), f->color());
+    }
+
+    /* Draw tangent */
     if (m_tangent && f == m_selectedFunction) {
       float tangentParameter[2];
       tangentParameter[0] = f->approximateDerivative(m_curveViewCursor->x(), context());

6ee81c2ed4d235dfc55e4e02af8e087325a3c6ff
Émilie Feral <emilie.feral@numworks.com>
2018-01-18 11:06:42 +0100

    [ion] Correct random() implementation in simulator, blackbox and emscripten

--- ion/src/shared/random.cpp
+++ ion/src/shared/random.cpp
@@ -2,5 +2,11 @@
 #include <stdlib.h>
 
 uint32_t Ion::random() {
-  return rand();
+  /* rand() returns a pseudo-random integral number in the range between 0 and
+   * RAND_MAX which is garantueed to be at least 32767.*/
+  uint32_t result = 0;
+  for (int i = 0; i < 131077; i++) {
+    result += rand();
+  }
+  return result;
 }

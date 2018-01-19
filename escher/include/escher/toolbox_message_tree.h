2cb895666509e758f87b5947c2264298be789cb4
Léa Saviot <lea.saviot@numworks.com>
2018-01-19 16:18:55 +0100

    [apps/math_toolbox] Remove the pointedPath from ToolboxMessageTree.
    Change-Id: I2deb6564321da13ea27975216aa949b68298533c

--- escher/include/escher/toolbox_message_tree.h
+++ escher/include/escher/toolbox_message_tree.h
@@ -6,26 +6,20 @@
 
 class ToolboxMessageTree : public MessageTree {
 public:
-  constexpr ToolboxMessageTree(I18n::Message label = (I18n::Message)0, I18n::Message text = (I18n::Message)0, I18n::Message insertedText = (I18n::Message)0, const ToolboxMessageTree * children = nullptr, int numberOfChildren = 0, int * pointedLayoutPath = nullptr, int pointedLayoutPathLength = 0) :
+  constexpr ToolboxMessageTree(I18n::Message label = (I18n::Message)0, I18n::Message text = (I18n::Message)0, I18n::Message insertedText = (I18n::Message)0, const ToolboxMessageTree * children = nullptr, int numberOfChildren = 0) :
     MessageTree(label, numberOfChildren),
     m_children(children),
     m_text(text),
-    m_insertedText(insertedText),
-    m_pointedLayoutPath(pointedLayoutPath),
-    m_pointedLayoutPathLength(pointedLayoutPathLength)
+    m_insertedText(insertedText)
   {
   };
   const MessageTree * children(int index) const override { return &m_children[index]; }
   I18n::Message text() const { return m_text; }
   I18n::Message insertedText() const { return m_insertedText; }
-  int * pointedPath() const { return m_pointedLayoutPath; }
-  int pointedPathLength() const { return m_pointedLayoutPathLength; }
 private:
   const ToolboxMessageTree * m_children;
   I18n::Message m_text;
   I18n::Message m_insertedText;
-  int * m_pointedLayoutPath;
-  int m_pointedLayoutPathLength;
 };
 
 #endif

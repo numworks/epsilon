9943afb7e11e71bbd98016265b39aaffdfa6a76e
Émilie Feral <emilie.feral@numworks.com>
2018-01-22 14:24:14 +0100

    [apps] Graph: add a method to get the intersection between 2 functions

--- apps/graph/cartesian_function.h
+++ apps/graph/cartesian_function.h
@@ -20,16 +20,18 @@ public:
   Point nextMinimumFrom(double start, double step, double max, Poincare::Context * context) const;
   Point nextMaximumFrom(double start, double step, double max, Poincare::Context * context) const;
   double nextRootFrom(double start, double step, double max, Poincare::Context * context) const;
+  Point nextIntersectionFrom(double start, double step, double max, Poincare::Context * context, const Shared::Function * function) const;
   char symbol() const override;
 private:
   constexpr static double k_sqrtEps = 1.4901161193847656E-8; // sqrt(DBL_EPSILON)
   constexpr static double k_goldenRatio = 0.381966011250105151795413165634361882279690820194237137864; // (3-sqrt(5))/2
-  typedef double (*Evaluation)(const Shared::Function * function, double abscissa, Poincare::Context * context);
-  Point nextMinimumOfFunction(double start, double step, double max, Evaluation evaluation, Poincare::Context * context, bool lookForRootMinimum) const;
-  void bracketMinimum(double start, double step, double max, double result[3], Evaluation evaluation, Poincare::Context * context) const;
-  Point brentMinimum(double ax, double bx, Evaluation evaluation, Poincare::Context * context) const;
-  void bracketRoot(double start, double step, double max, double result[2], Poincare::Context * context) const;
-  double brentRoot(double ax, double bx, double precision, Poincare::Context * context) const;
+  typedef double (*Evaluation)(double abscissa, Poincare::Context * context, const Shared::Function * function0, const Shared::Function * function1);
+  Point nextMinimumOfFunction(double start, double step, double max, Evaluation evaluation, Poincare::Context * context, const Shared::Function * function = nullptr, bool lookForRootMinimum = false) const;
+  void bracketMinimum(double start, double step, double max, double result[3], Evaluation evaluation, Poincare::Context * context, const Shared::Function * function= nullptr) const;
+  Point brentMinimum(double ax, double bx, Evaluation evaluation, Poincare::Context * context, const Shared::Function * function = nullptr) const;
+  double nextIntersectionWithFunction(double start, double step, double max, Evaluation evaluation, Poincare::Context * context, const Shared::Function * function) const;
+  void bracketRoot(double start, double step, double max, double result[2], Evaluation evaluation, Poincare::Context * context, const Shared::Function * function) const;
+  double brentRoot(double ax, double bx, double precision, Evaluation evaluation, Poincare::Context * context, const Shared::Function * function) const;
   bool m_displayDerivative;
 };
 

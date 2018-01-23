d60bea711b20c40f4fbee68f3a867d93ac01be7a
Émilie Feral <emilie.feral@numworks.com>
2018-01-23 09:49:25 +0100

    [poincare] Arithmetic::PrimeFactorization returns special cases when the factorization fails. factor(integer) can fail and returns Undefined

--- poincare/src/factor.cpp
+++ poincare/src/factor.cpp
@@ -31,31 +31,38 @@ Expression * Factor::shallowBeautify(Context& context, AngleUnit angleUnit) {
   if (r->isZero()) {
     return replaceWith(r, true);
   }
-  Multiplication * numeratorDecomp = addFactorsOfIntegerDecomposition(r->numerator(), context, angleUnit);
+  Expression * numeratorDecomp = createMultiplicationOfIntegerPrimeDecomposition(r->numerator(), context, angleUnit);
   Expression * result = numeratorDecomp;
+  if (result->type() == Type::Undefined) {
+    return replaceWith(result, true);
+  }
+  assert(numeratorDecomp->type() == Type::Multiplication);
   if (!r->denominator().isOne()) {
-    Multiplication * denominatorDecomp = addFactorsOfIntegerDecomposition(r->denominator(), context, angleUnit);
+    Expression * denominatorDecomp = createMultiplicationOfIntegerPrimeDecomposition(r->denominator(), context, angleUnit);
+    if (denominatorDecomp->type() == Type::Undefined) {
+      delete result;
+      return replaceWith(denominatorDecomp, true);
+    }
+    assert(denominatorDecomp->type() == Type::Multiplication);
     result = new Division(numeratorDecomp, denominatorDecomp, false);
-    denominatorDecomp->squashUnaryHierarchy();
+    static_cast<Multiplication *>(denominatorDecomp)->squashUnaryHierarchy();
   }
   if (r->sign() == Sign::Negative) {
     result = new Opposite(result, false);
   }
   replaceWith(result, true);
   if (result == numeratorDecomp) {
-    return numeratorDecomp->squashUnaryHierarchy();
+    return static_cast<Multiplication *>(numeratorDecomp)->squashUnaryHierarchy();
   }
-  numeratorDecomp->squashUnaryHierarchy();
+  static_cast<Multiplication *>(numeratorDecomp)->squashUnaryHierarchy();
   return result;
 }
 
-Multiplication * Factor::addFactorsOfIntegerDecomposition(Integer i, Context & context, AngleUnit angleUnit) {
+Expression * Factor::createMultiplicationOfIntegerPrimeDecomposition(Integer i, Context & context, AngleUnit angleUnit) {
   assert(!i.isZero());
   i.setNegative(false);
   Multiplication * m = new Multiplication();
-  if (Arithmetic::k_primorial32.isLowerThan(i) || i.isOne()) {
-    /* We do not want to break i in prime factor because it might be take too
-     * many factors... More than k_maxNumberOfPrimeFactors. */
+  if (i.isOne()) {
     m->addOperand(new Rational(i));
     return m;
   }
@@ -63,6 +70,9 @@ Multiplication * Factor::addFactorsOfIntegerDecomposition(Integer i, Context & c
   Integer coefficients[Arithmetic::k_maxNumberOfPrimeFactors];
   Arithmetic::PrimeFactorization(&i, factors, coefficients, Arithmetic::k_maxNumberOfPrimeFactors);
   int index = 0;
+  if (coefficients[0].isMinusOne()) {
+    return new Undefined();
+  }
   while (!coefficients[index].isZero() && index < Arithmetic::k_maxNumberOfPrimeFactors) {
     Expression * factor = new Rational(factors[index]);
     if (!coefficients[index].isOne()) {

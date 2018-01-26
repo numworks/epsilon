01459ebed63171b10d5d57404927fccc2250efad
Damien Nicolet <zardam@gmail.com>
2018-01-26 23:39:25 +0100

    Full interrupt vector table definition, more registers and correct memory remap & vector table register after bootloader exit

--- ion/src/device/regs/exti.h
+++ ion/src/device/regs/exti.h
@@ -11,16 +11,18 @@ public:
     void set(int index, bool state) volatile { setBitRange(index, index, state); }
   };
 
-  //class IMR : public MaskRegister { };
+  class IMR : public MaskRegister { };
   class EMR : public MaskRegister { };
   class RTSR : public MaskRegister { };
   class FTSR : public MaskRegister { };
+  class PR : public MaskRegister { };
 
   constexpr EXTI() {};
-  //REGS_REGISTER_AT(IMR, 0x00);
+  REGS_REGISTER_AT(IMR, 0x00);
   REGS_REGISTER_AT(EMR, 0x04);
   REGS_REGISTER_AT(RTSR, 0x08);
   REGS_REGISTER_AT(FTSR, 0x0C);
+  REGS_REGISTER_AT(PR, 0x14);
 private:
   constexpr uint32_t Base() const {
     return 0x40013C00;

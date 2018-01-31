36b95dc3c31cb710d28ddfcec73b273511b8f9ef
Émilie Feral <emilie.feral@numworks.com>
2018-01-31 15:37:02 +0100

    [apps] Calculation: input text stores in cache have to be checked again when used as its length context dependent (because of 'ans' symbol)

--- apps/calculation/app.cpp
+++ apps/calculation/app.cpp
@@ -53,30 +53,36 @@ bool App::textFieldDidReceiveEvent(::TextField * textField, Ion::Events::Event e
   if ((event == Ion::Events::Var ||  event == Ion::Events::XNT) && TextFieldDelegateApp::textFieldDidReceiveEvent(textField, event)) {
     return true;
   }
-  /* Here, we check that the expression entered by the user can be printed with
-   * less than k_printedExpressionLength characters. Otherwise, we prevent the
-   * user from adding this expression to the calculation store. */
   if (textField->isEditing() && textField->textFieldShouldFinishEditing(event)) {
     if (textField->text()[0] == 0) {
       return true;
     }
-    Expression * exp = Expression::parse(textField->text());
-    if (exp == nullptr) {
-      textField->app()->displayWarning(I18n::Message::SyntaxError);
-      return true;
-    }
-    Expression::ReplaceSymbolWithExpression(&exp, Symbol::SpecialSymbols::Ans, static_cast<Snapshot *>(snapshot())->calculationStore()->ansExpression(localContext()));
-    char buffer[Calculation::k_printedExpressionSize];
-    int length = exp->writeTextInBuffer(buffer, sizeof(buffer));
-    delete exp;
-    /* if the buffer is totally full, it is VERY likely that writeTextInBuffer
-     * escaped before printing utterly the expression. */
-    if (length >= Calculation::k_printedExpressionSize-1) {
+    if (!textInputIsCorrect(textField->text())) {
       displayWarning(I18n::Message::SyntaxError);
       return true;
     }
   }
   return false;
+}
+
+bool App::textInputIsCorrect(const char * text) {
+  /* Here, we check that the expression entered by the user can be printed with
+   * less than k_printedExpressionLength characters. Otherwise, we prevent the
+   * user from adding this expression to the calculation store. */
+  Expression * exp = Expression::parse(text);
+  if (exp == nullptr) {
+    return false;
+  }
+  Expression::ReplaceSymbolWithExpression(&exp, Symbol::SpecialSymbols::Ans, static_cast<Snapshot *>(snapshot())->calculationStore()->ansExpression(localContext()));
+  char buffer[Calculation::k_printedExpressionSize];
+  int length = exp->writeTextInBuffer(buffer, sizeof(buffer));
+  delete exp;
+  /* if the buffer is totally full, it is VERY likely that writeTextInBuffer
+   * escaped before printing utterly the expression. */
+  if (length >= Calculation::k_printedExpressionSize-1) {
+    return false;
+  }
+  return true;
 }
 
 const char * App::XNT() {

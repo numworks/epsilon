507720cc6eee729e991a37242e03ebc2f9abfdac
Romain Goyet <romain.goyet@numworks.com>
2018-02-18 12:36:04 +0100

    [build] Don't use the Set module
    It's been deprecated since Python 2.6

--- apps/i18n.py
+++ apps/i18n.py
@@ -3,7 +3,6 @@
 import sys
 import re
 import unicodedata
-from sets import Set
 import argparse
 
 ion_special_characters = {
@@ -53,8 +52,8 @@ def locale_from_filename(filename):
 
 def parse_files(files):
     data = {}
-    messages = Set()
-    universal_messages = Set()
+    messages = set()
+    universal_messages = set()
     for path in files:
         locale = locale_from_filename(path)
         if locale not in data:

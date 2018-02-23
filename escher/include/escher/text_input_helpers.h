4b8efaaed0c4adb08fb11b79bda21be36e117640
Émilie Feral <emilie.feral@numworks.com>
2018-02-23 11:36:30 +0100

    [escher] TextInputHelper: special case for 'random()' and handle parenthesis already completed (ie 'u(n)')

--- escher/include/escher/text_input_helpers.h
+++ escher/include/escher/text_input_helpers.h
@@ -8,9 +8,12 @@ namespace TextInputHelpers {
 int CursorIndexInCommand(const char * text);
 /* Returns the index of the cursor position in a Command, which is the smallest
  * index between :
- *   - After the first open parenthesis
- *   - The end of the text */
-
+ *   - After the first open parenthesis/quote if the following element is
+ *   either a quote, a coma or a parenthesi
+ *   - The end of the text
+ *   - Special case: when the text preceding the parenthesis is 'random', the
+ *   cursor position is the end of the text. */
+constexpr static const char * k_random = "random";
 }
 
 #endif

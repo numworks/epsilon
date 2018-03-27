e939a04d1bfbdfc0a2f7e7714160da5cdd7f825b
Léa Saviot <lea.saviot@numworks.com>
2018-03-27 17:29:25 +0200

    [usb] Clean Interface code.
    Change-Id: I06526265a6876bc905bcce668f38f24df64c9b27

--- ion/src/device/usb/interface.cpp
+++ ion/src/device/usb/interface.cpp
@@ -16,7 +16,7 @@ bool Interface::processSetupInRequest(SetupPacket * request, uint8_t * transferB
     case (int) Request::SetInterface:
       return setInterface(request, transferBufferLength);
     case (int) Request::GetInterface:
-      return getInterface(transferBuffer, transferBufferLength);
+      return getInterface(transferBuffer, transferBufferLength, transferBufferMaxLength);
     case (int) Request::ClearFeature:
       return clearFeature(transferBufferLength);
     case (int) Request::SetFeature:
@@ -28,35 +28,35 @@ bool Interface::processSetupInRequest(SetupPacket * request, uint8_t * transferB
 bool Interface::getStatus(uint8_t * transferBuffer, uint16_t * transferBufferLength, uint16_t transferBufferMaxLength) {
   *transferBufferLength = min(2, transferBufferMaxLength);
   for (int i = 0; i<*transferBufferLength; i++) {
-    transferBuffer[i] = 0; // By specification
+    transferBuffer[i] = 0; // Reserved, must be set to 0
   }
   return true;
 }
 
-bool Interface::getInterface(uint8_t * transferBuffer, uint16_t * transferBufferLength) {
-  *transferBufferLength = 1;
-  transferBuffer[0] = getActiveInterfaceAlternative();
+bool Interface::getInterface(uint8_t * transferBuffer, uint16_t * transferBufferLength, uint16_t transferBufferMaxLength) {
+  *transferBufferLength = min(1, transferBufferMaxLength);;
+  if (*transferBufferLength > 0) {
+    transferBuffer[0] = getActiveInterfaceAlternative();
+  }
   return true;
 }
 
 bool Interface::setInterface(SetupPacket * request, uint16_t * transferBufferLength) {
   // We support one interface only
   setActiveInterfaceAlternative(request->wValue());
-  /* There is one interface alternative only, we no need to set it again, just
-   * reset the endpoint. */
-  //m_ep0->reset(); //TODO Needed?
+  // There is one interface alternative only, we no need to set it again.
   *transferBufferLength = 0;
   return true;
 }
 
 bool Interface::clearFeature(uint16_t * transferBufferLength) {
-  //TODO ?
+  // Not needed for now
   *transferBufferLength = 0;
   return true;
 }
 
 bool Interface::setFeature(uint16_t * transferBufferLength) {
-  //TODO ?
+  // Not needed for now
   *transferBufferLength = 0;
   return true;
 }

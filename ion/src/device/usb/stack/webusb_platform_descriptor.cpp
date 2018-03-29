a489ca34311f04541e1b6d65d36d72345d82fa6f
Léa Saviot <lea.saviot@numworks.com>
2018-03-29 14:05:53 +0200

    [usb] PlatformDeviceCapabilityDescriptor class.
    Change-Id: I5db54eb2c8beb47fad952c63da4e37c392a426cb

--- ion/src/device/usb/stack/webusb_platform_descriptor.cpp
+++ ion/src/device/usb/stack/webusb_platform_descriptor.cpp
@@ -4,19 +4,17 @@ namespace Ion {
 namespace USB {
 namespace Device {
 
+constexpr uint8_t WebUSBPlatformDescriptor::k_webUSBUUID[];
+
 void WebUSBPlatformDescriptor::push(Channel * c) const {
-  DeviceCapabilityDescriptor::push(c);
-  c->push(m_bReserved);
-  for (int i = 0; i < k_platformCapabilityUUIDSize; i++) {
-    c->push(m_platformCapabilityUUID[i]);
-  }
+  PlatformDeviceCapabilityDescriptor::push(c);
   c->push(m_bcdVersion);
   c->push(m_bVendorCode);
   c->push(m_iLandingPage);
 }
 
 uint8_t WebUSBPlatformDescriptor::bLength() const {
-  return DeviceCapabilityDescriptor::bLength() + sizeof(uint8_t) + k_platformCapabilityUUIDSize*sizeof(uint8_t) + sizeof(uint16_t) + 2*sizeof(uint8_t);
+  return PlatformDeviceCapabilityDescriptor::bLength() + sizeof(uint16_t) + 2*sizeof(uint8_t);
 }
 
 }
--- ion/src/device/usb/stack/webusb_platform_descriptor.h
+++ ion/src/device/usb/stack/webusb_platform_descriptor.h
@@ -1,22 +1,16 @@
 #ifndef ION_DEVICE_USB_STACK_WEBUSB_PLATFORM_DESCRIPTOR_H
 #define ION_DEVICE_USB_STACK_WEBUSB_PLATFORM_DESCRIPTOR_H
 
-#include "device_capability_descriptor.h"
+#include "platform_device_capability_descriptor.h"
 
 namespace Ion {
 namespace USB {
 namespace Device {
 
-class WebUSBPlatformDescriptor : public DeviceCapabilityDescriptor {
+class WebUSBPlatformDescriptor : public PlatformDeviceCapabilityDescriptor {
 public:
   constexpr WebUSBPlatformDescriptor(uint8_t bVendorCode, uint8_t iLandingPage) :
-    DeviceCapabilityDescriptor(0x05),
-    m_bReserved(0),
-    m_platformCapabilityUUID{
-      /* Little-endian encoding of {3408b638-09a9-47a0-8bfd-a0768815b665}.
-       * See https://wicg.github.io/webusb/#webusb-platform-capability-descriptor */
-      0x38, 0xB6, 0x08, 0x34, 0xA9, 0x09, 0xA0, 0x47,
-      0x8B, 0xFD, 0xA0, 0x76, 0x88, 0x15, 0xB6, 0x65},
+    PlatformDeviceCapabilityDescriptor(k_webUSBUUID),
     m_bcdVersion(0x0100),
     m_bVendorCode(bVendorCode),
     m_iLandingPage(iLandingPage)
@@ -26,9 +20,11 @@ protected:
   void push(Channel * c) const override;
   virtual uint8_t bLength() const override;
 private:
-  constexpr static uint8_t k_platformCapabilityUUIDSize = 16;
-  uint8_t m_bReserved;
-  uint8_t m_platformCapabilityUUID[k_platformCapabilityUUIDSize];
+  /* Little-endian encoding of {3408B638-09A9-47A0-8BFD-A0768815B665}.
+   * See https://wicg.github.io/webusb/#webusb-platform-capability-descriptor */
+  constexpr static uint8_t k_webUSBUUID[] = {
+    0x38, 0xB6, 0x08, 0x34, 0xA9, 0x09, 0xA0, 0x47,
+    0x8B, 0xFD, 0xA0, 0x76, 0x88, 0x15, 0xB6, 0x65};
   uint16_t m_bcdVersion;
   uint8_t m_bVendorCode;
   uint8_t m_iLandingPage;

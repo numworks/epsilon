68bdb7ff583c2ab5c756ffcc07080961a788c248
Léa Saviot <lea.saviot@numworks.com>
2018-03-29 11:12:02 +0200

    [usb] Unlock the flash only if it is locked.
    Change-Id: I0cfc34fc158c8babb025b143fc77f4d64440876e

--- ion/src/device/usb/dfu_interface.cpp
+++ ion/src/device/usb/dfu_interface.cpp
@@ -306,10 +306,12 @@ void DFUInterface::unlockFlashMemory() {
   /* After a reset, program and erase operations are forbidden on the flash.
    * They can be unlocked by writting the appropriate keys in the FLASH_KEY
    * register. */
-  FLASH.KEYR()->set(0x45670123);
-  FLASH.KEYR()->set(0xCDEF89AB);
-  // Set the parallelism size
-  FLASH.CR()->setPSIZE(FLASH::CR::PSIZE::X32);
+  if (FLASH.CR()->getLOCK()) {
+    FLASH.KEYR()->set(0x45670123);
+    FLASH.KEYR()->set(0xCDEF89AB);
+    // Set the parallelism size
+    FLASH.CR()->setPSIZE(FLASH::CR::PSIZE::X32);
+  }
 }
 
 void DFUInterface::lockFlashMemory() {

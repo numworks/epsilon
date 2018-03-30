4b2825cf6db20abd64453f2f8fa30e5dfec0c4c5
Léa Saviot <lea.saviot@numworks.com>
2018-03-30 14:32:59 +0200

    [usb] USB aplication that displays the link to the workshop.
    Change-Id: I8458fe2a7f1eb4f04fc63f9e0b49d251c20f4cb6

---
+++ apps/usb/usb_connected_controller.h
@@ -0,0 +1,39 @@
+#ifndef USB_USB_CONNECTED_CONTROLLER_H
+#define USB_USB_CONNECTED_CONTROLLER_H
+
+#include <escher.h>
+
+namespace USB {
+
+class USBConnectedController : public ViewController {
+public:
+  USBConnectedController();
+  View * view() override { return &m_contentView; }
+  bool handleEvent(Ion::Events::Event event) override { return false; }
+private:
+  class ContentView : public View {
+  public:
+    ContentView(I18n::Message * messages, KDColor * colors);
+    void drawRect(KDContext * ctx, KDRect rect) const override;
+  private:
+    constexpr static KDCoordinate k_titleMargin = 40;
+    constexpr static KDCoordinate k_paragraphHeight = 100;
+    constexpr static KDCoordinate k_paragraphMargin = 13;
+    int numberOfSubviews() const override { return 7; }
+    View * subviewAtIndex(int index) override;
+    void layoutSubviews() override;
+    MessageTextView m_titleTextView;
+    MessageTextView m_messageTextView1;
+    MessageTextView m_messageTextView2;
+    MessageTextView m_messageTextView3;
+    MessageTextView m_messageTextView4;
+    MessageTextView m_messageTextView5;
+    MessageTextView m_messageTextView6;
+  };
+  ContentView m_contentView;
+};
+
+}
+
+#endif
+

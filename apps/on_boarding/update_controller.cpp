2da0a46e207c7170a7eb804084788b5dba8b4e61
Léa Saviot <lea.saviot@numworks.com>
2018-04-05 14:57:36 +0200

    [apps] MessageView for OnBoarding update and USBConnected controllers
    Change-Id: I3a775a81a62c408c9d02d98be25518d03fe5f9bf

--- apps/on_boarding/update_controller.cpp
+++ apps/on_boarding/update_controller.cpp
@@ -4,12 +4,63 @@
 
 namespace OnBoarding {
 
-static I18n::Message sOnBoardingMessages[] = {I18n::Message::UpdateAvailable, I18n::Message::UpdateMessage1, I18n::Message::UpdateMessage2, I18n::Message::UpdateMessage3, I18n::Message::UpdateMessage4};
+UpdateController::MessageViewWithSkip::MessageViewWithSkip(I18n::Message * messages, KDColor * colors, uint8_t numberOfMessages) :
+  MessageView(messages, colors, numberOfMessages),
+  m_skipView(KDText::FontSize::Small, I18n::Message::Skip, 1.0f, 0.5f),
+  m_okView()
+{
+}
 
-static KDColor sOnBoardingColors[] = {KDColorBlack, KDColorBlack, KDColorBlack, KDColorBlack, Palette::YellowDark};
+int UpdateController::MessageViewWithSkip::numberOfSubviews() const {
+ return MessageView::numberOfSubviews() + 2;
+}
+
+View * UpdateController::MessageViewWithSkip::subviewAtIndex(int index) {
+  uint8_t numberOfMainMessages = MessageView::numberOfSubviews();
+  if (index < numberOfMainMessages) {
+    return MessageView::subviewAtIndex(index);
+  }
+  if (index == numberOfMainMessages) {
+    return &m_skipView;
+  }
+  if (index == numberOfMainMessages + 1) {
+    return &m_okView;
+  }
+  assert(false);
+  return nullptr;
+}
+
+void UpdateController::MessageViewWithSkip::layoutSubviews() {
+  // Layout the main message
+  MessageView::layoutSubviews();
+  // Layout the "skip (OK)"
+  KDCoordinate height = bounds().height();
+  KDCoordinate width = bounds().width();
+  KDCoordinate textHeight = KDText::charSize(KDText::FontSize::Small).height();
+  KDSize okSize = m_okView.minimalSizeForOptimalDisplay();
+  m_skipView.setFrame(KDRect(0, height-k_bottomMargin-textHeight, width-okSize.width()-k_okMargin-k_skipMargin, textHeight));
+  m_okView.setFrame(KDRect(width - okSize.width()-k_okMargin, height-okSize.height()-k_okMargin, okSize));
+}
+
+static I18n::Message sOnBoardingMessages[] = {
+  I18n::Message::UpdateAvailable,
+  I18n::Message::UpdateMessage1,
+  I18n::Message::UpdateMessage2,
+  I18n::Message::BlankMessage,
+  I18n::Message::UpdateMessage3,
+  I18n::Message::UpdateMessage4};
+
+static KDColor sOnBoardingColors[] = {
+  KDColorBlack,
+  KDColorBlack,
+  KDColorBlack,
+  KDColorWhite,
+  KDColorBlack,
+  Palette::YellowDark};
 
 UpdateController::UpdateController() :
-  MessageController(sOnBoardingMessages, sOnBoardingColors)
+  ViewController(nullptr),
+  m_messageViewWithSkip(sOnBoardingMessages, sOnBoardingColors, 6)
 {
 }
 
--- apps/usb/usb_connected_controller.cpp
+++ apps/usb/usb_connected_controller.cpp
@@ -1,6 +1,5 @@
 #include "usb_connected_controller.h"
-#include "../apps_container.h"
-#include <assert.h>
+#include "../i18n.h"
 
 namespace USB {
 
@@ -9,6 +8,7 @@ static I18n::Message sUSBConnectedMessages[] = {
   I18n::Message::ConnectedMessage1,
   I18n::Message::ConnectedMessage2,
   I18n::Message::ConnectedMessage3,
+  I18n::Message::BlankMessage,
   I18n::Message::ConnectedMessage4,
   I18n::Message::ConnectedMessage5,
   I18n::Message::ConnectedMessage6};
@@ -18,63 +18,14 @@ static KDColor sUSBConnectedColors[] = {
   KDColorBlack,
   KDColorBlack,
   Palette::YellowDark,
+  KDColorWhite,
   KDColorBlack,
   KDColorBlack,
   KDColorBlack};
 
-USBConnectedController::ContentView::ContentView(I18n::Message * messages, KDColor * colors) :
-  m_titleTextView(KDText::FontSize::Large, messages[0], 0.5f, 0.5f, colors[0]),
-  m_messageTextView1(KDText::FontSize::Small, messages[1], 0.5f, 0.5f, colors[1]),
-  m_messageTextView2(KDText::FontSize::Small, messages[2], 0.5f, 0.5f, colors[2]),
-  m_messageTextView3(KDText::FontSize::Small, messages[3], 0.5f, 0.5f, colors[3]),
-  m_messageTextView4(KDText::FontSize::Small, messages[4], 0.5f, 0.5f, colors[4]),
-  m_messageTextView5(KDText::FontSize::Small, messages[5], 0.5f, 0.5f, colors[5]),
-  m_messageTextView6(KDText::FontSize::Small, messages[6], 0.5f, 0.5f, colors[6])
-{
-}
-
-void USBConnectedController::ContentView::drawRect(KDContext * ctx, KDRect rect) const {
-  ctx->fillRect(bounds(), KDColorWhite);
-}
-
-View * USBConnectedController::ContentView::subviewAtIndex(int index) {
-  switch (index) {
-    case 0:
-      return &m_titleTextView;
-    case 1:
-      return &m_messageTextView1;
-    case 2:
-      return &m_messageTextView2;
-    case 3:
-      return &m_messageTextView3;
-    case 4:
-      return &m_messageTextView4;
-    case 5:
-      return &m_messageTextView5;
-    case 6:
-      return &m_messageTextView6;
-    default:
-      assert(false);
-      return nullptr;
-  }
-}
-
-void USBConnectedController::ContentView::layoutSubviews() {
-  KDCoordinate width = bounds().width();
-  KDCoordinate titleHeight = m_titleTextView.minimalSizeForOptimalDisplay().height();
-  KDCoordinate textHeight = KDText::charSize(KDText::FontSize::Small).height();
-  m_titleTextView.setFrame(KDRect(0, Metric::MessageScreenTitleMargin, width, titleHeight));
-  m_messageTextView1.setFrame(KDRect(0, Metric::MessageScreenParagraphHeight, width, textHeight));
-  m_messageTextView2.setFrame(KDRect(0, Metric::MessageScreenParagraphHeight+textHeight, width, textHeight));
-  m_messageTextView3.setFrame(KDRect(0, Metric::MessageScreenParagraphHeight+2*textHeight, width, textHeight));
-  m_messageTextView4.setFrame(KDRect(0, Metric::MessageScreenParagraphHeight+3*textHeight+Metric::MessageScreenParagraphMargin, width, textHeight));
-  m_messageTextView5.setFrame(KDRect(0, Metric::MessageScreenParagraphHeight+4*textHeight+Metric::MessageScreenParagraphMargin, width, textHeight));
-  m_messageTextView6.setFrame(KDRect(0, Metric::MessageScreenParagraphHeight+5*textHeight+Metric::MessageScreenParagraphMargin, width, textHeight));
-}
-
 USBConnectedController::USBConnectedController() :
   ViewController(nullptr),
-  m_contentView(sUSBConnectedMessages, sUSBConnectedColors)
+  m_messageView(sUSBConnectedMessages, sUSBConnectedColors, 8)
 {
 }
 

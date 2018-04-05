d37193cbe17e8e1e2578d0dabcc5a7e95bb4288f
Léa Saviot <lea.saviot@numworks.com>
2018-04-05 15:40:20 +0200

    [ion] Add dummy USB functions
    Change-Id: Id67dd947997a2812dd718fdf1ded2f3a2b68ff48

--- ion/src/shared/dummy/usb.cpp
+++ ion/src/shared/dummy/usb.cpp
@@ -4,8 +4,18 @@ bool Ion::USB::isPlugged() {
   return false;
 }
 
+bool Ion::USB::isEnumerated() {
+  return false;
+}
+
 void Ion::USB::removeSoftDisconnect() {
 }
 
 void Ion::USB::DFU() {
+}
+
+void Ion::USB::enable() {
+}
+
+void Ion::USB::disable() {
 }

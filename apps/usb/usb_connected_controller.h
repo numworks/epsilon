50cc2a310b85736c3bf8e798294cb6a2a264751f
Léa Saviot <lea.saviot@numworks.com>
2018-04-05 14:57:36 +0200

    [apps] MessageView for OnBoarding update and USBConnected controllers
    Change-Id: I3a775a81a62c408c9d02d98be25518d03fe5f9bf

--- apps/usb/usb_connected_controller.h
+++ apps/usb/usb_connected_controller.h
@@ -2,32 +2,17 @@
 #define USB_USB_CONNECTED_CONTROLLER_H
 
 #include <escher.h>
+#include "../shared/message_view.h"
 
 namespace USB {
 
 class USBConnectedController : public ViewController {
 public:
   USBConnectedController();
-  View * view() override { return &m_contentView; }
+  View * view() override { return &m_messageView; }
   bool handleEvent(Ion::Events::Event event) override { return false; }
 private:
-  class ContentView : public View {
-  public:
-    ContentView(I18n::Message * messages, KDColor * colors);
-    void drawRect(KDContext * ctx, KDRect rect) const override;
-  private:
-    int numberOfSubviews() const override { return 7; }
-    View * subviewAtIndex(int index) override;
-    void layoutSubviews() override;
-    MessageTextView m_titleTextView;
-    MessageTextView m_messageTextView1;
-    MessageTextView m_messageTextView2;
-    MessageTextView m_messageTextView3;
-    MessageTextView m_messageTextView4;
-    MessageTextView m_messageTextView5;
-    MessageTextView m_messageTextView6;
-  };
-  ContentView m_contentView;
+  MessageView m_messageView;
 };
 
 }

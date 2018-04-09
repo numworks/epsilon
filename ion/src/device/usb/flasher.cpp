72b2dd4d848ae78b9002c06834eae20745f3865b
Romain Goyet <romain.goyet@numworks.com>
2018-04-09 13:37:11 +0200

    [ion] The flasher starts on a yellow screen

--- ion/src/device/usb/flasher.cpp
+++ ion/src/device/usb/flasher.cpp
@@ -3,6 +3,7 @@
 #include <ion.h>
 
 void ion_main(int argc, char * argv[]) {
+  Ion::Display::pushRectUniform(KDRect(0,0,Ion::Display::Width,Ion::Display::Height), KDColorYellow);
   while (true) {
     Ion::USB::enable();
     while (!OTG.GINTSTS()->getENUMDNE()) {

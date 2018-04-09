3dfcd14d82233b742f3fcdfb2062a8ed541c1c66
Émilie Feral <emilie.feral@numworks.com>
2018-04-09 18:11:42 +0200

    [poincare] fix characteristicXRange to take into account radian unit

--- poincare/include/poincare/symbol.h
+++ poincare/include/poincare/symbol.h
@@ -40,7 +40,7 @@ public:
   bool isMatrixSymbol() const;
   bool isScalarSymbol() const;
   bool isApproximate(Context & context) const;
-  float characteristicXRange(Context & context, AngleUnit angleUnit) const override;
+  float characteristicXRange(Context & context, AngleUnit angleUnit = AngleUnit::Default) const override;
   bool hasAnExactRepresentation(Context & context) const;
 private:
   const char * textForSpecialSymbols(char name) const;

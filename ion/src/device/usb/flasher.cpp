9c16b70b5570bf0192cd5ff80b6f51ce438394fe
Romain Goyet <romain.goyet@numworks.com>
2018-04-09 11:57:17 +0200

    [usb/flasher] Enable USB before jumping in, correct keyboard logic

--- ion/src/device/usb/flasher.cpp
+++ ion/src/device/usb/flasher.cpp
@@ -1,8 +1,10 @@
 #include "../regs/regs.h"
 #include "../usb/calculator.h"
+#include <ion.h>
 
 void ion_main(int argc, char * argv[]) {
   while (true) {
+    Ion::USB::enable();
     while (!OTG.GINTSTS()->getENUMDNE()) {
     }
     Ion::USB::Device::Calculator::PollAndReset(false);

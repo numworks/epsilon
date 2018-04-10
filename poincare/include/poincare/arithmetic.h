7de609028ca2cec45b61e277290af1ddbadaf87b
Romain Goyet <romain.goyet@numworks.com>
2018-04-10 15:03:42 +0200

    [poincare] Don't make Integer::k_primorial32 a global variable

--- poincare/include/poincare/arithmetic.h
+++ poincare/include/poincare/arithmetic.h
@@ -13,7 +13,6 @@ public:
   static void PrimeFactorization(const Integer * i, Integer * outputFactors, Integer * outputCoefficients, int outputLength);
   constexpr static int k_numberOfPrimeFactors = 1000;
   constexpr static int k_maxNumberOfPrimeFactors = 32;
-  static const Integer k_primorial32;
 private:
   /* When decomposing an integer into primes factors, we look for its prime
    * factors among integer from 2 to 10000. */

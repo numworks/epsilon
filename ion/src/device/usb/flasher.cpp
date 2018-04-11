da8360033e9ded922ccc4bf33a672a5722ec8365
Léa Saviot <lea.saviot@numworks.com>
2018-04-11 14:00:37 +0200

    [usb] Use proper function to scan registers.
    Change-Id: I37f0f8ce105c7791fa8890013d672cca7dff4126

--- ion/src/device/usb/flasher.cpp
+++ ion/src/device/usb/flasher.cpp
@@ -6,7 +6,7 @@ void ion_main(int argc, char * argv[]) {
   Ion::Display::pushRectUniform(KDRect(0,0,Ion::Display::Width,Ion::Display::Height), KDColor::RGB24(0xFFFF00));
   while (true) {
     Ion::USB::enable();
-    while (!OTG.GINTSTS()->getENUMDNE()) {
+    while (!Ion::USB::isEnumerated()) {
     }
     Ion::USB::Device::Calculator::PollAndReset(false);
   }

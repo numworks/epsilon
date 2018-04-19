06c9676d3059c583790066f94748e1f38ba06e30
Léa Saviot <lea.saviot@numworks.com>
2018-04-19 16:13:32 +0200

    [poincare] Rename BracketRight/LeftLayout -> Right/LeftBracketLayout.
    Change-Id: I382ac960aeca861d1003cd2373709a6759ded685

--- poincare/src/layout/left_bracket_layout.h
+++ poincare/src/layout/left_bracket_layout.h
@@ -1,19 +1,19 @@
-#ifndef POINCARE_BRACKET_RIGHT_LAYOUT_H
-#define POINCARE_BRACKET_RIGHT_LAYOUT_H
+#ifndef POINCARE_BRACKET_LEFT_LAYOUT_H
+#define POINCARE_BRACKET_LEFT_LAYOUT_H
 
 #include <poincare/src/layout/bracket_left_right_layout.h>
 #include <poincare/layout_engine.h>
 
 namespace Poincare {
 
-class BracketRightLayout : public BracketLeftRightLayout {
+class LeftBracketLayout : public BracketLeftRightLayout {
 public:
   using BracketLeftRightLayout::BracketLeftRightLayout;
   ExpressionLayout * clone() const override;
   int writeTextInBuffer(char * buffer, int bufferSize, int numberOfSignificantDigits = PrintFloat::k_numberOfStoredSignificantDigits) const override {
-    return LayoutEngine::writeOneCharInBuffer(buffer, bufferSize, ']');
+    return LayoutEngine::writeOneCharInBuffer(buffer, bufferSize, '[');
   }
-  bool isRightBracket() const override { return true; }
+  bool isLeftBracket() const override { return true; }
 protected:
   void render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) override;
   void computeOperandHeight() override;

1b505d1cb4cf6a0a8d7cf44d8c9eb7a35d7cf213
Léa Saviot <lea.saviot@numworks.com>
2018-04-19 11:06:26 +0200

    [poincare] Linear parenthesis height increase.
    Change-Id: I8e073b10e4e7ea7741520e7e5e9ff5c08c3cb902

--- poincare/src/layout/parenthesis_left_right_layout.h
+++ poincare/src/layout/parenthesis_left_right_layout.h
@@ -18,6 +18,7 @@ public:
   constexpr static KDCoordinate k_externHeightMargin = 2;
   constexpr static KDCoordinate k_widthMargin = 5;
   constexpr static KDCoordinate k_lineThickness = 1;
+  constexpr static KDCoordinate k_verticalMargin = 4;
 protected:
   KDColor s_parenthesisWorkingBuffer[k_parenthesisCurveHeight*k_parenthesisCurveWidth];
   KDSize computeSize() override;

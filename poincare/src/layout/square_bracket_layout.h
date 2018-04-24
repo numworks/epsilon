99b320c75440f353a25c0aa88f1aa9ebbdb80bc2
Léa Saviot <lea.saviot@numworks.com>
2018-04-24 11:25:46 +0200

    [poincare] Factorize Parenthesis/SquareBracket layout in BracketLayout.
    Change-Id: I2aeddb950803f460b75e6e9a0efd3c0e2f16164e

--- poincare/src/layout/square_bracket_layout.h
+++ poincare/src/layout/square_bracket_layout.h
@@ -1,28 +1,21 @@
 #ifndef POINCARE_SQUARE_BRACKET_LAYOUT_H
 #define POINCARE_SQUARE_BRACKET_LAYOUT_H
 
-#include <poincare/static_layout_hierarchy.h>
+#include "bracket_layout.h"
 
 namespace Poincare {
 
-class SquareBracketLayout : public StaticLayoutHierarchy<0> {
+class SquareBracketLayout : public BracketLayout {
 public:
-  SquareBracketLayout();
-  void invalidAllSizesPositionsAndBaselines() override;
-  ExpressionLayoutCursor cursorLeftOf(ExpressionLayoutCursor * cursor, bool * shouldRecomputeLayout) override;
-  ExpressionLayoutCursor cursorRightOf(ExpressionLayoutCursor * cursor, bool * shouldRecomputeLayout) override;
+  using BracketLayout::BracketLayout;
 protected:
   constexpr static KDCoordinate k_bracketWidth = 5;
   constexpr static KDCoordinate k_lineThickness = 1;
   constexpr static KDCoordinate k_widthMargin = 5;
   constexpr static KDCoordinate k_externWidthMargin = 2;
-  KDSize computeSize() override;
-  void computeBaseline() override;
-  KDCoordinate operandHeight();
-  void computeOperandHeight();
-  KDPoint positionOfChild(ExpressionLayout * child) override;
-  bool m_operandHeightComputed;
-  uint16_t m_operandHeight;
+  KDSize computeSize() override {
+    return KDSize(k_externWidthMargin + k_lineThickness + k_widthMargin, operandHeight() + k_lineThickness);
+  }
 };
 }
 

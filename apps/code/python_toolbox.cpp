8801b9f383ea7f53c81b8438f701c9600a0f769b
Léa Saviot <lea.saviot@numworks.com>
2018-04-26 17:52:11 +0200

    [apps/code] Fix cursor position when inserting Python commands
    Change-Id: I2f5cdc72220cc61b0c8830d746c63f705a7c928e

--- apps/code/python_toolbox.cpp
+++ apps/code/python_toolbox.cpp
@@ -293,7 +293,7 @@ bool PythonToolbox::selectLeaf(ToolboxMessageTree * selectedMessageTree) {
   const char * editedText = I18n::translate(node->insertedText());
   int strippedEditedTextMaxLength = strlen(editedText)+1;
   char strippedEditedText[strippedEditedTextMaxLength];
-  Shared::ToolboxHelpers::TextToInsertForCommandMessage(node->insertedText(), strippedEditedText, strippedEditedTextMaxLength);
+  Shared::ToolboxHelpers::TextToInsertForCommandMessage(node->insertedText(), strippedEditedText, strippedEditedTextMaxLength, true);
   sender()->handleEventWithText(strippedEditedText, true);
   app()->dismissModalViewController();
   return true;

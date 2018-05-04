2aae9071bcbb00880912b7cbd8954b3b8e3b6046
Romain Goyet <romain.goyet@numworks.com>
2018-05-04 17:06:16 +0200

    [ion] Encode the serial number in Base64
    Also, expose serialNumber() in Ion and keep "copySerialNumber" in
    Ion::Device. This allows the DFU bootloader to remain .bss-free.

--- ion/src/device/bench/command/mcu_serial.cpp
+++ ion/src/device/bench/command/mcu_serial.cpp
@@ -1,5 +1,6 @@
 #include "command.h"
 #include <ion.h>
+#include "../../device.h"
 
 namespace Ion {
 namespace Device {
@@ -11,8 +12,8 @@ void MCUSerial(const char * input) {
     reply(sSyntaxError);
     return;
   }
-  char response[11+Ion::SerialNumberLength + 1] = {'M', 'C', 'U', '_', 'S', 'E', 'R', 'I', 'A', 'L', '=', 0};
-  Ion::getSerialNumber(response+11);
+  char response[11 + Ion::Device::SerialNumberLength + 1] = {'M', 'C', 'U', '_', 'S', 'E', 'R', 'I', 'A', 'L', '=', 0};
+  Ion::Device::copySerialNumber(response + 11);
   reply(response);
 }
 

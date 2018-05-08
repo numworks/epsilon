1e78f02ffc6cb05cd42421a771a0017f3c3d371c
Léa Saviot <lea.saviot@numworks.com>
2018-05-08 17:48:47 +0200

    [poincare] Fix blackbox bug about variable initialization
    Change-Id: I8538ce9aa75ecee5f9e7f572be13fc590c994db7

--- poincare/src/expression_layout_cursor.cpp
+++ poincare/src/expression_layout_cursor.cpp
@@ -147,13 +147,12 @@ void ExpressionLayoutCursor::addEmptySquarePowerLayout() {
 void ExpressionLayoutCursor::addEmptyTenPowerLayout() {
   HorizontalLayout * newSibling = new HorizontalLayout();
   EmptyLayout * powerLayout = new EmptyLayout();
-  int numberOfChildren = 4;
-  ExpressionLayout * childLayouts[numberOfChildren] = {
+  ExpressionLayout * childLayouts[] = {
     new CharLayout(Ion::Charset::MiddleDot),
     new CharLayout('1'),
     new CharLayout('0'),
     new VerticalOffsetLayout(powerLayout, VerticalOffsetLayout::Type::Superscript, false)};
-  newSibling->addChildrenAtIndex(childLayouts, numberOfChildren, 0, false);
+  newSibling->addChildrenAtIndex(childLayouts, 4, 0, false);
   m_pointedExpressionLayout->addSibling(this, newSibling);
   setPointedExpressionLayout(powerLayout);
   setPosition(ExpressionLayoutCursor::Position::Right);

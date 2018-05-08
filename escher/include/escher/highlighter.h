9ecd4e18ea1ba55e0e8d135e2e8091f964029de3
Damien Nicolet <zardam@gmail.com>
2018-05-08 14:47:08 +0200

    [apps] Code: Add a destructor to Highlighter class

--- escher/include/escher/highlighter.h
+++ escher/include/escher/highlighter.h
@@ -7,6 +7,7 @@ class Highlighter {
 public:
   virtual bool highlight(char * buffer, char * attr_buffer, size_t bufferSize) = 0;
   virtual bool cursorMoved(char * buffer, char * attr_buffer, size_t bufferSize, int location) = 0;
+  virtual ~Highlighter() {};
 };
 
 #endif

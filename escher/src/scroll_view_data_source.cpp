daee2998c556884c5abf16bf87cb84d5fd47cd8b
Romain Goyet <romain.goyet@numworks.com>
2018-05-09 17:14:04 +0200

    [escher] Add a ScrollViewDelegate

--- escher/src/scroll_view_data_source.cpp
+++ escher/src/scroll_view_data_source.cpp
@@ -1,19 +1,12 @@
 #include <escher/scroll_view_data_source.h>
 
-ScrollViewDataSource::ScrollViewDataSource() :
-  m_offset(KDPointZero)
-{
-}
-
-KDPoint ScrollViewDataSource::offset() const {
-  return m_offset;
-}
-
 bool ScrollViewDataSource::setOffset(KDPoint offset) {
   if (offset != m_offset) {
     m_offset = offset;
+    if (m_delegate) {
+      m_delegate->scrollViewDidChangeOffset(this);
+    }
     return true;
   }
   return false;
 }
-

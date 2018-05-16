90166264f6a30f9c132c395b2d8af5033460db74
Léa Saviot <lea.saviot@numworks.com>
2018-05-16 16:15:17 +0200

    [escher] Fix signed/unsigned int comparison
    Change-Id: I72b6dc3f46ee585814a540fc999001e113db0ea1

--- escher/src/text_input_helpers.cpp
+++ escher/src/text_input_helpers.cpp
@@ -4,16 +4,17 @@
 
 namespace TextInputHelpers {
 
-int CursorIndexInCommand(const char * text) {
-  int textLength = strlen(text);
-  for (int i = 0; i < textLength - 1; i++) {
-    if (text[i] == '\'' &&  text[i+1] == '\'') {
-      return i + 1;
-    } else if (text[i] == Ion::Charset::Empty) {
-      return i;
+size_t CursorIndexInCommand(const char * text) {
+  size_t index = 0;
+  while (text[index] != 0) {
+    if (text[index] == '\'' &&  text[index+1] == '\'') {
+      return index + 1;
+    } else if (text[index] == Ion::Charset::Empty) {
+      return index;
     }
+    index++;
   }
-  return textLength;
+  return index;
 }
 
 }

c7aab7b69ba94e9f8348bee611f5eae84d5f929d
Léa Saviot <lea.saviot@numworks.com>
2018-05-21 17:33:08 +0200

    [apps/statistics] Clean BoxRange

--- apps/statistics/box_range.cpp
+++ apps/statistics/box_range.cpp
@@ -21,14 +21,6 @@ float BoxRange::xMax() {
   return max + k_displayRightMarginRatio*(max - min);
 }
 
-float BoxRange::yMin() {
-  return -k_displayBottomMarginRatio;
-}
-
-float BoxRange::yMax() {
-  return 1.0f+k_displayTopMarginRatio;
-}
-
 float BoxRange::xGridUnit() {
   return computeGridUnit(Axis::X, xMin(), xMax());
 }

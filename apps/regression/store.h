76371830559528ea4ab86367df4a678e7b049fa2
Léa Saviot <lea.saviot@numworks.com>
2018-05-21 13:19:01 +0200

    [apps] Multiple series in regression/store

--- apps/regression/store.h
+++ apps/regression/store.h
@@ -12,37 +12,37 @@ public:
   // Dots
   /* Return the closest dot to x above the regression curve if direction > 0,
    * below otherwise*/
-  int closestVerticalDot(int direction, float x);
+  int closestVerticalDot(int series, int direction, float x);
   /* Return the closest dot to dot given on the right if direction > 0,
    * on the left otherwise*/
-  int nextDot(int direction, int dot);
+  int nextDot(int series, int direction, int dot);
 
   // Window
   void setDefault() override;
 
   // Calculation
-  double numberOfPairs() const { return m_numberOfPairs; }
-  double squaredValueSumOfColumn(int i);
-  double columnProductSum();
-  double meanOfColumn(int i);
-  double varianceOfColumn(int i);
-  double standardDeviationOfColumn(int i);
-  double covariance();
-  double slope();
-  double yIntercept();
-  double yValueForXValue(double x);
-  double xValueForYValue(double y);
-  double correlationCoefficient();
-  double squaredCorrelationCoefficient();
+  double numberOfPairs(int series) const { return m_numberOfPairs; }
+  double squaredValueSumOfColumn(int series, int i);
+  double columnProductSum(int series);
+  double meanOfColumn(int series, int i);
+  double varianceOfColumn(int series, int i);
+  double standardDeviationOfColumn(int series, int i);
+  double covariance(int series);
+  double slope(int series);
+  double yIntercept(int series);
+  double yValueForXValue(int series, double x);
+  double xValueForYValue(int series, double y);
+  double correlationCoefficient(int series);
+  double squaredCorrelationCoefficient(int series);
 private:
   constexpr static float k_displayTopMarginRatio = 0.12f;
   constexpr static float k_displayRightMarginRatio = 0.05f;
   constexpr static float k_displayBottomMarginRatio = 0.5f;
   constexpr static float k_displayLeftMarginRatio = 0.05f;
-  InteractiveCurveViewRangeDelegate::Range computeYRange(InteractiveCurveViewRange * interactiveCurveViewRange) override;
+  InteractiveCurveViewRangeDelegate::Range computeYRange(int series, InteractiveCurveViewRange * interactiveCurveViewRange) override;
   float addMargin(float x, float range, bool isMin) override;
-  float maxValueOfColumn(int i);
-  float minValueOfColumn(int i);
+  float maxValueOfColumn(int series, int i);
+  float minValueOfColumn(int series, int i);
 };
 
 typedef double (Store::*ArgCalculPointer)(int);

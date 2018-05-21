fa5e3b4b0d601cb02bc4586c945a329a535d5593
Léa Saviot <lea.saviot@numworks.com>
2018-05-21 17:25:04 +0200

    [apps/statistics] Reorganize BoxController

--- apps/statistics/box_controller.h
+++ apps/statistics/box_controller.h
@@ -11,14 +11,21 @@ namespace Statistics {
 class BoxController : public ViewController, public ButtonRowDelegate, public AlternateEmptyViewDelegate {
 public:
   BoxController(Responder * parentResponder, ButtonRowController * header, Store * store, BoxView::Quantile * selectedQuantile);
-  const char * title() override;
-  View * view() override;
-  bool handleEvent(Ion::Events::Event event) override;
-  void didBecomeFirstResponder() override;
+
+  // AlternateEmptyViewDelegate
   bool isEmpty() const override;
   I18n::Message emptyMessage() override;
   Responder * defaultController() override;
+
+  // ViewController
+  const char * title() override;
+  View * view() override;
   void viewWillAppear() override;
+  ViewController::DisplayParameter displayParameter() override { return ViewController::DisplayParameter::DoNotShowOwnTitle; }
+
+  // Responder
+  bool handleEvent(Ion::Events::Event event) override;
+  void didBecomeFirstResponder() override;
   void willExitResponderChain(Responder * nextFirstResponder) override;
 private:
   Responder * tabController() const;

13107037b0c0a0c9731a6ef801b0a8f58153784e
Léa Saviot <lea.saviot@numworks.com>
2018-05-22 16:30:54 +0200

    [apps/statistics] Show several histograms

--- apps/statistics/histogram_view.h
+++ apps/statistics/histogram_view.h
@@ -8,19 +8,24 @@
 
 namespace Statistics {
 
+class HistogramController;
+
 class HistogramView : public Shared::CurveView {
 public:
-  HistogramView(Store * store, Shared::BannerView * bannerView);
+  HistogramView(HistogramController * controller, Store * store, int series, Shared::BannerView * bannerView);
+  int series() const { return m_series; }
   void reload() override;
   void drawRect(KDContext * ctx, KDRect rect) const override;
   void setHighlight(float start, float end);
 private:
   char * label(Axis axis, int index) const override;
+  HistogramController * m_controller;
   Store * m_store;
   char m_labels[k_maxNumberOfXLabels][Poincare::PrintFloat::bufferSizeForFloatsWithPrecision(Constant::ShortNumberOfSignificantDigits)];
   static float EvaluateHistogramAtAbscissa(float abscissa, void * model, void * context);
   float m_highlightedBarStart;
   float m_highlightedBarEnd;
+  int m_series;
 };
 
 }

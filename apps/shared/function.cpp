b266c4cbd105661a21bfd6e3538243e9de7d94c7
Émilie Feral <emilie.feral@numworks.com>
2018-05-23 10:50:34 +0200

    [shared] Make a parent class to Function: ExpressionModel (factorize code for furtur model Equation)

--- apps/shared/function.cpp
+++ apps/shared/function.cpp
@@ -8,87 +8,27 @@ using namespace Poincare;
 namespace Shared {
 
 Function::Function(const char * name, KDColor color) :
-  m_expression(nullptr),
-  m_text{0},
+  ExpressionModel(),
   m_name(name),
   m_color(color),
-  m_layout(nullptr),
   m_active(true)
 {
 }
 
-Function& Function::operator=(const Function& other) {
-  // Self-assignment is benign
-  m_color = other.m_color;
-  m_name = other.m_name;
-  m_active = other.m_active;
-  setContent(other.m_text);
-  return *this;
-}
-
 uint32_t Function::checksum() {
   char data[k_dataLengthInBytes/sizeof(char)] = {};
-  strlcpy(data, m_text, TextField::maxBufferSize());
+  strlcpy(data, text(), TextField::maxBufferSize());
   data[k_dataLengthInBytes-2] = m_name != nullptr ? m_name[0] : 0;
   data[k_dataLengthInBytes-1] = m_active ? 1 : 0;
   return Ion::crc32((uint32_t *)data, k_dataLengthInBytes/sizeof(uint32_t));
-}
-
-void Function::setContent(const char * c) {
-  strlcpy(m_text, c, sizeof(m_text));
-  if (m_layout != nullptr) {
-    delete m_layout;
-    m_layout = nullptr;
-  }
-  if (m_expression != nullptr) {
-    delete m_expression;
-    m_expression = nullptr;
-  }
 }
 
 void Function::setColor(KDColor color) {
   m_color = color;
 }
 
-Function::~Function() {
-  if (m_layout != nullptr) {
-    delete m_layout;
-    m_layout = nullptr;
-  }
-  if (m_expression != nullptr) {
-    delete m_expression;
-    m_expression = nullptr;
-  }
-}
-
-const char * Function::text() const {
-  return m_text;
-}
-
 const char * Function::name() const {
   return m_name;
-}
-
-Poincare::Expression * Function::expression(Poincare::Context * context) const {
-  if (m_expression == nullptr) {
-    m_expression = Expression::ParseAndSimplify(m_text, *context);
-  }
-  return m_expression;
-}
-
-Poincare::ExpressionLayout * Function::layout() {
-  if (m_layout == nullptr) {
-    Expression * nonSimplifiedExpression = Expression::parse(m_text);
-    if (nonSimplifiedExpression != nullptr) {
-      m_layout = nonSimplifiedExpression->createLayout(PrintFloat::Mode::Decimal);
-      delete nonSimplifiedExpression;
-    }
-  }
-  return m_layout;
-}
-
-bool Function::isDefined() {
-  return m_text[0] != 0;
 }
 
 bool Function::isActive() {
@@ -99,10 +39,6 @@ void Function::setActive(bool active) {
   m_active = active;
 }
 
-bool Function::isEmpty() {
-  return m_text[0] == 0;
-}
-
 template<typename T>
 T Function::templatedApproximateAtAbscissa(T x, Poincare::Context * context) const {
   Poincare::VariableContext<T> variableContext = Poincare::VariableContext<T>(symbol(), context);
@@ -110,17 +46,6 @@ T Function::templatedApproximateAtAbscissa(T x, Poincare::Context * context) con
   Poincare::Complex<T> e = Poincare::Complex<T>::Float(x);
   variableContext.setExpressionForSymbolName(&e, &xSymbol, variableContext);
   return expression(context)->approximateToScalar<T>(variableContext);
-}
-
-void Function::tidy() {
-  if (m_layout != nullptr) {
-    delete m_layout;
-    m_layout = nullptr;
-  }
-  if (m_expression != nullptr) {
-    delete m_expression;
-    m_expression = nullptr;
-  }
 }
 
 }

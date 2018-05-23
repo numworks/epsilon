f6e5b7edbbb56da3841e0d4342ac63569f15f5f6
Léa Saviot <lea.saviot@numworks.com>
2018-05-23 11:54:58 +0200

    [apps/stats] Color the histogram

--- apps/statistics/histogram_view.h
+++ apps/statistics/histogram_view.h
@@ -12,7 +12,7 @@ class HistogramController;
 
 class HistogramView : public Shared::CurveView {
 public:
-  HistogramView(HistogramController * controller, Store * store, int series, Shared::BannerView * bannerView);
+  HistogramView(HistogramController * controller, Store * store, int series, Shared::BannerView * bannerView, KDColor selectedHistogramColor = Palette::Select, KDColor notSelectedHistogramColor = Palette::GreyMiddle, KDColor selectedBarColor = Palette::YellowDark);
   int series() const { return m_series; }
   void reload() override;
   void drawRect(KDContext * ctx, KDRect rect) const override;
@@ -26,6 +26,9 @@ private:
   float m_highlightedBarStart;
   float m_highlightedBarEnd;
   int m_series;
+  KDColor m_selectedHistogramColor;
+  KDColor m_notSelectedHistogramColor;
+  KDColor m_selectedBarColor;
 };
 
 }

6fe7fe3eb2cab85ae80a68c8098884ec150a53b1
Léa Saviot <lea.saviot@numworks.com>
2018-05-23 15:21:29 +0200

    [apps/stats] Histograms axis labels follow the selection

--- apps/statistics/histogram_controller.h
+++ apps/statistics/histogram_controller.h
@@ -46,6 +46,8 @@ private:
     int indexOfSubviewAtSeries(int series);
     HistogramBannerView * bannerView() { return &m_bannerView; }
     void setDisplayBanner(bool display) { m_displayBanner = display; }
+    void selectHistogram(int index);
+    void deselectHistogram(int index);
     // View
     void drawRect(KDContext * ctx, KDRect rect) const override;
     int numberOfSubviews() const override;
@@ -53,6 +55,7 @@ private:
     KDRect bannerFrame() const;
     View * subviewAtIndex(int index) override;
     void layoutSubviews() override;
+    void changeHistogramSelection(int index, bool select);
     HistogramView m_histogramView1;
     HistogramView m_histogramView2;
     HistogramView m_histogramView3;

e46aadbbcff30dbca47000a0bf55c82d5a708c60
Léa Saviot <lea.saviot@numworks.com>
2018-05-25 16:48:25 +0200

    [apps/stats] Series name in the box banner

--- apps/statistics/box_banner_view.cpp
+++ apps/statistics/box_banner_view.cpp
@@ -3,18 +3,19 @@
 namespace Statistics {
 
 BoxBannerView::BoxBannerView() :
+  m_seriesName(KDText::FontSize::Small, 0.0f, 0.5f, KDColorBlack, Palette::GreyMiddle),
   m_calculationName(KDText::FontSize::Small, I18n::Message::Minimum, 0.0f, 0.5f, KDColorBlack, Palette::GreyMiddle),
   m_calculationValue(KDText::FontSize::Small, 1.0f, 0.5f, KDColorBlack, Palette::GreyMiddle)
 {
 }
 
 TextView * BoxBannerView::textViewAtIndex(int index) const {
-  const TextView * textViews[2] = {&m_calculationName, &m_calculationValue};
+  const TextView * textViews[3] = {&m_seriesName, &m_calculationName, &m_calculationValue};
   return (TextView *)textViews[index];
 }
 
 MessageTextView * BoxBannerView::messageTextViewAtIndex(int index) const {
-  return index == 0 ? (MessageTextView *)&m_calculationName : nullptr;
+  return index == 1 ? (MessageTextView *)&m_calculationName : nullptr;
 }
 
 }

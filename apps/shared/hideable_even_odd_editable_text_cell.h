d7d426307c0bcfba82bd1cfd274b8c665bee0030
Léa Saviot <lea.saviot@numworks.com>
2018-05-28 11:43:29 +0200

    [apps/shared] Class Hideable

--- apps/shared/hideable_even_odd_editable_text_cell.h
+++ apps/shared/hideable_even_odd_editable_text_cell.h
@@ -3,20 +3,18 @@
 
 #include <escher/even_odd_editable_text_cell.h>
 #include <escher/palette.h>
+#include "hideable.h"
 
 namespace Shared {
 
-class HideableEvenOddEditableTextCell : public EvenOddEditableTextCell {
+class HideableEvenOddEditableTextCell : public EvenOddEditableTextCell, public Hideable {
 public:
   HideableEvenOddEditableTextCell(Responder * parentResponder = nullptr, TextFieldDelegate * delegate = nullptr, char * draftTextBuffer = nullptr) :
     EvenOddEditableTextCell(parentResponder, delegate, draftTextBuffer),
-    m_hide(false)
+    Hideable()
   {}
   KDColor backgroundColor() const override;
-  static KDColor hideColor() { return Palette::WallScreenDark; }
-  void setHide(bool hide);
-private:
-  bool m_hide;
+  void setHide(bool hide) override;
 };
 
 }

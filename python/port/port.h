0383f43e6c827b853806c5b624ac5e79414576a0
Romain Goyet <romain.goyet@numworks.com>
2018-05-29 14:40:19 +0200

    [python] Clean up the configuration files

--- python/port/port.h
+++ python/port/port.h
@@ -34,10 +34,4 @@ void registerScriptProvider(ScriptProvider * s);
 
 };
 
-// Will implement :
-// mp_lexer_new_from_file -> Ask the context about a file
-// mp_import_stat
-// mp_hal_stdout_tx_strn_cooked -> Tell the context Python printed text
-// mp_hal_input
-
 #endif

163513dfc039a4b2953c5fd8a7437514ce7849b7
Léa Saviot <lea.saviot@numworks.com>
2018-06-05 17:37:59 +0200

    [apps/stats/reg] More beautiful formul input view

--- apps/shared/buffer_text_view_with_text_field.h
+++ apps/shared/buffer_text_view_with_text_field.h
@@ -6,21 +6,23 @@ namespace Shared {
 
 class BufferTextViewWithTextField : public View, public Responder {
 public:
-  constexpr static KDCoordinate k_height = 50; //TODO
   BufferTextViewWithTextField(Responder * parentResponder, TextFieldDelegate * delegate = nullptr, KDText::FontSize size = KDText::FontSize::Large);
   KDSize minimalSizeForOptimalDisplay() const override;
   TextField * textField() { return &m_textField; }
   void setBufferText(const char * text);
-  void setTextFieldText(const char * text);
+  void drawRect(KDContext * ctx, KDRect rect) const override;
 
   // Responder
   void didBecomeFirstResponder() override;
 private:
   constexpr static int k_textFieldBufferSize = TextField::maxBufferSize();
-  constexpr static KDCoordinate k_bufferTextWidth = 50; //TODO
+  constexpr static KDCoordinate k_bufferTextWidth = 35;
+  constexpr static KDCoordinate k_textFieldVerticalMargin = 3;
+  constexpr static KDCoordinate k_borderWidth = 1;
   int numberOfSubviews() const override { return 2; }
   View * subviewAtIndex(int index) override;
   void layoutSubviews() override;
+  KDRect textFieldFrame() const;
   BufferTextView m_bufferTextView;
   TextField m_textField;
   char m_textFieldBuffer[k_textFieldBufferSize];

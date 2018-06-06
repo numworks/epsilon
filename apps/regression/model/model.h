4170d718c1809f347ba070da66f0b0d47bd70612
Léa Saviot <lea.saviot@numworks.com>
2018-06-06 16:57:53 +0200

    [apps/reg] Put levenberg-marquardt methods in Model

--- apps/regression/model/model.h
+++ apps/regression/model/model.h
@@ -2,8 +2,12 @@
 #define REGRESSION_MODEL_H
 
 #include <stdint.h>
+#include <poincare/context.h>
+#include <poincare/expression.h>
 
 namespace Regression {
+
+class Store;
 
 class Model {
 public:
@@ -18,10 +22,27 @@ public:
     Trigonometric = 7,
     Logistic    = 8
   };
+  static constexpr int k_numberOfModels = 9;
   static constexpr int k_maxNumberOfCoefficients = 5;
   virtual double evaluate(double * modelCoefficients, double x) const = 0;
+  virtual void fit(Store * store, int series, double * modelCoefficients, Poincare::Context * context);
+private:
+  // Model attributes
   virtual double partialDerivate(double * modelCoefficients, int derivateCoefficientIndex, double x) const = 0;
   virtual double numberOfCoefficients() const = 0;
+
+  // Levenberg-Marquardt
+  static constexpr double k_maxIterations = 1000;
+  static constexpr double k_initialLambda = 0.001;
+  static constexpr double k_lambdaFactor = 10;
+  static constexpr double k_chi2ChangeCondition = 0.0005;
+  static constexpr int k_consecutiveSmallChi2ChangesLimit = 10;
+  void fitLevenbergMarquardt(Store * store, int series, double * modelCoefficients, Poincare::Context * context);
+  double chi2(Store * store, int series, double * modelCoefficients) const;
+  double alphaPrimeCoefficient(Store * store, int series, double * modelCoefficients, int k, int l, double lambda) const;
+  double alphaCoefficient(Store * store, int series, double * modelCoefficients, int k, int l) const;
+  double betaCoefficient(Store * store, int series, double * modelCoefficients, int k) const;
+  void solveLinearSystem(double * solutions, Poincare::Expression * coefficients[Model::k_maxNumberOfCoefficients][Model::k_maxNumberOfCoefficients], Poincare::Expression * * constants, int solutionDimension, Poincare::Context * context);
 };
 
 }

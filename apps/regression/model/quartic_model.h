86e4b234fbc26221099bd30126b025702ca0e16c
Léa Saviot <lea.saviot@numworks.com>
2018-06-08 15:47:23 +0200

    [apps/reg] Do not show "Yprediction given x" if not available

--- apps/regression/model/quartic_model.h
+++ apps/regression/model/quartic_model.h
@@ -12,6 +12,7 @@ public:
   Poincare::ExpressionLayout * layout() override;
   I18n::Message formulaMessage() const override { return I18n::Message::QuarticRegressionFormula; }
   double evaluate(double * modelCoefficients, double x) const override;
+  bool levelSetAvailable(double * modelCoefficients) const override { return false; }
   double levelSet(double * modelCoefficients, double y) const override;
   double partialDerivate(double * modelCoefficients, int derivateCoefficientIndex, double x) const override;
   int numberOfCoefficients() const override { return 5; }

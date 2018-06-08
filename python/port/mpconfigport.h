5008f40a66695ab5b113c587d3fb7161fd8dc464
Romain Goyet <romain.goyet@numworks.com>
2018-06-08 09:47:44 +0200

    [python] Enable builtins filter

--- python/port/mpconfigport.h
+++ python/port/mpconfigport.h
@@ -50,9 +50,6 @@
 // Whether to support property object
 #define MICROPY_PY_BUILTINS_PROPERTY (0)
 
-// Whether to support filter function(type)
-#define MICROPY_PY_BUILTINS_FILTER (0)
-
 // Whether to support reversed function(type)
 #define MICROPY_PY_BUILTINS_REVERSED (0)
 

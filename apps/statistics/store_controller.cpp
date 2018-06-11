13a4057786c0ad78c6c47f27001410b211105279
Léa Saviot <lea.saviot@numworks.com>
2018-06-11 11:24:16 +0200

    [apps/stats/reg] Do not fill with formula if undefined values

--- apps/statistics/store_controller.cpp
+++ apps/statistics/store_controller.cpp
@@ -32,8 +32,8 @@ void StoreController::setFormulaLabel() {
   static_cast<ContentView *>(view())->formulaInputView()->setBufferText(text);
 }
 
-void StoreController::fillColumnWithFormula(Expression * formula) {
-  privateFillColumnWithFormula(formula, Symbol::isSeriesSymbol);
+bool StoreController::fillColumnWithFormula(Expression * formula) {
+  return privateFillColumnWithFormula(formula, Symbol::isSeriesSymbol);
 }
 
 void StoreController::willDisplayCellAtLocation(HighlightCell * cell, int i, int j) {

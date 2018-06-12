1d94f9c56d26536834354ae4b47d84d648b4b603
Léa Saviot <lea.saviot@numworks.com>
2018-06-12 10:54:26 +0200

    [apps/stat/reg] Fix memory leak in Store Context

--- apps/shared/store_context.h
+++ apps/shared/store_context.h
@@ -2,7 +2,9 @@
 #define SHARED_STORE_CONTEXT_H
 
 #include <poincare/context.h>
+#include <poincare/complex.h>
 #include "double_pair_store.h"
+#include <cmath>
 
 namespace Shared {
 
@@ -12,7 +14,8 @@ public:
     Poincare::Context(),
     m_store(store),
     m_seriesPairIndex(-1),
-    m_parentContext(nullptr)
+    m_parentContext(nullptr),
+    m_value(Poincare::Complex<double>::Float(NAN))
   {}
   void setParentContext(Poincare::Context * parentContext) { m_parentContext = parentContext; }
   void setSeriesPairIndex(int j) { m_seriesPairIndex = j; }
@@ -21,6 +24,7 @@ protected:
   Shared::DoublePairStore * m_store;
   int m_seriesPairIndex;
   Poincare::Context * m_parentContext;
+  Poincare::Complex<double> m_value;
 };
 
 }

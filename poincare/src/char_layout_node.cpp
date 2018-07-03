de1614b12edfeba91d33707372771c9a055ef3a6
Léa Saviot <lea.saviot@numworks.com>
2018-07-03 10:02:29 +0200

    [poincare] Adapt CharLayoutNode and HorizontalLayoutNode

---
+++ poincare/src/char_layout_node.cpp
@@ -0,0 +1,45 @@
+#include <poincare/char_layout_node.h>
+
+namespace Poincare {
+
+void CharLayoutNode::moveCursorLeft(LayoutCursor * cursor, bool * shouldRecomputeLayout) {
+  if (cursor->position() == LayoutCursor::Position::Right) {
+    cursor->setPosition(LayoutCursor::Position::Left);
+    return;
+  }
+  LayoutNode * parentNode = parent();
+  if (parentNode != nullptr) {
+    parentNode->moveCursorLeft(cursor, shouldRecomputeLayout);
+  }
+}
+
+void CharLayoutNode::moveCursorRight(LayoutCursor * cursor, bool * shouldRecomputeLayout) {
+  if (cursor->position() == LayoutCursor::Position::Left) {
+    cursor->setPosition(LayoutCursor::Position::Right);
+    return;
+  }
+  LayoutNode * parentNode = parent();
+  if (parentNode != nullptr) {
+    parentNode->moveCursorRight(cursor, shouldRecomputeLayout);
+  }
+}
+
+// Sizing and positioning
+void CharLayoutNode::computeSize() {
+  assert(!m_sized);
+  m_frame.setSize(KDText::charSize(m_fontSize));
+  m_sized = true;
+}
+
+void CharLayoutNode::computeBaseline() {
+  assert(!m_baselined);
+  m_baseline = (KDText::charSize(m_fontSize).height()+1)/2; //TODO +1 ?
+  m_baselined = true;
+}
+
+void CharLayoutNode::render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) {
+  char string[2] = {m_char, 0};
+  ctx->drawString(string, p, m_fontSize, expressionColor, backgroundColor);
+}
+
+}

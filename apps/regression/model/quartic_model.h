682f12a1ef2c86eff13ca47bd5a693360d7defba
Léa Saviot <lea.saviot@numworks.com>
2018-07-09 10:19:28 +0200

    [apps/regression] Fix simplifiedExpression memoization
    The model does not keep a pointer to the simplifiedExpression, the
    method calling simplifiedExpression should delete it afterwards

--- apps/regression/model/quartic_model.h
+++ apps/regression/model/quartic_model.h
@@ -8,15 +8,13 @@ namespace Regression {
 
 class QuarticModel : public Model {
 public:
-  QuarticModel() : m_expression(nullptr) {}
+  using Model::Model;
   Poincare::ExpressionLayout * layout() override;
   Poincare::Expression * simplifiedExpression(double * modelCoefficients, Poincare::Context * context) override;
   I18n::Message formulaMessage() const override { return I18n::Message::QuarticRegressionFormula; }
   double evaluate(double * modelCoefficients, double x) const override;
   double partialDerivate(double * modelCoefficients, int derivateCoefficientIndex, double x) const override;
   int numberOfCoefficients() const override { return 5; }
-private:
-  Poincare::Expression * m_expression;
 };
 
 }
--- apps/regression/model/trigonometric_model.h
+++ apps/regression/model/trigonometric_model.h
@@ -8,15 +8,13 @@ namespace Regression {
 
 class TrigonometricModel : public Model {
 public:
-  TrigonometricModel() : m_expression(nullptr) {}
+  using Model::Model;
   Poincare::ExpressionLayout * layout() override;
   Poincare::Expression * simplifiedExpression(double * modelCoefficients, Poincare::Context * context) override;
   I18n::Message formulaMessage() const override { return I18n::Message::TrigonometricRegressionFormula; }
   double evaluate(double * modelCoefficients, double x) const override;
   double partialDerivate(double * modelCoefficients, int derivateCoefficientIndex, double x) const override;
   int numberOfCoefficients() const override { return 4; }
-private:
-  Poincare::Expression * m_expression;
 };
 
 }

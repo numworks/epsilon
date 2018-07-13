48a972f72e5e994738852f57135606bfe361c2bb
Léa Saviot <lea.saviot@numworks.com>
2018-07-13 14:39:29 +0200

    [poincare] ProductLayoutNode

---
+++ poincare/include/poincare/product_layout_node.h
@@ -0,0 +1,31 @@
+#ifndef POINCARE_PRODUCT_LAYOUT_NODE_H
+#define POINCARE_PRODUCT_LAYOUT_NODE_H
+
+#include <poincare/layout_engine.h>
+#include <poincare/sequence_layout_node.h>
+
+namespace Poincare {
+
+class ProductLayoutNode : public SequenceLayoutNode {
+public:
+  using SequenceLayoutNode::SequenceLayoutNode;
+  int writeTextInBuffer(char * buffer, int bufferSize, int numberOfSignificantDigits = PrintFloat::k_numberOfStoredSignificantDigits) const override;
+protected:
+  void render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) override;
+private:
+  constexpr static KDCoordinate k_lineThickness = 1;
+};
+
+class ProductLayoutRef : public LayoutReference<ProductLayoutNode> {
+public:
+  ProductLayoutRef(TreeNode * n) : LayoutReference<ProductLayoutNode>(n) {}
+  ProductLayoutRef(LayoutRef argument, LayoutRef lowerB, LayoutRef upperB) : LayoutReference<ProductLayoutNode>() {
+    addChildTreeAtIndex(argument, 0);
+    addChildTreeAtIndex(lowerB, 1);
+    addChildTreeAtIndex(upperB, 2);
+  }
+};
+
+}
+
+#endif

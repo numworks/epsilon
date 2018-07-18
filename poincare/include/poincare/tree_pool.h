b1640449d3bae527da3eb71ce4380b5a34f7fa26
Émilie Feral <emilie.feral@numworks.com>
2018-07-18 17:37:11 +0200

    [poincare] Tree: enable to create node of variable size given in parameters

--- poincare/include/poincare/tree_pool.h
+++ poincare/include/poincare/tree_pool.h
@@ -19,13 +19,13 @@ public:
   TreeNode * last() const { return reinterpret_cast<TreeNode *>(const_cast<char *>(m_cursor)); }
 
   template <typename T>
-  TreeNode * createTreeNode() {
+  TreeNode * createTreeNode(size_t size) {
     int nodeIdentifier = generateIdentifier();
     if (nodeIdentifier == -1) {
       T::FailedAllocationStaticNode()->retain();
       return T::FailedAllocationStaticNode();
     }
-    void * ptr = alloc(sizeof(T));
+    void * ptr = alloc(size);
     if (ptr == nullptr) {
       T::FailedAllocationStaticNode()->retain();
       return T::FailedAllocationStaticNode();
@@ -80,7 +80,7 @@ public:
 private:
   constexpr static int BufferSize = 1024;//2048;
   constexpr static int MaxNumberOfNodes = BufferSize/sizeof(TreeNode);
-  constexpr static int MaxNumberOfStaticNodes = 2;
+  constexpr static int MaxNumberOfStaticNodes = 2; // ExpressionNode, LayoutNode
 
   // TreeNode
   void discardTreeNode(TreeNode * node) {

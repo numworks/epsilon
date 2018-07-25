2e8c7dee92822c5606020bded47acfccfae4608d
Léa Saviot <lea.saviot@numworks.com>
2018-07-25 17:02:09 +0200

    [poincare] MatrixLayoutNode

--- poincare/include/poincare/layout_node.h
+++ poincare/include/poincare/layout_node.h
@@ -107,6 +107,7 @@ public:
   virtual bool willAddChildAtIndex(LayoutNode * l, int * index, LayoutCursor * cursor) { return true; }
   virtual bool willAddSibling(LayoutCursor * cursor, LayoutNode * sibling, bool moveCursor) { return true; }
   virtual bool willReplaceChild(LayoutNode * oldChild, LayoutNode * newChild, LayoutCursor * cursor, bool force);
+  virtual void didReplaceChildAtIndex(int index, LayoutCursor * cursor, bool force) {}
   virtual bool willRemoveChild(LayoutNode * l, LayoutCursor * cursor, bool force);
   virtual void didRemoveChildAtIndex(int index, LayoutCursor * cursor, bool force) {}
 protected:

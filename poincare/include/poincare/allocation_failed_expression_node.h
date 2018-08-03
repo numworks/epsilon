ef52ac323b8ef4f8eaf48c3aeda9c73337b4f5cc
Léa Saviot <lea.saviot@numworks.com>
2018-08-03 11:16:50 +0200

    [poincare] Ghost nodes

--- poincare/include/poincare/allocation_failed_expression_node.h
+++ poincare/include/poincare/allocation_failed_expression_node.h
@@ -1,28 +1,22 @@
 #ifndef POINCARE_ALLOCATION_FAILED_EXPRESSION_NODE_H
 #define POINCARE_ALLOCATION_FAILED_EXPRESSION_NODE_H
 
-#include <poincare/expression_node.h>
-#include <poincare/expression_reference.h>
-#include <poincare/complex.h>
-#include <stdio.h>
+#include <poincare/ghost_expression_node.h>
+#include <poincare/allocation_failed_layout_node.h>
 
 namespace Poincare {
 
-class AllocationFailedExpressionNode : public ExpressionNode {
+class AllocationFailedExpressionNode : public GhostExpressionNode {
 public:
   // ExpressionNode
   Type type() const override { return Type::AllocationFailure; }
-  EvaluationReference<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return ComplexReference<float>::Undefined(); }
-  EvaluationReference<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return ComplexReference<double>::Undefined(); }
-  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
-  int writeTextInBuffer(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode = Preferences::PrintFloatMode::Decimal, int numberOfSignificantDigits = 0) const override;
+  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override { return AllocationFailedLayoutRef(); }
 
   // TreeNode
   size_t size() const override { return sizeof(AllocationFailedExpressionNode); }
 #if TREE_LOG
-  const char * description() const override { return "Allocation Failed";  }
+  const char * description() const override { return "AllocationFailedExpression";  }
 #endif
-  int numberOfChildren() const override { return 0; }
   bool isAllocationFailure() const override { return true; }
 };
 

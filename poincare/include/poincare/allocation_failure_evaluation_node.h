5f620b058818cf3087d7c080e16dad119675d926
Léa Saviot <lea.saviot@numworks.com>
2018-08-08 17:52:23 +0200

    [poincare] AllocationFailureEvaluationNode

---
+++ poincare/include/poincare/allocation_failure_evaluation_node.h
@@ -0,0 +1,32 @@
+#ifndef POINCARE_ALLOCATION_FAILURE_EVALUATION_H
+#define POINCARE_ALLOCATION_FAILURE_EVALUATION_H
+
+#include <poincare/evaluation.h>
+#include <poincare/complex.h>
+#include <poincare/undefined.h>
+
+namespace Poincare {
+
+template <template<typename> typename T, typename U>
+class AllocationFailureEvaluationNode : public T<U> {
+public:
+  // EvaluationNode
+  typename EvaluationNode<U>::Type type() const override { return EvaluationNode<U>::Type::AllocationFailure; }
+  bool isUndefined() const override { return true; }
+  U toScalar() const override { return NAN; }
+  Expression complexToExpression(Preferences::Preferences::ComplexFormat complexFormat) const override { return Undefined(); }
+  std::complex<U> trace() const override { return std::complex<U>(NAN); }
+  std::complex<U> determinant() const override { return std::complex<U>(NAN); }
+  Evaluation<U> inverse() const override { return Complex<U>::Undefined(); }
+  virtual Evaluation<U> transpose() const override { return Complex<U>::Undefined(); }
+  // TreeNode
+  size_t size() const override { return sizeof(AllocationFailureEvaluationNode<T, U>); }
+#if TREE_LOG
+  const char * description() const override { return "AllocationFailureEvaluation";  }
+#endif
+  bool isAllocationFailure() const override { return true; }
+};
+
+}
+
+#endif

d1fd73f420a2bf5b674e3ee3253d613c6ff39848
Léa Saviot <lea.saviot@numworks.com>
2018-08-10 12:04:37 +0200

    [poincare] Left/Right Parenthesis and SquareBracket, Integral layouts

--- poincare/src/right_square_bracket_layout_node.cpp
+++ poincare/src/right_square_bracket_layout_node.cpp
@@ -1,6 +1,13 @@
 #include <poincare/right_square_bracket_layout_node.h>
+#include <poincare/allocation_failure_layout_node.h>
 
 namespace Poincare {
+
+RightSquareBracketLayoutNode * RightSquareBracketLayoutNode::FailedAllocationStaticNode() {
+  static AllocationFailureLayoutNode<RightSquareBracketLayoutNode> failure;
+  TreePool::sharedPool()->registerStaticNodeIfRequired(&failure);
+  return &failure;
+}
 
 void RightSquareBracketLayoutNode::render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) {
   ctx->fillRect(KDRect(p.x()+k_widthMargin, p.y(), k_lineThickness, childHeight()), expressionColor);

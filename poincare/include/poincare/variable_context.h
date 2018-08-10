28d9ce89faa0bc62948989f62214ac495353edc9
Léa Saviot <lea.saviot@numworks.com>
2018-08-10 10:19:06 +0200

    [poincare] VariableContext

--- poincare/include/poincare/variable_context.h
+++ poincare/include/poincare/variable_context.h
@@ -2,7 +2,7 @@
 #define POINCARE_VARIABLE_CONTEXT_H
 
 #include <poincare/context.h>
-#include <poincare/approximation.h>
+#include <poincare/float.h>
 
 namespace Poincare {
 
@@ -11,11 +11,14 @@ class VariableContext : public Context {
 public:
   VariableContext(char name, Context * parentContext = nullptr);
   void setApproximationForVariable(T value);
-  void setExpressionForSymbolName(const Expression * expression, const Symbol * symbol, Context & context) override;
-  const Expression * expressionForSymbol(const Symbol * symbol) override;
+
+  // Context
+  void setExpressionForSymbolName(const Expression expression, const Symbol symbol, Context & context) override;
+  const Expression expressionForSymbol(const Symbol symbol) override;
+
 private:
   char m_name;
-  Approximation<T> m_value;
+  Float<T> m_value;
   Context * m_parentContext;
 };
 

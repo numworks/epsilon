e9c42e221b5aab612079227f1529ae9d7506f6ee
Émilie Feral <emilie.feral@numworks.com>
2018-08-10 10:14:06 +0200

    [calculation] Fix Calculation

--- apps/calculation/calculation.h
+++ apps/calculation/calculation.h
@@ -16,22 +16,21 @@ public:
     Equal
   };
   Calculation();
-  ~Calculation(); // Delete expression and layout, if needed
   Calculation& operator=(const Calculation& other);
   Calculation(const Calculation& other) = delete;
   Calculation(Calculation&& other) = delete;
   Calculation& operator=(Calculation&& other) = delete;
   /* c.reset() is the equivalent of c = Calculation() without copy assingment. */
   void reset();
-  void setContent(const char * c, Poincare::Context * context, Poincare::Expression * ansExpression);
+  void setContent(const char * c, Poincare::Context * context, Poincare::Expression ansExpression);
   KDCoordinate height(Poincare::Context * context);
   const char * inputText();
   const char * exactOutputText();
   const char * approximateOutputText();
-  Poincare::Expression * input();
+  Poincare::Expression input();
   Poincare::LayoutRef createInputLayout();
-  Poincare::Expression * approximateOutput(Poincare::Context * context);
-  Poincare::Expression * exactOutput(Poincare::Context * context);
+  Poincare::Expression approximateOutput(Poincare::Context * context);
+  Poincare::Expression exactOutput(Poincare::Context * context);
   Poincare::LayoutRef createExactOutputLayout(Poincare::Context * context);
   Poincare::LayoutRef createApproximateOutputLayout(Poincare::Context * context);
   bool isEmpty();
@@ -47,9 +46,9 @@ private:
   char m_inputText[k_printedExpressionSize];
   char m_exactOutputText[k_printedExpressionSize];
   char m_approximateOutputText[k_printedExpressionSize];
-  Poincare::Expression * m_input;
-  Poincare::Expression * m_exactOutput;
-  Poincare::Expression * m_approximateOutput;
+  Poincare::Expression m_input;
+  Poincare::Expression m_exactOutput;
+  Poincare::Expression m_approximateOutput;
   KDCoordinate m_height;
   EqualSign m_equalSign;
 };

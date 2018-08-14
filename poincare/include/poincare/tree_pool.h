dab9b5cd2106c0a7362ca39f28c508c4832eb4b2
Émilie Feral <emilie.feral@numworks.com>
2018-08-14 13:21:13 +0200

    [poincare] Fix and add tests on Integer::Division

--- poincare/include/poincare/tree_pool.h
+++ poincare/include/poincare/tree_pool.h
@@ -116,7 +116,7 @@ public:
   }
 
 private:
-  constexpr static int BufferSize = 1024;//2048;
+  constexpr static int BufferSize = 2048;
   constexpr static int MaxNumberOfNodes = BufferSize/sizeof(TreeNode);
   constexpr static int MaxNumberOfStaticNodes = 200; // TODO: count how may are needed
 

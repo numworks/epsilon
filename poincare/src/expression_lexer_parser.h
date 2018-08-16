0e09ddd6f54c13673b39fda68066c3b2c8fc2c95
Émilie Feral <emilie.feral@numworks.com>
2018-08-16 16:02:02 +0200

    [poincare] Add a common header for lexer and parser to defined YYTYPE

---
+++ poincare/src/expression_lexer_parser.h
@@ -0,0 +1,13 @@
+#include <poincare.h>
+
+/* Usually, YYSTYPE is defined as the union of the objects it might be. Here,
+ * some of these objects are non-trivial (specific copy-constructors and
+ * destructors), so they cannot be part of a union. We must use a struct to
+ * define YYSTYPE. */
+
+struct OurNodeValue{
+    Poincare::Expression expression;
+    //Poincare::Symbol symbol;
+    char character;
+};
+#define YYSTYPE OurNodeValue

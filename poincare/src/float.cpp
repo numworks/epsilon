96e0c799999eca54c4c066928476a9aea1187972
Léa Saviot <lea.saviot@numworks.com>
2018-08-17 17:18:26 +0200

    [poincare] Implement class Float

--- poincare/src/float.cpp
+++ poincare/src/float.cpp
@@ -3,10 +3,29 @@
 
 namespace Poincare {
 
-template<typename T> FloatNode<T> * FloatNode<T>::FailedAllocationStaticNode() {
+template<typename T>
+FloatNode<T> * FloatNode<T>::FailedAllocationStaticNode() {
   static AllocationFailureExpressionNode<FloatNode<T>> failure;
   TreePool::sharedPool()->registerStaticNodeIfRequired(&failure);
   return &failure;
+}
+
+template<typename T>
+Expression FloatNode<T>::setSign(Sign s, Context & context, Preferences::AngleUnit angleUnit) const {
+  return Float<T>(-m_value);
+}
+
+template<typename T>
+int FloatNode<T>::simplificationOrderSameType(const ExpressionNode * e, bool canBeInterrupted) const {
+  assert(e->type() == ExpressionNode::Type::Float);
+  const FloatNode<T> * other = static_cast<const FloatNode<T> *>(e);
+  if (value() < other->value()) {
+    return -1;
+  }
+  if (value() > other->value()) {
+    return 1;
+  }
+  return 0;
 }
 
 template class FloatNode<float>;

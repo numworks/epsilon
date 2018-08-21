7ca084ca9192e257e229ba0188d54cb0dd4998fd
Léa Saviot <lea.saviot@numworks.com>
2018-08-21 14:37:38 +0200

    [poincare] SquareRoot

--- poincare/include/poincare/square_root.h
+++ poincare/include/poincare/square_root.h
@@ -1,32 +1,55 @@
 #ifndef POINCARE_SQUARE_ROOT_H
 #define POINCARE_SQUARE_ROOT_H
 
-#include <poincare/layout_helper.h>
-#include <poincare/static_hierarchy.h>
 #include <poincare/approximation_helper.h>
+#include <poincare/expression.h>
+#include <poincare/layout_helper.h>
 
 namespace Poincare {
 
-class SquareRoot : public StaticHierarchy<1>  {
-  using StaticHierarchy<1>::StaticHierarchy;
+class SquareRootNode : public ExpressionNode  {
 public:
-  Type type() const override;
+  // ExpressionNode
+  Type type() const override { return Type::SquareRoot; }
+
+  // TreeNode
+  static SquareRootNode * FailedAllocationStaticNode();
+  SquareRootNode * failedAllocationStaticNode() override { return FailedAllocationStaticNode(); }
+  size_t size() const override { return sizeof(SquareRootNode); }
+  int numberOfChildren() const override { return 1; }
+#if POINCARE_TREE_LOG
+  virtual void logNodeName(std::ostream & stream) const override {
+    stream << "SquareRoot";
+  }
+#endif
+
+
 private:
-  /* Layout */
-  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
+  // Layout
+  LayoutReference createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
-  /* Simplification */
-  Expression shallowReduce(Context& context, Preferences::AngleUnit angleUnit) override;
-  /* Evaluation */
-  template<typename T> static std::complex<T> computeOnComplex(const std::complex<T> c, Preferences::AngleUnit angleUnit);
+  // Simplification
+  Expression shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const override;
+  // Evaluation
+  template<typename T> static Complex<T> computeOnComplex(const std::complex<T> c, Preferences::AngleUnit angleUnit);
   Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override {
     return ApproximationHelper::Map<float>(this, context, angleUnit,computeOnComplex<float>);
   }
   Evaluation<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override {
     return ApproximationHelper::Map<double>(this, context, angleUnit, computeOnComplex<double>);
   }
-
 };
+
+class SquareRoot : public Expression {
+public:
+  SquareRoot(const SquareRootNode * n) : Expression(n) {}
+  SquareRoot(Expression operand) : Expression(TreePool::sharedPool()->createTreeNode<SquareRootNode>()) {
+    replaceChildAtIndexInPlace(0, operand);
+  }
+
+  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit);
+};
+
 
 }
 

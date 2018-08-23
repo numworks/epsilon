691abe79ca95de8ca3743d9e84f2493f23f853cf
Léa Saviot <lea.saviot@numworks.com>
2018-08-23 11:19:25 +0200

    [poincare] Derivative

--- poincare/include/poincare/derivative.h
+++ poincare/include/poincare/derivative.h
@@ -1,29 +1,46 @@
 #ifndef POINCARE_DERIVATIVE_H
 #define POINCARE_DERIVATIVE_H
 
+#include <poincare/expression.h>
 #include <poincare/layout_helper.h>
-#include <poincare/static_hierarchy.h>
+#include <poincare/serialization_helper.h>
 #include <poincare/variable_context.h>
 
 namespace Poincare {
 
-class Derivative : public StaticHierarchy<2> {
-  using StaticHierarchy<2>::StaticHierarchy;
+class DerivativeNode : public ExpressionNode {
 public:
-  Type type() const override;
+  // Allocation Failure
+  static DerivativeNode * FailedAllocationStaticNode();
+  DerivativeNode * failedAllocationStaticNode() override { return FailedAllocationStaticNode(); }
+
+  // TreeNode
+  size_t size() const override { return sizeof(DerivativeNode); }
+  int numberOfChildren() const override { return 2; }
+#if POINCARE_TREE_LOG
+  virtual void logNodeName(std::ostream & stream) const override {
+    stream << "Derivative";
+  }
+#endif
+
+  // Properties
+  Type type() const override { return Type::Derivative; }
   int polynomialDegree(char symbolName) const override;
+
 private:
-  /* Layout */
-  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
+  // Layout
+  LayoutReference createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
     return LayoutHelper::Prefix(this, floatDisplayMode, numberOfSignificantDigits, name());
   }
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
     return SerializationHelper::Prefix(this, buffer, bufferSize, floatDisplayMode, numberOfSignificantDigits, name());
   }
   const char * name() const { return "diff"; }
-  /* Simplification */
-  Expression shallowReduce(Context& context, Preferences::AngleUnit angleUnit) override;
-  /* Evaluation */
+
+  // Simplification
+  Expression shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const override;
+
+  // Evaluation
   Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<float>(context, angleUnit); }
   Evaluation<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<double>(context, angleUnit); }
   template<typename T> Evaluation<T> templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const;
@@ -33,6 +50,17 @@ private:
   constexpr static double k_maxErrorRateOnApproximation = 0.001;
   constexpr static double k_minInitialRate = 0.01;
   constexpr static double k_rateStepSize = 1.4;
+};
+
+class Derivative : public Expression {
+public:
+  Derivative(const DerivativeNode * n) : Expression(n) {}
+  Derivative(Expression child1, Expression child2) : Expression(TreePool::sharedPool()->createTreeNode<DerivativeNode>()) {
+    replaceChildAtIndexInPlace(0, child1);
+    replaceChildAtIndexInPlace(1, child2);
+  }
+
+  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) const;
 };
 
 }
--- poincare/src/derivative.cpp
+++ poincare/src/derivative.cpp
@@ -3,57 +3,41 @@
 #include <poincare/simplification_helper.h>
 #include <poincare/undefined.h>
 #include <cmath>
-extern "C" {
 #include <assert.h>
 #include <float.h>
-}
 
 namespace Poincare {
 
-Expression::Type Derivative::type() const {
-  return Type::Derivative;
+DerivativeNode * DerivativeNode::FailedAllocationStaticNode() {
+  static AllocationFailureExpressionNode<DerivativeNode> failure;
+  TreePool::sharedPool()->registerStaticNodeIfRequired(&failure);
+  return &failure;
 }
 
-Expression * Derivative::clone() const {
-  Derivative * a = new Derivative(m_operands, true);
-  return a;
-}
-
-int Derivative::polynomialDegree(char symbolName) const {
+int DerivativeNode::polynomialDegree(char symbolName) const {
   if (symbolName == 'x') {
-    if (operand(1)->polynomialDegree(symbolName) != 0) {
+    if (childAtIndex(1)->polynomialDegree(symbolName) != 0) {
       return -1;
     }
     return 0;
   }
-  return Expression::polynomialDegree(symbolName);
+  return ExpressionNode::polynomialDegree(symbolName);
 }
 
-Expression Derivative::shallowReduce(Context& context, Preferences::AngleUnit angleUnit) {
-  Expression * e = Expression::defaultShallowReduce(context, angleUnit);
-  if (e != this) {
-    return e;
-  }
-#if MATRIX_EXACT_REDUCING
-  if (operand(0)->type() == Type::Matrix || operand(1)->type() == Type::Matrix) {
-    return replaceWith(new Undefined(), true);
-  }
-#endif
-  // TODO: to be implemented diff(+) -> +diff() etc
-  return this;
+Expression DerivativeNode::shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const {
+  return Derivative(this).shallowReduce(context, angleUnit);
 }
 
 template<typename T>
-Complex<T> * Derivative::templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const {
+Evaluation<T> DerivativeNode::templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const {
   static T min = sizeof(T) == sizeof(double) ? DBL_MIN : FLT_MIN;
   static T epsilon = sizeof(T) == sizeof(double) ? DBL_EPSILON : FLT_EPSILON;
-  Evaluation<T> * xInput = operand(1)->privateApproximate(T(), context, angleUnit);
-  T x = xInput->toScalar();
-  delete xInput;
-  T functionValue = operand(0)->approximateWithValueForSymbol('x', x, context, angleUnit);
+  Evaluation<T> xInput = childAtIndex(1)->approximate(T(), context, angleUnit);
+  T x = xInput.toScalar();
+  T functionValue = Expression(childAtIndex(0)).approximateWithValueForSymbol('x', x, context, angleUnit);
   // No complex/matrix version of Derivative
   if (std::isnan(x) || std::isnan(functionValue)) {
-    return new Complex<T>(Complex<T>::Undefined());
+    return Complex<T>::Undefined();
   }
 
   T error, result;
@@ -63,39 +47,38 @@ Complex<T> * Derivative::templatedApproximate(Context& context, Preferences::Ang
     h /= 10.0;
   } while ((std::fabs(error/result) > k_maxErrorRateOnApproximation || std::isnan(error)) && h >= epsilon);
 
-  /* if the error is too big regarding the value, do not return the answer */
+  // If the error is too big regarding the value, do not return the answer.
   if (std::fabs(error/result) > k_maxErrorRateOnApproximation || std::isnan(error)) {
-    return new Complex<T>(Complex<T>::Undefined());
+    return Complex<T>::Undefined();
   }
   if (std::fabs(error) < min) {
-    return new Complex<T>(result);
+    return Complex<T>(result);
   }
   error = std::pow((T)10, std::floor(std::log10(std::fabs(error)))+2);
-  return new Complex<T>(std::round(result/error)*error);
+  return Complex<T>(std::round(result/error)*error);
 }
 
 template<typename T>
-T Derivative::growthRateAroundAbscissa(T x, T h, Context & context, Preferences::AngleUnit angleUnit) const {
-  T expressionPlus = operand(0)->approximateWithValueForSymbol('x', x+h, context, angleUnit);
-  T expressionMinus = operand(0)->approximateWithValueForSymbol('x', x-h, context, angleUnit);
+T DerivativeNode::growthRateAroundAbscissa(T x, T h, Context & context, Preferences::AngleUnit angleUnit) const {
+  T expressionPlus = Expression(childAtIndex(0)).approximateWithValueForSymbol('x', x+h, context, angleUnit);
+  T expressionMinus = Expression(childAtIndex(0)).approximateWithValueForSymbol('x', x-h, context, angleUnit);
   return (expressionPlus - expressionMinus)/(2*h);
 }
 
 template<typename T>
-T Derivative::riddersApproximation(Context & context, Preferences::AngleUnit angleUnit, T x, T h, T * error) const {
+T DerivativeNode::riddersApproximation(Context & context, Preferences::AngleUnit angleUnit, T x, T h, T * error) const {
   /* Ridders' Algorithm
    * Blibliography:
    * - Ridders, C.J.F. 1982, Advances in Helperering Software, vol. 4, no. 2,
    * pp. 75–76. */
 
   *error = sizeof(T) == sizeof(float) ? FLT_MAX : DBL_MAX;
-  // Initialize hh
   assert(h != 0.0);
-  /* Make hh an exactly representable number */
-  volatile T temp =  x+h;
+  // Initialize hh, make hh an exactly representable number
+  volatile T temp = x+h;
   T hh = temp - x;
-  /* a is matrix storing the function extrapolations for different stepsizes at
-  * different order */
+  /* A is matrix storing the function extrapolations for different stepsizes at
+   * different order */
   T a[10][10];
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
@@ -105,20 +88,20 @@ T Derivative::riddersApproximation(Context & context, Preferences::AngleUnit ang
   a[0][0] = growthRateAroundAbscissa(x, hh, context, angleUnit);
   T ans = 0;
   T errt = 0;
-  /* Loop on i: change the step size */
+  // Loop on i: change the step size
   for (int i = 1; i < 10; i++) {
     hh /= k_rateStepSize;
-    /* Make hh an exactly representable number */
-    volatile T temp =  x+hh;
+    // Make hh an exactly representable number
+    volatile T temp = x+hh;
     hh = temp - x;
     a[0][i] = growthRateAroundAbscissa(x, hh, context, angleUnit);
     T fac = k_rateStepSize*k_rateStepSize;
-    /* Loop on j: compute extrapolation for several orders */
+    // Loop on j: compute extrapolation for several orders
     for (int j = 1; j < 10; j++) {
       a[j][i] = (a[j-1][i]*fac-a[j-1][i-1])/(fac-1);
       fac = k_rateStepSize*k_rateStepSize*fac;
       errt = std::fabs(a[j][i]-a[j-1][i]) > std::fabs(a[j][i]-a[j-1][i-1]) ? std::fabs(a[j][i]-a[j-1][i]) : std::fabs(a[j][i]-a[j-1][i-1]);
-      /* Update error and answer if error decreases */
+      // Update error and answer if error decreases
       if (errt < *error) {
         *error = errt;
         ans = a[j][i];
@@ -131,6 +114,20 @@ T Derivative::riddersApproximation(Context & context, Preferences::AngleUnit ang
     }
   }
   return ans;
+}
+
+Expression Derivative::shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const {
+  Expression e = Expression::defaultShallowReduce(context, angleUnit);
+  if (e.isUndefinedOrAllocationFailure()) {
+    return e;
+  }
+#if MATRIX_EXACT_REDUCING
+  if (childAtIndex(0).type() == ExpressionNode::Type::Matrix || childAtIndex(1).type() == ExpressionNode::Type::Matrix) {
+    return Undefined();
+  }
+#endif
+  // TODO: to be implemented diff(+) -> +diff() etc
+  return *this;
 }
 
 }

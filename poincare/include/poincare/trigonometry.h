bc189674c4248b5c956519d9858c6b90395fa260
Léa Saviot <lea.saviot@numworks.com>
2018-08-23 11:24:19 +0200

    [poincare] Finish translating Trigonometry

--- poincare/include/poincare/trigonometry.h
+++ poincare/include/poincare/trigonometry.h
@@ -17,7 +17,7 @@ public:
   static Expression shallowReduceInverseFunction(Expression e, Context& context, Preferences::AngleUnit angleUnit);
   static bool ExpressionIsEquivalentToTangent(const Expression e);
   constexpr static int k_numberOfEntries = 37;
-  static Expression table(const Expression e, Expression::Type type, Context & context, Preferences::AngleUnit angleUnit); // , Function f, bool inverse
+  static Expression table(const Expression e, ExpressionNode::Type type, Context & context, Preferences::AngleUnit angleUnit); // , Function f, bool inverse
   template <typename T> static std::complex<T> ConvertToRadian(const std::complex<T> c, Preferences::AngleUnit angleUnit);
   template <typename T> static std::complex<T> ConvertRadianToAngleUnit(const std::complex<T> c, Preferences::AngleUnit angleUnit);
   template <typename T> static std::complex<T> RoundToMeaningfulDigits(const std::complex<T> c);

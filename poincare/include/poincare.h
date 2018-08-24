38e1da636b161030c4ddb4f08434918d5c55dddc
Léa Saviot <lea.saviot@numworks.com>
2018-08-24 15:30:44 +0200

    [poincare] ArcSine

--- poincare/include/poincare.h
+++ poincare/include/poincare.h
@@ -108,6 +108,7 @@
 #include <poincare/global_context.h>
 #include <poincare/addition.h>
 #include <poincare/arc_cosine.h>
+#include <poincare/arc_sine.h>
 #include <poincare/cosine.h>
 #include <poincare/sine.h>
 #include <poincare/tangent.h>

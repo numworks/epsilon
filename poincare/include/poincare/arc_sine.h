16a005e104524df3d4181971445e0d45a3bcfd7b
Léa Saviot <lea.saviot@numworks.com>
2018-08-24 15:30:44 +0200

    [poincare] ArcSine

--- poincare/include/poincare/arc_sine.h
+++ poincare/include/poincare/arc_sine.h
@@ -1,36 +1,58 @@
 #ifndef POINCARE_ARC_SINE_H
 #define POINCARE_ARC_SINE_H
 
-#include <poincare/layout_helper.h>
-#include <poincare/static_hierarchy.h>
 #include <poincare/approximation_helper.h>
+#include <poincare/expression.h>
+#include <poincare/serialization_helper.h>
 #include <poincare/trigonometry.h>
 
 namespace Poincare {
 
-class ArcSine : public StaticHierarchy<1>  {
-  using StaticHierarchy<1>::StaticHierarchy;
+class ArcSineNode : public ExpressionNode {
 public:
-  Type type() const override;
-private:
-  /* Layout */
-  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
-    return LayoutHelper::Prefix(this, floatDisplayMode, numberOfSignificantDigits, name());
+  // Allocation Failure
+  static ArcSineNode * FailedAllocationStaticNode();
+  ArcSineNode * failedAllocationStaticNode() override { return FailedAllocationStaticNode(); }
+
+  // TreeNode
+  size_t size() const override { return sizeof(ArcSineNode); }
+  int numberOfChildren() const override { return 1; }
+#if POINCARE_TREE_LOG
+  virtual void logNodeName(std::ostream & stream) const override {
+    stream << "ArcSine";
   }
+#endif
+
+  // Properties
+  Type type() const override { return Type::ArcSine; }
+private:
+  // Layout
+  LayoutReference createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
     return SerializationHelper::Prefix(this, buffer, bufferSize, floatDisplayMode, numberOfSignificantDigits, name());
   }
   const char * name() const { return "asin"; }
-  /* Simplification */
-  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) override;
-  /* Evaluation */
-  template<typename T> static std::complex<T> computeOnComplex(const std::complex<T> c, Preferences::AngleUnit angleUnit);
+  // Simplification
+  Expression shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const override;
+  //Evaluation
+  template<typename T> static Complex<T> computeOnComplex(const std::complex<T> c, Preferences::AngleUnit angleUnit);
   Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override {
-    return ApproximationHelper::Map<float>(this, context, angleUnit, computeOnComplex<float>);
+    return ApproximationHelper::Map<float>(this, context, angleUnit,computeOnComplex<float>);
   }
   Evaluation<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override {
     return ApproximationHelper::Map<double>(this, context, angleUnit, computeOnComplex<double>);
   }
+};
+
+class ArcSine : public Expression {
+public:
+  ArcSine() : Expression(TreePool::sharedPool()->createTreeNode<ArcSineNode>()) {}
+  ArcSine(const ArcSineNode * n) : Expression(n) {}
+  ArcSine(Expression operand) : ArcSine() {
+    replaceChildAtIndexInPlace(0, operand);
+  }
+
+  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) const;
 };
 
 }

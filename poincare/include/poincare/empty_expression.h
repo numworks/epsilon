45a6dfca0aa6b7125e0a392ee4832efb1654bbe2
Léa Saviot <lea.saviot@numworks.com>
2018-08-27 14:41:17 +0200

    [poincare] EmptyExpression

--- poincare/include/poincare/empty_expression.h
+++ poincare/include/poincare/empty_expression.h
@@ -1,26 +1,43 @@
 #ifndef POINCARE_EMPTY_EXPRESSION_H
 #define POINCARE_EMPTY_EXPRESSION_H
 
-#include <poincare/static_hierarchy.h>
-#include <poincare/evaluation.h>
+#include <poincare/expression.h>
 
 namespace Poincare {
 
-/* An empty expression awaits completion by the user. */
+// An empty expression awaits completion by the user.
 
-class EmptyExpression : public StaticHierarchy<0> {
+class EmptyExpressionNode : public ExpressionNode  {
 public:
-  Type type() const override {
-    return Type::EmptyExpression;
+  // Allocation Failure
+  static EmptyExpressionNode * FailedAllocationStaticNode();
+  EmptyExpressionNode * failedAllocationStaticNode() override { return FailedAllocationStaticNode(); }
+
+  // TreeNode
+  size_t size() const override { return sizeof(EmptyExpressionNode); }
+  int numberOfChildren() const override { return 0; }
+#if POINCARE_TREE_LOG
+  virtual void logNodeName(std::ostream & stream) const override {
+    stream << "EmptyExpression";
   }
+#endif
+
+  // Properties
+  Type type() const override { return Type::EmptyExpression; }
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
 private:
-  /* Layout */
-  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
-  /* Evaluation */
+  // Layout
+  LayoutReference createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
+  // Evaluation
   Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<float>(context, angleUnit); }
   Evaluation<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<double>(context, angleUnit); }
   template<typename T> Evaluation<T> templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const;
+};
+
+class EmptyExpression : public Expression {
+public:
+  EmptyExpression() : Expression(TreePool::sharedPool()->createTreeNode<EmptyExpressionNode>()) {}
+  EmptyExpression(const EmptyExpressionNode * n) : Expression(n) {}
 };
 
 }

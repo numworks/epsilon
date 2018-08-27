c3b8129bebb5da1db492a50fcce3dcf42bf444d8
Émilie Feral <emilie.feral@numworks.com>
2018-08-27 13:55:45 +0200

    [poincare] Fix Determinant

--- poincare/include/poincare/determinant.h
+++ poincare/include/poincare/determinant.h
@@ -1,30 +1,53 @@
 #ifndef POINCARE_DETERMINANT_H
 #define POINCARE_DETERMINANT_H
 
-#include <poincare/layout_helper.h>
-#include <poincare/static_hierarchy.h>
+#include <poincare/serialization_helper.h>
+#include <poincare/expression.h>
 
 namespace Poincare {
 
-class Determinant : public StaticHierarchy<1> {
-  using StaticHierarchy<1>::StaticHierarchy;
+class DeterminantNode : public ExpressionNode {
 public:
-  Type type() const override;
+  // Allocation Failure
+  static DeterminantNode * FailedAllocationStaticNode();
+  DeterminantNode * failedAllocationStaticNode() override { return FailedAllocationStaticNode(); }
+
+  // TreeNode
+  size_t size() const override { return sizeof(DeterminantNode); }
+  int numberOfChildren() const override { return 1; }
+#if POINCARE_TREE_LOG
+  virtual void logNodeName(std::ostream & stream) const override {
+    stream << "Determinant";
+  }
+#endif
+
+  Type type() const override { return Type::Determinant; }
 private:
   /* Layout */
-  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
-    return LayoutHelper::Prefix(this, floatDisplayMode, numberOfSignificantDigits, name());
-  }
+  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
+  /* Serialization */
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
     return SerializationHelper::Prefix(this, buffer, bufferSize, floatDisplayMode, numberOfSignificantDigits, name());
   }
   const char * name() const { return "det"; }
   /* Simplification */
-  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) override;
-  /* Evaluation */
+  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) const override;
+  /* Approximation */
   Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<float>(context, angleUnit); }
   Evaluation<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<double>(context, angleUnit); }
  template<typename T> Evaluation<T> templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const;
+};
+
+
+class Determinant : public Expression {
+public:
+  Determinant() : Expression(TreePool::sharedPool()->createTreeNode<DeterminantNode>()) {}
+  Determinant(const DeterminantNode * n) : Expression(n) {}
+  Determinant(Expression operand) : Determinant() {
+    replaceChildAtIndexInPlace(0, operand);
+  }
+
+  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) const;
 };
 
 }

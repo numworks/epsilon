73433e49e2f93e07f89c00e9dd2de11923bab047
Léa Saviot <lea.saviot@numworks.com>
2018-08-27 15:03:37 +0200

    [poincare] Equal

--- poincare/src/equal.cpp
+++ poincare/src/equal.cpp
@@ -1,11 +1,5 @@
-extern "C" {
-#include <assert.h>
-#include <stdlib.h>
-#include <math.h>
-#include <limits.h>
-}
 #include <poincare/equal.h>
-#include <ion.h>
+#include <poincare/allocation_failure_expression_node.h>
 #include <poincare/rational.h>
 #include <poincare/addition.h>
 #include <poincare/division.h>
@@ -17,39 +11,31 @@ extern "C" {
 #include <poincare/symbol.h>
 #include <poincare/char_layout_node.h>
 #include <poincare/horizontal_layout_node.h>
-
+#include <ion.h>
+extern "C" {
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+}
 namespace Poincare {
 
-ExpressionNode::Type Equal::type() const {
-  return Type::Equal;
+EqualNode * EqualNode::FailedAllocationStaticNode() {
+  static AllocationFailureExpressionNode<EqualNode> failure;
+  TreePool::sharedPool()->registerStaticNodeIfRequired(&failure);
+  return &failure;
 }
 
-Expression * Equal::clone() const {
-  return new Equal(operands(), true);
+Expression EqualNode::standardEquation(Context & context, Preferences::AngleUnit angleUnit) const {
+  Expression sub = Subtraction(Expression(childAtIndex(0)), Expression(childAtIndex(1)));
+  return sub.deepReduce(context, angleUnit);
 }
 
-int Equal::polynomialDegree(char symbolName) const {
-  return -1;
+Expression EqualNode::shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const {
+  return Equal(this).shallowReduce(context, angleUnit);
 }
 
-Expression * Equal::standardEquation(Context & context, Preferences::AngleUnit angleUnit) const {
-  Expression * sub = new Subtraction(childAtIndex(0), childAtIndex(1), true);
-  Reduce(&sub, context, angleUnit);
-  return sub;
-}
-
-Expression Equal::shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const {
-  Expression e = Expression::defaultShallowReduce(context, angleUnit);
-  if (e.isUndefinedOrAllocationFailure()) {
-    return e;
-  }
-  if (childAtIndex(0)->isIdenticalTo(childAtIndex(1))) {
-    return replaceWith(RationalReference(1), true);
-  }
-  return this;
-}
-
-LayoutRef Equal::createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const {
+LayoutRef EqualNode::createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const {
   HorizontalLayoutRef result;
   result.addOrMergeChildAtIndex(childAtIndex(0)->createLayout(floatDisplayMode, numberOfSignificantDigits), 0, false);
   result.addChildAtIndex(CharLayoutRef('='), result.numberOfChildren(), result.numberOfChildren(), nullptr);
@@ -57,9 +43,24 @@ LayoutRef Equal::createLayout(Preferences::PrintFloatMode floatDisplayMode, int 
   return result;
 }
 
+int EqualNode::serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const {
+  return SerializationHelper::Infix(this, buffer, bufferSize, floatDisplayMode, numberOfSignificantDigits, "=");
+}
+
 template<typename T>
-Evaluation<T> Equal::templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const {
-  return new Complex<T>(Complex<T>::Undefined());
+Evaluation<T> EqualNode::templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const {
+  return Complex<T>::Undefined();
+}
+
+Expression Equal::shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const {
+  Expression e = Expression::defaultShallowReduce(context, angleUnit);
+  if (e.isUndefinedOrAllocationFailure()) {
+    return e;
+  }
+  if (childAtIndex(0).isIdenticalTo(childAtIndex(1))) {
+    return Rational(1);
+  }
+  return *this;
 }
 
 }

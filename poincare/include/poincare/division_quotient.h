f0728b940d8d4a1af7e1ed1351039d63fbacad50
Léa Saviot <lea.saviot@numworks.com>
2018-08-27 14:31:56 +0200

    [poincare] DivisionQuotient and DivisionRemainder

--- poincare/include/poincare/division_quotient.h
+++ poincare/include/poincare/division_quotient.h
@@ -1,32 +1,52 @@
 #ifndef POINCARE_DIVISION_QUOTIENT_H
 #define POINCARE_DIVISION_QUOTIENT_H
 
+#include <poincare/approximation_helper.h>
+#include <poincare/expression.h>
 #include <poincare/layout_helper.h>
-#include <poincare/static_hierarchy.h>
-#include <poincare/evaluation.h>
-#include <poincare/char_layout_node.h>
 
 namespace Poincare {
 
-class DivisionQuotient : public StaticHierarchy<2> {
-  using StaticHierarchy<2>::StaticHierarchy;
+class DivisionQuotientNode : public ExpressionNode {
 public:
-  Type type() const override;
+  static DivisionQuotientNode * FailedAllocationStaticNode();
+  DivisionQuotientNode * failedAllocationStaticNode() override { return FailedAllocationStaticNode(); }
+
+  // TreeNode
+  size_t size() const override { return sizeof(DivisionQuotientNode); }
+  int numberOfChildren() const override { return 2; }
+#if POINCARE_TREE_LOG
+  virtual void logNodeName(std::ostream & stream) const override {
+    stream << "DivisionQuotient";
+  }
+#endif
+
+  // ExpressionNode
+  Type type() const override { return Type::DivisionQuotient; }
 private:
- /* Layout */
-  LayoutRef createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
-    return LayoutHelper::Prefix(this, floatDisplayMode, numberOfSignificantDigits, name());
-  }
-  int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override {
-    return SerializationHelper::Prefix(this, buffer, bufferSize, floatDisplayMode, numberOfSignificantDigits, name());
-  }
+  // Layout
+  LayoutReference createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
+  int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
   const char * name() const { return "quo"; }
-  /* Simplification */
-  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) override;
-  /* Evaluation */
+  // Simplification
+  Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit) const override;
+  // Evaluation
   Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<float>(context, angleUnit); }
   Evaluation<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<double>(context, angleUnit); }
   template<typename T> Evaluation<T> templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const;
+};
+
+class DivisionQuotient : public Expression {
+public:
+  DivisionQuotient() : Expression(TreePool::sharedPool()->createTreeNode<DivisionQuotientNode>()) {}
+  DivisionQuotient(const DivisionQuotientNode * n) : Expression(n) {}
+  DivisionQuotient(Expression child1, Expression child2) : DivisionQuotient() {
+    replaceChildAtIndexInPlace(0, child1);
+    replaceChildAtIndexInPlace(1, child2);
+  }
+
+  // Expression
+  Expression shallowReduce(Context& context, Preferences::AngleUnit angleUnit) const;
 };
 
 }

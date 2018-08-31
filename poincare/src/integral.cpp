4b490088c18906e49338889c14be148c624b4c79
Léa Saviot <lea.saviot@numworks.com>
2018-08-31 17:18:36 +0200

    [poincare] Update LeastCommonMultiple, Integral, Logarithm

--- poincare/src/integral.cpp
+++ poincare/src/integral.cpp
@@ -191,9 +191,11 @@ T IntegralNode::adaptiveQuadrature(T a, T b, T eps, int numberOfIterations, Cont
 
 
 Expression Integral::shallowReduce(Context & context, Preferences::AngleUnit angleUnit) {
-  Expression e = Expression::defaultShallowReduce(context, angleUnit);
-  if (e.isUndefinedOrAllocationFailure()) {
-    return e;
+  {
+    Expression e = Expression::defaultShallowReduce(context, angleUnit);
+    if (e.isUndefinedOrAllocationFailure()) {
+      return e;
+    }
   }
 #if MATRIX_EXACT_REDUCING
   if (childAtIndex(0).type() == ExpressionNode::Type::Matrix

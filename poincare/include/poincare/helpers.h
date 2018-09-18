1852a71f95a1bdfb1445b4ba27d45ffb00b60ec8
Léa Saviot <lea.saviot@numworks.com>
2018-09-18 15:24:12 +0200

    [poincare] Remove TreePool::insert method, use Helper::Rotate instead
    This removes the use of an enormous buffer on the stack and uses instead
    a linear algorithm with one temporary data.

---
+++ poincare/include/poincare/helpers.h
@@ -0,0 +1,18 @@
+#ifndef POINCARE_HELPERS_H
+#define POINCARE_HELPERS_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+namespace Poincare {
+
+namespace Helpers {
+
+size_t Gcd(size_t a, size_t b);
+bool Rotate(uint32_t * dst, uint32_t * src, size_t len);
+
+}
+
+}
+
+#endif

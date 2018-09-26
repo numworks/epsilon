3239afd381b5b2b50e85c6d916321a3cd4f45426
Ruben Dashyan <ruben.dashyan@numworks.com>
2018-09-26 17:30:24 +0200

    [poincare] Second version of homemade parser
    Using function stack instead of auxiliary stack.

--- poincare/src/parsing/tokenizer.h
+++ poincare/src/parsing/tokenizer.h
@@ -9,14 +9,14 @@ class Tokenizer {
 public:
   Tokenizer(const char * input) : m_text(input) {};
   Token popToken();
-  const char * text() const { return m_text; };
 private:
-  const char popChar() { return *++m_text; };
-  const char currentChar() const { return *m_text; };
+  const char popChar() { return *++m_text; }
+  const char currentChar() const { return *m_text; }
   bool canPopChar(char c);
   size_t popInteger();
   Token popNumber();
   Token popIdentifier();
+
   const char * m_text;
 };
 

e58395e8c8dfa68e7855e56189058b1a1e392a1a
Émilie Feral <emilie.feral@numworks.com>
2018-09-27 15:49:10 +0200

    [sequence] Get rid of Symbol::SpecialSymbols in Sequence

--- apps/sequence/cache_context.cpp
+++ apps/sequence/cache_context.cpp
@@ -1,4 +1,5 @@
 #include "cache_context.h"
+#include "sequence_store.h"
 #include <cmath>
 
 using namespace Poincare;
@@ -15,8 +16,9 @@ CacheContext<T>::CacheContext(Context * parentContext) :
 
 template<typename T>
 const Expression CacheContext<T>::expressionForSymbol(const Symbol & symbol) {
-  if (symbol.name() == Symbol::SpecialSymbols::un || symbol.name() == Symbol::SpecialSymbols::un1 ||
-      symbol.name() == Symbol::SpecialSymbols::vn || symbol.name() == Symbol::SpecialSymbols::vn1) {
+  // [u|v](n(+1)?)
+  if ((symbol.name()[0] ==  SequenceStore::k_sequenceNames[0][0] || symbol.name()[0] ==  SequenceStore::k_sequenceNames[1][0])
+   && (strcmp(symbol.name()+1, "(n)") == 0 || strcmp(symbol.name()+1, "(n+1)") == 0)) {
     return Float<T>(m_values[nameIndexForSymbol(symbol)][rankIndexForSymbol(symbol)]);
   }
   return VariableContext<T>::expressionForSymbol(symbol);
@@ -29,34 +31,22 @@ void CacheContext<T>::setValueForSymbol(T value, const Poincare::Symbol & symbol
 
 template<typename T>
 int CacheContext<T>::nameIndexForSymbol(const Poincare::Symbol & symbol) {
-  switch (symbol.name()) {
-    case Symbol::SpecialSymbols::un:
-      return 0;
-    case Symbol::SpecialSymbols::un1:
-      return 0;
-    case Symbol::SpecialSymbols::vn:
-      return 1;
-    case Symbol::SpecialSymbols::vn1:
-      return 1;
-    default:
-      return 0;
+  assert(strlen(symbol.name()) == 4 || strlen(symbol.name()) == 6); //  [u|v](n(+1)?)
+  if (symbol.name()[0] == SequenceStore::k_sequenceNames[0][0]) { // u
+    return 0;
   }
+  // v
+  return 1;
 }
 
 template<typename T>
 int CacheContext<T>::rankIndexForSymbol(const Poincare::Symbol & symbol) {
-  switch (symbol.name()) {
-    case Symbol::SpecialSymbols::un:
-      return 0;
-    case Symbol::SpecialSymbols::un1:
-      return 1;
-    case Symbol::SpecialSymbols::vn:
-      return 0;
-    case Symbol::SpecialSymbols::vn1:
-      return 1;
-    default:
-      return 0;
+  assert(strlen(symbol.name()) == 4 || strlen(symbol.name()) == 6); // u(n) or u(n+1)
+  if (symbol.name()[3] == '(') { // .(n)
+    return 0;
   }
+  // .(n+1)
+  return 1;
 }
 
 template class CacheContext<float>;

11af64ac56d6169ea1c604d86e579772ffb9b6d7
Léa Saviot <lea.saviot@numworks.com>
2018-09-27 11:19:23 +0200

    [poincare] GlobalContext stores expressions and functions

--- poincare/src/global_context.cpp
+++ poincare/src/global_context.cpp
@@ -7,68 +7,58 @@
 
 namespace Poincare {
 
-GlobalContext::FileName GlobalContext::fileNameForSymbol(const Symbol & s) const {
-  FileName fn;
-  fn.nameWithExtension[0] = s.name();
-  constexpr char extension[] = ".exp";
-  strlcpy(fn.nameWithExtension+1, extension, k_extensionSize);
-  return fn;
-}
+constexpr char GlobalContext::expExtension[];
+constexpr char GlobalContext::funcExtension[];
+//constexpr char GlobalContext::seqExtension[];
 
 const Expression GlobalContext::expressionForSymbol(const Symbol & symbol) {
   // Constant symbols
-  if (symbol.name() == Ion::Charset::SmallPi) {
+  if (symbol.name()[0] == Ion::Charset::SmallPi && symbol.name()[1] == 0) {
     return Float<double>(M_PI);
   }
-  if (symbol.name() == Ion::Charset::Exponential) {
+  if (symbol.name()[0] == Ion::Charset::Exponential && symbol.name()[1] == 0) {
     return Float<double>(M_E);
   }
   // Look up the file system for symbol
-  FileName symbolFileName = fileNameForSymbol(symbol);
-  Ion::Storage::Record record = Ion::Storage::sharedStorage()->recordNamed(symbolFileName.nameWithExtension);
-  if (record.isNull())  {
-    // Default A-Z value: 0
-    if (Symbol::isScalarSymbol(symbol.name())) {
-      return Decimal(Integer(0), 0);
-    }
-    // No other default value
-    return Expression(); // unitialized Expression
-  }
-  // Build Expression in the Tree Pool
-  return Expression(static_cast<ExpressionNode *>(TreePool::sharedPool()->copyTreeFromAddress(record.value().buffer, record.value().size)));
+  return ExpressionForRecord(RecordWithName(symbol.name()));
 }
 
 void GlobalContext::setExpressionForSymbolName(const Expression & expression, const Symbol & symbol, Context & context) {
-  // Initiate Record features
-  FileName symbolFileName = fileNameForSymbol(symbol);
+  /* If the new expression contains the symbol, replace it because it will be
+   * destroyed afterwards (to be able to do A+2->A) */
+  Ion::Storage::Record record = RecordWithName(symbol.name());
+  Expression e = ExpressionForRecord(record);
+  Expression finalExpression = expression.clone().replaceSymbolWithExpression(symbol.name(), e);
+  // Delete any record with same name (as it is going to be overriden)
+  record.destroy();
 
-  // evaluate before deleting anything (to be able to evaluate A+2->A)
-  Preferences * preferences = Preferences::sharedPreferences();
-  Expression evaluation = expression.isUninitialized() ? Expression() : expression.approximate<double>(context, preferences->angleUnit(), preferences->complexFormat());
-  // Delete any record with same name (as it is going to be override)
-  Ion::Storage::sharedStorage()->recordNamed(symbolFileName.nameWithExtension).destroy();
+  Ion::Storage::Record::ErrorStatus err = Ion::Storage::sharedStorage()->createRecordWithExtension(symbol.name(), ExtensionForExpression(finalExpression), finalExpression.addressInPool(), finalExpression.size());
+  if (err != Ion::Storage::Record::ErrorStatus::None) {
+    assert(err == Ion::Storage::Record::ErrorStatus::NotEnoughSpaceAvailable);
+    // TODO: return false to set flag that the file system is full?
+  }
+}
 
- if (Symbol::isMatrixSymbol(symbol.name())) {
-   // Matrix symbol: force evaluation to be a Matrix
-    if (!evaluation.isUninitialized() && evaluation.type() != ExpressionNode::Type::Matrix) {
-      evaluation = Matrix(evaluation);
-    }
-  } else if (Symbol::isScalarSymbol(symbol.name())) {
-    // Scalar symbol: force evaluation not to be a Matrix
-    if (!evaluation.isUninitialized() && evaluation.type() == ExpressionNode::Type::Matrix) {
-      evaluation = Undefined();
-    }
-  } else {
-    // Other symbols than A-Z or M0-M9 are not stored yet TODO
-    evaluation = Expression();
+Ion::Storage::Record GlobalContext::RecordWithName(const char * name) {
+  const char * extensions[2] = {expExtension, funcExtension/*, seqExtension*/};
+  return Ion::Storage::sharedStorage()->recordBaseNamedWithExtensions(name, extensions, 2);
+}
+
+const char * GlobalContext::ExtensionForExpression(const Expression & exp) {
+  // TODO Once we have matrix exact reducing, add a .mat extension and check here if the root is a matrix
+  // TODO .seq
+  if (exp.type() == ExpressionNode::Type::Function) {
+    return funcExtension;
   }
-  if (!evaluation.isUninitialized()) {
-    Ion::Storage::Record::ErrorStatus err = Ion::Storage::sharedStorage()->createRecord(symbolFileName.nameWithExtension, evaluation.addressInPool(), evaluation.size());
-    if (err != Ion::Storage::Record::ErrorStatus::None) {
-      assert(err == Ion::Storage::Record::ErrorStatus::NotEnoughSpaceAvailable);
-      // TODO: return false to set flag that the file system is full?
-    }
+  return expExtension;
+}
+
+const Expression GlobalContext::ExpressionForRecord(const Ion::Storage::Record & record) {
+  if (record.isNull()) {
+    return Undefined();
   }
+  // Build the Expression in the Tree Pool
+  return Expression(static_cast<ExpressionNode *>(TreePool::sharedPool()->copyTreeFromAddress(record.value().buffer, record.value().size)));
 }
 
 }

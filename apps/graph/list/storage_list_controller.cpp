ac1e79f9074599a8ea618e77fb6fda43233f65b3
Léa Saviot <lea.saviot@numworks.com>
2018-10-04 16:29:12 +0200

    [apps/graph] Display the function's whole name

--- apps/graph/list/storage_list_controller.cpp
+++ apps/graph/list/storage_list_controller.cpp
@@ -44,7 +44,21 @@ HighlightCell * StorageListController::expressionCells(int index) {
 void StorageListController::willDisplayTitleCellAtIndex(HighlightCell * cell, int j) {
   Shared::BufferFunctionTitleCell * myFunctionCell = (Shared::BufferFunctionTitleCell *)cell;
   StorageCartesianFunction function = m_functionStore->modelAtIndex(j);
-  char bufferName[5] = {*function.name(),'(', m_functionStore->symbol(),')', 0};
+  char bufferName[BufferTextView::k_maxNumberOfChar];
+  const char * functionName = function.name();
+  int index = 0;
+  const char ofXSring[4] = {'(',  m_functionStore->symbol(),')', 0};
+  size_t ofXSringSize = strlen(ofXSring);
+  while (*functionName != Ion::Storage::k_dotChar
+      && index < BufferTextView::k_maxNumberOfChar - ofXSringSize)
+  {
+    // We keep room to write the final "(x)" //TODO should we?
+    assert(functionName < function.name() + strlen(function.name()));
+    bufferName[index] = *functionName;
+    functionName++;
+    index++;
+  }
+  strlcpy(&bufferName[index], ofXSring, ofXSringSize);
   myFunctionCell->setText(bufferName);
   KDColor functionNameColor = function.isActive() ? function.color() : Palette::GreyDark;
   myFunctionCell->setColor(functionNameColor);

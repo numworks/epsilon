8ec98870b4a48ed0142278822230b608542044ac
Léa Saviot <lea.saviot@numworks.com>
2018-10-09 16:02:35 +0200

    [apps] Fix Globalcontext::setExpressionForSymbol

--- apps/shared/global_context.cpp
+++ apps/shared/global_context.cpp
@@ -34,11 +34,19 @@ void GlobalContext::setExpressionForSymbol(const Expression & expression, const 
    * destroyed afterwards (to be able to do A+2->A) */
   Ion::Storage::Record record = RecordWithName(symbol.name());
   Expression e = expressionForSymbolAndRecord(symbol, record);
+  if (e.isUninitialized()) {
+    e = Undefined();
+  }
   Expression finalExpression = expression.clone().replaceSymbolWithExpression(symbol, e);
-  // Delete any record with same name (as it is going to be overriden)
-  record.destroy();
 
-  Ion::Storage::Record::ErrorStatus err = Ion::Storage::sharedStorage()->createRecordWithExtension(symbol.name(), ExtensionForExpression(symbol), finalExpression.addressInPool(), finalExpression.size());
+  Ion::Storage::Record::ErrorStatus err = Ion::Storage::Record::ErrorStatus::None;
+  if (symbol.type() == ExpressionNode::Type::Symbol) {
+    err = setExpressionForActualSymbol(finalExpression, symbol, record);
+  } else {
+    assert(symbol.type() == ExpressionNode::Type::Function);
+    err = setExpressionForFunction(finalExpression, symbol, record);
+  }
+
   if (err != Ion::Storage::Record::ErrorStatus::None) {
     assert(err == Ion::Storage::Record::ErrorStatus::NotEnoughSpaceAvailable);
     sStorageMemoryFull = true;
@@ -85,6 +93,38 @@ const Expression GlobalContext::expressionForFunction(const SymbolAbstract & sym
   assert(symbol.type() == ExpressionNode::Type::Function);
   StorageCartesianFunction f = StorageCartesianFunction(r);
   return f.expression();
+}
+
+Ion::Storage::Record::ErrorStatus GlobalContext::setExpressionForActualSymbol(const Expression & expression, const SymbolAbstract & symbol, Ion::Storage::Record previousRecord) {
+  // Delete any record with same name (as it is going to be overriden)
+  previousRecord.destroy();
+  return Ion::Storage::sharedStorage()->createRecordWithExtension(symbol.name(), expExtension, expression.addressInPool(), expression.size());
+}
+
+Ion::Storage::Record::ErrorStatus GlobalContext::setExpressionForFunction(const Expression & expressionToStore, const SymbolAbstract & symbol, Ion::Storage::Record previousRecord) {
+  size_t expressionToStoreSize = expressionToStore.isUninitialized() ? 0 : expressionToStore.size();
+  size_t newDataSize = sizeof(StorageCartesianFunction::CartesianFunctionRecordData) + expressionToStoreSize;
+  Ion::Storage::Record::ErrorStatus error = Ion::Storage::Record::ErrorStatus::None;
+  if (Ion::Storage::FullNameHasExtension(previousRecord.fullName(), funcExtension, strlen(funcExtension))) {
+    // The previous record was also a function: we want to keep its metadata
+    // Prepare the new data to store
+    Ion::Storage::Record::Data newData = previousRecord.value();
+    newData.size = newDataSize;
+    // Set the data
+    error = previousRecord.setValue(newData);
+  } else {
+    // The previous record was not a function. Destroy it and create the new record.
+    previousRecord.destroy();
+    // Prepare the new data to store
+    StorageCartesianFunction::CartesianFunctionRecordData newData;
+    // Create the record
+    error = Ion::Storage::sharedStorage()->createRecordWithExtension(symbol.name(), funcExtension, &newData, newDataSize);
+  }
+  if (error == Ion::Storage::Record::ErrorStatus::None && !expressionToStore.isUninitialized()) {
+    void * newDataExpressionAddress = StorageCartesianFunction(Ion::Storage::sharedStorage()->recordBaseNamedWithExtension(symbol.name(), funcExtension)).expressionAddress();
+    memcpy(newDataExpressionAddress, expressionToStore.addressInPool(), expressionToStore.size());
+  }
+  return error;
 }
 
 }

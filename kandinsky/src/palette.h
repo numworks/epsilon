29882768bddba4bb5828ba3c22930e3cd241d4d6
Romain Goyet <romain.goyet@numworks.com>
2018-10-09 09:48:08 +0200

    [kandinsky] Use LZ4 to compress fonts
    This is a work-in-progress

--- kandinsky/src/palette.h
+++ kandinsky/src/palette.h
@@ -2,6 +2,7 @@
 #define KANDINSKY_PALETTE_H
 
 #include <kandinsky/color.h>
+#include <assert.h>
 
 template <int S>
 class KDPalette {
@@ -9,11 +10,12 @@ public:
   static KDPalette Gradient(KDColor fromColor, KDColor toColor) {
     KDPalette p;
     for (int i=0; i<S; i++) {
-      p.m_colors[i] = KDColor::blend(fromColor, toColor, (0xFF*i)/S);
+      p.m_colors[i] = KDColor::blend(fromColor, toColor, (0xFF*i)/(S-1));
     }
     return p;
   }
   KDColor colorAtIndex(int i) const {
+    assert(i>=0 && i<S);
     return m_colors[i];
   }
 private:

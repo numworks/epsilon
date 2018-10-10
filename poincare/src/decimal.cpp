a6e50dd5f4464c5bb8952ae8c397e8ce3489899f
Léa Saviot <lea.saviot@numworks.com>
2018-10-10 10:14:41 +0200

    [poincare] Fix Decimal::templatedApproximate
    Before, we used an unsigned mantissa and needed to set the sign
    afterwards, but with the signed mantissa we must not do this.

--- poincare/src/decimal.cpp
+++ poincare/src/decimal.cpp
@@ -205,8 +205,7 @@ template<typename T> T DecimalNode::templatedApproximate() const {
   Integer m = signedMantissa();
   T f = m.approximate<T>();
   int numberOfDigits = Integer::NumberOfBase10DigitsWithoutSign(m);
-  T result = f*std::pow((T)10.0, (T)(m_exponent-numberOfDigits+1));
-  return m_negative ? -result : result;
+  return f*std::pow((T)10.0, (T)(m_exponent-numberOfDigits+1));
 }
 
 int Decimal::Exponent(const char * integralPart, int integralPartLength, const char * fractionalPart, int fractionalPartLength, const char * exponent, int exponentLength) {

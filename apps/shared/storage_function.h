25e58f821e6e0b4fbd64f6db5671ccfd280bd684
Émilie Feral <emilie.feral@numworks.com>
2018-10-10 09:42:59 +0200

    [shared] Fix StorageFunction

--- apps/shared/storage_function.h
+++ apps/shared/storage_function.h
@@ -10,7 +10,6 @@ namespace Shared {
 class StorageFunction : public StorageExpressionModel {
 public:
   // Constructors
-  StorageFunction(const char * name = nullptr) : StorageExpressionModel(name) {}
   StorageFunction(Ion::Storage::Record record) : StorageExpressionModel(record){}
 
   // Checksum
@@ -43,8 +42,6 @@ protected:
     bool m_active;
   };
 private:
-  constexpr static size_t k_dataLengthInBytes = (TextField::maxBufferSize()+2)*sizeof(char)+2;
-  static_assert((k_dataLengthInBytes & 0x3) == 0, "The function data size is not a multiple of 4 bytes (cannot compute crc)"); // Assert that dataLengthInBytes is a multiple of 4
   template<typename T> T templatedApproximateAtAbscissa(T x, Poincare::Context * context) const;
   FunctionRecordData * recordData() const;
   virtual const char * symbol() const = 0;

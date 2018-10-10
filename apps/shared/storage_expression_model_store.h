94f95b434a8e26a49da68ff46cb7a3c1efac7aa3
Émilie Feral <emilie.feral@numworks.com>
2018-10-10 16:21:00 +0200

    [shared] New StorageExpressionModelStore with memoization of 5 models

--- apps/shared/storage_expression_model_store.h
+++ apps/shared/storage_expression_model_store.h
@@ -12,23 +12,30 @@ namespace Shared {
 class StorageExpressionModelStore {
   // TODO find better name (once we remove ExpressionModelStore?)
 public:
+  StorageExpressionModelStore();
+
   // Getters
-  virtual const char * modelExtension() const = 0;
   int numberOfModels() const;
   int numberOfDefinedModels() const;
-  StorageExpressionModel modelAtIndex(int i) const;
-  StorageExpressionModel definedModelAtIndex(int i) const;
+  StorageExpressionModel * modelAtIndex(int i) const;
+  StorageExpressionModel * definedModelAtIndex(int i) const;
 
   // Add and Remove
-  virtual Ion::Storage::Record::ErrorStatus addEmptyModel() = 0;
-  void removeModel(StorageExpressionModel * f) {
-    assert(f != nullptr);
-    f->destroy();
-  }
   virtual void removeAll();
+  void removeModel(StorageExpressionModel * f);
 
   // Other
   void tidy();
+private:
+  virtual const char * modelExtension() const = 0;
+  // Memoization of k_maxNumberOfMemoizedModels consecutive models
+  constexpr static int k_maxNumberOfMemoizedModels = 5;
+  mutable int m_firstMemoizedModelIndex;
+  mutable uint32_t m_memoizedModelChecksum[k_maxNumberOfMemoizedModels];
+  void setMemoizedModelAtIndex(int cacheIndex, Ion::Storage::Record) const;
+  virtual void privateSetMemoizedModelAtIndex(int cacheIndex, Ion::Storage::Record record) const = 0;
+  virtual void moveMemoizedModel(int previousIndex, int nextIndex) const = 0;
+  virtual StorageExpressionModel * memoizedModelAtIndex(int cacheIndex) const = 0;
 };
 
 }

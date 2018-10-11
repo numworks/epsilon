e290c928e0f75a8af429e9ff99240646c0d9eb10
Émilie Feral <emilie.feral@numworks.com>
2018-10-11 17:15:59 +0200

    [shared] StorageFunction: add a method to write the name of the function with an argument (f(x))

--- apps/graph/list/storage_list_controller.cpp
+++ apps/graph/list/storage_list_controller.cpp
@@ -45,20 +45,7 @@ void StorageListController::willDisplayTitleCellAtIndex(HighlightCell * cell, in
   Shared::BufferFunctionTitleCell * myFunctionCell = (Shared::BufferFunctionTitleCell *)cell;
   StorageFunction * function = m_functionStore->modelAtIndex(j);
   char bufferName[BufferTextView::k_maxNumberOfChar];
-  const char * functionName = function->fullName();
-  int index = 0;
-  const char ofXSring[4] = {'(',  m_functionStore->symbol(),')', 0};
-  size_t ofXSringSize = strlen(ofXSring);
-  while (*functionName != Ion::Storage::k_dotChar
-      && index < BufferTextView::k_maxNumberOfChar - ofXSringSize)
-  {
-    // We keep room to write the final "(x)" //TODO should we?
-    assert(functionName < function->fullName() + strlen(function->fullName()));
-    bufferName[index] = *functionName;
-    functionName++;
-    index++;
-  }
-  strlcpy(&bufferName[index], ofXSring, ofXSringSize+1);
+  function->nameWithArgument(bufferName, BufferTextView::k_maxNumberOfChar, m_functionStore->symbol());
   myFunctionCell->setText(bufferName);
   KDColor functionNameColor = function->isActive() ? function->color() : Palette::GreyDark;
   myFunctionCell->setColor(functionNameColor);

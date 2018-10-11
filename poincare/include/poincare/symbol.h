be68046cb493cf7b022c927e626b1e176eb3d0f6
Léa Saviot <lea.saviot@numworks.com>
2018-10-11 17:45:18 +0200

    [poincare] Remove old function attributes

--- poincare/include/poincare/symbol.h
+++ poincare/include/poincare/symbol.h
@@ -17,9 +17,6 @@ public:
   virtual void logNodeName(std::ostream & stream) const override {
     stream << "Symbol";
   }
-  virtual void logAttributes(std::ostream & stream) const override {
-    stream << " name=\"" << m_name << "\"";
-  }
 #endif
 
   // Expression Properties

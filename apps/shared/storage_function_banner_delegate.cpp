03f5ec3ca1b3fc6477fd6ae65a98a7e5f7cf2eee
Léa Saviot <lea.saviot@numworks.com>
2018-10-12 14:29:29 +0200

    [apps] Fix strlcpy buffer sizes

--- apps/shared/storage_function_banner_delegate.cpp
+++ apps/shared/storage_function_banner_delegate.cpp
@@ -14,11 +14,11 @@ void StorageFunctionBannerDelegate::reloadBannerViewForCursorOnFunction(CurveVie
   const char * legend = "0=";
   int legendLength = strlen(legend);
   int numberOfChar = 0;
-  strlcpy(buffer, legend, legendLength+1);
+  strlcpy(buffer, legend, bufferSize);
   numberOfChar += legendLength;
   buffer[0] = symbol;
   numberOfChar += PoincareHelpers::ConvertFloatToText<double>(cursor->x(), buffer+numberOfChar, PrintFloat::bufferSizeForFloatsWithPrecision(Constant::MediumNumberOfSignificantDigits), Constant::MediumNumberOfSignificantDigits);
-  strlcpy(buffer+numberOfChar, space, spaceLength+1);
+  strlcpy(buffer+numberOfChar, space, bufferSize - numberOfChar);
   buffer[k_maxDigitLegendLength+2] = 0;
   bannerView()->setLegendAtIndex(buffer, 0);
 

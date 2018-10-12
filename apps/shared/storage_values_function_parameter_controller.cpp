1ab2896fb40f4a8940acc38226566b7aaa623c4e
Léa Saviot <lea.saviot@numworks.com>
2018-10-12 12:10:59 +0200

    [apps/graph] Display function name in hide/show derivative controllers

--- apps/shared/storage_values_function_parameter_controller.cpp
+++ apps/shared/storage_values_function_parameter_controller.cpp
@@ -4,14 +4,7 @@
 namespace Shared {
 
 const char * StorageValuesFunctionParameterController::title() {
-  strlcpy(m_pageTitle, I18n::translate(I18n::Message::FunctionColumn), k_maxNumberOfCharsInTitle);
-  for (int currentChar = 0; currentChar < k_maxNumberOfCharsInTitle-1; currentChar++) {
-    if (m_pageTitle[currentChar] == '(') {
-      m_pageTitle[currentChar-1] = *m_function->fullName();
-      m_pageTitle[currentChar+1] = m_symbol;
-      break;
-    }
-  }
+  m_function->nameWithArgument(m_pageTitle, k_maxNumberOfCharsInTitle, m_symbol);
   return m_pageTitle;
 }
 

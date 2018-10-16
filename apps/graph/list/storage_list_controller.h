933838ff5e653906dba862eb4359ffdbedaa563f
Léa Saviot <lea.saviot@numworks.com>
2018-10-16 15:43:30 +0200

    [apps/graph] Rename functions

--- apps/graph/list/storage_list_controller.h
+++ apps/graph/list/storage_list_controller.h
@@ -5,24 +5,29 @@
 #include "list_parameter_controller.h"
 #include "text_field_function_title_cell.h"
 #include "../storage_cartesian_function_store.h"
-#include <apps/shared/storage_function_list_controller.h>
-#include <apps/shared/buffer_function_title_cell.h>
 #include <apps/shared/function_expression_cell.h>
+#include <apps/shared/storage_function_list_controller.h>
+#include <apps/shared/text_field_delegate.h>
 
 namespace Graph {
 
-class StorageListController : public Shared::StorageFunctionListController {
+class StorageListController : public Shared::StorageFunctionListController, public Shared::TextFieldDelegate {
 public:
   StorageListController(Responder * parentResponder, StorageCartesianFunctionStore * functionStore, ButtonRowController * header, ButtonRowController * footer);
   const char * title() override;
+  void renameSelectedFunction();
+  bool textFieldDidFinishEditing(TextField * textField, const char * text, Ion::Events::Event event) override;
 private:
+  constexpr static int k_maxNumberOfDisplayableRows = 5;
   Shared::StorageListParameterController * parameterController() override;
   int maxNumberOfDisplayableRows() override;
   Shared::FunctionTitleCell * titleCells(int index) override;
   HighlightCell * expressionCells(int index) override;
   void willDisplayTitleCellAtIndex(HighlightCell * cell, int j) override;
   void willDisplayExpressionCellAtIndex(HighlightCell * cell, int j) override;
-  constexpr static int k_maxNumberOfDisplayableRows = 5;
+  Shared::TextFieldDelegateApp * textFieldDelegateApp() override {
+    return static_cast<Shared::TextFieldDelegateApp *>(app());
+  }
   TextFieldFunctionTitleCell m_functionTitleCells[k_maxNumberOfDisplayableRows];
   Shared::FunctionExpressionCell m_expressionCells[k_maxNumberOfDisplayableRows];
   ListParameterController m_parameterController;

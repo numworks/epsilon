3dbcbd69b5f9ebadced2e5e66361783a7e030ae3
Ruben Dashyan <ruben.dashyan@numworks.com>
2018-10-26 16:36:51 +0200

    [poincare] Add parsing tests

--- poincare/test/simplify_mix.cpp
+++ poincare/test/simplify_mix.cpp
@@ -53,7 +53,7 @@ QUIZ_CASE(poincare_simplify_mix) {
   assert_parsed_expression_simplify_to("R(-I)", "R(-I)");
   assert_parsed_expression_simplify_to("A*cos(9)IIln(2)", "-cos(9)*ln(2)*A");
   assert_parsed_expression_simplify_to("(R(2)+R(2)*I)/2(R(2)+R(2)*I)/2(R(2)+R(2)*I)/2", "(R(2)-R(2)*I)/32");
-  assert_parsed_expression_simplify_to("root(5^(-I)3^9,I)", "(5^(-19683*I))^(-I)");
+  assert_parsed_expression_simplify_to("root(5^((-I)3^9),I)", "(5^(-19683*I))^(-I)");
   assert_parsed_expression_simplify_to("I^I", "I^I");
 
   //assert_parsed_expression_simplify_to("log(cos(9)^ln(6), cos(9))", "ln(2)+ln(3)"); // TODO: for this to work, we must know the sign of cos(9)

fcf7cc33cfd63f414bbeb674156bfef151094652
Jean-Baptiste Boric <jblbeurope@gmail.com>
2018-10-30 18:10:25 +0100

    [python] Implement turtle.position()

--- python/port/modturtle.h
+++ python/port/modturtle.h
@@ -4,9 +4,11 @@ mp_obj_t turtle_forward(mp_obj_t px);
 mp_obj_t turtle_backward(mp_obj_t px);
 mp_obj_t turtle_right(mp_obj_t deg);
 mp_obj_t turtle_left(mp_obj_t deg);
-mp_obj_t turtle_goto(mp_obj_t x, mp_obj_t y);
+mp_obj_t turtle_goto(size_t n_args, const mp_obj_t *args);
 mp_obj_t turtle_setheading(mp_obj_t deg);
 mp_obj_t turtle_speed(mp_obj_t speed);
+
+mp_obj_t turtle_position();
 
 mp_obj_t turtle_pendown();
 mp_obj_t turtle_penup();
@@ -20,4 +22,5 @@ mp_obj_t turtle_hideturtle();
 mp_obj_t turtle___init__();
 
 void turtle_initpen(int size);
+void turtle_moveto(float x, float y);
 void turtle_deinit();

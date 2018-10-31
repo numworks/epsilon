de06f330e5e7ac8adccd718990408e4c36a7e990
Romain Goyet <romain.goyet@numworks.com>
2018-10-31 10:28:57 +0100

    [kandinsky] Assume UTF-8 encoding for strings

---
+++ kandinsky/src/unicode/utf8decoder.cpp
@@ -0,0 +1,26 @@
+#include <kandinsky/unicode/utf8decoder.h>
+#include <assert.h>
+
+static inline int leading_ones(uint8_t value) {
+  for (int i=0; i<8; i++) {
+    if (!(value & 0x80)) {
+      return i;
+    }
+    value = value << 1;
+  }
+  assert(false);
+}
+
+static inline uint8_t last_k_bits(uint8_t value, uint8_t bits) {
+  return (value & ((1<<bits)-1));
+}
+
+Codepoint UTF8Decoder::nextCodepoint() {
+  int leadingOnes = leading_ones(*m_string);
+  uint32_t result = last_k_bits(*m_string++, 8-leadingOnes-1);
+  for (int i=0; i<(leadingOnes-1); i++) {
+    result <<= 6;
+    result += (*m_string++ & 0x3F);
+  }
+  return Codepoint(result);
+}

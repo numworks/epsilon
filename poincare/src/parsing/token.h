3d48f8ac9380a84944bcf5fd480c091cb9bb58a8
Ruben Dashyan <ruben.dashyan@numworks.com>
2018-11-05 17:58:24 +0100

    [poincare] Complete and clean Parser

--- poincare/src/parsing/token.h
+++ poincare/src/parsing/token.h
@@ -3,12 +3,19 @@
 
 #include <poincare/expression.h>
 
+/* The Token class is meant to be the output type of the Tokenizer.
+ * While processing a text input, the Tokenizer indeed produces (pops)
+ * the successive Tokens, that are then consumed by the Parser.
+ * Each Token has a Type and may hold other relevant data intended for the Parser:
+ *   - an Expression, specifically for a Number Token,
+ *   - a string (m_text, m_length), specifically for an Identifier Token. */
+
 namespace Poincare {
 
 class Token {
 public:
   enum Type {
-    // Ordered from lower to higher precedence to make parser's job easier
+    // Ordered from lower to higher precedence to make Parser's job easier
     EndOfStream = 0, // Must be the first
     Equal,
     Store,
@@ -20,18 +27,20 @@ public:
     Minus,
     Times,
     Slash,
-    /* The ImplicitTimes Token allows to parse input text where the Times Token is omitted.
-     * Eventhough the Tokenizer will never pop ImplicitTimes Tokens,
-     * the ImplicitTimes Token Type is defined here with the desired precedence,
-     * allowing the Parser to insert such Tokens where needed. */
     ImplicitTimes,
+      /* The ImplicitTimes Token allows to parse text where the Times Token is omitted.
+       * Eventhough the Tokenizer will never pop ImplicitTimes Tokens,
+       * the ImplicitTimes Token Type is defined here with the desired precedence,
+       * in order to allow the Parser to insert such Tokens where needed. */
     Caret,
     Bang,
     LeftBracket,
     LeftParenthesis,
     LeftBrace,
+    Empty,
+    Constant,
     Number,
-    Symbol,
+    Identifier,
     Undefined
   };
 

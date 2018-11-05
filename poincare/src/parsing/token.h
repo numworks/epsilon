87d84e6bbddbbee7b2c4e2964e060216800bfe95
Ruben Dashyan <ruben.dashyan@numworks.com>
2018-11-05 17:40:02 +0100

    [poincare] Add a string member to Parser's Token class
    in the form of a pair m_text, m_length. Note that m_text
    is not null-terminated. Moreover, the method compareTo is implemented
    to compare m_text to any other (null-terminated) string.

--- poincare/src/parsing/token.h
+++ poincare/src/parsing/token.h
@@ -40,11 +40,28 @@ public:
   Type type() const { return m_type; }
   bool is(Type t) const { return m_type == t; }
   bool isEndOfStream() const { return is(Type::EndOfStream); }
+
   Expression expression() const { return m_expression; }
+  const char * text() const { return m_text; }
+  size_t length() const { return m_length; }
+
   void setExpression(Expression e) { m_expression = e; }
+  void setString(const char * text, size_t length) {
+    m_text = text;
+    m_length = length;
+  }
+  int compareTo(const char * name) const {
+    // Compare m_text to name, similarly to strcmp, assuming
+    //  - m_text is not null-terminated
+    //  - name is.
+    int diff = strncmp(m_text, name, m_length);
+    return (diff != 0) ? diff : strcmp("", name + m_length);
+  }
 private:
   Type m_type;
   Expression m_expression;
+  const char * m_text;
+  size_t m_length;
 };
 
 }

4d2c2b20d8bb895333b707d9c274020e6755ecee
Léa Saviot <lea.saviot@numworks.com>
2018-11-05 17:58:42 +0100

    [apps/code] Handle too many Script default names used
    When adding to many scripts without naming them, one can overflow the
    number of default generated script names, in which case we force the
    user to enter a name.

--- apps/code/script.h
+++ apps/code/script.h
@@ -9,16 +9,18 @@ namespace Code {
  * Script:                        | AutoImportationStatus | Content |*/
 
 class Script : public Ion::Storage::Record {
+private:
+  // Default script names are chosen between script1 and script99
+  static constexpr int k_maxNumberOfDefaultScriptNames = 99;
+  static constexpr int k_defaultScriptNameNumberMaxSize = 2; // Numbers from 1 to 99 have 2 digits max
 public:
   static constexpr size_t k_importationStatusSize = 1;
-  static constexpr int k_defaultScriptNameMaxSize = 6 + 2 + 1;
-  /* k_defaultScriptNameMaxSize is the length of a name between script1 and
-   * script99
-   * 6 = strlen("script")
-   * 2 = maxLength of integers between 1 and 99
+  static constexpr int k_defaultScriptNameMaxSize = 6 + k_defaultScriptNameNumberMaxSize + 1;
+  /* 6 = strlen("script")
+   * k_defaultScriptNameNumberMaxSize = maxLength of integers between 1 and 99
    * 1 = null-terminating char */
 
-  static void DefaultName(char buffer[], size_t bufferSize);
+  static bool DefaultName(char buffer[], size_t bufferSize);
   static bool nameCompliant(const char * name);
 
   Script(Ion::Storage::Record r) : Record(r) {}
--- apps/code/script_store.h
+++ apps/code/script_store.h
@@ -47,10 +47,10 @@ private:
   /* If the storage available space has a smaller size than
    * k_fullFreeSpaceSizeLimit, we consider the script store as full.
    * To be able to add a new empty record, the available space should at least
-   * stores a Script with default name "script99.py" (12 char), the importation
-   * status (1 char), the default content "from math import *\n" (20 char) and
-   * 10 char of free space. */
-  static constexpr int k_fullFreeSpaceSizeLimit = sizeof(Ion::Storage::record_size_t)+12+1+20+10;
+   * be able to store a Script with default name and its extension, the
+   * importation status (1 char), the default content "from math import *\n"
+   * (20 char) and 10 char of free space. */
+  static constexpr int k_fullFreeSpaceSizeLimit = sizeof(Ion::Storage::record_size_t)+Script::k_defaultScriptNameMaxSize+k_scriptExtensionLength+1+20+10;
   static constexpr size_t k_fileInput2ParseNodeStructKind = 1;
   static constexpr size_t k_functionDefinitionParseNodeStructKind = 3;
   static constexpr size_t k_expressionStatementParseNodeStructKind = 5;

0e5da52d3de7b186bc9b0e022d99e9d105310880
Léa Saviot <lea.saviot@numworks.com>
2018-11-07 10:02:01 +0100

    [apps] Remove useless code in variable_box_controller.h

--- apps/variable_box_controller.h
+++ apps/variable_box_controller.h
@@ -34,9 +34,6 @@ public:
   };
   void lockDeleteEvent(Page page);
 private:
-  // TODO: use the "(x)" defined in CartesianFunctionSomething
-  constexpr static int k_functionArgLength = 3;
-  constexpr static const char * k_functionArg = "(x)";
   constexpr static int k_maxNumberOfDisplayedRows = 6; //240/Metric::ToolboxRowHeight
   constexpr static int k_numberOfMenuRows = 2;
   constexpr static KDCoordinate k_leafMargin = 20;

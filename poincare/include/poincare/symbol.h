ada8f5f3c424bd051b5c1c163b9d73a7d276042f
Léa Saviot <lea.saviot@numworks.com>
2018-11-08 16:47:32 +0100

    [poincare] Replace symbols with their expressions iteratively
    ... not recursively.

--- poincare/include/poincare/symbol.h
+++ poincare/include/poincare/symbol.h
@@ -31,6 +31,7 @@ public:
 
   /* Simplification */
   Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit, bool replaceSymbols = true) override;
+  Expression replaceSymbols(Context & context) override;
 
   /* Approximation */
   Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override { return templatedApproximate<float>(context, angleUnit); }
@@ -75,6 +76,7 @@ public:
   Expression shallowReduce(Context & context, Preferences::AngleUnit angleUnit, bool replaceSymbols = true);
   Expression replaceSymbolWithExpression(const SymbolAbstract & symbol, const Expression & expression);
   int getPolynomialCoefficients(Context & context, const char * symbolName, Expression coefficients[]) const;
+  Expression replaceSymbols(Context & context);
 private:
   SymbolNode * node() const { return static_cast<SymbolNode *>(Expression::node()); }
 };

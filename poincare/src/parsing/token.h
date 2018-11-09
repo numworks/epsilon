9f1a85806b6f8b9888606e66005c65872e23e4be
Léa Saviot <lea.saviot@numworks.com>
2018-11-09 16:48:52 +0100

    [poincare] More elegant prevention of parsing "3=4>x" and "4<x=3"

--- poincare/src/parsing/token.h
+++ poincare/src/parsing/token.h
@@ -17,8 +17,13 @@ public:
   enum Type {
     // Ordered from lower to higher precedence to make Parser's job easier
     EndOfStream = 0, // Must be the first
-    Equal,
     Store,
+    Equal,
+      /* Equal should have a higher precedence than Store, because
+       * Tokenizer::parseEqual looks for a right hand side until it finds a
+       * token of lesser precedence than Equal, and this prevents expressions
+       * such as "3=4>a". Tokenizer::parseStore uses a special algorithm that
+       * prevents (3>4=a). */
     RightBracket,
     RightParenthesis,
     RightBrace,

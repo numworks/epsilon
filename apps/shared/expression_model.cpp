f3a1aaf1fabfbeadba22e7ef119bb85c08e8a6a5
Léa Saviot <lea.saviot@numworks.com>
2018-11-14 11:55:09 +0100

    [apps/poincare] Expressions are not reduced before storing in Storage

--- apps/shared/expression_model.cpp
+++ apps/shared/expression_model.cpp
@@ -21,7 +21,7 @@ const char * ExpressionModel::text() const {
 
 Poincare::Expression ExpressionModel::expression(Poincare::Context * context) const {
   if (m_expression.isUninitialized()) {
-    m_expression = PoincareHelpers::ParseAndSimplify(m_text, *context);
+    m_expression = Expression::parse(m_text);
   }
   return m_expression;
 }

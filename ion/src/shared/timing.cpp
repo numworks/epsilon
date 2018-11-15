fe2de01a4c1118c544da325b588caac8cc423b2d
Romain Goyet <romain.goyet@numworks.com>
2018-11-15 15:14:49 +0100

    [ion/timing] Use std::chrono::steady_clock

--- ion/src/shared/timing.cpp
+++ ion/src/shared/timing.cpp
@@ -1,9 +1,9 @@
 #include <ion.h>
 #include <chrono>
 
-static auto start = std::chrono::high_resolution_clock::now();
+static auto start = std::chrono::steady_clock::now();
 
 uint32_t Ion::millis() {
-  auto elapsed = std::chrono::high_resolution_clock::now() - start;
+  auto elapsed = std::chrono::steady_clock::now() - start;
   return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
 }

24b9490d6c44c3ec3a5bdd102dab740a16e323b6
Émilie Feral <emilie.feral@numworks.com>
2018-11-16 17:42:46 +0100

    [poincare] Factorize 'expand' and 'matches' methods between Symbol and Function

--- poincare/src/symbol_abstract.cpp
+++ poincare/src/symbol_abstract.cpp
@@ -1,4 +1,6 @@
 #include <poincare/symbol_abstract.h>
+#include <poincare/symbol.h>
+#include <poincare/expression.h>
 #include <poincare/helpers.h>
 #include <string.h>
 
@@ -29,12 +31,6 @@ int SymbolAbstractNode::simplificationOrderSameType(const ExpressionNode * e, bo
   return strcmp(name(), static_cast<const SymbolAbstractNode *>(e)->name());
 }
 
-/* TreePool uses adresses and sizes that are multiples of 4 in order to make
- * node moves faster.*/
-size_t SymbolAbstract::AlignedNodeSize(size_t nameLength, size_t nodeSize) {
-  return Helpers::AlignedSize(nodeSize+nameLength+1, 4);
-}
-
 size_t SymbolAbstract::TruncateExtension(char * dst, const char * src, size_t len) {
   const char * cur = src;
   const char * end = src+len-1;
@@ -43,6 +39,31 @@ size_t SymbolAbstract::TruncateExtension(char * dst, const char * src, size_t le
   }
   *dst = 0;
   return cur-src;
+}
+
+bool SymbolAbstract::matches(const SymbolAbstract & symbol, ExpressionTest test, Context & context) {
+  Expression e = SymbolAbstract::Expand(symbol, context, false);
+  return !e.isUninitialized() && test(e, context, false);
+}
+
+Expression SymbolAbstract::Expand(const SymbolAbstract & symbol, Context & context, bool clone) {
+  bool isFunction = symbol.type() == ExpressionNode::Type::Function;
+  /* Always clone the expression for Function because we are going to alter e
+   * by replacing all UnknownX in it. */
+  Expression e = context.expressionForSymbol(symbol, clone || isFunction);
+  /* Replace all the symbols iteratively. This prevents a memory failure when
+   * symbols are defined circularly. */
+  e = Expression::ExpressionWithoutSymbols(e, context);
+  if (!e.isUninitialized() && isFunction) {
+    e = e.replaceSymbolWithExpression(Symbol(Symbol::SpecialSymbols::UnknownX), symbol.childAtIndex(0));
+  }
+  return e;
+}
+
+/* TreePool uses adresses and sizes that are multiples of 4 in order to make
+ * node moves faster.*/
+size_t SymbolAbstract::AlignedNodeSize(size_t nameLength, size_t nodeSize) {
+  return Helpers::AlignedSize(nodeSize+nameLength+1, 4);
 }
 
 }

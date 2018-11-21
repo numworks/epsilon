bf6da61ca2b2bde0ccc46c350cb7fee86f1b82d5
Léa Saviot <lea.saviot@numworks.com>
2018-11-21 11:58:00 +0100

    [escher/text_field] Fix privateHandleEvent text reset

--- escher/include/escher/text_field_delegate.h
+++ escher/include/escher/text_field_delegate.h
@@ -7,9 +7,9 @@ class TextFieldDelegate {
 public:
   virtual bool textFieldShouldFinishEditing(TextField * textField, Ion::Events::Event event) = 0;
   virtual bool textFieldDidReceiveEvent(TextField * textField, Ion::Events::Event event) = 0;
-  virtual bool textFieldDidFinishEditing(TextField * textField, const char * text, Ion::Events::Event event) { return false; };
-  virtual bool textFieldDidAbortEditing(TextField * textField) {return false;};
-  virtual bool textFieldDidHandleEvent(TextField * textField, bool returnValue, bool textSizeDidChange) { return returnValue; };
+  virtual bool textFieldDidFinishEditing(TextField * textField, const char * text, Ion::Events::Event event) { return false; }
+  virtual bool textFieldDidAbortEditing(TextField * textField) { return false; }
+  virtual bool textFieldDidHandleEvent(TextField * textField, bool returnValue, bool textSizeDidChange) { return returnValue; }
 };
 
 #endif

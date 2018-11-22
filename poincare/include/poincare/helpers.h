2598f9ebb2ba06e128c9bd76c4f87e819a617c2e
Émilie Feral <emilie.feral@numworks.com>
2018-11-22 16:07:20 +0100

    [poincare] Helpers: add a method to padd a size to a given alignment

--- poincare/include/poincare/helpers.h
+++ poincare/include/poincare/helpers.h
@@ -8,6 +8,7 @@ namespace Poincare {
 
 namespace Helpers {
 
+size_t AlignedSize(size_t realSize, size_t alignment);
 size_t Gcd(size_t a, size_t b);
 bool Rotate(uint32_t * dst, uint32_t * src, size_t len);
 

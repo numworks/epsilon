168c12df96aff3e607fdcf8c5d47dad480ec4271
Émilie Feral <emilie.feral@numworks.com>
2018-11-22 09:57:40 +0100

    [apps] Reset (exam mode) delete all records at once

--- apps/apps_container.cpp
+++ apps/apps_container.cpp
@@ -95,8 +95,8 @@ App::Snapshot * AppsContainer::usbConnectedAppSnapshot() {
 }
 
 void AppsContainer::reset() {
-  // Empty stored variable
-  Ion::Storage::sharedStorage()->destroyRecordsWithExtension("exp");
+  // Empty storage (delete functions, variables, python scripts)
+  Ion::Storage::sharedStorage()->destroyAllRecords();
   // Empty clipboard
   Clipboard::sharedClipboard()->reset();
   for (int i = 0; i < numberOfApps(); i++) {

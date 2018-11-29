45e31afcff6400041c53eeebb2c45c38efb01b98
Léa Saviot <lea.saviot@numworks.com>
2018-11-29 15:41:34 +0100

    [apps] In FunctionTitleCell, draw the equal separately
    This way, we can better manage the margins around the equal

--- apps/graph/list/text_field_function_title_cell.h
+++ apps/graph/list/text_field_function_title_cell.h
@@ -38,8 +38,6 @@ public:
 
   // Responder
   void didBecomeFirstResponder() override;
-protected:
-  KDRect textFieldFrame() const;
 private:
   constexpr static float k_horizontalAlignment = 1.0f;
   constexpr static int k_textFieldBufferSize = Shared::StorageFunction::k_maxNameWithArgumentAndEqualSize;

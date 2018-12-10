cb187f7670e2c02ab47650bccc240b8bbd4a6252
Léa Saviot <lea.saviot@numworks.com>
2018-12-10 16:25:21 +0100

    [apps] Sandbox handles/absorbs only keyboard events (except Home, OnOff)
    The sandbox prevented USB enumeration by absorbing the event.

--- apps/code/sandbox_controller.cpp
+++ apps/code/sandbox_controller.cpp
@@ -18,6 +18,7 @@ void SandboxController::viewWillAppear() {
 }
 
 bool SandboxController::handleEvent(Ion::Events::Event event) {
+  // The sandbox handles or "absorbs" all keyboard events except Home and OnOff
   if (event == Ion::Events::Home || event == Ion::Events::OnOff) {
     stackViewController()->pop();
     return false;
@@ -25,7 +26,7 @@ bool SandboxController::handleEvent(Ion::Events::Event event) {
   if (event == Ion::Events::OK || event == Ion::Events::Back) {
     stackViewController()->pop();
   }
-  return true;
+  return event.isKeyboardEvent();
 }
 
 }

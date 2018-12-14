f10b286a3531071f79a448a59b70e897b216eafe
Émilie Feral <emilie.feral@numworks.com>
2018-12-14 17:56:59 +0100

    [poincare] Fix precision of approximation of arc trigonomtry when the result is in real numbers

--- poincare/src/arc_sine.cpp
+++ poincare/src/arc_sine.cpp
@@ -25,14 +25,24 @@ Expression ArcSineNode::shallowReduce(Context & context, Preferences::AngleUnit 
 
 template<typename T>
 Complex<T> ArcSineNode::computeOnComplex(const std::complex<T> c, Preferences::AngleUnit angleUnit) {
-  std::complex<T> result = std::asin(c);
-  /* asin has a branch cut on ]-inf, -1[U]1, +inf[: it is then multivalued on
-   * this cut. We followed the convention chosen by the lib c++ of llvm on
-   * ]-inf+0i, -1+0i[ (warning: asin takes the other side of the cut values on
-   * ]-inf-0i, -1-0i[) and choose the values on ]1+0i, +inf+0i[ to comply with
-   * asin(-x) = -asin(x) and tan(arcsin(x)) = x/sqrt(1-x^2). */
-  if (c.imag() == 0 && c.real() > 1) {
-    result.imag(-result.imag()); // other side of the cut
+  std::complex<T> result;
+  if (c.imag() == 0 && std::fabs(c.real()) <= 1.0) {
+    /* asin: [-1;1] -> R
+     * In these cases we rather use std::asin(double) because asin on complexes
+     * is not as precise as asin on double in std library. For instance,
+     * - asin(complex<double>(0.03,0.0) = complex(0.0300045,1.11022e-16)
+     * - asin(0.03) = 0.0300045 */
+    result = std::asin(c.real());
+  } else {
+    result = std::asin(c);
+    /* asin has a branch cut on ]-inf, -1[U]1, +inf[: it is then multivalued on
+     * this cut. We followed the convention chosen by the lib c++ of llvm on
+     * ]-inf+0i, -1+0i[ (warning: asin takes the other side of the cut values on
+     * ]-inf-0i, -1-0i[) and choose the values on ]1+0i, +inf+0i[ to comply with
+     * asin(-x) = -asin(x) and tan(arcsin(x)) = x/sqrt(1-x^2). */
+    if (c.imag() == 0 && c.real() > 1) {
+      result.imag(-result.imag()); // other side of the cut
+    }
   }
   result = Trigonometry::RoundToMeaningfulDigits(result, c);
   return Complex<T>(Trigonometry::ConvertRadianToAngleUnit(result, angleUnit));

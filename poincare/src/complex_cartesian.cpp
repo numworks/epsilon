c2311b66852bbda8e0db2887f75e1cdd8029fb06
Émilie Feral <emilie.feral@numworks.com>
2018-12-17 16:12:39 +0100

    [poincare] When simplifying an expression, also compute the approximation from the reduced expression to avoid loss of precision.
    If an expression is reduced to a ComplexCartesian(a, b), we compute the
    approximation from ComplexCartesian(a,b) instead of going through the
    potentially complicated expression resulting from beautifying
    (sqrt(a^2+b^2)*exp(i*(arctan(b/a) -Pi/2*sign(b)) for example)

--- poincare/src/complex_cartesian.cpp
+++ poincare/src/complex_cartesian.cpp
@@ -26,6 +26,19 @@ Expression ComplexCartesianNode::shallowBeautify(Context & context, Preferences:
   return ComplexCartesian(this).shallowBeautify(context, angleUnit);
 }
 
+template<typename T>
+Complex<T> ComplexCartesianNode::templatedApproximate(Context& context, Preferences::AngleUnit angleUnit) const {
+  Evaluation<T> realEvaluation = childAtIndex(0)->approximate(T(), context, angleUnit);
+  Evaluation<T> imagEvalution = childAtIndex(1)->approximate(T(), context, angleUnit);
+  assert(realEvaluation.type() == EvaluationNode<T>::Type::Complex);
+  assert(imagEvalution.type() == EvaluationNode<T>::Type::Complex);
+  std::complex<T> a = static_cast<Complex<T> &>(realEvaluation).stdComplex();
+  std::complex<T> b = static_cast<Complex<T> &>(imagEvalution).stdComplex();
+  assert(a.imag() == 0.0);
+  assert(b.imag() == 0.0);
+  return Complex<T>(a.real(), b.real());
+}
+
 Expression ComplexCartesian::shallowReduce(Context & context, Preferences::AngleUnit angleUnit) {
   if (imag().isRationalZero()) {
     Expression r = real();

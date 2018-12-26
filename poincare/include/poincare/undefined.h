b6ad82ea98f7a4dc4127a3ed62e185ffd0758fa2
Émilie Feral <emilie.feral@numworks.com>
2018-12-26 15:30:26 +0100

    [poincare] Unreal: approximating unreal set the sApproximationEncounterComplex to true

--- poincare/include/poincare/undefined.h
+++ poincare/include/poincare/undefined.h
@@ -35,7 +35,7 @@ public:
   // Layout
   Layout createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode = Preferences::PrintFloatMode::Decimal, int numberOfSignificantDigits = 0) const override;
-private:
+protected:
   template<typename T> Evaluation<T> templatedApproximate() const;
 };
 

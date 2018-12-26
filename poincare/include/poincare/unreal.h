4e345b705096a1f3e1c0aa9db051294eb6493fbc
Émilie Feral <emilie.feral@numworks.com>
2018-12-26 15:30:26 +0100

    [poincare] Unreal: approximating unreal set the sApproximationEncounterComplex to true

--- poincare/include/poincare/unreal.h
+++ poincare/include/poincare/unreal.h
@@ -19,10 +19,24 @@ public:
   // Properties
   Type type() const override { return Type::Unreal; }
 
+  // Approximation
+  Evaluation<float> approximate(SinglePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override {
+    Expression::SetEncounterComplex(true);
+    return templatedApproximate<float>();
+  }
+  Evaluation<double> approximate(DoublePrecision p, Context& context, Preferences::AngleUnit angleUnit) const override {
+    Expression::SetEncounterComplex(true);
+    return templatedApproximate<double>();
+  }
+
   // Layout
   Layout createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode = Preferences::PrintFloatMode::Decimal, int numberOfSignificantDigits = 0) const override;
 private:
+  template<typename T> Evaluation<T> templatedApproximate() const {
+    Expression::SetEncounterComplex(true);
+    return UndefinedNode::templatedApproximate<T>();
+  }
 };
 
 class Unreal final : public Number {

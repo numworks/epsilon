8943c65817f4424ec9a34dcdc3aece7579839235
Léa Saviot <lea.saviot@numworks.com>
2018-12-27 17:21:10 +0100

    [apps/code] Change the default scripts: squares instead of factorial

--- apps/code/script_template.h
+++ apps/code/script_template.h
@@ -7,12 +7,9 @@ class ScriptTemplate {
 public:
   constexpr ScriptTemplate(const char * name, const char * value) : m_name(name), m_value(value) {}
   static const ScriptTemplate * Empty();
-  static const ScriptTemplate * Factorial();
-  static const ScriptTemplate * Fibonacci();
+  static const ScriptTemplate * Squares();
   static const ScriptTemplate * Mandelbrot();
   static const ScriptTemplate * Polynomial();
-  static const ScriptTemplate * Spiral();
-  static const ScriptTemplate * Koch();
   const char * name() const { return m_name; }
   const char * content() const { return m_value+1; }
   const char * value() const { return m_value; }

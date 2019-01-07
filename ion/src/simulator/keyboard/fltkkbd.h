1ac0d0ec62840b14b2be331bde15b03e40358dbb
Léa Saviot <lea.saviot@numworks.com>
2019-01-07 17:45:34 +0100

    [simulator] Process letter keyboard events

--- ion/src/simulator/keyboard/fltkkbd.h
+++ ion/src/simulator/keyboard/fltkkbd.h
@@ -7,10 +7,12 @@
 
 class FltkKbd : public Fl_Group {
 public:
+  constexpr static int sNumberOfLettersInAlphabet = 26;
   FltkKbd(int x, int y, int w, int h);
   bool key_down(Ion::Keyboard::Key key);
 private:
   Fl_Button * m_buttons[Ion::Keyboard::NumberOfKeys];
+  Fl_Button * m_alphabetShortcuts[2*sNumberOfLettersInAlphabet];
 };
 
 #endif

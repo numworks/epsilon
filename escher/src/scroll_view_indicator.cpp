7114fbc8dc4f2768feea88529d74be3500ac4f33
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-01-08 15:19:48 +0100

    [escher] Simplify ScrollViewIndicator

--- escher/src/scroll_view_indicator.cpp
+++ escher/src/scroll_view_indicator.cpp
@@ -7,8 +7,8 @@ extern "C" {
 ScrollViewIndicator::ScrollViewIndicator(ScrollViewIndicator::Direction direction) :
   View(),
   m_direction(direction),
-  m_start(0),
-  m_end(0),
+  m_offset(0),
+  m_visibleLength(0),
   m_indicatorColor(Palette::GreyDark),
   m_backgroundColor(Palette::GreyMiddle),
   m_margin(14)
@@ -19,52 +19,37 @@ void ScrollViewIndicator::drawRect(KDContext * ctx, KDRect rect) const {
   KDRect frame = KDRectZero;
   if (m_direction == Direction::Horizontal) {
     frame = KDRect(m_margin, (m_frame.height() - k_indicatorThickness)/2,
-        m_frame.width() - 2*m_margin, k_indicatorThickness);
+        totalLength(), k_indicatorThickness);
   } else {
     assert(m_direction == Direction::Vertical);
     frame = KDRect((m_frame.width() - k_indicatorThickness)/2, m_margin,
-        k_indicatorThickness, m_frame.height() - 2*m_margin);
+        k_indicatorThickness, totalLength());
   }
   ctx->fillRect(frame, m_backgroundColor);
   KDRect indicatorFrame = KDRectZero;
   if (m_direction == Direction::Horizontal) {
-    KDCoordinate indicatorWidth = m_frame.width() - 2*m_margin;
-    indicatorFrame = KDRect(m_margin+m_start*indicatorWidth, (m_frame.height() - k_indicatorThickness)/2,
-        (m_end-m_start)*indicatorWidth, k_indicatorThickness);
+    indicatorFrame = KDRect(m_margin+m_offset*totalLength(), (m_frame.height() - k_indicatorThickness)/2,
+        m_visibleLength*totalLength(), k_indicatorThickness);
   } else {
     assert(m_direction == Direction::Vertical);
-    KDCoordinate indicatorHeight = m_frame.height() - 2*m_margin;
-    indicatorFrame = KDRect((m_frame.width() - k_indicatorThickness)/2, m_margin+m_start*indicatorHeight,
-        k_indicatorThickness, (m_end-m_start)*indicatorHeight);
+    indicatorFrame = KDRect((m_frame.width() - k_indicatorThickness)/2, m_margin+m_offset*totalLength(),
+        k_indicatorThickness, m_visibleLength*totalLength());
   }
   ctx->fillRect(indicatorFrame, m_indicatorColor);
 }
 
-float ScrollViewIndicator::start() const {
-  return m_start;
-}
-
-void ScrollViewIndicator::setStart(float start) {
-  if (m_start != start) {
-    m_start = start;
+void ScrollViewIndicator::update(KDCoordinate totalContentLength, KDCoordinate contentOffset, KDCoordinate visibleContentLength) {
+  float offset = contentOffset;
+  float visibleLength = visibleContentLength;
+  offset = offset / totalContentLength;
+  visibleLength = visibleLength / totalContentLength;
+  if (m_offset != offset || m_visibleLength != visibleLength) {
+    m_offset = offset;
+    m_visibleLength = visibleLength;
     markRectAsDirty(bounds());
   }
 }
 
-float ScrollViewIndicator::end() const {
-  return m_end;
-}
-
-void ScrollViewIndicator::setEnd(float end) {
-  if (m_end != end) {
-    m_end = end;
-    markRectAsDirty(bounds());
-  }
-}
-
-KDRect ScrollViewIndicator::frame() {
-  return m_frame;
-}
 #if ESCHER_VIEW_LOGGING
 const char * ScrollViewIndicator::className() const {
   return "ScrollViewIndicator";
@@ -72,7 +57,7 @@ const char * ScrollViewIndicator::className() const {
 
 void ScrollViewIndicator::logAttributes(std::ostream &os) const {
   View::logAttributes(os);
-  os << " start=\"" << m_start << "\"";
-  os << " end=\"" << m_end << "\"";
+  os << " offset=\"" << m_offset << "\"";
+  os << " visibleLength=\"" << m_visibleLength << "\"";
 }
 #endif

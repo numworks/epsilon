75474819f559bc5a2f7291c04c41c2ba70cd3b27
Léa Saviot <lea.saviot@numworks.com>
2019-01-08 10:53:34 +0100

    [simulator] Process more keyboard event, such as ! ? ;

--- ion/src/simulator/keyboard/fltkkbd.h
+++ ion/src/simulator/keyboard/fltkkbd.h
@@ -8,11 +8,16 @@
 class FltkKbd : public Fl_Group {
 public:
   constexpr static int sNumberOfLettersInAlphabet = 26;
+  constexpr static int sNumberOfOtherShortcuts = 22;
   FltkKbd(int x, int y, int w, int h);
   bool key_down(Ion::Keyboard::Key key);
 private:
+  void initButtons(int x, int y, int w, int h);
+  void initAlphabetShortcuts();
+  void initOtherShortcuts();
   Fl_Button * m_buttons[Ion::Keyboard::NumberOfKeys];
-  Fl_Button * m_alphabetShortcuts[2*sNumberOfLettersInAlphabet];
+  Fl_Button * m_alphabetShortcuts[2 * sNumberOfLettersInAlphabet];
+  Fl_Button * m_otherShortcuts[sNumberOfOtherShortcuts];
 };
 
 #endif

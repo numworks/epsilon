815c22f80150fab92e4194242bad80c3eafddab3
Romain Goyet <romain.goyet@numworks.com>
2019-01-09 10:40:13 +0100

    [ion/f730] Use the proper GPIOs for the keyboard

--- ion/src/f730/keyboard.h
+++ ion/src/f730/keyboard.h
@@ -11,27 +11,27 @@ namespace Device {
 
 /*  Pin | Role              | Mode
  * -----+-------------------+--------------------
+ *  PA0 | Keyboard row A    | Output, open drain
+ *  PA1 | Keyboard row B    | Output, open drain
+ *  PA2 | Keyboard row C    | Output, open drain
+ *  PA3 | Keyboard row D    | Output, open drain
+ *  PA4 | Keyboard row E    | Output, open drain
+ *  PA5 | Keyboard row F    | Output, open drain
+ *  PA6 | Keyboard row G    | Output, open drain
+ *  PA7 | Keyboard row H    | Output, open drain
+ *  PA8 | Keyboard row I    | Output, open drain
  *  PC0 | Keyboard column 1 | Input, pulled up
  *  PC1 | Keyboard column 2 | Input, pulled up
  *  PC2 | Keyboard column 3 | Input, pulled up
  *  PC3 | Keyboard column 4 | Input, pulled up
  *  PC4 | Keyboard column 5 | Input, pulled up
  *  PC5 | Keyboard column 6 | Input, pulled up
- *  PE0 | Keyboard row A    | Output, open drain
- *  PE1 | Keyboard row B    | Output, open drain
- *  PE2 | Keyboard row C    | Output, open drain
- *  PE3 | Keyboard row D    | Output, open drain
- *  PE4 | Keyboard row E    | Output, open drain
- *  PE5 | Keyboard row F    | Output, open drain
- *  PE6 | Keyboard row G    | Output, open drain
- *  PE7 | Keyboard row H    | Output, open drain
- *  PE8 | Keyboard row I    | Output, open drain
  */
 
 void init();
 void shutdown();
 
-constexpr GPIO RowGPIO = GPIOE;
+constexpr GPIO RowGPIO = GPIOA;
 constexpr uint8_t numberOfRows = 9;
 constexpr uint8_t RowPins[numberOfRows] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 

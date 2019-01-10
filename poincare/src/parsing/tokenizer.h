7cd8ddf57179efa625d2a91578be46513d204698
Léa Saviot <lea.saviot@numworks.com>
2019-01-10 14:40:45 +0100

    [poincare/tokenizer] Coding style

--- poincare/src/parsing/tokenizer.h
+++ poincare/src/parsing/tokenizer.h
@@ -1,12 +1,11 @@
 #ifndef POINCARE_PARSING_TOKENIZER_H
 #define POINCARE_PARSING_TOKENIZER_H
 
-/* In order to parse a text input into an Expression,
- * (an instance of) the Tokenizer reads the successive
- * characters of the input, pops the Tokens it recognizes,
- * which are then consumed by the Parser.
- * For each Token, the Tokenizer determines a Type and
- * may save other relevant data intended for the Parser. */
+/* In order to parse a text input into an Expression, (an instance of) the
+ * Tokenizer reads the successive characters of the input, pops the Tokens it
+ * recognizes, which are then consumed by the Parser. For each Token, the
+ * Tokenizer determines a Type and may save other relevant data intended for the
+ * Parser. */
 
 #include "token.h"
 
@@ -14,7 +13,7 @@ namespace Poincare {
 
 class Tokenizer {
 public:
-  Tokenizer(const char * text) : m_nextCharP(text) {};
+  Tokenizer(const char * text) : m_nextCharP(text) {}
   Token popToken();
 private:
   const char popChar();

3f56cb70415a13b09090f258fdff49449ecaa425
Léa Saviot <lea.saviot@numworks.com>
2019-01-11 14:02:50 +0100

    [poincare] CodePointLayout

---
+++ poincare/include/poincare/code_point_layout.h
@@ -0,0 +1,76 @@
+#ifndef POINCARE_CODEPOINT_LAYOUT_NODE_H
+#define POINCARE_CODEPOINT_LAYOUT_NODE_H
+
+#include <kandinsky/include/kandinsky/unicode/code_point.h>
+#include <poincare/layout_cursor.h>
+#include <poincare/layout.h>
+
+namespace Poincare {
+
+/* TODO: Make several code point classes depending on codepoint size?
+ * (m_codePoint sometimes fits in a char, no need for a whole CodePoint */
+
+class CodePointLayoutNode final : public LayoutNode {
+public:
+  static constexpr const KDFont * k_defaultFont = KDFont::LargeFont;
+  CodePointLayoutNode(CodePoint c = KDCodePointNull, const KDFont * font = k_defaultFont) :
+    LayoutNode(),
+    m_codePoint(c),
+    m_font(font)
+  {}
+
+  // CodePointLayout
+  CodePoint codePoint() const { return m_codePoint; }
+  const KDFont * font() const { return m_font; }
+
+  // LayoutNode
+  void moveCursorLeft(LayoutCursor * cursor, bool * shouldRecomputeLayout) override;
+  void moveCursorRight(LayoutCursor * cursor, bool * shouldRecomputeLayout) override;
+  int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const override;
+  bool isCodePoint() const override { return true; }
+  bool isCollapsable(int * numberOfOpenParenthesis, bool goingLeft) const override;
+  bool canBeOmittedMultiplicationLeftFactor() const override;
+  bool canBeOmittedMultiplicationRightFactor() const override;
+
+  // TreeNode
+  size_t size() const override { return sizeof(CodePointLayoutNode); }
+  int numberOfChildren() const override { return 0; }
+#if POINCARE_TREE_LOG
+  virtual void logNodeName(std::ostream & stream) const override {
+    stream << "CodePointLayout";
+  }
+  virtual void logAttributes(std::ostream & stream) const override {
+    stream << " CodePoint=\"" << m_codePoint << "\"";
+  }
+#endif
+
+protected:
+  // LayoutNode
+  KDSize computeSize() override;
+  KDCoordinate computeBaseline() override;
+  KDPoint positionOfChild(LayoutNode * child) override {
+    assert(false);
+    return KDPointZero;
+  }
+
+private:
+  void render(KDContext * ctx, KDPoint p, KDColor expressionColor, KDColor backgroundColor) override;
+  bool isMultiplicationCodePoint() const;
+  CodePoint m_codePoint;
+  const KDFont * m_font;
+};
+
+class CodePointLayout final : public Layout {
+public:
+  CodePointLayout(const CodePointLayoutNode * n) : Layout(n) {}
+  static CharLayout Builder(CodePoint c, const KDFont * font = KDFont::LargeFont);
+  const KDFont * font() const { return const_cast<CodePointLayout *>(this)->node()->font(); }
+  CodePoint codePoint() const { return const_cast<CodePointLayout *>(this)->node()->codePoint(); }
+private:
+  using Layout::node;
+  CodePointLayoutNode * node() { return static_cast<CodePointLayoutNode *>(Layout::node()); }
+};
+
+}
+
+#endif

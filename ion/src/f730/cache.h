584afe4ffa77483394f311eabc7218d1eac31483
Romain Goyet <romain.goyet@numworks.com>
2019-01-15 13:31:34 +0100

    [ion/f730] Enable L1 cache

--- ion/src/f730/cache.h
+++ ion/src/f730/cache.h
@@ -5,18 +5,32 @@ namespace Ion {
 namespace Device {
 namespace Cache {
 
-void dsb() {
+inline void dsb() {
   asm volatile("dsb 0xF":::"memory");
 }
 
-void isb() {
+inline void isb() {
   asm volatile("isb 0xF":::"memory");
 }
 
 void enableDCache() {
+  CM4.CSSELR()->set(0);
   dsb();
-  isb();
-  // FIXME: Invalidate D-Cache
+
+  //Associativity = 6
+
+  uint32_t sets = CM4.CCSIDR()->getNUMSETS();
+  do {
+    uint32_t ways = CM4.CCSIDR()->getASSOCIATIVITY();
+    do {
+      class CM4::DCISW dcisw;
+      dcisw.setSET(sets);
+      dcisw.setWAY(ways);
+      CM4.DCISW()->set(dcisw);
+    } while (ways-- != 0);
+  } while (sets-- != 0);
+
+  dsb();
   CM4.CCR()->setDC(true);
   dsb();
   isb();
@@ -26,6 +40,8 @@ void enableICache() {
   dsb();
   isb();
   CM4.ICIALLU()->set(0); // Invalidate I-Cache
+  dsb();
+  isb();
   CM4.CCR()->setIC(true);
   dsb();
   isb();

37e70113d6671a9f718bb14380a6a219c6aa5311
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-01-18 11:38:22 +0100

    [ion/f730/battery] Fix charge level (correcting ADC channel)

--- ion/src/f730/battery.h
+++ ion/src/f730/battery.h
@@ -10,7 +10,7 @@ namespace Device {
 /*  Pin | Role              | Mode                  | Function
  * -----+-------------------+-----------------------+----------
  *  PE3 | BAT_CHRG          | Input, pulled up      | Low = charging, high = full
- *  PB1 | VBAT_SNS          | Analog                | ADC1_1
+ *  PB1 | VBAT_SNS          | Analog                | ADC1_IN9
  */
 
 void init();
@@ -23,7 +23,7 @@ constexpr uint8_t ChargingPin = 3;
 
 constexpr GPIO ADCGPIO = GPIOB;
 constexpr uint8_t ADCPin = 1;
-constexpr uint8_t ADCChannel = 1;
+constexpr uint8_t ADCChannel = 9;
 
 constexpr float ADCReferenceVoltage = 2.8f;
 constexpr float ADCDividerBridgeRatio = 2.0f;

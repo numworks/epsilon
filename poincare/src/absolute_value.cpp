220b07b22bf7d492289dd86b194e124ad8e84cf3
Émilie Feral <emilie.feral@numworks.com>
2019-01-21 11:36:30 +0100

    [poincare] AbsoluteValue: fix shallowReduce to reduce abs(0) to 0

--- poincare/src/absolute_value.cpp
+++ poincare/src/absolute_value.cpp
@@ -51,12 +51,17 @@ Expression AbsoluteValue::shallowReduce(Context & context, Preferences::ComplexF
   Expression c = childAtIndex(0);
   if (c.isReal(context)) {
     float app = c.node()->approximate(float(), context, complexFormat, angleUnit).toScalar();
-    if (!std::isnan(app) && app >= Expression::Epsilon<float>()) {
-      // abs(a) = a with a > 0
+    if (!std::isnan(app) &&
+        ((c.isNumber() && app >= 0) || app >= Expression::Epsilon<float>())) {
+      /* abs(a) = a with a >= 0
+       * To check that a > 0, if a is a number we can use float comparison;
+       * in other cases, we are more conservative and rather check that
+       * a > epsilon ~ 1E-7 to avoid potential error due to float precision. */
       replaceWithInPlace(c);
       return c;
-    } else if (!std::isnan(app) && app <= -Expression::Epsilon<float>()) {
-      // abs(a) = -a with a < 0
+    } else if (!std::isnan(app) &&
+               ((c.isNumber() && app < 0.0f) || app <= -Expression::Epsilon<float>())) {
+      // abs(a) = -a with a < 0 (same comment as above to check that a < 0)
       Multiplication m(Rational(-1), c);
       replaceWithInPlace(m);
       return m.shallowReduce(context, complexFormat, angleUnit, target);

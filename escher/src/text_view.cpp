ac17ac5040f99f1ca68d5638b832fc0ffc3e848d
Léa Saviot <lea.saviot@numworks.com>
2019-01-23 14:53:23 +0100

    [escher] Clean TextView

--- escher/src/text_view.cpp
+++ escher/src/text_view.cpp
@@ -1,16 +1,5 @@
 #include <escher/text_view.h>
 
-TextView::TextView(const KDFont * font, float horizontalAlignment, float verticalAlignment,
-    KDColor textColor, KDColor backgroundColor) :
-  View(),
-  m_font(font),
-  m_horizontalAlignment(horizontalAlignment),
-  m_verticalAlignment(verticalAlignment),
-  m_textColor(textColor),
-  m_backgroundColor(backgroundColor)
-{
-}
-
 void TextView::setBackgroundColor(KDColor backgroundColor) {
   m_backgroundColor = backgroundColor;
   markRectAsDirty(bounds());
@@ -44,14 +33,9 @@ void TextView::drawRect(KDContext * ctx, KDRect rect) const {
     return;
   }
   KDSize textSize = m_font->stringSize(text());
-  KDPoint origin(m_horizontalAlignment*(m_frame.width() - textSize.width()),
-    m_verticalAlignment*(m_frame.height() - textSize.height()));
+  KDPoint origin(
+      m_horizontalAlignment * (m_frame.width() - textSize.width()),
+      m_verticalAlignment * (m_frame.height() - textSize.height()));
   ctx->fillRect(bounds(), m_backgroundColor);
   ctx->drawString(text(), origin, m_font, m_textColor, m_backgroundColor);
 }
-
-#if ESCHER_VIEW_LOGGING
-const char * TextView::className() const {
-  return "TextView";
-}
-#endif

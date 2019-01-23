0dc4ca3ecd8ba4e1c8c316632cef086fba2c2f4c
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-01-23 17:19:47 +0100

    [escher/scroll_view] Prepare ScrollViewIndicator for future ScrollViewArrow

--- escher/include/escher/scroll_view_indicator.h
+++ escher/include/escher/scroll_view_indicator.h
@@ -6,37 +6,37 @@
 class ScrollViewIndicator : public View {
 public:
   ScrollViewIndicator();
-
-  void setIndicatorColor(KDColor c) { m_indicatorColor = c; }
-  KDColor indicatorColor() const { return m_indicatorColor; }
-  void setBackgroundColor(KDColor c) { m_backgroundColor = c; }
-  KDColor backgroundColor() const { return m_backgroundColor; }
   void setMargin(KDCoordinate m) { m_margin = m; }
   KDCoordinate margin() const { return m_margin; }
-
-  void update(KDCoordinate totalContentLength, KDCoordinate contentOffset, KDCoordinate visibleContentLength);
-  bool visible() const { return 0 < m_offset || m_visibleLength < 1; }
 protected:
 #if ESCHER_VIEW_LOGGING
   virtual const char * className() const override;
   virtual void logAttributes(std::ostream &os) const override;
 #endif
-  constexpr static KDCoordinate k_indicatorThickness = 4;
-  float m_offset;
-  float m_visibleLength;
-  KDColor m_indicatorColor;
-  KDColor m_backgroundColor;
+  KDColor m_color;
   KDCoordinate m_margin;
 };
 
-class ScrollViewHorizontalIndicator : public ScrollViewIndicator {
+class ScrollViewBar : public ScrollViewIndicator {
+public:
+  ScrollViewBar();
+  void update(KDCoordinate totalContentLength, KDCoordinate contentOffset, KDCoordinate visibleContentLength);
+  bool visible() const { return 0 < m_offset || m_visibleLength < 1; }
+protected:
+  constexpr static KDCoordinate k_indicatorThickness = 4;
+  float m_offset;
+  float m_visibleLength;
+  KDColor m_trackColor;
+};
+
+class ScrollViewHorizontalBar : public ScrollViewBar {
 public:
   void drawRect(KDContext * ctx, KDRect rect) const override;
 private:
   KDCoordinate totalLength() const { return m_frame.width() - 2*m_margin; }
 };
 
-class ScrollViewVerticalIndicator : public ScrollViewIndicator {
+class ScrollViewVerticalBar : public ScrollViewBar {
 public:
   void drawRect(KDContext * ctx, KDRect rect) const override;
 private:
--- escher/src/scroll_view_indicator.cpp
+++ escher/src/scroll_view_indicator.cpp
@@ -6,49 +6,20 @@ extern "C" {
 
 ScrollViewIndicator::ScrollViewIndicator() :
   View(),
-  m_offset(0),
-  m_visibleLength(0),
-  m_indicatorColor(Palette::GreyDark),
-  m_backgroundColor(Palette::GreyMiddle),
+  m_color(Palette::GreyDark),
   m_margin(14)
 {
 }
 
-void ScrollViewHorizontalIndicator::drawRect(KDContext * ctx, KDRect rect) const {
-  ctx->fillRect(
-    KDRect(
-      m_margin, (m_frame.height() - k_indicatorThickness)/2,
-      totalLength(), k_indicatorThickness
-    ),
-    m_backgroundColor
-  );
-  ctx->fillRect(
-    KDRect(
-      m_margin+m_offset*totalLength(), (m_frame.height() - k_indicatorThickness)/2,
-      m_visibleLength*totalLength(), k_indicatorThickness
-    ),
-    m_indicatorColor
-  );
+ScrollViewBar::ScrollViewBar() :
+  ScrollViewIndicator(),
+  m_offset(0),
+  m_visibleLength(0),
+  m_trackColor(Palette::GreyMiddle)
+{
 }
 
-void ScrollViewVerticalIndicator::drawRect(KDContext * ctx, KDRect rect) const {
-  ctx->fillRect(
-    KDRect(
-      (m_frame.width() - k_indicatorThickness)/2, m_margin,
-      k_indicatorThickness, totalLength()
-    ),
-    m_backgroundColor
-  );
-  ctx->fillRect(
-    KDRect(
-      (m_frame.width() - k_indicatorThickness)/2, m_margin+m_offset*totalLength(),
-      k_indicatorThickness, m_visibleLength*totalLength()
-    ),
-    m_indicatorColor
-  );
-}
-
-void ScrollViewIndicator::update(KDCoordinate totalContentLength, KDCoordinate contentOffset, KDCoordinate visibleContentLength) {
+void ScrollViewBar::update(KDCoordinate totalContentLength, KDCoordinate contentOffset, KDCoordinate visibleContentLength) {
   float offset = contentOffset;
   float visibleLength = visibleContentLength;
   offset = offset / totalContentLength;
@@ -58,6 +29,40 @@ void ScrollViewIndicator::update(KDCoordinate totalContentLength, KDCoordinate c
     m_visibleLength = visibleLength;
     markRectAsDirty(bounds());
   }
+}
+
+void ScrollViewHorizontalBar::drawRect(KDContext * ctx, KDRect rect) const {
+  ctx->fillRect(
+    KDRect(
+      m_margin, (m_frame.height() - k_indicatorThickness)/2,
+      totalLength(), k_indicatorThickness
+    ),
+    m_trackColor
+  );
+  ctx->fillRect(
+    KDRect(
+      m_margin+m_offset*totalLength(), (m_frame.height() - k_indicatorThickness)/2,
+      m_visibleLength*totalLength(), k_indicatorThickness
+    ),
+    m_color
+  );
+}
+
+void ScrollViewVerticalBar::drawRect(KDContext * ctx, KDRect rect) const {
+  ctx->fillRect(
+    KDRect(
+      (m_frame.width() - k_indicatorThickness)/2, m_margin,
+      k_indicatorThickness, totalLength()
+    ),
+    m_trackColor
+  );
+  ctx->fillRect(
+    KDRect(
+      (m_frame.width() - k_indicatorThickness)/2, m_margin+m_offset*totalLength(),
+      k_indicatorThickness, m_visibleLength*totalLength()
+    ),
+    m_color
+  );
 }
 
 #if ESCHER_VIEW_LOGGING

e6b61b257b0275de1e964131b868f2f39c081560
Léa Saviot <lea.saviot@numworks.com>
2019-01-23 17:00:48 +0100

    [unicode] Methods optimized when code points are one char long

--- ion/include/ion/unicode/utf8_helper.h
+++ ion/include/ion/unicode/utf8_helper.h
@@ -6,11 +6,19 @@
 
 namespace UTF8Helper {
 
+// Returns the number of occurences of a code point in a string
+int CountOccurrences(const char * s, CodePoint c);
+
+// Returns the first occurence of a code point in a string
 const char * CodePointSearch(const char * s, CodePoint c);
-/* CopyAndRemoveCodePoint copies src into dst while removing all code points c.
- * It also updates an index that should be lower if code points where removed
- * before it. */
+
+/* Copy src into dst while removing all code points c. Also update an index
+ * that should be lower if code points where removed before it. */
 void CopyAndRemoveCodePoint(char * dst, size_t dstSize, const char * src, CodePoint c, const char * * indexToDUpdate = nullptr);
+
+// Perform an action each time a code point is found
+typedef void (*CodePointAction)(char * codePointLocation, void * contextPointer, int contextInt);
+void PerformAtCodePoints(const char * s, CodePoint c, CodePointAction action, void * contextPointer, int contextInt);
 
 };
 

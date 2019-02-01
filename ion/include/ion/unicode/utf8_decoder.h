f655e4e580046ab075e0cd374d503ec6ed05a9d0
Léa Saviot <lea.saviot@numworks.com>
2019-02-01 10:36:16 +0100

    [unicode] Comment about avoiding accents in literals

--- ion/include/ion/unicode/utf8_decoder.h
+++ ion/include/ion/unicode/utf8_decoder.h
@@ -14,7 +14,19 @@
  * 0 and 7F         -> 0xxxxxxx
  * 80 and 7FF       -> 110xxxxx 10xxxxxx
  * 800 and FFFF     -> 1110xxxx 10xxxxxx 10xxxxxx
- * 10000 and 10FFFF -> 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx */
+ * 10000 and 10FFFF -> 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+ *
+ * WARNING: A single glyph can have several code point translations. For
+ * instance, 'ê' can be the code point 'e' followed by the code point '^', but
+ * it can also be the unique code point 'ê'. We assume NFKD normalization, where
+ * 'ê' will be 'e' followed by a combining code point '^'.
+ * The i18n literals are processed by i18n.py, where we enforce the NFKD
+ * normalization, but other string literals (for instance the script templates)
+ * will be encoded as the text editor chooses.
+ *
+ * /!\ All literals except the i18n should thus avoid containing ambiguous
+ *     glyphs such as accentuated letters.
+ */
 
 class UTF8Decoder {
 public:

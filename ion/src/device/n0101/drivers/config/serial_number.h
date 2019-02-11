3f86f2c54e65c1f2d1344da7035aa1898050fb5b
Léa Saviot <lea.saviot@numworks.com>
2019-02-11 12:29:03 +0100

    [ion] Fix Serial Number adresses on different models

---
+++ ion/src/device/n0101/drivers/config/serial_number.h
@@ -0,0 +1,15 @@
+#ifndef ION_DEVICE_N0101_SERIAL_NUMBER_H
+#define ION_DEVICE_N0101_SERIAL_NUMBER_H
+
+namespace Ion {
+namespace Device {
+namespace SerialNumber {
+
+constexpr static int RawUniqueIDAddress = 0x1FF07A10;
+
+
+}
+}
+}
+
+#endif

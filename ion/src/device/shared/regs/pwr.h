b6c291d276d42014c666d018c5c436e5c53f3bf9
Léa Saviot <lea.saviot@numworks.com>
2019-02-12 17:22:28 +0100

    [ion] PWR registers differ between models

--- ion/src/device/shared/regs/pwr.h
+++ ion/src/device/shared/regs/pwr.h
@@ -2,6 +2,7 @@
 #define REGS_PWR_H
 
 #include "register.h"
+#include <regs/config/pwr.h>
 
 namespace Ion {
 namespace Device {
@@ -14,6 +15,15 @@ public:
     REGS_BOOL_FIELD(LPDS, 0);
     REGS_BOOL_FIELD(PPDS, 1);
     REGS_BOOL_FIELD(FPDS, 9);
+#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
+    REGS_BOOL_FIELD(LPUDS, 10);
+    REGS_BOOL_FIELD(MRUDS, 11);
+    enum class UnderDrive {
+      Disable = 0,
+      Enable = 3
+    };
+    REGS_FIELD_W(UDEN, UnderDrive, 19, 18);
+#endif
   };
 
   constexpr PWR() {};

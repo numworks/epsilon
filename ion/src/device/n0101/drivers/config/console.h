425042d9571ad6300d09620ab878350170d2b9b6
Léa Saviot <lea.saviot@numworks.com>
2019-02-12 14:15:43 +0100

    [ion/console] Tune the value of the USART baud rate

--- ion/src/device/n0101/drivers/config/console.h
+++ ion/src/device/n0101/drivers/config/console.h
@@ -13,6 +13,15 @@ constexpr static GPIOPin RxPin = GPIOPin(GPIOC, 7);
 constexpr static GPIOPin TxPin = GPIOPin(GPIOC, 6);
 constexpr static GPIO::AFR::AlternateFunction AlternateFunction = GPIO::AFR::AlternateFunction::AF8;
 
+/* The baud rate of the UART is set by the following equation:
+ * BaudRate = f/USARTDIV, where f is the clock frequency and USARTDIV a divider.
+ * In other words, USARTDIV = f/BaudRate. All frequencies in Hz.
+ *
+ * In our case, we configure the minicom to use a 115200 BaudRate and
+ * f = fAPB2 = 96 MHz, so USARTDIV = 833.333 */
+constexpr static int USARTDIVValue = 833;
+
+
 }
 }
 }

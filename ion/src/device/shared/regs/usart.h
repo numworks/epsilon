3b2c1050c98d134351181c1b9d4f0ba4fd177472
Léa Saviot <lea.saviot@numworks.com>
2019-02-12 14:11:56 +0100

    [ion] Fix USART registers for n0101

--- ion/src/device/shared/regs/usart.h
+++ ion/src/device/shared/regs/usart.h
@@ -2,6 +2,7 @@
 #define REGS_USART_H
 
 #include "register.h"
+#include <regs/config/usart.h>
 
 class USART {
 public:
@@ -11,11 +12,16 @@ public:
     REGS_BOOL_FIELD(TXE, 7);
   };
 
-  class DR : Register32 {
+  // RDR and TDR are the same DR register on some models.
+  class RDR : Register32 {
   public:
     uint16_t get() volatile {
       return (uint16_t)getBitRange(8, 0);
     }
+  };
+
+  class TDR : Register32 {
+  public:
     void set(uint16_t v) volatile {
       setBitRange(8, 0, v);
     }
@@ -23,26 +29,28 @@ public:
 
   class BRR : Register32 {
   public:
+    // TODO one value only on 0-15
     REGS_FIELD(DIV_FRAC, uint8_t, 3, 0);
     REGS_FIELD(DIV_MANTISSA, uint16_t, 15, 4);
   };
 
   class CR1 : Register32 {
   public:
-    REGS_BOOL_FIELD(UE, 13);
+    REGS_BOOL_FIELD(UE, Ion::Device::USART::Config::UEOffset);
     REGS_BOOL_FIELD(TE, 3);
     REGS_BOOL_FIELD(RE, 2);
   };
 
   constexpr USART(int i) : m_index(i) {}
   constexpr operator int() const { return m_index; }
-  REGS_REGISTER_AT(SR, 0x00);
-  REGS_REGISTER_AT(DR, 0x04);
-  REGS_REGISTER_AT(BRR, 0x08);
-  REGS_REGISTER_AT(CR1, 0x0C);
+  REGS_REGISTER_AT(SR, Ion::Device::USART::Config::SROffset);
+  REGS_REGISTER_AT(RDR, Ion::Device::USART::Config::RDROffset);
+  REGS_REGISTER_AT(TDR, Ion::Device::USART::Config::TDROffset);
+  REGS_REGISTER_AT(BRR, Ion::Device::USART::Config::BRROffset);
+  REGS_REGISTER_AT(CR1, Ion::Device::USART::Config::CR1Offset);
 private:
   constexpr uint32_t Base() const {
-    return ((uint32_t []){0x40011000, 0x40004400, 0x40004800})[m_index-1];
+    return ((uint32_t []){0x40011000, 0x40004400, 0x40004800, 0x40004C00, 0x40005000, 0x40011400})[m_index-1];
   };
   int m_index;
 };

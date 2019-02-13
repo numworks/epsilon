f2722b02051d3e0b90df6a0575337cc7ac566922
Romain Goyet <romain.goyet@numworks.com>
2019-02-13 15:57:23 +0100

    [ion/device/regs] Use macros to configure the registers

--- ion/src/device/n0100/regs/config/usart.h
+++ ion/src/device/n0100/regs/config/usart.h
@@ -1,21 +1,12 @@
 #ifndef ION_DEVICE_N0100_REGS_CONFIG_USART_H
 #define ION_DEVICE_N0100_REGS_CONFIG_USART_H
 
-namespace Ion {
-namespace Device {
-namespace USART {
-namespace Config {
+#define REGS_USART_SR_OFFSET 0x00
+#define REGS_USART_RDR_OFFSET 0x04
+#define REGS_USART_TDR_OFFSET 0x04
+#define REGS_USART_BRR_OFFSET 0x08
+#define REGS_USART_CR1_OFFSET 0x0C
 
-constexpr static int SROffset = 0x00;
-constexpr static int RDROffset = 0x04;
-constexpr static int TDROffset = 0x04;
-constexpr static int BRROffset = 0x08;
-constexpr static int CR1Offset = 0x0C;
-constexpr static int UEOffset = 13;
-
-}
-}
-}
-}
+#define REGS_USART_CR1_UE_BIT 13
 
 #endif

513a4cf8520640dca8da01eaf340c6dd816d07f8
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-02-20 18:12:33 +0100

    [apps] BannerView::numberOfSubviews
    Shared::BannerView::numberOfSubviews made pure virtual.
    Define constexpr attributes instead of magic numbers.
    Move numberOfSubviews to header file.

--- apps/sequence/graph/banner_view.cpp
+++ apps/sequence/graph/banner_view.cpp
@@ -8,10 +8,6 @@ BannerView::BannerView() :
 {
 }
 
-int BannerView::numberOfSubviews() const {
-  return 2;
-}
-
 TextView * BannerView::textViewAtIndex(int i) const {
   const TextView * views[2] = {&m_abscissaView, &m_sequenceView};
   return (TextView *)views[i];

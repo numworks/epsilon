529c73947d141009b636a9336e45c0e24836d1be
Émilie Feral <emilie.feral@numworks.com>
2019-02-21 12:50:29 +0100

    [apps/shared] Change name StorageExpressionModel --> ExpressionModelHandle

--- apps/shared/storage_expression_model_store.h
+++ apps/shared/storage_expression_model_store.h
@@ -1,7 +1,7 @@
 #ifndef SHARED_STORAGE_EXPRESSION_MODEL_STORE_H
 #define SHARED_STORAGE_EXPRESSION_MODEL_STORE_H
 
-#include "storage_expression_model.h"
+#include "expression_model_handle.h"
 #include "expiring_pointer.h"
 #include <ion/storage.h>
 #include <assert.h>
@@ -17,10 +17,10 @@ public:
 
   // Getters
   int numberOfModels() const;
-  int numberOfDefinedModels() const { return numberOfModelsSatisfyingTest([](StorageExpressionModel * m) { return m->isDefined(); }); }
+  int numberOfDefinedModels() const { return numberOfModelsSatisfyingTest([](ExpressionModelHandle * m) { return m->isDefined(); }); }
   Ion::Storage::Record recordAtIndex(int i) const;
-  Ion::Storage::Record definedRecordAtIndex(int i) const { return recordStatifyingTestAtIndex(i, [](StorageExpressionModel * m) { return m->isDefined(); }); }
-  ExpiringPointer<StorageExpressionModel> modelForRecord(Ion::Storage::Record record) const { return ExpiringPointer<StorageExpressionModel>(privateModelForRecord(record)); }
+  Ion::Storage::Record definedRecordAtIndex(int i) const { return recordStatifyingTestAtIndex(i, [](ExpressionModelHandle * m) { return m->isDefined(); }); }
+  ExpiringPointer<ExpressionModelHandle> modelForRecord(Ion::Storage::Record record) const { return ExpiringPointer<ExpressionModelHandle>(privateModelForRecord(record)); }
 
   // Add and Remove
   virtual Ion::Storage::Record::ErrorStatus addEmptyModel() = 0;
@@ -32,14 +32,14 @@ public:
   void storageDidChangeForRecord(const Ion::Storage::Record record) const { resetMemoizedModelsExceptRecord(record); }
 protected:
   constexpr static int k_maxNumberOfMemoizedModels = 10;
-  typedef bool (*ModelTest)(StorageExpressionModel * model);
+  typedef bool (*ModelTest)(ExpressionModelHandle * model);
   int numberOfModelsSatisfyingTest(ModelTest test) const;
   Ion::Storage::Record recordStatifyingTestAtIndex(int i, ModelTest test) const;
-  StorageExpressionModel * privateModelForRecord(Ion::Storage::Record record) const;
+  ExpressionModelHandle * privateModelForRecord(Ion::Storage::Record record) const;
 private:
   void resetMemoizedModelsExceptRecord(const Ion::Storage::Record record = Ion::Storage::Record()) const;
   virtual void setMemoizedModelAtIndex(int cacheIndex, Ion::Storage::Record) const = 0;
-  virtual StorageExpressionModel * memoizedModelAtIndex(int cacheIndex) const = 0;
+  virtual ExpressionModelHandle * memoizedModelAtIndex(int cacheIndex) const = 0;
   virtual const char * modelExtension() const = 0;
   /* Memoization of k_maxNumberOfMemoizedModels. When the required model is not
    * present, we override the m_oldestMemoizedIndex model. This actually

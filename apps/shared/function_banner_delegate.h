7b4a4b4db45f4041daf523dcedb34eb15c34a3de
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-02-22 11:32:05 +0100

    [apps/shared] FunctionBannerDelegate::bannerView() returns a XYBannerView *

--- apps/shared/function_banner_delegate.h
+++ apps/shared/function_banner_delegate.h
@@ -2,7 +2,7 @@
 #define SHARED_FUNCTION_BANNER_DELEGATE_H
 
 #include "function_store.h"
-#include "banner_view.h"
+#include "xy_banner_view.h"
 #include "curve_view_cursor.h"
 
 namespace Shared {
@@ -12,7 +12,7 @@ public:
   constexpr static int k_maxNumberOfCharacters = 50;
 protected:
   void reloadBannerViewForCursorOnFunction(CurveViewCursor * cursor, Ion::Storage::Record record, FunctionStore * functionStore, char symbol);
-  virtual BannerView * bannerView() = 0;
+  virtual XYBannerView * bannerView() = 0;
 };
 
 }

14b0e464b650dcb0d2be7ed181f777f7775a0c7b
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-02-22 15:44:49 +0100

    [apps/graph] Graph::GraphControllerHelper::BannerView() returns a Graph::BannerView *
    Instead of a Shared::BannerView *

--- apps/graph/graph/graph_controller_helper.h
+++ apps/graph/graph/graph_controller_helper.h
@@ -1,7 +1,7 @@
 #ifndef GRAPH_GRAPH_CONTROLLER_HELPER_H
 #define GRAPH_GRAPH_CONTROLLER_HELPER_H
 
-#include "../../shared/banner_view.h"
+#include "banner_view.h"
 #include "../../shared/text_field_delegate_app.h"
 #include "../../shared/interactive_curve_view_range.h"
 #include "../cartesian_function_store.h"
@@ -14,7 +14,7 @@ class GraphControllerHelper {
 protected:
   bool privateMoveCursorHorizontally(Shared::CurveViewCursor * cursor, int direction, Shared::InteractiveCurveViewRange * range, int numberOfStepsInGradUnit, Ion::Storage::Record record, App * app, float cursorTopMarginRatio, float cursorRightMarginRatio, float cursorBottomMarginRatio, float cursorLeftMarginRatio);
   void reloadDerivativeInBannerViewForCursorOnFunction(Shared::CurveViewCursor * cursor, Ion::Storage::Record record, App * app);
-  virtual Shared::BannerView * bannerView() = 0;
+  virtual BannerView * bannerView() = 0;
 };
 
 }

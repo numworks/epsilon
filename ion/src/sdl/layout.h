4ff5a291776b16ff18eeae1bdaf78a8228b6ca5f
Romain Goyet <romain.goyet@numworks.com>
2019-02-25 18:22:23 +0100

    [ion/sdl] Use an areaOfInterest for the layout

--- ion/src/sdl/layout.h
+++ ion/src/sdl/layout.h
@@ -8,11 +8,12 @@ namespace Ion {
 namespace SDL {
 namespace Layout {
 
-void setSize(int w, int h);
+void setFrame(SDL_Rect * rect);
+void getAreaOfInterest(SDL_FRect * fRect);
 
 void getScreenRect(SDL_Rect * rect);
 Ion::Keyboard::Key keyAt(SDL_Point * p);
-Ion::Keyboard::Key keyAtF(SDL_FPoint * p);
+//Ion::Keyboard::Key keyAtF(SDL_FPoint * p);
 
 void getKeyRect(int validKeyIndex, SDL_Rect * rect);
 

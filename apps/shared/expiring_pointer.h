4aac9204e2858344425c5b96c7d5f9c5f5631ee6
Émilie Feral <emilie.feral@numworks.com>
2019-02-28 14:50:19 +0100

    [shared] ExpiringPointer: add constructor to enable pointer casting

--- apps/shared/expiring_pointer.h
+++ apps/shared/expiring_pointer.h
@@ -7,12 +7,21 @@ namespace Shared {
 
 template <class T>
 class ExpiringPointer {
+  template<typename U>
+  friend class ExpiringPointer;
 public:
   ExpiringPointer(T * rawPointer, bool reinitGlobal = true) : m_rawPointer(rawPointer) {
 #if DEBUG
     s_global = rawPointer;
 #endif
   }
+  template<class U>
+  ExpiringPointer(ExpiringPointer<U> p) : m_rawPointer(static_cast<T *>(p.m_rawPointer)) {
+#if DEBUG
+    s_global = m_rawPointer;
+#endif
+  }
+  bool isNull() { return m_rawPointer == nullptr; }
   T *operator->() {
 #if DEBUG
     assert(m_rawPointer != nullptr && m_rawPointer == s_global);

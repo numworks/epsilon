ac51a98bee74b5203cee1f857a22ce35a756e9a6
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-03-04 14:40:45 +0100

    [apps/shared] XYBannerView contains a TextField

--- apps/shared/xy_banner_view.cpp
+++ apps/shared/xy_banner_view.cpp
@@ -8,14 +8,28 @@ XYBannerView::XYBannerView(
   InputEventHandlerDelegate * inputEventHandlerDelegate,
   TextFieldDelegate * textFieldDelegate
 ) :
-  m_abscissaView(Font(), 0.5f, 0.5f, TextColor(), BackgroundColor()),
+  m_abscissaSymbol(Font(), 1.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_abscissaValue(
+    parentResponder,
+    m_draftTextBuffer,
+    m_draftTextBuffer,
+    TextField::maxBufferSize(),
+    inputEventHandlerDelegate,
+    textFieldDelegate,
+    false,
+    Font(),
+    0.0f, 0.5f,
+    TextColor(),
+    BackgroundColor()
+  ),
   m_ordinateView(Font(), 0.5f, 0.5f, TextColor(), BackgroundColor())
 {
+  m_draftTextBuffer[0] = 0;
 }
 
 View * XYBannerView::subviewAtIndex(int index) {
   assert(0 <= index && index < numberOfSubviews());
-  View * subviews[] = {&m_abscissaView, &m_ordinateView};
+  View * subviews[] = {&m_abscissaSymbol, &m_abscissaValue, &m_ordinateView};
   return subviews[index];
 }
 

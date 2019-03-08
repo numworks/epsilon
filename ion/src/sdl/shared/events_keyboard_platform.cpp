74b821a1c1cebb0c24789fec610d6fedffbb64eb
Romain Goyet <romain.goyet@numworks.com>
2019-03-08 13:59:35 +0100

    [ion] Share the keyboard-to-event code between platforms

---
+++ ion/src/sdl/shared/events_keyboard_platform.cpp
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#include <ion/events.h>
+#include <SDL.h>
+
+namespace Ion {
+namespace Events {
+
+Event getPlatformEvent() {
+  Ion::SDL::Main::refresh();
+
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    // The while is important: it'll do a fast-pass over all useless SDL events
+    if (event.type == SDL_WINDOWEVENT) {
+      if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+        Ion::SDL::Main::relayout();
+      }
+    }
+    if (event.type == SDL_QUIT) {
+      return Termination;
+    }
+  }
+  return None;
+}
+
+}
+}

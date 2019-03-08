017b74c2713723ee81b1e73b0b9fb6d0cea3550a
Léa Saviot <lea.saviot@numworks.com>
2019-03-08 16:01:59 +0100

    [ion] LED timers differ between models

--- ion/src/device/n0101/drivers/config/led.h
+++ ion/src/device/n0101/drivers/config/led.h
@@ -10,10 +10,14 @@ namespace Config {
 
 using namespace Regs;
 
+static constexpr int RedChannel = 1;
+static constexpr int GreenChannel = 2;
+static constexpr int BlueChannel = 3;
+
 constexpr static AFGPIOPin RGBPins[] = {
-  AFGPIOPin(GPIOB, 4,  GPIO::AFR::AlternateFunction::AF2, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::High),
-  AFGPIOPin(GPIOB, 5,  GPIO::AFR::AlternateFunction::AF2, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::High),
-  AFGPIOPin(GPIOB, 0,  GPIO::AFR::AlternateFunction::AF2, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::High),
+  AFGPIOPin(GPIOB, 4,  GPIO::AFR::AlternateFunction::AF2, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::High), // RED
+  AFGPIOPin(GPIOB, 5,  GPIO::AFR::AlternateFunction::AF2, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::High), // GREEN
+  AFGPIOPin(GPIOB, 0,  GPIO::AFR::AlternateFunction::AF2, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::High) // BLUE
 };
 
 }

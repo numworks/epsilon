e59af4311c1915b38f29d8c0abf440c478cdebb7
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-03-08 11:40:45 +0100

    [apps] Factorize portions checking whether text approximates to double

--- apps/shared/float_parameter_controller.cpp
+++ apps/shared/float_parameter_controller.cpp
@@ -122,11 +122,8 @@ bool FloatParameterController::textFieldShouldFinishEditing(TextField * textFiel
 }
 
 bool FloatParameterController::textFieldDidFinishEditing(TextField * textField, const char * text, Ion::Events::Event event) {
-  AppsContainer * appsContainer = ((TextFieldDelegateApp *)app())->container();
-  Context * globalContext = appsContainer->globalContext();
-  double floatBody = PoincareHelpers::ApproximateToScalar<double>(text, *globalContext);
-  if (std::isnan(floatBody) || std::isinf(floatBody)) {
-    app()->displayWarning(I18n::Message::UndefinedValue);
+  double floatBody;
+  if (textFieldDelegateApp()->hasUndefinedValue(text, floatBody)) {
     return false;
   }
   if (!setParameterAtIndex(selectedRow(), floatBody)) {

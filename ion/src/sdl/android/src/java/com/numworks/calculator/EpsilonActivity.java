d9c0fe062012285b208a20cc648830e09b301d7a
Romain Goyet <romain.goyet@numworks.com>
2019-03-12 16:14:02 +0100

    [ion/sdl/android] Add analytics

--- ion/src/sdl/android/src/java/com/numworks/calculator/EpsilonActivity.java
+++ ion/src/sdl/android/src/java/com/numworks/calculator/EpsilonActivity.java
@@ -1,33 +1,49 @@
 package com.numworks.calculator;
 
-import android.graphics.Bitmap;
-//import android.graphics.Bitmap.CompressFormat;
-import android.graphics.BitmapFactory;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.util.Log;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+import com.google.android.gms.analytics.HitBuilders;
 
 import org.libsdl.app.SDLActivity;
 
 public class EpsilonActivity extends SDLActivity {
+  private static GoogleAnalytics sAnalytics;
+  private static Tracker sTracker;
+
   protected String[] getLibraries() {
     return new String[] {
       "epsilon"
     };
   }
 
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    sAnalytics = GoogleAnalytics.getInstance(this);
+    sTracker = sAnalytics.newTracker("UA-93775823-3");
+  }
+
+  @Override
+  public void onResume() {
+    sTracker.setScreenName("Calculator");
+    sTracker.send(new HitBuilders.ScreenViewBuilder().build());
+  }
+
   public Bitmap retrieveBitmapAsset(String identifier) {
-    Log.w("LoadTexture", "Retrieving bitmap asset");
-    Log.w("LoadTexture", "Asset is " + identifier);
     Bitmap bitmap = null;
     try {
-      Log.w("LoadTexture", "Rez is " + this.getResources().getAssets().open(identifier));
       bitmap = BitmapFactory.decodeStream(
         this.getResources().getAssets().open(identifier)
       );
     } catch (Exception e) {
       Log.w("LoadTexture", "Coundn't load a file:" + identifier);
     }
-
     return bitmap;
   }
 }

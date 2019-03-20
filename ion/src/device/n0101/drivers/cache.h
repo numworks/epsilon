079d9479556b8e6d202b55f81b3b7831e7c8f27e
Léa Saviot <lea.saviot@numworks.com>
2019-03-20 15:43:36 +0100

    [ion] Cache invalidation methods

--- ion/src/device/n0101/drivers/cache.h
+++ ion/src/device/n0101/drivers/cache.h
@@ -7,16 +7,22 @@ namespace Ion {
 namespace Device {
 namespace Cache {
 
+/* Data synchronisation barrier
+ * Ensures that the processor stalls until the memory write is complete */
 inline void dsb() {
   asm volatile("dsb 0xF":::"memory");
 }
 
+/* Instructions synchronisation barrier
+ * Ensures that the subsequent instructions are loaded in the new context */
 inline void isb() {
   asm volatile("isb 0xF":::"memory");
 }
 
+void invalidateDCache();
 void enableDCache();
 
+void invalidateICache();
 void enableICache();
 
 }

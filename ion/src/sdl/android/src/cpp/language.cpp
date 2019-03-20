91cad805c1be9043a76894a3602c7959bec98635
Romain Goyet <romain.goyet@numworks.com>
2019-03-20 17:53:06 +0100

    [ion/sdl/android] Fix the build

--- ion/src/sdl/android/src/cpp/language.cpp
+++ ion/src/sdl/android/src/cpp/language.cpp
@@ -1,6 +1,6 @@
 #include "../../../shared/platform.h"
 
-const char * IonSDLPlatformGetLanguageCode() {
+char * IonSDLPlatformGetLanguageCode() {
   // The Android NDK cannot retrieve the current locale
   // This will be handled by the Java side
   return nullptr;

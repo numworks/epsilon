643031526f5f325b1e27785e975f2235742d8f1d
Léa Saviot <lea.saviot@numworks.com>
2019-03-21 13:18:17 +0100

    [ion] Calibrate msleep / usleep for N0101

--- ion/src/device/n0101/drivers/config/timing.h
+++ ion/src/device/n0101/drivers/config/timing.h
@@ -8,12 +8,12 @@ namespace Device {
 namespace Timing {
 namespace Config {
 
-// TODO: calibrate msleep
-constexpr static int LoopsPerMillisecond = 8852;
-constexpr static int LoopsPerMicrosecond = 9;
+constexpr static int LoopsPerMillisecond = 4426;
+constexpr static int LoopsPerMicrosecond = 36;
 // CPU clock is 96 MHz, and systick clock source is divided by 8
 // To get 1 ms systick overflow we need to reset it to
 // 96 000 000 (Hz) / 8 / 1 000 (ms/s) - 1 (because the counter resets *after* counting to 0)
+// TODO: calibrate systick
 constexpr static int SysTickPerMillisecond = 12000;
 
 

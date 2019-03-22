af83a8d0155fb9cb07ebbaa0b4aa7bb79e445ba3
Léa Saviot <lea.saviot@numworks.com>
2019-03-22 17:51:33 +0100

    [ion] Before a jump reset, deinit the flash and shutdown the board

--- ion/src/device/shared/drivers/reset.cpp
+++ ion/src/device/shared/drivers/reset.cpp
@@ -1,6 +1,8 @@
 #include "reset.h"
 #include <regs/regs.h>
 #include <drivers/cache.h>
+#include <drivers/board.h>
+#include <drivers/external_flash.h>
 
 extern void * _isr_start;
 
@@ -16,18 +18,22 @@ void core() {
 }
 
 void jump() {
-  // TODO: shutdown all clocks and peripherial to mimic a hardware reset
   // Disable cache before reset
   Ion::Device::Cache::disableDCache();
   Ion::Device::Cache::disableICache();
 
-  uint32_t * stackPointerAddress = reinterpret_cast<uint32_t *>(&_isr_start);
-  uint32_t * resetHandlerAddress = stackPointerAddress + 1;
+  /* Shutdown all clocks and periherals to mimic a hardware reset. Special
+   * deinit for he external flash (it needs a reset command). */
+  ExternalFlash::deinit();
+  Board::shutdown();
 
   /* Jump to the reset service routine after having reset the stack pointer.
    * Both addresses are fetched from the base of the Flash memory, just like a
    * real reset would. These operations should be made at once, otherwise the C
    * compiler might emit some instructions that modify the stack inbetween. */
+
+  uint32_t * stackPointerAddress = reinterpret_cast<uint32_t *>(&_isr_start);
+  uint32_t * resetHandlerAddress = stackPointerAddress + 1;
 
   asm volatile (
       "msr MSP, %[stackPointer] ; bx %[resetHandler]"

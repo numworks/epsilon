9fc3240debfe93377621284fb2e7857772f32dd2
David <0b101@users.noreply.github.com>
2019-03-25 17:30:19 -0500

    Made Python Font Small

--- escher/include/escher/text_area.h
+++ escher/include/escher/text_area.h
@@ -9,7 +9,7 @@
 
 class TextArea : public TextInput, public InputEventHandler {
 public:
-  TextArea(Responder * parentResponder, View * contentView, const KDFont * font = KDFont::LargeFont);
+  TextArea(Responder * parentResponder, View * contentView, const KDFont * font = KDFont::SmallFont);
   void setDelegates(InputEventHandlerDelegate * inputEventHandlerDelegate, TextAreaDelegate * delegate) { m_inputEventHandlerDelegate = inputEventHandlerDelegate; m_delegate = delegate; }
   bool handleEvent(Ion::Events::Event event) override;
   bool handleEventWithText(const char * text, bool indentation = false, bool forceCursorRightOfText = false) override;

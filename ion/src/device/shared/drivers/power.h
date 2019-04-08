609d343da4d6d18779f84241b82f2f768176a959
Émilie Feral <emilie.feral@numworks.com>
2019-04-08 14:07:34 +0200

    [ion] Power: re-design suspend implementation

---
+++ ion/src/device/shared/drivers/power.h
@@ -0,0 +1,16 @@
+#ifndef ION_DEVICE_SHARED_POWER_H
+#define ION_DEVICE_SHARED_POWER_H
+
+namespace Ion {
+namespace Device {
+namespace Power {
+
+void stopConfiguration();
+void sleepConfiguration();
+//void standby();
+
+}
+}
+}
+
+#endif

0fcd26e11f308dc5f19b726e5e339ee2e7adb410
Émilie Feral <emilie.feral@numworks.com>
2019-04-08 17:33:15 +0200

    [ion] Fix comments on Keyboard

--- ion/src/device/n0101/drivers/config/keyboard.h
+++ ion/src/device/n0101/drivers/config/keyboard.h
@@ -12,15 +12,37 @@
  *  PC3 | Keyboard column 4 | Input, pulled up
  *  PC4 | Keyboard column 5 | Input, pulled up
  *  PC5 | Keyboard column 6 | Input, pulled up
- *  PE0 | Keyboard row A    | Output, open drain
- *  PE1 | Keyboard row B    | Output, open drain
- *  PE2 | Keyboard row C    | Output, open drain
- *  PE3 | Keyboard row D    | Output, open drain
- *  PE4 | Keyboard row E    | Output, open drain
- *  PE5 | Keyboard row F    | Output, open drain
- *  PE6 | Keyboard row G    | Output, open drain
- *  PE7 | Keyboard row H    | Output, open drain
- *  PE8 | Keyboard row I    | Output, open drain
+ *  PA1 | Keyboard row A    | Output, open drain
+ *  PA0 | Keyboard row B    | Output, open drain
+ *  PA2 | Keyboard row C    | Output, open drain
+ *  PA3 | Keyboard row D    | Output, open drain
+ *  PA4 | Keyboard row E    | Output, open drain
+ *  PA5 | Keyboard row F    | Output, open drain
+ *  PA6 | Keyboard row G    | Output, open drain
+ *  PA7 | Keyboard row H    | Output, open drain
+ *  PA8 | Keyboard row I    | Output, open drain
+ *
+ * The keyboard is a matrix that is laid out as follow:
+ *
+ * -+------+------+------+------+------+------+
+ *  | K_A1 | K_A2 | K_A3 | K_A4 | K_A5 | K_A6 |
+ * -+------+------+------+------+------+------+
+ *  | K_B1 |      | K_B3 |      |      |      |
+ * -+------+------+------+------+------+------+
+ *  | K_C1 | K_C2 | K_C3 | K_C4 | K_C5 | K_C6 |
+ * -+------+------+------+------+------+------+
+ *  | K_D1 | K_D2 | K_D3 | K_D4 | K_D5 | K_D6 |
+ * -+------+------+------+------+------+------+
+ *  | K_E1 | K_E2 | K_E3 | K_E4 | K_E5 | K_E6 |
+ * -+------+------+------+------+------+------+
+ *  | K_F1 | K_F2 | K_F3 | K_F4 | K_F5 |      |
+ * -+------+------+------+------+------+------+
+ *  | K_G1 | K_G2 | K_G3 | K_G4 | K_G5 |      |
+ * -+------+------+------+------+------+------+
+ *  | K_H1 | K_H2 | K_H3 | K_H4 | K_H5 |      |
+ * -+------+------+------+------+------+------+
+ *  | K_I1 | K_I2 | K_I3 | K_I4 | K_I5 |      |
+ * -+------+------+------+------+------+------|
  */
 
 namespace Ion {

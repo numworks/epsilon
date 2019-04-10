505dcf8566877882b5a078965ec8339a0c420ef6
Émilie Feral <emilie.feral@numworks.com>
2019-04-10 11:25:03 +0200

    [ion] Re-order platform events (plug, enumeration, charging)

--- ion/src/device/shared/drivers/events_keyboard_platform.cpp
+++ ion/src/device/shared/drivers/events_keyboard_platform.cpp
@@ -11,21 +11,23 @@ bool sLastUSBEnumerated = false;
 bool sLastBatteryCharging = false;
 
 Event getPlatformEvent() {
-  // First, check if the USB plugged status has changed
+  // First, check if the USB device has been connected to an USB host
+  bool usbEnumerated = USB::isEnumerated();
+  if (usbEnumerated != sLastUSBEnumerated) {
+    sLastUSBPlugged = USB::isPlugged();
+    sLastBatteryCharging = Battery::isCharging();
+    sLastUSBEnumerated = usbEnumerated;
+    if (usbEnumerated) {
+      return Events::USBEnumeration;
+    }
+  }
+
+  // Second, check if the USB plugged status has changed
   bool usbPlugged = USB::isPlugged();
   if (usbPlugged != sLastUSBPlugged) {
     sLastUSBPlugged = usbPlugged;
     sLastBatteryCharging = Battery::isCharging();
     return Events::USBPlug;
-  }
-
-  // Second, check if the USB device has been connected to an USB host
-  bool usbEnumerated = USB::isEnumerated();
-  if (usbEnumerated != sLastUSBEnumerated) {
-    sLastUSBEnumerated = usbEnumerated;
-    if (usbEnumerated) {
-      return Events::USBEnumeration;
-    }
   }
 
   // Third, check if the battery changed charging state

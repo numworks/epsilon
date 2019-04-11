400c38d2a7511d570561784b22e94438cef7b993
Émilie Feral <emilie.feral@numworks.com>
2019-04-11 15:54:08 +0200

    [ion] Add PWR regs

--- ion/src/device/shared/regs/pwr.h
+++ ion/src/device/shared/regs/pwr.h
@@ -14,6 +14,7 @@ public:
   public:
     REGS_BOOL_FIELD(LPDS, 0);
     REGS_BOOL_FIELD(PPDS, 1);
+    REGS_BOOL_FIELD(CSBF, 3);
     REGS_BOOL_FIELD(FPDS, 9);
     REGS_BOOL_FIELD(LPUDS, 10); // Called LPLVDS in N0100
     REGS_BOOL_FIELD(MRUDS, 11); // Called MRLVDS in N100
@@ -36,6 +37,10 @@ public:
 
   class CSR : Register32 {
   public:
+    REGS_BOOL_FIELD_R(WUIF, 0);
+    REGS_BOOL_FIELD_R(SBF, 1);
+    REGS_BOOL_FIELD_W(EIWUP, 8);
+    REGS_BOOL_FIELD_W(BRE, 9);
     REGS_BOOL_FIELD_R(VOSRDY, 14);
 #if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
     REGS_BOOL_FIELD_R(ODRDY, 16);
@@ -43,9 +48,30 @@ public:
 #endif
   };
 
+#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
+  class CR2 : Register32 {
+  public:
+    REGS_BOOL_FIELD_W(WUPP3, 10);
+    REGS_BOOL_FIELD_W(WUPP2, 9);
+    REGS_BOOL_FIELD_W(WUPP1, 8);
+    REGS_BOOL_FIELD_W(CWUPF1, 0);
+  };
+
+  class CSR2 : Register32 {
+  public:
+    REGS_BOOL_FIELD_W(EWUP3, 10);
+    REGS_BOOL_FIELD_W(EWUP2, 9);
+    REGS_BOOL_FIELD_W(EWUP1, 8);
+  };
+#endif
+
   constexpr PWR() {};
   REGS_REGISTER_AT(CR, 0x00);
   REGS_REGISTER_AT(CSR, 0x04);
+#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
+  REGS_REGISTER_AT(CR2, 0x08);
+  REGS_REGISTER_AT(CSR2, 0x0C);
+#endif
 private:
   constexpr uint32_t Base() const {
     return 0x40007000;

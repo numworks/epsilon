867d50bfd29a9e93c474c14a625ea2f4110e31a3
Émilie Feral <emilie.feral@numworks.com>
2019-04-11 15:49:03 +0200

    [ion] Fix PWR regs

--- ion/src/device/shared/regs/pwr.h
+++ ion/src/device/shared/regs/pwr.h
@@ -17,13 +17,13 @@ public:
     REGS_BOOL_FIELD(FPDS, 9);
     REGS_BOOL_FIELD(LPUDS, 10); // Called LPLVDS in N0100
     REGS_BOOL_FIELD(MRUDS, 11); // Called MRLVDS in N100
-#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
     enum class Voltage {
       Scale3 = 0x01,
       Scale2 = 0x10,
       Scale1 = 0x11
     };
     REGS_FIELD_W(VOS, Voltage, 15, 14);
+#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
     REGS_BOOL_FIELD_W(ODEN, 16);
     REGS_BOOL_FIELD_W(ODSWEN, 17);
     enum class UnderDrive {
@@ -34,20 +34,18 @@ public:
 #endif
   };
 
-#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
-  class CSR1 : Register32 {
+  class CSR : Register32 {
   public:
     REGS_BOOL_FIELD_R(VOSRDY, 14);
+#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
     REGS_BOOL_FIELD_R(ODRDY, 16);
     REGS_BOOL_FIELD_R(ODSWRDY, 17);
-  };
 #endif
+  };
 
   constexpr PWR() {};
   REGS_REGISTER_AT(CR, 0x00);
-#if REGS_PWR_CONFIG_ADDITIONAL_FIELDS
-  REGS_REGISTER_AT(CSR1, 0x04);
-#endif
+  REGS_REGISTER_AT(CSR, 0x04);
 private:
   constexpr uint32_t Base() const {
     return 0x40007000;

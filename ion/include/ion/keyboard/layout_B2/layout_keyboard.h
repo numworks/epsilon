3f9992b762761fdeece6a6c2ecc724909dece62a
Émilie Feral <emilie.feral@numworks.com>
2019-04-12 15:42:55 +0200

    [ion] Change name keyboard_layout --> layout_keyboard

--- ion/include/ion/keyboard/layout_B2/layout_keyboard.h
+++ ion/include/ion/keyboard/layout_B2/layout_keyboard.h
@@ -1,12 +1,12 @@
-#ifndef ION_KEYBOARD_LAYOUT_B3_H
-#define ION_KEYBOARD_LAYOUT_B3_H
+#ifndef ION_KEYBOARD_LAYOUT_B2_LAYOUT_H
+#define ION_KEYBOARD_LAYOUT_B2_LAYOUT_H
 
 namespace Ion {
 namespace Keyboard {
 
 enum class Key : uint8_t {
   A1=0,  A2=1,  A3=2,  A4=3,  A5=4,  A6=5,
-  B1=6,  /*B2=7,*/  B3=8,  /*B4=9,  B5=10, B6=11, */
+  B1=6,  B2=7,  /*B3=8, B4=9,  B5=10, B6=11, */
   C1=12, C2=13, C3=14, C4=15, C5=16, C6=17,
   D1=18, D2=19, D3=20, D4=21, D5=22, D6=23,
   E1=24, E2=25, E3=26, E4=27, E5=28, E6=29,
@@ -17,7 +17,7 @@ enum class Key : uint8_t {
   None = 54
 };
 
-constexpr Key PowerKey = Key::B3;
+constexpr Key PowerKey = Key::B2;
 
 }
 }

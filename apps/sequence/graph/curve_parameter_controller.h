deaed26f761f08b045a1891b4b09440c9f9d80d2
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-04-12 15:10:41 +0200

    [apps/shared] Clean FunctionCurveParameterController constructor

--- apps/sequence/graph/curve_parameter_controller.h
+++ apps/sequence/graph/curve_parameter_controller.h
@@ -1,7 +1,6 @@
 #ifndef SEQUENCE_CURVE_PARAMETER_CONTROLLER_H
 #define SEQUENCE_CURVE_PARAMETER_CONTROLLER_H
 
-#include <escher.h>
 #include "../../shared/function_curve_parameter_controller.h"
 #include "go_to_parameter_controller.h"
 

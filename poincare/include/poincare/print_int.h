3babc1256d3f9e55676bece7e60c20633236c4f0
Léa Saviot <lea.saviot@numworks.com>
2019-04-18 17:28:26 +0200

    [poincare] print_int helper

---
+++ poincare/include/poincare/print_int.h
@@ -0,0 +1,14 @@
+#ifndef POINCARE_PRINT_INT_H
+#define POINCARE_PRINT_INT_H
+
+namespace Poincare {
+
+namespace PrintInt {
+
+bool PadIntInBuffer(int integer, char * buffer, int bufferLength);
+
+}
+
+}
+
+#endif

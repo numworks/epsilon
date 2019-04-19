af1c92c71e8599789196bf0e00e060e6ebcb1efd
Léa Saviot <lea.saviot@numworks.com>
2019-04-19 14:45:33 +0200

    [poincare/print_int] Fix printInt

--- poincare/src/print_int.cpp
+++ poincare/src/print_int.cpp
@@ -6,12 +6,12 @@ namespace Poincare {
 
 bool PrintInt::PadIntInBuffer(int integer, char * buffer, int bufferLength) {
   assert(integer >= 0);
-  int wantedLength = std::log10(integer);
+  int wantedLength = std::log10(integer*1.0);
   if (wantedLength > bufferLength) {
     return false;
   }
   for (int i = 0; i < bufferLength; i++) {
-    buffer[i] = '0' + ((int)(integer/std::pow(10, bufferLength - 1 - i))) - ((int)(integer/std::pow(10, bufferLength-i)))*10;
+    buffer[i] = '0' + ((int)(integer/std::pow(10.0, bufferLength - 1.0 - i))) - ((int)(integer/std::pow(10.0, bufferLength-i)))*10;
   }
   return true;
 }

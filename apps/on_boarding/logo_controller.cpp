d66f0aea8ceab3a7f5bcabbe242a2d09db59565a
Léa Saviot <lea.saviot@numworks.com>
2019-04-29 10:18:28 +0200

    [apps/POST] Test not performed if calculator plugged

--- apps/on_boarding/logo_controller.cpp
+++ apps/on_boarding/logo_controller.cpp
@@ -18,7 +18,27 @@ bool LogoController::fire() {
 }
 
 void LogoController::viewWillAppear() {
-  m_previousLEDColor = PowerOnSelfTest::Perform();
+  /* The backlight is already initialized if the on boarding has been
+   * interrupted by the "Calculator connected" pop up, in which case we do not
+   * want to perform the tests: we would need to shutdown the backlight so the
+   * tests are not visible, which would still be visible for the user. The
+   * interruption does not happen on the production line, and the tests are not
+   * mandatory for the end-user.*/
+  bool backlightInitialized = Ion::Backlight::isInitialized();
+  if (Ion::USB::isPlugged() || backlightInitialized) {
+    m_previousLEDColor = Ion::LED::getColor();
+  } else {
+    m_previousLEDColor = PowerOnSelfTest::Perform();
+    /* If EPSILON_ONBOARDING_APP == 1, the backlight is not initialized in
+     * Ion::Device::Board::initPeripherals, so that the LCD test is not visible to
+     * the user. We thus need to initialize the backlight after the test. Before,
+     * we push a white rect on the display to hide redrawing glitches. */
+    Ion::Display::pushRectUniform(KDRect(KDPointZero, Ion::Display::Width, Ion::Display::Height), KDColorWhite);
+    Ion::Timing::msleep(50);
+  }
+  if (!backlightInitialized) {
+    Ion::Backlight::init();
+  }
   ViewController::viewWillAppear();
 }
 

6960aed4015594442fabc63f6ef9be79fcfced5c
Léa Saviot <lea.saviot@numworks.com>
2019-04-30 17:14:47 +0200

    [apps/post_and_hardware_tests] Clean code

--- apps/shared/post_and_hardware_tests.h
+++ apps/shared/post_and_hardware_tests.h
@@ -2,6 +2,7 @@
 #define SHARED_POST_AND_HARDWARE_TESTS_H
 
 #include <kandinsky/color.h>
+#include <ion/display.h>
 
 namespace Shared {
 
@@ -12,7 +13,12 @@ public:
   static bool FastLCDDataOK();
   static bool LCDDataOK();
 private:
+  constexpr static int k_stampHeight = 10;
+  constexpr static int k_stampWidth = 10;
   constexpr static int k_invalidPixelsLimit = 2;
+  static_assert(Ion::Display::Width % k_stampWidth == 0, "Stamps must tesselate the display");
+  static_assert(Ion::Display::Height % k_stampHeight == 0, "Stamps must tesselate the display");
+  static_assert(k_stampHeight % 2 == 0 || k_stampWidth % 2 == 0, "Even number of XOR needed.");
   static void ColorPixelBuffer(KDColor * pixels, int numberOfPixels, KDColor c);
   static bool TestDisplayColorTiling(KDColor c);
   static bool TestDisplayColorUniform(KDColor c);

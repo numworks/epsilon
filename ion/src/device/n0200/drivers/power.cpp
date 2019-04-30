561bccf507fd04d13cd20fce90419946809c767f
Émilie Feral <emilie.feral@numworks.com>
2019-04-30 12:09:38 +0200

    [ion] N0200: improve code spliting between internal/external flash
    Part II: split standby in two methods: one executed from the external
    flash, one executed from the internal flash. This avoids to force many
    functions to be linked in the internal flash.

--- ion/src/device/n0200/drivers/power.cpp
+++ ion/src/device/n0200/drivers/power.cpp
@@ -1,16 +1,27 @@
 #include <drivers/power.h>
 #include <drivers/board.h>
+#include <drivers/external_flash.h>
 #include <drivers/reset.h>
 
 namespace Ion {
 namespace Power {
 
-void __attribute__((noinline)) standby() {
-  Device::Power::waitUntilOnOffKeyReleased();
-  Device::Power::standbyConfiguration();
-  Device::Board::shutdown();
+/* We isolate the standby code that needs to be executed from the internal
+ * flash (because the external flash is then shut down). We forbid inlining to
+ * avoid inlining these instructions in the external flash. */
+
+void __attribute__((noinline)) internal_flash_standby() {
+  Device::ExternalFlash::shutdown();
+  Device::Board::shutdownClocks();
   Device::Power::enterLowPowerMode();
   Device::Reset::core();
+}
+
+void standby() {
+  Device::Power::waitUntilOnOffKeyReleased();
+  Device::Power::standbyConfiguration();
+  Device::Board::shutdownPeripherals();
+  internal_flash_standby();
 }
 
 }

a931694ed0e583fb71e4091c09380d1a9f468bf2
Émilie Feral <emilie.feral@numworks.com>
2019-05-09 10:49:32 +0200

    [on-boarding] PowerOnSelfTest: test VBlankOK and FastLCDDataOK before testing BatteryOK

--- apps/on_boarding/power_on_self_test.cpp
+++ apps/on_boarding/power_on_self_test.cpp
@@ -6,16 +6,15 @@ namespace OnBoarding {
 
 KDColor PowerOnSelfTest::Perform() {
   KDColor previousLEDColor = Ion::LED::getColor();
-  if (!Shared::POSTAndHardwareTests::BatteryOK()) {
-    Ion::LED::setColor(KDColorRed);
-  } else {
-    Ion::LED::setColor(KDColorBlue);
+  Ion::LED::setColor(KDColorBlue);
+  if (Shared::POSTAndHardwareTests::VBlankOK()
+        && Shared::POSTAndHardwareTests::FastLCDDataOK()) {
     /* If VBlank test fails, we end up in an infinite loop and the LED will be
      * lit up in blue. */
-    if (Shared::POSTAndHardwareTests::VBlankOK()
-        && Shared::POSTAndHardwareTests::FastLCDDataOK())
-    {
+    if (Shared::POSTAndHardwareTests::BatteryOK()) {
       Ion::LED::setColor(KDColorGreen);
+    } else {
+      Ion::LED::setColor(KDColorRed);
     }
   }
   return previousLEDColor;

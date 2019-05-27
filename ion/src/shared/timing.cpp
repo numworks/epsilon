9b81735212d09c08c6a34d29e7efce042d7a833b
Émilie Feral <emilie.feral@numworks.com>
2019-05-27 09:29:55 -0400

    [ion] Fix simulator build

--- ion/src/shared/timing.cpp
+++ ion/src/shared/timing.cpp
@@ -3,7 +3,7 @@
 
 static auto start = std::chrono::steady_clock::now();
 
-uint64_t Ion::Timing::millis() {
+volatile uint64_t Ion::Timing::millis() {
   auto elapsed = std::chrono::steady_clock::now() - start;
   return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
 }

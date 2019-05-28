4f9b766bf94020b4b62dae966db97becce15ad30
Léa Saviot <lea.saviot@numworks.com>
2019-05-28 16:49:56 +0200

    [apps/POSTAndHWTest] In LCDDataOK iterate on Text and Tiling tests

--- apps/on_boarding/power_on_self_test.cpp
+++ apps/on_boarding/power_on_self_test.cpp
@@ -8,11 +8,14 @@ KDColor PowerOnSelfTest::Perform() {
   KDColor previousLEDColor = Ion::LED::getColor();
   KDColor resultColor = KDColorGreen;
 
-  // Screen tests
+  /* Screen tests
+   * CAUTION: Timing is important. TextLCDTestOK fails only if done at an
+   * unknown time, which happens to be for VBlankOK being done just before.
+   * TextLCDTestOK is done many times in a row in the HardwareTest, so if the
+   * screen passes the POST and fails the Hardware Test, we will need to find
+   * the right time to sleep here before launching TextLCDTestOK. */
   bool screenTestsOK = Shared::POSTAndHardwareTests::VBlankOK() && Shared::POSTAndHardwareTests::TextLCDTestOK();
-
   // We push a white screen so that the LCD Data test is invisible for the user.
-  Ion::Display::waitForVBlank();
   Ion::Display::pushRectUniform(KDRect(0, 0, Ion::Display::Width, Ion::Display::Height), KDColorWhite);
   Ion::Display::waitForVBlank();
 

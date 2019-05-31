8d21fbea7b02f3744f354882ad6abcb075729151
Léa Saviot <lea.saviot@numworks.com>
2019-05-31 15:33:26 +0200

    [ion] PrintInt::Left and PrintInt::Right
    Right pads the left of the integer with zeroes.

--- ion/src/device/bench/command/screen_id.cpp
+++ ion/src/device/bench/command/screen_id.cpp
@@ -15,12 +15,12 @@ void ScreenID(const char * input) {
   }
   uint32_t screenID = Ion::Device::Display::panelIdentifier();
 
-  constexpr int bufferSize = 20;
+  constexpr int bufferSize = 10 + 1; // screenID is a uint32_t so 10 digits max
   char buffer[bufferSize];
   for (int i = 0; i < bufferSize; i++) {
     buffer[i] = 0;
   }
-  Poincare::PrintInt::PadIntInBuffer(screenID, buffer, bufferSize - 1);
+  Poincare::PrintInt::Left(screenID, buffer, bufferSize - 1);
   reply(buffer);
 }
 

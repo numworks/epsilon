7ec97ff6e65aa8eb7482175884fe76a79f101a96
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-06-06 17:38:01 +0200

    [apps] Define LayoutFieldDelegate::expressionFieldDelegateApp() directly from ::app()

--- apps/shared/layout_field_delegate.h
+++ apps/shared/layout_field_delegate.h
@@ -1,7 +1,6 @@
 #ifndef SHARED_LAYOUT_FIELD_DELEGATE_H
 #define SHARED_LAYOUT_FIELD_DELEGATE_H
 
-#include <escher/layout_field_delegate.h>
 #include "expression_field_delegate_app.h"
 
 namespace Shared {
@@ -14,7 +13,9 @@ public:
   bool layoutFieldDidAbortEditing(LayoutField * layoutField) override;
   void layoutFieldDidChangeSize(LayoutField * layoutField) override;
 private:
-  virtual ExpressionFieldDelegateApp * expressionFieldDelegateApp() = 0;
+  ExpressionFieldDelegateApp * expressionFieldDelegateApp() {
+    return static_cast<ExpressionFieldDelegateApp *>(::app());
+  }
 };
 
 }

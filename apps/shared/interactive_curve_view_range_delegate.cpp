f30860b096ca18ebc37f07b0cc0a20ffaef92a41
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-06-12 11:56:26 +0200

    [apps/interactive_curve_view_range_delegate] Remove redundant checks

--- apps/shared/interactive_curve_view_range_delegate.cpp
+++ apps/shared/interactive_curve_view_range_delegate.cpp
@@ -22,28 +22,11 @@ bool InteractiveCurveViewRangeDelegate::didChangeRange(InteractiveCurveViewRange
   if (interactiveCurveViewRange->yMin() == addMargin(min, range, true) && interactiveCurveViewRange->yMax() == addMargin(max, range, false)) {
     return false;
   }
-  if (min == max) {
-    float step = max != 0.0f ? interactiveCurveViewRange->computeGridUnit(CurveViewRange::Axis::Y, max) : 1.0f;
-    min = min - step;
-    max = max + step;
-  }
   if (min == FLT_MAX && max == -FLT_MAX) {
     min = -1.0f;
     max = 1.0f;
   }
-  if (min == FLT_MAX) {
-    float step = max != 0.0f ? interactiveCurveViewRange->computeGridUnit(CurveViewRange::Axis::Y, std::fabs(max)) : 1.0f;
-    min = max-step;
-  }
-  if (max == -FLT_MAX) {
-    float step = min != 0.0f ? interactiveCurveViewRange->computeGridUnit(CurveViewRange::Axis::Y, std::fabs(min)) : 1.0f;
-    max = min+step;
-  }
   range = max - min;
-  if (range < InteractiveCurveViewRange::k_minFloat) {
-    max += InteractiveCurveViewRange::k_minFloat;
-    min -= InteractiveCurveViewRange::k_minFloat;
-  }
   interactiveCurveViewRange->setYMin(addMargin(min, range, true));
   interactiveCurveViewRange->setYMax(addMargin(max, range, false));
   if (std::isinf(interactiveCurveViewRange->xMin())) {

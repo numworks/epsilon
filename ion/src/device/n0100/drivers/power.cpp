b9bed1fd15cb4e621e14697cf99e986733def06d
Émilie Feral <emilie.feral@numworks.com>
2019-06-19 08:53:17 -0400

    [ion] Power: add a comment to explain why standby use stopConfiguration on N0100

--- ion/src/device/n0100/drivers/power.cpp
+++ ion/src/device/n0100/drivers/power.cpp
@@ -7,6 +7,10 @@ namespace Power {
 
 void standby() {
   Device::Power::waitUntilOnOffKeyReleased();
+  /* N0100 cannot use standby mode because the power key is not correctly wired
+   * to a wake up pin in order to trigger a wake up event. If the device is in
+   * standby, we won't be able to wake it up. Instead, we use the stop mode (as
+   * it is the low power mode consuming less after standby mode). */
   Device::Power::stopConfiguration();
   Device::Board::shutdownPeripherals();
   Device::WakeUp::onOnOffKeyDown();

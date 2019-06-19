45b7274fca01c16de1cd778791f61240fc82453a
Léa Saviot <lea.saviot@numworks.com>
2019-06-19 09:59:02 +0200

    [ion/n0100] Fix updateColorWithPlugAndCharge
    Make sure not to erase the exam mode led color

--- ion/src/device/n0100/drivers/led.cpp
+++ ion/src/device/n0100/drivers/led.cpp
@@ -1,6 +1,4 @@
 #include <ion/led.h>
-#include <ion/battery.h>
-#include <ion/usb.h>
 
 namespace Ion {
 namespace LED {
@@ -8,7 +6,7 @@ namespace LED {
 KDColor updateColorWithPlugAndCharge() {
   /* Prevent updating LED color with charging status as the end of charge
    * current is never detected on N0100 model. */
-  return KDColorBlack;
+  return getColor();
 }
 
 }

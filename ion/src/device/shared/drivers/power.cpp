bd5d94ba6ccfc954dc6fefcf19ac96255042bcc4
Émilie Feral <emilie.feral@numworks.com>
2019-06-19 09:10:58 -0400

    [ion] Discard old comment

--- ion/src/device/shared/drivers/power.cpp
+++ ion/src/device/shared/drivers/power.cpp
@@ -132,7 +132,6 @@ void configWakeUp() {
 }
 
 void stopConfiguration() {
-  // This is done differently on the models
   PWR.CR()->setMRUDS(true); // Main regulator in Low Voltage and Flash memory in Deep Sleep mode when the device is in Stop mode
   PWR.CR()->setLPUDS(true); // Low-power regulator in under-drive mode if LPDS bit is set and Flash memory in power-down when the device is in Stop under-drive mode
   PWR.CR()->setLPDS(true); // Low-power Voltage regulator on. Takes longer to wake up.

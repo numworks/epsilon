e507c22d84a9531be98bd27ff2bc762d31025c1e
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-06-26 10:05:40 +0200

    [apps/graph/list] Add TypeParameterController to ListParameterController

--- apps/graph/list/list_parameter_controller.h
+++ apps/graph/list/list_parameter_controller.h
@@ -2,6 +2,7 @@
 #define GRAPH_LIST_LIST_PARAM_CONTROLLER_H
 
 #include <apps/shared/list_parameter_controller.h>
+#include "type_parameter_controller.h"
 
 namespace Graph {
 
@@ -12,18 +13,23 @@ public:
   ListParameterController(ListController * listController, Responder * parentResponder, I18n::Message functionColorMessage, I18n::Message deleteFunctionMessage, SelectableTableViewDelegate * tableDelegate = nullptr) :
     Shared::ListParameterController(parentResponder, functionColorMessage, deleteFunctionMessage, tableDelegate),
     m_listController(listController),
+    m_typeCell(),
+    m_typeParameterController(this),
     m_renameCell(I18n::Message::Rename)
   {}
   // ListViewDataSource
   HighlightCell * reusableCell(int index, int type) override;
+  void willDisplayCellForIndex(HighlightCell * cell, int index) override;
 protected:
   bool handleEnterOnRow(int rowIndex) override;
 private:
   int totalNumberOfCells() const override {
-    return Shared::ListParameterController::totalNumberOfCells() + 1;
+    return Shared::ListParameterController::totalNumberOfCells() + 2;
   }
   void renameFunction();
   ListController * m_listController;
+  MessageTableCellWithChevronAndMessage m_typeCell;
+  TypeParameterController m_typeParameterController;
   MessageTableCell m_renameCell;
 };
 

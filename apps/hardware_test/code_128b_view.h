e5e880f8e38d735d97b1873e5b367ec123b0b051
Léa Saviot <lea.saviot@numworks.com>
2019-07-03 10:28:22 +0200

    [apps/hardware_test] Centered screen test done at serial number view

--- apps/hardware_test/code_128b_view.h
+++ apps/hardware_test/code_128b_view.h
@@ -12,11 +12,14 @@ public:
   void setData(const char * data);
   void layoutSubviews() override;
 private:
-  static constexpr KDCoordinate charPatternWidth = 11;
+  static constexpr KDCoordinate k_outlineThickness = 1;
+  static constexpr KDCoordinate k_charPatternWidth = 11;
+  static constexpr KDCoordinate k_stringOffset = 3;
+  static constexpr KDColor k_borderColor = KDColorRed;
   void updateModuleWidth();
   int checksum() const;
   void drawQuietZoneAt(KDContext * ctx, KDCoordinate * x) const;
-  void drawPatternAt(KDContext * ctx, uint16_t pattern, KDCoordinate * x, KDCoordinate width = charPatternWidth) const;
+  void drawPatternAt(KDContext * ctx, uint16_t pattern, KDCoordinate * x, KDCoordinate width = k_charPatternWidth) const;
   void drawCharAt(KDContext * ctx, char c, KDCoordinate * x) const;
   int m_moduleWidth;
   const char * m_data;
--- apps/hardware_test/dead_pixels_test_controller.h
+++ apps/hardware_test/dead_pixels_test_controller.h
@@ -11,13 +11,13 @@ public:
   DeadPixelsTestController(Responder * parentResponder) :
     ViewController(parentResponder),
     m_colorIndex(0),
-    m_view(KDColorRed)
+    m_view(KDColorBlack)
   {}
   View * view() override { return &m_view; }
   bool handleEvent(Ion::Events::Event event) override;
 private:
-  constexpr static int k_numberOfColors = 4;
-  constexpr static KDColor k_colors[k_numberOfColors - 1] = {KDColorBlue, KDColorGreen, KDColorWhite}; // KDColorRed is the first color, set in the constructor
+  constexpr static int k_numberOfAdditionalColors = 4;
+  constexpr static KDColor k_colors[k_numberOfAdditionalColors] = {KDColorRed, KDColorBlue, KDColorGreen, KDColorWhite}; // KDColorBlack is the first color, set in the constructor
   int m_colorIndex;
   SolidColorView m_view;
 };

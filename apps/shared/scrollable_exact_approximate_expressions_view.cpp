05678b48f613d96f295b19b7c859f3fe19be1534
Léa Saviot <lea.saviot@numworks.com>
2019-07-26 15:48:26 +0200

    [apps/calc] Remove whole calculation memoization in HistoryViewCell
    It is no longer compatible with the new calculations as they no longer
    have a constant size.

--- apps/shared/scrollable_exact_approximate_expressions_view.cpp
+++ apps/shared/scrollable_exact_approximate_expressions_view.cpp
@@ -11,7 +11,8 @@ ScrollableExactApproximateExpressionsView::ContentCell::ContentCell() :
   m_rightExpressionView(),
   m_approximateSign(KDFont::LargeFont, I18n::Message::AlmostEqual, 0.5f, 0.5f, Palette::GreyVeryDark),
   m_leftExpressionView(),
-  m_selectedSubviewPosition((SubviewPosition)0)
+  m_selectedSubviewPosition((SubviewPosition)0),
+  m_displayLeftExpression(true)
 {
 }
 
@@ -77,7 +78,7 @@ Poincare::Layout ScrollableExactApproximateExpressionsView::ContentCell::layout(
 }
 
 int ScrollableExactApproximateExpressionsView::ContentCell::numberOfSubviews() const {
-  if (m_leftExpressionView.layout().isUninitialized()) {
+  if (!m_displayLeftExpression || m_leftExpressionView.layout().isUninitialized()) {
     return 1;
   }
   return 3;

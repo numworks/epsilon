e670d6c5cee727c9fa698ea65dbe0ca121a0e4dc
Émilie Feral <emilie.feral@numworks.com>
2019-08-01 15:42:48 +0200

    [poincare] Expression: fix deepBeautify

--- poincare/src/expression.cpp
+++ poincare/src/expression.cpp
@@ -610,7 +610,7 @@ Expression Expression::deepBeautify(ExpressionNode::ReductionContext reductionCo
   int nbChildren = e.numberOfChildren();
   for (int i = 0; i < nbChildren; i++) {
     Expression child = e.childAtIndex(i);
-    child.deepBeautify(reductionContext);
+    child = child.deepBeautify(reductionContext);
     // We add missing Parentheses after beautifying the parent and child
     if (e.node()->childNeedsUserParentheses(child)) {
       e.replaceChildAtIndexInPlace(i, Parenthesis::Builder(child));

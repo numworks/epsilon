f7c0dadd432459a5bef4b50949d42c443e085812
Léa Saviot <lea.saviot@numworks.com>
2019-08-06 10:50:57 +0200

    [poincare/print_int] Returns the wanted length

--- poincare/src/print_int.cpp
+++ poincare/src/print_int.cpp
@@ -4,46 +4,31 @@
 
 namespace Poincare {
 
-bool PrintIntInBuffer(uint32_t integer, char * buffer, int bufferLength, bool left) {
-  assert(integer >= 0);
-  if (bufferLength == 0) {
-    return false;
-  }
-
-  // Case integer is 0
-  if (integer == 0) {
-    if (left) {
-      buffer[0] = '0';
-    } else {
-      for (int i = 0; i < bufferLength; i++) {
-        buffer[i] = '0';
-      }
-    }
-    return true;
-  }
-
+int PrintIntInBuffer(uint32_t integer, char * buffer, int bufferLength, bool left) {
   int wantedLength = std::log10(integer*1.0)+1;
-  if (wantedLength > bufferLength) {
-    return false;
+  assert(wantedLength > 0);
+  if (bufferLength == 0 || wantedLength > bufferLength) {
+    return wantedLength;
   }
-  int modulo = 10;
-  int startIndex = left ? wantedLength - 1 : bufferLength - 1;
-  for (int i = startIndex; i >= 0; i--) {
-    int c = integer % modulo;
-    buffer[i] = '0' + c;
-    integer = (integer - c) / modulo;
+
+  uint32_t modulo = 10;
+  uint32_t startIndexPlusOne = left ? wantedLength : bufferLength;
+  for (int i = startIndexPlusOne; i > 0; i--) {
     /* If the integer is written on the right, after all digits are written
      * integer is equal to 0 and the buffer is padded with 0 on the left. */
+    uint32_t c = integer % modulo;
+    buffer[i-1] = '0' + c;
+    integer = (integer - c) / modulo;
   }
-  return true;
+  return wantedLength;
 }
 
 
-bool PrintInt::Left(uint32_t integer, char * buffer, int bufferLength) {
+int PrintInt::Left(uint32_t integer, char * buffer, int bufferLength) {
   return PrintIntInBuffer(integer, buffer, bufferLength, true);
 }
 
-bool PrintInt::Right(uint32_t integer, char * buffer, int bufferLength) {
+int PrintInt::Right(uint32_t integer, char * buffer, int bufferLength) {
   return PrintIntInBuffer(integer, buffer, bufferLength, false);
 }
 

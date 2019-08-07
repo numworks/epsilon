76bce37061b2ef484da5d522881ba76f110ebe8a
Léa Saviot <lea.saviot@numworks.com>
2019-08-07 10:56:13 +0200

    [apps/post_and_hardware_tests] Comment display tests

--- apps/shared/post_and_hardware_tests.h
+++ apps/shared/post_and_hardware_tests.h
@@ -6,14 +6,19 @@
 
 namespace Shared {
 
-// TODO LEA: comment about ColorsLCDPixelFailures and detected screen issues
 class POSTAndHardwareTests {
 public:
+  // Tests if battery is Full
   static bool BatteryOK();
+  // Tests if VBlank signal is received
   static bool VBlankOK();
+  // Tests if tiling of different colors is displayed OK
   static int LCDDataGlyphFailures();
+  // Tests if pixels sent fast are displayed OK
   static int LCDTimingGlyphFailures();
+  // Tests if uniform tiling is displayed OK
   static int ColorsLCDPixelFailures();
+  // Tests if text is displayed OK
   static int TextLCDGlyphFailures();
 private:
   static constexpr int k_stampSize = 8;

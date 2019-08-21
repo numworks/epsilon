cb2ad7bd5803ebbbe6d63077c66ef41512b198d0
Léa Saviot <lea.saviot@numworks.com>
2019-08-21 16:56:33 +0200

    [poincare/hyperbolic_funcitons] Some hyperbolic func simplifications

--- poincare/src/hyperbolic_trigonometric_function.cpp
+++ poincare/src/hyperbolic_trigonometric_function.cpp
@@ -1,5 +1,6 @@
 #include <poincare/hyperbolic_trigonometric_function.h>
-
+#include <poincare/multiplication.h>
+#include <poincare/rational.h>
 
 namespace Poincare {
 
@@ -14,9 +15,101 @@ Expression HyperbolicTrigonometricFunction::shallowReduce(ExpressionNode::Reduct
       return e;
     }
   }
+
+  Expression thisExpression = *this;
+
+  // Step 1. Map on matrix child if possible
   Expression c = childAtIndex(0);
   if (childAtIndex(0).type() == ExpressionNode::Type::Matrix) {
     return mapOnMatrixFirstChild(reductionContext);
+  }
+
+  // Step 2. TODO EMILIE ?? Try finding an easy standard calculation reduction
+
+  // Step 3. Look for an expression of type "cosh(acosh(z))", return z
+  // TODO LEA OK ? Definition domains
+  ExpressionNode::Type t = type();
+  ExpressionNode::Type childT = childAtIndex(0).type();
+  {
+    Expression result;
+    if (t == ExpressionNode::Type::HyperbolicCosine) {
+      if (childT == ExpressionNode::Type::HyperbolicArcCosine) {
+        Expression e = childAtIndex(0).childAtIndex(0);
+        if (reductionContext.complexFormat() != Preferences::ComplexFormat::Real
+            || (e.isReal(reductionContext.context())
+              && e.approximateToScalar<double>(
+                reductionContext.context(),
+                reductionContext.complexFormat(),
+                reductionContext.angleUnit()) >= 1.0))
+        {
+          result = e;
+        }
+      }
+    } else if (t == ExpressionNode::Type::HyperbolicArcCosine) {
+      if (childT == ExpressionNode::Type::HyperbolicCosine) {
+        Expression e = childAtIndex(0).childAtIndex(0);
+        if (reductionContext.complexFormat() != Preferences::ComplexFormat::Real
+            || (e.isReal(reductionContext.context())
+              && e.approximateToScalar<double>(
+                reductionContext.context(),
+                reductionContext.complexFormat(),
+                reductionContext.angleUnit()) >= 0.0))
+        {
+          result = e;
+        }
+      }
+    } else if (t == ExpressionNode::Type::HyperbolicSine) {
+      if (childT == ExpressionNode::Type::HyperbolicArcSine) {
+        result = childAtIndex(0).childAtIndex(0);
+      }
+    } else if (t == ExpressionNode::Type::HyperbolicArcSine) {
+      if (childT == ExpressionNode::Type::HyperbolicSine) {
+        result = childAtIndex(0).childAtIndex(0);
+      }
+    } else if (t == ExpressionNode::Type::HyperbolicTangent) {
+      if (childT == ExpressionNode::Type::HyperbolicArcTangent) {
+        Expression e = childAtIndex(0).childAtIndex(0);
+        if (reductionContext.complexFormat() != Preferences::ComplexFormat::Real
+            || (e.isReal(reductionContext.context())
+              && std::fabs(e.approximateToScalar<double>(
+                reductionContext.context(),
+                reductionContext.complexFormat(),
+                reductionContext.angleUnit())) < 1.0))
+        {
+          result = e;
+        }
+      }
+    } else {
+      assert(t == ExpressionNode::Type::HyperbolicArcTangent);
+      if (childT == ExpressionNode::Type::HyperbolicTangent) {
+        result = childAtIndex(0).childAtIndex(0);
+      }
+    }
+    if (!result.isUninitialized()) {
+      replaceWithInPlace(result);
+      return result;
+    }
+  }
+
+  // Step 4. Look for an expression of type "cosh(-x)", return "+/-cosh(x)"
+  if (t != ExpressionNode::Type::HyperbolicArcCosine) {
+    Expression positiveArg = childAtIndex(0).makePositiveAnyNegativeNumeralFactor(reductionContext);
+    if (!positiveArg.isUninitialized()) {
+      // The argument was of form cosh(-a)
+      if (t == ExpressionNode::Type::HyperbolicCosine) {
+        // cosh(-a) = cosh(a)
+        return shallowReduce(reductionContext);
+      } else {
+        /* sinh(-a) = -sinh(a) or tanh(-a) = -tanh(a) or arcsinh(-a) = -arcsinh(a)
+         * or arctanh(-a) = -arctanh(a) */
+        assert(t == ExpressionNode::Type::HyperbolicSine || t == ExpressionNode::Type::HyperbolicArcSine || t == ExpressionNode::Type::HyperbolicTangent || t == ExpressionNode::Type::HyperbolicArcTangent);
+        Multiplication m = Multiplication::Builder(Rational::Builder(-1));
+        thisExpression.replaceWithInPlace(m);
+        m.addChildAtIndexInPlace(thisExpression, 1, 1);
+        thisExpression.shallowReduce(reductionContext);
+        return m.shallowReduce(reductionContext);
+      }
+    }
   }
   return *this;
 }

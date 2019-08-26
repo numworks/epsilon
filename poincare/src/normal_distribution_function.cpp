337173c326957e84e004d77abd24e4d544dffb85
Léa Saviot <lea.saviot@numworks.com>
2019-08-26 10:44:19 +0200

    [poincare/normal_distribution_function] Factorize code in parent class

---
+++ poincare/src/normal_distribution_function.cpp
@@ -0,0 +1,41 @@
+#include <poincare/normal_distribution_function.h>
+#include <poincare/normal_distribution.h>
+#include <assert.h>
+
+namespace Poincare {
+
+Expression NormalDistributionFunctionNode::shallowReduce(ReductionContext reductionContext) {
+  return NormalDistributionFunction(this).shallowReduce(reductionContext.context());
+}
+
+Expression NormalDistributionFunction::shallowReduce(Context * context, bool * stopReduction) {
+  if (stopReduction != nullptr) {
+    *stopReduction = true;
+  }
+  {
+    Expression e = Expression::defaultShallowReduce();
+    if (e.isUndefined()) {
+      return e;
+    }
+  }
+
+  Expression mu = childAtIndex(1);
+  Expression var = childAtIndex(2);
+
+  // Check mu and var
+  bool muAndVarOK = false;
+  bool couldCheckMuAndVar = NormalDistribution::ExpressionParametersAreOK(&muAndVarOK, mu, var, context);
+  if (!couldCheckMuAndVar) {
+    return *this;
+  }
+  if (!muAndVarOK) {
+    return replaceWithUndefinedInPlace();
+  }
+
+  if (stopReduction != nullptr) {
+    *stopReduction = false;
+  }
+  return *this;
+}
+
+}

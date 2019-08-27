4aa380516a6404e4946b3f131799d4e1eff40b13
Léa Saviot <lea.saviot@numworks.com>
2019-08-27 15:57:25 +0200

    [poincare] VariableContext can only contain a Symbol, not a function
    In the solver app, the equation x(x+1)(x+2)(x+3) would have the roots 0,
    -2 and -3 because the parsed function "x applied to x+1" would be replaced
    by the value 0 for the symbol x during the root finding algorithm.

--- poincare/src/variable_context.cpp
+++ poincare/src/variable_context.cpp
@@ -1,6 +1,7 @@
 #include <poincare/variable_context.h>
 #include <poincare/preferences.h>
 #include <poincare/symbol.h>
+#include <poincare/undefined.h>
 
 #include <cmath>
 
@@ -20,6 +21,7 @@ void VariableContext::setApproximationForVariable(T value) {
 
 void VariableContext::setExpressionForSymbolAbstract(const Expression & expression, const SymbolAbstract & symbol, Context * context) {
   if (strcmp(symbol.name(), m_name) == 0) {
+    assert(symbol.type() == ExpressionNode::Type::Symbol);
     if (expression.isUninitialized()) {
       return;
     }
@@ -31,7 +33,10 @@ void VariableContext::setExpressionForSymbolAbstract(const Expression & expressi
 
 const Expression VariableContext::expressionForSymbolAbstract(const SymbolAbstract & symbol, bool clone) {
   if (strcmp(symbol.name(), m_name) == 0) {
-    return clone ? m_value.clone() : m_value;
+    if (symbol.type() == ExpressionNode::Type::Symbol) {
+      return clone ? m_value.clone() : m_value;
+    }
+    return Undefined::Builder();
   } else {
     return m_parentContext->expressionForSymbolAbstract(symbol, clone);
   }

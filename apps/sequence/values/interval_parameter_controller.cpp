7ea69a12464d36993b63cc328cdfef41387b2950
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-09-03 14:06:42 +0200

    [apps/sequence/values] IntervalParameterController constructor is inherited from base class

--- apps/sequence/values/interval_parameter_controller.cpp
+++ apps/sequence/values/interval_parameter_controller.cpp
@@ -5,11 +5,6 @@ using namespace Shared;
 
 namespace Sequence {
 
-IntervalParameterController::IntervalParameterController(Responder * parentResponder, InputEventHandlerDelegate * inputEventHandlerDelegate, Interval * interval) :
-  Shared::IntervalParameterController(parentResponder, inputEventHandlerDelegate, interval)
-{
-}
-
 bool IntervalParameterController::setParameterAtIndex(int parameterIndex, double f) {
   if (f < 0) {
     Container::activeApp()->displayWarning(I18n::Message::ForbiddenValue);

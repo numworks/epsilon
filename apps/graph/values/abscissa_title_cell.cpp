b4e4cbe9ade4992b3e6adb169c9917f806652a6b
Léa Saviot <lea.saviot@numworks.com>
2019-09-09 18:28:16 +0200

    [apps/shared] Separable class

--- apps/graph/values/abscissa_title_cell.cpp
+++ apps/graph/values/abscissa_title_cell.cpp
@@ -1,26 +1,23 @@
 #include "abscissa_title_cell.h"
 #include <apps/shared/hideable_even_odd_editable_text_cell.h>
-#include "escher/metric.h"
 
 namespace Graph {
-
-void AbscissaTitleCell::setSeparatorLeft(bool separator) {
-  if (m_separatorLeft != separator) {
-    m_separatorLeft = separator;
-    reloadCell();
-  }
-}
 
 void AbscissaTitleCell::drawRect(KDContext * ctx, KDRect rect) const {
   EvenOddMessageTextCell::drawRect(ctx, rect);
   // Draw the separator
-  KDRect separatorRect(0, 0, Metric::TableSeparatorThickness, bounds().height());
-  ctx->fillRect(separatorRect, m_separatorLeft ? Shared::HideableEvenOddEditableTextCell::hideColor() : backgroundColor());
+  if (m_separatorLeft) {
+    KDRect r = separatorRect(bounds());
+    ctx->fillRect(r, Shared::HideableEvenOddEditableTextCell::hideColor());
+  }
 }
 
 void AbscissaTitleCell::layoutSubviews() {
-  KDRect rect = bounds();
-  m_messageTextView.setFrame(KDRect(rect.left() + (m_separatorLeft ? Metric::TableSeparatorThickness : 0), rect.top(), rect.width() - (m_separatorLeft ? Metric::TableSeparatorThickness : 0), rect.height()));
+  m_messageTextView.setFrame(rectWithoutSeparator(bounds()));
+}
+
+void AbscissaTitleCell::didSetSeparator() {
+  reloadCell();
 }
 
 }

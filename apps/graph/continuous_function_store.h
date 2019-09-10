0c5235930984417267f5d564b8fd37bf862a6d2e
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-09-10 11:50:49 +0200

    [apps/shared/continuous_function_store] Factor numberOfActiveFunctionsOfType and activeRecordOfTypeAtIndex

--- apps/graph/continuous_function_store.h
+++ apps/graph/continuous_function_store.h
@@ -8,14 +8,22 @@ namespace Graph {
 
 class ContinuousFunctionStore : public Shared::FunctionStore {
 public:
+  int numberOfActiveFunctionsOfType(Shared::ContinuousFunction::PlotType plotType) const {
+    return numberOfModelsSatisfyingTest(&isFunctionActiveOfType, &plotType);
+  }
+  Ion::Storage::Record activeRecordOfTypeAtIndex(Shared::ContinuousFunction::PlotType plotType, int i) const {
+    return recordSatisfyingTestAtIndex(i, &isFunctionActiveOfType, &plotType);
+  }
   Shared::ExpiringPointer<Shared::ContinuousFunction> modelForRecord(Ion::Storage::Record record) const { return Shared::ExpiringPointer<Shared::ContinuousFunction>(static_cast<Shared::ContinuousFunction *>(privateModelForRecord(record))); }
-  int numberOfActiveFunctionsOfType(Shared::ContinuousFunction::PlotType plotType) const;
-  Ion::Storage::Record activeRecordOfTypeAtIndex(Shared::ContinuousFunction::PlotType plotType, int index) const;
 private:
   Ion::Storage::Record::ErrorStatus addEmptyModel() override;
   const char * modelExtension() const override { return Ion::Storage::funcExtension; }
   Shared::ExpressionModelHandle * setMemoizedModelAtIndex(int cacheIndex, Ion::Storage::Record record) const override;
   Shared::ExpressionModelHandle * memoizedModelAtIndex(int cacheIndex) const override;
+  static bool isFunctionActiveOfType(Shared::ExpressionModelHandle * model, void * context) {
+    Shared::ContinuousFunction::PlotType plotType = *static_cast<Shared::ContinuousFunction::PlotType *>(context);
+    return isFunctionActive(model, context) && plotType == static_cast<Shared::ContinuousFunction *>(model)->plotType();
+  }
   mutable Shared::ContinuousFunction m_functions[k_maxNumberOfMemoizedModels];
 };
 

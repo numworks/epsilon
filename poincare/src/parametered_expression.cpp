22893f215a69ef4f4ab9366f67253c514b9baffe
Ruben Dashyan <ruben.dashyan@numworks.com>
2019-09-13 16:09:50 +0200

    [poincare/parametered_expression] Turn replaceUnknown into replaceSymbolWithExpression
    That is the only place where those methods did differ.

--- poincare/src/parametered_expression.cpp
+++ poincare/src/parametered_expression.cpp
@@ -5,26 +5,27 @@
 
 namespace Poincare {
 
-Expression ParameteredExpressionNode::replaceUnknown(const Symbol & symbol, const Symbol & unknownSymbol) {
-  return ParameteredExpression(this).replaceUnknown(symbol, unknownSymbol);
+Expression ParameteredExpressionNode::replaceSymbolWithExpression(const SymbolAbstract & symbol, const Expression & expression) {
+  return ParameteredExpression(this).replaceSymbolWithExpression(symbol, expression);
 }
 
-Expression ParameteredExpression::replaceUnknown(const Symbol & symbolToReplace, const Symbol & unknownSymbol) {
+Expression ParameteredExpression::replaceSymbolWithExpression(const SymbolAbstract & symbol, const Expression & expression) {
   Expression c = childAtIndex(ParameterChildIndex());
   assert(c.type() == ExpressionNode::Type::Symbol);
   Symbol& parameterChild = static_cast<Symbol &>(c);
-  if (strcmp(parameterChild.name(), symbolToReplace.name()) != 0) {
-    // If the unknown is not the parameter, replace normally
-    return defaultReplaceSymbolWithExpression(symbolToReplace, unknownSymbol);
+  if (symbol.type() != ExpressionNode::Type::Symbol ||
+      strcmp(symbol.name(), parameterChild.name()) != 0) {
+    // If the symbol is not the parameter, replace normally
+    return defaultReplaceSymbolWithExpression(symbol, expression);
   }
 
-  /* If the unknown is the parameter, replace the unknown in all children except
+  /* If the symbol is the parameter, replace it in all children except
    * in the parameter and the parametered children */
   int childrenCount = numberOfChildren();
   static_assert(ParameteredChildIndex() == 0 && ParameterChildIndex() == 1,
-      "ParameteredExpression::replaceUnknown might not be valid");
+      "ParameteredExpression::replaceSymbolWithExpression might not be valid");
   for (int i = 2; i < childrenCount; i++) {
-    childAtIndex(i).replaceUnknown(symbolToReplace, unknownSymbol);
+    childAtIndex(i).replaceSymbolWithExpression(symbol, expression);
   }
   return *this;
 }

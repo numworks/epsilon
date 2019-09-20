7eabf903ed23c18c22cc1b95d4947dc82711897f
Émilie Feral <emilie.feral@numworks.com>
2019-09-20 12:00:51 +0200

    [apps/shared] Re-implement Sequence cursor

--- apps/shared/cursor_view.h
+++ apps/shared/cursor_view.h
@@ -8,6 +8,10 @@ namespace Shared {
 class CursorView : public View {
 public:
   virtual void setCursorFrame(KDRect frame) { View::setFrame(frame); }
+  void drawRect(KDContext * ctx, KDRect rect) const override;
+  KDSize minimalSizeForOptimalDisplay() const override;
+private:
+  constexpr static KDCoordinate k_size = 19;
 };
 
 }

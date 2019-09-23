72fb43ffa8c66ea038134bc9319d8330fab9240c
Léa Saviot <lea.saviot@numworks.com>
2019-09-23 11:32:54 +0200

    [apps/graph] Block navigation to definition domain

--- apps/graph/graph/graph_controller_helper.cpp
+++ apps/graph/graph/graph_controller_helper.cpp
@@ -9,6 +9,9 @@ using namespace Poincare;
 
 namespace Graph {
 
+static inline double minDouble(double x, double y) { return x < y ? x : y; }
+static inline double maxDouble(double x, double y) { return x > y ? x : y; }
+
 bool GraphControllerHelper::privateMoveCursorHorizontally(Shared::CurveViewCursor * cursor, int direction, Shared::InteractiveCurveViewRange * range, int numberOfStepsInGradUnit, Ion::Storage::Record record) {
   ExpiringPointer<ContinuousFunction> function = App::app()->functionStore()->modelForRecord(record);
   double tCursorPosition = cursor->t();
@@ -18,11 +21,12 @@ bool GraphControllerHelper::privateMoveCursorHorizontally(Shared::CurveViewCurso
   double dir = (direction > 0 ? 1.0 : -1.0);
   ContinuousFunction::PlotType type = function->plotType();
   if (type == ContinuousFunction::PlotType::Cartesian) {
-    t+= dir * range->xGridUnit()/numberOfStepsInGradUnit;
+    t += dir * range->xGridUnit()/numberOfStepsInGradUnit;
   } else {
     assert(type == ContinuousFunction::PlotType::Polar || type == ContinuousFunction::PlotType::Parametric);
     t += dir * (tMax-tMin)/k_definitionDomainDivisor;
   }
+  t = maxDouble(tMin, minDouble(tMax, t));
   Coordinate2D<double> xy = function->evaluateXYAtParameter(t, App::app()->localContext());
   cursor->moveTo(t, xy.x1(), xy.x2());
   return true;

9b293159196f52d7a5a3d0551dbe8ea1ab213e26
Léa Saviot <lea.saviot@numworks.com>
2019-09-23 15:45:47 +0200

    [apps/graph] jumpToLeftRightCurve takes into account vertical distance

--- apps/graph/graph/graph_controller.cpp
+++ apps/graph/graph/graph_controller.cpp
@@ -180,6 +180,8 @@ void GraphController::jumpToLeftRightCurve(double t, int direction, int function
   }
   int nextCurveIndex = -1;
   double xDelta = DBL_MAX;
+  double nextY = 0.0;
+  double nextT = 0.0;
   for (int i = 0; i < functionsCount; i++) {
     Ion::Storage::Record currentRecord = functionStore()->activeRecordAtIndex(i);
     if (currentRecord == record) {
@@ -187,7 +189,8 @@ void GraphController::jumpToLeftRightCurve(double t, int direction, int function
     }
     ExpiringPointer<CartesianFunction> f = functionStore()->modelForRecord(currentRecord);
     assert(f->plotType() == CartesianFunction::PlotType::Cartesian);
-    // Select the closest horizontal curve
+    /* Select the closest horizontal curve, then the closest vertically, then
+     * the lowest curve index. */
     double currentTMin = f->tMin();
     double currentTMax = f->tMax();
     assert(!std::isnan(currentTMin));
@@ -199,23 +202,24 @@ void GraphController::jumpToLeftRightCurve(double t, int direction, int function
         (t >= currentTMin ? 0.0 : currentTMin - t) :
         (t <= currentTMax ? 0.0 : t - currentTMax);
       assert(currentXDelta >= 0.0);
-      if (currentXDelta < xDelta) {
-        nextCurveIndex = i;
-        xDelta = currentXDelta;
+      if (currentXDelta <= xDelta) {
+        double potentialNextTMin = f->tMin();
+        double potentialNextTMax = f->tMax();
+        double potentialNextT = maxDouble(potentialNextTMin, minDouble(potentialNextTMax, t));
+        Coordinate2D<double> xy = f->evaluateXYAtParameter(potentialNextT, App::app()->localContext());
+        if (currentXDelta < xDelta || std::abs(xy.x2() - m_cursor->y()) < std::abs(nextY - m_cursor->y())) {
+          nextCurveIndex = i;
+          xDelta = currentXDelta;
+          nextY = xy.x2();
+          nextT = potentialNextT;
+        }
       }
     }
   }
   if (nextCurveIndex < 0) {
     return;
   }
-  Ion::Storage::Record currentRecord = functionStore()->activeRecordAtIndex(nextCurveIndex);
-  ExpiringPointer<ContinuousFunction> f = functionStore()->modelForRecord(currentRecord);
-
-  double nextTMin = f->tMin();
-  double nextTMax = f->tMax();
-  double nextT = maxDouble(nextTMin, minDouble(nextTMax, t));
-  Coordinate2D<double> xy = f->evaluateXYAtParameter(nextT, App::app()->localContext());
-  m_cursor->moveTo(nextT, xy.x1(), xy.x2());
+  m_cursor->moveTo(nextT, nextT, nextY);
   selectFunctionWithCursor(nextCurveIndex);
   return;
 }

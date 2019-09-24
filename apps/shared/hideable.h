6a983a5bdf0235651dfa3116e5091ee06d334813
Émilie Feral <emilie.feral@numworks.com>
2019-09-24 16:29:32 +0200

    [apps/graph] ValuesController: factorize code to hide the cells

--- apps/shared/hideable.h
+++ apps/shared/hideable.h
@@ -13,6 +13,7 @@ public:
   static KDColor hideColor() { return Palette::WallScreenDark; }
   bool hidden() const { return m_hide; }
   virtual void setHide(bool hide) { m_hide = hide; }
+  virtual void reinit() {}
 private:
   bool m_hide;
 };

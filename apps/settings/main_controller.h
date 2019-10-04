100a7fd96ecf958e836568a4c2d9b65f42d284cb
Émilie Feral <emilie.feral@numworks.com>
2019-10-04 10:40:51 +0200

    [apps/settings] MainController: get rid of magic numbers

--- apps/settings/main_controller.h
+++ apps/settings/main_controller.h
@@ -27,6 +27,19 @@ public:
   void willDisplayCellForIndex(HighlightCell * cell, int index) override;
   void viewWillAppear() override;
 private:
+  constexpr static int k_indexOfAngleUnitCell = 0;
+  constexpr static int k_indexOfDisplayModeCell = k_indexOfAngleUnitCell + 1;
+  constexpr static int k_indexOfEditionModeCell = k_indexOfDisplayModeCell + 1;
+  constexpr static int k_indexOfComplexFormatCell = k_indexOfEditionModeCell + 1;
+  constexpr static int k_indexOfBrightnessCell = k_indexOfComplexFormatCell + 1;
+  constexpr static int k_indexOfLanguageCell = k_indexOfBrightnessCell + 1;
+  constexpr static int k_indexOfExamModeCell = k_indexOfLanguageCell + 1;
+  /* Pop-up cell and About cell are located at the same index because pop-up
+   * cell is optional. We must always correct k_indexOfAboutCell with
+   * hasPrompt() (TODO: make hasPrompt() constexpr and correct
+   * k_indexOfAboutCell) */
+  constexpr static int k_indexOfPopUpCell = k_indexOfExamModeCell + 1;
+  constexpr static int k_indexOfAboutCell = k_indexOfExamModeCell + 1;
   static const SettingsMessageTree * model();
   StackViewController * stackController() const;
   I18n::Message promptMessage() const;

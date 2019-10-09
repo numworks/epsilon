0f6b099b7bddeeaa4ef5e057ea3ebd1056f31655
Léa Saviot <lea.saviot@numworks.com>
2019-10-09 17:09:30 +0200

    [ion/device/timing] Fix SysTick when changing HCLK
    There will still be some drift due to the systick counter reset

--- ion/src/device/shared/drivers/timing.cpp
+++ ion/src/device/shared/drivers/timing.cpp
@@ -18,10 +18,11 @@ void msleep(uint32_t ms) {
   // We decrease the AHB clock frequency to save power while sleeping.
   Device::Board::setClockFrequency(Device::Board::Frequency::Low);
   for (volatile uint32_t i=0; i<Config::LoopsPerMillisecond*ms; i++) {
-      __asm volatile("nop");
+    __asm volatile("nop");
   }
   Device::Board::setClockFrequency(Device::Board::standardFrequency());
 }
+
 void usleep(uint32_t us) {
   for (volatile uint32_t i=0; i<Config::LoopsPerMicrosecond*us; i++) {
     __asm volatile("nop");
@@ -44,12 +45,7 @@ using namespace Regs;
 volatile uint64_t MillisElapsed = 0;
 
 void init() {
-  /* Systick clock source is the CPU clock HCLK divided by 8. To get 1 ms
-   * systick overflow, we need to set it to :
-   * (HCLK (MHz) * 1 000 000 / 8 )/ 1000 (ms/s) = HCLK (MHz) * 1000 / 8. */
-  constexpr int SysTickPerMillisecond = Ion::Device::Clocks::Config::HCLKFrequency * 1000 / 8;
-  CORTEX.SYST_RVR()->setRELOAD(SysTickPerMillisecond - 1); // Remove 1 because the counter resets *after* counting to 0
-  CORTEX.SYST_CVR()->setCURRENT(0);
+  setSysTickFrequency(Ion::Device::Clocks::Config::HCLKFrequency);
   CORTEX.SYST_CSR()->setCLKSOURCE(CORTEX::SYST_CSR::CLKSOURCE::AHB_DIV8);
   CORTEX.SYST_CSR()->setTICKINT(true);
   CORTEX.SYST_CSR()->setENABLE(true);
@@ -58,6 +54,15 @@ void init() {
 void shutdown() {
   CORTEX.SYST_CSR()->setENABLE(false);
   CORTEX.SYST_CSR()->setTICKINT(false);
+}
+
+void setSysTickFrequency(int frequencyInMHz) {
+  /* Systick clock source is the CPU clock HCLK divided by 8. To get 1 ms
+   * systick overflow, we need to set it to :
+   * (HCLK (MHz) * 1 000 000 / 8 )/ 1000 (ms/s) = HCLK (MHz) * 1000 / 8. */
+  int SysTickPerMillisecond = frequencyInMHz * 1000 / 8;
+  CORTEX.SYST_RVR()->setRELOAD(SysTickPerMillisecond - 1); // Remove 1 because the counter resets *after* counting to 0
+  CORTEX.SYST_CVR()->setCURRENT(0); // setCURRENT will set the value to 0, no matter the argument, as per the CORTEX user guide
 }
 
 }

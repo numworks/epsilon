e7e559e080d1c7a84c3807ea5ba23173e0b954d5
Léa Saviot <lea.saviot@numworks.com>
2019-10-10 15:27:17 +0200

    [apps/code] Code cleaning

--- apps/code/editor_view.h
+++ apps/code/editor_view.h
@@ -24,13 +24,13 @@ public:
   void scrollViewDidChangeOffset(ScrollViewDataSource * scrollViewDataSource) override;
   void didBecomeFirstResponder() override;
 private:
-  int numberOfSubviews() const override;
+  int numberOfSubviews() const override { return 2; }
   View * subviewAtIndex(int index) override;
   void layoutSubviews(bool force = false) override;
 
   class GutterView : public View {
   public:
-    GutterView(const KDFont * font);
+    GutterView(const KDFont * font) : View(), m_font(font), m_offset(0) {}
     void drawRect(KDContext * ctx, KDRect rect) const override;
     void setOffset(KDCoordinate offset);
     KDSize minimalSizeForOptimalDisplay() const override;

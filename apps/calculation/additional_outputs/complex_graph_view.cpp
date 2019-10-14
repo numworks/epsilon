970eadd325d9817135a8a2d3f48e6ff21654b671
Émilie Feral <emilie.feral@numworks.com>
2019-10-14 10:40:22 +0200

    [apps/shared][apps/calculation] Create a methods Shared::CurveView::drawAxisLabel and use it in ComplexGraphView

--- apps/calculation/additional_outputs/complex_graph_view.cpp
+++ apps/calculation/additional_outputs/complex_graph_view.cpp
@@ -16,6 +16,8 @@ void ComplexGraphView::drawRect(KDContext * ctx, KDRect rect) const {
   drawDot(ctx, rect, m_complex->x(), m_complex->y(), KDColorBlack);
   drawLabel(ctx, rect, Axis::Horizontal, m_complex->x());
   drawLabel(ctx, rect, Axis::Vertical, m_complex->y());
+  drawAxisLabel(ctx, rect, Axis::Horizontal, "Re", m_complex->x() > 0.0f);
+  drawAxisLabel(ctx, rect, Axis::Vertical, "Im", m_complex->y() > 0.0f);
 
 
 }

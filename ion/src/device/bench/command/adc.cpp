8b0d8aa935b1df6bb1bc474775f3c42e1c174cc3
Émilie Feral <emilie.feral@numworks.com>
2019-10-16 11:51:44 +0200

    [ion] Fix bench build

--- ion/src/device/bench/command/adc.cpp
+++ ion/src/device/bench/command/adc.cpp
@@ -15,7 +15,7 @@ void ADC(const char * input) {
   }
   float result = Ion::Battery::voltage();
   constexpr int precision = 8;
-  constexpr int bufferSize = Poincare::PrintFloat::bufferSizeForFloatsWithPrecision(precision);
+  constexpr int bufferSize = Poincare::PrintFloat::charSizeForFloatsWithPrecision(precision);
   constexpr int glyphLength = Poincare::PrintFloat::glyphLengthForFloatWithPrecision(precision);
   char responseBuffer[bufferSize+4] = {'A', 'D', 'C', '='}; // ADC=
   Poincare::PrintFloat::ConvertFloatToText<float>(result, responseBuffer+4, bufferSize, glyphLength, precision, Poincare::Preferences::PrintFloatMode::Decimal);
--- ion/src/device/bench/runner.cpp
+++ ion/src/device/bench/runner.cpp
@@ -1,7 +1,7 @@
 #include <ion/src/device/bench/bench.h>
 #include <ion/backlight.h>
 
-void ion_main(int argc, char * argv[]) {
+void ion_main(int argc, const char * const argv[]) {
   // Initialize the backlight
   Ion::Backlight::init();
   Ion::Device::Bench::run();

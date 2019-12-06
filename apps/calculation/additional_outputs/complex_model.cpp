a61b2bef37610fc19d7539bb1849c89fbfc04974
Émilie Feral <emilie.feral@numworks.com>
2019-12-06 15:54:36 +0100

    [calculation] Improve ComplexListController

--- apps/calculation/additional_outputs/complex_model.cpp
+++ apps/calculation/additional_outputs/complex_model.cpp
@@ -8,28 +8,28 @@ ComplexModel::ComplexModel(std::complex<float> c) :
 {
 }
 
-float rangeBound(float value, bool max) {
-  if (std::isnan(value) || std::isinf(value)) {
-    return max ? 1.0f : -1.0f;
+float rangeBound(float value, float direction) {
+  if (std::isnan(value) || std::isinf(value) || value == 0.0f) {
+    return direction;
   }
-  float factor = max ? 1.2f : -1.2f;
-  return factor*std::fabs(value);
+  float factor = direction*value >= 0.0f ? 1.2f : -0.3f;
+  return factor*value;
 }
 
 float ComplexModel::xMin() const {
-  return rangeBound(real(), false);
+  return rangeBound(real(), -1.0f);
 }
 
 float ComplexModel::xMax() const {
-  return rangeBound(real(), true);
+  return rangeBound(real(), 1.0f);
 }
 
 float ComplexModel::yMin() const {
-  return rangeBound(imag(), false);
+  return rangeBound(imag(), -1.0f);
 }
 
 float ComplexModel::yMax() const {
-  return rangeBound(imag(), true);
+  return rangeBound(imag(), 1.0f);
 }
 
 }

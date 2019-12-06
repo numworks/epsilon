6eb1b56fe5e17bffd6c06d00b9595b90e243570d
Émilie Feral <emilie.feral@numworks.com>
2019-12-06 15:54:03 +0100

    [calculation] AdditionalInformation are base on the Calculation input instead of exact output

--- apps/calculation/history_controller.cpp
+++ apps/calculation/history_controller.cpp
@@ -75,7 +75,7 @@ bool HistoryController::handleEvent(Ion::Events::Event event) {
          * m_controller->setType(additionalInformationType)
          * m_controller->setCalculation()...*/
         if (additionalInfoType == Expression::AdditionalInformationType::Complex) {
-           m_complexController.setExpression(calculation->exactOutput());
+           m_complexController.setExpression(calculation->input());
           Container::activeApp()->displayModalViewController(&m_complexController, 0.f, 0.f, Metric::CommonTopMargin, Metric::PopUpLeftMargin, 0, Metric::PopUpRightMargin);
         }
       } else {

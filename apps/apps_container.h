f49e39af3566e67bff083a2bcf2c9518752dc63f
Léa Saviot <lea.saviot@numworks.com>
2019-12-09 16:40:16 +0100

    [apps/ion/exam_mode] Store the exam mode activation in the flash
    This way, it is not cleared by a reset

--- apps/apps_container.h
+++ apps/apps_container.h
@@ -46,6 +46,7 @@ public:
   void setShiftAlphaStatus(Ion::Events::ShiftAlphaStatus newStatus);
   OnBoarding::PopUpController * promptController();
   void redrawWindow();
+  void activateExamMode();
   // Exam pop-up controller delegate
   void examDeactivatingPopUpIsDismissed() override;
   // Ion::StorageDelegate
--- apps/global_preferences.cpp
+++ apps/global_preferences.cpp
@@ -5,6 +5,22 @@ GlobalPreferences * GlobalPreferences::sharedGlobalPreferences() {
   return &globalPreferences;
 }
 
+bool GlobalPreferences::examMode() const {
+  if (m_examMode == ExamMode::Unknown) {
+    m_examMode = (ExamMode)Ion::ExamMode::FetchExamMode();
+  }
+  assert((int)m_examMode == 0 || (int)m_examMode == 1);
+  return (bool)m_examMode;
+}
+
+void GlobalPreferences::setExamMode(bool activateExamMode) {
+  if (((bool)examMode()) == activateExamMode) {
+    return;
+  }
+  Ion::ExamMode::ToggleExamMode();
+  m_examMode = (ExamMode)activateExamMode;
+}
+
 void GlobalPreferences::setBrightnessLevel(int brightnessLevel) {
   if (m_brightnessLevel != brightnessLevel) {
     brightnessLevel = brightnessLevel < 0 ? 0 : brightnessLevel;

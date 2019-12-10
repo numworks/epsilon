c85af9a89691a5f52151c233ee370c3ad5c8ba02
Léa Saviot <lea.saviot@numworks.com>
2019-12-10 11:40:54 +0100

    [ion/device] Flash driver handles Internal and external flash

---
+++ ion/src/device/shared/drivers/internal_flash.h
@@ -0,0 +1,27 @@
+#ifndef ION_DEVICE_SHARED_INTERNAL_FLASH_H
+#define ION_DEVICE_SHARED_INTERNAL_FLASH_H
+
+#include <stddef.h>
+#include <regs/regs.h>
+
+namespace Ion {
+namespace Device {
+namespace InternalFlash {
+
+void MassErase();
+
+int SectorAtAddress(uint32_t address);
+void EraseSector(int i);
+
+void WriteMemory(uint8_t * destination, uint8_t * source, size_t length);
+
+/* The Device is powered by a 2.8V LDO. This allows us to perform writes to the
+ * Flash 32 bits at once. */
+constexpr Regs::FLASH::CR::PSIZE MemoryAccessWidth = Regs::FLASH::CR::PSIZE::X32;
+typedef uint32_t MemoryAccessType;
+
+}
+}
+}
+
+#endif

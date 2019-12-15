450a03ef3e6929f9705d936830c733368d19f7dd
Romain Goyet <romain.goyet@numworks.com>
2019-12-15 17:02:01 -0500

    [ion/cache] Follow CMSIS' order

--- ion/src/device/n0110/drivers/cache.cpp
+++ ion/src/device/n0110/drivers/cache.cpp
@@ -7,18 +7,18 @@ namespace Cache {
 using namespace Regs;
 
 void privateCleanInvalidateDisableDCache(bool clean, bool invalidate, bool disable) {
+  // Select Level 1 data cache
   CORTEX.CSSELR()->set(0);
   dsb();
 
-  // Associativity = 6
-
-  uint32_t sets = CORTEX.CCSIDR()->getNUMSETS();
-  uint32_t ways = CORTEX.CCSIDR()->getASSOCIATIVITY();
-
+  // Disable D-Cache
   if (disable) {
     CORTEX.CCR()->setDC(false);
     dsb();
   }
+
+  uint32_t sets = CORTEX.CCSIDR()->getNUMSETS();
+  uint32_t ways = CORTEX.CCSIDR()->getASSOCIATIVITY();
 
   do {
     uint32_t w = ways;

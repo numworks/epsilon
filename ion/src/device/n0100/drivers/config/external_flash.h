320a3d5daeba8198294dced66d053dd775444257
Léa Saviot <lea.saviot@numworks.com>
2019-12-18 10:55:59 +0100

    [ion/external_flash] Finer flash sectors
    Instead of 128 sectors of 64K, there are now 8 sectors of 4K, 1 of 32K
    and 127 of 64K. This makes a more finely erasable flash, which is needed
    for the exam mode.

--- ion/src/device/n0100/drivers/config/external_flash.h
+++ ion/src/device/n0100/drivers/config/external_flash.h
@@ -22,7 +22,10 @@ using namespace Regs;
 
 constexpr static uint32_t StartAddress = 0xFFFFFFFF;
 constexpr static uint32_t EndAddress = 0xFFFFFFFF;
-constexpr static int NumberOfSectors = 0;
+constexpr static int NumberOf4KSectors = 0;
+constexpr static int NumberOf32KSectors = 0;
+constexpr static int NumberOf64KSectors = 0;
+constexpr static int NumberOfSectors = NumberOf4KSectors + NumberOf32KSectors + NumberOf64KSectors;
 constexpr static AFGPIOPin Pins[] = {};
 
 }

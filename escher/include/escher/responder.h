0412a45cea77557235d2fc811ce33692970a8495
Léa Saviot <lea.saviot@numworks.com>
2019-12-19 15:36:57 +0100

    [escher/responder] Inline some methods

--- escher/include/escher/responder.h
+++ escher/include/escher/responder.h
@@ -5,15 +5,15 @@
 
 class Responder {
 public:
-  Responder(Responder * parentResponder);
+  Responder(Responder * parentResponder) : m_parentResponder(parentResponder) {}
   virtual bool handleEvent(Ion::Events::Event event) { return false; }; // Default implementation does nothing
-  virtual void didBecomeFirstResponder();
-  virtual void willResignFirstResponder();
-  virtual void didEnterResponderChain(Responder * previousFirstResponder);
-  virtual void willExitResponderChain(Responder * nextFirstResponder);
-  Responder * parentResponder() const;
+  virtual void didBecomeFirstResponder() {}
+  virtual void willResignFirstResponder() {}
+  virtual void didEnterResponderChain(Responder * previousFirstResponder) {}
+  virtual void willExitResponderChain(Responder * nextFirstResponder) {}
+  Responder * parentResponder() const { return m_parentResponder; }
   Responder * commonAncestorWith(Responder * responder);
-  void setParentResponder(Responder * responder);
+  void setParentResponder(Responder * responder) { m_parentResponder = responder; }
 private:
   Responder * m_parentResponder;
 };

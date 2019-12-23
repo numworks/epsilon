27c669f4d8189454f06e6f4c64ad4978583ebc97
Émilie Feral <emilie.feral@numworks.com>
2019-12-23 18:30:10 +0100

    [calculation] Create a class ListController to factorize IllustratedListController and SimpleListController

--- apps/calculation/additional_outputs/illustrated_list_controller.h
+++ apps/calculation/additional_outputs/illustrated_list_controller.h
@@ -3,19 +3,19 @@
 
 #include <escher.h>
 #include "scrollable_input_exact_approximate_expressions_cell.h"
+#include "list_controller.h"
 #include "../calculation_store.h"
 #include <apps/i18n.h>
 
 namespace Calculation {
 
-class IllustratedListController : public StackViewController, public ListViewDataSource, public SelectableTableViewDataSource, public SelectableTableViewDelegate {
+class IllustratedListController : public ListController, public SelectableTableViewDelegate {
 public:
   IllustratedListController(Responder * parentResponder);
 
   // Responder
-  bool handleEvent(Ion::Events::Event event) override;
-  void didBecomeFirstResponder() override;
   void viewDidDisappear() override;
+  void didEnterResponderChain(Responder * previousFirstResponder) override;
 
   //ListViewDataSource
   int numberOfRows() const override;
@@ -29,26 +29,15 @@ public:
   void tableViewDidChangeSelection(SelectableTableView * t, int previousSelectedCellX, int previousSelectedCellY, bool withinTemporarySelection) override;
 
   // IllustratedListController
-  virtual void setExpression(Poincare::Expression e);
+  void setExpression(Poincare::Expression e) override;
 
 protected:
   Poincare::Expression m_savedExpression;
   CalculationStore m_calculationStore;
 private:
   virtual CodePoint expressionSymbol() const = 0;
-  class ListController : public ViewController {
-  public:
-    ListController(IllustratedListController * dataSource);
-    const char * title() override { return I18n::translate(I18n::Message::AdditionalResults); }
-    View * view() override { return &m_selectableTableView; }
-    void didBecomeFirstResponder() override;
-    SelectableTableView * selectableTableView() { return &m_selectableTableView; }
-  private:
-    SelectableTableView m_selectableTableView;
-  };
   constexpr static int k_maxNumberOfAdditionalCalculations = 4;
   constexpr static KDCoordinate k_illustrationHeight = 100;
-  ListController m_listController;
   // Cells
   virtual HighlightCell * illustrationCell() = 0;
   ScrollableInputExactApproximateExpressionsCell m_additionalCalculationCells[k_maxNumberOfAdditionalCalculations];

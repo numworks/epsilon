f838d31ec1c1519cd8678605c68a78cd410fe022
redgl0w <43498612+RedGl0w@users.noreply.github.com>
2019-12-24 09:21:14 +0100

    Update events_modifier.cpp

--- ion/src/shared/events_modifier.cpp
+++ ion/src/shared/events_modifier.cpp
@@ -18,6 +18,10 @@ bool isAlphaActive() {
   return sShiftAlphaStatus == ShiftAlphaStatus::Alpha || sShiftAlphaStatus == ShiftAlphaStatus::ShiftAlpha || sShiftAlphaStatus == ShiftAlphaStatus::AlphaLock || sShiftAlphaStatus == ShiftAlphaStatus::ShiftAlphaLock;
 ;
 }
+  
+bool isLockActive() {
+  return sShiftAlphaStatus == ShiftAlphaStatus::AlphaLock || sShiftAlphaStatus == ShiftAlphaStatus::ShiftAlphaLock;
+}
 
 void setShiftAlphaStatus(ShiftAlphaStatus s) {
   sShiftAlphaStatus = s;

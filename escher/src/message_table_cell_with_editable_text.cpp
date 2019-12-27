1b5fd2b36de412c67681353030b069fba64f2e34
Émilie Feral <emilie.feral@numworks.com>
2019-12-27 09:45:47 +0100

    [escher] TableCell: make the label margin optional

--- escher/src/message_table_cell_with_editable_text.cpp
+++ escher/src/message_table_cell_with_editable_text.cpp
@@ -52,7 +52,7 @@ void MessageTableCellWithEditableText::layoutSubviews(bool force) {
   KDSize labelSize = labelView()->minimalSizeForOptimalDisplay();
   /* Handle textfield that has no defined width (as their width evolves with
    * the length of edited text */
-  textFieldSize = KDSize(bounds().width() - 2*k_separatorThickness - labelSize.width()-2*k_labelMargin-k_accessoryMargin, textFieldSize.height());
+  textFieldSize = KDSize(bounds().width() - 2*k_separatorThickness - labelSize.width()-2*labelMargin()-k_accessoryMargin, textFieldSize.height());
   m_textField.setFrame(KDRect(
     bounds().width() - textFieldSize.width() - k_separatorThickness-k_accessoryMargin,
     (bounds().height()-textFieldSize.height()-k_accessoryMargin)/2,

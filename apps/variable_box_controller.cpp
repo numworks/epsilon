f9cbe7938e24b0036319063a9ba53546870b0646
Émilie Feral <emilie.feral@numworks.com>
2019-12-31 11:45:41 +0100

    [escher] ExpressionTableCell & ExpressionTableCellWithExpression: reloadScroll when displaying cell

--- apps/variable_box_controller.cpp
+++ apps/variable_box_controller.cpp
@@ -120,6 +120,7 @@ void VariableBoxController::willDisplayCellForIndex(HighlightCell * cell, int in
   Layout symbolLayout = LayoutHelper::String(symbolName, symbolLength);
   myCell->setLayout(symbolLayout);
   myCell->setAccessoryLayout(expressionLayoutForRecord(record, index));
+  myCell->reloadScroll();
   myCell->reloadCell();
 }
 

74d9ebd5bb1bc3ba84967da1db4309230d0adc51
Émilie Feral <emilie.feral@numworks.com>
2020-01-07 11:28:26 +0100

    [apps/calculation] additional_outputs, TrigonometryModel: fix range to display the half circle where the angle is

--- apps/calculation/additional_outputs/trigonometry_model.h
+++ apps/calculation/additional_outputs/trigonometry_model.h
@@ -3,6 +3,7 @@
 
 #include "../../shared/curve_view_range.h"
 #include <complex>
+#include <poincare/trigonometry.h>
 
 namespace Calculation {
 
@@ -10,12 +11,13 @@ class TrigonometryModel : public Shared::CurveViewRange {
 public:
   TrigonometryModel();
   // CurveViewRange
-  float xMin() const override { return -1.5f; }
-  float xMax() const override { return 1.5f; }
-  float yMin() const override { return -1.5f; }
-  float yMax() const override { return 1.5f; }
+  float xMin() const override { return -2.2f; }
+  float xMax() const override { return 2.2f; }
+  float yMin() const override { return std::sin(angle()) >= 0.0f ? -0.2f : -1.2f; }
+  float yMax() const override { return std::sin(angle()) >= 0.0f ? 1.2f : 0.2f; }
 
   void setAngle(float f) { m_angle = f; }
+  float angle() const { return m_angle*M_PI/Poincare::Trigonometry::PiInAngleUnit(Poincare::Preferences::sharedPreferences()->angleUnit()); }
 private:
   float m_angle;
 };

da3d06d7eb99a275098618a04578aa8ef65d3b02
Émilie Feral <emilie.feral@numworks.com>
2020-01-07 11:29:21 +0100

    [apps/calculation] additional_outputs: TrigonometryGraphView: first draft of drawRect

--- apps/calculation/additional_outputs/trigonometry_graph_cell.cpp
+++ apps/calculation/additional_outputs/trigonometry_graph_cell.cpp
@@ -13,6 +13,15 @@ TrigonometryGraphView::TrigonometryGraphView(TrigonometryModel * model) :
 
 void TrigonometryGraphView::drawRect(KDContext * ctx, KDRect rect) const {
   ctx->fillRect(rect, KDColorWhite);
+  drawGrid(ctx, rect);
+  drawAxes(ctx, rect);
+  // Draw the circle
+  drawCurve(ctx, rect, 0, 2.0f*M_PI, M_PI/180.0f, [](float t, void * model, void * context) {
+      return Poincare::Coordinate2D<float>(std::cos(t), std::sin(t));
+    }, nullptr, nullptr, true, Palette::GreyDark);
+  drawSegment(ctx, rect, Axis::Vertical, std::cos(m_model->angle()), 0.0f, std::sin(m_model->angle()), Palette::Red);
+  drawSegment(ctx, rect, Axis::Horizontal, std::sin(m_model->angle()), 0.0f, std::cos(m_model->angle()), Palette::Red);
+  drawDot(ctx, rect, std::cos(m_model->angle()), std::sin(m_model->angle()), Palette::Red);
   // TODO
 }
 

a03a575cd0d9fe91aa1e8deb5c12ace8ab8247a4
Émilie Feral <emilie.feral@numworks.com>
2020-01-07 11:27:51 +0100

    [apps/calculation] additional_outputs: TrigonometryListController set the right angle model to the view

--- apps/calculation/additional_outputs/trigonometry_list_controller.cpp
+++ apps/calculation/additional_outputs/trigonometry_list_controller.cpp
@@ -7,13 +7,16 @@ namespace Calculation {
 
 void TrigonometryListController::setExpression(Poincare::Expression e) {
   IllustratedListController::setExpression(e.childAtIndex(0));
-  //TODO
-  //m_model.setAngle(std::complex<float>(1.2f,2.3f));
 
+  // Fill calculation store
   Poincare::Context * context = App::app()->localContext();
   m_calculationStore.push("sin(θ)", context);
   m_calculationStore.push("cos(θ)", context);
   m_calculationStore.push("θ", context);
+
+  // Set trigonometry illustration
+  float angle = Shared::PoincareHelpers::ApproximateToScalar<float>(m_calculationStore.calculationAtIndex(0)->approximateOutput(context), context);
+  m_model.setAngle(angle);
 }
 
 }

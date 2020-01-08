8175df9abd6bc184b6d5a32d777d19fdc607d341
Émilie Feral <emilie.feral@numworks.com>
2020-01-08 16:53:33 +0100

    [apps/calculation] additional_outputs: improve drawRect of ComplexGraphView

--- apps/calculation/additional_outputs/complex_graph_cell.cpp
+++ apps/calculation/additional_outputs/complex_graph_cell.cpp
@@ -13,41 +13,40 @@ ComplexGraphView::ComplexGraphView(ComplexModel * complexModel) :
 
 void ComplexGraphView::drawRect(KDContext * ctx, KDRect rect) const {
   ctx->fillRect(rect, KDColorWhite);
+  drawGrid(ctx, rect);
+  drawAxes(ctx, rect);
+  // Draw graduations
+  drawLabelsAndGraduations(ctx, rect, Axis::Vertical, true);
+  drawLabelsAndGraduations(ctx, rect, Axis::Horizontal, true);
+
   float real = m_complex->real();
   float imag = m_complex->imag();
-  /* Draw the partial ellipse indicating the angle theta
-   * - the ellipse parameters are a = real/2 and b = imag/2,
-   * - the ellipse equation is x^2/a^2 + y^2/b^2 = 1, so we draw the curve of
-   *   equation y = imag/real*(real*real/4-x*x)
-   * - we draw the ellipse from the dot (xStart, yStart) chosen so that
-   *   yStart = 0 which gives xStart = real/2 to the dot (xEnd, yEnd) chosen to
-   *   be on the slope y = x*imag/real which gives xStart = real/(2*sqrt(2)). */
-  drawCurve(ctx, rect, real/(2.0f*std::sqrt(2.0f)), real/2.0f, real/100.0f, [](float t, void * model, void * context) {
-      ComplexModel * complexModel = (ComplexModel *)model;
-      float real = complexModel->real();
-      float imag = complexModel->imag();
-      return Poincare::Coordinate2D<float>(t, (imag/real)*std::sqrt(real*real/4.0f - t*t));
-    }, m_complex, nullptr, false, Palette::Blue, false);
   // Draw the segment from the origin to the dot (real, imag) of equation y = x*imag/real
-  drawCartesianCurve(ctx, rect, 0, real, [](float t, void * model, void * context) {
+  drawCurve(ctx, rect, 0.0f, 1.0f, 0.01f,
+      [](float t, void * model, void * context) {
         ComplexModel * complexModel = (ComplexModel *)model;
-        return Poincare::Coordinate2D<float>(t, t*complexModel->imag()/complexModel->real());
-      }, m_complex, nullptr, Palette::Red, false);
-  drawAxes(ctx, rect);
-  drawDot(ctx, rect, real, imag, KDColorBlack);
-  //drawLabel(ctx, rect, Axis::Horizontal, real);
-  //drawLabel(ctx, rect, Axis::Vertical, imag);
-  //drawAxisLabel(ctx, rect, Axis::Horizontal, "Re", real > 0.0f);
-  //drawAxisLabel(ctx, rect, Axis::Vertical, "Im", imag > 0.0f);
-  char buffer[k_labelBufferMaxSize];
-  PrintFloat::ConvertFloatToText<float>(
-      std::arg(*m_complex),
-      buffer,
-      k_labelBufferMaxSize,
-      k_labelBufferMaxGlyphLength,
-      Poincare::Preferences::ShortNumberOfSignificantDigits,
-      Preferences::PrintFloatMode::Decimal);
-  ctx->drawString(buffer, KDPoint(floatToPixel(Axis::Horizontal, std::fabs(real)/2.0f)+KDFont::SmallFont->glyphSize().width(), floatToPixel(Axis::Vertical, 0.0f)-KDFont::SmallFont->glyphSize().height()), KDFont::SmallFont);
+        return Poincare::Coordinate2D<float>(complexModel->real()*t, complexModel->imag()*t);
+      }, m_complex, nullptr, false, Palette::GreyDark, false);
+  /* Draw the partial ellipse indicating the angle theta
+   * - the ellipse parameters are a = |real|5 and b = |imag|/5,
+   * - the parametric ellipse equation is x(t) = a*cos(t) and y(t) = b*sin(t)
+   * - we draw the ellipse from t = 0 to t = the phase of the complex
+   */
+  drawCurve(ctx, rect, 0.0f, 1.0f, 0.01f,
+      [](float t, void * model, void * context) {
+        ComplexModel * complexModel = (ComplexModel *)model;
+        float factor = 5.0f;
+        float a = std::fabs(complexModel->real())/factor;
+        float b = std::fabs(complexModel->imag())/factor;
+        float ph = std::arg(*complexModel);
+        return Poincare::Coordinate2D<float>(a*std::cos(t*ph), b*std::sin(t*ph));
+    }, m_complex, nullptr, false, Palette::GreyDark, false);
+  // Draw dashed segment to indicate real and imaginary
+  drawSegment(ctx, rect, Axis::Vertical, real, 0.0f, imag, Palette::Red, 1, 3);
+  drawSegment(ctx, rect, Axis::Horizontal, imag, 0.0f, real, Palette::Red, 1, 3);
+  // Draw complex position on the plan
+  drawDot(ctx, rect, real, imag, Palette::Red, true);
+  // Draw labels
 }
 
 }

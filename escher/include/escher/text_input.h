d37a1e7d0a43c7ed9d38b10ee211f8c031f500fa
Léa Saviot <lea.saviot@numworks.com>
2020-01-08 14:56:49 +0100

    [escher/text_input] Methods reordering

--- escher/include/escher/text_input.h
+++ escher/include/escher/text_input.h
@@ -23,26 +23,36 @@ protected:
     ContentView(const KDFont * font) :
       View(),
       m_cursorView(),
+      m_font(font),
       m_selectionStart(nullptr),
       m_selectionEnd(nullptr),
-      m_font(font),
       m_cursorLocation(nullptr)
     {}
+
+    // Font
     void setFont(const KDFont * font);
     const KDFont * font() const { return m_font; }
+
+    // Cursor location
     const char * cursorLocation() const { assert(m_cursorLocation != nullptr); return m_cursorLocation; }
     void setCursorLocation(const char * cursorLocation);
+    KDRect cursorRect();
+
+    // Virtual text get/add/remove
     virtual const char * text() const = 0;
     virtual bool insertTextAtLocation(const char * text, const char * location) = 0;
     virtual bool removePreviousGlyph() = 0;
     virtual bool removeEndOfLine() = 0;
-    KDRect cursorRect();
+
+    // Selection
     const char * selectionStart() const { return m_selectionStart; }
     const char * selectionEnd() const { return m_selectionEnd; }
     void addSelection(const char * left, const char * right);
     bool resetSelection(); // returns true if the selection was indeed reset
     bool selectionIsEmpty() const;
     virtual size_t deleteSelection() = 0;
+
+    // Reload
     void reloadRectFromPosition(const char * position, bool includeFollowingLines = false);
   protected:
     virtual void layoutSubviews(bool force = false) override;
@@ -50,9 +60,9 @@ protected:
     virtual KDRect glyphFrameAtPosition(const char * buffer, const char * position) const = 0;
     virtual KDRect dirtyRectFromPosition(const char * position, bool includeFollowingLines) const;
     TextCursorView m_cursorView;
+    const KDFont * m_font;
     const char * m_selectionStart;
     const char * m_selectionEnd;
-    const KDFont * m_font;
     const char * m_cursorLocation;
   private:
     int numberOfSubviews() const override { return 1; }

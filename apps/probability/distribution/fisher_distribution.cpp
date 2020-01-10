f76b79c6208e9044b724db01d9acabf2f73a115d
Léa Saviot <lea.saviot@numworks.com>
2020-01-10 17:36:06 +0100

    [apps/proba] Implement Fisher formulae

--- apps/probability/distribution/fisher_distribution.cpp
+++ apps/probability/distribution/fisher_distribution.cpp
@@ -1,9 +1,12 @@
 #include "fisher_distribution.h"
-#include <poincare/normal_distribution.h>
+#include <poincare/beta_function.h>
+#include <poincare/regularized_incomplete_beta_function.h>
 #include <cmath>
 #include <float.h>
 
 namespace Probability {
+
+static inline double maxDouble(double x, double y) { return x > y ? x : y; }
 
 float FisherDistribution::xMin() const {
   return -k_displayLeftMarginRatio * xMax();
@@ -39,7 +42,15 @@ I18n::Message FisherDistribution::parameterDefinitionAtIndex(int index) {
 }
 
 float FisherDistribution::evaluateAtAbscissa(float x) const {
-  return 1.0; //TODO LEA Poincare::FisherDistribution::EvaluateAtAbscissa(x, m_parameter1, m_parameter2);
+  if (x < 0.0f) {
+    return NAN;
+  }
+  const float d1 = m_parameter1;
+  const float d2 = m_parameter2;
+  const float f = d1*x/(d1*x+d2);
+  const float numerator = std::pow(f, d1/2.0f) * std::pow(1.0f - f, d2/2.0f);
+  const float denominator = x * Poincare::BetaFunction(d1/2.0f, d2/2.0f);
+  return numerator / denominator;
 }
 
 bool FisherDistribution::authorizedValueAtIndex(float x, int index) const {
@@ -53,11 +64,22 @@ void FisherDistribution::setParameterAtIndex(float f, int index) {
 }
 
 double FisherDistribution::cumulativeDistributiveFunctionAtAbscissa(double x) const {
-  return 1.0; //TODO LEA
+  const double d1 = m_parameter1;
+  const double d2 = m_parameter2;
+  return Poincare::RegularizedIncompleteBetaFunction(d1/2.0, d2/2.0, d1*x/(d1*x+d2));
 }
 
 double FisherDistribution::cumulativeDistributiveInverseForProbability(double * probability) {
-  return 1.0; //TODO LEA
+  /* We have to compute the values of the interval in which to look for x.
+   * We cannot put xMin because xMin is < 0 for display purposes, and negative
+   * values are not accepted.
+   * The maximum of the interval: we want */
+  if (*probability < DBL_EPSILON) {
+    return 0.0;
+  }
+  return cumulativeDistributiveInverseForProbabilityUsingIncreasingFunctionRoot(probability, DBL_EPSILON, maxDouble(xMax(), 100.0));  // Ad-hoc value;
+
+
 }
 
 }

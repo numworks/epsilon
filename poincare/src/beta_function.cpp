7bef81b919b495e6ba5c2226d8410c88782827ae
Léa Saviot <lea.saviot@numworks.com>
2020-01-10 17:36:06 +0100

    [apps/proba] Implement Fisher formulae

---
+++ poincare/src/beta_function.cpp
@@ -0,0 +1,14 @@
+#include <poincare/beta_function.h>
+#include <math.h>
+#include <cmath>
+
+namespace Poincare {
+
+double BetaFunction(double a, double b) {
+  if (a < 0.0 || b < 0.0) {
+    return NAN;
+  }
+  return std::exp(std::lgamma(a) + std::lgamma(b) - std::lgamma(a+b));
+}
+
+}

68a789fa2dedc598f26876d646245919298a8a91
Émilie Feral <emilie.feral@numworks.com>
2020-01-15 16:29:34 +0100

    [apps/calculation] additional_outputs: make a parent class to TrigonometryGraphCell and ComplexGraphCell inheriting from BorderedCell

---
+++ apps/calculation/additional_outputs/illustration_cell.cpp
@@ -0,0 +1,12 @@
+#include "illustration_cell.h"
+
+using namespace Shared;
+using namespace Poincare;
+
+namespace Calculation {
+
+void IllustrationCell::layoutSubviews(bool force) {
+  view()->setFrame(KDRect(Metric::CellSeparatorThickness, Metric::CellSeparatorThickness, bounds().width() - 2*Metric::CellSeparatorThickness, bounds().height() - 2*Metric::CellSeparatorThickness), force);
+}
+
+}

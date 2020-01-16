b8f261b3d45b9d3bef4644dbe171d2e8a75b8e57
Émilie Feral <emilie.feral@numworks.com>
2020-01-16 10:28:09 +0100

    [escher] Create a class Bordered instead of BorderedCell: this enable non-cell view to use it as well

---
+++ escher/include/escher/bordered.h
@@ -0,0 +1,16 @@
+#ifndef ESCHER_BORDERED_H
+#define ESCHER_BORDERED_H
+
+#include <escher/metric.h>
+#include <kandinsky/context.h>
+
+class Bordered {
+public:
+  void drawBorderOfRect(KDContext * ctx, KDRect rect, KDColor borderColor) const;
+  void drawInnerRect(KDContext * ctx, KDRect rect, KDColor backgroundColor) const;
+protected:
+  constexpr static KDCoordinate k_separatorThickness = Metric::CellSeparatorThickness;
+};
+
+#endif
+

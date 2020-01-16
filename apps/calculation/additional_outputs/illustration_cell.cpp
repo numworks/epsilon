dda2ac094530fc3bca6afff75157754bac6624f1
Émilie Feral <emilie.feral@numworks.com>
2020-01-16 10:28:09 +0100

    [escher] Create a class Bordered instead of BorderedCell: this enable non-cell view to use it as well

--- apps/calculation/additional_outputs/illustration_cell.cpp
+++ apps/calculation/additional_outputs/illustration_cell.cpp
@@ -9,4 +9,8 @@ void IllustrationCell::layoutSubviews(bool force) {
   view()->setFrame(KDRect(Metric::CellSeparatorThickness, Metric::CellSeparatorThickness, bounds().width() - 2*Metric::CellSeparatorThickness, bounds().height() - 2*Metric::CellSeparatorThickness), force);
 }
 
+void IllustrationCell::drawRect(KDContext * ctx, KDRect rect) const {
+  drawBorderOfRect(ctx, bounds(), Palette::GreyBright);
+}
+
 }

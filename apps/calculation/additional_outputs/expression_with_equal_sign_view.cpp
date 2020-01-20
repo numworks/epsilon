d0a7e3eebc011ee1b10369fb676df17fad79d2c8
Émilie Feral <emilie.feral@numworks.com>
2020-01-20 17:52:27 +0100

    [apps/calculation] ExpressionWithEqualSignView inherits from ExpressionView instead of composing
    This is useful for future change on
    ScrollableExactApproximateExpressionView

--- apps/calculation/additional_outputs/expression_with_equal_sign_view.cpp
+++ apps/calculation/additional_outputs/expression_with_equal_sign_view.cpp
@@ -3,24 +3,30 @@
 namespace Calculation {
 
 KDSize ExpressionWithEqualSignView::minimalSizeForOptimalDisplay() const {
-  KDSize expressionSize = m_expressionView.minimalSizeForOptimalDisplay();
+  KDSize expressionSize = ExpressionView::minimalSizeForOptimalDisplay();
   KDSize equalSize = m_equalSign.minimalSizeForOptimalDisplay();
   return KDSize(expressionSize.width() + equalSize.width() + Metric::CommonLargeMargin, expressionSize.height());
 }
 
-View * ExpressionWithEqualSignView::subviewAtIndex(int index) {
-  if (index == 0) {
-    return &m_expressionView;
+void ExpressionWithEqualSignView::drawRect(KDContext * ctx, KDRect rect) const {
+  if (m_layout.isUninitialized()) {
+    return;
   }
-  assert(index == 1);
+  // Do not color the whole background to avoid coloring behind the equal symbol
+  KDSize expressionSize = ExpressionView::minimalSizeForOptimalDisplay();
+  ctx->fillRect(KDRect(0, 0, expressionSize), m_backgroundColor);
+  m_layout.draw(ctx, drawingOrigin(), m_textColor, m_backgroundColor, m_selectionStart, m_selectionEnd, Palette::Select);
+}
+
+View * ExpressionWithEqualSignView::subviewAtIndex(int index) {
+  assert(index == 0);
   return &m_equalSign;
 }
 
 void ExpressionWithEqualSignView::layoutSubviews(bool force) {
-  KDSize expressionSize = m_expressionView.minimalSizeForOptimalDisplay();
+  KDSize expressionSize = ExpressionView::minimalSizeForOptimalDisplay();
   KDSize equalSize = m_equalSign.minimalSizeForOptimalDisplay();
-  KDCoordinate expressionBaseline = m_expressionView.layout().baseline();
-  m_expressionView.setFrame(KDRect(0, 0, expressionSize), force);
+  KDCoordinate expressionBaseline = layout().baseline();
   m_equalSign.setFrame(KDRect(expressionSize.width() + Metric::CommonLargeMargin, expressionBaseline - equalSize.height()/2, equalSize), force);
 }
 

3de0ee7f988e3a78da7e0991fad1e8854cd2616b
Émilie Feral <emilie.feral@numworks.com>
2020-01-20 17:52:27 +0100

    [apps/calculation] ExpressionWithEqualSignView inherits from ExpressionView instead of composing
    This is useful for future change on
    ScrollableExactApproximateExpressionView

--- escher/include/escher/expression_view.h
+++ escher/include/escher/expression_view.h
@@ -26,19 +26,20 @@ public:
   KDSize minimalSizeForOptimalDisplay() const override;
   KDPoint drawingOrigin() const;
   KDPoint absoluteDrawingOrigin() const;
-private:
+protected:
   /* Warning: we do not need to delete the previous expression layout when
    * deleting object or setting a new expression layout. Indeed, the expression
    * layout is always possessed by a controller which only gives a pointer to
    * the expression view (without cloning it). The named controller is then
    * responsible for freeing the expression layout when required. */
   mutable Poincare::Layout m_layout; // TODO find better way to have minimalSizeForOptimalDisplay const
-  Poincare::Layout * m_selectionStart;
-  Poincare::Layout * m_selectionEnd;
-  float m_horizontalAlignment;
-  float m_verticalAlignment;
   KDColor m_textColor;
   KDColor m_backgroundColor;
+  Poincare::Layout * m_selectionStart;
+  Poincare::Layout * m_selectionEnd;
+private:
+  float m_horizontalAlignment;
+  float m_verticalAlignment;
   KDCoordinate m_horizontalMargin;
 };
 

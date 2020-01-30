4b996b333b324faf8e5d503b26afec5e7f0263c4
Léa Saviot <lea.saviot@numworks.com>
2020-01-30 16:20:04 +0100

    [poincare/empty_context] Add missing file

---
+++ poincare/include/poincare/empty_context.h
@@ -0,0 +1,20 @@
+#ifndef POINCARE_EMPTY_CONTEXT_H
+#define POINCARE_EMPTY_CONTEXT_H
+
+#include <poincare/context.h>
+#include <poincare/expression.h>
+#include <assert.h>
+
+namespace Poincare {
+
+class EmptyContext : public Context {
+public:
+  // Context
+  SymbolAbstractType expressionTypeForIdentifier(const char * identifier, int length) override { return SymbolAbstractType::None; }
+  void setExpressionForSymbolAbstract(const Expression & expression, const SymbolAbstract & symbol) override { assert(false); }
+  const Expression expressionForSymbolAbstract(const SymbolAbstract & symbol, bool clone) override { return Expression(); }
+};
+
+}
+
+#endif

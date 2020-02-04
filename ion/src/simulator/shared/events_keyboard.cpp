2cb246b8fae9d380b238aee46de418f7d79c770f
Léa Saviot <lea.saviot@numworks.com>
2020-02-04 11:33:28 +0100

    [ion/simulator] Keyboard shift + left/right/up/down now works

--- ion/src/simulator/shared/events_keyboard.cpp
+++ ion/src/simulator/shared/events_keyboard.cpp
@@ -124,6 +124,18 @@ static Event eventFromSDLKeyboardEvent(SDL_KeyboardEvent event) {
         return Ans;
     }
   }
+  if (event.keysym.mod & KMOD_SHIFT) {
+    switch(event.keysym.sym) {
+      case SDLK_UP:
+        return ShiftUp;
+      case SDLK_DOWN:
+        return ShiftDown;
+      case SDLK_LEFT:
+        return ShiftLeft;
+      case SDLK_RIGHT:
+        return ShiftRight;
+    }
+  }
   switch(event.keysym.sym) {
     case SDLK_UP:
       return Up;

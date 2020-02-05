3febb762b464104659abc3910c7f381d2ca34e81
Léa Saviot <lea.saviot@numworks.com>
2020-02-05 17:26:54 +0100

    [poincare/test] Add tests on unit convert simplification

--- poincare/test/simplification.cpp
+++ poincare/test/simplification.cpp
@@ -923,6 +923,9 @@ QUIZ_CASE(poincare_simplification_unit_convert) {
   assert_parsed_expression_simplify_to("10_m^2→_mm×_km", "10×_km×_mm");
   assert_parsed_expression_simplify_to("2_hour+2_min→_s", "7320×_s");
   assert_parsed_expression_simplify_to("2×_kg×_m^2×_s^(-2)→_J", "2×_J");
+  assert_parsed_expression_simplify_to("300000×_m^3→_km^(2.3+0.7)", "0.0003×_km^3");
+  assert_parsed_expression_simplify_to("3×_m→6_km", "0.003×_km");
+  assert_parsed_expression_simplify_to("4×_min→_s^3/_s^2", "240_s");
 
   assert_parsed_expression_simplify_to("1→2", Undefined::Name());
   assert_parsed_expression_simplify_to("1→u(n)", Undefined::Name());
@@ -941,6 +944,16 @@ QUIZ_CASE(poincare_simplification_unit_convert) {
   assert_parsed_expression_simplify_to("1→ans", Undefined::Name());
   assert_parsed_expression_simplify_to("ans→ans", Undefined::Name());
   assert_parsed_expression_simplify_to("1→3_m", Undefined::Name());
+  assert_parsed_expression_simplify_to("4→_km/_m", Undefined::Name());
+  assert_parsed_expression_simplify_to("3×_min→_s+1-1", Undefined::Name());
+
+  assert_simplify("2→a");
+  assert_parsed_expression_simplify_to("3_m→a×_km", Undefined::Name());
+  assert_simplify("2→f(x)");
+  assert_parsed_expression_simplify_to("3_m→f(2)×_km", Undefined::Name());
+  // Clean the storage for other tests
+  Ion::Storage::sharedStorage()->recordNamed("a.exp").destroy();
+  Ion::Storage::sharedStorage()->recordNamed("f.func").destroy();
 }
 
 QUIZ_CASE(poincare_simplification_complex_format) {

6fc0e23a8f14e5ee26399153b59fa4599bc9a22a
Ruben Dashyan <ruben.dashyan@numworks.com>
2020-02-06 15:15:53 +0100

    [poincare/unit] Lighten Representative constructor

--- poincare/test/parsing.cpp
+++ poincare/test/parsing.cpp
@@ -295,7 +295,7 @@ QUIZ_CASE(poincare_parsing_units) {
       Expression unit = parse_expression(buffer, nullptr, false);
       quiz_assert_print_if_failure(unit.type() == ExpressionNode::Type::Unit, "Should be parsed as a Unit");
       if (rep->isPrefixable()) {
-        for (const Unit::Prefix * pre = Unit::AllPrefixes; pre < Unit::AllPrefixes + Unit::AllPrefixesCount; pre++) {
+        for (const Unit::Prefix * pre = Unit::AllPrefixes; pre < Unit::AllPrefixes + sizeof(Unit::AllPrefixes)/sizeof(Unit::Prefix); pre++) {
           Unit::Builder(dim, rep, pre).serialize(buffer, bufferSize, Preferences::PrintFloatMode::Decimal, Preferences::VeryShortNumberOfSignificantDigits);
           Expression unit = parse_expression(buffer, nullptr, false);
           quiz_assert_print_if_failure(unit.type() == ExpressionNode::Type::Unit, "Should be parsed as a Unit");

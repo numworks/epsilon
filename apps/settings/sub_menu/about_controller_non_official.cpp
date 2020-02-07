085b557ac3e37a35d7716e557ec7462229b8d986
Émilie Feral <emilie.feral@numworks.com>
2020-02-07 10:34:37 +0100

    [apps] Add comment around warning regarding non-official releases

--- apps/settings/sub_menu/about_controller_non_official.cpp
+++ apps/settings/sub_menu/about_controller_non_official.cpp
@@ -5,8 +5,10 @@ namespace Settings {
 
 void AboutController::viewWillAppear() {
   GenericSubController::viewWillAppear();
+  // --------------------- Please don't edit these lines ----------------------
   I18n::Message cautionMessages[] = {I18n::Message::AboutWarning1, I18n::Message::AboutWarning2, I18n::Message::AboutWarning3, I18n::Message::AboutWarning4};
   m_view.setMessages(cautionMessages, sizeof(cautionMessages)/sizeof(I18n::Message));
+  // --------------------------------------------------------------------------
 }
 
 }

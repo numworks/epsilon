481095b0d8b2ad80adb42104694b8b0d19468de3
Romain Goyet <romain.goyet@numworks.com>
2020-02-13 16:17:16 -0500

    [escher] Inline empty functions in ViewController

--- escher/src/view_controller.cpp
+++ escher/src/view_controller.cpp
@@ -1,17 +1,4 @@
 #include <escher/view_controller.h>
 
-ViewController::ViewController(Responder * parentResponder) :
-  Responder(parentResponder)
-{
-}
-
-const char * ViewController::title() {
-  return nullptr;
-}
-
 void ViewController::viewWillAppear() {
 }
-
-void ViewController::viewDidDisappear() {
-}
-

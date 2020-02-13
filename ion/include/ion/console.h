f20224b9b9b9f32f06fbc22f52c8983e4e168f8f
Romain Goyet <romain.goyet@numworks.com>
2020-02-13 16:17:53 -0500

    [ion] Console::writeLine can ommit trailing CR/LF

--- ion/include/ion/console.h
+++ ion/include/ion/console.h
@@ -8,7 +8,7 @@ void writeChar(char c);
 char readChar();
 
 // The lines are NULL-terminated
-void writeLine(const char * line);
+void writeLine(const char * line, bool appendCRLF = true);
 void readLine(char * line, int maxLineLength);
 bool transmissionDone();
 

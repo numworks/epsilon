b06de129025e6fbded11b0c73d4fbfafe12c071c
Romain Goyet <romain.goyet@numworks.com>
2020-02-13 19:03:32 -0500

    [escher] Add a telemetry API based off ion's

---
+++ escher/include/escher/telemetry.h
@@ -0,0 +1,10 @@
+#ifndef ESCHER_TELEMETRY_H
+#define ESCHER_TELEMETRY_H
+
+#if EPSILON_TELEMETRY
+#define TELEMETRY_ID(x) const char * telemetryId() const override { return (x); }
+#else
+#define TELEMETRY_ID(x)
+#endif
+
+#endif

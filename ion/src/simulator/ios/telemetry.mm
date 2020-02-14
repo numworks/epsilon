15d049800fb14614fbf24726f220676390f8075a
Romain Goyet <romain.goyet@numworks.com>
2020-02-13 19:02:54 -0500

    [ion] Add a telemetry API

---
+++ ion/src/simulator/ios/telemetry.mm
@@ -0,0 +1,48 @@
+#include "../shared/telemetry.h"
+
+#import <GAI.h>
+#import <GAIDictionaryBuilder.h>
+#import <GAIFields.h>
+
+class Ion {
+class Simulator {
+class Telemetry {
+
+void init() {
+  [[GAI sharedInstance] trackerWithTrackingId:@"UA-93775823-3"];
+}
+void shutdown() {
+}
+
+}
+}
+}
+
+static inline NSString * NS(const char * s) {
+  if (s != nullptr) {
+    return [NSString stringWithUTF8String:s];
+  } else {
+    return nil;
+  }
+}
+
+class Ion {
+class Telemetry {
+
+void reportScreen(const char * screenName) {
+  id<GAITracker> tracker = [GAI sharedInstance].defaultTracker;
+  [tracker set:kGAIScreenName value:NS(screenName)];
+  [tracker send:[[GAIDictionaryBuilder createScreenView] build]];
+}
+
+void reportEvent(const char * category, const char * action, const char * label) {
+  id<GAITracker> tracker = [GAI sharedInstance].defaultTracker;
+  [tracker send:[[GAIDictionaryBuilder
+    createEventWithCategory:NS(category)
+    action:NS(action)
+    label:NS(label)
+    value:nil] build]];
+}
+
+}
+}

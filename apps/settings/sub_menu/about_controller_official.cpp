f1e39b6709fd3528706a35cd72c46eb389f654f2
Quentin Guidée <quentin.guidee@gmail.com>
2020-02-20 22:14:54 +0100

    Fix compilation in official

--- apps/settings/sub_menu/about_controller_official.cpp
+++ apps/settings/sub_menu/about_controller_official.cpp
@@ -4,7 +4,7 @@ namespace Settings {
 
 void AboutController::viewWillAppear() {
   GenericSubController::viewWillAppear();
-  m_view.setMessages(nullptr, 0);
+  //m_view.setMessages(nullptr, 0);
 }
 
 }

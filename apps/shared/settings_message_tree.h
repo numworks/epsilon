cc6d01cb19da9cf388eda308b226a05f4d5f77fb
Léa Saviot <lea.saviot@numworks.com>
2020-02-21 10:48:20 +0100

    [apps/settings_message_tree] Remove .cpp file

--- apps/shared/settings_message_tree.h
+++ apps/shared/settings_message_tree.h
@@ -1,5 +1,6 @@
 #ifndef SHARED_SETTINGS_MESSAGE_TREE_H
 #define SHARED_SETTINGS_MESSAGE_TREE_H
+
 #include <escher/message_tree.h>
 #include <apps/i18n.h>
 
@@ -10,15 +11,16 @@ public:
   constexpr SettingsMessageTree(I18n::Message label = I18n::Message::Default) :
     MessageTree(label, 0),
     m_children(nullptr)
-  {
-  };
+  {}
+
   template <int N>
   constexpr SettingsMessageTree(I18n::Message label, const SettingsMessageTree (&children)[N] = nullptr) :
     MessageTree(label, N),
     m_children(children)
-  {
-  };
-  const MessageTree * children(int index) const override;
+  {}
+
+  const MessageTree * children(int index) const override { return &m_children[index]; }
+
 private:
   const SettingsMessageTree * m_children;
 };

2913bcb8b51ceabc4b18cb2290550bb906d48777
David <0b101@users.noreply.github.com>
2020-02-26 10:44:52 -0600

    [Fix] Bug with the default display mode

--- apps/calculation/history_view_cell.cpp
+++ apps/calculation/history_view_cell.cpp
@@ -186,7 +186,7 @@ void HistoryViewCell::layoutSubviews(bool force) {
   force);
   KDSize outputSize = m_scrollableOutputView.minimalSizeForOptimalDisplay();
   int singleLine = outputSize.width() + inputSize.width() < bounds().width() - 6;
-  int outputHeight = (singleLine) ? (maxCoordinate(0, inputSize.height() - outputSize.height()) / 2) + maxCoordinate(0, (inputSize.height() - outputSize.height()) / 2) : inputSize.height();
+  int outputHeight = (singleLine && Poincare::Preferences::sharedPreferences()->resultDisplay() == Poincare::Preferences::ResultDisplay::Compact) ? (maxCoordinate(0, inputSize.height() - outputSize.height()) / 2) + maxCoordinate(0, (inputSize.height() - outputSize.height()) / 2) : inputSize.height();
   m_scrollableOutputView.setFrame(KDRect(
     maxCoordinate(0, maxFrameWidth - outputSize.width()),
     outputHeight,

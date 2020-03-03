aa4a33d84940252e511dc83fe3281fcf05f39a7d
Ruben Dashyan <ruben.dashyan@numworks.com>
2020-03-03 10:11:01 +0100

    [poincare/layout_node] Remove unused method

--- poincare/src/layout_node.cpp
+++ poincare/src/layout_node.cpp
@@ -39,16 +39,6 @@ void LayoutNode::draw(KDContext * ctx, KDPoint p, KDColor expressionColor, KDCol
   }
 }
 
-KDPoint LayoutNode::origin() {
-  LayoutNode * p = parent();
-  if (p == nullptr) {
-    return absoluteOrigin();
-  } else {
-    return KDPoint(absoluteOrigin().x() - p->absoluteOrigin().x(),
-        absoluteOrigin().y() - p->absoluteOrigin().y());
-  }
-}
-
 KDPoint LayoutNode::absoluteOrigin() {
   LayoutNode * p = parent();
   if (!m_positioned) {

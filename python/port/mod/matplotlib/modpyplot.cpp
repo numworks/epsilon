18b1975fa1094369d8217b45f72e2f31f5346f13
Romain Goyet <romain.goyet@numworks.com>
2020-03-11 10:01:49 -0400

    [python] Add an "axis" function to the matplotlib module

--- python/port/mod/matplotlib/modpyplot.cpp
+++ python/port/mod/matplotlib/modpyplot.cpp
@@ -1,17 +1,72 @@
 extern "C" {
 #include "modpyplot.h"
 }
+#include <assert.h>
 #include "port.h"
 #include "plot_controller.h"
 
-Matplotlib::PlotController sPlotController;
+Matplotlib::PlotStore * sPlotStore = nullptr;
+Matplotlib::PlotController * sPlotController = nullptr;
+
+// Internal functions
+
+mp_obj_t modpyplot___init__() {
+  static Matplotlib::PlotStore plotStore;
+  static Matplotlib::PlotController plotController(&plotStore);
+  sPlotStore = &plotStore;
+  sPlotController = &plotController;
+  sPlotStore->flush();
+  return mp_const_none;
+}
+
+void modpyplot_gc_collect() {
+  if (sPlotStore != nullptr) {
+    return;
+  }
+  MicroPython::collectRootsAtAddress(
+    reinterpret_cast<char *>(&sPlotStore),
+    sizeof(Matplotlib::PlotStore)
+  );
+}
+
+/* axis(arg)
+ *  - arg = [xmin, xmax, ymin, ymax]
+ *  - arg = True, False
+ * Returns : xmin, xmax, ymin, ymax : float */
+
+mp_obj_t modpyplot_axis(mp_obj_t arg) {
+  mp_obj_is_type(arg, &mp_type_enumerate);
+#warning Use mp_obj_is_bool when upgrading uPy
+  if (mp_obj_is_type(arg, &mp_type_bool)) {
+    sPlotStore->setGrid(mp_obj_is_true(arg));
+  } else {
+    mp_obj_t * items;
+    mp_obj_get_array_fixed_n(arg, 4, &items);
+    sPlotStore->setXMin(mp_obj_get_float(items[0]));
+    sPlotStore->setXMax(mp_obj_get_float(items[1]));
+    sPlotStore->setYMin(mp_obj_get_float(items[2]));
+    sPlotStore->setYMax(mp_obj_get_float(items[3]));
+  }
+
+  // Build the return value
+  mp_obj_t coords[4];
+  coords[0] = mp_obj_new_float(sPlotStore->xMin());
+  coords[1] = mp_obj_new_float(sPlotStore->xMax());
+  coords[2] = mp_obj_new_float(sPlotStore->yMin());
+  coords[3] = mp_obj_new_float(sPlotStore->yMax());
+  return mp_obj_new_tuple(4, coords);
+}
 
 mp_obj_t modpyplot_plot(mp_obj_t x, mp_obj_t y) {
+  assert(sPlotStore != nullptr);
+  sPlotStore->addDots(x, y);
   // Ensure x and y are arrays
   // "Push" x and y on bigger arrays
+  return mp_const_none;
 }
 
 mp_obj_t modpyplot_show() {
   MicroPython::ExecutionEnvironment * env = MicroPython::ExecutionEnvironment::currentExecutionEnvironment();
-  env->displayViewController(&sPlotController);
+  env->displayViewController(sPlotController);
+  return mp_const_none;
 }
--- python/port/mod/matplotlib/plot_store.cpp
+++ python/port/mod/matplotlib/plot_store.cpp
@@ -1,1 +1,69 @@
 #include "plot_store.h"
+
+namespace Matplotlib {
+
+PlotStore::PlotStore() : Shared::InteractiveCurveViewRange(),
+ m_grid(false)
+{
+  flush();
+}
+
+void PlotStore::flush() {
+  m_dots = mp_obj_new_list(0, nullptr);
+}
+
+void PlotStore::addDots(mp_obj_t x, mp_obj_t y) {
+  mp_obj_t items[2] = {x, y};
+  mp_obj_t tuple = mp_obj_new_tuple(2, items);
+  mp_obj_list_append(m_dots, tuple);
+
+  //mp_obj_tuple_t * t = static_cast<mp_obj_tuple_t *>(MP_OBJ_TO_PTR(mp_obj_new_tuple(3, NULL)));
+  //t->items[0] = MP_OBJ_NEW_SMALL_INT(r);
+}
+
+PlotStore::Dot PlotStore::dotAtIndex(int i) {
+  size_t numberOfTuples;
+  mp_obj_t * tuples;
+  mp_obj_list_get(m_dots, &numberOfTuples, &tuples);
+
+  mp_obj_t firstTuple = tuples[0];
+  size_t numberOfObjects;
+  mp_obj_t * objects;
+  mp_obj_tuple_get(firstTuple, &numberOfObjects, &objects);
+
+  mp_obj_t x = objects[0];
+  mp_obj_t y = objects[1];
+
+  size_t numberOfX;
+  mp_obj_t * xValues;
+  mp_obj_list_get(x, &numberOfX, &xValues);
+  size_t numberOfY;
+  mp_obj_t * yValues;
+  mp_obj_list_get(y, &numberOfY, &yValues);
+
+  float goodX = mp_obj_get_float(xValues[i]);
+  float goodY = mp_obj_get_float(yValues[i]);
+
+  return PlotStore::Dot(goodX, goodY, KDColorRed);
+}
+
+int PlotStore::numberOfDots() {
+  size_t numberOfTuples;
+  mp_obj_t * tuples;
+  mp_obj_list_get(m_dots, &numberOfTuples, &tuples);
+
+  mp_obj_t firstTuple = tuples[0];
+  size_t numberOfObjects;
+  mp_obj_t * objects;
+  mp_obj_tuple_get(firstTuple, &numberOfObjects, &objects);
+
+  mp_obj_t x = objects[0];
+
+  size_t numberOfX;
+  mp_obj_t * xValues;
+  mp_obj_list_get(x, &numberOfX, &xValues);
+
+  return numberOfX;
+}
+
+}

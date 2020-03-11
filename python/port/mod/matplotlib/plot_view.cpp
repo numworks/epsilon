71fb3d1941a9870e04e00143b226270031bb55aa
Romain Goyet <romain.goyet@numworks.com>
2020-03-11 10:01:49 -0400

    [python] Add an "axis" function to the matplotlib module

--- python/port/mod/matplotlib/plot_view.cpp
+++ python/port/mod/matplotlib/plot_view.cpp
@@ -4,10 +4,17 @@ namespace Matplotlib {
 
 void PlotView::drawRect(KDContext * ctx, KDRect rect) const {
   ctx->fillRect(rect, KDColorWhite);
-  drawGrid(ctx, rect);
+  if (m_store->grid()) {
+    drawGrid(ctx, rect);
+  }
   drawAxes(ctx, rect);
   drawLabelsAndGraduations(ctx, rect, Axis::Vertical, true);
   drawLabelsAndGraduations(ctx, rect, Axis::Horizontal, true);
+
+  for (int i=0; i<m_store->numberOfDots(); i++) {
+    PlotStore::Dot dot = m_store->dotAtIndex(i);
+    drawDot(ctx, rect, dot.x(), dot.y(), dot.color());
+  }
 }
 
 }

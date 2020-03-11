590c5fd12fc7f6f6a7c1b0c3053daeecb6060513
Romain Goyet <romain.goyet@numworks.com>
2020-03-11 17:53:19 -0400

    [python/matplotlib] Add grid control

--- python/port/mod/matplotlib/modpyplot.h
+++ python/port/mod/matplotlib/modpyplot.h
@@ -4,10 +4,11 @@ mp_obj_t modpyplot___init__();
 void modpyplot_gc_collect();
 
 mp_obj_t modpyplot_axis(mp_obj_t arg);
+mp_obj_t modpyplot_grid(mp_obj_t b);
 mp_obj_t modpyplot_plot(mp_obj_t x, mp_obj_t y);
 mp_obj_t modpyplot_scatter(mp_obj_t x, mp_obj_t y);
 mp_obj_t modpyplot_text(mp_obj_t x, mp_obj_t y, mp_obj_t s);
-//mp_obj_t grid();
+//mp_obj_t arrow();
 //mp_obj_t bar();
 //mp_obj_t hist();
 mp_obj_t modpyplot_show();

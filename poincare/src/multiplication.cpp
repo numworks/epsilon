da6306cb11b4cfc41e147a8c406316b6fe78fd55
Léa Saviot <lea.saviot@numworks.com>
2020-03-13 13:39:39 +0100

    [poincare/multiplication] Interrupt reduction if overflow
    When the overflow is due to the max size a rational can hold, stop the
    reduction, otherwise some false results can appear.
    For instance: 1.0092^50*ln(1.0092) was computed to 0 due to this problem

--- poincare/src/multiplication.cpp
+++ poincare/src/multiplication.cpp
@@ -760,6 +760,14 @@ Expression Multiplication::privateShallowReduce(ExpressionNode::ReductionContext
       if (childAtIndex(0).isNumber()) {
         Number o0 = childAtIndex(0).convert<Rational>();
         Number m = Number::Multiplication(o0, static_cast<Number &>(o));
+        if ((IsInfinity(m, context) || m.isUndefined())
+            && !IsInfinity(o0, context) && !o0.isUndefined()
+            && !IsInfinity(o, context) && !o.isUndefined())
+        {
+          // Stop the reduction due to a multiplication overflow
+          SetInterruption(true);
+          return *this;
+        }
         replaceChildAtIndexInPlace(0, m);
         removeChildAtIndexInPlace(i);
       } else {

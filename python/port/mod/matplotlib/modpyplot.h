a0319905d70c99d4efba4e4e08013a44cf256b92
Romain Goyet <romain.goyet@numworks.com>
2020-03-16 11:07:32 -0400

    [wip] Clean Rect and drawing

--- python/port/mod/matplotlib/modpyplot.h
+++ python/port/mod/matplotlib/modpyplot.h
@@ -4,6 +4,7 @@ mp_obj_t modpyplot___init__();
 void modpyplot_gc_collect();
 
 mp_obj_t modpyplot_axis(mp_obj_t arg);
+mp_obj_t modpyplot_bar(mp_obj_t x, mp_obj_t height);
 mp_obj_t modpyplot_grid(mp_obj_t b);
 mp_obj_t modpyplot_plot(mp_obj_t x, mp_obj_t y);
 mp_obj_t modpyplot_scatter(mp_obj_t x, mp_obj_t y);

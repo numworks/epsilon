e87e2e8fae99a3adc246ddccd6a198835bd9a98f
Émilie Feral <emilie.feral@numworks.com>
2020-03-17 13:50:48 +0100

    [python] matplotlib: label are centered around their position

--- python/port/mod/matplotlib/plot_view.cpp
+++ python/port/mod/matplotlib/plot_view.cpp
@@ -59,7 +59,7 @@ void PlotView::traceLabel(KDContext * ctx, KDRect r, PlotStore::Label label) con
   drawLabel(ctx, r,
     label.x(), label.y(), label.string(),
     KDColorBlack,
-    RelativePosition::Before,
+    RelativePosition::None,
     RelativePosition::None
   );
 }

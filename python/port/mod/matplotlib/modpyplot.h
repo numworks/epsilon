ae6fc32872fe0b801a291fb82a480cc70407497a
Émilie Feral <emilie.feral@numworks.com>
2020-03-18 10:59:18 +0100

    [python] matplotlib port: draft version of arrow method

--- python/port/mod/matplotlib/modpyplot.h
+++ python/port/mod/matplotlib/modpyplot.h
@@ -3,6 +3,7 @@
 mp_obj_t modpyplot___init__();
 void modpyplot_gc_collect();
 
+mp_obj_t modpyplot_arrow(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_axis(mp_obj_t arg);
 mp_obj_t modpyplot_bar(mp_obj_t x, mp_obj_t height);
 mp_obj_t modpyplot_grid(mp_obj_t b);
@@ -10,5 +11,4 @@ mp_obj_t modpyplot_hist(mp_obj_t x);
 mp_obj_t modpyplot_plot(mp_obj_t x, mp_obj_t y);
 mp_obj_t modpyplot_scatter(mp_obj_t x, mp_obj_t y);
 mp_obj_t modpyplot_text(mp_obj_t x, mp_obj_t y, mp_obj_t s);
-//mp_obj_t arrow();
 mp_obj_t modpyplot_show();

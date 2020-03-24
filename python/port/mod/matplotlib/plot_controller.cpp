f5ec825271a0eded274cf203a15fa477afa807ff
Émilie Feral <emilie.feral@numworks.com>
2020-03-24 17:17:04 +0100

    [python] Matplotlib: reload labels in PlotController::viewWillAppear

--- python/port/mod/matplotlib/plot_controller.cpp
+++ python/port/mod/matplotlib/plot_controller.cpp
@@ -2,6 +2,11 @@
 
 namespace Matplotlib {
 
+void PlotController::viewWillAppear() {
+  MicroPython::ExecutionViewControllerHelper::viewWillAppear(this);
+  curveView()->reload();
+}
+
 void PlotController::viewDidDisappear() {
   MicroPython::ExecutionViewControllerHelper::viewDidDisappear(this);
   m_store->flush();

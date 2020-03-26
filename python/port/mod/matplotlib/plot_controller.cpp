179473c5ac46eaa2178a13e85a302504d7355779
Émilie Feral <emilie.feral@numworks.com>
2020-03-26 09:42:36 +0100

    [apps/code][python] ConsoleController doesn't keep any pointer/boolean to know if the sandbox/matplotlib view controller is displayed. Its state won't be always right; instead, use the StackViewController depth.
    This fixes the following bug: when popping the sandbox/matplotlib view
    controller, the first responder token was not given to the console
    controller!

--- python/port/mod/matplotlib/plot_controller.cpp
+++ python/port/mod/matplotlib/plot_controller.cpp
@@ -3,12 +3,10 @@
 namespace Matplotlib {
 
 void PlotController::viewWillAppear() {
-  MicroPython::ExecutionViewControllerHelper::viewWillAppear(this);
   curveView()->reload();
 }
 
 void PlotController::viewDidDisappear() {
-  MicroPython::ExecutionViewControllerHelper::viewDidDisappear(this);
   m_store->flush();
 }
 

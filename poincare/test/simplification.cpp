94e1fdfc7a30b54eb87146958c5ee01b3cfa4f94
Ruben Dashyan <ruben.dashyan@numworks.com>
2020-03-26 14:21:05 +0100

    [poincare/test/simplification] Uncomment passing tests

--- poincare/test/simplification.cpp
+++ poincare/test/simplification.cpp
@@ -305,10 +305,10 @@ QUIZ_CASE(poincare_simplification_units) {
    * expression */
   assert_parsed_expression_simplify_to("0×_s", "0×_s");
   assert_parsed_expression_simplify_to("inf×_s", "inf×_s");
-  //assert_parsed_expression_simplify_to("-inf×_s", "-inf×_s");
+  assert_parsed_expression_simplify_to("-inf×_s", "-inf×_s");
   assert_parsed_expression_simplify_to("2_s+3_s-5_s", "0×_s");
   assert_parsed_expression_simplify_to("normcdf(0,20,3)×_s", "0×_s");
-  //assert_parsed_expression_simplify_to("log(0)×_s", "-inf×_s");
+  assert_parsed_expression_simplify_to("log(0)×_s", "-inf×_s");
   assert_parsed_expression_simplify_to("log(undef)*_s", "undef");
 
   /* Units with invalid exponent */

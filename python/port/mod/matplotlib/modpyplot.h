e8063977672b7c207afaf456b06d20763289ee80
Émilie Feral <emilie.feral@numworks.com>
2020-03-26 14:45:29 +0100

    [python] matplotlib: improve 'grid'

--- python/port/mod/matplotlib/modpyplot.h
+++ python/port/mod/matplotlib/modpyplot.h
@@ -6,7 +6,7 @@ void modpyplot_gc_collect();
 mp_obj_t modpyplot_arrow(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_axis(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_bar(mp_obj_t x, mp_obj_t height);
-mp_obj_t modpyplot_grid(mp_obj_t b);
+mp_obj_t modpyplot_grid(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_hist(mp_obj_t x);
 mp_obj_t modpyplot_plot(mp_obj_t x, mp_obj_t y);
 mp_obj_t modpyplot_scatter(mp_obj_t x, mp_obj_t y);

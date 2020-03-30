e22beaad0059f84d412c833fc2dc8f27578e86d1
Émilie Feral <emilie.feral@numworks.com>
2020-03-30 12:36:08 +0200

    [python] WIP: change change pyplot name: pyplot --> matplotlib.pyplot

--- python/port/mod/matplotlib/modpyplot_table.c
+++ python/port/mod/matplotlib/modpyplot_table.c
@@ -12,7 +12,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_0(modpyplot_show_obj, modpyplot_show);
 STATIC MP_DEFINE_CONST_FUN_OBJ_3(modpyplot_text_obj, modpyplot_text);
 
 STATIC const mp_rom_map_elem_t modpyplot_module_globals_table[] = {
-  { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_pyplot) },
+  { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_matplotlib_dot_pyplot) },
   { MP_ROM_QSTR(MP_QSTR___init__), MP_ROM_PTR(&modpyplot___init___obj) },
   { MP_ROM_QSTR(MP_QSTR_arrow), MP_ROM_PTR(&modpyplot_arrow_obj) },
   { MP_ROM_QSTR(MP_QSTR_axis), MP_ROM_PTR(&modpyplot_axis_obj) },

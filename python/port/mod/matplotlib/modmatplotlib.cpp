000ec9fa0d2f2088e18acb43f5c4bd91f7eee4ca
Émilie Feral <emilie.feral@numworks.com>
2020-03-30 13:58:02 +0200

    [python] matplotlib: make pyplot a submodule of matplotlib

---
+++ python/port/mod/matplotlib/modmatplotlib.cpp
@@ -0,0 +1,13 @@
+extern "C" {
+#include "modmatplotlib.h"
+#include "pyplot/modpyplot.h"
+}
+#include <assert.h>
+
+
+// Internal functions
+
+mp_obj_t modmatplotlib___init__() {
+  modpyplot___init__();
+  return mp_const_none;
+}

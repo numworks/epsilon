7a97d52d5623c37ac32d8e78bebe2b714b66c279
Émilie Feral <emilie.feral@numworks.com>
2020-04-01 16:13:11 +0200

    [python] Change math module test to be true on all platforms

--- python/test/math.cpp
+++ python/test/math.cpp
@@ -5,7 +5,7 @@ QUIZ_CASE(python_math) {
   TestExecutionEnvironment env = init_environement();
   assert_command_execution_succeeds(env, "from math import *");
   assert_command_execution_succeeds(env, "e", "2.718281828459045\n");
-  assert_command_execution_succeeds(env, "gamma(9)", "40320.0\n");
+  assert_command_execution_succeeds(env, "gamma(3)", "2.0\n");
   deinit_environment();
 }
 

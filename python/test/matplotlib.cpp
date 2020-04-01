5b9f43622fbc387431d6a73da9fc754ab1a381e0
Émilie Feral <emilie.feral@numworks.com>
2020-04-01 13:57:11 +0200

    [python] Tidy matplotlib tests

--- python/test/matplotlib.cpp
+++ python/test/matplotlib.cpp
@@ -7,6 +7,10 @@ arrow(2,3,4,5)
 show()
 )";
 
+QUIZ_CASE(python_matplotlib_pyplot_arrow) {
+  assert_script_execution_succeeds(s_pyplotArrowScript);
+}
+
 static const char * s_pyplotAxisScript = R"(#
 from matplotlib.pyplot import *
 axis((2,3,4,5))
@@ -20,6 +24,11 @@ static const char * s_pyplotAxisErrorScript = R"(#
 from matplotlib.pyplot import *
 axis(2,3,4,5)
 )";
+
+QUIZ_CASE(python_matplotlib_pyplot_axis) {
+  assert_script_execution_succeeds(s_pyplotAxisScript, "(2.0, 3.0, 4.0, 5.0)\n");
+  assert_script_execution_fails(s_pyplotAxisErrorScript);
+}
 
 static const char * s_pyplotBarScript = R"(#
 from matplotlib.pyplot import *
@@ -37,12 +46,21 @@ bar([1,2,3],[1,2,3,4],[1,2,3],[1,2,3])
 show()
 )";
 
+QUIZ_CASE(python_matplotlib_pyplot_bar) {
+  assert_script_execution_succeeds(s_pyplotBarScript);
+  assert_script_execution_fails(s_pyplotBarErrorScript);
+}
+
 static const char * s_pyplotGridScript = R"(#
 from matplotlib.pyplot import *
 grid(True)
 grid()
 show()
 )";
+
+QUIZ_CASE(python_matplotlib_pyplot_grid) {
+  assert_script_execution_succeeds(s_pyplotGridScript);
+}
 
 static const char * s_pyplotHistScript = R"(#
 from matplotlib.pyplot import *
@@ -52,6 +70,10 @@ hist([2,3,4,5,6],[0,2,3])
 hist([2,3,4,5,6],[0,2,3, 4,5,6,7])
 show()
 )";
+
+QUIZ_CASE(python_matplotlib_pyplot_hist) {
+  assert_script_execution_succeeds(s_pyplotHistScript);
+}
 
 static const char * s_pyplotPlotScript = R"(#
 from matplotlib.pyplot import *
@@ -65,6 +87,11 @@ from matplotlib.pyplot import *
 plot([2,3,4,5,6],2)
 )";
 
+QUIZ_CASE(python_matplotlib_pyplot_plot) {
+  assert_script_execution_succeeds(s_pyplotPlotScript);
+  assert_script_execution_fails(s_pyplotPlotErrorScript);
+}
+
 static const char * s_pyplotScatterScript = R"(#
 from matplotlib.pyplot import *
 scatter(2,3)
@@ -77,23 +104,17 @@ from matplotlib.pyplot import *
 scatter([2,3,4,5,6],2)
 )";
 
+QUIZ_CASE(python_matplotlib_pyplot_scatter) {
+  assert_script_execution_succeeds(s_pyplotScatterScript);
+  assert_script_execution_fails(s_pyplotScatterErrorScript);
+}
+
 static const char * s_pyplotTextScript = R"(#
 from matplotlib.pyplot import *
 text(2,3,'hello')
 show()
 )";
 
-QUIZ_CASE(python_matplotlib_pyplot) {
-  assert_script_execution_succeeds(s_pyplotArrowScript);
-  assert_script_execution_succeeds(s_pyplotAxisScript, "(2.0, 3.0, 4.0, 5.0)\n");
-  assert_script_execution_fails(s_pyplotAxisErrorScript);
-  assert_script_execution_succeeds(s_pyplotBarScript);
-  assert_script_execution_fails(s_pyplotBarErrorScript);
-  assert_script_execution_succeeds(s_pyplotGridScript);
-  assert_script_execution_succeeds(s_pyplotHistScript);
-  assert_script_execution_succeeds(s_pyplotPlotScript);
-  assert_script_execution_fails(s_pyplotPlotErrorScript);
-  assert_script_execution_succeeds(s_pyplotScatterScript);
-  assert_script_execution_fails(s_pyplotScatterErrorScript);
+QUIZ_CASE(python_matplotlib_pyplot_text) {
   assert_script_execution_succeeds(s_pyplotTextScript);
 }

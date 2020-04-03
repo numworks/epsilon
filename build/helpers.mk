99b916e179ec027b1a73d66d7c8bd085e233404f
Romain Goyet <romain.goyet@numworks.com>
2020-04-02 22:37:34 -0400

    [build] Share multi-app code

--- build/helpers.mk
+++ build/helpers.mk
@@ -34,3 +34,22 @@ endef
 define object_for
 $(call direct_object_for,$(call any_variant,$(1)))
 endef
+
+# Multi-arch helpers
+ifdef ARCHS
+ifndef ARCH
+
+# This rule allow us to build any executable (%) for a specified ARCH ($1)
+# We depend on a phony target to make sure this rule is always executed
+.PHONY: force_remake
+define rule_for_arch_executable
+.PRECIOUS: $$(BUILD_DIR)/$(1)/%.$$(EXE)
+$$(BUILD_DIR)/$(1)/%.$$(EXE): force_remake
+	$(Q) echo "MAKE    ARCH=$(1)"
+	$(Q) $$(MAKE) ARCH=$(1) --silent $$*.$$(EXE)
+endef
+
+$(foreach ARCH,$(ARCHS),$(eval $(call rule_for_arch_executable,$(ARCH))))
+
+endif
+endif

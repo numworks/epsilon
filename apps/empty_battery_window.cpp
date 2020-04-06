b00f300b07500b5fa934612c119ebcfe7f17bf1c
Léa Saviot <lea.saviot@numworks.com>
2020-04-06 11:41:26 +0200

    [apps/i18n] Remove Language::Default
    We never translate a i18n message to something else than the currrent
    language selected

--- apps/empty_battery_window.cpp
+++ apps/empty_battery_window.cpp
@@ -1,5 +1,4 @@
 #include "empty_battery_window.h"
-#include "global_preferences.h"
 #include <apps/i18n.h>
 extern "C" {
 #include <assert.h>
@@ -12,7 +11,7 @@ EmptyBatteryWindow::EmptyBatteryWindow() :
 
 void EmptyBatteryWindow::drawRect(KDContext * ctx, KDRect rect) const {
   ctx->fillRect(bounds(), KDColorWhite);
-  const char * warningMessage = I18n::translate(I18n::Message::LowBattery, GlobalPreferences::sharedGlobalPreferences()->language());
+  const char * warningMessage = I18n::translate(I18n::Message::LowBattery);
   KDSize warningSize = KDFont::LargeFont->stringSize(warningMessage);
   ctx->drawString(warningMessage, KDPoint((Ion::Display::Width - warningSize.width())/2, (Ion::Display::Height - warningSize.height())/2), KDFont::LargeFont);
 }

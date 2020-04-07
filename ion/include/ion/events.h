35d446353fc31a897de3e6234576efd33cf9f82b
Romain Goyet <romain.goyet@numworks.com>
2020-04-07 14:54:28 -0400

    [escher] Avoid long switches on Left/Up/Right/Down

--- ion/include/ion/events.h
+++ ion/include/ion/events.h
@@ -18,6 +18,7 @@ public:
   constexpr Event(int i) : m_id(i){} // TODO: Assert here that i>=0 && i<255
 
   uint8_t id() const { return m_id; }
+  constexpr explicit operator uint8_t() const { return m_id; }
 #if DEBUG
   const char * name() const;
 #endif

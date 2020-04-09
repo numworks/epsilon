b09883b43f92a32e3a7227d85ab65cb10b5d8332
Léa Saviot <lea.saviot@numworks.com>
2020-04-09 10:52:30 +0200

    [liba/openbds] Use OpenBSD and not FreeBSD s_erff.c and s_logbf.c files

--- liba/src/external/openbsd/s_logbf.c
+++ liba/src/external/openbsd/s_logbf.c
@@ -1,7 +1,5 @@
 /* s_logbf.c -- float version of s_logb.c.
  * Conversion to float by Ian Lance Taylor, Cygnus Support, ian@cygnus.com.
- * Taken from openlibm at 5b0e7e981321687ac0abe711fdeb30adcd9da932 and
- * modified for Numworks Epsilon by Neven Sajko <nsajko@gmail.com>
  */
 
 /*
@@ -10,19 +8,13 @@
  *
  * Developed at SunPro, a Sun Microsystems, Inc. business.
  * Permission to use, copy, modify, and distribute this
- * software is freely granted, provided that this notice
+ * software is freely granted, provided that this notice 
  * is preserved.
  * ====================================================
  */
 
-//__FBSDID("$FreeBSD: src/lib/msun/src/s_logbf.c,v 1.9 2008/02/22 02:30:35 das Exp $");
-
 #include "math.h"
-
 #include "math_private.h"
-
-static const float
-two25 = 3.355443200e+07;		/* 0x4c000000 */
 
 float
 logbf(float x)
@@ -32,11 +24,8 @@ logbf(float x)
 	ix &= 0x7fffffff;			/* high |x| */
 	if(ix==0) return (float)-1.0/fabsf(x);
 	if(ix>=0x7f800000) return x*x;
-	if(ix<0x00800000) {
-		x *= two25;		 /* convert subnormal x to normal */
-		GET_FLOAT_WORD(ix,x);
-		ix &= 0x7fffffff;
-		return (float) ((ix>>23)-127-25);
-	} else
-		return (float) ((ix>>23)-127);
+	if((ix>>=23)==0) 			/* IEEE 754 logb */
+		return -126.0; 
+	else
+		return (float) (ix-127); 
 }

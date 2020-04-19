8ea938260168748eb7db43a56a7e97233d126efc
M4x1m3 <M4x1me@protonmail.com>
2020-04-19 13:34:51 +0200

    [ion/3ds] Added keyboard support.

--- ion/src/simulator/3ds/display.cpp
+++ ion/src/simulator/3ds/display.cpp
@@ -4,6 +4,8 @@
 #include <ion/display.h>
 #include <string.h>
 
+#include "keyboard_debug.h"
+
 namespace Ion {
 namespace Simulator {
 namespace Display {
@@ -11,7 +13,9 @@ namespace Display {
 void* pixels;
 
 void init() {
-  gfxSetScreenFormat(GFX_TOP, GSP_BGR8_OES);
+  // gfxSetScreenFormat(GFX_TOP, GSP_BGR8_OES);
+  //  gfxSetScreenFormat(GFX_BOTTOM, GSP_BGR8_OES);
+  gfxSetDoubleBuffering(GFX_BOTTOM, false);
   pixels = nullptr;
 }
 
@@ -25,14 +29,20 @@ void draw() {
   
   for(int i = 0; i < Ion::Display::Width; i++) {
     for(int j = 0; j < Ion::Display::Height; j++) {
-      ((u8*)pixels)[239*3 - j*3 + 0 + i*3*240 + 3*240*40] = Framebuffer::address()[i + j * Ion::Display::Width].blue();
-      ((u8*)pixels)[239*3 - j*3 + 1 + i*3*240 + 3*240*40] = Framebuffer::address()[i + j * Ion::Display::Width].green();
-      ((u8*)pixels)[239*3 - j*3 + 2 + i*3*240 + 3*240*40] = Framebuffer::address()[i + j * Ion::Display::Width].red();
+      u32 pixel = 239*3 - j*3 + i*3*240 + 3*240*40;
+      ((u8*)pixels)[pixel + 0] = Framebuffer::address()[i + j * Ion::Display::Width].blue();
+      ((u8*)pixels)[pixel + 1] = Framebuffer::address()[i + j * Ion::Display::Width].green();
+      ((u8*)pixels)[pixel + 2] = Framebuffer::address()[i + j * Ion::Display::Width].red();
     }
   }
   
-	gfxFlushBuffers();
-	gfxSwapBuffers();
+  u8* fb = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL);
+  memcpy(fb, keyboard_debug_bgr, keyboard_debug_bgr_len);
+  
+  gfxFlushBuffers();
+  gfxSwapBuffers();
+  
+  gspWaitForVBlank();
 }
 
 }
--- ion/src/simulator/3ds/keyboard.cpp
+++ ion/src/simulator/3ds/keyboard.cpp
@@ -3,6 +3,7 @@
 #include "main.h"
 
 #include <3ds.h>
+#include <stdio.h>
 
 void IonSimulatorKeyboardKeyDown(int keyNumber) {
 }
@@ -10,13 +11,136 @@ void IonSimulatorKeyboardKeyDown(int keyNumber) {
 void IonSimulatorKeyboardKeyUp(int keyNumber) {
 }
 
+#define DETECT_KEY(x1, y1, w, h, key) \
+  if (touch.px >= (x1) && touch.py >= (y1) && touch.px <= ((x1) + (w) - 1) && touch.py <= ((y1) + (h) - 1)) state.setKey(key);
+
 namespace Ion {
 namespace Keyboard {
 
+bool m_heldLastFrame = false;
+
 State scan() {
 	hidScanInput();
+	
+	State state;
+	
+	touchPosition touch;
+	hidTouchRead(&touch);
+	
+	
+	if (touch.px == 0 && touch.py == 0) {
+	  if (m_heldLastFrame) {
+	    m_heldLastFrame = false;
+	  }
+	  
+	} else {
+	  if (!m_heldLastFrame) {
+	    m_heldLastFrame = true;
+	    
+      DETECT_KEY(271, 13 , 31, 31, Key::OK)
+      DETECT_KEY(271, 46 , 31, 31, Key::Back)
+      DETECT_KEY(227, 16 , 37, 25, Key::Home)
+      DETECT_KEY(227, 48 , 37, 25, Key::OnOff)
+      
+      DETECT_KEY(174, 9  , 22, 23, Key::Up)
+      DETECT_KEY(174, 54 , 22, 23, Key::Down)
+      DETECT_KEY(151, 32 , 23, 22, Key::Left)
+      DETECT_KEY(196, 32 , 23, 22, Key::Right)
+      
+      
+      DETECT_KEY(3  , 30 , 41, 27, Key::LeftParenthesis)
+      DETECT_KEY(52 , 30 , 41, 27, Key::RightParenthesis)
+      DETECT_KEY(101, 30 , 41, 27, Key::Plus)
+      
+      DETECT_KEY(3  , 66 , 41, 27, Key::Multiplication)
+      DETECT_KEY(52 , 66 , 41, 27, Key::Division)
+      DETECT_KEY(101, 66 , 41, 27, Key::Minus)
+      
+      DETECT_KEY(3  , 102, 41, 27, Key::Seven)
+      DETECT_KEY(52 , 102, 41, 27, Key::Eight)
+      DETECT_KEY(101, 102, 41, 27, Key::Nine)
+      
+      DETECT_KEY(3  , 138, 41, 27, Key::Four)
+      DETECT_KEY(52 , 138, 41, 27, Key::Five)
+      DETECT_KEY(101, 138, 41, 27, Key::Six)
+      
+      DETECT_KEY(3  , 174, 41, 27, Key::One)
+      DETECT_KEY(52 , 174, 41, 27, Key::Two)
+      DETECT_KEY(101, 174, 41, 27, Key::Three)
+      
+      DETECT_KEY(3  , 210, 41, 27, Key::Zero)
+      DETECT_KEY(52 , 210, 41, 27, Key::Dot)
+      DETECT_KEY(101, 210, 41, 27, Key::EE)
+      DETECT_KEY(150, 210, 41, 27, Key::Ans)
+      DETECT_KEY(199, 210, 41, 27, Key::EXE)
+      
+      
+      DETECT_KEY(149, 82 , 34, 23, Key::Sqrt)
+      DETECT_KEY(189, 82 , 34, 23, Key::Square)
+      DETECT_KEY(229, 82 , 34, 23, Key::Comma)
+      DETECT_KEY(269, 82 , 34, 23, Key::Power)
+      
+      DETECT_KEY(149, 114, 34, 23, Key::Shift)
+      DETECT_KEY(189, 114, 34, 23, Key::Alpha)
+      DETECT_KEY(229, 114, 34, 23, Key::XNT)
+      DETECT_KEY(269, 114, 34, 23, Key::Var)
+      
+      DETECT_KEY(149, 146, 34, 23, Key::Exp)
+      DETECT_KEY(189, 146, 34, 23, Key::Ln)
+      DETECT_KEY(229, 146, 34, 23, Key::Log)
+      DETECT_KEY(269, 146, 34, 23, Key::Imaginary)
+      
+      DETECT_KEY(149, 178, 34, 23, Key::Sine)
+      DETECT_KEY(189, 178, 34, 23, Key::Cosine)
+      DETECT_KEY(229, 178, 34, 23, Key::Tangent)
+      DETECT_KEY(269, 178, 34, 23, Key::Pi)
+      
+      
+      DETECT_KEY(245, 212, 34, 23, Key::Toolbox)
+      DETECT_KEY(285, 212, 34, 23, Key::Backspace)
+      
+      
+	    // printf("\x1b[2;0H%03d; %03d", touch.px, touch.py);
+	    // Handle touchscreen here.
+	  }
+	}
+	
+	u32 kDown = hidKeysDown();
+	
+	if (kDown & KEY_DUP) {
+	  state.setKey(Key::Up);
+	}
+	if (kDown & KEY_DDOWN) {
+	  state.setKey(Key::Down);
+	}
+	if (kDown & KEY_DLEFT) {
+	  state.setKey(Key::Left);
+	}
+	if (kDown & KEY_DRIGHT) {
+	  state.setKey(Key::Right);
+	}
+	if (kDown & KEY_A) {
+	  state.setKey(Key::OK);
+	}
+	if (kDown & KEY_B) {
+	  state.setKey(Key::Back);
+	}
+	if (kDown & KEY_Y) {
+	  state.setKey(Key::Shift);
+	}
+	if (kDown & KEY_X) {
+	  state.setKey(Key::Alpha);
+	}
+	if (kDown & KEY_START) {
+	  state.setKey(Key::Home);
+	}
+	if (kDown & KEY_SELECT) {
+	  state.setKey(Key::OnOff);
+	}
+	
   Simulator::Main::refresh();
-  return 0;
+  
+  return state;
 }
 
 }

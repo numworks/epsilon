1a9014645cacbc4314ee7f0b2655470a2b2ccb6b
Émilie Feral <emilie.feral@numworks.com>
2020-04-24 17:44:05 +0200

    [apps] additional_outputs: fill unit additional results

--- apps/calculation/additional_outputs/unit_list_controller.cpp
+++ apps/calculation/additional_outputs/unit_list_controller.cpp
@@ -1,6 +1,13 @@
 #include "unit_list_controller.h"
 #include "../app.h"
 #include "../../shared/poincare_helpers.h"
+#include <poincare/unit_convert.h>
+#include <poincare/multiplication.h>
+#include <poincare/power.h>
+#include <poincare/undefined.h>
+#include <poincare/unit.h>
+
+using namespace Poincare;
 
 namespace Calculation {
 
@@ -9,18 +16,68 @@ int UnitListController::numberOfRows() const { //FIXME
 }
 
 void UnitListController::computeLayoutAtIndex(int index) { //FIXME
-  Poincare::Expression expressionAtIndex;
+  Expression expressionAtIndex = m_expression.clone();
   if (index == 0) {
-    expressionAtIndex = m_expression;
-  } else {
-    assert(index == 1);
-    Poincare::ExpressionNode::ReductionContext reductionContext(
+    // SI units only
+    Shared::PoincareHelpers::Simplify(&expressionAtIndex, App::app()->localContext(), ExpressionNode::ReductionTarget::User, Poincare::ExpressionNode::SymbolicComputation::ReplaceAllDefinedSymbolsWithDefinition, Poincare::ExpressionNode::UnitConversion::InternationalSystem);
+  } else if (index == 1) {
+    ExpressionNode::ReductionContext reductionContext(
         App::app()->localContext(),
-        Poincare::Preferences::sharedPreferences()->complexFormat(),
-        Poincare::Preferences::sharedPreferences()->angleUnit(),
-        Poincare::ExpressionNode::ReductionTarget::SystemForApproximation,
-        Poincare::ExpressionNode::SymbolicComputation::ReplaceAllSymbolsWithDefinitionsOrUndefined);
-    expressionAtIndex = m_expression.reduce(reductionContext);
+        Preferences::sharedPreferences()->complexFormat(),
+        Preferences::sharedPreferences()->angleUnit(),
+        ExpressionNode::ReductionTarget::User,
+        ExpressionNode::SymbolicComputation::ReplaceAllSymbolsWithDefinitionsOrUndefined);
+    Expression units;
+    // Reduce to be able to recognize units
+    expressionAtIndex = expressionAtIndex.reduce(reductionContext);
+    expressionAtIndex = expressionAtIndex.removeUnit(&units);
+    bool requireSimplification = true;
+    bool canChangeUnitPrefix = true;
+    if (Unit::IsISSpeed(units)) {
+      expressionAtIndex = UnitConvert::Builder(
+          m_expression.clone(),
+          Multiplication::Builder(
+            Unit::Kilometer(),
+            Power::Builder(
+              Unit::Hour(),
+              Rational::Builder(-1)
+              )
+            )
+          );
+      canChangeUnitPrefix = false; // We force km/h
+    } else if (Unit::IsISVolume(units)) {
+      expressionAtIndex = UnitConvert::Builder(
+          m_expression.clone(),
+          Unit::Liter()
+          );
+    } else if (Unit::IsISEnergy(units)) {
+      expressionAtIndex = UnitConvert::Builder(
+          m_expression.clone(),
+          Multiplication::Builder(
+              Unit::Watt(),
+              Unit::Hour()
+            )
+          );
+    } else if (Unit::IsISTime(units)) {
+      double value = Shared::PoincareHelpers::ApproximateToScalar<double>(expressionAtIndex, App::app()->localContext());
+      expressionAtIndex = Unit::BuildTimeSplit(value);
+      requireSimplification = false;
+      canChangeUnitPrefix = false;
+    } else {
+      requireSimplification = false;
+      canChangeUnitPrefix = false;
+    }
+
+    if (requireSimplification) {
+      Shared::PoincareHelpers::Simplify(&expressionAtIndex, App::app()->localContext(), ExpressionNode::ReductionTarget::User);
+    }
+    if (canChangeUnitPrefix) {
+      Expression newUnits;
+      expressionAtIndex = expressionAtIndex.removeUnit(&newUnits);
+      double value = Shared::PoincareHelpers::ApproximateToScalar<double>(expressionAtIndex, App::app()->localContext());
+      Unit::ChooseBestMultipleForValue(&newUnits, &value, reductionContext);
+      expressionAtIndex = Multiplication::Builder(Number::FloatNumber(value), newUnits);
+    }
   }
   m_layouts[index] = Shared::PoincareHelpers::CreateLayout(expressionAtIndex);
 }

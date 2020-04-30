c028ab86b6efaef322bd8c1fe9b2afe75faf61c3
Léa Saviot <lea.saviot@numworks.com>
2020-04-30 15:10:57 +0200

    [apps/escher] Move ModalViewEmptyController to escher

--- apps/code/variable_box_empty_controller.h
+++ apps/code/variable_box_empty_controller.h
@@ -1,20 +1,20 @@
 #ifndef APPS_CODE_VARIABLE_BOX_EMPTY_CONTROLLER_H
 #define APPS_CODE_VARIABLE_BOX_EMPTY_CONTROLLER_H
 
-#include <apps/variable_box_empty_controller.h>
+#include <escher/modal_view_empty_controller.h>
 
 namespace Code {
 
-class VariableBoxEmptyController : public ::VariableBoxEmptyController {
+class VariableBoxEmptyController : public ModalViewEmptyController {
 public:
   VariableBoxEmptyController() :
-    ::VariableBoxEmptyController(),
+    ModalViewEmptyController(),
     m_view()
   {}
   // View Controller
   View * view() override { return &m_view; }
 private:
-  class VariableBoxEmptyView : public ::VariableBoxEmptyController::VariableBoxEmptyView {
+  class VariableBoxEmptyView : public ModalViewEmptyController::ModalViewEmptyView {
   public:
     constexpr static int k_numberOfMessages = 1;
     VariableBoxEmptyView();

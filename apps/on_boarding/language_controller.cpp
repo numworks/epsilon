b8244ec0c5cb335f78247bc557e0ba6b130b2179
Léa Saviot <lea.saviot@numworks.com>
2020-05-11 15:37:38 +0200

    [apps] Fix margins of LanguageController
    Add margin at the bottom and fix margin values

--- apps/on_boarding/language_controller.cpp
+++ apps/on_boarding/language_controller.cpp
@@ -1,11 +1,15 @@
 #include "language_controller.h"
 #include "../global_preferences.h"
 #include "../apps_container.h"
+#include <algorithm>
 
 namespace OnBoarding {
 
 LanguageController::LanguageController(Responder * parentResponder) :
-  Shared::LanguageController(parentResponder, (Ion::Display::Height - I18n::NumberOfLanguages*Metric::ParameterCellHeight)/2)
+  Shared::LanguageController(
+    parentResponder,
+    std::max(static_cast<int>(Metric::CommonLeftMargin),
+      (Ion::Display::Height - I18n::NumberOfLanguages*Metric::ParameterCellHeight)/2))
 {
 }
 

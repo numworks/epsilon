44c74a6db521f1d6a9e425bd2e79ab840f69e201
Émilie Feral <emilie.feral@numworks.com>
2020-05-18 10:50:12 +0200

    [python] matplotlib: add color argument to plot, scatter, hist, bar and arrow

--- python/port/mod/matplotlib/pyplot/modpyplot.h
+++ python/port/mod/matplotlib/pyplot/modpyplot.h
@@ -10,6 +10,6 @@ mp_obj_t modpyplot_bar(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_grid(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_hist(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_plot(size_t n_args, const mp_obj_t *args);
-mp_obj_t modpyplot_scatter(mp_obj_t x, mp_obj_t y);
+mp_obj_t modpyplot_scatter(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_text(mp_obj_t x, mp_obj_t y, mp_obj_t s);
 mp_obj_t modpyplot_show();

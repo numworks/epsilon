829922a8d2df0950f71afafe3d110a7fa3a321b3
Léa Saviot <lea.saviot@numworks.com>
2020-05-18 11:42:59 +0200

    [apps/calculation] Calculation::height takes verticalMargin argument

--- apps/calculation/calculation.h
+++ apps/calculation/calculation.h
@@ -84,7 +84,7 @@ public:
   Poincare::Layout createApproximateOutputLayout(Poincare::Context * context, bool * couldNotCreateApproximateLayout);
 
   // Memoization of height
-  KDCoordinate height(Poincare::Context * context, bool expanded = false, bool forceSingleLine = false);
+  KDCoordinate height(Poincare::Context * context, float verticalMargin, bool expanded, bool forceSingleLine);
 
   // Displayed output
   DisplayOutput displayOutput(Poincare::Context * context);

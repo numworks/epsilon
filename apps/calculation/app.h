d77e924014413e03f41cf5d3227f286a17d5449b
Arthur <arthur@MacBook-Pro-de-Arthur.local>
2020-05-20 11:12:56 +0200

    [Apps/Calculation] Modified the calculationStore of additional results
    -Additional results are now stored in smaller calculationStores
    
    Change-Id: I7d09bfb09e57cbb80e9385668adeb29a31398bc0

--- apps/calculation/app.h
+++ apps/calculation/app.h
@@ -19,12 +19,16 @@ public:
   };
   class Snapshot : public ::App::Snapshot {
   public:
+    Snapshot();
     App * unpack(Container * container) override;
     void reset() override;
     Descriptor * descriptor() override;
     CalculationStore * calculationStore() { return &m_calculationStore; }
   private:
     CalculationStore m_calculationStore;
+    // Set the size of the buffer needed to store the calculations
+    static constexpr int k_calculationBufferSize = 10 * (sizeof(Calculation) + Calculation::k_numberOfExpressions * Constant::MaxSerializedExpressionSize + sizeof(Calculation *));
+    char m_calculationBuffer[k_calculationBufferSize];
   };
   static App * app() {
     return static_cast<App *>(Container::activeApp());
@@ -34,6 +38,7 @@ public:
   bool layoutFieldDidReceiveEvent(::LayoutField * layoutField, Ion::Events::Event event) override;
   // TextFieldDelegateApp
   bool isAcceptableExpression(const Poincare::Expression expression) override;
+
 private:
   App(Snapshot * snapshot);
   HistoryController m_historyController;

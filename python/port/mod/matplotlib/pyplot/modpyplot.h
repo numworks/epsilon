649b4a486840582054b8538fad483ad074928f55
Arthur Camouseigt <arthur.camouseigt@numworks.com>
2020-05-25 14:38:58 +0200

    [matplotlib/modpyplot.cpp] Adding keyword arguments support for plot function
    plot function can now take into account the following keywords arguments :
    - color
    
    Change-Id: I29b82ba25fa68ec4d8e1797112f133440dee22a0

--- python/port/mod/matplotlib/pyplot/modpyplot.h
+++ python/port/mod/matplotlib/pyplot/modpyplot.h
@@ -9,7 +9,7 @@ mp_obj_t modpyplot_axis(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_bar(size_t n_args, const mp_obj_t *args, mp_map_t* kw_args);
 mp_obj_t modpyplot_grid(size_t n_args, const mp_obj_t *args);
 mp_obj_t modpyplot_hist(size_t n_args, const mp_obj_t *args, mp_map_t* kw_args);
-mp_obj_t modpyplot_plot(size_t n_args, const mp_obj_t *args);
+mp_obj_t modpyplot_plot(size_t n_args, const mp_obj_t *args, mp_map_t* kw_args);
 mp_obj_t modpyplot_scatter(size_t n_args, const mp_obj_t *args, mp_map_t* kw_args);
 mp_obj_t modpyplot_text(mp_obj_t x, mp_obj_t y, mp_obj_t s);
 mp_obj_t modpyplot_show();
--- python/port/mod/matplotlib/pyplot/modpyplot_table.c
+++ python/port/mod/matplotlib/pyplot/modpyplot_table.c
@@ -6,7 +6,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modpyplot_axis_obj, 0, 1, modpyplot_a
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(modpyplot_bar_obj, 2, modpyplot_bar);
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modpyplot_grid_obj, 0, 1, modpyplot_grid);
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(modpyplot_hist_obj, 1, modpyplot_hist);
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modpyplot_plot_obj, 1, 3, modpyplot_plot);
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(modpyplot_plot_obj, 1, modpyplot_plot);
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(modpyplot_scatter_obj, 2, modpyplot_scatter);
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(modpyplot_show_obj, modpyplot_show);
 STATIC MP_DEFINE_CONST_FUN_OBJ_3(modpyplot_text_obj, modpyplot_text);

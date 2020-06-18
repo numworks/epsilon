9556ca0bce8f06018f5098c9290d59ec284388b1
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-06-18 17:25:08 +0200

    [apps/shared] CountryController
    Created a CountryController class to chose the country in the settings
    and at onboarding.
    
    Change-Id: I6787e8b45744a8a98e83f59e5d2c4760364635cb

---
+++ apps/shared/country_controller.h
@@ -0,0 +1,37 @@
+#ifndef SHARED_COUNTRY_CONTROLLER_H
+#define SHARED_COUNTRY_CONTROLLER_H
+
+#include <escher.h>
+#include <apps/i18n.h>
+
+namespace Shared {
+
+class CountryController : public ViewController, public SimpleListViewDataSource, public SelectableTableViewDataSource {
+public:
+  CountryController(Responder * parentResponder, KDCoordinate verticalMargin);
+  void resetSelection();
+
+  View * view() override { return &m_selectableTableView; }
+  const char * title() override {return I18n::translate(I18n::Message::Country); }
+  void didBecomeFirstResponder() override {Container::activeApp()->setFirstResponder(&m_selectableTableView); }
+  void viewWillAppear() override;
+  bool handleEvent(Ion::Events::Event event) override;
+
+  int numberOfRows() const override { return I18n::NumberOfCountries; }
+  KDCoordinate cellHeight() override { return Metric::ParameterCellHeight; }
+  HighlightCell * reusableCell(int index) override { return &m_cells[index]; }
+  int reusableCellCount() const override { return I18n::NumberOfCountries; }
+
+  void willDisplayCellForIndex(HighlightCell * cell, int index) override;
+
+protected:
+  SelectableTableView m_selectableTableView;
+
+private:
+  MessageTableCell m_cells[I18n::NumberOfCountries];
+  // TODO : Add variables for static text
+};
+
+}
+
+#endif

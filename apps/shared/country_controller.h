fc663521cfdd672e3ac2b095e469a4bc58ca2f75
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-06-22 11:32:43 +0200

    [apps/settings] Added message to country menu
    Change-Id: Iec064d0ac86f54a10cf63236b6f958d53cecb2e2

--- apps/shared/country_controller.h
+++ apps/shared/country_controller.h
@@ -14,9 +14,9 @@ public:
   CountryController(Responder * parentResponder, KDCoordinate verticalMargin);
   virtual void resetSelection();
 
-  View * view() override { return &m_selectableTableView; }
-  const char * title() override {return I18n::translate(I18n::Message::Country); }
-  void didBecomeFirstResponder() override {Container::activeApp()->setFirstResponder(&m_selectableTableView); }
+  View * view() override { return &m_contentView; }
+  const char * title() override { return I18n::translate(I18n::Message::Country); }
+  void didBecomeFirstResponder() override { Container::activeApp()->setFirstResponder(selectableTableView()); }
   void viewWillAppear() override;
   bool handleEvent(Ion::Events::Event event) override;
 
@@ -28,11 +28,32 @@ public:
   void willDisplayCellForIndex(HighlightCell * cell, int index) override;
 
 protected:
-  SelectableTableView m_selectableTableView;
+  class ContentView : public View {
+  public:
+    ContentView(CountryController * controller, SelectableTableViewDataSource * dataSource);
+    SelectableTableView * selectableTableView() { return &m_selectableTableView; }
+    void drawRect(KDContext * ctx, KDRect rect) const override;
+    void shouldDisplayTitle(bool flag) { m_displayTitle = flag; }
+  protected:
+    void layoutSubviews(bool force = false) override;
+    KDCoordinate layoutTitleSubview(bool force, KDCoordinate verticalOrigin);
+    KDCoordinate layoutSubtitleSubview(bool force, KDCoordinate verticalOrigin);
+    KDCoordinate layoutTableSubview(bool force, KDCoordinate verticalOrigin);
+  private:
+    constexpr static int k_numberOfTextLines = 2;
+    int numberOfSubviews() const override { return 1 + 1 + k_numberOfTextLines; }
+    View * subviewAtIndex(int i) override;
+    SelectableTableView m_selectableTableView;
+    MessageTextView m_titleMessage;
+    MessageTextView m_messageLines[k_numberOfTextLines];
+    bool m_displayTitle;
+  };
+
+  SelectableTableView * selectableTableView() { return m_contentView.selectableTableView(); }
+  ContentView m_contentView;
 
 private:
   MessageTableCell m_cells[I18n::NumberOfCountries];
-  // TODO : Add variables for static text
 };
 
 }

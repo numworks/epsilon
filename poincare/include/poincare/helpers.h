3ad6217f0cdac48494688740e24bb39cb03d6977
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-06-24 14:42:29 +0200

    [poincare] Move IsApproximatelyEqual to a better helper
    Change-Id: I056a96b3721005e01c6ef3f166a80a08195ff338

--- poincare/include/poincare/helpers.h
+++ poincare/include/poincare/helpers.h
@@ -11,7 +11,6 @@ namespace Helpers {
 size_t AlignedSize(size_t realSize, size_t alignment);
 size_t Gcd(size_t a, size_t b);
 bool Rotate(uint32_t * dst, uint32_t * src, size_t len);
-bool IsApproximatelyEqual(double observedValue, double expectedValue, double precision, double reference);
 
 }
 

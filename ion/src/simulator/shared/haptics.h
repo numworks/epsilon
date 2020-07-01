f92a6a6a6653a51e784adb0b0ab671c469661844
Léa Saviot <lea.saviot@numworks.com>
2020-07-01 15:58:24 +0200

    [ion/simulator] Add haptic feedback

---
+++ ion/src/simulator/shared/haptics.h
@@ -0,0 +1,16 @@
+#ifndef ION_SIMULATOR_HAPTICS_H
+#define ION_SIMULATOR_HAPTICS_H
+
+namespace Ion {
+namespace Simulator {
+namespace Haptics {
+
+void init();
+void perform();
+void shutdown();
+
+}
+}
+}
+
+#endif

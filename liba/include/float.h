15988f0fae547291accd6da971d5457e295161df
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-07-03 16:41:52 +0200

    [liba/float] Redefined FLT_EPSILON
    FLT_EPSILON is now compliant with the definition : the difference
    between 1 and the next representable float.
    
    Change-Id: I4f79c3eee93447e76893d850bd84265c9d45aca5

--- liba/include/float.h
+++ liba/include/float.h
@@ -5,10 +5,8 @@
 
 #define FLT_MAX 1E+37f
 #define FLT_MIN 1E-37f
-/* TODO Redefine FLT_EPSILON to comply with the definition :
- * FLT_EPSILON is the difference between 1 and the very next floating point
- * number. */
-#define FLT_EPSILON 1E-5f
+#define FLT_EPSILON 1.1920928955078125e-7
+
 #define DBL_MAX 1.79769313486231571e+308
 #define DBL_MIN 2.22507385850720138e-308
 #define DBL_EPSILON 2.2204460492503131e-16

771f86997056b427ab7bcc2e0344422b0ad28fe3
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-07-17 14:22:29 +0200

    [apps/i18n] Moved country preferences
    Instead of being hardcoded in the python script, preferences specific to
    each country are defined in the country_preferences.h and .csv files.
    
    Change-Id: I71e276341e1586935b4d5814be5b1be80fa170a0

--- apps/exam_mode_configuration_official.cpp
+++ apps/exam_mode_configuration_official.cpp
@@ -6,12 +6,12 @@
 constexpr Shared::SettingsMessageTree ExamModeConfiguration::s_modelExamChildren[2] = {Shared::SettingsMessageTree(I18n::Message::ActivateExamMode), Shared::SettingsMessageTree(I18n::Message::ActivateDutchExamMode)};
 
 int ExamModeConfiguration::numberOfAvailableExamMode() {
-  if (GlobalPreferences::sharedGlobalPreferences()->availableExamModes() == I18n::AvailableExamModes::StandardOnly
+  if (GlobalPreferences::sharedGlobalPreferences()->availableExamModes() == CountryPreferences::AvailableExamModes::StandardOnly
       || GlobalPreferences::sharedGlobalPreferences()->isInExamMode())
   {
     return 1;
   }
-  assert(GlobalPreferences::sharedGlobalPreferences()->availableExamModes() == I18n::AvailableExamModes::All);
+  assert(GlobalPreferences::sharedGlobalPreferences()->availableExamModes() == CountryPreferences::AvailableExamModes::All);
   return 2;
 }
 

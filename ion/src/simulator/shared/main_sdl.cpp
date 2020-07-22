b11c58f11c59e651d9b9bfe50ab4e6ad64083683
M4x1m3 <M4x1me@protonmail.com>
2020-07-22 17:16:33 +0200

    [simu] Persistant script store (and other stuff)

--- ion/src/simulator/shared/main_sdl.cpp
+++ ion/src/simulator/shared/main_sdl.cpp
@@ -10,12 +10,20 @@
 #include <stdio.h>
 #include <ion/timing.h>
 #include <ion/events.h>
+#include <ion/storage.h>
 #include <SDL.h>
 #include <vector>
+#include <string>
 
 static bool argument_screen_only = false;
 static bool argument_fullscreen = false;
 static bool argument_unresizable = false;
+static bool argument_volatile = false;
+static char* pref_path = nullptr;
+static char* file_buffer = nullptr;
+
+static void loadPython(std::vector<const char *>* arguments);
+static void savePython();
 
 void Ion::Timing::msleep(uint32_t ms) {
   SDL_Delay(ms);
@@ -26,9 +34,20 @@ void print_help(char * program_name) {
   printf("Options:\n");
   printf("  -f, --fullscreen          Starts the emulator in fullscreen\n");
   printf("  -s, --screen-only         Disable the keyboard.\n");
+  printf("  -v, --volatile            Disable saving and loading python scripts from file.\n");
   printf("  -u, --unresizable         Disable resizing the window.\n");
   printf("  -h, --help                Show this help menu.\n");
 }
+
+int event_filter(void* userdata, SDL_Event* e) {
+  if (e->type == SDL_APP_TERMINATING || e->type == SDL_APP_WILLENTERBACKGROUND) {
+    savePython();
+  }
+
+  return 1;
+}
+
+
 
 int main(int argc, char * argv[]) {
   std::vector<const char *> arguments(argv, argv + argc);
@@ -43,6 +62,8 @@ int main(int argc, char * argv[]) {
       argument_fullscreen = true;
     } else if(strcmp(argv[i], "-u")==0 || strcmp(argv[i], "--unresizable")==0) {
       argument_unresizable = true;
+    } else if(strcmp(argv[i], "-v")==0 || strcmp(argv[i], "--volatile")==0) {
+      argument_volatile = true;
     }
   }
 
@@ -57,11 +78,121 @@ int main(int argc, char * argv[]) {
     arguments.push_back(language);
   }
 
+  if (!argument_volatile) {
+    loadPython(&arguments);
+    SDL_SetEventFilter(event_filter, NULL);
+  }
+
   Ion::Simulator::Main::init();
+
   ion_main(arguments.size(), &arguments[0]);
+
+  if (!argument_volatile) {
+    savePython();
+  }
+  
   Ion::Simulator::Main::quit();
 
+  if (file_buffer != nullptr)
+    SDL_free(file_buffer);
+  if (pref_path != nullptr)
+    SDL_free(pref_path);
+
   return 0;
+}
+
+static void loadPython(std::vector<const char *>* arguments) {
+  pref_path = SDL_GetPrefPath("io.github.omega", "omega-simulator");
+  std::string path(pref_path);
+  printf("Loading from %s\n", (path + "python.dat").c_str());
+  
+  SDL_RWops* save_file = SDL_RWFromFile((path + "python.dat").c_str(), "rb");
+  
+  if (save_file != NULL) {
+    // Calculate checksum
+    uint64_t checksum = 0;
+    uint64_t calc_checksum = 0;
+    
+    SDL_RWread(save_file, &checksum, sizeof(uint64_t), 1);
+    
+    uint8_t curr_check = 0;
+    
+    while(SDL_RWread(save_file, &curr_check, sizeof(uint8_t), 1)) {
+      calc_checksum += curr_check;
+    }
+    
+    if (checksum == calc_checksum) {
+      arguments->push_back("--code-wipe");
+      arguments->push_back("true");
+      
+      uint64_t length = SDL_RWseek(save_file, 0, RW_SEEK_END) - sizeof(uint64_t);
+      
+      SDL_RWseek(save_file, sizeof(uint64_t), RW_SEEK_SET);
+      
+      file_buffer = (char*) SDL_malloc(length);
+      SDL_RWread(save_file, file_buffer, length, 1);
+      
+      // printf("Length: %ld\n", length);
+      size_t i = 0;
+      while(i < length) {
+        uint16_t size = *(uint16_t*)(file_buffer + i);
+        arguments->push_back("--code-script");
+        arguments->push_back((char*)(file_buffer + i + sizeof(uint16_t)));
+        // printf("Loaded size=%d i=%ld, %s\n", size, i+size, (char*)(file_buffer + i + sizeof(uint16_t)));
+        i += size + sizeof(uint16_t);
+      }
+    }
+  }
+}
+
+static void savePython() {
+  std::string path(pref_path);
+
+  printf("Saving to %s\n", (path + "python.dat").c_str());
+  
+  SDL_RWops* save_file = SDL_RWFromFile((path + "python.dat").c_str(), "wb+");
+
+  if (save_file != NULL) {
+    
+    // Placeholder for checksum
+    uint64_t checksum = 0;
+    SDL_RWwrite(save_file, &checksum, sizeof(uint64_t), 1);
+    
+    uint16_t num = (uint16_t) Ion::Storage::sharedStorage()->numberOfRecordsWithExtension("py");
+    
+    // Write all checksums
+    for(uint16_t i = 0; i < num; i++) {
+      Ion::Storage::Record record = Ion::Storage::sharedStorage()->recordWithExtensionAtIndex("py", i);
+      
+      const char* record_name = record.fullName();
+      uint16_t record_name_len = strlen(record_name);
+      
+      Ion::Storage::Record::Data record_data = record.value();
+      
+      uint16_t total_length = record_name_len + record_data.size;
+      
+      SDL_RWwrite(save_file, &total_length, sizeof(uint16_t), 1);
+      
+      SDL_RWwrite(save_file, record_name, record_name_len, 1);
+      SDL_RWwrite(save_file, ":", 1, 1);
+      // Remove import status, keep trailing \x00
+      SDL_RWwrite(save_file, ((char*)record_data.buffer + 1), record_data.size - 1, 1);
+    }
+    
+    // Compute and write checksum
+    
+    SDL_RWseek(save_file, sizeof(uint64_t), RW_SEEK_SET);
+    uint8_t curr_check = 0;
+    
+    while(SDL_RWread(save_file, &curr_check, sizeof(uint8_t), 1)) {
+      checksum += curr_check;
+    }
+    
+    SDL_RWseek(save_file, 0, RW_SEEK_SET);
+    SDL_RWwrite(save_file, &checksum, sizeof(uint64_t), 1);
+    
+    SDL_RWclose(save_file);
+  }
 }
 
 namespace Ion {

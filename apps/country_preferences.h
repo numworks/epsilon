d6c3eb7b865c97d047ee6b16475fe8f0d5401bd6
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-07-22 16:43:25 +0200

    [apps/home] Apps placement defined by country
    Instead of relying on the order in which the apps are passed at compile
    time, the look of the Home app is defined in a config file, and depends
    on the country chosen.
    
    Change-Id: If7f56a284e0001d6d75ece1e7efdf5fd1d0a9978

--- apps/country_preferences.h
+++ apps/country_preferences.h
@@ -15,20 +15,28 @@ public:
     CumulatedFrequency
   };
 
-  constexpr CountryPreferences(AvailableExamModes availableExamModes, MethodForQuartiles methodForQuartiles, Poincare::Preferences::UnitFormat unitFormat) :
+  enum class HomeAppsLayout : uint8_t {
+    Default,
+    HidePython,
+  };
+
+  constexpr CountryPreferences(AvailableExamModes availableExamModes, MethodForQuartiles methodForQuartiles, Poincare::Preferences::UnitFormat unitFormat, HomeAppsLayout homeAppsLayout) :
     m_availableExamModes(availableExamModes),
     m_methodForQuartiles(methodForQuartiles),
-    m_unitFormat(unitFormat)
+    m_unitFormat(unitFormat),
+    m_homeAppsLayout(homeAppsLayout)
   {}
 
   constexpr AvailableExamModes availableExamModes() const { return m_availableExamModes; }
   constexpr MethodForQuartiles methodForQuartiles() const { return m_methodForQuartiles; }
   constexpr Poincare::Preferences::UnitFormat unitFormat() const { return m_unitFormat; }
+  constexpr HomeAppsLayout homeAppsLayout() const { return m_homeAppsLayout; }
 
 private:
   const AvailableExamModes m_availableExamModes;
   const MethodForQuartiles m_methodForQuartiles;
   const Poincare::Preferences::UnitFormat m_unitFormat;
+  const HomeAppsLayout m_homeAppsLayout;
 };
 
 #endif

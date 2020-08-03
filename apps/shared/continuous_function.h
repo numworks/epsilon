2cdeffc0f3afa29db472e283324b1c977a0b6351
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-08-03 17:10:17 +0200

    [apps/shared] New automatic zoom on curves
    Initial zoom for displaying curves is computed according to the
    following rules :
      - For polar and parametric curves, the algorithm has not changed.
        Vertical and horizontal ranges are chosen in order to display the
        full curve with orthonormal graduations.
      - For cartesian curves, the horizontal range is chosen in order to
        display all points of interest (roots, extrema, asymptotes...).
        The vertical range is fitted to be able to display those points, but
        also cuts out points outside of the function's order of magnitude.
    
    Change-Id: Idf8233fc2e6586b85d34c4da152c83e75513d85c

--- apps/shared/continuous_function.h
+++ apps/shared/continuous_function.h
@@ -70,6 +70,8 @@ public:
   void setTMax(float tMax);
   float rangeStep() const override { return plotType() == PlotType::Cartesian ? NAN : (tMax() - tMin())/k_polarParamRangeSearchNumberOfPoints; }
 
+  void rangeForDisplay(float * xMin, float * xMax, float * yMin, float * yMax, Poincare::Context * context, bool tuneXRange = true) const;
+
   // Extremum
   Poincare::Coordinate2D<double> nextMinimumFrom(double start, double step, double max, Poincare::Context * context) const;
   Poincare::Coordinate2D<double> nextMaximumFrom(double start, double step, double max, Poincare::Context * context) const;
@@ -88,6 +90,12 @@ private:
   typedef Poincare::Coordinate2D<double> (*ComputePointOfInterest)(Poincare::Expression e, char * symbol, double start, double step, double max, Poincare::Context * context);
   Poincare::Coordinate2D<double> nextPointOfInterestFrom(double start, double step, double max, Poincare::Context * context, ComputePointOfInterest compute) const;
   template <typename T> Poincare::Coordinate2D<T> privateEvaluateXYAtParameter(T t, Poincare::Context * context) const;
+
+  // Ranges
+  void fullXYRange(float * xMin, float * xMax, float * yMin, float * yMax, Poincare::Context * context) const;
+  void interestingXAndYRangesForDisplay(float * xMin, float * xMax, float * yMin, float * yMax, Poincare::Context * context, bool tuneXRange = true) const;
+  void refinedYRangeForDisplay(float xMin, float xMax, float * yMin, float * yMax, Poincare::Context * context) const;
+
   /* RecordDataBuffer is the layout of the data buffer of Record
    * representing a ContinuousFunction. See comment on
    * Shared::Function::RecordDataBuffer about packing. */

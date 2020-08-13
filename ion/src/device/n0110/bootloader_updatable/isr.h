e95c33639154b3c8d0acae1417b4aeafa81b1af5
Émilie Feral <emilie.feral@numworks.com>
2020-08-13 14:12:38 +0200

    [ion] Split internal external flashes in two distinct firmwares

--- ion/src/device/n0110/bootloader_updatable/isr.h
+++ ion/src/device/n0110/bootloader_updatable/isr.h
@@ -8,6 +8,7 @@ extern "C" {
 void start();
 void abort();
 void isr_systick();
+//void svcall_handler(unsigned int * args);
 
 #ifdef __cplusplus
 }
---
+++ ion/src/device/n0110/external_flash/isr.h
@@ -0,0 +1,15 @@
+#ifndef ION_DEVICE_BOOT_ISR_EXTERNAL_H
+#define ION_DEVICE_BOOT_ISR_EXTERNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void start();
+void abort();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

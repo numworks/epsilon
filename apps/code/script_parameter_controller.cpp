e7de1968a6b01e2302d151d764fcaf30169cd675
Romain Goyet <romain.goyet@numworks.com>
2020-08-28 17:06:29 -0400

    [apps/code] Add a Script::Status class

--- apps/code/script_parameter_controller.cpp
+++ apps/code/script_parameter_controller.cpp
@@ -42,7 +42,7 @@ bool ScriptParameterController::handleEvent(Ion::Events::Event event) {
         m_menuController->renameSelectedScript();
         return true;
       case 2:
-        m_script.toggleAutoimportationStatus();
+        m_script.toggleAutoImportation();
         m_selectableTableView.reloadData();
         m_menuController->reloadConsole();
         Container::activeApp()->setFirstResponder(&m_selectableTableView);
@@ -81,7 +81,7 @@ HighlightCell * ScriptParameterController::reusableCell(int index) {
 void ScriptParameterController::willDisplayCellForIndex(HighlightCell * cell, int index) {
   if (cell == &m_autoImportScript) {
     SwitchView * switchView = (SwitchView *)m_autoImportScript.accessoryView();
-    switchView->setState(m_script.autoImportationStatus());
+    switchView->setState(m_script.autoImportation());
   }
 }
 

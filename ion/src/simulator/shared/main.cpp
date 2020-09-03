c2a9225912a0fd16a4c982be353ddcbf78a8cfa8
Romain Goyet <romain.goyet@numworks.com>
2020-09-03 17:35:53 -0400

    [ion/simulator] Proper argument parsing

--- ion/src/simulator/shared/main.cpp
+++ ion/src/simulator/shared/main.cpp
@@ -5,25 +5,71 @@
 #include <vector>
 #include <ion.h>
 
-int main(int argc, char * argv[]) {
-  std::vector<const char *> arguments(argv, argv + argc);
+/* The Args class allows parsing and editing command-line arguments
+ * The editing part allows us to add/remove arguments before forwarding them to
+ * ion_main. */
 
-  char * language = IonSimulatorGetLanguageCode();
-  if (language != nullptr) {
-    arguments.push_back("--language");
-    arguments.push_back(language);
+class Args {
+public:
+  Args(int argc, char * argv[]) : m_arguments(argv, argv+argc) {}
+  bool has(const char * key) const;
+  const char * pop(const char * key);
+  void push(const char * key, const char * value) {
+    m_arguments.push_back(key);
+    m_arguments.push_back(value);
+  }
+  void push(const char * argument) { m_arguments.push_back(argument); }
+  int argc() const { return m_arguments.size(); }
+  const char * const * argv() const { return &m_arguments[0]; }
+private:
+  std::vector<const char *>::const_iterator find(const char * name) const;
+  std::vector<const char *> m_arguments;
+};
+
+bool Args::has(const char * name) const {
+  return find(name) != m_arguments.end();
+}
+
+const char * Args::pop(const char * argument) {
+  auto nameIt = find(argument);
+  if (nameIt != m_arguments.end()) {
+    auto valueIt = std::next(nameIt);
+    if (valueIt != m_arguments.end()) {
+      const char * value = *valueIt;
+      m_arguments.erase(valueIt);
+      m_arguments.erase(nameIt);
+      return value;
+    }
+    m_arguments.erase(nameIt);
+  }
+  return nullptr;
+}
+
+std::vector<const char *>::const_iterator Args::find(const char * name) const {
+  return std::find_if(
+    m_arguments.begin(), m_arguments.end(),
+    [name](const char * s) { return strcmp(s, name) == 0; }
+  );
+}
+
+int main(int argc, char * argv[]) {
+  Args args(argc, argv);
+
+  if (!args.has("--language")) {
+    args.push("--language", IonSimulatorGetLanguageCode());
   }
 
+  using namespace Ion::Simulator;
 #if EPSILON_TELEMETRY
-  Ion::Simulator::Telemetry::init();
+  Telemetry::init();
 #endif
-  Ion::Simulator::Window::init();
-  Ion::Simulator::Haptics::init();
-  ion_main(arguments.size(), &arguments[0]);
-  Ion::Simulator::Haptics::shutdown();
-  Ion::Simulator::Window::quit();
+  Window::init();
+  Haptics::init();
+  ion_main(args.argc(), args.argv());
+  Haptics::shutdown();
+  Window::quit();
 #if EPSILON_TELEMETRY
-  Ion::Simulator::Telemetry::shutdown();
+  Telemetry::shutdown();
 #endif
 
   return 0;

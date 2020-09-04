c17a00d3b6d4b1da2b6695fa5add4201b21e23b7
Romain Goyet <romain.goyet@numworks.com>
2020-09-03 22:57:12 -0400

    [ion/simulator] Clean the Keyboard implementation

---
+++ ion/src/simulator/shared/keyboard.h
@@ -0,0 +1,19 @@
+#ifndef ION_SIMULATOR_KEYBOARD_H
+#define ION_SIMULATOR_KEYBOARD_H
+
+#include <ion/keyboard.h>
+#include <SDL.h>
+
+namespace Ion {
+namespace Simulator {
+namespace Keyboard {
+
+void keyDown(Ion::Keyboard::Key k);
+void keyUp(Ion::Keyboard::Key k);
+bool scanHandlesSDLKey(SDL_Scancode key);
+
+}
+}
+}
+
+#endif

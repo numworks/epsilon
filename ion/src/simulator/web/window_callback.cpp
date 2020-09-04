17f23270cf9ade6de0593f13a8b36569e483d582
Romain Goyet <romain.goyet@numworks.com>
2020-09-04 17:05:59 -0400

    [ion/simulator] Clean the callbacks

---
+++ ion/src/simulator/web/window_callback.cpp
@@ -0,0 +1,16 @@
+#include "../shared/window.h"
+#include <emscripten.h>
+
+namespace Ion {
+namespace Simulator {
+namespace Window {
+
+void didRefresh() {
+  /* Notify JS that the display has been refreshed.
+   * This gives us a chance to mirror the display in fullscreen mode. */
+  EM_ASM(if (typeof Module.onDisplayRefresh === "function") { Module.onDisplayRefresh(); });
+}
+
+}
+}
+}

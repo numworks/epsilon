33a8b42f08df32b5b139e8fc54c32fddada3d37f
Romain Goyet <romain.goyet@numworks.com>
2020-09-04 17:05:59 -0400

    [ion/simulator] Clean the callbacks

---
+++ ion/src/simulator/shared/dummy/window.cpp
@@ -0,0 +1,24 @@
+#include "../window.h"
+
+namespace Ion {
+namespace Simulator {
+namespace Window {
+
+void init() {
+  Ion::Simulator::Framebuffer::setActive(false);
+}
+
+void quit() {
+}
+
+void setNeedsRefresh() {
+}
+
+void relayout() {
+}
+
+void refresh() {
+}
+}
+}
+}

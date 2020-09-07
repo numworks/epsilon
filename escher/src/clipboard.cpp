ea36c6e5d7e0f3cbc3742dfb136d6aae5b42877e
Arthur Camouseigt <arthur.camouseigt@numworks.com>
2020-09-07 16:18:49 +0200

    [Clipboard] Changed the general copy/paste
    Fixed issues due to copy/paste of empty formulas. When pasted, empty
    formulas are now recognized by the parser and apear with the
    correct layout
    
    Without this process, copying an empty integral then pasting it gives :
    int((), x, (), ()) instead of drawing an empty integral
    
    Change-Id: I680aaf4eea953149e2d57efa8153ab4d3f93e2a7

--- escher/src/clipboard.cpp
+++ escher/src/clipboard.cpp
@@ -12,6 +12,31 @@ void Clipboard::store(const char * storedText, int length) {
   strlcpy(m_textBuffer, storedText, length == -1 ? TextField::maxBufferSize() : std::min(TextField::maxBufferSize(), length + 1));
 }
 
+const char * Clipboard::storedText() {
+  /* In order to allow copy/paste of empty formulas, we need to add empty
+   * layouts between empty system parenthesis. This way, when the expression
+   * is parsed, it is recognized as a proper formula and appears with the correct
+   * visual layout.
+   * Without this process, copying an empty integral then pasting it gives :
+   * int((), x, (), ()) instead of drawing an empty integral.
+   *
+   * Furthermore, in case the user switches from linear to natural writing mode
+   * we need to add an empty layout between parenthesis to allow proper layout
+   * construction. */
+  constexpr int numberOfPairs = 6;
+  constexpr UTF8Helper::TextPair textPairs[numberOfPairs] = {
+    UTF8Helper::TextPair("()", "(\x11)"),
+    UTF8Helper::TextPair("[]", "[\x11]"),
+    UTF8Helper::TextPair("[,", "[\x11,"),
+    UTF8Helper::TextPair(",,", ",\x11,"),
+    UTF8Helper::TextPair(",]", ",\x11]"),
+    UTF8Helper::TextPair("\x12\x13", "\x12\x11\x13"),
+  };
+
+  UTF8Helper::tryAndReplacePatternsInStringByPatterns(m_textBuffer, TextField::maxBufferSize(), (UTF8Helper::TextPair *) &textPairs, numberOfPairs, true);
+  return m_textBuffer;
+}
+
 void Clipboard::reset() {
   strlcpy(m_textBuffer, "", 1);
 }

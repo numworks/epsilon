2e2af80f6a3305f64b41e351b83c4b91c928d943
Romain Goyet <romain.goyet@numworks.com>
2020-09-10 16:33:46 -0400

    [ion/simulator] Add StateFile support

---
+++ ion/src/simulator/shared/state_file.h
@@ -0,0 +1,15 @@
+#ifndef ION_SIMULATOR_STATE_FILE_H
+#define ION_SIMULATOR_STATE_FILE_H
+
+namespace Ion {
+namespace Simulator {
+namespace StateFile {
+
+void load(const char * filename);
+void save(const char * filename);
+
+}
+}
+}
+
+#endif

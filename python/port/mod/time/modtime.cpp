4a9fffc7abeb000066acaed64cbd15da2ef992dc
Joachim LF <joachimlf@pm.me>
2020-09-13 11:40:16 +0200

    [Python] Added "assertion" of positive time
    [Python] Added "assertion" of positive time

--- python/port/mod/time/modtime.cpp
+++ python/port/mod/time/modtime.cpp
@@ -1,17 +1,21 @@
 extern "C" {
 #include "modtime.h"
+#include <py/runtime.h>
 }
 #include <ion/timing.h>
 #include "../../helpers.h"
 #include <py/smallint.h>
-#include <py/runtime.h>
 
 mp_obj_t modtime_sleep(mp_obj_t seconds_o) {
 #if MICROPY_PY_BUILTINS_FLOAT
-  micropython_port_interruptible_msleep(1000 * mp_obj_get_float(seconds_o));
+  mp_float_t tts = 1000 * mp_obj_get_float(seconds_o);
 #else
-  micropython_port_interruptible_msleep(1000 * mp_obj_get_int(seconds_o));
+  mp_int_t tts = 1000 * mp_obj_get_int(seconds_o);
 #endif
+  if(tts < 0){
+    mp_raise_ValueError("sleep length must be non-negative");
+  }
+  micropython_port_interruptible_msleep(tts);
   return mp_const_none;
 }
 

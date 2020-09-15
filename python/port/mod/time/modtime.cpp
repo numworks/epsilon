4de2bc3367126e5403be94d2d7c3b10b8afdb749
Joachim LF <joachimlf@pm.me>
2020-09-15 07:16:44 +0200

    [Python] Made a few changes in time module

--- python/port/mod/time/modtime.cpp
+++ python/port/mod/time/modtime.cpp
@@ -1,18 +1,19 @@
 extern "C" {
 #include "modtime.h"
+#include <py/smallint.h>
 #include <py/runtime.h>
 }
 #include <ion/timing.h>
 #include "../../helpers.h"
-#include <py/smallint.h>
 
 mp_obj_t modtime_sleep(mp_obj_t seconds_o) {
+  int32_t tts;
 #if MICROPY_PY_BUILTINS_FLOAT
-  mp_float_t tts = 1000 * mp_obj_get_float(seconds_o);
+  tts = 1000 * mp_obj_get_float(seconds_o);
 #else
-  mp_int_t tts = 1000 * mp_obj_get_int(seconds_o);
+  tts = 1000 * mp_obj_get_int(seconds_o);
 #endif
-  if(tts < 0){
+  if (tts < 0) {
     mp_raise_ValueError("sleep length must be non-negative");
   }
   micropython_port_interruptible_msleep(tts);

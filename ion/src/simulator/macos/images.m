76899d9a0e31a281117131d9a1cd8808eed0acee
Émilie Feral <emilie.feral@numworks.com>
2020-09-17 11:53:27 +0200

    [ion] Simulator: key layouts assets are PNG instead of JPG I (fix macos)

--- ion/src/simulator/macos/images.m
+++ ion/src/simulator/macos/images.m
@@ -3,7 +3,7 @@
 #include <SDL.h>
 #include <AppKit/AppKit.h>
 
-SDL_Texture * IonSimulatorLoadImage(SDL_Renderer * renderer, const char * identifier, bool withTransparency, uint8_t alpha) {
+SDL_Texture * IonSimulatorLoadImage(SDL_Renderer * renderer, const char * identifier) {
   NSImage * nsImage = [NSImage imageNamed:[NSString stringWithUTF8String:identifier]];
   CGImageRef cgImage = [nsImage CGImageForProposedRect:NULL
                                                context:NULL
@@ -15,11 +15,12 @@ SDL_Texture * IonSimulatorLoadImage(SDL_Renderer * renderer, const char * identi
   size_t height = CGImageGetHeight(cgImage);
 
   size_t bytesPerPixel = 4;
-  size_t bitsPerPixel = bytesPerPixel*8;
   size_t bytesPerRow = bytesPerPixel * width;
   size_t bitsPerComponent = 8;
 
-  void * bitmapData = malloc(height * width * bytesPerPixel);
+  size_t size = height * width * bytesPerPixel;
+  void * bitmapData = malloc(size);
+  memset(bitmapData, 0, size);
 
   CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
   CGContextRef context = CGBitmapContextCreate(
@@ -33,21 +34,24 @@ SDL_Texture * IonSimulatorLoadImage(SDL_Renderer * renderer, const char * identi
   CGContextRelease(context);
   CGColorSpaceRelease(colorSpace);
 
-  SDL_Surface * surface = SDL_CreateRGBSurfaceWithFormatFrom(
-      bitmapData,
-      width,
-      height,
-      bitsPerPixel,
-      bytesPerRow,
-      SDL_PIXELFORMAT_ABGR8888);
+  SDL_Texture * texture = SDL_CreateTexture(
+    renderer,
+    SDL_PIXELFORMAT_ABGR8888,
+    SDL_TEXTUREACCESS_STATIC,
+    width,
+    height
+  );
 
-  SDL_SetColorKey(surface, withTransparency, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-  SDL_SetSurfaceAlphaMod(surface, alpha);
+  SDL_UpdateTexture(
+    texture,
+    NULL,
+    bitmapData,
+    bytesPerPixel * width
+  );
 
-  SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
 
   free(bitmapData);
-  SDL_FreeSurface(surface);
 
   return texture;
 }

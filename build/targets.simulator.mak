98703180100c13658fc30a302d03d1be3569cb66
Romain Goyet <romain.goyet@numworks.com>
2020-09-17 16:42:28 -0400

    [ion/simulator] Get rid of the headless target

--- build/targets.simulator.mak
+++ build/targets.simulator.mak
@@ -1,8 +1,1 @@
-# Headless targets
-$(eval $(call rule_for_epsilon_flavor,headless))
-HANDY_TARGETS += epsilon.headless
-
-$(BUILD_DIR)/test.headless.$(EXE): $(call flavored_object_for,$(test_runner_src),headless)
-HANDY_TARGETS += test.headless
-
 -include build/targets.simulator.$(TARGET).mak
--- ion/src/simulator/shared/timing.cpp
+++ ion/src/simulator/shared/timing.cpp
@@ -1,9 +1,24 @@
 #include <ion/timing.h>
+#include "window.h"
 #include <chrono>
+#include <SDL.h>
 
 static auto start = std::chrono::steady_clock::now();
 
-uint64_t Ion::Timing::millis() {
+namespace Ion {
+namespace Timing {
+
+uint64_t millis() {
   auto elapsed = std::chrono::steady_clock::now() - start;
   return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
+}
+
+void msleep(uint32_t ms) {
+  if (Simulator::Window::isHeadless()) {
+    return;
+  }
+  SDL_Delay(ms);
+}
+
+}
 }

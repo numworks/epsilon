5c7fb9bd2b90708027d1b208b7403eba8934a483
Émilie Feral <emilie.feral@numworks.com>
2020-09-17 11:54:46 +0200

    [ion] Simulator: key layouts assets are PNG instead of JPG IV (fix linux)

--- ion/src/simulator/linux/assets.py
+++ ion/src/simulator/linux/assets.py
@@ -1,27 +1,29 @@
-# This script generates a .s file representing jpg assets in order to access
+# This script generates a .s file representing assets in order to access
 # them from C code
 
 import sys
 import re
 import argparse
 import io
+import os
 
-parser = argparse.ArgumentParser(description="Process some jpg files.")
-parser.add_argument('--files', nargs='+', help='a list of jpg file names')
+parser = argparse.ArgumentParser(description="Process some asset files.")
+parser.add_argument('--files', nargs='+', help='a list of file names')
 parser.add_argument('--implementation', help='the .s file to generate')
 args = parser.parse_args()
 
-def print_jpg(f, jpg):
-    f.write(".global _ion_simulator_" + jpg + "_start\n")
-    f.write(".global _ion_simulator_" + jpg + "_end\n")
-    f.write("_ion_simulator_" + jpg + "_start:\n")
-    f.write("    .incbin \"ion/src/simulator/assets/" + jpg + ".jpg\"\n")
-    f.write("_ion_simulator_" + jpg + "_end:\n\n")
+def print_asset(f, asset):
+    asset_basename = os.path.splitext(asset)[0]
+    f.write(".global _ion_simulator_" + asset_basename + "_start\n")
+    f.write(".global _ion_simulator_" + asset_basename + "_end\n")
+    f.write("_ion_simulator_" + asset_basename + "_start:\n")
+    f.write("    .incbin \"ion/src/simulator/assets/" + asset + "\"\n")
+    f.write("_ion_simulator_" + asset_basename + "_end:\n\n")
 
 def print(files, path):
     f = open(path, "w")
-    for jpg in files:
-        print_jpg(f, jpg)
+    for asset in files:
+        print_asset(f, asset)
 
     f.close()
 

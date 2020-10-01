e8b4525270b232e686cb620f1f5498672f6984b8
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-01 15:49:23 +0200

    [ion/events] Create event ExternalText
    Added an event to represent the typing of text that is not on the
    device's keyboard. ExternalText event's text is read from a buffer, that
    will be filled when the event is generated.
    
    ExternalText only exists on the simulator.
    
    Change-Id: Ie78d2c7c2de91da986a1ce2130a5ecd123db48ee

--- ion/include/ion/events.h
+++ ion/include/ion/events.h
@@ -229,6 +229,9 @@ constexpr Event TimerFire = Event::Special(2);
 constexpr Event USBEnumeration = Event::Special(3);
 constexpr Event USBPlug = Event::Special(4);
 constexpr Event BatteryCharging = Event::Special(5);
+/* This event is only used in the simulator, to handle text that cannot be
+ * associated with a key. */
+// constexpr Event ExternalText = Event::Special(6);
 
 }
 }

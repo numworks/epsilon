96b8e1859ad8e1aaf0cc810b2230dd8ba368de91
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-01 15:49:23 +0200

    [ion/events] Create event ExternalText
    Added an event to represent the typing of text that is not on the
    device's keyboard. ExternalText event's text is read from a buffer, that
    will be filled when the event is generated.
    
    ExternalText only exists on the simulator.
    
    Change-Id: Ie78d2c7c2de91da986a1ce2130a5ecd123db48ee

--- ion/src/device/shared/events.cpp
+++ ion/src/device/shared/events.cpp
@@ -1,9 +1,25 @@
 #include <ion/events.h>
+#include <layout_events.h>
 
 namespace Ion {
 namespace Events {
 
 void didPressNewKey() {
+}
+
+bool Event::isDefined() const {
+  if (isKeyboardEvent()) {
+    return s_dataForEvent[m_id].isDefined();
+  } else {
+    return (*this == None || *this == Termination || *this == USBEnumeration || *this == USBPlug || *this == BatteryCharging);
+  }
+}
+
+const char * Event::text() const {
+  if (m_id >= 4*PageSize) {
+    return nullptr;
+  }
+  return s_dataForEvent[m_id].text();
 }
 
 }
--- ion/src/simulator/shared/events.cpp
+++ ion/src/simulator/shared/events.cpp
@@ -1,5 +1,7 @@
-#include <ion/events.h>
+#include "events.h"
 #include "haptics.h"
+#include <ion/events.h>
+#include <layout_events.h>
 #include <SDL.h>
 
 namespace Ion {
@@ -7,6 +9,29 @@ namespace Events {
 
 void didPressNewKey() {
   Simulator::Haptics::rumble();
+}
+
+char * sharedExternalTextBuffer() {
+  static char buffer[32];
+  return buffer;
+}
+
+const char * Event::text() const {
+  if (m_id >= 4*PageSize) {
+    if (*this == ExternalText) {
+      return const_cast<const char *>(sharedExternalTextBuffer());
+    }
+    return nullptr;
+  }
+  return s_dataForEvent[m_id].text();
+}
+
+bool Event::isDefined() const {
+  if (isKeyboardEvent()) {
+    return s_dataForEvent[m_id].isDefined();
+  } else {
+    return (*this == None || *this == Termination || *this == USBEnumeration || *this == USBPlug || *this == BatteryCharging || *this == ExternalText);
+  }
 }
 
 }

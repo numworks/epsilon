ce2d1db9ba514b922a5a319a10beb17185e65452
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-06 16:11:28 +0200

    [ion/events] Factor includes of layout_events.h
    Change-Id: Id7f68f4dfb4727453a02437a0b824492a8c94730

--- ion/src/device/shared/events.cpp
+++ ion/src/device/shared/events.cpp
@@ -1,5 +1,4 @@
 #include <ion/events.h>
-#include <layout_events.h>
 
 namespace Ion {
 namespace Events {
@@ -8,18 +7,11 @@ void didPressNewKey() {
 }
 
 bool Event::isDefined() const {
-  if (isKeyboardEvent()) {
-    return s_dataForEvent[m_id].isDefined();
-  } else {
-    return (*this == None || *this == Termination || *this == USBEnumeration || *this == USBPlug || *this == BatteryCharging);
-  }
+  return defaultIsDefined();
 }
 
 const char * Event::text() const {
-  if (m_id >= 4*PageSize) {
-    return nullptr;
-  }
-  return s_dataForEvent[m_id].text();
+  return defaultText();
 }
 
 }

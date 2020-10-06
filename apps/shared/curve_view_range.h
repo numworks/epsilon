a35770d42d943f53d6dabef60980310e191616b6
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-06 11:57:45 +0200

    [graph] Force same grid unit on orthonormal graphs
    Change-Id: I2abca9eb1c4a14057323ee97e1044715f89620ff

--- apps/shared/curve_view_range.h
+++ apps/shared/curve_view_range.h
@@ -27,9 +27,10 @@ public:
   }
   constexpr static float k_maxNumberOfXGridUnits = 18.0f;
   constexpr static float k_maxNumberOfYGridUnits = 13.0f;
-private:
+protected:
   constexpr static float k_minNumberOfXGridUnits = 7.0f;
   constexpr static float k_minNumberOfYGridUnits = 5.0f;
+private:
   /* The grid units is constrained to be a number of type: k*10^n with k = 1,2 or 5
    * and n a relative integer. The choice of x and y grid units depend on the
    * grid range.*/

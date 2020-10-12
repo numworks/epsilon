09c061e871299394a7ec3333aea96ec5049e5ee3
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-12 12:44:30 +0200

    [poincare/zoom] Method RangeFromSingleValue
    This method adds margins to a range whose bounds are equal.
    
    Change-Id: I2b2fe26fe431deda112389060d401f738a75b1ae

--- apps/shared/range_1D.h
+++ apps/shared/range_1D.h
@@ -19,7 +19,7 @@ class __attribute__((packed)) Range1D final {
 public:
   /* If m_min and m_max are too close, we cannot divide properly the range by
    * the number of pixels, which creates a drawing problem. */
-  constexpr static float k_minFloat = 1E-4f;
+  constexpr static float k_minFloat = Poincare::Zoom::k_minimalRangeLength;
   constexpr static float k_default = Poincare::Zoom::k_defaultHalfRange;
   Range1D(float min = -k_default, float max = k_default) :
     m_min(min),

ad80c2c4d75687c3b3bb1a6cd2bb22e1e5a614d3
Léa Saviot <lea.saviot@numworks.com>
2020-10-13 09:54:41 +0200

    [ion/keyboard] Do not define constexpr arrays in headers
    They are duplicated in each compilation unit where they are called

---
+++ ion/include/ion/keyboard/layout_events.h
@@ -0,0 +1,24 @@
+#ifndef ION_KEYBOARD_LAYOUT_EVENTS_H
+#define ION_KEYBOARD_LAYOUT_EVENTS_H
+
+#include <ion/events.h>
+#include <ion/unicode/code_point.h>
+#include "event_data.h"
+
+namespace Ion {
+namespace Events {
+
+extern const EventData s_dataForEvent[4*Event::PageSize];
+
+#ifndef NDEBUG
+extern const const char * s_nameForEvent[255];
+
+inline const char * Event::name() const {
+  return s_nameForEvent[m_id];
+}
+#endif
+
+}
+}
+
+#endif

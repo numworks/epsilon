7499a9e0a4f1390ab3177985e727a690c7713db9
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-15 17:19:48 +0200

    [apps] Set Country from language in main.cpp
    Change-Id: I09f6733fa36e8f0c7870bc26eb8662cda79ae715

--- apps/main.cpp
+++ apps/main.cpp
@@ -38,6 +38,7 @@ void ion_main(int argc, const char * const argv[]) {
       for (int j = 0; j < I18n::NumberOfLanguages; j++) {
         if (strcmp(requestedLanguageId, I18n::translate(I18n::LanguageISO6391Names[j])) == 0) {
           GlobalPreferences::sharedGlobalPreferences()->setLanguage((I18n::Language)j);
+          GlobalPreferences::sharedGlobalPreferences()->setCountry(I18n::DefaultCountryForLanguage[j]);
           break;
         }
       }

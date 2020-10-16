3a87e47de5ec7eceba99c67027cebf2a82cca5e1
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-16 16:10:51 +0200

    [interactive_curve_view_controller] Zoom button
    Renamed zoom button
    
    Change-Id: I476c0991d5ab88c6642b793fbe10debbcca4e014

--- apps/shared/interactive_curve_view_controller.cpp
+++ apps/shared/interactive_curve_view_controller.cpp
@@ -25,7 +25,7 @@ InteractiveCurveViewController::InteractiveCurveViewController(Responder * paren
     graphController->normalizeButtonAction();
     return true;
   }, this), KDFont::SmallFont),
-  m_zoomButton(this, I18n::Message::Navigate, Invocation([](void * context, void * sender) {
+  m_navigationButton(this, I18n::Message::Navigate, Invocation([](void * context, void * sender) {
     InteractiveCurveViewController * graphController = (InteractiveCurveViewController *) context;
     graphController->navigationButtonAction();
     return true;
@@ -138,7 +138,7 @@ int InteractiveCurveViewController::numberOfButtons(ButtonRowController::Positio
 }
 
 Button * InteractiveCurveViewController::buttonAtIndex(int index, ButtonRowController::Position position) const {
-  const Button * buttons[] = {&m_autoButton, &m_normalizeButton, &m_zoomButton, &m_rangeButton};
+  const Button * buttons[] = {&m_autoButton, &m_normalizeButton, &m_navigationButton, &m_rangeButton};
   return (Button *)buttons[index];
 }
 

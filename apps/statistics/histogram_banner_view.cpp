2740056472491c7788deb97e206ced05e578157f
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-21 10:51:17 +0200

    [apps/statiastics] Rename i18n messages accurately
    Change-Id: If88f91d6e925dbb2ad293b537661eb3d137ea826

--- apps/statistics/histogram_banner_view.cpp
+++ apps/statistics/histogram_banner_view.cpp
@@ -7,9 +7,9 @@ namespace Statistics {
 HistogramBannerView::HistogramBannerView() :
   m_intervalLegendView(Font(), I18n::Message::Interval, 0.0f, 0.5f, TextColor(), BackgroundColor()),
   m_intervalView(Font(), 0.0f, 0.5f, TextColor(), BackgroundColor()),
-  m_sizeLegendView(Font(), I18n::Message::Size, 0.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_sizeLegendView(Font(), I18n::Message::Frequency, 0.0f, 0.5f, TextColor(), BackgroundColor()),
   m_sizeView(Font(), 0.0f, 0.5f, TextColor(), BackgroundColor()),
-  m_frequencyLegendView(Font(), I18n::Message::Frequency, 0.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_frequencyLegendView(Font(), I18n::Message::RelativeFrequency, 0.0f, 0.5f, TextColor(), BackgroundColor()),
   m_frequencyView(Font(), 0.0f, 0.5f, TextColor(), BackgroundColor())
 {
 }

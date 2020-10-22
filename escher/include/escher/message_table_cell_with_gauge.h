461d99d874169679a9a9ac0743d5a7ebdb726419
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-22 17:04:03 +0200

    [escher] Add namespace for MessageTableCellWithGauge
    Change-Id: Id3358016fea7458e5b678bf374387c50da8afba2

--- escher/include/escher/message_table_cell_with_gauge.h
+++ escher/include/escher/message_table_cell_with_gauge.h
@@ -4,6 +4,8 @@
 #include <escher/message_table_cell.h>
 #include <escher/gauge_view.h>
 
+namespace Escher {
+
 class MessageTableCellWithGauge : public MessageTableCell {
 public:
   MessageTableCellWithGauge(I18n::Message message = (I18n::Message)0, const KDFont * font = KDFont::SmallFont);
@@ -12,5 +14,7 @@ public:
 private:
   GaugeView m_accessoryView;
 };
+
+}
 
 #endif

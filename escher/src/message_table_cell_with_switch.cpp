a03b6005980c2b9dde4ef11a4aeecccea372b6c3
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-22 17:05:21 +0200

    [escher] Add namespace for MessageTableCellWithSwitch
    Change-Id: Icfa6bbf40265d9da60a4205239ca2dc5eb0efdc0

--- escher/src/message_table_cell_with_switch.cpp
+++ escher/src/message_table_cell_with_switch.cpp
@@ -1,6 +1,8 @@
 #include <escher/message_table_cell_with_switch.h>
 #include <escher/palette.h>
 
+namespace Escher {
+
 MessageTableCellWithSwitch::MessageTableCellWithSwitch(I18n::Message message, const KDFont * font) :
   MessageTableCell(message, font),
   m_accessoryView()
@@ -9,4 +11,6 @@ MessageTableCellWithSwitch::MessageTableCellWithSwitch(I18n::Message message, co
 
 View * MessageTableCellWithSwitch::accessoryView() const {
   return (View *)&m_accessoryView;
+}
+
 }

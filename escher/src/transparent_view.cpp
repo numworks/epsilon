3a0d8be73d7463a2cd063ce51fbe2e649d9c6626
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-22 16:31:55 +0200

    [escher] Add namespace for TransparentView
    Change-Id: I3ea1f9dfeebf2c65d7132fc1dffed754fc9aac92

--- escher/src/transparent_view.cpp
+++ escher/src/transparent_view.cpp
@@ -1,8 +1,12 @@
 #include <escher/transparent_view.h>
 
+namespace Escher {
+
 void TransparentView::markRectAsDirty(KDRect rect) {
   if (m_superview) {
     m_superview->markRectAsDirty(rect.translatedBy(m_frame.origin()));
   }
   View::markRectAsDirty(rect);
+}
+
 }

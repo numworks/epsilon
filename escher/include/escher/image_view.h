9dfd93ba33c198b1f5c88fb3242bf658300a3194
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-22 16:51:40 +0200

    [escher] Add namespace for ImageView
    Change-Id: Id0af810853823e3f332cf84f68ef35dc8daa8355

--- escher/include/escher/image_view.h
+++ escher/include/escher/image_view.h
@@ -4,6 +4,8 @@
 #include <escher/view.h>
 #include <escher/image.h>
 
+namespace Escher {
+
 class ImageView : public View {
 public:
   ImageView();
@@ -12,5 +14,7 @@ public:
 private:
   const Image * m_image;
 };
+
+}
 
 #endif

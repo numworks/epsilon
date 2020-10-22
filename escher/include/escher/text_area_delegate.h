44259e0160fe4f7ce8b0a57e94db8c77426a4a33
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-22 16:42:56 +0200

    [escher] Add namespace for TextAreaDelegate
    Change-Id: If7eec68935ad31c3b1c38b291fb23c7c08a06442

--- escher/include/escher/text_area_delegate.h
+++ escher/include/escher/text_area_delegate.h
@@ -1,6 +1,8 @@
 #ifndef ESCHER_TEXT_AREA_DELEGATE_H
 #define ESCHER_TEXT_AREA_DELEGATE_H
 
+namespace Escher {
+
 class TextArea;
 
 class TextAreaDelegate {
@@ -8,4 +10,5 @@ public:
   virtual bool textAreaDidReceiveEvent(TextArea * textArea, Ion::Events::Event event) = 0;
 };
 
+}
 #endif

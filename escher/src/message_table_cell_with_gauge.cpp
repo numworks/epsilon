f271a9c6c44184b201d4b24f336705b7a7b9ce81
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-22 17:04:03 +0200

    [escher] Add namespace for MessageTableCellWithGauge
    Change-Id: Id3358016fea7458e5b678bf374387c50da8afba2

--- escher/src/message_table_cell_with_gauge.cpp
+++ escher/src/message_table_cell_with_gauge.cpp
@@ -1,6 +1,8 @@
 #include <escher/message_table_cell_with_gauge.h>
 #include <escher/palette.h>
 
+namespace Escher {
+
 MessageTableCellWithGauge::MessageTableCellWithGauge(I18n::Message message, const KDFont * font) :
   MessageTableCell(message, font),
   m_accessoryView()
@@ -15,4 +17,6 @@ void MessageTableCellWithGauge::setHighlighted(bool highlight) {
   MessageTableCell::setHighlighted(highlight);
   KDColor backgroundColor = highlight? Palette::Select : KDColorWhite;
   m_accessoryView.setBackgroundColor(backgroundColor);
+}
+
 }

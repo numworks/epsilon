6ba88921b917fa0c5084d605145f70eebc40cb16
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-27 11:30:09 +0100

    [apps/calculation] Add Unknown NullStatus Todo
    Change-Id: Ic67eca0b95350c72167057c40f18ae969793ef18

--- apps/calculation/additional_outputs/matrix_list_controller.cpp
+++ apps/calculation/additional_outputs/matrix_list_controller.cpp
@@ -52,6 +52,7 @@ void MatrixListController::setExpression(Poincare::Expression e) {
     // 2. Matrix inverse if invertible matrix
     // A squared matrix is invertible if and only if determinant is non null
     if (!determinant.isUndefined() && determinant.nullStatus(context) != ExpressionNode::NullStatus::Null) {
+      // TODO: Handle ExpressionNode::NullStatus::Unknown
       m_indexMessageMap[index] = messageIndex++;
       m_layouts[index++] = getLayoutFromExpression(MatrixInverse::Builder(m_expression.clone()), context, preferences);
     }

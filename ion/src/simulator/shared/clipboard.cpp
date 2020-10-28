686fff6e408c235c565f9cce147543c61b1469b7
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-10-28 16:15:07 +0100

    [ion/clipboard] Update comments 2
    Change-Id: Ib628a888f6712ad6ea62ccb20192845d19e84986

--- ion/src/simulator/shared/clipboard.cpp
+++ ion/src/simulator/shared/clipboard.cpp
@@ -15,8 +15,8 @@ void write(const char * text) {
 }
 
 const char * read() {
-  /* The buffer's size is chosen to be around the size of a typical large
-   * python script, allowing the user the insert most scripts into the
+  /* The buffer size is chosen to be around the size of a typical large
+   * python script, allowing the user to insert most scripts into the
    * simulator using the paste feature. */
   constexpr size_t bufferSize = 8192;
   static char buffer[bufferSize];
@@ -25,6 +25,10 @@ const char * read() {
     return nullptr;
   }
 
+  /* If version has not changed, the user has not copied any text since the
+   * last call to write. A copy of the text already exists in
+   * Escher::Clipboard, and has been translated to best suit the current app :
+   * we return nullptr to use that text.  */
   uint32_t version = crc32Byte(reinterpret_cast<const uint8_t *>(buffer), strlen(buffer));
   if (version == localClipboardVersion) {
     return nullptr;

4d8097bc2cf644343de3b5056d948b0b414fcab9
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-10-28 11:00:33 +0100

    [apps/sequence] Add Escher namespace
    Change-Id: I8a461c15ad1831c97db7ed911fb0b0efcc629db1

--- apps/sequence/app.h
+++ apps/sequence/app.h
@@ -17,16 +17,16 @@ namespace Sequence {
 
 class App : public Shared::FunctionApp {
 public:
-  class Descriptor : public ::App::Descriptor {
+  class Descriptor : public Escher::App::Descriptor {
   public:
     I18n::Message name() override;
     I18n::Message upperName() override;
-    const Image * icon() override;
+    const Escher::Image * icon() override;
   };
   class Snapshot : public Shared::FunctionApp::Snapshot {
   public:
     Snapshot();
-    App * unpack(Container * container) override;
+    App * unpack(Escher::Container * container) override;
     void reset() override;
     Descriptor * descriptor() override;
     Shared::SequenceStore * functionStore() override { return static_cast<Shared::GlobalContext *>(AppsContainer::sharedAppsContainer()->globalContext())->sequenceStore(); }
@@ -38,42 +38,42 @@ public:
     Shared::Interval m_interval;
   };
   static App * app() {
-    return static_cast<App *>(Container::activeApp());
+    return static_cast<App *>(Escher::Container::activeApp());
   }
   Snapshot * snapshot() const {
-    return static_cast<Snapshot *>(::App::snapshot());
+    return static_cast<Snapshot *>(Escher::App::snapshot());
   }
   TELEMETRY_ID("Sequence");
   // TODO: override variableBoxForInputEventHandler to lock sequence in the variable box once they appear there
   // NestedMenuController * variableBoxForInputEventHandler(InputEventHandler * textInput) override;
   CodePoint XNT() override { return 'n'; }
-  NestedMenuController * variableBoxForInputEventHandler(InputEventHandler * textInput) override;
+  Escher::NestedMenuController * variableBoxForInputEventHandler(Escher::InputEventHandler * textInput) override;
   Shared::SequenceContext * localContext() override;
   Shared::SequenceStore * functionStore() override { return static_cast<Shared::GlobalContext *>(AppsContainer::sharedAppsContainer()->globalContext())->sequenceStore(); }
   Shared::Interval * interval() { return snapshot()->interval(); }
   ValuesController * valuesController() override {
     return &m_valuesController;
   }
-  InputViewController * inputViewController() override {
+  Escher::InputViewController * inputViewController() override {
     return &m_inputViewController;
   }
 private:
   App(Snapshot * snapshot);
   Shared::SequenceContext m_sequenceContext;
   ListController m_listController;
-  ButtonRowController m_listFooter;
-  ButtonRowController m_listHeader;
-  StackViewController m_listStackViewController;
+  Escher::ButtonRowController m_listFooter;
+  Escher::ButtonRowController m_listHeader;
+  Escher::StackViewController m_listStackViewController;
   GraphController m_graphController;
-  AlternateEmptyViewController m_graphAlternateEmptyViewController;
-  ButtonRowController m_graphHeader;
-  StackViewController m_graphStackViewController;
+  Escher::AlternateEmptyViewController m_graphAlternateEmptyViewController;
+  Escher::ButtonRowController m_graphHeader;
+  Escher::StackViewController m_graphStackViewController;
   ValuesController m_valuesController;
-  AlternateEmptyViewController m_valuesAlternateEmptyViewController;
-  ButtonRowController m_valuesHeader;
-  StackViewController m_valuesStackViewController;
-  TabViewController m_tabViewController;
-  InputViewController m_inputViewController;
+  Escher::AlternateEmptyViewController m_valuesAlternateEmptyViewController;
+  Escher::ButtonRowController m_valuesHeader;
+  Escher::StackViewController m_valuesStackViewController;
+  Escher::TabViewController m_tabViewController;
+  Escher::InputViewController m_inputViewController;
 };
 
 }

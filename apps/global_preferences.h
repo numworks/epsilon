80e41a3be9d041d828c3e563f34feae94e216530
Léa Saviot <lea.saviot@numworks.com>
2020-11-03 15:40:15 +0100

    [apps/global_preferences] Coding style

--- apps/global_preferences.h
+++ apps/global_preferences.h
@@ -5,34 +5,54 @@
 
 class GlobalPreferences {
 public:
+  static GlobalPreferences * sharedGlobalPreferences();
+
+  I18n::Language language() const { return m_language; }
+  void setLanguage(I18n::Language language) { m_language = language; }
+
+  I18n::Country country() const { return m_country; }
+  void setCountry(I18n::Country country) { m_country = country; }
+
+  // Country preferences
+  CountryPreferences::AvailableExamModes availableExamModes() const {
+    return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].availableExamModes();
+  }
+  CountryPreferences::MethodForQuartiles methodForQuartiles() const {
+    return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].methodForQuartiles();
+  }
+  Poincare::Preferences::UnitFormat unitFormat() const {
+    return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].unitFormat();
+  }
+  CountryPreferences::HomeAppsLayout homeAppsLayout() const {
+    return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].homeAppsLayout();
+  }
+
   enum class ExamMode : int8_t {
     Unknown = -1,
     Off = 0,
     Standard = 1,
     Dutch = 2
   };
-  static GlobalPreferences * sharedGlobalPreferences();
-  I18n::Language language() const { return m_language; }
-  void setLanguage(I18n::Language language) { m_language = language; }
-  I18n::Country country() const { return m_country; }
-  void setCountry(I18n::Country country) { m_country = country; }
-  CountryPreferences::AvailableExamModes availableExamModes() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].availableExamModes(); }
-  CountryPreferences::MethodForQuartiles methodForQuartiles() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].methodForQuartiles(); }
-  Poincare::Preferences::UnitFormat unitFormat() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].unitFormat(); }
-  CountryPreferences::HomeAppsLayout homeAppsLayout() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].homeAppsLayout(); }
   bool isInExamMode() const { return (int8_t)examMode() > 0; }
   ExamMode examMode() const;
   void setExamMode(ExamMode examMode);
+  static_assert((int8_t)GlobalPreferences::ExamMode::Off == 0, "GlobalPreferences::isInExamMode() is not right");
+  static_assert((int8_t)GlobalPreferences::ExamMode::Unknown < 0, "GlobalPreferences::isInExamMode() is not right");
+
   bool showPopUp() const { return m_showPopUp; }
   void setShowPopUp(bool showPopUp) { m_showPopUp = showPopUp; }
+
+  constexpr static int NumberOfBrightnessStates = 12;
   int brightnessLevel() const { return m_brightnessLevel; }
   void setBrightnessLevel(int brightnessLevel);
+
   const KDFont * font() const { return m_font; }
   void setFont(const KDFont * font) { m_font = font; }
-  constexpr static int NumberOfBrightnessStates = 12;
+
 private:
   static_assert(I18n::NumberOfLanguages > 0, "I18n::NumberOfLanguages is not superior to 0"); // There should already have been an error when processing an empty EPSILON_I18N flag
   static_assert(I18n::NumberOfCountries > 0, "I18n::NumberOfCountries is not superior to 0"); // There should already have been an error when processing an empty EPSILON_COUNTRIES flag
+
   GlobalPreferences() :
     m_language((I18n::Language)0),
     m_country((I18n::Country)0),
@@ -42,8 +62,6 @@ private:
     m_font(KDFont::LargeFont) {}
   I18n::Language m_language;
   I18n::Country m_country;
-  static_assert((int8_t)GlobalPreferences::ExamMode::Off == 0, "GlobalPreferences::isInExamMode() is not right");
-  static_assert((int8_t)GlobalPreferences::ExamMode::Unknown < 0, "GlobalPreferences::isInExamMode() is not right");
   mutable ExamMode m_examMode;
   bool m_showPopUp;
   int m_brightnessLevel;

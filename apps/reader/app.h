a0acdc171b44bbe5473bb84e9cfdc996469bd832
Gabriel <gabriel.fournier.79@gmail.com>
2020-11-13 00:48:46 +0100

    Squelette

---
+++ apps/reader/app.h
@@ -0,0 +1,28 @@
+#ifndef READER_H
+#define READER_H
+
+#include <escher.h>
+
+namespace reader {
+
+class App : public ::App {
+public:
+  class Descriptor : public ::App::Descriptor {
+  public:
+    I18n::Message name() override;
+    I18n::Message upperName() override;
+    const Image * icon() override;
+  };
+  class Snapshot : public ::App::Snapshot {
+  public:
+    App * unpack(Container * container) override;
+    Descriptor * descriptor() override;
+  };
+private:
+  App(Snapshot * snapshot);
+
+};
+
+}
+
+#endif

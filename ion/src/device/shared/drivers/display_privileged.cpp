6d97dc1e9607207f9b1ff4c3bd38799657e02727
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-12-03 15:46:15 +0100

    Poc : using registers to store data

--- ion/src/device/shared/drivers/display_privileged.cpp
+++ ion/src/device/shared/drivers/display_privileged.cpp
@@ -13,17 +13,11 @@ using namespace Device::Display;
 
 void pushRectUniform(KDRect r, KDColor c) {
   // Store r and c
-  // register KDRect *r asm ("r2");
-  // register KDColor *c asm ("r3");
   setTempKD(&r);
   setTempC(c);
   pushRectUniformSVC();
   Ion::Timing::msleep(100);
   stampB();
-  // svc(SVC_PUSH_RECT_UNIFORM);
-  // pushRU();
-  // (void)r;
-  // (void)c;
 }
 
 }
@@ -36,32 +30,11 @@ namespace Display {
 
 using namespace Device::Display;
 
-void pushRectSVC() {
-  // Retrieve r and pixels
-  // register KDRect *pR asm ("r2");
-  // register (const KDColor *) *pPixels asm ("r3");
-  // KDRect r =*pR;
-  // const KDColor * pixels = *pPixels;
-#if USE_DMA
-  waitForPendingDMAUploadCompletion();
-#endif
-  pushR();
-  // setDrawingArea(getTempKD(), Orientation::Landscape);
-  // pushPixels(getTempPixels(), getTempKD().width()*getTempKD().height());
-}
-
 void pushRectUniformSVC() {
-  // Retrieve r and c
-  // register KDRect *pR asm ("r2");
-  // register KDColor *pC asm ("r3");
-  // KDRect r = *pR;
-  // const KDColor c= *pC;
 #if USE_DMA
   waitForPendingDMAUploadCompletion();
 #endif
   pushRU();
-  // setDrawingArea(getTempKD(), Orientation::Portrait);
-  // pushColor(getTempC(), getTempKD().width()*getTempKD().height());
 }
 
 }

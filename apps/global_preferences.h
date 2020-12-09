9f9e6499d515c19d84159cf8c6dd0bcb7f28f8ed
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-12-09 15:38:17 +0100

    [apps/solver] Change discriminant symbol for NL and DE countries

--- apps/global_preferences.h
+++ apps/global_preferences.h
@@ -22,6 +22,7 @@ public:
   CountryPreferences::MethodForQuartiles methodForQuartiles() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].methodForQuartiles(); }
   Poincare::Preferences::UnitFormat unitFormat() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].unitFormat(); }
   CountryPreferences::HomeAppsLayout homeAppsLayout() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].homeAppsLayout(); }
+  const char * discriminantSymbol() const { return I18n::CountryPreferencesArray[static_cast<uint8_t>(m_country)].discriminantSymbol(); }
   bool isInExamMode() const { return (int8_t)examMode() > 0; }
   ExamMode examMode() const;
   void setExamMode(ExamMode examMode);

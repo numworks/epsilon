d494bc36852bdc426fa3ea7f447524f9d4e8a42e
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2020-12-09 15:38:17 +0100

    [apps/solver] Change discriminant symbol for NL and DE countries

--- apps/country_preferences.h
+++ apps/country_preferences.h
@@ -20,23 +20,31 @@ public:
     HidePython,
   };
 
-  constexpr CountryPreferences(AvailableExamModes availableExamModes, MethodForQuartiles methodForQuartiles, Poincare::Preferences::UnitFormat unitFormat, HomeAppsLayout homeAppsLayout) :
+  enum class DiscriminantSymbol : uint8_t {
+    Delta,
+    D,
+  };
+
+  constexpr CountryPreferences(AvailableExamModes availableExamModes, MethodForQuartiles methodForQuartiles, Poincare::Preferences::UnitFormat unitFormat, HomeAppsLayout homeAppsLayout, DiscriminantSymbol discriminantSymbol) :
     m_availableExamModes(availableExamModes),
     m_methodForQuartiles(methodForQuartiles),
     m_unitFormat(unitFormat),
-    m_homeAppsLayout(homeAppsLayout)
+    m_homeAppsLayout(homeAppsLayout),
+    m_discriminantSymbol(discriminantSymbol)
   {}
 
   constexpr AvailableExamModes availableExamModes() const { return m_availableExamModes; }
   constexpr MethodForQuartiles methodForQuartiles() const { return m_methodForQuartiles; }
   constexpr Poincare::Preferences::UnitFormat unitFormat() const { return m_unitFormat; }
   constexpr HomeAppsLayout homeAppsLayout() const { return m_homeAppsLayout; }
+  constexpr const char * discriminantSymbol() const { return m_discriminantSymbol == DiscriminantSymbol::Delta ? "Δ" : "D"; }
 
 private:
   const AvailableExamModes m_availableExamModes;
   const MethodForQuartiles m_methodForQuartiles;
   const Poincare::Preferences::UnitFormat m_unitFormat;
   const HomeAppsLayout m_homeAppsLayout;
+  const DiscriminantSymbol m_discriminantSymbol;
 };
 
 #endif

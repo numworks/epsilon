32f988572b7dffff59ce46cf75fe84b813b6396f
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-12-10 18:07:48 +0100

    [poincare/power] Reduce threhsold to not reduce
    Powers of rational numbers were not reduced if the result was larger
    than DBL_MAX. This threshold is lowered to FLT_MAX, to allow for the
    approximation of powers such as 1.0092^50 and their drawing.

--- poincare/test/simplification.cpp
+++ poincare/test/simplification.cpp
@@ -59,7 +59,7 @@ QUIZ_CASE(poincare_simplification_rational) {
   assert_parsed_expression_simplify_to("12348/34564", "3087/8641");
   assert_parsed_expression_simplify_to("1-0.3-0.7", "0");
   assert_parsed_expression_simplify_to("123456789123456789+112233445566778899", "235690234690235688");
-  assert_parsed_expression_simplify_to("56^56", "79164324866862966607842406018063254671922245312646690223362402918484170424104310169552592050323456");
+  assert_parsed_expression_simplify_to("56^56", "56^56");
   assert_parsed_expression_simplify_to("999^999", "999^999");
   assert_parsed_expression_simplify_to("999^-999", "1/999^999");
   assert_parsed_expression_simplify_to("0^0", Undefined::Name());

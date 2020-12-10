f8b126cb3b2c8e9473bfda68102450a4a43ea137
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-12-10 11:17:45 +0100

    [shared] Quit the Navigate menu with OK and EXE

--- apps/shared/zoom_curve_view_controller.cpp
+++ apps/shared/zoom_curve_view_controller.cpp
@@ -1,4 +1,5 @@
 #include "zoom_curve_view_controller.h"
+#include <escher/stack_view_controller.h>
 #include <cmath>
 #include <assert.h>
 
@@ -10,6 +11,9 @@ bool ZoomCurveViewController::handleEvent(Ion::Events::Event event) {
   if (event == Ion::Events::Plus || event == Ion::Events::Minus) {
     return handleZoom(event);
   }
+  if (event == Ion::Events::EXE || event == Ion::Events::OK) {
+    return handleEnter();
+  }
   return false;
 }
 
@@ -17,6 +21,12 @@ bool ZoomCurveViewController::handleZoom(Ion::Events::Event event) {
   float ratio = event == Ion::Events::Plus ? 1.f / k_zoomOutRatio : k_zoomOutRatio;
   interactiveCurveViewRange()->zoom(ratio, xFocus(), yFocus());
   curveView()->reload();
+  return true;
+}
+
+bool ZoomCurveViewController::handleEnter() {
+  Escher::StackViewController * stack = static_cast<Escher::StackViewController *>(parentResponder());
+  stack->pop();
   return true;
 }
 

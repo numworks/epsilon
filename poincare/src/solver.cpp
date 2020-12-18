9f06ef762cd89c204fc730a64665c8cf21b6367c
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-12-18 15:07:08 +0100

    [poincare/solver] Change solver API
    The solver uses SolverHelper<double>::ValueAtAbscissa to represent a
    function.
    Expression methods nextRoot/nextMinimum/etc are shorthands to access the
    Solver methods NextRoot and NextMinimum.

--- poincare/src/solver.cpp
+++ poincare/src/solver.cpp
@@ -89,113 +89,65 @@ bool SolverHelper<T>::RootExistsOnInterval(T fa, T fb, T fc) {
        || ((fb < static_cast<T>(0) && fc > static_cast<T>(0)) || (fb > static_cast<T>(0) && fc < static_cast<T>(0))));
 }
 
-Coordinate2D<double> Solver::BrentMinimum(double ax, double bx, ValueAtAbscissa evaluation, Context * context, Preferences::ComplexFormat complexFormat, Preferences::AngleUnit angleUnit, const void * context1, const void * context2, const void * context3) {
-  /* Bibliography: R. P. Brent, Algorithms for finding zeros and extrema of
-   * functions without calculating derivatives */
-  if (ax > bx) {
-    return BrentMinimum(bx, ax, evaluation, context, complexFormat, angleUnit, context1, context2, context3);
+double Solver::NextRoot(ValueAtAbscissa evaluation, Context * context, const void * auxiliary, double start, double end, double minimalStep, double stepPrecision) {
+  SolverHelper<double>::BracketSearch search = [](double a, double b, double c, double fa, double fb, double fc, SolverHelper<double>::ValueAtAbscissa f, Context * context, const void * auxiliary) {
+    Coordinate2D<double> result(NAN, NAN);
+    if (SolverHelper<double>::RootExistsOnInterval(fa, fb, fc)) {
+      result = Coordinate2D<double>(BrentRoot(a, c, std::fabs(c-b)/k_precisionByGradUnit, f, context, auxiliary), 0.);
+    } else if (SolverHelper<double>::MinimumExistsOnInterval(fa, fb, fc)) {
+      result = BrentMinimum(a, c, f, context, auxiliary);
+    } else if (SolverHelper<double>::MaximumExistsOnInterval(fa, fb, fc)) {
+      const void * pack[2] = { &f, auxiliary };
+      ValueAtAbscissa fOpposite = [](double x, Context * ctx, const void * aux) {
+        ValueAtAbscissa fOriginal = static_cast<const ValueAtAbscissa * const *>(aux)[0][0];
+        const void * auxOriginal = static_cast<const void * const *>(aux)[1];
+        return -fOriginal(x, ctx, auxOriginal);
+      };
+      result = BrentMinimum(a, c, fOpposite, context, pack);
+    }
+    result = RoundCoordinatesToZero(result, a, c, f, context, auxiliary);
+    if (result.x2() == 0.) {
+      return result;
+    }
+    return Coordinate2D<double>(NAN, NAN);
+  };
+  double result = SolverHelper<double>::NextPointOfInterest(evaluation, context, auxiliary, search, start, end, stepPrecision, minimalStep).x1();
+  // Because of float approximation, exact zero is never reached
+  if (std::fabs(result) < stepPrecision * k_zeroPrecision) {
+    return 0.;
   }
-  double e = 0.0;
-  double a = ax;
-  double b = bx;
-  double x = a+k_goldenRatio*(b-a);
-  double v = x;
-  double w = x;
-  double fx = evaluation(x, context, complexFormat, angleUnit, context1, context2, context3);
-  double fw = fx;
-  double fv = fw;
-
-  double d = NAN;
-  double u, fu;
-
-  for (int i = 0; i < 100; i++) {
-    double m = 0.5*(a+b);
-    double tol1 = k_sqrtEps*std::fabs(x)+1E-10;
-    double tol2 = 2.0*tol1;
-    if (std::fabs(x-m) <= tol2-0.5*(b-a))  {
-      double middleFax = evaluation((x+a)/2.0, context, complexFormat, angleUnit, context1, context2, context3);
-      double middleFbx = evaluation((x+b)/2.0, context, complexFormat, angleUnit, context1, context2, context3);
-      double fa = evaluation(a, context, complexFormat, angleUnit, context1, context2, context3);
-      double fb = evaluation(b, context, complexFormat, angleUnit, context1, context2, context3);
-      if (middleFax-fa <= k_sqrtEps && fx-middleFax <= k_sqrtEps && fx-middleFbx <= k_sqrtEps && middleFbx-fb <= k_sqrtEps) {
-        return Coordinate2D<double>(x, fx);
-      }
-    }
-    double p = 0;
-    double q = 0;
-    double r = 0;
-    if (std::fabs(e) > tol1) {
-      r = (x-w)*(fx-fv);
-      q = (x-v)*(fx-fw);
-      p = (x-v)*q -(x-w)*r;
-      q = 2.0*(q-r);
-      if (q>0.0) {
-        p = -p;
-      } else {
-        q = -q;
-      }
-      r = e;
-      e = d;
-    }
-    if (std::fabs(p) < std::fabs(0.5*q*r) && p<q*(a-x) && p<q*(b-x)) {
-      d = p/q;
-      u= x+d;
-      if (u-a < tol2 || b-u < tol2) {
-        d = x < m ? tol1 : -tol1;
-      }
-    } else {
-      e = x<m ? b-x : a-x;
-      d = k_goldenRatio*e;
-    }
-    u = x + (std::fabs(d) >= tol1 ? d : (d>0 ? tol1 : -tol1));
-    fu = evaluation(u, context, complexFormat, angleUnit, context1, context2, context3);
-    if (fu <= fx || (std::isnan(fx) && !std::isnan(fu))) {
-      if (u<x) {
-        b = x;
-      } else {
-        a = x;
-      }
-      v = w;
-      fv = fw;
-      w = x;
-      fw = fx;
-      x = u;
-      fx = fu;
-    } else {
-      if (u<x) {
-        a = u;
-      } else {
-        b = u;
-      }
-      if (fu <= fw || w == x) {
-        v = w;
-        fv = fw;
-        w = u;
-        fw = fu;
-      } else if (fu <= fv || v == x || v == w) {
-        v = u;
-        fv = fu;
-      }
-    }
-  }
-  return Coordinate2D<double>(x, fx);
+  return result;
 }
 
-double Solver::BrentRoot(double ax, double bx, double precision, ValueAtAbscissa evaluation, Context * context, Preferences::ComplexFormat complexFormat, Preferences::AngleUnit angleUnit, const void * context1, const void * context2, const void * context3) {
+Coordinate2D<double> Solver::NextMinimum(ValueAtAbscissa evaluation, Context * context, const void * auxiliary, double start, double end, double minimalStep, double stepPrecision) {
+  SolverHelper<double>::BracketSearch search = [](double a, double b, double c, double fa, double fb, double fc, SolverHelper<double>::ValueAtAbscissa f, Context * context, const void * auxiliary) {
+    Coordinate2D<double> res = SolverHelper<double>::MinimumExistsOnInterval(fa, fb, fc) ? BrentMinimum(a, c, f, context, auxiliary) : Coordinate2D<double>(NAN, NAN);
+    return RoundCoordinatesToZero(res, a, c, f, context, auxiliary);
+  };
+  Coordinate2D<double> result = SolverHelper<double>::NextPointOfInterest(evaluation, context, auxiliary, search, start, end, stepPrecision, minimalStep);
+  /* Ignore extremum whose value is undefined or too big because they are
+   * really unlikely to be local extremum. */
+  if (std::isnan(result.x2()) || std::fabs(result.x2()) > k_maxFloat) {
+    result.setX1(NAN);
+  }
+  return result;
+}
+
+double Solver::BrentRoot(double ax, double bx, double precision, ValueAtAbscissa evaluation, Context * context, const void * auxiliary) {
   if (ax > bx) {
-    return BrentRoot(bx, ax, precision, evaluation, context, complexFormat, angleUnit, context1, context2, context3);
+    return BrentRoot(bx, ax, precision, evaluation, context, auxiliary);
   }
   double a = ax;
   double b = bx;
   double c = bx;
   double d = b-a;
   double e = b-a;
-  double fa = evaluation(a, context, complexFormat, angleUnit, context1, context2, context3);
+  double fa = evaluation(a, context, auxiliary);
   if (fa == 0) {
     // We are looking for a root. If a is already a root, just return it.
     return a;
   }
-  double fb = evaluation(b, context, complexFormat, angleUnit, context1, context2, context3);
+  double fb = evaluation(b, context, auxiliary);
   double fc = fb;
   for (int i = 0; i < 100; i++) {
     if ((fb > 0.0 && fc > 0.0) || (fb < 0.0 && fc < 0.0)) {
@@ -215,7 +167,7 @@ double Solver::BrentRoot(double ax, double bx, double precision, ValueAtAbscissa
     double tol1 = 2.0*DBL_EPSILON*std::fabs(b)+0.5*precision;
     double xm = 0.5*(c-b);
     if (std::fabs(xm) <= tol1 || fb == 0.0) {
-      double fbcMiddle = evaluation(0.5*(b+c), context, complexFormat, angleUnit, context1, context2, context3);
+      double fbcMiddle = evaluation(0.5*(b+c), context, auxiliary);
       bool isContinuous = (fb <= fbcMiddle && fbcMiddle <= fc) || (fc <= fbcMiddle && fbcMiddle <= fb);
       if (isContinuous) {
         return b;
@@ -253,18 +205,109 @@ double Solver::BrentRoot(double ax, double bx, double precision, ValueAtAbscissa
     } else {
       b += xm > 0.0 ? tol1 : -tol1;
     }
-    fb = evaluation(b, context, complexFormat, angleUnit, context1, context2, context3);
+    fb = evaluation(b, context, auxiliary);
   }
   return NAN;
 }
 
+Coordinate2D<double> Solver::BrentMinimum(double ax, double bx, ValueAtAbscissa evaluation, Context * context, const void * auxiliary) {
+  /* Bibliography: R. P. Brent, Algorithms for finding zeros and extrema of
+   * functions without calculating derivatives */
+  if (ax > bx) {
+    return BrentMinimum(bx, ax, evaluation, context, auxiliary);
+  }
+  double e = 0.0;
+  double a = ax;
+  double b = bx;
+  double x = a+k_goldenRatio*(b-a);
+  double v = x;
+  double w = x;
+  double fx = evaluation(x, context, auxiliary);
+  double fw = fx;
+  double fv = fw;
 
-Coordinate2D<double> Solver::IncreasingFunctionRoot(double ax, double bx, double resultPrecision, ValueAtAbscissa evaluation, Context * context, Preferences::ComplexFormat complexFormat, Preferences::AngleUnit angleUnit, const void * context1, const void * context2, const void * context3, double * resultEvaluation) {
+  double d = NAN;
+  double u, fu;
+
+  for (int i = 0; i < 100; i++) {
+    double m = 0.5*(a+b);
+    double tol1 = k_sqrtEps*std::fabs(x)+1E-10;
+    double tol2 = 2.0*tol1;
+    if (std::fabs(x-m) <= tol2-0.5*(b-a))  {
+      double middleFax = evaluation((x+a)/2.0, context, auxiliary);
+      double middleFbx = evaluation((x+b)/2.0, context, auxiliary);
+      double fa = evaluation(a, context, auxiliary);
+      double fb = evaluation(b, context, auxiliary);
+      if (middleFax-fa <= k_sqrtEps && fx-middleFax <= k_sqrtEps && fx-middleFbx <= k_sqrtEps && middleFbx-fb <= k_sqrtEps) {
+        return Coordinate2D<double>(x, fx);
+      }
+    }
+    double p = 0;
+    double q = 0;
+    double r = 0;
+    if (std::fabs(e) > tol1) {
+      r = (x-w)*(fx-fv);
+      q = (x-v)*(fx-fw);
+      p = (x-v)*q -(x-w)*r;
+      q = 2.0*(q-r);
+      if (q>0.0) {
+        p = -p;
+      } else {
+        q = -q;
+      }
+      r = e;
+      e = d;
+    }
+    if (std::fabs(p) < std::fabs(0.5*q*r) && p<q*(a-x) && p<q*(b-x)) {
+      d = p/q;
+      u= x+d;
+      if (u-a < tol2 || b-u < tol2) {
+        d = x < m ? tol1 : -tol1;
+      }
+    } else {
+      e = x<m ? b-x : a-x;
+      d = k_goldenRatio*e;
+    }
+    u = x + (std::fabs(d) >= tol1 ? d : (d>0 ? tol1 : -tol1));
+    fu = evaluation(u, context, auxiliary);
+    if (fu <= fx || (std::isnan(fx) && !std::isnan(fu))) {
+      if (u<x) {
+        b = x;
+      } else {
+        a = x;
+      }
+      v = w;
+      fv = fw;
+      w = x;
+      fw = fx;
+      x = u;
+      fx = fu;
+    } else {
+      if (u<x) {
+        a = u;
+      } else {
+        b = u;
+      }
+      if (fu <= fw || w == x) {
+        v = w;
+        fv = fw;
+        w = u;
+        fw = fu;
+      } else if (fu <= fv || v == x || v == w) {
+        v = u;
+        fv = fu;
+      }
+    }
+  }
+  return Coordinate2D<double>(x, fx);
+}
+
+Coordinate2D<double> Solver::IncreasingFunctionRoot(double ax, double bx, double resultPrecision, ValueAtAbscissa evaluation, Context * context, const void * auxiliary, double * resultEvaluation) {
   assert(ax < bx);
   double min = ax;
   double max = bx;
   double currentAbscissa = min;
-  double eval = evaluation(currentAbscissa, context, complexFormat, angleUnit, context1, context2, context3);
+  double eval = evaluation(currentAbscissa, context, auxiliary);
   if (eval >= 0) {
     if (resultEvaluation != nullptr) {
       *resultEvaluation = eval;
@@ -291,7 +334,7 @@ Coordinate2D<double> Solver::IncreasingFunctionRoot(double ax, double bx, double
     if (currentAbscissa == min || currentAbscissa == max) {
       break;
     }
-    eval = evaluation(currentAbscissa, context, complexFormat, angleUnit, context1, context2, context3);
+    eval = evaluation(currentAbscissa, context, auxiliary);
     if (eval > DBL_EPSILON) {
       max = currentAbscissa;
     } else if (eval < -DBL_EPSILON) {
@@ -307,7 +350,7 @@ Coordinate2D<double> Solver::IncreasingFunctionRoot(double ax, double bx, double
 }
 
 template<typename T>
-T Solver::CumulativeDistributiveInverseForNDefinedFunction(T * probability, ValueAtAbscissa evaluation, Context * context, Preferences::ComplexFormat complexFormat, Preferences::AngleUnit angleUnit, const void * context1, const void * context2, const void * context3) {
+T Solver::CumulativeDistributiveInverseForNDefinedFunction(T * probability, ValueAtAbscissa evaluation, Context * context, const void * auxiliary) {
   T precision = sizeof(T) == sizeof(double) ? DBL_EPSILON : FLT_EPSILON;
   assert(*probability <= (((T)1.0) - precision) && *probability >= precision);
   (void) precision;
@@ -316,13 +359,13 @@ T Solver::CumulativeDistributiveInverseForNDefinedFunction(T * probability, Valu
   T delta = 0.0;
   do {
     delta = std::fabs(*probability-p);
-    p += evaluation(k++, context, complexFormat, angleUnit, context1, context2, context3);
+    p += evaluation(k++, context, auxiliary);
     if (p >= k_maxProbability && std::fabs(*probability-(T)1.0) <= delta) {
       *probability = (T)1.0;
       return (T)(k-1);
     }
   } while (std::fabs(*probability-p) <= delta && k < k_maxNumberOfOperations && p < (T)1.0);
-  p -= evaluation(--k, context, complexFormat, angleUnit, context1, context2, context3);
+  p -= evaluation(--k, context, auxiliary);
   if (k == k_maxNumberOfOperations) {
     *probability = (T)1.0;
     return INFINITY;
@@ -335,11 +378,11 @@ T Solver::CumulativeDistributiveInverseForNDefinedFunction(T * probability, Valu
 }
 
 template<typename T>
-T Solver::CumulativeDistributiveFunctionForNDefinedFunction(T x, ValueAtAbscissa evaluation, Context * context, Preferences::ComplexFormat complexFormat, Preferences::AngleUnit angleUnit, const void * context1, const void * context2, const void * context3) {
+T Solver::CumulativeDistributiveFunctionForNDefinedFunction(T x, ValueAtAbscissa evaluation, Context * context, const void * auxiliary) {
   int end = std::round(x);
   double result = 0.0;
   for (int k = 0; k <=end; k++) {
-    result += evaluation(k, context, complexFormat, angleUnit, context1, context2, context3);
+    result += evaluation(k, context, auxiliary);
     /* Avoid too long loop */
     if (k > k_maxNumberOfOperations) {
       break;
@@ -352,9 +395,22 @@ T Solver::CumulativeDistributiveFunctionForNDefinedFunction(T x, ValueAtAbscissa
   return result;
 }
 
-template float Solver::CumulativeDistributiveInverseForNDefinedFunction(float *, ValueAtAbscissa, Context *, Preferences::ComplexFormat, Preferences::AngleUnit, const void *, const void *, const void *);
-template double Solver::CumulativeDistributiveInverseForNDefinedFunction(double *, ValueAtAbscissa, Context *, Preferences::ComplexFormat, Preferences::AngleUnit, const void *, const void *, const void *);
-template float Solver::CumulativeDistributiveFunctionForNDefinedFunction(float, ValueAtAbscissa, Context *, Preferences::ComplexFormat, Preferences::AngleUnit, const void *, const void *, const void *);
-template double Solver::CumulativeDistributiveFunctionForNDefinedFunction(double, ValueAtAbscissa, Context *, Preferences::ComplexFormat, Preferences::AngleUnit, const void *, const void *, const void *);
-
+Coordinate2D<double> Solver::RoundCoordinatesToZero(Coordinate2D<double> xy, double a, double b, ValueAtAbscissa f, Context * context, const void * auxiliary) {
+  Coordinate2D<double> result(xy);
+  float tolerance = std::fabs(b - a) * k_zeroPrecision;
+  if (std::fabs(result.x1()) < tolerance) {
+    result.setX1(0.);
+    result.setX2(f(0., context, auxiliary));
+  }
+  if (std::fabs(result.x2()) < tolerance) {
+    result.setX2(0.);
+  }
+  return result;
 }
+
+template float Solver::CumulativeDistributiveInverseForNDefinedFunction(float *, ValueAtAbscissa, Context *, const void *);
+template double Solver::CumulativeDistributiveInverseForNDefinedFunction(double *, ValueAtAbscissa, Context *, const void *);
+template float Solver::CumulativeDistributiveFunctionForNDefinedFunction(float, ValueAtAbscissa, Context *, const void *);
+template double Solver::CumulativeDistributiveFunctionForNDefinedFunction(double, ValueAtAbscissa, Context *, const void *);
+}
+

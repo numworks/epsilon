bb5aed891201eaf75bdd694db8fd9d68f07a5380
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2020-12-22 14:08:39 +0100

    [poincare/solver] Special case for finding roots of powers
    Find roots of a Power by looking for the roots of its first child, and
    then checking that those roots are roots of the first expression.

--- poincare/include/poincare/solver.h
+++ poincare/include/poincare/solver.h
@@ -30,6 +30,8 @@ private:
 
 class Solver {
 public:
+  static constexpr double k_zeroPrecision = 1e-5;
+
   typedef SolverHelper<double>::ValueAtAbscissa ValueAtAbscissa;
 
   // Minimum
@@ -50,7 +52,6 @@ private:
   constexpr static double k_maxProbability = 0.9999995;
   constexpr static double k_sqrtEps = 1.4901161193847656E-8; // sqrt(DBL_EPSILON)
   constexpr static double k_goldenRatio = 0.381966011250105151795413165634361882279690820194237137864; // (3-sqrt(5))/2
-  static constexpr double k_zeroPrecision = 1e-5;
   static constexpr double k_maxFloat = 1e100;
   static constexpr double k_precisionByGradUnit = 1e6;
 

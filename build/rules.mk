1269b4725f923a2b75235a986e7efaba49d72c6e
Émilie Feral <emilie.feral@numworks.com>
2020-12-28 14:08:42 +0100

    [build] elf2dfu.py takes a new argument to sign dfu

--- build/rules.mk
+++ build/rules.mk
@@ -26,7 +26,7 @@ $(eval $(call rule_for, \
 
 $(eval $(call rule_for, \
   DFUSE, %.dfu, %.elf, \
-  $$(PYTHON) build/device/elf2dfu.py $$< $$@, \
+  $$(PYTHON) build/device/elf2dfu.py $$(DFUFLAGS) $$< $$@, \
   local \
 ))
 

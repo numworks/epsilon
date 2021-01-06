dffb76478dfdd063c33f8d32cde6967aeec67ca5
Émilie Feral <emilie.feral@numworks.com>
2021-01-06 15:45:44 +0100

    [ion] userland: complete Display API

--- ion/src/device/userland/drivers/display.cpp
+++ ion/src/device/userland/drivers/display.cpp
@@ -28,6 +28,36 @@ void pullRect(KDRect r, KDColor * pixels) {
   pullRectSVC(&r, pixels);
 }
 
+void SVC_ATTRIBUTES waitForVBlankSVC(bool * res) {
+  SVC(SVC_DISPLAY_WAIT_FOR_V_BLANK);
+}
+
+bool waitForVBlank() {
+  bool res;
+  waitForVBlankSVC(&res);
+  return res;
+}
+
+void SVC_ATTRIBUTES displayUniformTilingSize10SVC(KDColor * c, int * res) {
+  SVC(SVC_DISPLAY_UNIFORM_TILING_SIZE_10);
+}
+
+int displayUniformTilingSize10(KDColor c) {
+  int res;
+  displayUniformTilingSize10SVC(&c, &res);
+  return res;
+}
+
+void SVC_ATTRIBUTES displayColoredTilingSize10SVC(int * res) {
+  SVC(SVC_DISPLAY_COLORED_TILING_SIZE_10);
+}
+
+int displayColoredTilingSize10() {
+  int res;
+  displayColoredTilingSize10SVC(&res);
+  return res;
+}
+
 void SVC_ATTRIBUTES POSTPushMulticolorSVC(int * rootNumberTiles, int * tileSize) {
   SVC(SVC_DISPLAY_POST_PUSH_MULTICOLOR);
 }

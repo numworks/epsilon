3220fbdf2edf03971df072a594fce3ec9b22a9b3
Émilie Feral <emilie.feral@numworks.com>
2021-01-14 15:01:44 +0100

    [ion] Implement Ion::serialNumber via SVC

--- ion/src/device/shared/drivers/serial_number.cpp
+++ ion/src/device/shared/drivers/serial_number.cpp
@@ -6,6 +6,14 @@ namespace Ion {
 namespace Device {
 namespace SerialNumber {
 
+const char * read() {
+  static char serialNumber[Length + 1] = {0};
+  if (serialNumber[0] == 0) {
+    copy(serialNumber);
+  }
+  return serialNumber;
+}
+
 void copy(char * buffer) {
   const unsigned char * rawUniqueID = reinterpret_cast<const unsigned char *>(Config::UniqueDeviceIDAddress);
   Base64::encode(rawUniqueID, 12, buffer);

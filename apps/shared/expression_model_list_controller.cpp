025d26b2298e27f802bb2658c9debe0243f21978
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-01-15 12:56:46 +0100

    [apps/shared] Temporarily Improve ExpressionModelListController

--- apps/shared/expression_model_list_controller.cpp
+++ apps/shared/expression_model_list_controller.cpp
@@ -124,17 +124,18 @@ int ExpressionModelListController::memoizedIndexFromCumulatedHeight(KDCoordinate
   }
 
   KDCoordinate currentCumulatedHeight = memoizedCumulatedHeightFromIndex(currentSelectedRow);
+  // Take advantage of currentCumulatedHeight if :
   if (offsetY > currentCumulatedHeight) {
-    int iMax = std::min(k_memoizedCellsCount/2 + 1, rowsCount - currentSelectedRow);
-    for (int i = 0; i < iMax; i++) {
+    // Searched index is above currentSelectedRow
+    for (int i = 0; i < rowsCount - currentSelectedRow; i++) {
       currentCumulatedHeight+= memoizedRowHeight(currentSelectedRow + i);
       if (offsetY <= currentCumulatedHeight) {
         return currentSelectedRow + i;
       }
     }
-  } else {
-    int iMax = std::min(k_memoizedCellsCount/2, currentSelectedRow);
-    for (int i = 1; i <= iMax; i++) {
+  } else if (offsetY > currentCumulatedHeight / 2 || currentSelectedRow <= k_memoizedCellsCount / 2) {
+    // Searched index seems closer to currentSelectedRow than 0.
+    for (int i = 1; i <= currentSelectedRow; i++) {
       currentCumulatedHeight-= memoizedRowHeight(currentSelectedRow-i);
       if (offsetY > currentCumulatedHeight) {
         return currentSelectedRow - i;

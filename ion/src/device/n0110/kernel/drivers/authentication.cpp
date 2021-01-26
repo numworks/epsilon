775b4ad6219ce4444ee28f546aef59e1837f48d9
Émilie Feral <emilie.feral@numworks.com>
2021-01-26 10:46:12 +0100

    [ion] Authentication: memoize the authentication state to avoid slowing down device wake-up

--- ion/src/device/n0110/kernel/drivers/authentication.cpp
+++ ion/src/device/n0110/kernel/drivers/authentication.cpp
@@ -9,28 +9,44 @@ namespace Ion {
 namespace Device {
 namespace Authentication {
 
+enum class Status {
+  Unknown,
+  Official,
+  Unofficial
+};
+
 bool isAuthenticated(void * pointer) {
-  /* The standard memory layout is:
-   * | code size |         code        |   signature   |
-   * */
-  uint32_t start = reinterpret_cast<uint32_t>(pointer);
-  if (!Flash::AddressIsInExternalFlash(start)) {
-    // Out of memory
-    return false;
+  static Status sStatus = Status::Unknown;
+  if (sStatus == Status::Unknown) {
+    /* The standard memory layout is:
+     * | code size |         code        |   signature   |
+     * */
+    uint32_t start = reinterpret_cast<uint32_t>(pointer);
+    if (!Flash::AddressIsInExternalFlash(start)) {
+      // Out of memory
+      sStatus = Status::Unofficial;
+      return false;
+    }
+    uint32_t size = *static_cast<uint32_t *>(pointer);
+    if (!Flash::AddressIsInExternalFlash(start + size)) {
+      // Overflow of external memory
+      sStatus = Status::Unofficial;
+      return false;
+    }
+    uint8_t * code = static_cast<uint8_t *>(pointer) + Board::Config::SizeSize;
+    assert(code && size > 0);
+    uint8_t digest[Ion::Device::Hash::Sha256DigestBytes];
+    uint8_t decryptedSignature[Hash::Sha256DigestBytes];
+    Hash::sha256(code, size, digest);
+    uint8_t * signature = static_cast<uint8_t *>(code) + size;
+    decrypt(signature, decryptedSignature);
+    if (memcmp(digest, decryptedSignature, Hash::Sha256DigestBytes) == 0) {
+      sStatus = Status::Official;
+    } else {
+      sStatus = Status::Unofficial;
+    }
   }
-  uint32_t size = *static_cast<uint32_t *>(pointer);
-  if (!Flash::AddressIsInExternalFlash(start + size)) {
-    // Overflow of external memory
-    return false;
-  }
-  uint8_t * code = static_cast<uint8_t *>(pointer) + Board::Config::SizeSize;
-  assert(code && size > 0);
-  uint8_t digest[Ion::Device::Hash::Sha256DigestBytes];
-  uint8_t decryptedSignature[Hash::Sha256DigestBytes];
-  Hash::sha256(code, size, digest);
-  uint8_t * signature = static_cast<uint8_t *>(code) + size;
-  decrypt(signature, decryptedSignature);
-  return memcmp(digest, decryptedSignature, Hash::Sha256DigestBytes) == 0;
+  return sStatus == Status::Official;
 }
 
 }

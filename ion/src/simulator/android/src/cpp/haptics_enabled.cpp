5a20d914df5a58bf205d72a16d325762504d118d
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-02-03 11:50:50 +0100

    [ion/simulator/android] Fix haptic feedback memory leaks

--- ion/src/simulator/android/src/cpp/haptics_enabled.cpp
+++ ion/src/simulator/android/src/cpp/haptics_enabled.cpp
@@ -15,7 +15,14 @@ bool isEnabled() {
   if (j_methodId == 0) {
     return false;
   }
-  return (env->CallBooleanMethod(activity, j_methodId) != JNI_FALSE);
+  bool result = (env->CallBooleanMethod(activity, j_methodId) != JNI_FALSE);
+  /* Local references are automatically deleted if a native function called from
+   * Java side returns. For SDL this native function is main() itself. Therefore
+   * references need to be manually deleted because otherwise the references
+   * will first be cleaned if main() returns (application exit). */
+  env->DeleteLocalRef(j_class);
+  env->DeleteLocalRef(activity);
+  return result;
 }
 
 }

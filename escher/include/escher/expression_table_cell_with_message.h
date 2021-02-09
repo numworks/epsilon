29a764d4f01ea78f303e7712a179a1c7da570e68
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-02-09 10:09:35 +0100

    [escher] Add ExpressionTableCellWithMessage source file

--- escher/include/escher/expression_table_cell_with_message.h
+++ escher/include/escher/expression_table_cell_with_message.h
@@ -8,25 +8,10 @@
 namespace Escher {
 class ExpressionTableCellWithMessage : public ExpressionTableCell {
 public:
-  ExpressionTableCellWithMessage(Responder * parentResponder = nullptr, I18n::Message message = (I18n::Message)0) :
-  ExpressionTableCell(parentResponder),
-  m_subLabelView(KDFont::SmallFont, (I18n::Message)0, 0.0f, 0.5f, Palette::GrayDark)
-  {}
-  View * subLabelView() const override {
-    if (strlen(m_subLabelView.text()) == 0) {
-      return nullptr;
-    }
-    return (View *)&m_subLabelView;
-  }
-  void setHighlighted(bool highlight) override {
-    ExpressionTableCell::setHighlighted(highlight);
-    KDColor backgroundColor = isHighlighted()? Palette::Select : KDColorWhite;
-    m_subLabelView.setBackgroundColor(backgroundColor);
-  }
-  void setSubLabelMessage(I18n::Message textBody) {
-    m_subLabelView.setMessage(textBody);
-    reloadCell();
-  }
+  ExpressionTableCellWithMessage(Responder * parentResponder = nullptr, I18n::Message message = (I18n::Message)0);
+  View * subLabelView() const override;
+  void setHighlighted(bool highlight) override;
+  void setSubLabelMessage(I18n::Message textBody);
 private:
   MessageTextView m_subLabelView;
 };

b51789a4f8adf11765acf06e142636e29e852760
Émilie Feral <emilie.feral@numworks.com>
2021-02-11 15:48:53 +0100

    [ion] Duplicate checkpoints - two potential checkpoints can be set: home or custom

--- ion/src/device/userland/drivers/circuit_breaker.cpp
+++ ion/src/device/userland/drivers/circuit_breaker.cpp
@@ -5,28 +5,42 @@
 namespace Ion {
 namespace CircuitBreaker {
 
-void SVC_ATTRIBUTES hasCheckpointSVC(bool * res) {
+void SVC_ATTRIBUTES hasCheckpointSVC(CircuitBreaker::Checkpoint * checkpoint, bool * res) {
   SVC(SVC_CIRCUIT_BREAKER_HAS_CHECKPOINT);
 }
 
-bool hasCheckpoint() {
+bool hasCheckpoint(Checkpoint c) {
   bool res;
-  hasCheckpointSVC(&res);
+  hasCheckpointSVC(&c, &res);
   return res;
 }
 
-void __attribute__((naked)) loadCheckpoint() {
-  SVC(SVC_CIRCUIT_BREAKER_LOAD_CHECKPOINT);
+void SVC_ATTRIBUTES unsetCheckpointSVC(CircuitBreaker::Checkpoint * checkpoint) {
+  SVC(SVC_CIRCUIT_BREAKER_UNSET_CHECKPOINT);
+}
+
+void unsetCheckpoint(Checkpoint c) {
+  unsetCheckpointSVC(&c);
+}
+
+void __attribute__((naked)) loadHomeCheckpoint() {
+  SVC(SVC_CIRCUIT_BREAKER_LOAD_HOME_CHECKPOINT);
   assert(false);
 }
 
-void __attribute__((naked)) setCheckpoint() {
-  SVC(SVC_CIRCUIT_BREAKER_SET_CHECKPOINT);
+void __attribute__((naked)) setHomeCheckpoint() {
+  SVC(SVC_CIRCUIT_BREAKER_SET_HOME_CHECKPOINT);
   asm("bx lr");
 }
 
-void unsetCheckpoint() {
-  SVC(SVC_CIRCUIT_BREAKER_UNSET_CHECKPOINT);
+void __attribute__((naked)) loadCustomCheckpoint() {
+  SVC(SVC_CIRCUIT_BREAKER_LOAD_CUSTOM_CHECKPOINT);
+  assert(false);
+}
+
+void __attribute__((naked)) setCustomCheckpoint() {
+  SVC(SVC_CIRCUIT_BREAKER_SET_CUSTOM_CHECKPOINT);
+  asm("bx lr");
 }
 
 }

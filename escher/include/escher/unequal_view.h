8bf41a41aee847152bb4ded532a6ad1114a97c0c
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-02-19 14:45:48 +0100

    [shared/interactive_curve_view_controller] Axes button dynamic size
    The Axes button changes size according to whether the ≠ pictogram is
    displayed.

--- escher/include/escher/unequal_view.h
+++ escher/include/escher/unequal_view.h
@@ -8,7 +8,7 @@ namespace Escher {
 class UnequalView : public ToggleableView {
 public:
   constexpr static KDCoordinate k_size = 9;
-  KDSize minimalSizeForOptimalDisplay() const override { return KDSize(k_size, k_size); }
+  KDSize minimalSizeForOptimalDisplay() const override { return m_state ? KDSize(k_size, k_size) : KDSizeZero; }
   void drawRect(KDContext * ctx, KDRect rect) const override;
 };
 

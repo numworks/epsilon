341488508d22c4f8aee9d27001a63edc04872c02
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-02-26 10:53:50 +0100

    [ion/device/n0110] Deduce Vbus config from PCB version
    Revised PCB (version number > 0) should configure Vbus as a GPIO instead
    of an alternate function.

---
+++ ion/src/device/n0100/drivers/usb.cpp
@@ -0,0 +1,18 @@
+#include <drivers/usb.h>
+#include <drivers/config/usb.h>
+
+namespace Ion {
+namespace Device {
+namespace USB {
+
+void initVbus() {
+  Config::VbusPin.init();
+}
+
+void shutdownVbus() {
+  Config::VbusPin.shutdown();
+}
+
+}
+}
+}

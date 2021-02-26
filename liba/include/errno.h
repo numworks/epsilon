8469e13fe726b670e11e7ae31d460c380df4d05a
Émilie Feral <emilie.feral@numworks.com>
2021-02-26 14:55:19 +0100

    [ion] Build libsodium library on the device

--- liba/include/errno.h
+++ liba/include/errno.h
@@ -4,11 +4,23 @@
 /* No such file or directory */
 #define ENOENT 2
 
+/*Interrupted function call*/
+#define EINTR 4
+
+/* EIO Input/output error */
+#define EIO 5
+
+/* No such device or address */
+#define ENXIO 6
+
 /* Invalid argument */
 #define EINVAL 22
 
 /* Not enough space */
 #define ENOMEM 12
+
+/* Resource temporarily unavailable */
+#define EAGAIN 35
 
 extern int errno;
 
--- liba/include/limits.h
+++ liba/include/limits.h
@@ -2,5 +2,6 @@
 #define LIBA_LIMITS_H
 
 #define INT_MAX 0x7fffffff
+#define CHAR_BIT 8
 
 #endif

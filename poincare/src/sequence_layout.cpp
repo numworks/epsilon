0d0cecd566116426549e352c5819a16e37eb2955
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-03-01 10:57:08 +0100

    [poincare] Set default sum/product XNT to i

--- poincare/src/sequence_layout.cpp
+++ poincare/src/sequence_layout.cpp
@@ -150,7 +150,7 @@ void SequenceLayoutNode::deleteBeforeCursor(LayoutCursor * cursor) {
 }
 
 CodePoint SequenceLayoutNode::XNTCodePoint(int childIndex) const {
-  return (childIndex == k_argumentLayoutIndex || childIndex == k_variableLayoutIndex) ? CodePoint('n') : UCodePointNull;
+  return (childIndex == k_argumentLayoutIndex || childIndex == k_variableLayoutIndex) ? CodePoint('i') : UCodePointNull;
 }
 
 // Protected

d1ee904a4e75f4bf264d04fae9610013ee7b96db
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-03-03 11:05:26 +0100

    [escher] Optimize BufferTableCell

--- escher/include/escher/buffer_table_cell.h
+++ escher/include/escher/buffer_table_cell.h
@@ -5,17 +5,25 @@
 #include <escher/table_cell.h>
 
 namespace Escher {
-
+/* BufferTableCell is here optimized for its use in code VariableBoxController.
+ * It cannot be selected and has smaller vertical margins than other TableCell.
+ */
 class BufferTableCell : public TableCell {
 public:
   BufferTableCell();
   const View * labelView() const override { return &m_labelView; }
-  void setHighlighted(bool highlight) override;
+  void setHighlighted(bool highlight) override { assert(!highlight); }
   void setLabelText(const char * textBody);
   void appendText(const char * textBody);
   void setTextColor(KDColor color);
   void setMessageFont(const KDFont * font);
   void setBackgroundColor(KDColor color);
+  // Overriding TableCell row Height
+  void layoutSubviews(bool force = false) override;
+  KDSize minimalSizeForOptimalDisplay() const override {
+    return KDSize(bounds().width(), k_subtitleRowHeight);
+  }
+  constexpr static KDCoordinate k_subtitleRowHeight = 23;
 protected:
   KDColor backgroundColor() const override { return m_backgroundColor; }
 private:
--- escher/src/buffer_table_cell.cpp
+++ escher/src/buffer_table_cell.cpp
@@ -6,15 +6,10 @@ namespace Escher {
 
 BufferTableCell::BufferTableCell() :
   TableCell(),
-  m_labelView(KDFont::LargeFont, 1.0f, 0.5f, Palette::GrayDark),
+  // Text is aligned to the left, and vertically centered
+  m_labelView(KDFont::LargeFont, 0.0f, 0.5f, Palette::GrayDark),
   m_backgroundColor(KDColorWhite)
 {
-}
-
-void BufferTableCell::setHighlighted(bool highlight) {
-  HighlightCell::setHighlighted(highlight);
-  KDColor backgroundColor = highlight? Palette::Select : m_backgroundColor;
-  m_labelView.setBackgroundColor(backgroundColor);
 }
 
 void BufferTableCell::setLabelText(const char * textBody) {
@@ -40,6 +35,26 @@ void BufferTableCell::setMessageFont(const KDFont * font) {
 void BufferTableCell::setBackgroundColor(KDColor color) {
   m_backgroundColor = color;
   m_labelView.setBackgroundColor(color);
+}
+
+void BufferTableCell::layoutSubviews(bool force)  {
+  KDCoordinate width = bounds().width();
+  KDCoordinate height = bounds().height();
+  if (width == 0 || height == 0) {
+    return;
+  }
+  width -= k_separatorThickness + Metric::CellRightMargin;
+  KDCoordinate x = k_separatorThickness + Metric::CellLeftMargin;
+  /* With an enforced 23px height, no vertical margins are enforced. Vertical
+   * centering is handled in BufferTextView. Additionally, an offset is added at
+   * the top to lower the baseline of the buffet view to look more centered. */
+  KDCoordinate topOffset = 1;
+  height -= k_separatorThickness + topOffset;
+  KDCoordinate y = k_separatorThickness + topOffset;
+
+  assert(width > 0 && height > 0);
+
+  m_labelView.setFrame(KDRect(x, y, width, height), force);
 }
 
 }

c15a5b5fe348dd130c0c95d45dd5aa7a5f2ca262
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-03-03 11:05:26 +0100

    [escher] Optimize BufferTableCell

--- apps/code/variable_box_controller.h
+++ apps/code/variable_box_controller.h
@@ -42,7 +42,7 @@ public:
 
 private:
   constexpr static size_t k_maxNumberOfDisplayedItems = (Ion::Display::Height - Escher::Metric::TitleBarHeight - Escher::Metric::PopUpTopMargin) / Escher::TableCell::k_minimalSmallFontCellHeight + 2; // +2 if the cells are cropped on top and at the bottom
-  constexpr static size_t k_maxNumberOfDisplayedSubtitles = k_maxNumberOfDisplayedItems-k_maxNumberOfDisplayedItems/2; // TODO Hugo : At most one subtitle cell for one item
+  constexpr static size_t k_maxNumberOfDisplayedSubtitles = (Ion::Display::Height - Escher::Metric::TitleBarHeight - Escher::Metric::PopUpTopMargin) / (Escher::BufferTableCell::k_subtitleRowHeight + Escher::TableCell::k_minimalSmallFontCellHeight) + 2; // Subtitles are at least followed by one item row
   constexpr static size_t k_totalBuiltinNodesCount = 107;
   constexpr static size_t k_maxScriptNodesCount = k_totalBuiltinNodesCount + 64; // 64 Chosen without particular reasons
   constexpr static uint8_t k_maxOrigins = 10; // currentScriptOrigin + builtinsOrigin + 8 importedOrigins max

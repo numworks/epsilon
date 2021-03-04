bac1cb25f7b1628087b54267ec28b60d01b75bf3
Émilie Feral <emilie.feral@numworks.com>
2021-03-04 14:42:22 +0100

    [ion] Change CircuitBreaker API to be easily use in Poincare

--- ion/include/ion/circuit_breaker.h
+++ ion/include/ion/circuit_breaker.h
@@ -6,22 +6,27 @@
 namespace Ion {
 namespace CircuitBreaker {
 
-bool busy();
+enum class Status {
+  Busy, // Waiting after a pendSV
+  Interrupted, // An interruption was encountered and the execution jumped to the checkpoint
+  Set, // The checkpoint has just been set
+  Ignored // The checkpoint was ignored because it's already set at a deeper checkpoint
+};
+
+Status status();
 
 // Custom checkpoint
 
 constexpr int k_checkpointBufferSize = 340;
 typedef uint8_t CheckpointBuffer[k_checkpointBufferSize];
 
-bool clearCustomCheckpointFlag();
 void storeCustomCheckpoint(CheckpointBuffer buffer);
 void resetCustomCheckpoint(CheckpointBuffer * buffer = nullptr);
 void loadCustomCheckpoint();
-void setCustomCheckpoint(bool overridePreviousCheckoint = false);
+Status setCustomCheckpoint(bool overridePreviousCheckpoint = false);
 
 // Home checkpoint
-bool clearHomeCheckpointFlag();
-void setHomeCheckpoint();
+Status setHomeCheckpoint();
 
 }
 }

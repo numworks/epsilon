65f8223998d58f3fabe3d7eb206055bfa3d38d0a
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-03-05 12:50:06 +0100

    [apps/solver] Improve controller's code, fix margins

--- apps/solver/solutions_controller.h
+++ apps/solver/solutions_controller.h
@@ -22,19 +22,19 @@ public:
   const char * title() override;
   Escher::View * view() override { return &m_contentView; }
   void viewWillAppear() override;
-  void viewDidDisappear() override;
+  void viewDidDisappear() override { selectCellAtLocation(-1, -1); }
   void didEnterResponderChain(Escher::Responder * previousFirstResponder) override;
   TELEMETRY_ID("Solutions");
 
   /* AlternateEmptyViewDefaultDelegate */
   bool isEmpty() const override { return false; }  // View cannot be empty
   I18n::Message emptyMessage() override { assert(false); }
-  Escher::Responder * defaultController() override;
+  Escher::Responder * defaultController() override { return parentResponder()->parentResponder(); }
   /* TableViewDataSource */
   int numberOfRows() const override;
   int numberOfColumns() const override { return 2; }
   void willDisplayCellAtLocation(Escher::HighlightCell * cell, int i, int j) override;
-  KDCoordinate columnWidth(int i) override;
+  KDCoordinate columnWidth(int i) override { return i == 0 ? k_symbolCellWidth : k_valueCellWidth; }
   KDCoordinate rowHeight(int j) override;
   KDCoordinate cumulatedWidthFromIndex(int i) override;
   int indexFromCumulatedWidth(KDCoordinate offsetX) override;
@@ -56,7 +56,6 @@ private:
     void setWarningMessages(I18n::Message message0, I18n::Message message1);
     Escher::SelectableTableView * selectableTableView() { return &m_selectableTableView; }
   private:
-    constexpr static KDCoordinate k_middleMargin = 50;
     int numberOfSubviews() const override;
     Escher::View * subviewAtIndex(int index) override;
     void layoutSubviews(bool force = false) override;
@@ -73,11 +72,19 @@ private:
     void setHorizontalAlignment(float alignment) { m_messageView.setAlignment(alignment, k_verticalAlignment); }
     void setMessage(I18n::Message message) { m_messageView.setMessage(message); }
   private:
-    constexpr static float k_verticalAlignment = 0.8f;
+    /* Text is placed at the very top of the cell to simplify text centering
+     * when there are no cells above. To add a "margin" in other cases, we
+     * precede the message row with an empty row. */
+    constexpr static float k_verticalAlignment = 0.0f;
     int numberOfSubviews() const override { return 1; }
     Escher::View * subviewAtIndex(int index) override { assert(index == 0); return &m_messageView; }
     void layoutSubviews(bool force = false) override { m_messageView.setFrame(bounds(), force); }
     Escher::MessageTextView m_messageView;
+  };
+
+  class EmptyCell : public Escher::HighlightCell {
+  public:
+    void drawRect(KDContext * ctx, KDRect rect) const override { ctx->fillRect(bounds(), SolutionsController::ContentView::k_backgroundColor); }
   };
 
   // Cell types
@@ -86,6 +93,7 @@ private:
   constexpr static int k_exactValueCellType = 2;
   constexpr static int k_approximateValueCellType = 3;
   constexpr static int k_messageCellType = 4;
+  constexpr static int k_emptyCellType = 5;
 
   // Heights and widths
   constexpr static KDCoordinate k_defaultCellHeight = 20;
@@ -94,7 +102,7 @@ private:
 
   // Number of cells
   constexpr static int k_maxNumberOfVisibleCells = (Ion::Display::Height - 3 * Escher::Metric::TitleBarHeight) / k_defaultCellHeight + 1; // When displaying approximate solutions for cos(x) = 0 between 0 and 1800 and scrolling down
-  static_assert(k_maxNumberOfVisibleCells == 10, "k_maxNumberOfVisibleCells has changed"); //This assert is just for information purposes
+  static_assert(k_maxNumberOfVisibleCells == 10, "k_maxNumberOfVisibleCells has changed"); // This assert is just for information purposes
   static_assert(k_maxNumberOfVisibleCells <= EquationStore::k_maxNumberOfSolutions + Poincare::Expression::k_maxNumberOfVariables, "We can reduce the number of cells in Solver:SolutionsController.");
   constexpr static int k_maxNumberOfSymbols = EquationStore::k_maxNumberOfSolutions + Poincare::Expression::k_maxNumberOfVariables;
   constexpr static int k_numberOfSymbolCells = (k_maxNumberOfVisibleCells < k_maxNumberOfSymbols) ? k_maxNumberOfVisibleCells : k_maxNumberOfSymbols;
@@ -102,10 +110,9 @@ private:
   constexpr static int k_numberOfExactValueCells = (k_maxNumberOfVisibleCells < k_maxNumberOfExactValues) ? k_maxNumberOfVisibleCells : k_maxNumberOfExactValues;
   constexpr static int k_numberOfApproximateValueCells = 1 + (k_maxNumberOfVisibleCells < EquationStore::k_maxNumberOfApproximateSolutions ? k_maxNumberOfVisibleCells : EquationStore::k_maxNumberOfApproximateSolutions);
   constexpr static int k_numberOfMessageCells = 2;
+  constexpr static int k_numberOfEmptyCells = 2;
 
-  bool usedUserVariables() const {
-    return m_equationStore->userVariablesUsed();
-  }
+  bool usedUserVariables() const { return m_equationStore->userVariablesUsed(); }
   int userVariablesMessageRow() const;
   int numberOfDisplayedSolutions() const;
   I18n::Message noSolutionMessage();
@@ -117,6 +124,7 @@ private:
   Shared::ScrollableTwoExpressionsCell m_exactValueCells[k_numberOfExactValueCells];
   Escher::EvenOddBufferTextCell m_approximateValueCells[k_numberOfApproximateValueCells];
   MessageCell m_messageCells[k_numberOfMessageCells];
+  EmptyCell m_emptyCell[k_numberOfEmptyCells];
   ContentView m_contentView;
 };
 

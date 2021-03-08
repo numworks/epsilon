7b57f31fbf1c5fc441969d78a5a3442a0ed1f791
Joachim LF <joachimlf@pm.me>
2021-03-08 17:56:49 +0100

    [Python/kandinsky] Added in draw_string choice of font

--- python/port/mod/kandinsky/modkandinsky_table.c
+++ python/port/mod/kandinsky/modkandinsky_table.c
@@ -3,7 +3,7 @@
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modkandinsky_color_obj, 1, 3, modkandinsky_color);
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(modkandinsky_get_pixel_obj, modkandinsky_get_pixel);
 STATIC MP_DEFINE_CONST_FUN_OBJ_3(modkandinsky_set_pixel_obj, modkandinsky_set_pixel);
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modkandinsky_draw_string_obj, 3, 5, modkandinsky_draw_string);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modkandinsky_draw_string_obj, 3, 6, modkandinsky_draw_string);
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modkandinsky_fill_rect_obj, 5, 5, modkandinsky_fill_rect);
 
 STATIC const mp_rom_map_elem_t modkandinsky_module_globals_table[] = {

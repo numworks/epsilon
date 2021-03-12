67c8f7b72ef70932c181add9cfd3a868a21fd223
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-03-12 16:44:10 +0100

    [ion/src] Improve comment on JNI references in isEnabled

--- ion/src/simulator/android/src/cpp/haptics_enabled.cpp
+++ ion/src/simulator/android/src/cpp/haptics_enabled.cpp
@@ -18,8 +18,9 @@ bool isEnabled() {
   bool result = (env->CallBooleanMethod(activity, j_methodId) != JNI_FALSE);
   /* Local references are automatically deleted if a native function called from
    * Java side returns. For SDL this native function is main() itself. Therefore
-   * references need to be manually deleted because otherwise the references
-   * will first be cleaned if main() returns (application exit). */
+   * references need to be manually deleted because otherwise they are uselessly
+   * accumulated each time isEnabled is called, which happens repetitively
+   * before the application exit. */
   env->DeleteLocalRef(j_class);
   env->DeleteLocalRef(activity);
   return result;

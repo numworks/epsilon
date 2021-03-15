0d5c488259aec73c2f33bc3bb0e873ae92f0f8b7
Émilie Feral <emilie.feral@numworks.com>
2021-03-15 10:35:53 +0100

    [ion] Bootloader displays a warning message when kernel is not authenticated

---
+++ ion/src/device/shared/warning_display.h
@@ -0,0 +1,14 @@
+#ifndef ION_DEVICE_SHARED_WARNING_DISPLAY_H
+#define ION_DEVICE_SHARED_WARNING_DISPLAY_H
+
+namespace Ion {
+namespace Device {
+namespace WarningDisplay {
+
+void showMessage(const char * const * messages, int numberOfMessages);
+
+}
+}
+}
+
+#endif

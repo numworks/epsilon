9ab124c2bca616bf28011609c21772e0f65288e8
Émilie Feral <emilie.feral@numworks.com>
2021-03-16 09:27:49 +0100

    [ion] Implement WarningDisplay pop-up for untrusted userland

---
+++ ion/src/device/kernel/warning_display.h
@@ -0,0 +1,16 @@
+#ifndef ION_DEVICE_KERNEL_WARNING_DISPLAY_H
+#define ION_DEVICE_KERNEL_WARNING_DISPLAY_H
+
+#include <shared/warning_display.h>
+
+namespace Ion {
+namespace Device {
+namespace WarningDisplay {
+
+void unauthenticatedUserland();
+
+}
+}
+}
+
+#endif

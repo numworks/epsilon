5c4394b168c33dd695fecf9b5292a445b9a95c78
Émilie Feral <emilie.feral@numworks.com>
2021-03-17 12:08:18 +0100

    [ion] Improve DFU leave process: display suitable pop-up info

--- ion/src/device/kernel/drivers/authentication.cpp
+++ ion/src/device/kernel/drivers/authentication.cpp
@@ -1,6 +1,8 @@
 #include <kernel/drivers/authentication.h>
 #include <drivers/reset.h>
+#include <drivers/timing.h>
 #include <kernel/warning_display.h>
+#include <assert.h>
 
 namespace Ion {
 namespace Device {
@@ -12,24 +14,27 @@ bool trustedUserland() {
   return s_trustedUserland;
 }
 
-void checkUserland(bool validKernelVersion) {
+void updateTrustAndSwitchSlot(int currentKernelVersion, int newKernelVersion, int currentUserlandVersion, int newUserlandVersion) {
   s_trustedUserland = false; //BootloaderFunction::TrampolineisAuthentication(leaveAddress, KernelSize);
+  if (currentKernelVersion > newKernelVersion || (s_trustedUserland && currentUserlandVersion > newUserlandVersion)) {
+    WarningDisplay::obsoleteSoftware();
+    Timing::msleep(5000);
+    return;
+  }
+  if (!s_trustedUserland && currentKernelVersion < newKernelVersion) {
+    WarningDisplay::kernelUpgradeRequired();
+    Timing::msleep(5000);
+    return;
+  }
   if (s_trustedUserland) {
-    if (validKernelVersion) {
-      Reset::coreWhilePlugged();
-    } else {
-      //WarningDisplay::downgradeAttack();
-      Reset::coreWhilePlugged();
-    }
+    assert(currentKernelVersion <= newKernelVersion && currentUserlandVersion <= newUserlandVersion);
+    Reset::coreWhilePlugged();
   } else {
-    if (validKernelVersion) {
-      // - shutdown the LED? Other decrease of privilege?
-      WarningDisplay::unauthenticatedUserland();
-      //Reset::jump(leaveAddress);
-    } else {
-      //WarningDisplay::obsoleteKernelRequired();
-      Reset::coreWhilePlugged();
-    }
+    assert(currentKernelVersion == newKernelVersion);
+    // - shutdown the LED? Other decrease of privilege?
+    WarningDisplay::unauthenticatedUserland();
+    Timing::msleep(3000);
+    //Reset::jump(leaveAddress);
   }
 }
 

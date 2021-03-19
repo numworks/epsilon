164384b3e65d68947bf2afe833f024225a2fb40a
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-03-19 09:47:40 +0100

    [statistics/histogram_banner_view] Group elements in banner
    We use the same construct as in Graph::BannerView to make sure legends
    and the values they label are on the same line.

--- apps/statistics/histogram_banner_view.cpp
+++ apps/statistics/histogram_banner_view.cpp
@@ -7,18 +7,21 @@ using namespace Escher;
 namespace Statistics {
 
 HistogramBannerView::HistogramBannerView() :
-  m_intervalLegendView(Font(), I18n::Message::Interval, 0.5f, 0.5f, TextColor(), BackgroundColor()),
-  m_intervalView(Font(), 0.5f, 0.5f, TextColor(), BackgroundColor()),
-  m_sizeLegendView(Font(), I18n::Message::Frequency, 0.5f, 0.5f, TextColor(), BackgroundColor()),
-  m_sizeView(Font(), 0.5f, 0.5f, TextColor(), BackgroundColor()),
-  m_frequencyLegendView(Font(), I18n::Message::RelativeFrequency, 0.5f, 0.5f, TextColor(), BackgroundColor()),
-  m_frequencyView(Font(), 0.5f, 0.5f, TextColor(), BackgroundColor())
+  m_intervalLegendView(Font(), I18n::Message::Interval, 1.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_intervalView(Font(), 0.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_labelledIntervalView(&m_intervalLegendView, &m_intervalView),
+  m_sizeLegendView(Font(), I18n::Message::Frequency, 1.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_sizeView(Font(), 0.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_labelledSizeView(&m_sizeLegendView, &m_sizeView),
+  m_frequencyLegendView(Font(), I18n::Message::RelativeFrequency, 1.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_frequencyView(Font(), 0.0f, 0.5f, TextColor(), BackgroundColor()),
+  m_labelledFrequencyView(&m_frequencyLegendView, &m_frequencyView)
 {
 }
 
 View * HistogramBannerView::subviewAtIndex(int index) {
   assert(0 <= index && index < numberOfSubviews());
-  View * subviews[] = {&m_intervalLegendView, &m_intervalView, &m_sizeLegendView, &m_sizeView, &m_frequencyLegendView, &m_frequencyView};
+  View * subviews[] = {&m_labelledIntervalView, &m_labelledSizeView, &m_labelledFrequencyView};
   return subviews[index];
 }
 
--- apps/statistics/histogram_banner_view.h
+++ apps/statistics/histogram_banner_view.h
@@ -15,15 +15,19 @@ public:
   Escher::BufferTextView * sizeView() { return &m_sizeView; }
   Escher::BufferTextView * frequencyView() { return &m_frequencyView; }
 private:
-  static constexpr int k_numberOfSubviews = 6;
+  static constexpr int k_numberOfSubviews = 3;
   int numberOfSubviews() const override { return k_numberOfSubviews; }
   Escher::View * subviewAtIndex(int index) override;
+
   Escher::MessageTextView m_intervalLegendView;
   Escher::BufferTextView m_intervalView;
+  BannerView::LabelledView m_labelledIntervalView;
   Escher::MessageTextView m_sizeLegendView;
   Escher::BufferTextView m_sizeView;
+  BannerView::LabelledView m_labelledSizeView;
   Escher::MessageTextView m_frequencyLegendView;
   Escher::BufferTextView m_frequencyView;
+  BannerView::LabelledView m_labelledFrequencyView;
 };
 
 }

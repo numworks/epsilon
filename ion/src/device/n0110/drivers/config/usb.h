b80daf70d35d5ed40569d9b9c9ddb1b532abfd1b
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-03-24 11:05:57 +0100

    [ion/device/n0110/usb] Consolidate configs of Vbus pin
    Even though the configuration of the Vbus pin depends on the hardware
    running Epsilon, we do not actually need one VbusPin object for each
    configuration. Indeed, an AFGPIOPin object can be used to configure the
    pin as a standard GPIO, provided one does not call its init() method.

--- ion/src/device/n0110/drivers/config/usb.h
+++ ion/src/device/n0110/drivers/config/usb.h
@@ -10,8 +10,13 @@ namespace Config {
 
 using namespace Regs;
 
-constexpr static GPIOPin VbusPin = GPIOPin(GPIOA, 9);
-constexpr static AFGPIOPin VbusAFPin = AFGPIOPin(GPIOA, 9, GPIO::AFR::AlternateFunction::AF10, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::Fast);
+/* On the STM32F730, PA9 does not actually support alternate function 10.
+ * However, because of the wiring of the USB connector on the N0110, detection
+ * of when the device is plugged requires the use of this undocumented setting.
+ * After the revision of the USB connector and ESD protection, can can now
+ * follow the specification and configure the Vbus pin as a floating-input GPIO.
+ */
+constexpr static AFGPIOPin VbusPin = AFGPIOPin(GPIOA, 9, GPIO::AFR::AlternateFunction::AF10, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::Fast);
 
 constexpr static AFGPIOPin DmPin = AFGPIOPin(GPIOA, 11, GPIO::AFR::AlternateFunction::AF10, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::Fast);
 constexpr static AFGPIOPin DpPin = AFGPIOPin(GPIOA, 12, GPIO::AFR::AlternateFunction::AF10, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::Fast);

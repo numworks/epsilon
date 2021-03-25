b4fbc1124f122fc2d92512ee966a2ff31e9eb7ad
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-03-25 13:34:57 +0100

    [apps/code] Fix helper string comparison

--- apps/code/helpers.cpp
+++ apps/code/helpers.cpp
@@ -1,5 +1,6 @@
 #include "helpers.h"
 #include <escher/clipboard.h>
+#include <string.h>
 
 using namespace Escher;
 
@@ -7,16 +8,20 @@ namespace Code {
 namespace Helpers {
 
 const char * PythonTextForEvent(Ion::Events::Event event) {
+  if (event == Ion::Events::XNT) {
+    return "x";
+  }
+  if (event.text() == nullptr) {
+    return nullptr;
+  }
   for (size_t i=0; i<NumberOfPythonTextPairs; i++) {
     UTF8Helper::TextPair pair = PythonTextPairs[i];
-    if (event.text() == pair.firstString()) {
+    if (strcmp(event.text(), pair.firstString()) == 0) {
       return pair.secondString();
-    }
-    if (event == Ion::Events::XNT) {
-      return "x";
     }
   }
   return nullptr;
 }
+
 }
 }

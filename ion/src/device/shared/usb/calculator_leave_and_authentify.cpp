aedb16c3c3aac52a00db5e2fc6c7077d3dd3a5d0
Émilie Feral <emilie.feral@numworks.com>
2021-03-25 16:35:56 +0100

    [ion] When leaving DFU, we Reset::core, jump on new userland or do nothing according to kernel/userland version + authentication

--- ion/src/device/shared/usb/calculator_leave_and_authentify.cpp
+++ ion/src/device/shared/usb/calculator_leave_and_authentify.cpp
@@ -1,16 +1,16 @@
 #include "calculator.h"
 #include <userland/drivers/board.h>
+#include <userland/drivers/reset.h>
 
 namespace Ion {
 namespace Device {
 namespace USB {
 
 void Calculator::leave(uint32_t leaveAddress) {
-  int currentKernelVersion = 12; //PlatformInfos.kernelVersion();
-  int newKernelVersion = 12; //getPlatformInfo(userlandAddressOtherSlot()).kernelVersion --> implement on userland to know the header
-  int currentUserlandVersion = 22;
-  int newUserlandVersion = 24;
-  Board::switchExecutableSlot(newKernelVersion - currentKernelVersion, newUserlandVersion - currentUserlandVersion); // FONCTION KERNEL QUI ADAPTE
+  uint32_t addressToJumpTo = Board::switchExecutableSlot();
+  if (addressToJumpTo) {
+    Reset::jump(Board::switchExecutableSlot());
+  }
 }
 
 }

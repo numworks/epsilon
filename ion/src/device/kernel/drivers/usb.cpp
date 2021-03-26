461d0b25020511bd95d00ae21092286bca4d577c
Émilie Feral <emilie.feral@numworks.com>
2021-03-26 17:16:41 +0100

    [ion] Keep slot information in SRAM when running DFU driver

--- ion/src/device/kernel/drivers/usb.cpp
+++ ion/src/device/kernel/drivers/usb.cpp
@@ -2,10 +2,33 @@
 #include <kernel/drivers/authentication.h>
 #include <kernel/drivers/board.h>
 #include <kernel/drivers/keyboard.h>
+#include <ion/src/shared/platform_info.h>
+#include <shared/drivers/config/board.h>
+#include <string.h>
 
 namespace Ion {
 namespace Device {
 namespace USB {
+
+class SlotInfo {
+public:
+  SlotInfo() :
+    m_header(Magic),
+    m_footer(Magic)
+  {
+  }
+  void update() {
+    m_slotA = Board::isRunningSlotA();
+    uint32_t slotStart = m_slotA ? Board::slotAUserlandStart() : Board::slotBUserlandStart();
+    memcpy(&m_platformInfoBuffer, reinterpret_cast<PlatformInfo *>(slotStart + Ion::Device::Board::Config::UserlandHeaderOffset), sizeof(PlatformInfo));
+  }
+private:
+  constexpr static uint32_t Magic = 0xEFEEDBBA;
+  uint32_t m_header;
+  bool m_slotA;
+  uint8_t m_platformInfoBuffer[sizeof(PlatformInfo)];
+  uint32_t m_footer;
+};
 
 void willExecuteDFU() {
   assert(Authentication::trustedUserland());
@@ -22,6 +45,10 @@ void willExecuteDFU() {
    * The interrupt service routines live in the Flash and could be overwritten
    * by garbage during a firmware upgrade opration, so we disable them. */
   Board::shutdownInterruptions();
+
+  /* 3- Keep usefull information about the currently running slot */
+  static SlotInfo slotInformation __attribute__((section(".slot_info")));
+  slotInformation.update();
 }
 
 void didExecuteDFU() {

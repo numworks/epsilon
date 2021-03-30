8b38ced9b90920a161ff9a3c49ac6067599878b7
Émilie Feral <emilie.feral@numworks.com>
2021-03-30 12:11:21 +0200

    [ion] Change design of SRAM memory layout (step I)

--- ion/src/device/n0110/shared/drivers/config/board.h
+++ ion/src/device/n0110/shared/drivers/config/board.h
@@ -32,9 +32,15 @@ constexpr static uint32_t BootloaderTrampolineAddress = BootloaderStartAddress +
 // Kernel
 constexpr static uint32_t KernelAStartAddress = ExternalFlash::Config::StartAddress + PersistingBytes::Config::BufferSize;
 constexpr static uint32_t KernelBStartAddress = KernelAStartAddress + ExternalFlash::Config::TotalSize/2;
-constexpr static uint32_t KernelSRAMAddress = 0x20000000;
-constexpr static uint32_t KernelSRAMDataBSSLength = 0x1000;
 constexpr static uint32_t KernelSize = 0x40000; // 256kB
+constexpr static uint32_t SRAMAddress = 0x20000000;
+constexpr static uint32_t KernelSRAMDataBSSLength = 0x1000;
+constexpr static uint32_t BootloaderSRAMDataBSSLength = 0x40;
+constexpr static uint32_t UserlandSRAMAddress = SRAMAddress + KernelSRAMDataBSSLength + BootloaderSRAMDataBSSLength;
+constexpr static uint32_t SRAMLength = 0x40000; // 256kB
+constexpr static uint32_t KernelStackLength = 0x2000; // 8K
+constexpr static uint32_t KernelStackAddress = SRAMLength - KernelStackLength; // 8K
+constexpr static uint32_t UserlandSRAMLength = SRAMLength - KernelSRAMDataBSSLength - BootloaderSRAMDataBSSLength - KernelStackLength;
 
 // Userland
 constexpr static uint32_t UserlandOffsetFromKernel = KernelSize + SizeSize;

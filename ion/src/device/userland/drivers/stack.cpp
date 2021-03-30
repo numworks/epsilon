d562b33c1345e495152dd78a2e0aecbf91225ba8
Émilie Feral <emilie.feral@numworks.com>
2021-03-30 12:11:21 +0200

    [ion] Change design of SRAM memory layout (step I)

--- ion/src/device/userland/drivers/stack.cpp
+++ ion/src/device/userland/drivers/stack.cpp
@@ -1,10 +1,10 @@
 #include <ion.h>
 
-extern const void * _stack_start;
-extern const void * _stack_end;
+extern const void * _process_stack_start;
+extern const void * _process_stack_end;
 
 bool Ion::stackSafe() {
   volatile int stackDummy;
   volatile void * stackPointer = &stackDummy;
-  return (stackPointer >= &_stack_end && stackPointer <= &_stack_start);
+  return (stackPointer >= &_process_stack_end && stackPointer <= &_process_stack_start);
 }

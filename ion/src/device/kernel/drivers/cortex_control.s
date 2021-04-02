8150c0285d9529b087c3afceb18b965197158829
Émilie Feral <emilie.feral@numworks.com>
2021-04-02 17:43:58 +0200

    [ion] Kernel uses the Main stack, userland uses the process stack

--- ion/src/device/kernel/drivers/cortex_control.s
+++ ion/src/device/kernel/drivers/cortex_control.s
@@ -5,7 +5,13 @@
 .thumb
 .global switch_to_unpriviledged
 switch_to_unpriviledged:
-  mov r0, #0x01
+  ldr r0, =_process_stack_start
+  msr psp, r0
+  mrs r0, control
+  /* The first bit defined the unprivileged mode, the second bit indicates to
+   * use the Process Stack Pointer */
+  orrs r0, r0, #0x3
   msr control, r0
+  isb 0xF
   bx  lr
 .type switch_to_unpriviledged, function

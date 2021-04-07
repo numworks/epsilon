5f341778aeb716d5ce074b2b0582898f16d0dfb9
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-04-07 17:24:59 +0200

    [apps] Update new description menu texts

--- apps/code/script_parameter_controller.cpp
+++ apps/code/script_parameter_controller.cpp
@@ -83,7 +83,7 @@ HighlightCell * ScriptParameterController::reusableCell(int index, int type) {
 
 void ScriptParameterController::willDisplayCellForIndex(HighlightCell * cell, int index) {
   if (cell == &m_autoImportScript && !m_script.isNull()) {
-    m_autoImportScript.setSubLabelMessage(I18n::Message::AutoImportScript);
+    m_autoImportScript.setSubLabelMessage(I18n::Message::AutoImportScriptSubLabel);
     SwitchView * switchView = (SwitchView *)m_autoImportScript.accessoryView();
     switchView->setState(m_script.autoImportation());
   }

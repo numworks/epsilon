4e0243386e82e46d900d0702a274a770bcf94008
Émilie Feral <emilie.feral@numworks.com>
2021-04-07 16:07:43 +0200

    [ion] switch_to_unpriviledged: reset the main stack when switching

--- ion/src/device/kernel/drivers/cortex_control.s
+++ ion/src/device/kernel/drivers/cortex_control.s
@@ -5,8 +5,12 @@
 .thumb
 .global switch_to_unpriviledged
 switch_to_unpriviledged:
+  // Initialize the process sp
   ldr r0, =_process_stack_start
   msr psp, r0
+  ldr r0, =_main_stack_start
+  msr msp, r0
+  // Reset the main sp
   mrs r0, control
   /* The first bit defined the unprivileged mode, the second bit indicates to
    * use the Process Stack Pointer */

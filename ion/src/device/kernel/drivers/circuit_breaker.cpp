db15999313bc01fce2085c51d592d5ac9e0c798c
Émilie Feral <emilie.feral@numworks.com>
2021-04-08 14:30:22 +0200

    [ion] CircuitBreaker takes into account that kernel and userland stacks are distinct

--- ion/src/device/kernel/drivers/circuit_breaker.cpp
+++ ion/src/device/kernel/drivers/circuit_breaker.cpp
@@ -8,6 +8,10 @@
 #include <stdint.h>
 #include <string.h>
 
+extern "C" {
+  extern const void * _main_stack_start;
+}
+
 namespace Ion {
 namespace Device {
 namespace CircuitBreaker {
@@ -22,19 +26,16 @@ using namespace Ion::CircuitBreaker;
  * registers of the cortex are read-only). We use pendSV to delay the context
  * switching until no other interruption remains to be executed. */
 
-/* Once in jumping to pendsv_handler, the stack holds the exception context
- * frame but also miscellaneous values that were pushed in the setCheckpoint
- * method of the userland and kernel pendsv_handler_as/pendsv_handler.
- * The stack looks like this:
+/* Once in jumping to pendsv_handler, the process stack holds the exception
+ * context frame but also miscellaneous values that were pushed by the
+ * setCheckpoint method of the userland. The main stack only holds values pushed
+ * by the kernel pendsv_handler.
+ *
+ * The process stack looks like this:
  *
  *        |     |
  *        |     |
- *       D+-----+D  <-- stack pointer stored by setjmp and restore by longjmp
- *        |     |
- *        |     |   <-- Miscellaneous values stored by pendsv_handler_as and
- *        |     |       pendsv_handler before executing setjmp
- *        |     |
- *       C+-----+C  <-- frame address
+ *       C+-----+C  <-- Process stack pointer
  *        |     |
  *        |Frame|   <-- Context frame stored on the stack to handle exception/
  *        |     |       thread mode switching
@@ -47,28 +48,41 @@ using namespace Ion::CircuitBreaker;
  *        |     |
  *        |     |
  *
+ * The main stack may look like this:
+ *        |     |
+ *        |     |
+ *       D+-----+D  <-- stack pointer stored by setjmp and restore by longjmp
+ *        |     |
+ *        |     |   <-- Miscellaneous values stored by
+ *        |     |       pendsv_handler before executing setjmp
+ *        |     |
+ *       C+-----+C  <-- frame address
+ *        |     |
+ *        |     |
+ *
  * - Details of BC section -
  * Basic context frame description:
  * | r0 | r1 | r2 | r3 | r12 | lr | return address | xPSR | Reserved | Reserved |
  * Extended context frame description:
  * | r0 | r1 | r2 | r3 | r12 | lr | return address | xPSR | s0 | s1 | ... | s 15 | FPSCR | Reserved | Reserved | Reserved |
  *
- * We need to store/restore a stack snapshot from the point A to B to ensure
- * that right after the longjmp:
+ * We need to store/restore a stack snapshot from the point A to C and C to D
+ * to ensure that right after the longjmp:
  * - the pendsv_handler ends correctly (C-D section)
  * - the pendsv_handler returns to the right method among the userland API
  *   Ion::CircuitBreaker (B-C section)
  * - the Ion::CircuitBreaker userland API returns correclty to the application
  *   code that used the Ion::CircuitBreaker API initially (A-B section)
  *
- * The userland setCheckpoint is reponsible for extracting the stack pointer
- * value when entering the fonction to assess the value of A address.
+ * The userland setCheckpoint is reponsible for extracting the function frame
+ * address when entering the fonction to assess the value of A address.
  * The pendsv_handler extracts the stack pointer right before executing setjmp
  * to get the value of D address.
  */
 
 // Empirically, the snapshot section AD is up to 168 bytes long
-static constexpr size_t k_stackSnapshotMaxSize = 256;
+static constexpr size_t k_processStackSnapshotMaxSize = 144;
+static constexpr size_t k_mainStackSnapshotMaxSize = 48;
 
 enum class InternalStatus {
   Set,
@@ -80,23 +94,17 @@ enum class InternalStatus {
 InternalStatus sInternalStatus = InternalStatus::Loaded;
 CheckpointType sCheckpointType = CheckpointType::Home;
 
-// Home checkpoint snapshot
-uint8_t sHomeStackSnapshot[k_stackSnapshotMaxSize];
-uint8_t * sHomeStackSnapshotAddress = nullptr;
-size_t sHomeStackSnapshotSize;
-jmp_buf sHomeRegisters;
+constexpr static int k_numberOfCheckpointTypes = 3;
 
-// User checkpoint snapshot
-uint8_t sUserStackSnapshot[k_stackSnapshotMaxSize];
-uint8_t * sUserStackSnapshotAddress = nullptr;
-size_t sUserStackSnapshotSize;
-jmp_buf sUserRegisters;
-
-// System checkpoint snapshot
-uint8_t sSystemStackSnapshot[k_stackSnapshotMaxSize];
-uint8_t * sSystemStackSnapshotAddress = nullptr;
-size_t sSystemStackSnapshotSize;
-jmp_buf sSystemRegisters;
+// Process snapshots
+uint8_t sProcessStackSnapshot[k_numberOfCheckpointTypes][k_processStackSnapshotMaxSize];
+uint8_t * sProcessStackSnapshotAddress[k_numberOfCheckpointTypes] = {nullptr, nullptr, nullptr};
+size_t sProcessStackSnapshotSize[k_numberOfCheckpointTypes];
+// Main snapshots
+uint8_t sMainStackSnapshot[k_numberOfCheckpointTypes][k_mainStackSnapshotMaxSize];
+uint8_t * sMainStackSnapshotAddress[k_numberOfCheckpointTypes] = {nullptr, nullptr, nullptr};
+// Registers buffers
+jmp_buf sRegisters[k_numberOfCheckpointTypes];
 
 Ion::CircuitBreaker::Status status() {
   if (sInternalStatus == InternalStatus::Set) {
@@ -109,30 +117,11 @@ Ion::CircuitBreaker::Status status() {
 }
 
 bool hasCheckpoint(CheckpointType type) {
-  switch (type) {
-    case CheckpointType::Home:
-      return sHomeStackSnapshotAddress != nullptr;
-    case CheckpointType::User:
-      return sUserStackSnapshotAddress != nullptr;
-    default:
-      assert(type == CheckpointType::System);
-      return sSystemStackSnapshotAddress != nullptr;
-  }
+  return sProcessStackSnapshotAddress[static_cast<int>(type)] != nullptr;
 }
 
 void unsetCheckpoint(CheckpointType type) {
-  switch (type) {
-    case CheckpointType::Home:
-      sHomeStackSnapshotAddress = nullptr;
-      return;
-    case CheckpointType::User:
-      sUserStackSnapshotAddress = nullptr;
-      return;
-    default:
-      assert(type == CheckpointType::System);
-      sSystemStackSnapshotAddress = nullptr;
-      return;
-  }
+  sProcessStackSnapshotAddress[static_cast<int>(type)] = nullptr;
 }
 
 static inline void setPendingAction(CheckpointType type, InternalStatus action) {
@@ -166,64 +155,61 @@ void loadCheckpoint(CheckpointType type) {
 using namespace Ion::Device::CircuitBreaker;
 
 void pendsv_handler() {
-  // status is supposed to be one of the pending status
-  assert(sInternalStatus == InternalStatus::PendingSetCheckpoint || sInternalStatus == InternalStatus::PendingLoadCheckpoint);
+  /* We restrain the use of the main stack since we don't take any snapshot of
+   * if before a setjmp. */
+  {
+    // status is supposed to be one of the pending status
+    assert(sInternalStatus == InternalStatus::PendingSetCheckpoint || sInternalStatus == InternalStatus::PendingLoadCheckpoint);
 
-  if (sInternalStatus == InternalStatus::PendingLoadCheckpoint) {
-    switch (sCheckpointType) {
-      case CheckpointType::Home:
-        longjmp(sHomeRegisters, 1);
-      case CheckpointType::User:
-        longjmp(sUserRegisters, 1);
-      default:
-        assert(sCheckpointType == CheckpointType::System);
-        longjmp(sSystemRegisters, 1);
-    }
-  } else {
-    assert(sInternalStatus == InternalStatus::PendingSetCheckpoint);
-    // Extract current stack pointer
-    uint8_t * stackPointerAddress = nullptr;
-    asm volatile ("mov %[stackPointer], sp" : [stackPointer] "=r" (stackPointerAddress) :);
-
-    // Store the stack frame
-    jmp_buf * regsBufferAddress;
-    if (sCheckpointType == CheckpointType::Home) {
-      regsBufferAddress = &sHomeRegisters;
-      sHomeStackSnapshotAddress = stackPointerAddress;
-      sHomeStackSnapshotSize = sASectionStart - stackPointerAddress;
-      assert(sHomeStackSnapshotSize <= k_stackSnapshotMaxSize);
-      memcpy(sHomeStackSnapshot, sHomeStackSnapshotAddress, sHomeStackSnapshotSize);
-    } else if (sCheckpointType == CheckpointType::User) {
-      regsBufferAddress = &sUserRegisters;
-      sUserStackSnapshotAddress = stackPointerAddress;
-      sUserStackSnapshotSize = sASectionStart - stackPointerAddress;
-      assert(sUserStackSnapshotSize <= k_stackSnapshotMaxSize);
-      memcpy(sUserStackSnapshot, sUserStackSnapshotAddress, sUserStackSnapshotSize);
-    } else {
-      assert(sCheckpointType == CheckpointType::System);
-      regsBufferAddress = &sSystemRegisters;
-      sSystemStackSnapshotAddress = stackPointerAddress;
-      sSystemStackSnapshotSize = sASectionStart - stackPointerAddress;
-      assert(sSystemStackSnapshotSize <= k_stackSnapshotMaxSize);
-      memcpy(sSystemStackSnapshot, sSystemStackSnapshotAddress, sSystemStackSnapshotSize);
-    }
-
-    // Store the interruption status cortex register
-    if (setjmp(*regsBufferAddress)) {
-      /* WARNING: at this point the stack is corrupted. You can't use variable
-       * stored on the stack. To prevent the compiler to do so, we ensure to
-       * user global variables only which aren't stored on the stack. */
-      // Restore stack frame
-      if (sCheckpointType == CheckpointType::Home) {
-        memcpy(sHomeStackSnapshotAddress, sHomeStackSnapshot, sHomeStackSnapshotSize);
-      } else if (sCheckpointType == CheckpointType::User) {
-        memcpy(sUserStackSnapshotAddress, sUserStackSnapshot, sUserStackSnapshotSize);
-      } else {
-        assert(sCheckpointType == CheckpointType::System);
-        memcpy(sSystemStackSnapshotAddress, sSystemStackSnapshot, sSystemStackSnapshotSize);
-      }
+    if (sInternalStatus == InternalStatus::PendingLoadCheckpoint) {
+      longjmp(sRegisters[static_cast<int>(sCheckpointType)], 1);
     }
   }
+
+  jmp_buf * regsBufferAddress;
+  {
+    assert(sInternalStatus == InternalStatus::PendingSetCheckpoint);
+    int checkpointTypeIndex = static_cast<int>(sCheckpointType);
+
+    // Registers buffer address
+    regsBufferAddress = &sRegisters[checkpointTypeIndex];
+
+    // Store main stack snapshot
+    uint8_t * mainStackPointer = nullptr;
+    asm volatile ("mrs %[stackPointer], msp" : [stackPointer] "=r" (mainStackPointer) :);
+    size_t mainSnapshotSize = reinterpret_cast<uint8_t *>(&_main_stack_start) - mainStackPointer;
+    assert(mainSnapshotSize <= k_mainStackSnapshotMaxSize);
+    memcpy(sMainStackSnapshot[checkpointTypeIndex], mainStackPointer, mainSnapshotSize);
+    sMainStackSnapshotAddress[checkpointTypeIndex] = mainStackPointer;
+
+    // Store process stack snapshot
+    uint8_t * processStackPointer = nullptr;
+    asm volatile ("mrs %[stackPointer], psp" : [stackPointer] "=r" (processStackPointer) :);
+    size_t processSnapshotSize = sASectionStart - processStackPointer;
+    assert(processSnapshotSize <= k_processStackSnapshotMaxSize);
+    memcpy(sProcessStackSnapshot[checkpointTypeIndex], processStackPointer, processSnapshotSize);
+    sProcessStackSnapshotSize[checkpointTypeIndex] = processSnapshotSize;
+    sProcessStackSnapshotAddress[checkpointTypeIndex] = processStackPointer;
+  }
+
+  // Store the interruption status cortex register
+  /* regsBufferAddress is the only variable stored on the main stack. It's
+   * alright since its value is not used after longjmp. */
+  if (setjmp(*regsBufferAddress)) {
+    /* WARNING: at this point the stack is corrupted. You can't use variable
+     * stored on the stack. To prevent the compiler to do so, we ensure to
+     * user global variables only which aren't stored on the stack. */
+    int checkpointTypeIndex = static_cast<int>(sCheckpointType);
+    // Restore process stack
+    memcpy(sProcessStackSnapshotAddress[checkpointTypeIndex], sProcessStackSnapshot[checkpointTypeIndex], sProcessStackSnapshotSize[checkpointTypeIndex]);
+    size_t mainSnapshotSize = reinterpret_cast<uint8_t *>(&_main_stack_start) - sMainStackSnapshotAddress[checkpointTypeIndex];
+    // Restore main stack
+    memcpy(sMainStackSnapshotAddress[checkpointTypeIndex], sMainStackSnapshot[checkpointTypeIndex], mainSnapshotSize);
+    uint8_t * processStackAddress = sProcessStackSnapshotAddress[static_cast<int>(sCheckpointType)];
+
+    asm volatile ("msr psp, %[stackPointer]" : [stackPointer] "=r" (processStackAddress) :);
+  }
+
   if (sInternalStatus == InternalStatus::PendingLoadCheckpoint) {
     sInternalStatus = InternalStatus::Loaded;
   } else {

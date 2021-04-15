018ea07796f58ca5479cfa6e6052cd9b7f1de5f0
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-04-15 15:02:40 +0200

    [ion/device] Add comments and rename variables for clarity

--- ion/src/device/shared/drivers/serial_number.cpp
+++ ion/src/device/shared/drivers/serial_number.cpp
@@ -20,6 +20,7 @@ const char * pcbVersion() {
   static char pcbVer[pcbVersionLength] = {'\0'};
   if (pcbVer[0] == '\0') {
     Device::Board::PCBVersion ver = Device::Board::pcbVersion();
+    /* As PCB version only uses 4 chars, value should be at most 9999. */
     assert(ver < 10000);
     for (int i = pcbVersionLength - 1; i >= 0; i--) {
       if (i == 2) {

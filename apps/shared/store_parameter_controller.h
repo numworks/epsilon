959ec57c27e16f4285562f7c5bab4ce535a9460f
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-04-15 15:46:53 +0200

    [apps/stat] Reorder lines in store parameters
    Change-Id: Id754c030f6b7261339477a80b18cc2c0b484b5cb

--- apps/shared/store_parameter_controller.h
+++ apps/shared/store_parameter_controller.h
@@ -46,9 +46,9 @@ private:
   virtual I18n::Message sortMessage() { return m_xColumnSelected ? I18n::Message::SortValues : I18n::Message::SortSizes; }
   void deleteColumn();
 
-  constexpr static int k_indexOfRemoveColumn = 0;
-  constexpr static int k_indexOfFillFormula = k_indexOfRemoveColumn + 1;
-  constexpr static int k_indexOfSortValues = k_indexOfFillFormula + 1;
+  constexpr static int k_indexOfSortValues = 0;
+  constexpr static int k_indexOfFillFormula = k_indexOfSortValues + 1;
+  constexpr static int k_indexOfRemoveColumn = k_indexOfFillFormula + 1;
   constexpr static int k_defaultCellType = 0;
   constexpr static int k_sortCellType = 1;
 

b8400d8037cbae6d656d4293cd68e223c382da5e
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-04-15 12:33:22 +0200

    [apps] correct cell height with different types
    Overridden nonMemoizedRowHeight to avoid hardcoding cell types order
    
    Change-Id: I7b6c0ef2b78f8d8b7d0be4210567c61c34a792c9

--- apps/shared/store_parameter_controller.cpp
+++ apps/shared/store_parameter_controller.cpp
@@ -119,6 +119,19 @@ HighlightCell * StoreParameterController::reusableCell(int index, int type) {
   return &m_cells[index];
 }
 
+KDCoordinate StoreParameterController::nonMemoizedRowHeight(int index) {
+  Escher::HighlightCell * cell;
+  if (index == k_indexOfSortValues) {
+    cell = reusableCell(0, k_sortCellType);
+  } else {
+    assert(index == k_defaultCellType);
+    // offset index if cell located after sortCell
+    int indexForDefaultType = (index < k_indexOfSortValues) ? index : index - 1;
+    cell = reusableCell(indexForDefaultType, k_defaultCellType);
+  }
+  return heightForCellAtIndex(cell, index, false);
+}
+
 void StoreParameterController::deleteColumn() {
   if (m_xColumnSelected) {
     m_store->deleteAllPairsOfSeries(m_series);

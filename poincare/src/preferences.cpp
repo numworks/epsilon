04268f6102af299b1722b56e39c94068915583b0
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-04-16 18:22:35 +0200

    [poincare/preferences] Set default precision to 10 digits

--- poincare/src/preferences.cpp
+++ poincare/src/preferences.cpp
@@ -1,18 +1,19 @@
 #include <poincare/preferences.h>
-#include <poincare/print_float.h>
 
 namespace Poincare {
 
+constexpr int Preferences::DefaultNumberOfPrintedSignificantDigits;
 constexpr int Preferences::LargeNumberOfSignificantDigits;
 constexpr int Preferences::MediumNumberOfSignificantDigits;
 constexpr int Preferences::ShortNumberOfSignificantDigits;
+constexpr int Preferences::VeryShortNumberOfSignificantDigits;
 
 Preferences::Preferences() :
   m_angleUnit(AngleUnit::Degree),
   m_displayMode(Preferences::PrintFloatMode::Decimal),
   m_editionMode(EditionMode::Edition2D),
   m_complexFormat(Preferences::ComplexFormat::Real),
-  m_numberOfSignificantDigits(PrintFloat::k_numberOfPrintedSignificantDigits)
+  m_numberOfSignificantDigits(Preferences::DefaultNumberOfPrintedSignificantDigits)
 {}
 
 Preferences * Preferences::sharedPreferences() {

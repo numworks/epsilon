fe99454d3b3eb387708e5e66d47b552354e5e372
Émilie Feral <emilie.feral@numworks.com>
2021-04-23 17:59:07 +0200

    [apps/home] AppCell accepts PointerTextView to display app name

--- apps/home/app_cell.h
+++ apps/home/app_cell.h
@@ -4,6 +4,8 @@
 #include <escher/app.h>
 #include <escher/highlight_cell.h>
 #include <escher/image_view.h>
+#include <escher/pointer_text_view.h>
+#include <ion/external_apps.h>
 
 namespace Home {
 
@@ -19,14 +21,17 @@ public:
   void setVisible(bool visible);
   void reloadCell() override;
   void setAppDescriptor(Escher::App::Descriptor * appDescriptor);
+  void setExternalApp(Ion::ExternalApps::App app);
 private:
   static constexpr KDCoordinate k_iconMargin = 22;
   static constexpr KDCoordinate k_iconWidth = 55;
   static constexpr KDCoordinate k_iconHeight = 56;
   static constexpr KDCoordinate k_nameWidthMargin = 4;
   static constexpr KDCoordinate k_nameHeightMargin = 1;
+  const Escher::TextView * textView() const;
   Escher::ImageView m_iconView;
-  Escher::MessageTextView m_nameView;
+  Escher::MessageTextView m_messageNameView;
+  Escher::PointerTextView m_pointerNameView;
   bool m_visible;
 };
 

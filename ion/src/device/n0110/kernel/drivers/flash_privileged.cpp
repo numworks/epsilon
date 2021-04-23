9a32e9614f46008379988928359d11ec23b1bb8c
Émilie Feral <emilie.feral@numworks.com>
2021-04-23 11:50:24 +0200

    [ion] N0110 flash: enable flashing the external apps sector while running the same slot

--- ion/src/device/n0110/kernel/drivers/flash_privileged.cpp
+++ ion/src/device/n0110/kernel/drivers/flash_privileged.cpp
@@ -1,5 +1,6 @@
 #include <shared/drivers/flash_privileged.h>
 #include <kernel/drivers/board.h>
+#include <shared/drivers/config/board.h>
 #include <shared/drivers/config/external_flash.h>
 
 namespace Ion {
@@ -8,9 +9,9 @@ namespace Flash {
 
 bool ForbiddenSector(int i) {
   if (Board::isRunningSlotA()) {
-    return i < ExternalFlash::Config::NumberOfSectors/2 || i >= ExternalFlash::Config::NumberOfSectors;
+    return i < ExternalFlash::Config::NumberOfSectors/2 - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors;
   }
-  return i < 0 || i >= ExternalFlash::Config::NumberOfSectors/2;
+  return i < 0 || (i >= ExternalFlash::Config::NumberOfSectors/2 && i < ExternalFlash::Config::NumberOfSectors - Board::Config::ExternalAppsNumberOfSector) || i >= ExternalFlash::Config::NumberOfSectors;
 }
 
 bool MassEraseEnable() {

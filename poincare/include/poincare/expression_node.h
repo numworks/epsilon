09dd52cebd41884775b6c42f703076fe87464e9c
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-04-26 14:49:27 +0200

    [poincare] Add symbolicComputation setter

--- poincare/include/poincare/expression_node.h
+++ poincare/include/poincare/expression_node.h
@@ -205,6 +205,7 @@ public:
     Preferences::UnitFormat unitFormat() const { return m_unitFormat; }
     ReductionTarget target() const { return m_target; }
     SymbolicComputation symbolicComputation() const { return m_symbolicComputation; }
+    void setSymbolicComputation(SymbolicComputation symbolicComputation) { m_symbolicComputation = symbolicComputation; }
     UnitConversion unitConversion() const { return m_unitConversion; }
   private:
     Preferences::UnitFormat m_unitFormat;

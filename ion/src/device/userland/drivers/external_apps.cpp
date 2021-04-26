9785fd9fb8540184403c8cf169c25f83b803fb90
Émilie Feral <emilie.feral@numworks.com>
2021-04-26 16:24:05 +0200

    [ion] ExternalApps: add misc. verification to avoid crashing on misformed external app

--- ion/src/device/userland/drivers/external_apps.cpp
+++ ion/src/device/userland/drivers/external_apps.cpp
@@ -3,6 +3,7 @@
 
 extern "C" {
   uint8_t _external_apps_start;
+  uint8_t _external_apps_end;
 }
 
 namespace Ion {
@@ -19,23 +20,53 @@ namespace ExternalApps {
  * - 4 bytes: the same magic code 0xBABECODE
  */
 
+constexpr int k_numberOfAppInfoElements = 8;
+constexpr uint32_t k_minAppSize = sizeof(uint32_t) * k_numberOfAppInfoElements;
+
 App::App(uint8_t * a) : m_startAddress(a) {
   assert(*reinterpret_cast<uint32_t *>(m_startAddress) == k_magic);
 }
 
+bool addressWithinExternalAppsSection(const uint8_t * address) {
+  return address >= &_external_apps_start && address < &_external_apps_end;
+}
+
 uint8_t * App::addressAtIndexInAppInfo(int index) const {
- return m_startAddress + *reinterpret_cast<uint32_t *>(m_startAddress + index*sizeof(uint32_t));
+  assert(index < k_numberOfAppInfoElements);
+  uint8_t * address = m_startAddress + *reinterpret_cast<uint32_t *>(m_startAddress + index*sizeof(uint32_t));
+ // Check that address is in authorized memory
+  if (!addressWithinExternalAppsSection(address)) {
+    return nullptr;
+  }
+  return address;
 }
 
 const char * App::name() const {
- return reinterpret_cast<const char *>(addressAtIndexInAppInfo(2));
+ const char * n = reinterpret_cast<const char *>(addressAtIndexInAppInfo(2));
+ if (n == nullptr) {
+   return nullptr;
+ }
+ // Check that all name is within authorized memory
+ const char * c = n;
+ while (*c != 0) {
+   if (!addressWithinExternalAppsSection(reinterpret_cast<const uint8_t *>(c++))) {
+     return nullptr;
+   }
+ }
+ return n;
 }
 
 uint32_t App::iconSize() const {
-  return *reinterpret_cast<uint32_t *>(m_startAddress + 3*sizeof(uint32_t));
+  uint32_t size = *reinterpret_cast<uint32_t *>(m_startAddress + 3*sizeof(uint32_t));
+  const uint8_t * data = iconData();
+  if (data == nullptr || !addressWithinExternalAppsSection(data + size - 1)) {
+    return 0;
+  }
+  return size;
 }
 
 const uint8_t * App::iconData() const {
+  // TODO: Add check on decompression?
   return reinterpret_cast<const uint8_t *>(addressAtIndexInAppInfo(4));
 }
 
@@ -50,7 +81,7 @@ bool App::appAtAddress(uint8_t * address) {
 AppIterator & AppIterator::operator++() {
   uint32_t sizeOfCurrentApp = *reinterpret_cast<uint32_t *>(m_currentAddress + 6*sizeof(uint32_t));
   m_currentAddress += sizeOfCurrentApp;
-  if (!App::appAtAddress(m_currentAddress)) {
+  if (m_currentAddress < &_external_apps_start || m_currentAddress > &_external_apps_end  - k_minAppSize || !App::appAtAddress(m_currentAddress)) {
     m_currentAddress = nullptr;
   }
   return *this;

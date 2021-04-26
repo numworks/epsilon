5269f5ef14813d29d02640e848e6d61d73f388b5
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-04-26 11:13:29 +0200

    [shared/interactive_curve_view_range] Add method to improve full range
    This delegate method is only called if both axes are marked as
    modifiable.
    This implementation zoom out when a function is only displayed close to
    the borders of a window and the center is left empty.

--- apps/shared/interactive_curve_view_range_delegate.h
+++ apps/shared/interactive_curve_view_range_delegate.h
@@ -15,12 +15,14 @@ public:
 
   static void DefaultComputeXRange(float xMinLimit, float xMaxLimit, float * xMin, float * xMax, float * yMinIntrinsic, float * yMaxIntrinsic, Poincare::Context * context, FunctionStore * functionStore);
   static void DefaultComputeYRange(float xMin, float xMax, float yMinIntrinsic, float yMaxIntrinsic, float ratio, float * yMin, float * yMax, Poincare::Context * context, FunctionStore * functionStore);
+  static void DefaultImproveFullRange(float * xMin, float * xMax, float * yMin, float * yMax, Poincare::Context * context, FunctionStore * functionStore);
   static float DefaultAddMargin(float x, float range, bool isVertical, bool isMin, float top, float bottom, float left, float right);
 
   virtual float interestingXMin() const { return -k_defaultXHalfRange; }
   virtual bool defaultRangeIsNormalized() const { return false; }
   virtual void computeXRange(float xMinLimit, float xMaxLimit, float * xMin, float * xMax, float * yMinIntrinsic, float * yMaxIntrinsic) { assert(false); }
   virtual void computeYRange(float xMin, float xMax, float yMinIntrinsic, float yMaxIntrinsic, float * yMin, float * yMax) { assert(false); }
+  virtual void improveFullRange(float * xMin, float * xMax, float * yMin, float * yMax) { assert(false); }
   virtual float addMargin(float x, float range, bool isVertical, bool isMin) = 0;
   virtual void updateBottomMargin() = 0;
   virtual void updateZoomButtons() = 0;

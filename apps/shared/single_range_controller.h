27cd317d0758964e6ff4e6f2934e27c603e98012
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-04-27 17:53:46 +0200

    [shared] Class SimpleFloatParameterController
    The class SimpleFloatParameterController implements all functionnalities
    of the old FloatParameterController class, but without the OK button.
    FloatParameterController dervies from SimpleFloatParameterController and
    adds an OK button.

--- apps/shared/single_range_controller.h
+++ apps/shared/single_range_controller.h
@@ -1,19 +1,19 @@
 #ifndef SHARED_SINGLE_RANGE_CONTROLLER_H
 #define SHARED_SINGLE_RANGE_CONTROLLER_H
 
-#include "float_parameter_controller.h"
+#include "simple_float_parameter_controller.h"
 #include "interactive_curve_view_range.h"
 #include <escher/message_table_cell_with_editable_text.h>
 #include <escher/message_table_cell_with_switch.h>
 
 namespace Shared {
 
-class SingleRangeController : public FloatParameterController<float> {
+class SingleRangeController : public SimpleFloatParameterController<float> {
 public:
   SingleRangeController(Escher::Responder * parentResponder, Escher::InputEventHandlerDelegate * inputEventHandlerDelegate, InteractiveCurveViewRange * interactiveCurveViewRange);
 
   const char * title() override { return I18n::translate(m_editXRange ? I18n::Message::ValuesOfX : I18n::Message::ValuesOfY); }
-  int numberOfRows() const override { return k_numberOfTextCells + 2; }
+  int numberOfRows() const override { return k_numberOfTextCells + 1; }
   void willDisplayCellForIndex(Escher::HighlightCell * cell, int index) override;
   bool handleEvent(Ion::Events::Event event) override;
   void setEditXRange(bool editXRange) { m_editXRange = editXRange; }

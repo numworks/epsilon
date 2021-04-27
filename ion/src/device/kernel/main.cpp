74a69d88e548feaea601f0dc47af742ef9d7ca6a
Émilie Feral <emilie.feral@numworks.com>
2021-04-27 17:35:33 +0200

    [ion] Remove unused code

--- ion/src/device/kernel/main.cpp
+++ ion/src/device/kernel/main.cpp
@@ -2,23 +2,11 @@
 #include <drivers/reset.h>
 #include <kernel/drivers/authentication.h>
 #include <kernel/drivers/cortex_control.h>
-#include <kernel/drivers/timing.h>
-#include <kernel/warning_display.h>
-#include <kernel/drivers/backlight.h>
-#include <shared/drivers/display.h>
-#include <ion/display.h>
-#include <kandinsky/font.h>
+#include <assert.h>
 #include <string.h>
 
 void kernel_main() {
-  // TODO: move this code after a DFU leave. There is no reason to execute a untrusted userland after a reset
-  // Display warning for unauthenticated software
-  if (!Ion::Device::Authentication::trustedUserland()) {
-    Ion::Device::Backlight::init();
-    Ion::Device::WarningDisplay::unauthenticatedUserland();
-    Ion::Timing::msleep(5000);
-    Ion::Device::Backlight::shutdown();
-  }
+  assert(Ion::Device::Authentication::trustedUserland());
 
   switch_to_unpriviledged();
 

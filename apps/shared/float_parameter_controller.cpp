4ff7348e752bc6de85b67e2eff4e5ac8185dfb36
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-04-27 17:53:46 +0200

    [shared] Class SimpleFloatParameterController
    The class SimpleFloatParameterController implements all functionnalities
    of the old FloatParameterController class, but without the OK button.
    FloatParameterController dervies from SimpleFloatParameterController and
    adds an OK button.

--- apps/shared/float_parameter_controller.cpp
+++ apps/shared/float_parameter_controller.cpp
@@ -1,9 +1,5 @@
 #include "float_parameter_controller.h"
-#include "../shared/poincare_helpers.h"
-#include <escher/message_table_cell_with_editable_text.h>
-#include <poincare/preferences.h>
 #include <assert.h>
-#include <cmath>
 
 using namespace Escher;
 using namespace Poincare;
@@ -12,66 +8,28 @@ namespace Shared {
 
 template<typename T>
 FloatParameterController<T>::FloatParameterController(Responder * parentResponder) :
-  SelectableListViewController(parentResponder),
-  m_okButton(&m_selectableTableView, I18n::Message::Ok, Invocation([](void * context, void * sender) {
+  SimpleFloatParameterController<T>(parentResponder),
+  m_okButton(&(this->m_selectableTableView), I18n::Message::Ok, Invocation([](void * context, void * sender) {
       FloatParameterController * parameterController = (FloatParameterController *) context;
       parameterController->buttonAction();
       return true;
     }, this))
-{
-}
-
-template<typename T>
-void FloatParameterController<T>::didBecomeFirstResponder() {
-  if (selectedRow() >= 0) {
-    int selRow = selectedRow();
-    selRow = selRow >= numberOfRows() ? numberOfRows()-1 : selRow;
-    int selColumn = selectedColumn();
-    selColumn = selColumn >= numberOfColumns() ? numberOfColumns() - 1 : selColumn;
-    selectCellAtLocation(selColumn, selRow);
-  }
-  Container::activeApp()->setFirstResponder(&m_selectableTableView);
-}
+{}
 
 template<typename T>
 void FloatParameterController<T>::viewWillAppear() {
-  ViewController::viewWillAppear();
-  if (selectedRow() == -1 || selectedRow() == numberOfRows()-1) {
-    selectCellAtLocation(0, 0);
-  } else {
-    int selRow = selectedRow();
-    selRow = selRow >= numberOfRows() ? numberOfRows()-1 : selRow;
-    int selColumn = selectedColumn();
-    selColumn = selColumn >= numberOfColumns() ? numberOfColumns() - 1 : selColumn;
-    selectCellAtLocation(selColumn, selRow);
-  }
-  resetMemoization();
-  m_selectableTableView.reloadData();
-}
-
-template<typename T>
-void FloatParameterController<T>::viewDidDisappear() {
-  if (parentResponder() == nullptr) {
-    m_selectableTableView.deselectTable();
-    m_selectableTableView.scrollToCell(0,0);
-  }
-}
-
-template<typename T>
-bool FloatParameterController<T>::handleEvent(Ion::Events::Event event) {
-  if (event == Ion::Events::Back) {
-    stackController()->pop();
-    return true;
-  }
-  return false;
+  if (this->selectedRow() == this->numberOfRows()-1) {
+    FloatParameterController<T>::selectCellAtLocation(0, 0);
+  };
+  SimpleFloatParameterController<T>::viewWillAppear();
 }
 
 template<typename T>
 int FloatParameterController<T>::typeAtIndex(int index) {
-  if (index == numberOfRows()-1) {
+  if (index == this->numberOfRows()-1) {
     return k_buttonCellType;
   }
-  return k_parameterCellType;
+  return SimpleFloatParameterController<T>::typeAtIndex(index);
 }
 
 template<typename T>
@@ -79,7 +37,7 @@ int FloatParameterController<T>::reusableCellCount(int type) {
   if (type == k_buttonCellType) {
     return 1;
   }
-  return reusableParameterCellCount(type);
+  return SimpleFloatParameterController<T>::reusableCellCount(type);
 }
 
 template<typename T>
@@ -87,75 +45,28 @@ HighlightCell * FloatParameterController<T>::reusableCell(int index, int type) {
   if (type == k_buttonCellType) {
     return &m_okButton;
   }
-  return reusableParameterCell(index, type);
+  return SimpleFloatParameterController<T>::reusableCell(index, type);
 }
 
 template<typename T>
 KDCoordinate FloatParameterController<T>::nonMemoizedRowHeight(int j) {
-  if (j == numberOfRows()-1) {
+  if (j == this->numberOfRows()-1) {
     return m_okButton.minimalSizeForOptimalDisplay().height();
   }
-  return MemoizedListViewDataSource::nonMemoizedRowHeight(j);
+  return SimpleFloatParameterController<T>::nonMemoizedRowHeight(j);
 }
 
 template<typename T>
 void FloatParameterController<T>::willDisplayCellForIndex(HighlightCell * cell, int index) {
-  if (index == numberOfRows()-1) {
+  if (index == this->numberOfRows()-1) {
     return;
   }
-  MessageTableCellWithEditableText * myCell = (MessageTableCellWithEditableText *) cell;
-  if (myCell->isEditing()) {
-    return;
-  }
-  constexpr int precision = Preferences::LargeNumberOfSignificantDigits;
-  constexpr int bufferSize = PrintFloat::charSizeForFloatsWithPrecision(precision);
-  char buffer[bufferSize];
-  PoincareHelpers::ConvertFloatToTextWithDisplayMode<T>(parameterAtIndex(index), buffer, bufferSize, precision, Preferences::PrintFloatMode::Decimal);
-  myCell->setSubLabelText(buffer);
-}
-
-template<typename T>
-bool FloatParameterController<T>::textFieldShouldFinishEditing(TextField * textField, Ion::Events::Event event) {
-  return (event == Ion::Events::Down && selectedRow() < numberOfRows()-1)
-     || (event == Ion::Events::Up && selectedRow() > 0)
-     || TextFieldDelegate::textFieldShouldFinishEditing(textField, event);
-}
-
-template<typename T>
-bool FloatParameterController<T>::textFieldDidFinishEditing(TextField * textField, const char * text, Ion::Events::Event event) {
-  T floatBody;
-  int row = selectedRow();
-  InfinityTolerance infTolerance = infinityAllowanceForRow(row);
-  if (textFieldDelegateApp()->hasUndefinedValue(text, floatBody, infTolerance == InfinityTolerance::PlusInfinity, infTolerance == InfinityTolerance::MinusInfinity)) {
-    return false;
-  }
-  if (!setParameterAtIndex(row, floatBody)) {
-    return false;
-  }
-  resetMemoization();
-  m_selectableTableView.reloadCellAtLocation(0, activeCell());
-  m_selectableTableView.reloadData();
-  if (event == Ion::Events::EXE || event == Ion::Events::OK) {
-    m_selectableTableView.selectCellAtLocation(selectedColumn(), row + 1);
-  } else {
-    m_selectableTableView.handleEvent(event);
-  }
-  return true;
-}
-
-template<typename T>
-int FloatParameterController<T>::activeCell() {
-  return selectedRow();
-}
-
-template<typename T>
-StackViewController * FloatParameterController<T>::stackController() {
-  return (StackViewController *)parentResponder();
+  SimpleFloatParameterController<T>::willDisplayCellForIndex(cell, index);
 }
 
 template<typename T>
 void FloatParameterController<T>::buttonAction() {
-  StackViewController * stack = stackController();
+  StackViewController * stack = FloatParameterController<T>::stackController();
   stack->pop();
 }
 

8c73d569e908f9b36cdd4e65fa6e22d55304928b
Émilie Feral <emilie.feral@numworks.com>
2021-04-28 12:44:08 +0200

    [ion] Non-authenticated userland can still flash the external app section

--- ion/src/device/n0110/kernel/drivers/flash_privileged.cpp
+++ ion/src/device/n0110/kernel/drivers/flash_privileged.cpp
@@ -1,4 +1,5 @@
 #include <shared/drivers/flash_privileged.h>
+#include <kernel/drivers/authentication.h>
 #include <kernel/drivers/board.h>
 #include <shared/drivers/config/board.h>
 #include <shared/drivers/config/external_flash.h>
@@ -8,11 +9,22 @@ namespace Device {
 namespace Flash {
 
 bool ForbiddenSector(int i) {
-  if (Board::isRunningSlotA()) {
-    return i < ExternalFlash::Config::NumberOfSectors/2 - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors;
+  if (Authentication::trustedUserland()) {
+    /* Authenticated userland can write the external apps section of both slots
+     * and the whole other slot. */
+    if (Board::isRunningSlotA()) {
+      return i < ExternalFlash::Config::NumberOfSectors/2 - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors;
+    }
+    return i < 0 || (i >= ExternalFlash::Config::NumberOfSectors/2 && i < ExternalFlash::Config::NumberOfSectors - Board::Config::ExternalAppsNumberOfSector) || i >= ExternalFlash::Config::NumberOfSectors;
   }
-  return i < 0 || (i >= ExternalFlash::Config::NumberOfSectors/2 && i < ExternalFlash::Config::NumberOfSectors - Board::Config::ExternalAppsNumberOfSector) || i >= ExternalFlash::Config::NumberOfSectors;
+  /* Non-authenticated userland can only write the external apps section of the
+   * running slot. */
+  if (Board::isRunningSlotA()) {
+    return i < ExternalFlash::Config::NumberOfSectors/2 - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors/2;
+  }
+  return i < ExternalFlash::Config::NumberOfSectors - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors;
 }
+
 
 bool MassEraseEnable() {
   return false;

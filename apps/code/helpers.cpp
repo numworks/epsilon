c32781ae5b27da8b6699390db454dd1a88a25045
Émilie Feral <emilie.feral@numworks.com>
2021-04-29 23:08:15 +0200

    [ion][apps] Fix build

--- apps/code/helpers.cpp
+++ apps/code/helpers.cpp
@@ -12,13 +12,13 @@ const char * PythonTextForEvent(Ion::Events::Event event) {
   if (event == Ion::Events::XNT) {
     return "x";
   }
-  if (event.text() == nullptr) {
+  char buffer[Ion::Events::EventData::k_maxDataSize] = {0};
+  Ion::Events::copyText(static_cast<uint8_t>(event), buffer, Ion::Events::EventData::k_maxDataSize);
+  if (buffer[0] == 0) {
     return nullptr;
   }
   for (size_t i=0; i<NumberOfPythonTextPairs; i++) {
     UTF8Helper::TextPair pair = PythonTextPairs[i];
-    char buffer[Ion::Events::EventData::k_maxDataSize] = {0};
-    Ion::Events::copyText(static_cast<uint8_t>(event), buffer, Ion::Events::EventData::k_maxDataSize);
     if (strcmp(buffer, pair.firstString()) == 0) {
       return pair.secondString();
     }

53d2e998e8d2c3813d51f32959fb229b1251c0cd
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-04-29 16:50:54 +0200

    [distribution] Add a new_app to replace probability
    Change-Id: I1b6d2272cde157e1600715c3a0fdd1089def5dce

---
+++ apps/probability/menu_controller.cpp
@@ -0,0 +1,40 @@
+#include "menu_controller.h"
+#include <escher/highlight_cell.h>
+#include <escher/responder.h>
+#include <escher/selectable_list_view_controller.h>
+#include <escher/message_table_cell.h>
+#include <assert.h>
+#include <apps/i18n.h>
+#include <escher/container.h>
+
+namespace Probability {
+
+MenuController::MenuController(Responder * parentResponder) :
+ SelectableListViewController(parentResponder),
+ m_labels{I18n::Message::ProbaApp, I18n::Message::ProbaApp, I18n::Message::ProbaApp}
+{ }
+
+void MenuController::willDisplayCellForIndex(Escher::HighlightCell * cell, int row) {
+  Escher::MessageTableCell * msgCell = static_cast<Escher::MessageTableCell *>(cell);
+  msgCell->setMessage(m_labels[row]);
+}
+
+void MenuController::didBecomeFirstResponder() {
+  if (selectedRow() == -1) {
+    selectCellAtLocation(0, 0);
+  } else {
+    selectCellAtLocation(selectedColumn(), selectedRow());
+  }
+  Escher::Container::activeApp()->setFirstResponder(&m_selectableTableView);
+}
+
+Escher::HighlightCell * MenuController::reusableCell(int index, int type) {
+  // Return correct cell
+  assert(type == 0);
+  assert(index >= 0 && index <= k_numberOfCells);
+  return &m_cells[index];
+}
+
+
+
+}
---
+++ apps/probability/menu_controller.h
@@ -0,0 +1,30 @@
+#ifndef MENU_CONTROLLER_H
+#define MENU_CONTROLLER_H
+
+#include <escher/selectable_list_view_controller.h>
+#include <escher/table_cell.h>
+#include <escher/message_table_cell.h>
+#include <apps/i18n.h>
+
+namespace Probability {
+
+// Controller
+class MenuController : public Escher::SelectableListViewController {
+public:
+  MenuController(Responder * parentResponder);
+  int numberOfRows() const override { return k_numberOfCells; }
+  void willDisplayCellForIndex(Escher::HighlightCell * cell, int row) override;
+  Escher::HighlightCell * reusableCell(int index, int type) override;
+  void didBecomeFirstResponder();
+  bool handleEvent(Ion::Events::Event event) override { return false; };
+  const char * title() override { return "Distribution"; }
+
+private:
+  constexpr static int k_numberOfCells = 3;
+  Escher::MessageTableCell m_cells[k_numberOfCells];
+  I18n::Message m_labels[k_numberOfCells];
+};
+
+}
+
+#endif /* MENU_CONTROLLER_H */

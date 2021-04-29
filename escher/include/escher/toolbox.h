eed4a932373003225bee16b9f1ebc1c768381e09
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-04-29 12:14:21 +0200

    [escher/toolbox] Improve while loop

--- escher/include/escher/toolbox.h
+++ escher/include/escher/toolbox.h
@@ -34,10 +34,10 @@ protected:
   MessageTableCellWithChevron * nodeCellAtIndex(int index) override = 0;
   I18n::Message subTitle() override { return m_messageTreeModel->label(); }
   virtual const ToolboxMessageTree * messageTreeModelAtIndex(int index) const;
-  mutable const ToolboxMessageTree * m_messageTreeModel;
   /* m_messageTreeModel points at the messageTree of the tree (describing the
-   * whole model) where we are located. It enables to know which rows are leaves
-   * and which are subtrees. */
+   * whole model) where we are located. We use it to know which rows are leaves
+   * and which rows are subtrees. */
+  mutable const ToolboxMessageTree * m_messageTreeModel;
 };
 
 }

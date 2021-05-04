62413d220e2111e0f9e227b642d4a218318dac11
Émilie Feral <emilie.feral@numworks.com>
2021-05-04 16:25:45 +0200

    [apps/third_party_app] Clean classes

--- apps/third_party_app/rocket.h
+++ apps/third_party_app/rocket.h
@@ -12,13 +12,13 @@ public:
   int x() const { return m_x; }
   int y() const { return m_y; }
   void setLocation(int x, int y);
-  void draw(const Color c) const;
   void forward();
   void tryToKill(Alien * a);
   bool off() const { return m_y < 0; }
   static constexpr int k_period = 10;
 private:
   static constexpr int k_length = 10;
+  void draw(const Color c) const;
   void switchOff() { m_y = -1; }
   int m_x;
   int m_y;

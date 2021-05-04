c591a5c29fd62f4cf427983a2900dc7c543e37dd
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-05-04 15:20:41 +0200

    Add a class to create a list of same-typed cells
    Change-Id: I05063b14d2587cc43e04bab118e970e2b4b737fa

--- apps/probability/categorical_type_controller.h
+++ apps/probability/categorical_type_controller.h
@@ -1,17 +1,19 @@
 #ifndef CATEGORICAL_TYPE_CONTROLLER_H
 #define CATEGORICAL_TYPE_CONTROLLER_H
 
-#include <escher/selectable_list_view_controller.h>
-#include <escher/responder.h>
+#include <escher/container.h>
 #include <escher/highlight_cell.h>
 #include <escher/message_table_cell_with_chevron.h>
-#include <escher/container.h>
+#include <escher/responder.h>
 
-class CategoricalTypeController : public Escher::SelectableListViewController {
-public:
+#include "gui/selectable_cell_list_controller.h"
+
+constexpr static int k_numberOfCategoricalCells = 2;
+
+class CategoricalTypeController
+    : public SelectableCellListController<Escher::MessageTableCellWithChevron, k_numberOfCategoricalCells> {
+ public:
   CategoricalTypeController(Escher::Responder * parent);
-  Escher::HighlightCell * reusableCell(int i, int type) override;
-  int numberOfRows() const override {return k_numberOfCells; }
   void didBecomeFirstResponder() override {
     // TODO factor out
     if (selectedRow() == -1) {
@@ -22,12 +24,9 @@ public:
     Escher::Container::activeApp()->setFirstResponder(&m_selectableTableView);
   }
 
-private:
-  constexpr static int k_numberOfCells = 2;
-  constexpr static int k_indexOfGoodness    = 0;
+ private:
+  constexpr static int k_indexOfGoodness = 0;
   constexpr static int k_indexOfHomogeneity = 1;
-  Escher::MessageTableCellWithChevron m_cells[k_numberOfCells];
-
 };
 
 #endif /* CATEGORICAL_TYPE_CONTROLLER_H */
--- apps/probability/test_controller.h
+++ apps/probability/test_controller.h
@@ -3,31 +3,29 @@
 
 #include <escher/message_table_cell_with_chevron_and_message.h>
 #include <escher/responder.h>
-#include <escher/selectable_list_view_controller.h>
 #include <ion/events.h>
 
+#include "gui/selectable_cell_list_controller.h"
 #include "categorical_type_controller.h"
 #include "hypothesis_controller.h"
 #include "type_controller.h"
 
-class TestController : public Escher::SelectableListViewController {
+constexpr static int k_numberOfTestCells = 5;
+
+class TestController : public SelectableCellListController<Escher::MessageTableCellWithChevronAndMessage, k_numberOfTestCells> {
  public:
   TestController(Escher::Responder * parentResponder, HypothesisController * hypothesisController,
                  TypeController * typeController, CategoricalTypeController * categoricalController);
-  int numberOfRows() const override { return k_numberOfCells; }
-  Escher::HighlightCell * reusableCell(int index, int type) override;
   void didBecomeFirstResponder() override;
   bool handleEvent(Ion::Events::Event e) override;
   const char * title() override { return "Test"; }
 
  private:
-  constexpr static int k_numberOfCells = 5;
   constexpr static int k_indexOfOneProp = 0;
   constexpr static int k_indexOfOneMean = 1;
   constexpr static int k_indexOfTwoProps = 2;
   constexpr static int k_indexOfTwoMeans = 3;
   constexpr static int k_indexOfCategorical = 4;
-  Escher::MessageTableCellWithChevronAndMessage m_cells[k_numberOfCells];
 
   HypothesisController * m_hypothesisController;
   CategoricalTypeController * m_categoricalController;

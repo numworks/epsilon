68c8190b1bd881b3d9d62fac8e1b0c3989f49426
Émilie Feral <emilie.feral@numworks.com>
2021-05-05 18:06:24 +0200

    [ion][apps] ExamMode in userland with external apps: reset the device

--- ion/src/device/userland/drivers/reset.cpp
+++ ion/src/device/userland/drivers/reset.cpp
@@ -1,4 +1,15 @@
 #include "reset.h"
+#include <userland/drivers/svcall.h>
+
+namespace Ion {
+namespace Reset {
+
+void SVC_ATTRIBUTES core() {
+  SVC_RETURNING_VOID(SVC_RESET_CORE)
+}
+
+}
+}
 
 namespace Ion {
 namespace Device {

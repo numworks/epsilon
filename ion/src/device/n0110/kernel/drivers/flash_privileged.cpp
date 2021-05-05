cd3afac4a1cae5f7dfdb51265cb12f70da0049a5
Émilie Feral <emilie.feral@numworks.com>
2021-05-05 17:05:56 +0200

    [ion] Fix Flash::ForbiddenSector to exclude PersistingBytes

--- ion/src/device/n0110/kernel/drivers/flash_privileged.cpp
+++ ion/src/device/n0110/kernel/drivers/flash_privileged.cpp
@@ -9,20 +9,30 @@ namespace Device {
 namespace Flash {
 
 bool ForbiddenSector(int i) {
+  int firstSectorSlotA = 0;
+  int firstSectorSlotAAfterPersistingBytes = ExternalFlash::Config::NumberOf4KSectors + ExternalFlash::Config::NumberOf32KSectors;
+  int firstSectorSlotB = SectorAtAddress(ExternalFlash::Config::StartAddress + ExternalFlash::Config::TotalSize/2);
+  int firstSectorSlotBAfterPersistingBytes = firstSectorSlotB + 1;
   if (Authentication::trustedUserland()) {
-    /* Authenticated userland can write the external apps section of both slots
-     * and the whole other slot. */
+    /* Authenticated userland can write:
+     * - the persisting bytes of the current slot
+     * - the external apps section of both slots
+     * - the whole other slot except its persisting bytes. */
     if (Board::isRunningSlotA()) {
-      return i < ExternalFlash::Config::NumberOfSectors/2 - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors;
+      return !((i >= firstSectorSlotA && i < firstSectorSlotAAfterPersistingBytes) ||
+          (i >= firstSectorSlotB - Board::Config::ExternalAppsNumberOfSector && i < ExternalFlash::Config::NumberOfSectors));
     }
-    return i < 0 || (i >= ExternalFlash::Config::NumberOfSectors/2 && i < ExternalFlash::Config::NumberOfSectors - Board::Config::ExternalAppsNumberOfSector) || i >= ExternalFlash::Config::NumberOfSectors;
+    return !(i >= firstSectorSlotAAfterPersistingBytes && i < ExternalFlash::Config::NumberOfSectors);
   }
-  /* Non-authenticated userland can only write the external apps section of the
-   * running slot. */
+  /* Non-authenticated userland can write:
+   * - the external apps section of the running slot,
+   * - the persisting bytes section of the running slot. */
   if (Board::isRunningSlotA()) {
-    return i < ExternalFlash::Config::NumberOfSectors/2 - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors/2;
+    return !((i >= firstSectorSlotA && i < firstSectorSlotAAfterPersistingBytes) ||
+        (i >= firstSectorSlotB - Board::Config::ExternalAppsNumberOfSector && i < firstSectorSlotB));
   }
-  return i < ExternalFlash::Config::NumberOfSectors - Board::Config::ExternalAppsNumberOfSector || i >= ExternalFlash::Config::NumberOfSectors;
+  return !((i >= firstSectorSlotB && i < firstSectorSlotBAfterPersistingBytes) ||
+    (i >= ExternalFlash::Config::NumberOfSectors - Board::Config::ExternalAppsNumberOfSector && i < ExternalFlash::Config::NumberOfSectors));
 }
 
 

5a4578a512ee29fa94e333347f911dc30e71b3f8
Émilie Feral <emilie.feral@numworks.com>
2021-05-06 13:54:32 +0200

    [ion] PCB char table is in userland data instead of kernel data (which is forbidden in non-privileged mode)

--- ion/src/device/userland/drivers/pcb_version.cpp
+++ ion/src/device/userland/drivers/pcb_version.cpp
@@ -3,8 +3,27 @@
 
 namespace Ion {
 
-const char * SVC_ATTRIBUTES pcbVersion() {
-  SVC_RETURNING_R0(SVC_PCB_VERSION, const char *)
+uint32_t SVC_ATTRIBUTES devicePcbVersion() {
+  SVC_RETURNING_R0(SVC_PCB_VERSION, uint32_t)
+}
+
+const char * pcbVersion() {
+  constexpr int pcbVersionLength = 5; // xx.yy
+  static char pcbVer[pcbVersionLength] = {'\0'};
+  if (pcbVer[0] == '\0') {
+    uint32_t ver = devicePcbVersion();
+    /* As PCB version only uses 4 chars, value should be at most 9999. */
+    assert(ver < 10000);
+    for (int i = pcbVersionLength - 1; i >= 0; i--) {
+      if (i == 2) {
+        pcbVer[i] = '.';
+      } else {
+        pcbVer[i] = '0' + ver % 10;
+        ver /= 10;
+      }
+    }
+  }
+  return pcbVer;
 }
 
 }

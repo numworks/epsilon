fe0ff6b3fd76647af5c04da0a167f4e340655181
Émilie Feral <emilie.feral@numworks.com>
2021-05-07 11:44:24 +0200

    [apps][ion] Fix python bug: the roots were collected within the kernel stack which crashes

--- ion/src/shared/stack_position.cpp
+++ ion/src/shared/stack_position.cpp
@@ -2,13 +2,8 @@
 
 namespace Ion {
 
-#if PLATFORM_DEVICE
-// On device, stack start address is always known. TODO : Factorize address
-static void * s_stackStart = reinterpret_cast<void *>(0x20000000 + 256*1024);
-#else
 // Stack start will be defined in ion_main.
 static void * s_stackStart = nullptr;
-#endif
 
 void * stackStart() {
   assert(s_stackStart != nullptr);

9ef1d59d831b44fcbbf7ade3bf6362493fbc9c0f
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-05-11 15:34:56 +0200

    Extract View from InputGoodnessController's implementation
    Change-Id: Iffab825adc684ab1941e69323385d5f1d1a97cbb

--- apps/probability/controllers/input_goodness_controller.cpp
+++ apps/probability/controllers/input_goodness_controller.cpp
@@ -12,82 +12,6 @@
 
 using namespace Probability;
 
-InputGoodnessView::InputGoodnessView(Responder * parentResponder, TableViewDataSource * dataSource,
-                                     InputEventHandlerDelegate * inputEventHandlerDelegate,
-                                     TextFieldDelegate * textFieldDelegate)
-    : VerticalLayout(),
-      Responder(parentResponder),
-      m_tableView(this, dataSource, &m_tableSelection),
-      m_significance(this, inputEventHandlerDelegate, textFieldDelegate),
-      m_next(this, I18n::Message::Ok, buttonActionInvocation()) {
-  // TODO
-  // m_significance.setMessage(I18n::Message::A);
-  // m_significance.setSubLabelMessage(I18n::Message::SignificanceLevel);
-  // m_significance.setAccessoryText("0.05");
-  setView(&m_tableView, 0);
-  setView(&m_significance, 1);
-  setView(&m_next, 2);
-}
-
-void InputGoodnessView::didBecomeFirstResponder() {
-  // Pass focus to subview
-  if (m_viewSelection.selectedRow() < 0) {
-    m_viewSelection.selectRow(0);
-  }
-  setResponderForSelectedRow();
-  highlightViewForSelectedRow();
-}
-
-bool InputGoodnessView::handleEvent(Ion::Events::Event event) {
-  if (event == Ion::Events::Up || event == Ion::Events::Down) {
-    if (event == Ion::Events::Up && m_viewSelection.selectedRow() > 0) {
-      m_viewSelection.selectRow(m_viewSelection.selectedRow() - 1);
-    }
-    if (event == Ion::Events::Down && m_viewSelection.selectedRow() < k_numberOfChildren - 1) {
-      m_viewSelection.selectRow(m_viewSelection.selectedRow() + 1);
-    }
-    setResponderForSelectedRow();
-    highlightViewForSelectedRow();
-    return true;
-  }
-  return false;
-}
-
-Responder * InputGoodnessView::responderForRow(int row) {
-  switch (m_viewSelection.selectedRow()) {
-    case k_indexOfTable:
-      return &m_tableView;
-    case k_indexOfSignificance:
-      return &m_significance;
-    case k_indexOfNext:
-      return &m_next;
-  }
-  assert(false);
-}
-
-void InputGoodnessView::setResponderForSelectedRow() {
-  Escher::Container::activeApp()->setFirstResponder(responderForRow(m_viewSelection.selectedRow()));
-}
-
-void Probability::InputGoodnessView::highlightViewForSelectedRow() {
-  // TODO set behavior in didBecomeFirstResponder?
-  m_significance.setHighlighted(false);
-  m_next.setHighlighted(false);
-  switch (m_viewSelection.selectedRow()) {
-    case k_indexOfTable:
-      if (m_tableView.selectedRow() < 0) {
-        m_tableView.selectCellAtLocation(0, 0);  // TODO or last ?
-      }
-      break;
-    case k_indexOfSignificance:
-      m_significance.setHighlighted(true);
-      break;
-    case k_indexOfNext:
-      m_next.setHighlighted(true);
-      break;
-  }
-}
-
 InputGoodnessDataSource::InputGoodnessDataSource(Responder * parent, SelectableTableView * tableView,
                                                  InputEventHandlerDelegate * inputEventHandlerDelegate,
                                                  TextFieldDelegate * delegate) {
@@ -115,8 +39,9 @@ InputGoodnessController::InputGoodnessController(StackViewController * parent,
                                                  InputEventHandlerDelegate * inputEventHandlerDelegate,
                                                  TextFieldDelegate * textFieldDelegate)
     : Page(parent),
-      m_data(parent, m_contentView.selectableTableView(), inputEventHandlerDelegate, textFieldDelegate),
-      m_contentView(this, &m_data, inputEventHandlerDelegate, textFieldDelegate) {}
+      m_data(parent, &m_table, inputEventHandlerDelegate, textFieldDelegate),
+      m_contentView(this, &m_table, inputEventHandlerDelegate, textFieldDelegate),
+      m_table(&m_contentView, &m_data, m_contentView.selectionDataSource()) {}
 
 void InputGoodnessController::didBecomeFirstResponder() {
   Escher::Container::activeApp()->setFirstResponder(&m_contentView);
---
+++ apps/probability/gui/input_table_view.h
@@ -0,0 +1,54 @@
+#ifndef TABLE_INPUT_CONTROLLER_H
+#define TABLE_INPUT_CONTROLLER_H
+
+#include <apps/shared/button_with_separator.h>
+#include <escher/input_event_handler_delegate.h>
+#include <escher/message_table_cell_with_editable_text_with_message.h>
+#include <escher/responder.h>
+#include <escher/selectable_table_view.h>
+#include <escher/table_view_data_source.h>
+#include <escher/text_field_delegate.h>
+#include <ion/events.h>
+
+#include "../abstract/button_delegate.h"
+#include "vertical_layout.h"
+
+namespace Probability {
+
+/* This view contains an EditableCell, a Button and a pointer to a TableView,
+ * layed out vertically, and is able to move selection between them.
+ */
+class InputTableView : public VerticalLayout<3>, public ButtonDelegate, public Responder {
+ public:
+  InputTableView(Responder * parentResponder, SelectableTableView * table,
+                 InputEventHandlerDelegate * inputEventHandlerDelegate, TextFieldDelegate * textFieldDelegate);
+
+  MessageTableCellWithEditableTextWithMessage * significanceLevelView() { return &m_significance; }
+  Shared::ButtonWithSeparator * nextButton() { return &m_next; }
+  // Responder
+  void didBecomeFirstResponder() override;
+  bool handleEvent(Ion::Events::Event event) override;
+
+  SelectableTableViewDataSource * selectionDataSource() { return &m_tableSelection; }
+
+ private:
+  void buttonAction() override {}
+  Responder * responderForRow(int row);
+  void setResponderForSelectedRow();
+  void highlightViewForSelectedRow();
+
+  constexpr static int k_indexOfTable = 0;
+  constexpr static int k_indexOfSignificance = 1;
+  constexpr static int k_indexOfNext = 2;
+
+  SelectableTableView * m_tableView;
+  MessageTableCellWithEditableTextWithMessage m_significance;
+  Shared::ButtonWithSeparator m_next;
+
+  SelectableTableViewDataSource m_tableSelection;
+  SelectableTableViewDataSource m_viewSelection;
+};
+
+}  // namespace Probability
+
+#endif /* TABLE_INPUT_CONTROLLER_H */

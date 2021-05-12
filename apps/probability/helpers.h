797199e52e764fe2861376164fa2549e0abcb916
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-05-12 17:25:13 +0200

    Helpers
    Change-Id: Ibb0c6cf8c4639a2bade18e02891138eb09636b2b

---
+++ apps/probability/helpers.h
@@ -0,0 +1,11 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <apps/i18n.h>
+#include <apps/shared/text_field_delegate_app.h>
+#include <assert.h>
+#include <escher/container.h>
+
+Shared::TextFieldDelegateApp * getProbabilityApp();
+
+#endif /* HELPER_H */

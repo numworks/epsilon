d39e3c7d6bc08bb76f09b4a0adfbd0556d19914c
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-05-25 14:07:37 +0200

    [hot reload] Factoring and cleaning up
    Change-Id: I2e8ea0bf12a29a0772375661a1c5bde209a0d52b

---
+++ ion/src/simulator/shared/unix/platform_files.cpp
@@ -0,0 +1,17 @@
+#include "../platform.h"
+
+namespace Ion {
+namespace Simulator {
+namespace Platform {
+
+const char * filePathInTempDir(const char * filename) {
+  static char path[64];
+  if (snprintf(path, sizeof(path), "/tmp/%s", filename) < 0) {
+    return nullptr;
+  }
+  return path;
+}
+
+}  // namespace Platform
+}  // namespace Simulator
+}  // namespace Ion

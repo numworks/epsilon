5d9a8c837509eb8dbe676b96bbed4daf6f6e76bd
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-05-25 16:16:58 +0200

    [apps/shared] Prevent double imprecisions in interval tables

---
+++ apps/shared/poincare_helpers.cpp
@@ -0,0 +1,25 @@
+#include "poincare_helpers.h"
+
+namespace Shared {
+
+namespace PoincareHelpers {
+
+template <class T>
+T GetDisplayedFloat(T t, int precision, Poincare::Context * context) {
+  assert(precision <= Poincare::PrintFloat::k_numberOfStoredSignificantDigits);
+  constexpr size_t bufferSize = Poincare::PrintFloat::charSizeForFloatsWithPrecision(Poincare::PrintFloat::k_numberOfStoredSignificantDigits);
+  char buffer[bufferSize];
+  // Get displayed value
+  int numberOfChar = ConvertFloatToText<T>(t, buffer, bufferSize, precision);
+  assert(numberOfChar <= bufferSize);
+  // Silence compiler warnings for assert
+  (void) numberOfChar;
+  // Extract displayed value
+  return ApproximateToScalar<T>(buffer, context);
+}
+
+template float GetDisplayedFloat<float>(float t, int precision, Poincare::Context * context);
+template double GetDisplayedFloat<double>(double t, int precision, Poincare::Context * context);
+}
+
+}

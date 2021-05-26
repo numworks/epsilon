e349b05c970a935e4c51c8c670be4c6aade58e18
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-05-26 16:24:33 +0200

    Reopen app with correct colors
    Change-Id: If7929b6a9b88dad2595ee8d2e2573e3349dc737e

--- apps/probability/app.cpp
+++ apps/probability/app.cpp
@@ -32,48 +32,71 @@ App::App(Snapshot * snapshot)
                        &m_categoricalTypeController),
       m_menuController(&m_stackViewController, &m_distributionController, &m_testController),
       m_stackViewController(&m_modalViewController, &m_menuController) {
-  switch (snapshot->navigation()->page()) {
+  // Reopen correct page
+  // TODO delegate decisions to controllers somehow
+  // TODO fix branching
+  Data::Page page = snapshot->navigation()->page();
+  Data::SubApp subapp = snapshot->navigation()->subapp();
+  Data::Test test = snapshot->data()->test();
+
+  switch (page) {
     case Data::Page::Menu:
       break;
     case Data::Page::Distribution:
-      m_stackViewController.push(&m_distributionController);
+      m_menuController.openPage(&m_distributionController);
       break;
     case Data::Page::Test:
-      m_stackViewController.push(&m_testController);
+      m_menuController.openPage(&m_testController);
       break;
     case Data::Page::Type:
-      m_stackViewController.push(&m_typeController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_typeController);
       break;
     case Data::Page::Hypothesis:
-      m_stackViewController.push(&m_hypothesisController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_hypothesisController);
       break;
     case Data::Page::Categorical:
-      m_stackViewController.push(&m_categoricalTypeController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_categoricalTypeController);
       break;
     case Data::Page::Input:
-      m_stackViewController.push(&m_hypothesisController);
-      m_stackViewController.push(&m_inputController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_hypothesisController);
+      m_hypothesisController.openPage(&m_inputController);
       break;
     case Data::Page::InputGoodness:
-      m_stackViewController.push(&m_inputGoodnessController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_categoricalTypeController);
+      m_categoricalTypeController.openPage(&m_inputGoodnessController);
       break;
     case Data::Page::InputHomogeneity:
-      m_stackViewController.push(&m_inputHomogeneityController);
-      break;
-    case Data::Page::Results:
-      m_stackViewController.push(&m_hypothesisController);
-      m_stackViewController.push(&m_resultsController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_categoricalTypeController);
+      m_categoricalTypeController.openPage(&m_inputHomogeneityController);
       break;
     case Data::Page::ResultsHomogeneity:
-      m_stackViewController.push(&m_inputHomogeneityController);
-      m_stackViewController.push(&m_homogeneityResultsController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_categoricalTypeController);
+      m_categoricalTypeController.openPage(&m_inputHomogeneityController);
+      m_inputHomogeneityController.openPage(&m_homogeneityResultsController);
+      break;
+    case Data::Page::Results:
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_hypothesisController);
+      m_hypothesisController.openPage(&m_inputController);
+      m_inputController.openPage(&m_resultsController);
       break;
     case Data::Page::ProbaGraph:
+      m_menuController.openPage(&m_distributionController);
       m_stackViewController.push(&m_calculationController);
       break;
     case Data::Page::Graph:
-      m_stackViewController.push(&m_inputController);
-      m_stackViewController.push(&m_graphController);
+      m_menuController.openPage(&m_testController);
+      m_testController.openPage(&m_hypothesisController);
+      m_hypothesisController.openPage(&m_inputController);
+      m_inputController.openPage(&m_resultsController);
+      m_resultsController.openPage(&m_graphController);
       break;
   }
 }

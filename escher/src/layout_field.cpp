cbd91cc68a41ed9ad36f8f8d24375c896fa0e9ec
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-05-28 17:29:35 +0200

    [escher/layout_field] Factor removing display types

--- escher/src/layout_field.cpp
+++ escher/src/layout_field.cpp
@@ -431,8 +431,7 @@ bool LayoutField::handleEventWithText(const char * text, bool indentation, bool 
       return true;
     }
     // The text is parsable, we create its layout an insert it.
-    Layout resultLayout = resultExpression.createLayout(Poincare::Preferences::sharedPreferences()->displayMode(), Poincare::PrintFloat::k_numberOfStoredSignificantDigits);
-    CodePointLayout::StripDisplayTypeFromCodePoints(resultLayout);
+    Layout resultLayout = resultExpression.createLayout(Poincare::Preferences::sharedPreferences()->displayMode(), Poincare::PrintFloat::k_numberOfStoredSignificantDigits, true);
     if (currentNumberOfLayouts + resultLayout.numberOfDescendants(true) >= k_maxNumberOfLayouts) {
       return true;
     }

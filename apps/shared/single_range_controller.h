ca55c35ac62e9731caf4258a5235269fe95842dd
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-06-01 12:12:01 +0200

    [shared/single_range_controller] Derive from FloatParameterController
    This adds a "Validate" button to the menu.

--- apps/shared/single_range_controller.h
+++ apps/shared/single_range_controller.h
@@ -1,19 +1,23 @@
 #ifndef SHARED_SINGLE_RANGE_CONTROLLER_H
 #define SHARED_SINGLE_RANGE_CONTROLLER_H
 
-#include "simple_float_parameter_controller.h"
+#include "float_parameter_controller.h"
 #include "interactive_curve_view_range.h"
 #include <escher/message_table_cell_with_editable_text.h>
 #include <escher/message_table_cell_with_switch.h>
 
 namespace Shared {
 
-class SingleRangeController : public SimpleFloatParameterController<float> {
+class SingleRangeController : public FloatParameterController<float> {
 public:
   SingleRangeController(Escher::Responder * parentResponder, Escher::InputEventHandlerDelegate * inputEventHandlerDelegate, InteractiveCurveViewRange * interactiveCurveViewRange);
 
   const char * title() override { return I18n::translate(m_editXRange ? I18n::Message::ValuesOfX : I18n::Message::ValuesOfY); }
-  int numberOfRows() const override { return k_numberOfTextCells + 1; }
+  int numberOfRows() const override { return k_numberOfTextCells + 2; }
+  int typeAtIndex(int index) override { return index == 0 ? k_autoCellType : FloatParameterController<float>::typeAtIndex(index); }
+  int reusableCellCount(int type) override { return type == k_autoCellType ? 1 : FloatParameterController<float>::reusableCellCount(type); }
+  Escher::HighlightCell * reusableCell(int index, int type) override;
+  KDCoordinate nonMemoizedRowHeight(int j) override;
   void willDisplayCellForIndex(Escher::HighlightCell * cell, int index) override;
   bool handleEvent(Ion::Events::Event event) override;
   bool editXRange() const { return m_editXRange; }
@@ -21,6 +25,8 @@ public:
 
 private:
   constexpr static int k_numberOfTextCells = 2;
+  constexpr static int k_autoCellType = 2;
+  static_assert(k_autoCellType != SimpleFloatParameterController<float>::k_parameterCellType && k_autoCellType != FloatParameterController<float>::k_buttonCellType, "k_autoCellType value already taken.");
 
   class LockableEditableCell : public Escher::MessageTableCellWithEditableText {
   public:
@@ -32,7 +38,7 @@ private:
 
   bool autoStatus() const { return m_editXRange ? m_range->xAuto() : m_range->yAuto(); }
   float parameterAtIndex(int index) override;
-  int reusableParameterCellCount(int type) override { return k_numberOfTextCells + 1; }
+  int reusableParameterCellCount(int type) override { return k_numberOfTextCells; }
   Escher::HighlightCell * reusableParameterCell(int index, int type) override;
   bool setParameterAtIndex(int parameterIndex, float f) override;
 

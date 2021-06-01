33b35981c83b7023ec63b780c4df96a3c0fbbc4e
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-01 14:01:41 +0200

    Better App recreation from snapshot
    Change-Id: I5e04922417f4bd12d800dcbb0f97460ed0a6ef8d

---
+++ apps/probability/models/data.cpp
@@ -0,0 +1,9 @@
+#include "data.h"
+
+namespace Probability {
+namespace Data {
+
+bool isProportion(Test t) { return t == Test::OneProp || t == Test::TwoProps; }
+
+}  // namespace Data
+}  // namespace Probability

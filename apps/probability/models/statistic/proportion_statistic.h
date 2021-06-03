197dddf035c2efcfe7c96f22d5117dc020a1de26
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-03 15:21:57 +0200

    Factor more for ProportionStatistics
    Change-Id: I2660de41b2aed186204faacc91115edcafe9f860

--- apps/probability/models/statistic/proportion_statistic.h
+++ apps/probability/models/statistic/proportion_statistic.h
@@ -3,18 +3,20 @@
 
 #include "statistic.h"
 
-namespace Probability
-{
+namespace Probability {
 
 class ProportionStatistic : public CachedStatistic {
+public:
+  const char * testCriticalValueSymbol() override { return "z"; };
+  const char * intervalCriticalValueSymbol() override { return "z*"; };
+  bool hasDegreeOfFreedom() override { return false; };
+
 protected:
   float _zCritical(float confidenceLevel);
   float _pVal(float z, char op);
   float _ME(float zCritical, float SE);
 };
 
-
-} // namespace Probability
-
+}  // namespace Probability
 
 #endif /* APPS_PROBABILITY_MODELS_STATISTIC_PROPORTION_STATISTIC_H */

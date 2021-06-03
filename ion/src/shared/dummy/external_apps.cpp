b78d4732d68acdbcb0b06614f5396400ee525da2
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-06-03 15:42:22 +0200

    [ion/simulator] Add dummy implementation for new Ion API

---
+++ ion/src/shared/dummy/external_apps.cpp
@@ -0,0 +1,39 @@
+#include <ion/external_apps.h>
+
+namespace Ion {
+namespace ExternalApps {
+
+App::App(uint8_t *) {}
+
+const char * App::name() const {
+  return "";
+}
+
+uint32_t App::iconSize() const {
+  return 0;
+}
+
+const uint8_t * App::iconData() const {
+  return nullptr;
+}
+
+void * App::entryPoint() const {
+  return nullptr;
+}
+
+AppIterator& AppIterator::operator++() {
+  return *this;
+}
+
+AppIterator Apps::begin() const {
+  return AppIterator(nullptr);
+}
+
+void setVisible(bool) {}
+
+int numberOfApps() {
+  return 0;
+}
+
+}
+}
---
+++ ion/src/shared/dummy/post_and_hardware_tests.cpp
@@ -0,0 +1,31 @@
+#include <ion/post_and_hardware_tests.h>
+
+namespace Ion {
+namespace POSTAndHardwareTests {
+
+bool BatteryOK() {
+  return true;
+}
+
+bool VBlankOK() {
+  return true;
+}
+
+int LCDDataGlyphFailures() {
+  return 0;
+}
+
+int LCDTimingGlyphFailures() {
+  return 0;
+}
+
+int ColorsLCDPixelFailures() {
+  return 0;
+}
+
+bool TextLCDGlyphFailures() {
+  return false;
+}
+
+}
+}

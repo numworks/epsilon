5e1400c0e4dbd71723fb8d0524324c041c9970c2
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-04 17:54:52 +0200

    Fix alignments
    Change-Id: Ie7158170964ec396e1cfb0dbfc014aa190c605ed

--- kandinsky/include/kandinsky/context.h
+++ kandinsky/include/kandinsky/context.h
@@ -2,8 +2,8 @@
 #define KANDINSKY_CONTEXT_H
 
 #include <kandinsky/color.h>
-#include <kandinsky/rect.h>
 #include <kandinsky/font.h>
+#include <kandinsky/rect.h>
 
 class KDContext {
 public:
@@ -16,10 +16,18 @@ public:
   void getPixels(KDRect r, KDColor * pixels);
 
   // Text
-  KDPoint drawString(const char * text, KDPoint p, const KDFont * font = KDFont::LargeFont, KDColor textColor = KDColorBlack, KDColor backgroundColor = KDColorWhite, int maxLength = -1);
-  KDPoint alignAndDrawString(const char * text, KDPoint p, KDSize frame, float horizontalAlignment = 0.5f, float verticalAlignment = 0.5f, const KDFont * font = KDFont::LargeFont, KDColor textColor = KDColorBlack, KDColor backgroundColor = KDColorWhite, int maxLength = -1);
+  KDPoint drawString(const char * text, KDPoint p, const KDFont * font = KDFont::LargeFont,
+                     KDColor textColor = KDColorBlack, KDColor backgroundColor = KDColorWhite,
+                     int maxLength = -1);
+  KDPoint alignAndDrawString(const char * text, KDPoint p, KDSize frame,
+                             float horizontalAlignment = 0.5f, float verticalAlignment = 0.5f,
+                             const KDFont * font = KDFont::LargeFont,
+                             KDColor textColor = KDColorBlack,
+                             KDColor backgroundColor = KDColorWhite, int maxLength = -1);
   // Check that a string is drawn.
-  int checkDrawnString(const char * text, KDPoint p, const KDFont * font = KDFont::LargeFont, KDColor textColor = KDColorBlack, KDColor backgroundColor = KDColorWhite, int maxLength = -1);
+  int checkDrawnString(const char * text, KDPoint p, const KDFont * font = KDFont::LargeFont,
+                       KDColor textColor = KDColorBlack, KDColor backgroundColor = KDColorWhite,
+                       int maxLength = -1);
 
   // Line. Not anti-aliased.
   void drawLine(KDPoint p1, KDPoint p2, KDColor c);
@@ -29,17 +37,21 @@ public:
   void fillRectWithPixels(KDRect rect, const KDColor * pixels, KDColor * workingBuffer);
   void blendRectWithMask(KDRect rect, KDColor color, const uint8_t * mask, KDColor * workingBuffer);
   void strokeRect(KDRect rect, KDColor color);
+
 protected:
-  KDContext(KDPoint origin, KDRect clippingRect) :
-    m_origin(origin),
-    m_clippingRect(clippingRect)
-  {}
+  KDContext(KDPoint origin, KDRect clippingRect) : m_origin(origin), m_clippingRect(clippingRect) {}
   virtual void pushRect(KDRect, const KDColor * pixels) = 0;
   virtual void pushRectUniform(KDRect rect, KDColor color) = 0;
   virtual void pullRect(KDRect rect, KDColor * pixels) = 0;
+
 private:
+  KDPoint alignAndDrawSingleLineString(const char * text, KDPoint p, KDSize frame,
+                                       float horizontalAlignment, const KDFont * font,
+                                       KDColor textColor, KDColor backgroundColor, int maxLength);
   KDRect absoluteFillRect(KDRect rect);
-  KDPoint pushOrPullString(const char * text, KDPoint p, const KDFont * font, KDColor textColor, KDColor backgroundColor, int maxByteLength, bool push, int * result = nullptr);
+  KDPoint pushOrPullString(const char * text, KDPoint p, const KDFont * font, KDColor textColor,
+                           KDColor backgroundColor, int maxByteLength, bool push,
+                           int * result = nullptr);
   KDPoint m_origin;
   KDRect m_clippingRect;
 };

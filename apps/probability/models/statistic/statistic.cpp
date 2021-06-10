764f4ede22afff4aadad86751fae04a71807a182
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-10 10:18:03 +0200

    Fix editing Alpha
    Change-Id: I4b6398a4b5871138e80fe047b6df3ca8727c62e2

--- apps/probability/models/statistic/statistic.cpp
+++ apps/probability/models/statistic/statistic.cpp
@@ -8,6 +8,15 @@ float Statistic::paramAtIndex(int i) {
   return i == indexOfThreshold() ? m_threshold : paramArray()[i];
 }
 
+void Statistic::setParamAtIndex(int i, float p) {
+  if (i == indexOfThreshold()) {
+    m_threshold = p;
+  } else {
+    assert(i < indexOfThreshold());
+    paramArray()[i] = p;
+  }
+}
+
 bool Statistic::testPassed() {
    return pValue() > threshold();
 }

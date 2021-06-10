d2ef03ca94d5e3757b55231ce9e1ea972f6ba057
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-10 10:21:57 +0200

    Compute StatisticViewRange::yMax
    Change-Id: Ib02e5cb3afa93871b85e269394414ad0f3857110

--- apps/probability/models/statistic_view_range.h
+++ apps/probability/models/statistic_view_range.h
@@ -19,7 +19,7 @@ public:
   StatisticViewRange(bool isLeftRange) : m_isLeftRange(isLeftRange) {}
   void setStatistic(Statistic * statistic) { m_statistic = statistic; }
   float yMin() const override { return k_yMin; }
-  float yMax() const override { return k_yMax; }
+  float yMax() const override;
   float xMin() const override;
   float xMax() const override;
   void setMode(GraphDisplayMode m) { m_mode = m; }
@@ -33,7 +33,8 @@ private:
   constexpr static float k_yMin = -0.1;
   constexpr static float k_yMax = 0.4;
   constexpr static int k_marginLeftOfMin = 30;
-  constexpr static int k_areaSize = 50;
+  constexpr static int k_areaWidth = 50;
+  constexpr static int k_areaHeight = 65;
   Statistic * m_statistic;
   bool m_isLeftRange;
   GraphDisplayMode m_mode;

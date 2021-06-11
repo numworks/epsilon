45fb163d9a4682dd87a5864253746a362e9a4cc5
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-06-11 09:57:31 +0200

    [poincare] Arithmetic and Polynomial review improvements

--- poincare/include/poincare/polynomial.h
+++ poincare/include/poincare/polynomial.h
@@ -13,13 +13,13 @@ public:
   static int CubicPolynomialRoots(Expression a, Expression b, Expression c, Expression d, Expression * root1, Expression * root2, Expression * root3, Expression * delta, Context * context, Preferences::ComplexFormat complexFormat, Preferences::AngleUnit angleUnit, bool * approximateSolutions = nullptr);
 
 private:
+  static constexpr int k_maxNumberOfNodesBeforeApproximatingDelta = 50;
   static Expression ReducePolynomial(const Expression * coefficients, int degree, Expression parameter, ExpressionNode::ReductionContext reductionContext);
   static Rational ReduceRationalPolynomial(const Rational * coefficients, int degree, Rational parameter);
   static bool IsRoot(const Expression * coefficients, int degree, Expression root, ExpressionNode::ReductionContext reductionContext) { return ReducePolynomial(coefficients, degree, root, reductionContext).nullStatus(reductionContext.context()) == ExpressionNode::NullStatus::Null; }
   static Expression RationalRootSearch(const Expression * coefficients, int degree, ExpressionNode::ReductionContext reductionContext);
   static Expression SumRootSearch(const Expression * coefficients, int degree, int relevantCoefficient, ExpressionNode::ReductionContext reductionContext);
   static Expression CardanoNumber(Expression delta0, Expression delta1, bool * approximate, ExpressionNode::ReductionContext reductionContext);
-  static constexpr int k_maxNumberOfNodesBeforeApproximatingDelta = 50;
 };
 
 }

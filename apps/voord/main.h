fc349a7eb0a6b00eb195f89d9ff0ca3b0e9550ca
Émilie Feral <emilie.feral@numworks.com>
2021-06-14 10:31:52 +0200

    [apps/voord] Remove external apps from sources -> voord has it own repo

--- apps/voord/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void app_main();
-
-#endif
--- apps/voord/sdk/stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef STDINT_H
-#define STDINT_H
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-
-#endif

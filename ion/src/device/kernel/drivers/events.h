933547ec5a4022beabf7bc4a4fca74844cefb43e
Émilie Feral <emilie.feral@numworks.com>
2021-06-15 10:09:32 +0200

    [ion] Kernel: display the spinner over the battery

--- ion/src/device/kernel/drivers/events.h
+++ ion/src/device/kernel/drivers/events.h
@@ -30,7 +30,7 @@ void stall();
 constexpr int k_spinnerHalfHeight = 4;
 constexpr int k_spinnerHalfWidth = 8;
 constexpr int k_barHeight = 18; // Escher::Metric::TitleBarHeight
-constexpr int k_spinnerX = 228;
+constexpr int k_spinnerX = 320 - 5 - 15; // Ion::Display::Width - Escher::Metric::TitleBarExternHorizontalMargin - BatteryView::k_batteryWidth
 constexpr int k_spinnerY = k_barHeight/2 - k_spinnerHalfHeight;
 void setSpinner(bool spinner);
 void spin();

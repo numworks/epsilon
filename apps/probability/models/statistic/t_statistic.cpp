b296e9b6ad141e21174c82d03a25d832d5ba2418
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-15 15:04:20 +0200

    Use student law in TStatistic
    Change-Id: I6794937c5577f3ffa81fd1fa8af8993445b5936d

--- apps/probability/models/statistic/t_statistic.cpp
+++ apps/probability/models/statistic/t_statistic.cpp
@@ -2,36 +2,37 @@
 
 #include <poincare/normal_distribution.h>
 
+#include "probability/models/student_law.h"
+
 namespace Probability {
 
 using namespace Poincare;
 
 float TStatistic::normedDensityFunction(float x) {
-  return NormalDistribution::EvaluateAtAbscissa<float>(x, 0, 1);
+  return StudentLaw::EvaluateAtAbscissa<float>(x, m_degreesOfFreedom);
 }
 
 float TStatistic::_tAlpha(float degreesOfFreedom, float alpha) {
-  return NormalDistribution::CumulativeDistributiveInverseForProbability<float>(alpha, 0, 1);
+  return StudentLaw::CumulativeDistributiveInverseForProbability<float>(alpha, degreesOfFreedom);
   ;
 }
 
 float TStatistic::_pVal(float degreesOfFreedom, float t) {
   switch (m_hypothesisParams.op()) {
     case HypothesisParams::ComparisonOperator::Higher:
-      return 1 - NormalDistribution::CumulativeDistributiveFunctionAtAbscissa<float>(t, 0, 1);
+      return 1 - StudentLaw::CumulativeDistributiveFunctionAtAbscissa<float>(t, degreesOfFreedom);
     case HypothesisParams::ComparisonOperator::Lower:
-      return NormalDistribution::CumulativeDistributiveFunctionAtAbscissa<float>(t, 0, 1);
+      return StudentLaw::CumulativeDistributiveFunctionAtAbscissa<float>(t, degreesOfFreedom);
     case HypothesisParams::ComparisonOperator::Different:
       assert(t > 0);
-      return NormalDistribution::CumulativeDistributiveFunctionAtAbscissa<float>(-t / 2, 0, 1) + 1 -
-             NormalDistribution::CumulativeDistributiveFunctionAtAbscissa<float>(t / 2, 0, 1);
+      return StudentLaw::CumulativeDistributiveFunctionAtAbscissa<float>(-t / 2, degreesOfFreedom) + 1 -
+             StudentLaw::CumulativeDistributiveFunctionAtAbscissa<float>(t / 2, degreesOfFreedom);
   }
 }
 
 float TStatistic::_tCritical(float degreesOfFreedom, float confidenceLevel) {
-  // TODO correct student law
-  return NormalDistribution::CumulativeDistributiveInverseForProbability<float>(
-      0.5 + confidenceLevel / 2, 0, 1);
+  return StudentLaw::CumulativeDistributiveInverseForProbability<float>(
+      0.5 + confidenceLevel / 2, degreesOfFreedom);
 }
 
 }  // namespace Probability

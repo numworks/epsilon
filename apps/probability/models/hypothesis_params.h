4750ce2fa2c737db25d3da286f3bb8db52dbecc1
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-17 11:10:13 +0200

    Fix initializer list
    Change-Id: I1a3bd3e800f06125a07c663c34f18dc299ac19c1

--- apps/probability/models/hypothesis_params.h
+++ apps/probability/models/hypothesis_params.h
@@ -10,7 +10,7 @@ public:
     Higher = '>',
     Different = '='  // TODO correct symbol
   };
-  HypothesisParams() : m_op(ComparisonOperator::Higher), m_firstParam(0) {}
+  HypothesisParams() : m_firstParam(0), m_op(ComparisonOperator::Higher) {}
 
   float firstParam() const { return m_firstParam; }
   void setFirstParam(float firstParam) { m_firstParam = firstParam; }

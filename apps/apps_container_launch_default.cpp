b22a1de63b7766fc27c56e8b55d0f0a8f7a42f85
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-06-22 10:31:14 +0200

    [apps/apps_container_launch_default] Remove deprecated call

--- apps/apps_container_launch_default.cpp
+++ apps/apps_container_launch_default.cpp
@@ -1,7 +1,5 @@
 #include "apps_container.h"
 
 Escher::App::Snapshot * AppsContainer::initialAppSnapshot() {
-  // The backlight has not been initialized
-  Ion::Backlight::init();
   return appSnapshotAtIndex(numberOfBuiltinApps() == 2 ? 1 : 0);
 }

dbc8e9b28afef9208df63b0a4dea7ff2ef240c42
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-06-25 11:07:22 +0200

    [ion/simulator/circuit_breaker] Add dummy implementation for web

---
+++ ion/src/simulator/shared/dummy/circuit_breaker.cpp
@@ -0,0 +1,29 @@
+#include <ion/circuit_breaker.h>
+#include <assert.h>
+
+namespace Ion {
+namespace CircuitBreaker {
+
+Status status() {
+  return Status::Set;
+}
+
+bool hasCheckpoint(CheckpointType) {
+  return false;
+}
+
+void unsetCheckpoint(CheckpointType) {}
+
+void loadCheckpoint(CheckpointType) {}
+
+Status statusAfterSetjmp(int, CheckpointType) {
+  return Status::Ignored;
+}
+
+jmp_buf sDummy;
+jmp_buf * jmpbufForType(CheckpointType) {
+  return &sDummy;
+}
+
+}
+}

738be7a77925ba5e1d67f18bf80f11460b4e544b
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-25 14:43:51 +0200

    Use transparent images for Distribution icons
    Change-Id: If578194fa947303a61fa69a82c2397bd0f244dcf

---
+++ apps/probability/gui/distribution_cell.cpp
@@ -0,0 +1,29 @@
+#include "distribution_cell.h"
+
+#include <assert.h>
+
+using namespace Escher;
+
+namespace Probability {
+
+DistributionCell::DistributionCell() :
+    TableCell(),
+    m_messageView(KDFont::LargeFont, (I18n::Message)0, 0, 0.5, KDColorBlack, KDColorWhite) {
+}
+
+void DistributionCell::reloadCell() {
+  HighlightCell::reloadCell();
+  KDColor backgroundColor = isHighlighted() ? Palette::Select : KDColorWhite;
+  m_messageView.setBackgroundColor(backgroundColor);
+  m_iconView.setHighlighted(isHighlighted());
+}
+
+void DistributionCell::setLabel(I18n::Message message) {
+  m_messageView.setMessage(message);
+}
+
+void DistributionCell::setImage(const Image * image) {
+  m_iconView.setImage(image);
+}
+
+}  // namespace Probability

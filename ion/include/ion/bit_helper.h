70a6d6285a1cfb53678f750efe81bcb9709cacca
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-06-29 11:14:31 +0200

    Improve and fix Ion::BitHelper
    Change-Id: Ia0e9236dbfc9bd964d56ba4510baa2cab2a31d44

---
+++ ion/include/ion/bit_helper.h
@@ -0,0 +1,52 @@
+#ifndef ION_BIT_HELPER_H
+#define ION_BIT_HELPER_H
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+namespace Ion {
+namespace BitHelper {
+
+constexpr static size_t k_numberOfBitsInByte = 8;
+constexpr static size_t k_numberOfBitsInInt = sizeof(int) * k_numberOfBitsInByte;
+
+template <typename T>
+size_t numberOfBitsInType() {
+  return sizeof(T) * k_numberOfBitsInByte;
+}
+
+template <typename T>
+bool bitAtIndex(T mask, int i) {
+  int t = sizeof(T);
+  assert(i >= 0 && i < numberOfBitsInType<T>());
+  return (mask >> i) & 1U;
+}
+
+template <typename T>
+void setBitAtIndex(T & mask, int i, bool b) {
+  assert(i >= 0 && i < numberOfBitsInType<T>());
+  constexpr static T one = 1;
+  if (b) {
+    mask |= (one << i);
+  } else {
+    mask &= ~(one << i);
+  }
+}
+
+inline size_t countLeadingZeros(unsigned int i) {
+  return __builtin_clz(i);
+}
+
+inline size_t countTrailingZeros(unsigned int i) {
+  return __builtin_ctz(i);
+}
+
+inline size_t numberOfOnes(unsigned int i) {
+  return __builtin_popcount(i);
+}
+
+}  // namespace BitHelper
+}  // namespace Ion
+
+#endif /* ION_BIT_HELPER_H */

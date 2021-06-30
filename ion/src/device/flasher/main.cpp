1cd2aea672be14e734a312896c8d272be6f16030
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-06-30 11:06:21 +0200

    [ion/flasher] Remove display files
    Flasher now only uses the "light" display mode, which displays a uniform
    yellow screen.

--- ion/src/device/flasher/main.cpp
+++ ion/src/device/flasher/main.cpp
@@ -1,12 +1,11 @@
 #include "../shared/usb/calculator.h"
-#include "display.h"
 #include <ion.h>
 
 void ion_main(int argc, const char * const argv[]) {
   // Initialize the backlight
   Ion::Backlight::init();
   // Initialize Flasher display
-  Flasher::Display::init();
+  Ion::Device::Display::pushRectUniform(KDRect(0,0,Ion::Display::Width,Ion::Display::Height), KDColorYellow);
   while (true) {
     Ion::USB::enable();
     while (!Ion::USB::isEnumerated()) {

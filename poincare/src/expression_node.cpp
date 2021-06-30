c793ac0451f3d904dff60d23506b166f2dcab254
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-06-30 13:01:08 +0200

    [poincare/expression_node.cpp] Remove defensive if

--- poincare/src/expression_node.cpp
+++ poincare/src/expression_node.cpp
@@ -20,11 +20,8 @@ Expression ExpressionNode::replaceSymbolWithExpression(const SymbolAbstract & sy
 }
 
 Expression ExpressionNode::setSign(Sign s, ReductionContext reductionContext) {
-  if (s == sign(reductionContext.context())) {
-    return Expression(this);
-  }
-  assert(false);
-  return Expression();
+  assert(s == sign(reductionContext.context()));
+  return Expression(this);
 }
 
 int ExpressionNode::polynomialDegree(Context * context, const char * symbolName) const {

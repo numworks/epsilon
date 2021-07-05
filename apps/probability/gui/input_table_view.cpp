c1fc08b3246fb04d3bb2f4c8cd6dc06683e3cc02
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-07-05 15:56:29 +0200

    Add spacers in InputGoodnessController
    Change-Id: Iac98efbcad6d156040466110d5546eb0d364f81d

--- apps/probability/gui/input_table_view.cpp
+++ apps/probability/gui/input_table_view.cpp
@@ -2,16 +2,21 @@
 
 using namespace Probability;
 
-InputTableView::InputTableView(Responder * parentResponder, ButtonDelegate * buttonDelegate, SelectableTableView * table,
+InputTableView::InputTableView(Responder * parentResponder,
+                               ButtonDelegate * buttonDelegate,
+                               SelectableTableView * table,
                                InputEventHandlerDelegate * inputEventHandlerDelegate,
-                               TextFieldDelegate * textFieldDelegate)
-    : VerticalLayout(),
-      Responder(parentResponder),
-      m_dataInputTableView(table),
-      m_significanceCell(this, inputEventHandlerDelegate, textFieldDelegate),
-      m_next(this, I18n::Message::Ok, buttonDelegate->buttonActionInvocation()) {
+                               TextFieldDelegate * textFieldDelegate) :
+    VerticalLayout(KDColorOrange),
+    Responder(parentResponder),
+    m_dataInputTableView(table),
+    m_significanceCell(this, inputEventHandlerDelegate, textFieldDelegate),
+    m_next(this, I18n::Message::Ok, buttonDelegate->buttonActionInvocation(), KDFont::LargeFont),
+    m_spacer1(Palette::WallScreenDark, 0, k_marginVertical),
+    m_spacer2(Palette::WallScreenDark, 0, k_marginVertical) {
   setMargins(Metric::CommonRightMargin, 0);
-  m_dataInputTableView->setMargins(Metric::CommonTopMargin, 0, Metric::CommonBottomMargin, 0);
+  m_dataInputTableView->setMargins(Metric::CommonTopMargin, 0, 0, 0);
+  m_dataInputTableView->setBackgroundColor(Palette::WallScreenDark);
   m_significanceCell.setMessage(I18n::Message::GreekAlpha);
   m_significanceCell.setSubLabelMessage(I18n::Message::SignificanceLevel);
   m_significanceCell.textField()->setText("0.05");  // TODO kinda ugly?
@@ -29,10 +34,10 @@ void InputTableView::didBecomeFirstResponder() {
 bool InputTableView::handleEvent(Ion::Events::Event event) {
   if (event == Ion::Events::Up || event == Ion::Events::Down) {
     if (event == Ion::Events::Up && m_viewSelection.selectedRow() > 0) {
-      m_viewSelection.selectRow(m_viewSelection.selectedRow() - 1);
+      m_viewSelection.selectRow(m_viewSelection.selectedRow() - 2);
     }
     if (event == Ion::Events::Down && m_viewSelection.selectedRow() < numberOfSubviews() - 1) {
-      m_viewSelection.selectRow(m_viewSelection.selectedRow() + 1);
+      m_viewSelection.selectRow(m_viewSelection.selectedRow() + 2);
     }
     setResponderForSelectedRow();
     selectCorrectView();
@@ -78,17 +83,22 @@ void InputTableView::selectCorrectView() {
 }
 
 Escher::View * InputTableView::subviewAtIndex(int i) {
-  switch (i)
-  {
-  case k_indexOfTable:
-    return m_dataInputTableView;
-    break;
-  case k_indexOfSignificance:
-    return &m_significanceCell;
-    break;
-  case k_indexOfNext:
-    return &m_next;
-    break;
+  switch (i) {
+    case k_indexOfTable:
+      return m_dataInputTableView;
+      break;
+    case k_indexOfSpacer1:
+      return &m_spacer1;
+      break;
+    case k_indexOfSignificance:
+      return &m_significanceCell;
+      break;
+    case k_indexOfSpacer2:
+      return &m_spacer2;
+      break;
+    case k_indexOfNext:
+      return &m_next;
+      break;
   }
   assert(false);
 }
--- apps/probability/gui/input_table_view.h
+++ apps/probability/gui/input_table_view.h
@@ -10,8 +10,9 @@
 #include <escher/text_field_delegate.h>
 #include <ion/events.h>
 
-#include "probability/abstract/button_delegate.h"
 #include "horizontal_or_vertical_layout.h"
+#include "probability/abstract/button_delegate.h"
+#include "spacer_view.h"
 
 namespace Probability {
 
@@ -20,14 +21,21 @@ namespace Probability {
  * Meant for InputGoodnessController and InputHomogeneityController.
  */
 class InputTableView : public VerticalLayout, public Responder {
- public:
-  InputTableView(Responder * parentResponder, ButtonDelegate * buttonDelegate, SelectableTableView * table,
-                 InputEventHandlerDelegate * inputEventHandlerDelegate, TextFieldDelegate * textFieldDelegate);
+public:
+  InputTableView(Responder * parentResponder,
+                 ButtonDelegate * buttonDelegate,
+                 SelectableTableView * table,
+                 InputEventHandlerDelegate * inputEventHandlerDelegate,
+                 TextFieldDelegate * textFieldDelegate);
 
-  MessageTableCellWithEditableTextWithMessage * significanceLevelView() { return &m_significanceCell; }
-  Shared::ButtonWithSeparator * nextButton() { return &m_next; }
+  MessageTableCellWithEditableTextWithMessage * significanceLevelView() {
+    return &m_significanceCell;
+  }
+  void drawRect(KDContext * ctx, KDRect rect) const {
+    ctx->fillRect(rect, Palette::WallScreenDark);
+  }
   // VerticalLayout
-  int numberOfSubviews() const override { return 3; }
+  int numberOfSubviews() const override { return 5; }
   Escher::View * subviewAtIndex(int i) override;
 
   // Responder
@@ -36,18 +44,23 @@ class InputTableView : public VerticalLayout, public Responder {
 
   SelectableTableViewDataSource * selectionDataSource() { return &m_tableSelection; }
 
- private:
+private:
+  constexpr static int k_marginVertical = 5;
   Responder * responderForRow(int row);
   void setResponderForSelectedRow();
   void selectCorrectView();
 
   constexpr static int k_indexOfTable = 0;
-  constexpr static int k_indexOfSignificance = 1;
-  constexpr static int k_indexOfNext = 2;
+  constexpr static int k_indexOfSpacer1 = 1;
+  constexpr static int k_indexOfSignificance = 2;
+  constexpr static int k_indexOfSpacer2 = 3;
+  constexpr static int k_indexOfNext = 4;
 
   SelectableTableView * m_dataInputTableView;
   MessageTableCellWithEditableTextWithMessage m_significanceCell;
-  Shared::ButtonWithSeparator m_next;
+  Escher::Button m_next;
+  SpacerView m_spacer1;
+  SpacerView m_spacer2;
 
   SelectableTableViewDataSource m_tableSelection;
   SelectableTableViewDataSource m_viewSelection;

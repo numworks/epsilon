e8f44673cd16f0e98ea75fbf6c94a9605f25644c
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-07-07 14:12:40 +0200

    Added a Chi2Law to extract Chi2 computation from Distribution
    Change-Id: I18cd42a93d8ee17e66efa8a46a7c94540e05aaf4

---
+++ apps/probability/models/chi2_law.h
@@ -0,0 +1,24 @@
+#ifndef APPS_PROBABILITY_MODELS_CHI2_LAW_H
+#define APPS_PROBABILITY_MODELS_CHI2_LAW_H
+
+#include "float.h"
+
+namespace Probability {
+
+class Chi2Law {
+public:
+  template <typename T>
+  static T EvaluateAtAbscissa(T x, T k);
+  template <typename T>
+  static T CumulativeDistributiveFunctionAtAbscissa(T x, T k);
+  template <typename T>
+  static T CumulativeDistributiveInverseForProbability(T probability, T k);
+
+private:
+  static constexpr int k_maxRegularizedGammaIterations = 1000;
+  static constexpr double k_regularizedGammaPrecision = DBL_EPSILON;
+};
+
+}  // namespace Probability
+
+#endif /* APPS_PROBABILITY_MODELS_CHI2_LAW_H */

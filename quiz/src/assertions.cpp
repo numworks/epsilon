dcef71a88e4444221c0658ca4c2cbaa498799b13
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-07-08 09:43:29 +0200

    Don't abort tests when fails in Debug
    Change-Id: Ic0818a1ad6c286ba77afdb3f11096de49ccb1f50

--- quiz/src/assertions.cpp
+++ quiz/src/assertions.cpp
@@ -1,6 +1,9 @@
 #include <ion.h>
 #include <stdlib.h>
 #include <quiz.h>
+#ifndef NDEBUG
+#include <ion/console.h>
+#endif
 
 void quiz_assert(bool condition) {
 #if PLATFORM_DEVICE
@@ -8,7 +11,11 @@ void quiz_assert(bool condition) {
   while (!condition) {}
 #else
   if (!condition) {
+#ifdef NDEBUG
     abort();
+#else
+    Ion::Console::writeLine("Error: Test failed!");
+#endif
   }
 #endif
 }

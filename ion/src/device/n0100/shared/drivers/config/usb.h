bb1413b2f337e6c4560377fdfc2c2499315d3112
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-07-09 15:25:25 +0200

    [ion/device/usb] Factor USB pins definitions

--- ion/src/device/n0100/shared/drivers/config/usb.h
+++ ion/src/device/n0100/shared/drivers/config/usb.h
@@ -1,20 +1,14 @@
 #ifndef ION_DEVICE_N0100_CONFIG_USB_H
 #define ION_DEVICE_N0100_CONFIG_USB_H
 
-#include <regs/regs.h>
 #include <drivers/config/board.h>
 #include <drivers/config/internal_flash.h>
+#include <drivers/config/usb_pins.h>
 
 namespace Ion {
 namespace Device {
 namespace USB {
 namespace Config {
-
-using namespace Regs;
-
-constexpr static AFGPIOPin VbusPin(GPIOA, 9, GPIO::AFR::AlternateFunction::AF10, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::Fast);
-constexpr static AFGPIOPin DmPin(GPIOA, 11, GPIO::AFR::AlternateFunction::AF10, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::Fast);
-constexpr static AFGPIOPin DpPin(GPIOA, 12, GPIO::AFR::AlternateFunction::AF10, GPIO::PUPDR::Pull::None, GPIO::OSPEEDR::OutputSpeed::Fast);
 
 static_assert(InternalFlash::Config::StartAddress + Board::Config::BootloaderSize == 0x08004000, "Bootloader's memory area should not be accessible via DFU");
 

382bef9db5144839bdb3c1f5508713146bed3c40
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-07-13 17:17:47 +0200

    Dynamically fill HomogeneityDataSource header text
    Change-Id: I8fa17738a438937b4393168c68c97ac2883c06fb

--- apps/probability/abstract/homogeneity_data_source.cpp
+++ apps/probability/abstract/homogeneity_data_source.cpp
@@ -7,24 +7,16 @@ using namespace Probability;
 
 HomogeneityTableDataSource::HomogeneityTableDataSource(TableViewDataSource * contentTable,
                                                        I18n::Message headerPrefix) :
-    m_contentTable(contentTable), m_topLeftCell(Escher::Palette::WallScreenDark) {
-  // Headers
-  constexpr int bufferSize = 20;
-  char txt[bufferSize];
-  const char * groupTxt = I18n::translate(headerPrefix);
-  int offset = strlcpy(txt, groupTxt, bufferSize);
-  offset += strlcpy(txt + offset, " ", bufferSize - offset);
+    m_contentTable(contentTable),
+    m_topLeftCell(Escher::Palette::WallScreenDark),
+    m_headerPrefix(headerPrefix) {
   // First row
   for (int i = 0; i < k_initialNumberOfColumns; i++) {
-    *(txt + offset) = '1' + i;
-    m_colHeader[i].setText(txt);
     m_colHeader[i].setAlignment(.5f, .5f);
     m_colHeader[i].setFont(KDFont::SmallFont);
   }
   // First column
   for (int i = 0; i < k_initialNumberOfRows; i++) {
-    *(txt + offset) = 'A' + i;
-    m_rowHeader[i].setText(txt);
     m_rowHeader[i].setAlignment(.5f, .5f);
     m_rowHeader[i].setFont(KDFont::SmallFont);
     m_rowHeader[i].setEven(i % 2 == 0);
@@ -56,4 +48,48 @@ int HomogeneityTableDataSource::typeAtLocation(int i, int j) {
     return 0;
   }
   return m_contentTable->typeAtLocation(i - 1, j - 1);
+}
+
+void Probability::HomogeneityTableDataSource::willDisplayCellAtLocation(
+    Escher::HighlightCell * cell,
+    int column,
+    int row) {
+  if (row == 0 && column == 0) {
+    return;  // Top left
+  }
+
+  // Headers
+  if (row == 0 || column == 0) {
+    Escher::EvenOddBufferTextCell * myCell = static_cast<Escher::EvenOddBufferTextCell *>(cell);
+    constexpr int bufferSize = 20;
+    char txt[bufferSize];
+    strcpy(txt, I18n::translate(m_headerPrefix));
+    int length = strlen(txt);
+    char digit;
+    if (row == 0) {
+      digit = '1' + (column - 1);
+    } else {
+      digit = 'A' + (row - 1);
+    }
+
+    txt[length] = digit;
+    txt[length + 1] = 0;
+    myCell->setText(txt);
+  }
+
+  else {
+    m_contentTable->willDisplayCellAtLocation(cell, column - 1, row - 1);
+  }
+}
+
+void Probability::HomogeneityTableDataSource::tableViewDidChangeSelection(
+    SelectableTableView * t,
+    int previousSelectedCellX,
+    int previousSelectedCellY,
+    bool withinTemporarySelection) {
+  // Prevent top left selection
+  if (t->selectedRow() == 0 && t->selectedColumn() == 0) {
+    t->selectRow(previousSelectedCellY);
+    t->selectColumn(previousSelectedCellX);
+  }
 }

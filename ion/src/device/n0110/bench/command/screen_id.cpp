6d4ac541c3fe698e26cdfcc21699d35f01998d19
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-07-16 09:58:09 +0200

    [ion/bench] Build screen ID test

--- ion/src/device/n0110/bench/command/screen_id.cpp
+++ ion/src/device/n0110/bench/command/screen_id.cpp
@@ -1,6 +1,5 @@
 #include "command.h"
-#include <ion.h>
-#include <ion/display.h>
+#include <drivers/display.h>
 #include <poincare/print_int.h>
 
 namespace Ion {
@@ -13,7 +12,7 @@ void ScreenID(const char * input) {
     reply(sSyntaxError);
     return;
   }
-  uint32_t screenID = Ion::Device::Display::panelIdentifier();
+  uint32_t screenID = Display::panelIdentifier();
 
   constexpr int bufferSize = 10 + 1; // screenID is a uint32_t so 10 digits max
   char buffer[bufferSize];

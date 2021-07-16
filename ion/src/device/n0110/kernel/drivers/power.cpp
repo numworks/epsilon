172f87518cc12ab5bbebbfd3a7b859a0420d9bc6
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-07-16 11:20:10 +0200

    [ion/bench] Build sleep, standby, and stop tests

--- ion/src/device/n0110/kernel/drivers/power.cpp
+++ ion/src/device/n0110/kernel/drivers/power.cpp
@@ -1,8 +1,8 @@
-#include <shared/drivers/power.h>
-#include <kernel/drivers/power.h>
+#include <drivers/power.h>
+#include <drivers/board.h>
+#include <drivers/keyboard.h>
 #include <drivers/wakeup.h>
-#include <kernel/drivers/board.h>
-#include <kernel/drivers/keyboard.h>
+#include <shared/drivers/power.h>
 
 namespace Ion {
 namespace Device {
@@ -11,14 +11,6 @@ namespace Power {
 /* We isolate the standby code that needs to be executed from the internal
  * flash (because the external flash is then shut down). We forbid inlining to
  * avoid inlining these instructions in the external flash. */
-
-void standby() {
-  Power::waitUntilOnOffKeyReleased();
-  Power::standbyConfiguration();
-  Board::shutdownPeripherals();
-  Board::shutdownPeripheralsClocks(false);
-  bootloaderSuspend();
-}
 
 void configWakeUp() {
   WakeUp::onOnOffKeyDown();

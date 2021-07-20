884f64d7977e63ee35b2d5045cc56e7e0a478fcd
Émilie Feral <emilie.feral@numworks.com>
2021-07-20 12:00:59 +0200

    [ion] Expose some practical functions in userland ISR for external apps: Step 1: draw_string

--- ion/src/device/userland/boot/isr.c
+++
@@ -1,9 +0,0 @@
-#include "isr.h"
-
-extern const void * _process_stack_start;
-typedef void (*ISR)(void);
-
-ISR InitialisationVector[2] __attribute__((section(".isr_vector_table"), used)) = {
-  (ISR)&_process_stack_start, // Stack start
-  start // Reset service routine,
-};

40c92585ae1f5d99acdfa5069a5150a04397864d
Émilie Feral <emilie.feral@numworks.com>
2021-07-22 17:25:24 +0200

    [ion] N0100 SerialNumber is not done via SVC since it's needed by DFU driver which can't access to the internal flash

---
+++ ion/src/device/n0110/userland/drivers/serial_number.cpp
@@ -0,0 +1,14 @@
+#include <ion.h>
+#include <userland/drivers/svcall.h>
+
+namespace Ion {
+namespace Device {
+namespace SerialNumber {
+
+void SVC_ATTRIBUTES copy(char * buffer) {
+  SVC_RETURNING_VOID(SVC_SERIAL_NUMBER_COPY)
+}
+
+}
+}
+}

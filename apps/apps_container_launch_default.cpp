6d086e4a47e5cf17659f055d63dc84f2c641afca
Émilie Feral <emilie.feral@numworks.com>
2021-07-26 17:05:13 +0200

    [apps] Initial app depends on the total number of apps and not on the numberOfBuiltinApps

--- apps/apps_container_launch_default.cpp
+++ apps/apps_container_launch_default.cpp
@@ -1,5 +1,5 @@
 #include "apps_container.h"
 
 Escher::App::Snapshot * AppsContainer::initialAppSnapshot() {
-  return appSnapshotAtIndex(numberOfBuiltinApps() == 2 ? 1 : 0);
+  return appSnapshotAtIndex(numberOfApps() == 2 ? 1 : 0);
 }

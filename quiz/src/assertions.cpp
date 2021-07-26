79f63e2e8204a8bfc2fb95476e615abf5c57cfaa
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-07-26 12:46:57 +0200

    [quiz] Stall on quiz_assert(false) on device

--- quiz/src/assertions.cpp
+++ quiz/src/assertions.cpp
@@ -3,7 +3,12 @@
 #include <quiz.h>
 
 void quiz_assert(bool condition) {
+#if PLATFORM_DEVICE
+  /* Freeze on device so that the error message can be read. */
+  while (!condition) {}
+#else
   if (!condition) {
     abort();
   }
+#endif
 }

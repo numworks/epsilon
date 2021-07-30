4e46482632d45ebdb9a24860b1f25b8afaf96244
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-07-30 09:59:09 +0200

    [escher] Add a new cell type

---
+++ escher/src/message_table_cell_with_message_with_buffer.cpp
@@ -0,0 +1,27 @@
+#include <escher/message_table_cell_with_message_with_buffer.h>
+#include <escher/palette.h>
+
+namespace Escher {
+
+MessageTableCellWithMessageWithBuffer::MessageTableCellWithMessageWithBuffer(I18n::Message message) :
+  MessageTableCellWithMessage(message),
+  m_accessoryView(KDFont::LargeFont, 1.0f, 0.5f, KDColorBlack)
+{
+}
+
+void MessageTableCellWithMessageWithBuffer::setAccessoryText(const char * textBody) {
+  m_accessoryView.setText(textBody);
+  layoutSubviews();
+}
+
+const char * MessageTableCellWithMessageWithBuffer::accessoryText() {
+  return m_accessoryView.text();
+}
+
+void MessageTableCellWithMessageWithBuffer::setHighlighted(bool highlight) {
+  MessageTableCellWithMessage::setHighlighted(highlight);
+  KDColor backgroundColor = isHighlighted()? Palette::Select : KDColorWhite;
+  m_accessoryView.setBackgroundColor(backgroundColor);
+}
+
+}

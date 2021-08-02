ce0a18c64f3e4bd00dd6ef74c2a9f97a56af779c
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-08-02 16:53:07 +0200

    [graph/graph_view] Add constraints on number of functions

--- apps/graph/graph/graph_view.cpp
+++ apps/graph/graph/graph_view.cpp
@@ -117,4 +117,25 @@ void GraphView::drawRect(KDContext * ctx, KDRect rect) const {
   }
 }
 
+bool GraphView::allFunctionsInterrupted(int numberOfFunctions) const {
+  /* The number of functions displayed at the same time is theoretically unbounded, but we only store the status of 32 functions. */
+  if (numberOfFunctions <= 0 || static_cast<size_t>(numberOfFunctions) > 8 * sizeof(m_functionsInterrupted)) {
+    return false;
+  }
+  return m_functionsInterrupted == static_cast<uint32_t>((1 << numberOfFunctions) - 1);
+}
+
+bool GraphView::functionWasInterrupted(int index) const {
+  if (index < 0 || static_cast<size_t>(index) >= 8 * sizeof(m_functionsInterrupted)) {
+    return false;
+  }
+  return (1 << index) & m_functionsInterrupted;
+}
+
+void GraphView::setFunctionInterrupted(int index) const {
+  if (index >= 0 & static_cast<size_t>(index) < 8 * sizeof(m_functionsInterrupted)) {
+    m_functionsInterrupted |= 1 << index;
+  }
+}
+
 }

b36e2fb5c9c0e04099e2c318973ea412bb5c4583
Émilie Feral <emilie.feral@numworks.com>
2021-08-05 13:59:49 +0200

    [ion] USB: string descriptor is dynamically computed depending on the running slot

--- ion/src/device/n0110/userland/drivers/usb.cpp
+++ ion/src/device/n0110/userland/drivers/usb.cpp
@@ -1,6 +1,9 @@
 #include <ion/usb.h>
-#include <userland/drivers/svcall.h>
+#include <ion/authentication.h>
+#include <shared/drivers/board_unprivileged.h>
 #include <shared/drivers/usb.h>
+#include <userland/drivers/config/usb.h>
+#include <userland/drivers/svcall.h>
 
 namespace Ion {
 namespace USB {
@@ -18,6 +21,14 @@ namespace USB {
 
 bool SVC_ATTRIBUTES shouldInterruptDFU() {
   SVC_RETURNING_R0(SVC_USB_SHOULD_INTERRUPT, bool)
+}
+
+const char * stringDescriptor() {
+  if (Authentication::clearanceLevel() == Ion::Authentication::ClearanceLevel::NumWorks || Authentication::clearanceLevel() == Ion::Authentication::ClearanceLevel::NumWorksAndThirdPartyApps) {
+    return Board::isRunningSlotA() ? Config::InterfaceFlashStringDescriptorAuthenticatedSlotA : Config::InterfaceFlashStringDescriptorAuthenticatedSlotB;
+  } else {
+    return Board::isRunningSlotA() ? Config::InterfaceFlashStringDescriptorThirdPartySlotA : Config::InterfaceFlashStringDescriptorThirdPartySlotB;
+  }
 }
 
 }

db162ce8b85a88fad04d46b95293c87ab4699afb
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-08-06 14:42:18 +0200

    [apps/usb] Update workshop URL

--- apps/usb/usb_connected_controller.cpp
+++ apps/usb/usb_connected_controller.cpp
@@ -8,11 +8,11 @@ constexpr I18n::Message sUSBConnectedMessages[] = {
   I18n::Message::USBConnected,
   I18n::Message::ConnectedMessage1,
   I18n::Message::ConnectedMessage2,
-  I18n::Message::ConnectedMessage3,
+  I18n::Message::TransferDataUrl,
   I18n::Message::BlankMessage,
+  I18n::Message::ConnectedMessage3,
   I18n::Message::ConnectedMessage4,
-  I18n::Message::ConnectedMessage5,
-  I18n::Message::ConnectedMessage6};
+  I18n::Message::ConnectedMessage5};
 
 constexpr KDColor sUSBConnectedColors[] = {
   KDColorBlack,

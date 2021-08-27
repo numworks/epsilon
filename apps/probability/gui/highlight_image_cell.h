917dbdf6dc6a719dac0f3032f3c1eccdf81b3e51
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-08-27 11:04:27 +0200

    Refactor HighlightImageCell
    Change-Id: Ieaf27b36558f9093b556942a79e4ab6a90142bd8

--- apps/probability/gui/highlight_image_cell.h
+++ apps/probability/gui/highlight_image_cell.h
@@ -9,19 +9,14 @@ namespace Probability {
 
 class HighlightImageCell : public Escher::HighlightCell {
 public:
-  HighlightImageCell() : Escher::HighlightCell() { m_contentView.setBackgroundColor(KDColorWhite); }
-  void setHighlighted(bool highlighted) override {
-    m_contentView.setBackgroundColor(highlighted ? Escher::Palette::Select : KDColorWhite);
-    HighlightCell::setHighlighted(highlighted);
-  };
+  HighlightImageCell();
+  void setHighlighted(bool highlighted) override;
 
   // Wrap TransparentImageView
   int numberOfSubviews() const override { return 1; }
   Escher::View * subviewAtIndex(int i) override { return &m_contentView; }
-  KDSize minimalSizeForOptimalDisplay() const override {
-    return m_contentView.minimalSizeForOptimalDisplay();
-  }
-  void layoutSubviews(bool force) override { m_contentView.setFrame(bounds(), force); }
+  KDSize minimalSizeForOptimalDisplay() const override;
+  void layoutSubviews(bool force) override;
   void setImage(const Escher::Image * image) { m_contentView.setImage(image); }
 
 private:

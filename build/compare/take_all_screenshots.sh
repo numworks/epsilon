55b75080c347be73def3db93efa619885bf1ae9b
Gregoire Chalony <gregoire.chalony@numworks.com>
2021-09-01 12:53:37 +0200

    Add a script to extract all images from a state file
    Change-Id: Ife13ab0bfd4cd2810a5ff3e4e07a9e310a69e8e9

---
+++ build/compare/take_all_screenshots.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Extract screenshot for each event in the state file
+
+if [ $# -lt 2 ]; then
+  echo "Not enough parameters"
+  echo "take_all_screenshots.sh path/to/Epsilon path/to/state_file [output_folder]"
+  exit 1
+fi
+
+epsilon=$1
+state_file=$2
+output_folder=${3:-screenshots}
+
+mkdir -p $output_folder
+
+HEADER_SIZE=15
+
+original_size=$(stat -f %z ${state_file})
+size=$original_size
+
+while [ $size -gt $HEADER_SIZE ]
+do
+  output=${output_folder}/img-$(( size - HEADER_SIZE )).png
+  echo $output
+  ./${epsilon} --headless --load-state-file <(head -c${size} ${state_file}) --take-screenshot ${output} > /dev/null
+  size=$(( size - 1 ))
+done
+
+echo ${output_folder}/bug.gif
+convert -loop 0 ${output_folder}/img*.png ${output_folder}/bug.gif
+
+echo "All done, screenshots taken in ${output_folder}/"

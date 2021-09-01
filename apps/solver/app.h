7b4a94296bd5879e4496d5139c0b93f535117b96
Laury <laurianfournier@gmail.com>
2021-09-01 17:31:51 +0200

    [calculation][poincare] Revert 'b8544e3' and improve equal simplification

--- apps/solver/app.h
+++ apps/solver/app.h
@@ -47,9 +47,6 @@ public:
   void willBecomeInactive() override;
   TELEMETRY_ID("Solver");
 private:
-  // TextFieldDelegateApp
-  bool isAcceptableExpression(const Poincare::Expression expression) override;
-
   App(Snapshot * snapshot);
   SolutionsController m_solutionsController;
   IntervalController m_intervalController;

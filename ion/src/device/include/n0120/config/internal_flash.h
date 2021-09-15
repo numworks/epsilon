4c8963aaf78d687d63f9bf7147a0e2ff2400667d
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-09-15 17:08:13 +0200

    [ion/device/n0120] Add some config files

---
+++ ion/src/device/include/n0120/config/internal_flash.h
@@ -0,0 +1,26 @@
+#ifndef ION_DEVICE_N0120_CONFIG_INTERNAL_FLASH_H
+#define ION_DEVICE_N0120_CONFIG_INTERNAL_FLASH_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+namespace Ion {
+namespace Device {
+namespace InternalFlash {
+namespace Config {
+
+constexpr int NumberOfSectors = 4;
+constexpr uint32_t SectorAddresses[NumberOfSectors + 1] = {
+  0x08000000,
+  0x08020000,
+  0x08040000,
+  0x08060000,
+  0x08080000,
+};
+
+}
+}
+}
+}
+
+#endif

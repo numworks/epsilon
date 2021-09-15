9eb1e493e4ce5ab28396aa9562c2c34bd6835864
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-09-15 12:53:42 +0200

    [apps/graph] Add special conics horizontal scroll

--- apps/graph/graph/graph_controller_helper.cpp
+++ apps/graph/graph/graph_controller_helper.cpp
@@ -11,7 +11,7 @@ using namespace Poincare;
 
 namespace Graph {
 
-bool GraphControllerHelper::privateMoveCursorHorizontally(Shared::CurveViewCursor * cursor, int direction, Shared::InteractiveCurveViewRange * range, int numberOfStepsInGradUnit, Ion::Storage::Record record, int scrollSpeed, int secondaryCurveIndex) {
+bool GraphControllerHelper::privateMoveCursorHorizontally(Shared::CurveViewCursor * cursor, int direction, Shared::InteractiveCurveViewRange * range, int numberOfStepsInGradUnit, Ion::Storage::Record record, int scrollSpeed, int * secondaryCurveIndex) {
   ExpiringPointer<ContinuousFunction> function = App::app()->functionStore()->modelForRecord(record);
   double tCursorPosition = cursor->t();
   double t = tCursorPosition;
@@ -28,13 +28,44 @@ bool GraphControllerHelper::privateMoveCursorHorizontally(Shared::CurveViewCurso
   function = App::app()->functionStore()->modelForRecord(record); // Reload the expiring pointer
   double dir = (direction > 0 ? 1.0 : -1.0);
   double step = function->isAlongX() ? static_cast<double>(range->xGridUnit())/numberOfStepsInGradUnit : (tMax-tMin)/k_definitionDomainDivisor;
+
+  if (function->plotType() == ContinuousFunction::PlotType::Ellipse || function->plotType() == ContinuousFunction::PlotType::Circle || (function->plotType() == ContinuousFunction::PlotType::Parabola && function->hasTwoCurves())) {
+    assert(secondaryCurveIndex != nullptr && function->hasTwoCurves());
+    if (*secondaryCurveIndex == 1) {
+      dir *= -1.0;
+    }
+    Coordinate2D<double> xy = function->evaluateXYAtParameter(t, App::app()->localContext(), *secondaryCurveIndex);
+    if (std::isfinite(xy.x2())) {
+      double t2 = t + dir * step * scrollSpeed;
+      xy = function->evaluateXYAtParameter(t2, App::app()->localContext(), *secondaryCurveIndex);
+      if (std::isnan(xy.x2())) {
+        *secondaryCurveIndex = 1 - *secondaryCurveIndex;
+        t -= dir * step * scrollSpeed;
+      }
+    }
+  }
+  if (function->plotType() == ContinuousFunction::PlotType::Hyperbola || function->plotType() == ContinuousFunction::PlotType::Circle) {
+    Coordinate2D<double> xy = function->evaluateXYAtParameter(t, App::app()->localContext(), *secondaryCurveIndex);
+    if (std::isfinite(xy.x2())) {
+      double t2 = t + dir * step * scrollSpeed;
+      xy = function->evaluateXYAtParameter(t2, App::app()->localContext(), *secondaryCurveIndex);
+      int tries = 0;
+      while (std::isnan(xy.x2()) && tries < 20) {
+        tries ++;
+        t2 += dir * step * scrollSpeed;
+        xy = function->evaluateXYAtParameter(t2, App::app()->localContext(), *secondaryCurveIndex);
+      }
+      t = t2 - dir * step * scrollSpeed;
+      t = std::max(tMin, std::min(tMax, t));
+    }
+  }
   t += dir * step * scrollSpeed;
 
   // If possible, round t so that f(x) matches f evaluated at displayed x
   t = FunctionBannerDelegate::getValueDisplayedOnBanner(t, App::app()->localContext(), Preferences::sharedPreferences()->numberOfSignificantDigits(), 0.05 * step, true);
 
   t = std::max(tMin, std::min(tMax, t));
-  Coordinate2D<double> xy = function->evaluateXYAtParameter(t, App::app()->localContext(), secondaryCurveIndex);
+  Coordinate2D<double> xy = function->evaluateXYAtParameter(t, App::app()->localContext(), *secondaryCurveIndex);
   cursor->moveTo(t, xy.x1(), xy.x2());
   return true;
 }

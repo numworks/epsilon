f10d28aa5088cbb89a5ca0e86c1f3f3cf6cea1b4
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-09-17 11:35:03 +0200

    [apps/graph] Use the right text for details menu

--- apps/graph/list/list_parameter_controller.cpp
+++ apps/graph/list/list_parameter_controller.cpp
@@ -71,7 +71,7 @@ void ListParameterController::willDisplayCellForIndex(HighlightCell * cell, int 
     assert(!m_record.isNull());
     Shared::ExpiringPointer<ContinuousFunction> function = myApp->functionStore()->modelForRecord(m_record);
     if (cell == &m_typeCell) {
-      m_typeCell.setMessage(I18n::Message::CurveType);
+      m_typeCell.setMessage(I18n::Message::Details);
       m_typeCell.setSubtitle(function->functionCategory());
     } else {
       assert(cell == &m_functionDomain);

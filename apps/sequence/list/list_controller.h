05e6aa55a574ffc39220700e0f5e3ae4f5cf538e
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-09-21 17:20:11 +0200

    [apps/sequence] Restore the Sequence App

--- apps/sequence/list/list_controller.h
+++ apps/sequence/list/list_controller.h
@@ -1,6 +1,7 @@
 #ifndef SEQUENCE_LIST_CONTROLLER_H
 #define SEQUENCE_LIST_CONTROLLER_H
 
+#include <escher/table_view_data_source.h>
 #include "../../shared/sequence_title_cell.h"
 #include "../../shared/sequence_store.h"
 #include "../../shared/function_expression_cell.h"
@@ -14,39 +15,83 @@
 
 namespace Sequence {
 
-class ListController : public Shared::FunctionListController, public Shared::InputEventHandlerDelegate, public Shared::TextFieldDelegate, public Shared::LayoutFieldDelegate {
+class ListController : public Shared::FunctionListController, public Shared::InputEventHandlerDelegate, public Shared::TextFieldDelegate, public Shared::LayoutFieldDelegate, public Escher::TableViewDataSource {
 public:
   ListController(Escher::Responder * parentResponder, Escher::InputEventHandlerDelegate * inputEventHandlerDelegate, Escher::ButtonRowController * header, Escher::ButtonRowController * footer);
   const char * title() override;
   int numberOfExpressionRows() const override;
   KDCoordinate expressionRowHeight(int j) override;
-  void willDisplayCellAtLocation(Escher::HighlightCell * cell, int i, int j) override;
   Escher::Toolbox * toolboxForInputEventHandler(Escher::InputEventHandler * handler) override;
   void selectPreviousNewSequenceCell();
   void editExpression(int sequenceDefinitionIndex, Ion::Events::Event event);
+  /* ViewController */
+  void viewWillAppear() override;
+  /* TableViewDataSource */
+  int numberOfRows() const override { return this->numberOfExpressionRows(); }
+  KDCoordinate rowHeight(int j) override { return ExpressionModelListController::memoizedRowHeight(j); }
+  // TODO Hugo : useful ?
+  KDCoordinate cumulatedHeightFromIndex(int j) override { return ExpressionModelListController::memoizedCumulatedHeightFromIndex(j); }
+  int indexFromCumulatedHeight(KDCoordinate offsetY) override { return ExpressionModelListController::memoizedIndexFromCumulatedHeight(offsetY); }
+  int numberOfColumns() const override { return 2; }
+  KDCoordinate columnWidth(int i) override;
+  KDCoordinate cumulatedWidthFromIndex(int i) override;
+  int indexFromCumulatedWidth(KDCoordinate offsetX) override;
+  int typeAtLocation(int i, int j) override;
+  Escher::HighlightCell * reusableCell(int index, int type) override;
+  int reusableCellCount(int type) override;
+  void willDisplayCellAtLocation(Escher::HighlightCell * cell, int i, int j) override;
+  /* Responder */
+  void didBecomeFirstResponder() override;
+  bool handleEvent(Ion::Events::Event event) override;
+  /* ViewController */
+  Escher::View * view() override { return &m_selectableTableView; }
+  /* SelectableTableViewDelegate*/
+  void tableViewDidChangeSelection(Escher::SelectableTableView * t, int previousSelectedCellX, int previousSelectedCellY, bool withinTemporarySelection) override;
+  /* ExpressionModelListController */
+  Escher::SelectableTableView * selectableTableView() override { return &m_selectableTableView; }
 private:
+  static constexpr KDCoordinate k_minTitleColumnWidth = 65;
+  static constexpr KDCoordinate k_functionTitleSumOfMargins = 25;
   static constexpr KDCoordinate k_expressionCellVerticalMargin = 3;
+  static constexpr int k_maxNumberOfRows = 3*Shared::MaxNumberOfSequences;
+  void computeTitlesColumnWidth(bool forceMax = false);
+  void resetMemoizationForIndex(int index) override;
+  void shiftMemoization(bool newCellIsUnder) override;
   bool editInitialConditionOfSelectedRecordWithText(const char * text, bool firstInitialCondition);
   ListParameterController * parameterController() override;
   int maxNumberOfDisplayableRows() override;
-  Shared::FunctionTitleCell * titleCells(int index) override;
-  Escher::HighlightCell * expressionCells(int index) override;
-  void willDisplayTitleCellAtIndex(Escher::HighlightCell * cell, int j) override;
+  Shared::FunctionTitleCell * titleCells(int index);
+  Escher::HighlightCell * functionCells(int index) override;
+  void willDisplayTitleCellAtIndex(Escher::HighlightCell * cell, int j);
   void willDisplayExpressionCellAtIndex(Escher::HighlightCell * cell, int j) override;
   int modelIndexForRow(int j) override;
   int sequenceDefinitionForRow(int j);
+  KDCoordinate maxFunctionNameWidth();
+  void didChangeModelsList() override;
+  KDCoordinate baseline(int j);
+  KDCoordinate privateBaseline(int j) const;
+  KDCoordinate notMemoizedCumulatedHeightFromIndex(int j) override {
+    return TableViewDataSource::cumulatedHeightFromIndex(j);
+  }
+  int notMemoizedIndexFromCumulatedHeight(KDCoordinate offsetY) override {
+    return TableViewDataSource::indexFromCumulatedHeight(offsetY);
+  }
   void addModel() override;
   void reinitSelectedExpression(Shared::ExpiringPointer<Shared::ExpressionModelHandle> model) override;
   void editExpression(Ion::Events::Event event) override;
   bool removeModelRow(Ion::Storage::Record record) override;
   Shared::SequenceStore * modelStore() override;
-  constexpr static int k_maxNumberOfRows = 3*Shared::MaxNumberOfSequences;
+  KDCoordinate nameWidth(int nameLength) const;
+  Escher::SelectableTableView m_selectableTableView;
+  Escher::EvenOddCell m_emptyCell;
   Shared::SequenceTitleCell m_sequenceTitleCells[k_maxNumberOfRows];
   Shared::FunctionExpressionCell m_expressionCells[k_maxNumberOfRows];
   ListParameterController m_parameterController;
   TypeParameterController m_typeParameterController;
   Escher::StackViewController m_typeStackController;
   SequenceToolbox m_sequenceToolbox;
+  KDCoordinate m_titlesColumnWidth;
+  KDCoordinate m_memoizedCellBaseline[k_memoizedCellsCount];
 };
 
 }
--- apps/shared/function_title_cell.cpp
+++ apps/shared/function_title_cell.cpp
@@ -25,8 +25,17 @@ void FunctionTitleCell::setBaseline(KDCoordinate baseline) {
 
 void FunctionTitleCell::drawRect(KDContext * ctx, KDRect rect) const {
   if (m_orientation == Orientation::VerticalIndicator){
+    KDColor separatorColor = m_even ? Palette::WallScreen : KDColorWhite;
+    KDColor backgroundColor = m_even ? KDColorWhite : Palette::WallScreen;
     // Draw the color indicator
-    ctx->fillRect(KDRect(0, 0, k_colorIndicatorThickness, bounds().height()), m_functionColor);
+    ctx->fillRect(KDRect(0, 0, k_colorIndicatorThickness, bounds().height() - k_separatorThickness), m_functionColor);
+    // Draw the horizontal separator
+    ctx->fillRect(KDRect(0, bounds().height()-k_separatorThickness, bounds().width()-k_colorIndicatorThickness, k_separatorThickness), separatorColor);
+    // Draw some background
+    ctx->fillRect(KDRect(bounds().width() - k_equalWidthWithMargins, 0, k_equalWidthWithMargins, bounds().height() - k_separatorThickness), backgroundColor);
+    // Draw '='
+    KDPoint p = KDPoint(bounds().width() - k_equalWidthWithMargins, m_baseline - font()->glyphSize().height()/2 - 1); // -1 is visually needed
+    ctx->drawString("=", p, font(), m_functionColor, backgroundColor);
   } else {
     // Draw the color indicator
     ctx->fillRect(KDRect(0, 0, bounds().width(), k_colorIndicatorThickness), m_functionColor);
@@ -35,7 +44,7 @@ void FunctionTitleCell::drawRect(KDContext * ctx, KDRect rect) const {
 
 KDRect FunctionTitleCell::subviewFrame() const {
   if (m_orientation == Orientation::VerticalIndicator) {
-    return KDRect(k_colorIndicatorThickness, 0, bounds().width() - k_colorIndicatorThickness, bounds().height());
+    return KDRect(k_colorIndicatorThickness, 0, bounds().width() - k_colorIndicatorThickness - k_equalWidthWithMargins, bounds().height() - k_separatorThickness);
   }
   return KDRect(0, k_colorIndicatorThickness, bounds().width(), bounds().height()-k_colorIndicatorThickness);
 }

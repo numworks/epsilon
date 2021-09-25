efda5614257aceee25b08322cfd42fe1c41d98b4
ArtichautCosmique <leo.grange@icloud.com>
2021-09-25 18:55:43 +0200

    [ion/device/flasher] Pimp my flasher (light & verbose) (#27)

--- ion/src/device/flasher/display_light.cpp
+++ ion/src/device/flasher/display_light.cpp
@@ -1,10 +1,16 @@
 #include <ion.h>
+#include <kandinsky.h>
 
 namespace Flasher {
 namespace Display {
 
 void init() {
-  Ion::Display::pushRectUniform(KDRect(0,0,Ion::Display::Width,Ion::Display::Height), KDColor::RGB24(0xFFFF00));
+  KDRect screen = KDRect(0,0,Ion::Display::Width,Ion::Display::Height);
+  Ion::Display::pushRectUniform(KDRect(0,0,Ion::Display::Width,Ion::Display::Height), KDColor::RGB24(0x5e81ac));
+  KDContext * ctx = KDIonContext::sharedContext();
+  ctx->setOrigin(KDPointZero);
+  ctx->setClippingRect(screen);
+  ctx->drawString("RECOVERY MODE", KDPoint(10, 10), KDFont::LargeFont, KDColorWhite, KDColor::RGB24(0x5e81ac));
 }
 
 }

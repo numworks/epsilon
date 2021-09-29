a441e52e1d77453ffb5a3ac0ebb290053b2da71c
Émilie Feral <emilie.feral@numworks.com>
2021-09-29 17:59:53 +0200

    [ion] WIP: Comment all frequency alternance
    This is useful to make a prototype work quiclky but requires serious
    rework...

--- ion/src/device/shared/drivers/board_frequency_voltage_h.cpp
+++ ion/src/device/shared/drivers/board_frequency_voltage_h.cpp
@@ -9,9 +9,11 @@ namespace Board {
 
 using namespace Regs;
 
+#warning Frequency calibration: Cf comment in include/N0120/config/clocks.h
+
 void setLowVoltageScaling() {
 #warning This line doesn\'t seem to be effective
-  setFrequencyWithoutSystick(Frequency::Low);
+  /*setFrequencyWithoutSystick(Frequency::Low);
   // Select HSI as sysclk source
   RCC.CFGR()->setSW(RCC::CFGR::SW::HSI);
   while(RCC.CFGR()->getSWS() != RCC::CFGR::SW::HSI) {}
@@ -21,11 +23,11 @@ void setLowVoltageScaling() {
   while (RCC.CR()->getPLL1RDY() || RCC.CR()->getPLL2RDY()) {}
   // Decrease VOS
   PWR.D3CR()->setVOS(PWR::D3CR::VOS::Scale3);
-  while (!PWR.D3CR()->getVOSRDY()) {}
+  while (!PWR.D3CR()->getVOSRDY()) {}*/
 }
 
 void setHighVoltageScaling() {
-  // Increase VOS
+  /*// Increase VOS
   PWR.D3CR()->setVOS(PWR::D3CR::VOS::Scale0);
   // Ensure VOS activation before changing the frequency
   while (!PWR.D3CR()->getVOSRDY()) {}
@@ -33,15 +35,15 @@ void setHighVoltageScaling() {
   RCC.CR()->setPLL1ON(true);
   RCC.CR()->setPLL2ON(true);
   while (!RCC.CR()->getPLL1RDY() || !RCC.CR()->getPLL2RDY()) {}
-  /* Select PLL as sysclk source */
+  // Select PLL as sysclk source
   RCC.CFGR()->setSW(RCC::CFGR::SW::PLL1);
   while(RCC.CFGR()->getSWS() != RCC::CFGR::SW::PLL1) {}
-  setFrequencyWithoutSystick(Frequency::High);
+  setFrequencyWithoutSystick(Frequency::High);*/
 }
 
 void setFrequencyWithoutSystick(Frequency f) {
-  RCC::Prescaler4Bits prescaler = f == Frequency::Low ? RCC::Prescaler4Bits::DivideBy512 : RCC::Prescaler4Bits::NoDivide;
-  RCC.D1CFGR()->setD1CPRE(prescaler);
+  //RCC::Prescaler4Bits prescaler = f == Frequency::Low ? RCC::Prescaler4Bits::DivideBy512 : RCC::Prescaler4Bits::NoDivide;
+  //RCC.D1CFGR()->setD1CPRE(prescaler);
 }
 
 }

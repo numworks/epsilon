3ef4956d0776cba5d26d10782811b843d20eb21d
Émilie Feral <emilie.feral@numworks.com>
2021-10-04 16:02:23 +0200

    [ion] Decrease frequency in msleep and configurate msleep timings

--- ion/src/device/include/n0120/config/timing.h
+++ ion/src/device/include/n0120/config/timing.h
@@ -6,15 +6,12 @@ namespace Device {
 namespace Timing {
 namespace Config {
 
-/* FIXME LoopsPerMillisecondLowFrequency must be recalculated, since the "low"
- * frequency is much slower than eight times slower. */
-/* FIXME Those values calculated theorically by adapting the values from a
- * 192MHz cpu to a 540MHz cpu, but are in practice 1.9 times to slow. We could
- * avoid this issue altogether by reworking the sleep. */
-
-#warning experimental values designed for Portuguese prototype (with the frequency alternance removed)
-constexpr static int LoopsPerMillisecondLowFrequency = 84919;//10*325;//50;//325;//152;
-//constexpr static int LoopsPerMillisecondHighFrequency = 21060;
+#warning experimental values
+#if NDEBUG
+constexpr static int LoopsPerMillisecond_PLL_Prescaled512 = 146;
+#else
+constexpr static int LoopsPerMillisecond_PLL_Prescaled512 = 35;
+#endif
 constexpr static int LoopsPerMicrosecond = 86;
 
 }

44561a3d7a35b61f64b43d8bd82ea8c237ec6910
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-10-14 09:58:20 +0200

    [apps/probability] Improve factorizable methods

--- apps/probability/controllers/homogeneity_table_view_controller.cpp
+++ apps/probability/controllers/homogeneity_table_view_controller.cpp
@@ -67,25 +67,29 @@ bool HomogeneityTableViewController::textFieldDidFinishEditing(Escher::TextField
   return true;
 }
 
+// See TODO from GoodnessTableViewController::deleteSelectedValue
 void HomogeneityTableViewController::deleteSelectedValue() {
-  int row = m_table.selectedRow(), col = m_table.selectedColumn();
+  int row = tableView()->selectedRow(), col = tableView()->selectedColumn();
   assert(row > 0 && col > 0);
+  // Remove value
   bool shouldDeleteRowOrCol = m_statistic->deleteParameterAtPosition(row - 1, col - 1);
   if (!shouldDeleteRowOrCol) {
-    m_table.reloadCellAtLocation(col, row);
+    // Only one cell needs to reload.
+    assert(row <= m_innerTableData.numberOfRows() && col <= m_innerTableData.numberOfColumns());
+    tableView()->reloadCellAtLocation(col, row);
   } else {
-    // A row and/or column has been deleted
+    // A row and/or column has been deleted, we should recompute data
     m_statistic->recomputeData();
-    /* Due to an initial number of rows/cols of 2, we cannot ensure that at most
+    /* Due to an initial number of rows/ols of 2, we cannot ensure that at most
      * one row and one col have been deleted here */
-    m_table.deselectTable();
+    tableView()->deselectTable();
     if (!m_innerTableData.recomputeDimensions()) {
       /* A row/col has been deleted, but size didn't change, meaning the number
        * of non-empty rows/cols was at maximum. However, recomputeData may have
        * moved up/left multiple cells, m_inputTableView should be reloaded. */
-      m_table.reloadData(false);
+      tableView()->reloadData(false);
     }
-    m_table.selectCellAtClippedLocation(col, row, false);
+    tableView()->selectCellAtClippedLocation(col, row, false);
   }
 }
 

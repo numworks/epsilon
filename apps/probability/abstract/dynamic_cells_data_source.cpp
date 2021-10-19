abc92fb1b36ec809e450e1a730875d0d846f0013
Émilie Feral <emilie.feral@numworks.com>
2021-10-19 15:38:29 +0200

    [apps] Prevent from crashing on dereferencing nullptr

--- apps/probability/abstract/dynamic_cells_data_source.cpp
+++ apps/probability/abstract/dynamic_cells_data_source.cpp
@@ -40,8 +40,9 @@ void DynamicCellsDataSource<T,N>::destroyCells() {
    * virtualization here but we didn't - we don't want to call a derived
    * destructor of children T class. */
   for (int i = 0; i < N; i++) {
+    // Make sure not to keep the first responder pointing on a destroyed cell
     Responder * cellResponder = m_cells[i].responder();
-    if (cellResponder->commonAncestorWith(App::app()->firstResponder()) == cellResponder) {
+    if (cellResponder && cellResponder->commonAncestorWith(App::app()->firstResponder()) == cellResponder) {
       App::app()->setFirstResponder(cellResponder->parentResponder());
     }
     m_cells[i].T::~T();

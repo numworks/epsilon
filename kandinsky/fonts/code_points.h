186a5a1de356e6a957a9e224ae6ac720e12f7291
Emilie Feral <emilie.feral@numworks.com>
2021-11-03 11:38:38 +0100

    [kandinsky] Add epsilon codepoint

--- kandinsky/fonts/code_points.h
+++ kandinsky/fonts/code_points.h
@@ -138,6 +138,7 @@ uint32_t CodePoints[] = {
   0x394,   // Δ // GREEK CAPITAL LETTER DELTA
   0x3a9,   // Ω // GREEK CAPITAL LETTER OMEGA
   0x3b1,   // α // GREEK SMALL LETTER ALPHA
+  0x3b5,   // ε // GREEK SMALL LETTER EPSILON
   0x3b8,   // θ // GREEK SMALL LETTER THETA
   0x3bb,   // λ // GREEK SMALL LETTER LAMDA
   0x3bc,   // μ // GREEK SMALL LETTER MU

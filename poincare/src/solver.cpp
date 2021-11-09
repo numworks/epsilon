f73ab477989304bf07a0a921921b2b7bce22591e
Emilie Feral <emilie.feral@numworks.com>
2021-11-09 16:50:48 +0100

    [poincare] Fix double comparison

--- poincare/src/solver.cpp
+++ poincare/src/solver.cpp
@@ -182,7 +182,7 @@ double Solver::BrentRoot(double ax, double bx, double precision, ValueAtAbscissa
   double d = b-a;
   double e = b-a;
   double fa = evaluation(a, context, auxiliary);
-  if (fa == 0) {
+  if (fa == 0.0) {
     // We are looking for a root. If a is already a root, just return it.
     return a;
   }

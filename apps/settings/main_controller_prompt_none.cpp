e761c5821f0b0d8590aae2f2121c19ec89060f8f
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-11-12 10:39:01 +0100

    [apps/settings] Clean-up remaining TODOs

--- apps/settings/main_controller_prompt_none.cpp
+++ apps/settings/main_controller_prompt_none.cpp
@@ -4,6 +4,21 @@
 
 namespace Settings {
 
+constexpr MessageTree s_modelMenu[] =
+  { MessageTree(I18n::Message::AngleUnit, s_modelAngleChildren),
+    MessageTree(I18n::Message::DisplayMode, s_modelFloatDisplayModeChildren),
+    MessageTree(I18n::Message::EditionMode, s_modelEditionModeChildren),
+    MessageTree(I18n::Message::ComplexFormat, s_modelComplexFormatChildren),
+    MessageTree(I18n::Message::Brightness),
+    MessageTree(I18n::Message::FontSizes, s_modelFontChildren),
+    MessageTree(I18n::Message::Language),
+    MessageTree(I18n::Message::Country),
+    MessageTree(I18n::Message::ExamMode, s_modelExamChildren),
+    MessageTree(I18n::Message::PressToTest),
+    MessageTree(I18n::Message::TestMode, s_modelTestModeMenu),
+    MessageTree(I18n::Message::About, s_modelAboutChildren),
+    MessageTree(I18n::Message::ResetCalculator)};
 
+constexpr MessageTree s_model = MessageTree(I18n::Message::SettingsApp, s_modelMenu);
 
 }

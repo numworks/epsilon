7e3ec96f9704566f048d1d767d40c4e8300cee8c
Emilie Feral <emilie.feral@numworks.com>
2021-11-18 12:09:14 +0100

    [poincare] Fix Float::sign to take into accound NAN values

--- poincare/include/poincare/float.h
+++ poincare/include/poincare/float.h
@@ -37,7 +37,7 @@ public:
 
   // Properties
   Type type() const override { return (sizeof(T) == sizeof(float)) ? Type::Float : Type::Double; }
-  Sign sign(Context * context) const override { return m_value < 0 ? Sign::Negative : Sign::Positive; }
+  Sign sign(Context * context) const override { return std::isnan(m_value) ? Sign::Unknown : (m_value < 0 ? Sign::Negative : Sign::Positive); }
   NullStatus nullStatus(Context * context) const override { return m_value == 0.0 ? NullStatus::Null : NullStatus::NonNull; }
   Expression setSign(Sign s, ReductionContext reductionContext) override;
   int simplificationOrderSameType(const ExpressionNode * e, bool ascending, bool ignoreParentheses) const override;

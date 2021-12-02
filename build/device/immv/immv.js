37425abe8dc0eace41a736f6588a52e4217384e3
Romain Goyet <romain.goyet@numworks.com>
2021-12-02 17:21:21 -0500

    [build/device/immv] Data can be reloaded

--- build/device/immv/immv.js
+++ build/device/immv/immv.js
@@ -1,7 +1,28 @@
-function InteractiveMemoryMapViewer(chartNodeSelector, legendNodeSelector, data) {
+function InteractiveMemoryMapViewer(chartNodeSelector, legendNodeSelector) {
   let gapRemovalRatio = 0.5
   let equalizingRatio = 0.5
   let fontHeight = 10 // In pixels
+  let source = undefined
+  let width = 0
+  let height = 0
+  let svg = undefined
+  let yDomAxis = undefined
+
+  function NumberToHumanBytes(number) {
+    if (number > 1024*1024*1024) {
+      return `${Math.round(number/(1024*1024*1024))} GB`
+    } else if (number > 1024*1024) {
+      return `${Math.round(number/(1024*1024))} MB`
+    } else if (number > 1024) {
+      return `${Math.round(number/1024)} KB`
+    } else {
+      return `${Math.round(number)} B`
+    }
+  }
+
+  function NumberToHumanAddress(number) {
+    return `0x${number.toString(16).toUpperCase()}`
+  }
 
   function compressedScaleFor(zone, gapRemovalRatio, equalizingRatio, rangeMin, rangeMax) {
     if (!zone.zones) {
@@ -169,186 +190,6 @@ function InteractiveMemoryMapViewer(chartNodeSelector, legendNodeSelector, data)
     return filteredTicks
   }
 
-  this.setGapRemovalRatio = function(ratio, transitionDuration) {
-    gapRemovalRatio = ratio
-    redraw(transitionDuration)
-  }
-
-  this.setEqualizingRatio = function(ratio, transitionDuration) {
-    equalizingRatio = ratio
-    redraw(transitionDuration)
-  }
-
-  let chartNode = document.querySelector(chartNodeSelector)
-  let legendNode = d3.select(legendNodeSelector)
-
-  // set the dimensions and margins of the graph
-  let margin = {top: 30, right: 30, bottom: 70, left: 100};
-  let width = chartNode.scrollWidth - margin.left - margin.right
-  let height = chartNode.scrollHeight - margin.top - margin.bottom
-
-  // append the svg object to the body of the page
-  let svg = d3.select(chartNodeSelector)
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  let cursor = svg
-    .append("rect")
-    .attr("class", "cursor")
-    .attr("x", "0")
-    .attr("y", "0")
-    .attr("width", width)
-    .attr("height", "0.1")
-  let cursorLegend = svg
-    .append("text")
-    .attr("text-anchor", "end")
-
-  let source = data[0]
-
-  let xScale = d3.scaleLinear().domain([0,1]).range([0, width])
-  let yScale = compressedScaleFor(source, gapRemovalRatio, equalizingRatio, 0, height)
-
-  let yAxis = d3.axisLeft(yScale)
-
-  let yDomAxis = svg.append("g")
-    //.attr("transform", "translate(0," + height + ")")
-    .call(yAxis)
-
-  // We need to create a rect to capture mouse events. We can't bind mousemove on svg
-  let pointerEventCatcher = svg
-    .append('rect')
-    .style("fill", "none")
-    .style("pointer-events", "all")
-    .attr('width', width)
-    .attr('height', height)
-
-  let selectionOrigin = undefined;
-  let selectionEnd = undefined;
-  pointerEventCatcher
-    .on("mousedown", function(e) {
-      let y = Math.max(Math.min(height,d3.pointer(e)[1]),0)
-      selectionOrigin = Math.round(yScale.invert(y))
-    })
-    .on("mouseup", function(e) {
-      cursor.attr("y", yScale(selectionEnd));
-      cursor.attr("height", "0.1");
-      source = {start: selectionOrigin, end: selectionEnd};
-      selectionOrigin = undefined;
-      selectionEnd = undefined;
-      redraw();
-    })
-  svg
-    .on("mousemove", function(e) {
-      let y = Math.max(Math.min(height,d3.pointer(e)[1]),0)
-      selectionEnd = Math.round(yScale.invert(y))
-      if (selectionOrigin) {
-        cursor.attr("y", Math.min(yScale(selectionOrigin), yScale(selectionEnd)));
-        cursor.attr("height", Math.abs(yScale(selectionEnd)-yScale(selectionOrigin)));
-      } else {
-        cursor.attr("y", y);
-        cursor.attr("height", "0.1");
-      }
-      cursorLegend
-        .attr("transform", `translate(${width}, ${y})`)
-        .text(NumberToHumanAddress(selectionEnd));
-    });
-
-
-  let maps = svg
-    .selectAll('g.maps')
-    .data(data)
-    .enter()
-    .append('g')
-    .attr("class", "maps")
-
-  maps.nodes()[0].classList.add("selected") // Mark first map as selected
-
-  let tabs = maps.append("g")
-  tabs
-    .append("rect")
-    .attr("class", "label")
-    .attr("width", "100")
-    .attr("height", "20")
-    .attr("y", height+10)
-  tabs
-    .append("text")
-    .text(d => d.name)
-    .attr("transform", `translate(5,${height+25})`)
-  tabs
-    .on("click", function(ev, datum) {
-      for (const map of this.parentElement.parentElement.querySelectorAll(".maps")) {
-        map.classList.remove("selected")
-      }
-      this.parentElement.classList.add("selected")
-      source = datum;
-      redraw(300);
-    })
-
-  let zones = maps
-    .attr("transform", function(d, i) { return `translate(${150*i},0)`; })
-    .selectAll('g.zone')
-    .data(function(d){
-      return d.zones;
-    })
-    .enter()
-    .append('g')
-    .attr("class", "zone")
-
-  function NumberToHumanBytes(number) {
-    if (number > 1024*1024*1024) {
-      return `${Math.round(number/(1024*1024*1024))} GB`
-    } else if (number > 1024*1024) {
-      return `${Math.round(number/(1024*1024))} MB`
-    } else if (number > 1024) {
-      return `${Math.round(number/1024)} KB`
-    } else {
-      return `${Math.round(number)} B`
-    }
-  }
-
-  function NumberToHumanAddress(number) {
-    return `0x${number.toString(16).toUpperCase()}`
-  }
-
-  zones.append("rect")
-    .attr("width", function(d) { return 100; })
-    .on("mouseover", function(element, datum) {
-      let legend = ''
-      legend += '<table>'
-      legend += '  <tr>'
-      legend += '    <th>Name</th>'
-      legend += '    <td><code>' + datum.name + '</code></td>'
-      legend += '  </tr>'
-      legend += '  <tr>'
-      legend += '    <th>Size</th>'
-      legend += '    <td><code>' + NumberToHumanBytes(datum.end-datum.start) + ' (' + (datum.end-datum.start).toLocaleString() + ' bytes)' + '</code></td>'
-      legend += '  </tr>'
-      legend += '  <tr>'
-      legend += '    <th>Start</th>'
-      legend += '    <td><code>' + NumberToHumanAddress(datum.start) + '</code></td>'
-      legend += '  </tr>'
-      legend += '  <tr>'
-      legend += '    <th>End</th>'
-      legend += '    <td><code>' + NumberToHumanAddress(datum.end) + '</code></td>'
-      legend += '  </tr>'
-      legend += '</table'
-      legendNode.html(legend)
-    })
-    .on("mouseout", function(element, datum) {
-      legendNode.html("")
-    })
-    .on("click", function(element, datum) {
-      console.log(datum)
-      source = datum
-      redraw(300)
-    })
-
-  zones.append("text")
-    .text(d => d.name)
-
   function redraw(transitionDuration = 0) {
     yScale = compressedScaleFor(source, gapRemovalRatio, equalizingRatio, height, 0)
     let maps = svg.selectAll("g.maps");
@@ -384,5 +225,179 @@ function InteractiveMemoryMapViewer(chartNodeSelector, legendNodeSelector, data)
       .call(yAxis.scale(yScale));
   }
 
-  redraw(0)
+  this.setGapRemovalRatio = function(ratio, transitionDuration) {
+    gapRemovalRatio = ratio
+    redraw(transitionDuration)
+  }
+
+  this.setEqualizingRatio = function(ratio, transitionDuration) {
+    equalizingRatio = ratio
+    redraw(transitionDuration)
+  }
+
+  this.load = function(data) {
+    let chartNode = document.querySelector(chartNodeSelector)
+    let legendNode = d3.select(legendNodeSelector)
+
+    // set the dimensions and margins of the graph
+    let margin = {top: 30, right: 30, bottom: 70, left: 100};
+    width = chartNode.scrollWidth - margin.left - margin.right
+    height = chartNode.scrollHeight - margin.top - margin.bottom
+    let columnMargin = 20
+    let numberOfColumns = data.length
+    columnWidth = (width/numberOfColumns)-columnMargin
+
+    // append the svg object to the body of the page
+    if (svg) {
+      d3.select(chartNodeSelector).select("svg").remove()
+    }
+    svg = d3.select(chartNodeSelector)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let cursor = svg
+      .append("rect")
+      .attr("class", "cursor")
+      .attr("x", "0")
+      .attr("y", "0")
+      .attr("width", width)
+      .attr("height", "0.1")
+    let cursorLegend = svg
+      .append("text")
+      .attr("text-anchor", "end")
+
+    source = data[0]
+
+    let xScale = d3.scaleLinear().domain([0,1]).range([0, width])
+    let yScale = d3.scaleLinear() // Placeholder
+    //let yScale = compressedScaleFor(source, gapRemovalRatio, equalizingRatio, 0, height)
+
+    let yAxis = d3.axisLeft(yScale)
+
+    yDomAxis = svg.append("g")
+    //.attr("transform", "translate(0," + height + ")")
+      .call(yAxis)
+
+    // We need to create a rect to capture mouse events. We can't bind mousemove on svg
+    let pointerEventCatcher = svg
+      .append('rect')
+      .style("fill", "none")
+      .style("pointer-events", "all")
+      .attr('width', width)
+      .attr('height', height)
+
+    let selectionOrigin = undefined;
+    let selectionEnd = undefined;
+    pointerEventCatcher
+      .on("mousedown", function(e) {
+        let y = Math.max(Math.min(height,d3.pointer(e)[1]),0)
+        selectionOrigin = Math.round(yScale.invert(y))
+      })
+      .on("mouseup", function(e) {
+        cursor.attr("y", yScale(selectionEnd));
+        cursor.attr("height", "0.1");
+        source = {start: selectionOrigin, end: selectionEnd};
+        selectionOrigin = undefined;
+        selectionEnd = undefined;
+        redraw();
+      })
+    svg
+      .on("mousemove", function(e) {
+        let y = Math.max(Math.min(height,d3.pointer(e)[1]),0)
+        selectionEnd = Math.round(yScale.invert(y))
+        if (selectionOrigin) {
+          cursor.attr("y", Math.min(yScale(selectionOrigin), yScale(selectionEnd)));
+          cursor.attr("height", Math.abs(yScale(selectionEnd)-yScale(selectionOrigin)));
+        } else {
+          cursor.attr("y", y);
+          cursor.attr("height", "0.1");
+        }
+        cursorLegend
+          .attr("transform", `translate(${width}, ${y})`)
+          .text(NumberToHumanAddress(selectionEnd));
+      });
+
+
+    let maps = svg
+      .selectAll('g.maps')
+      .data(data)
+      .enter()
+      .append('g')
+      .attr("class", "maps")
+
+    maps.nodes()[0].classList.add("selected") // Mark first map as selected
+
+    let tabs = maps.append("g")
+    tabs
+      .append("rect")
+      .attr("class", "label")
+      .attr("width", columnWidth)
+      .attr("height", "20")
+      .attr("y", height+10)
+    tabs
+      .append("text")
+      .text(d => d.name)
+      .attr("transform", `translate(5,${height+25})`)
+    tabs
+      .on("click", function(ev, datum) {
+        for (const map of this.parentElement.parentElement.querySelectorAll(".maps")) {
+          map.classList.remove("selected")
+        }
+        this.parentElement.classList.add("selected")
+        source = datum;
+        redraw(300);
+      })
+
+    let zones = maps
+      .attr("transform", function(d, i) { return `translate(${(columnWidth+columnMargin)*i},0)`; })
+      .selectAll('g.zone')
+      .data(function(d){
+        return d.zones;
+      })
+      .enter()
+      .append('g')
+      .attr("class", "zone")
+
+
+    zones.append("rect")
+      .attr("width", columnWidth)
+      .on("mouseover", function(element, datum) {
+        let legend = ''
+        legend += '<table>'
+        legend += '  <tr>'
+        legend += '    <th>Name</th>'
+        legend += '    <td><code>' + datum.name + '</code></td>'
+        legend += '  </tr>'
+        legend += '  <tr>'
+        legend += '    <th>Size</th>'
+        legend += '    <td><code>' + NumberToHumanBytes(datum.end-datum.start) + ' (' + (datum.end-datum.start).toLocaleString() + ' bytes)' + '</code></td>'
+        legend += '  </tr>'
+        legend += '  <tr>'
+        legend += '    <th>Start</th>'
+        legend += '    <td><code>' + NumberToHumanAddress(datum.start) + '</code></td>'
+        legend += '  </tr>'
+        legend += '  <tr>'
+        legend += '    <th>End</th>'
+        legend += '    <td><code>' + NumberToHumanAddress(datum.end) + '</code></td>'
+        legend += '  </tr>'
+        legend += '</table'
+        legendNode.html(legend)
+      })
+      .on("mouseout", function(element, datum) {
+        legendNode.html("")
+      })
+      .on("click", function(element, datum) {
+        console.log(datum)
+        source = datum
+        redraw(300)
+      })
+
+    zones.append("text")
+      .text(d => d.name)
+
+    redraw(0)
+  }
 }

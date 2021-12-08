6a7e2bddefc505dfd9dcbf8d6940b9c18d8d7cd4
Emilie Feral <emilie.feral@numworks.com>
2021-12-08 10:12:48 +0100

    [apps/graph] When a function is being deleted, ListParameter::numberOfRows (called when the SelectableTableView exits the responder chain) can still call DomainParameterController::isVisible and TypeParameterController::detailsNumberOfSections. We should handle these special cases.
    This fixes the following bug: add an empty function, go to the
    ListParameter, delete the function and return on the main list (this
    would eventually crash).

--- apps/graph/list/domain_parameter_controller.h
+++ apps/graph/list/domain_parameter_controller.h
@@ -25,7 +25,7 @@ public:
   void willDisplayCellForIndex(Escher::HighlightCell * cell, int index) override;
 
   void setRecord(Ion::Storage::Record record) { m_record = record; }
-  bool isVisible() const { return function()->canHaveCustomDomain(); }
+  bool isVisible() const { return m_record.isNull() ? 0 : function()->canHaveCustomDomain(); }
 private:
   constexpr static int k_totalNumberOfCell = 2;
   void viewWillAppear() override;

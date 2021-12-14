9e17911d148e06fa0f2a0d21c3e07158f338556d
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2021-12-14 15:37:07 +0100

    [poincare/matrix_layout] Remove matrix layout with one element
    If a matrix has only one or no element, and the user presses BACKSPACE
    at the beginning of the top left element, the matrix is removed, leaving
    only its sole element if any.

--- poincare/test/layout_cursor.cpp
+++ poincare/test/layout_cursor.cpp
@@ -83,31 +83,52 @@ QUIZ_CASE(poincare_layout_cursor_delete) {
    * --- -> "BackSpace" -> 12|34
    * |34
    * */
-  HorizontalLayout layout1 = HorizontalLayout::Builder(
-      FractionLayout::Builder(
-        LayoutHelper::String("12", 2),
-        LayoutHelper::String("34", 2)
-      )
-    );
-  LayoutCursor cursor1(layout1.childAtIndex(0).childAtIndex(1), LayoutCursor::Position::Left);
-  cursor1.performBackspace();
-  assert_layout_serialize_to(layout1, "1234");
-  quiz_assert(cursor1.isEquivalentTo(LayoutCursor(layout1.childAtIndex(1), LayoutCursor::Position::Right)));
+  {
+    HorizontalLayout layout = HorizontalLayout::Builder(
+        FractionLayout::Builder(
+          LayoutHelper::String("12", 2),
+          LayoutHelper::String("34", 2)
+          )
+        );
+    LayoutCursor cursor(layout.childAtIndex(0).childAtIndex(1), LayoutCursor::Position::Left);
+    cursor.performBackspace();
+    assert_layout_serialize_to(layout, "1234");
+    quiz_assert(cursor.isEquivalentTo(LayoutCursor(layout.childAtIndex(1), LayoutCursor::Position::Right)));
+  }
 
   /*      ø
    * 1 + --- -> "BackSpace" -> 1+|3
    *     |3
    * */
-  HorizontalLayout layout2 = HorizontalLayout::Builder(
-      CodePointLayout::Builder('1'),
-      CodePointLayout::Builder('+'),
-      FractionLayout::Builder(
-        EmptyLayout::Builder(),
-        CodePointLayout::Builder('3')
-      )
-    );
-  LayoutCursor cursor2(layout2.childAtIndex(2).childAtIndex(1), LayoutCursor::Position::Left);
-  cursor2.performBackspace();
-  assert_layout_serialize_to(layout2, "1+3");
-  quiz_assert(cursor2.isEquivalentTo(LayoutCursor(layout2.childAtIndex(1), LayoutCursor::Position::Right)));
+  {
+    HorizontalLayout layout = HorizontalLayout::Builder(
+        CodePointLayout::Builder('1'),
+        CodePointLayout::Builder('+'),
+        FractionLayout::Builder(
+          EmptyLayout::Builder(),
+          CodePointLayout::Builder('3')
+          )
+        );
+    LayoutCursor cursor(layout.childAtIndex(2).childAtIndex(1), LayoutCursor::Position::Left);
+    cursor.performBackspace();
+    assert_layout_serialize_to(layout, "1+3");
+    quiz_assert(cursor.isEquivalentTo(LayoutCursor(layout.childAtIndex(1), LayoutCursor::Position::Right)));
+  }
+
+  /*
+   * [[|1,▒][▒,▒]] -> "Backspace" -> |1
+   * */
+  {
+    HorizontalLayout layout = HorizontalLayout::Builder(
+        MatrixLayout::Builder(
+          CodePointLayout::Builder('1'),
+          EmptyLayout::Builder(),
+          EmptyLayout::Builder(),
+          EmptyLayout::Builder()
+          ));
+    LayoutCursor cursor(layout.childAtIndex(0).childAtIndex(0), LayoutCursor::Position::Left);
+    cursor.performBackspace();
+    assert_layout_serialize_to(layout, "1");
+    quiz_assert(cursor.isEquivalentTo(LayoutCursor(layout.childAtIndex(0), LayoutCursor::Position::Left)));
+  }
 }

0fbaae0fb7f4eff9b8383b46dc6e87c51e9b7b74
Emilie Feral <emilie.feral@numworks.com>
2021-12-14 15:06:18 +0100

    [ion] ExternalFlash: shutdown interruptions while writing/erasing external flash. isr_handler is located on external flash and can't be executed while erasing/writing is ongoing

--- ion/src/device/n0110/kernel/drivers/external_flash.cpp
+++ ion/src/device/n0110/kernel/drivers/external_flash.cpp
@@ -1,3 +1,4 @@
+#include <kernel/drivers/board.h>
 #include <shared/drivers/external_flash_write.h>
 #include <shared/drivers/trampoline.h>
 #include <assert.h>
@@ -6,13 +7,25 @@ namespace Ion {
 namespace Device {
 namespace ExternalFlash {
 
+TRAMPOLINE_INTERFACE(TRAMPOLINE_EXTERNAL_FLASH_ERASE_SECTOR, PrivateEraseSector, (i), void, int i)
+TRAMPOLINE_INTERFACE(TRAMPOLINE_EXTERNAL_FLASH_WRITE_MEMORY, PrivateWriteMemory, (destination, source, length), void, uint8_t * destination, const uint8_t * source, size_t length)
+
 void MassErase() {
   // Mass erase is not enabled on kernel
   assert(false);
 }
 
-TRAMPOLINE_INTERFACE(TRAMPOLINE_EXTERNAL_FLASH_ERASE_SECTOR, EraseSector, (i), void, int i)
-TRAMPOLINE_INTERFACE(TRAMPOLINE_EXTERNAL_FLASH_WRITE_MEMORY, WriteMemory, (destination, source, length), void, uint8_t * destination, const uint8_t * source, size_t length)
+void WriteMemory(uint8_t * destination, const uint8_t * source, size_t length) {
+  Board::shutdownInterruptions();
+  PrivateWriteMemory(destination, source, length);
+  Board::initInterruptions();
+}
+
+void EraseSector(int i) {
+  Board::shutdownInterruptions();
+  PrivateEraseSector(i);
+  Board::initInterruptions();
+}
 
 }
 }

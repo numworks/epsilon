30fe4b6720afeec31e84dff5f671bd3be637a8dd
Emilie Feral <emilie.feral@numworks.com>
2021-12-15 11:07:27 +0100

    [apps][ion] Change name: Board::heapRange --> Board::appSandboxRange

--- ion/src/device/userland/boot/isr.cpp
+++ ion/src/device/userland/boot/isr.cpp
@@ -10,6 +10,6 @@ typedef void (*ISR)(void);
 ISR InitialisationVector[] __attribute__((section(".isr_vector_table"), used)) = {
   reinterpret_cast<ISR>(&_process_stack_start), // Stack start
   start, // Reset service routine,
-  reinterpret_cast<ISR>(Ion::Device::Board::heapRange),
+  reinterpret_cast<ISR>(Ion::Device::Board::appSandboxRange),
   reinterpret_cast<ISR>(Ion::Display::drawString)
 };

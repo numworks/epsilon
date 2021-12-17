997ec49c16f5eefe00ddd959474e9e26c15892a9
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2021-12-17 18:46:40 +0100

    [apps/solver] Add simple & compound interest menus

---
+++ apps/solver/controllers/simple_interest_controller.h
@@ -0,0 +1,34 @@
+#ifndef SOLVER_CONTROLLERS_SIMPLE_INTEREST_CONTROLLER_H
+#define SOLVER_CONTROLLERS_SIMPLE_INTEREST_CONTROLLER_H
+
+#include <escher/message_table_cell_with_chevron_and_message.h>
+#include <escher/stack_view_controller.h>
+#include <ion/events.h>
+
+#include "../gui/selectable_cell_list_controller.h"
+
+namespace Solver {
+
+// TODO Hugo : Add "Choose a parameter to solve" intro message
+
+constexpr static int k_numberOfSimpleInterestCells = 4;
+
+class SimpleInterestController : public SelectableCellListPage<Escher::MessageTableCellWithChevronAndMessage,
+                                                     k_numberOfSimpleInterestCells> {
+public:
+  SimpleInterestController(Escher::StackViewController * parentResponder);
+  void didBecomeFirstResponder() override;
+  bool handleEvent(Ion::Events::Event e) override;
+  const char * title() override { return I18n::translate(I18n::Message::SimpleInterest); }
+  ViewController::TitlesDisplay titlesDisplay() override { return ViewController::TitlesDisplay::DisplayLastTitle; }
+  int numberOfRows() const override { return k_numberOfSimpleInterestCells; }
+
+  constexpr static int k_indexOfN = 0;
+  constexpr static int k_indexOfRPct = 1;
+  constexpr static int k_indexOfP = 2;
+  constexpr static int k_indexOfI = 3;
+};
+
+}  // namespace Solver
+
+#endif /* SOLVER_CONTROLLERS_SIMPLE_INTEREST_CONTROLLER_H */

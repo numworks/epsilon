bd714a6b47ab56e79315482e4326d9ea473cebcd
Emilie Feral <emilie.feral@numworks.com>
2022-01-04 12:04:30 +0100

    [ion] Factorize PCB versions reading between N0110 and N0120

--- ion/src/device/shared/drivers/board_unprivileged_standalone.cpp
+++ ion/src/device/shared/drivers/board_unprivileged_standalone.cpp
@@ -14,6 +14,17 @@ UserlandHeader * userlandHeader() {
   return reinterpret_cast<UserlandHeader *>(Config::UserlandStartAddress);
 }
 
+/* The following methods regarding PCB version are dummy implementations.
+ * Handling the PCB version is only necessary on the N0110 and N0120. */
+
+PCBVersion pcbVersion() {
+  return PCB_LATEST;
+}
+
+PCBVersion readPCBVersionInMemory() {
+  return PCB_LATEST;
+}
+
 }
 }
 }

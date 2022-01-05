8ba2b0bbe87063447b40750ef5276476d9e387e3
Emilie Feral <emilie.feral@numworks.com>
2022-01-05 11:22:00 +0100

    [ion] Remove useless keyword 'static'

--- ion/src/device/include/n0110/config/internal_flash.h
+++ ion/src/device/include/n0110/config/internal_flash.h
@@ -9,12 +9,12 @@ namespace Device {
 namespace InternalFlash {
 namespace Config {
 
-static constexpr int NumberOfForbiddenFirstSectors = 2;
-static constexpr uint32_t StartAddress = 0x08000000;
-static constexpr uint32_t EndAddress = 0x08010000;
-static constexpr uint32_t TotalSize = EndAddress - StartAddress;
-static constexpr int NumberOfSectors = 4;
-static constexpr uint32_t SectorAddresses[NumberOfSectors+1] = {
+constexpr int NumberOfForbiddenFirstSectors = 2;
+constexpr uint32_t StartAddress = 0x08000000;
+constexpr uint32_t EndAddress = 0x08010000;
+constexpr uint32_t TotalSize = EndAddress - StartAddress;
+constexpr int NumberOfSectors = 4;
+constexpr uint32_t SectorAddresses[NumberOfSectors+1] = {
   0x08000000, 0x08004000, 0x08008000, 0x0800C000,
   0x08010000
 };
@@ -24,10 +24,10 @@ static constexpr uint32_t SectorAddresses[NumberOfSectors+1] = {
 constexpr Regs::FLASH::CR::PSIZE MemoryAccessWidth = Regs::FLASH::CR::PSIZE::X32;
 typedef uint32_t MemoryAccessType;
 
-static constexpr uint32_t OTPStartAddress = 0x1FF07800;
-static constexpr uint32_t OTPLocksAddress = 0x1FF07A00;
-static constexpr int NumberOfOTPBlocks = 16;
-static constexpr uint32_t OTPBlockSize = 0x20;
+constexpr uint32_t OTPStartAddress = 0x1FF07800;
+constexpr uint32_t OTPLocksAddress = 0x1FF07A00;
+constexpr int NumberOfOTPBlocks = 16;
+constexpr uint32_t OTPBlockSize = 0x20;
 constexpr uint32_t OTPAddress(int block, int index) { return OTPStartAddress + block * OTPBlockSize + index * sizeof(uint32_t); };
 constexpr uint32_t OTPLockAddress(int block) { return OTPLocksAddress + block; }
 

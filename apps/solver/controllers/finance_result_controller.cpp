4d273f31cfe0b8d0e06fa3374e2e7e18da33a280
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-01-06 09:55:19 +0100

    [apps/solver] Add title for finance result

--- apps/solver/controllers/finance_result_controller.cpp
+++ apps/solver/controllers/finance_result_controller.cpp
@@ -46,12 +46,61 @@ bool FinanceResultController::handleEvent(Ion::Events::Event event) {
   return true;
 }
 
+/* TODO Hugo : Improve Probability::CalculationController::updateTitle, which
+ * may crash if text does not fit*/
 const char * FinanceResultController::title() {
-  // SimpleInterestParameter unknownParam = simpleInterestData()->getUnknown();
-  // Poincare::Print::customPrintf(m_titleBuffer, k_titleBufferSize,
-  //   I18n::translate(I18n::Message::FinanceSolving),
-  //   I18n::translate(SimpleInterestData::LabelForParameter(unknownParam)),
-  //   I18n::translate(SimpleInterestData::SublabelForParameter(unknownParam)));
-  // return m_titleBuffer;
-  return "TODO";
+  // Try fitting the known parameters values in the title
+  // We don't display the boolean parameter and the unknown one.
+  int totalParameters = (m_data->isSimpleInterest ? k_numberOfSimpleInterestParameters : k_numberOfCompoundInterestParameters) - 2;
+  // Using a minimal precision
+  constexpr int precision = Poincare::Preferences::ShortNumberOfSignificantDigits;
+  constexpr int bufferSize = Poincare::PrintFloat::charSizeForFloatsWithPrecision(precision);
+  char valueBuffer[bufferSize];
+  // Last char index before the text does not fit
+  int charIndex = 0;
+  int paramIndex = 0;
+  int unknownIndex = m_data->isSimpleInterest ? static_cast<int>(simpleInterestData()->getUnknown()) : static_cast<int>(compoundInterestData()->getUnknown());
+  for (int i = 0; i < totalParameters; i++) {
+    if (paramIndex == unknownIndex) {
+      // Avoid unknown parameter
+      paramIndex += 1;
+    }
+    // Parameter's name
+    I18n::Message label = m_data->isSimpleInterest ?
+      SimpleInterestData::LabelForParameter(static_cast<SimpleInterestParameter>(paramIndex)) :
+      CompoundInterestData::LabelForParameter(static_cast<CompoundInterestParameter>(paramIndex));
+    const char * labelBuffer = I18n::translate(label);
+    // Parameter's value
+    double value = m_data->isSimpleInterest ?
+      m_data->m_data.m_simpleInterestData.getValue(static_cast<SimpleInterestParameter>(paramIndex)) :
+      m_data->m_data.m_compoundInterestData.getValue(static_cast<CompoundInterestParameter>(paramIndex));
+    Shared::PoincareHelpers::ConvertFloatToTextWithDisplayMode<double>(value, valueBuffer, bufferSize, precision, Poincare::Preferences::PrintFloatMode::Decimal);
+    // Expected length
+    int nextLength = charIndex + strlen(labelBuffer) + strlen("=") + strlen(valueBuffer);
+    if (charIndex > 0) {
+      // Add a space between chars
+      nextLength += strlen(" ");
+    }
+    if (paramIndex != totalParameters) {
+      // If it is not the last parameter, ensure a "..." fits after anyway
+      nextLength += strlen("...");
+    }
+    if (nextLength >= k_titleBufferSize) {
+      // Text does not fit, insert "..."
+      charIndex += strlcpy(m_titleBuffer + charIndex, "...", k_titleBufferSize - charIndex);
+      break;
+    }
+    if (charIndex > 0) {
+      // Insert a space in between parameters
+      charIndex += strlcpy(m_titleBuffer + charIndex, " ", k_titleBufferSize - charIndex);
+    }
+    // Insert label, '=' and value
+    charIndex += strlcpy(m_titleBuffer + charIndex, labelBuffer, k_titleBufferSize - charIndex);
+    charIndex += strlcpy(m_titleBuffer + charIndex, "=", k_titleBufferSize - charIndex);
+    charIndex += strlcpy(m_titleBuffer + charIndex, valueBuffer, k_titleBufferSize - charIndex);
+    paramIndex += 1;
+    assert(charIndex < k_titleBufferSize);
+  }
+  m_titleBuffer[charIndex] = 0;
+  return m_titleBuffer;
 }

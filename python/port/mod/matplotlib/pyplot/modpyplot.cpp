37f01b5d3935c7ee1711625b6c08be728088a0f7
Emilie Feral <emilie.feral@numworks.com>
2022-01-06 14:59:55 +0100

    [python] Fix MicroPython dependencies

--- python/port/mod/matplotlib/pyplot/modpyplot.cpp
+++ python/port/mod/matplotlib/pyplot/modpyplot.cpp
@@ -144,12 +144,7 @@ mp_obj_t modpyplot_axis(size_t n_args, const mp_obj_t *args) {
       } else {
         mp_raise_ValueError("Unrecognized string given to axis; try 'on', 'off' or 'auto'");
       }
-#if MICROPY_OBJ_IMMEDIATE_OBJS
-/* This couldn't be done at the time of writing because mp_obj_is_bool didn't
- * exist just yet. */
-#error Use mp_obj_is_bool instead of mp_obj_is_type
-#endif
-    } else if (mp_obj_is_type(arg, &mp_type_bool)) {
+    } else if (mp_obj_is_bool(arg)) {
       sPlotStore->setAxesRequested(mp_obj_is_true(arg));
     } else if (mp_obj_is_type(arg, &mp_type_tuple) || mp_obj_is_type(arg, &mp_type_list)) {
       mp_obj_t * items;

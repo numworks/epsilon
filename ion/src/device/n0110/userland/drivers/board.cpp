e404ea9bdc2d5fc9eb05b573ce16a1fe230b346e
Emilie Feral <emilie.feral@numworks.com>
2022-01-07 15:13:54 +0100

    [ion] Complete userland header with RAM range useable by external apps (change name "storage flash" --> "external apps flash")

--- ion/src/device/n0110/userland/drivers/board.cpp
+++ ion/src/device/n0110/userland/drivers/board.cpp
@@ -2,7 +2,7 @@
 #include <shared/drivers/config/board.h>
 
 extern "C" {
-extern char _storage_flash_start;
+extern char _external_apps_flash_start;
 }
 
 namespace Ion {
@@ -10,7 +10,7 @@ namespace Device {
 namespace Board {
 
 bool isRunningSlotA() {
-  return reinterpret_cast<uint32_t>(&_storage_flash_start) < ExternalFlash::Config::StartAddress + ExternalFlash::Config::TotalSize/2;
+  return reinterpret_cast<uint32_t>(&_external_apps_flash_start) < ExternalFlash::Config::StartAddress + ExternalFlash::Config::TotalSize/2;
 }
 
 }

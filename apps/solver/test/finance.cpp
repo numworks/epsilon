ea380df165dc8ef5bfe1cb234de6f4cff3ba2ab1
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-01-21 13:27:39 +0100

    [apps/solver/data] Fix rPct formula when Pmt is 0

--- apps/solver/test/finance.cpp
+++ apps/solver/test/finance.cpp
@@ -58,13 +58,41 @@ void assert_compound_interest_solves(const double expectedValues[CompoundInteres
 
 
 QUIZ_CASE(equation_finance_simple_interest) {
+  // n, rPct, P, I
   SimpleInterestData data;
-  const double values[SimpleInterestData::k_numberOfDoubleValues] = {100.0, 1.0, -1000.0, 2.777777778};
-  assert_simple_interest_solves(values, true, &data);
+  {
+    const double values[SimpleInterestData::k_numberOfDoubleValues] = {
+      100.0, 1.0, -1000.0, 2.777777778};
+    assert_simple_interest_solves(values, true, &data);
+  }
+  {
+    const double values[SimpleInterestData::k_numberOfDoubleValues] = {
+      365.0, 10.0, 1000.0, -100.0};
+    assert_simple_interest_solves(values, false, &data);
+  }
 }
 
 QUIZ_CASE(equation_finance_compound_interest) {
+  // N, rPct, PV, Pmt, FV, PY, CY
   CompoundInterestData data;
-  const double values[CompoundInterestData::k_numberOfDoubleValues] = {72.0, 12.55741064, 12600, -250.0, 0.0, 12.0, 12.0};
-  assert_compound_interest_solves(values, false, &data, 1.0);
+  {
+    const double values[CompoundInterestData::k_numberOfDoubleValues] = {
+      72.0, 12.55741064, 12600.0, -250.0, 0.0, 12.0, 12.0};
+    assert_compound_interest_solves(values, false, &data, 10.0);
+  }
+  {
+    const double values[CompoundInterestData::k_numberOfDoubleValues] = {
+      8.0, 6.4, -37000.0, 0.0, 42009.9, 4.0, 4.0};
+    assert_compound_interest_solves(values, false, &data, 10.0);
+  }
+  {
+    const double values[CompoundInterestData::k_numberOfDoubleValues] = {
+      22.0, 0.0, -24000.0, 1000.0, 2000.0, 4.0, 4.0};
+    assert_compound_interest_solves(values, false, &data, 0.1);
+  }
+  {
+    const double values[CompoundInterestData::k_numberOfDoubleValues] = {
+      18.9692, 6.4, -37000.0, 0.0, 50000.0, 4.0, 4.0};
+    assert_compound_interest_solves(values, false, &data, 1.0);
+  }
 }

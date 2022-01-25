274512e8bb955b169f58730a5768d6bae21559da
Emilie Feral <emilie.feral@numworks.com>
2022-01-25 15:35:11 +0100

    [apps/probability] Remove tick icon in TestConclusionView

--- apps/probability/gui/test_conclusion_view.cpp
+++ apps/probability/gui/test_conclusion_view.cpp
@@ -18,51 +18,38 @@ TestConclusionView::TestConclusionView(Statistic * statistic) : m_statistic(stat
   m_textView1.setAlignment(KDContext::k_alignCenter, KDContext::k_alignCenter);
   m_textView2.setBackgroundColor(Escher::Palette::WallScreen);
   m_textView2.setAlignment(KDContext::k_alignCenter, KDContext::k_alignCenter);
-  m_icon.setImage(ImageStore::TestSuccess);
-}
-
-Escher::View * TestConclusionView::subviewAtIndex(int i) {
-  assert(i < numberOfSubviews());
-  Escher::View * subviews[] = {&m_textView1, &m_textView2, &m_icon};  // Draw icon above text
-  return subviews[i];
-}
-
-void TestConclusionView::layoutSubviews(bool force) {
-  int iconOriginHeight = (m_frame.height() - k_iconSize) / 2;
-  m_icon.setFrame(
-      KDRect(KDPoint(k_marginLeft, iconOriginHeight), m_icon.minimalSizeForOptimalDisplay()),
-      force);
-  KDSize textSize1 = m_textView1.minimalSizeForOptimalDisplay();
-  KDSize textSize2 = m_textView2.minimalSizeForOptimalDisplay();
-  KDCoordinate textHeight = textSize1.height() + textSize2.height();
-  KDCoordinate topOffset = (m_frame.height() - textHeight) / 2;
-  m_textView1.setFrame(KDRect(KDPoint(0, topOffset), KDSize(m_frame.width(), textSize1.height())),
-                       force);
-  m_textView2.setFrame(KDRect(KDPoint(0, topOffset + textSize1.height()),
-                              KDSize(m_frame.width(), textSize2.height())),
-                       force);
-}
-
-KDSize TestConclusionView::minimalSizeForOptimalDisplay() const {
-  KDSize iconSize = m_icon.minimalSizeForOptimalDisplay();
-  KDSize textSize1 = m_textView1.minimalSizeForOptimalDisplay();
-  KDSize textSize2 = m_textView2.minimalSizeForOptimalDisplay();
-  return KDSize(iconSize.width() + std::max(textSize1.width(), textSize2.width()),
-                std::max<int>(iconSize.height(), textSize1.height() + textSize2.height()));
 }
 
 void TestConclusionView::drawRect(KDContext * ctx, KDRect rect) const {
-  KDSize contentSize = minimalSizeForOptimalDisplay();
-  KDCoordinate topOffset = (m_frame.height() - contentSize.height()) / 2;
+  KDSize textsSize = minimalSizeForOptimalDisplay();
+  KDCoordinate topOffset = (m_frame.height() - textsSize.height()) / 2;
   ctx->fillRect(KDRect(0, 0, bounds().width(), topOffset), Escher::Palette::WallScreen);
-  ctx->fillRect(KDRect(0, bounds().height() - topOffset, bounds().width(), topOffset),
-                Escher::Palette::WallScreen);
+  ctx->fillRect(KDRect(0, bounds().height() - topOffset, bounds().width(), topOffset), Escher::Palette::WallScreen);
 }
 
 void TestConclusionView::reload() {
   generateConclusionTexts(m_statistic->canRejectNull());
-  m_icon.setImage(m_statistic->canRejectNull() ? ImageStore::TestSuccess : nullptr);
   markRectAsDirty(bounds());
+}
+
+KDSize TestConclusionView::minimalSizeForOptimalDisplay() const {
+  KDSize textSize1 = m_textView1.minimalSizeForOptimalDisplay();
+  KDSize textSize2 = m_textView2.minimalSizeForOptimalDisplay();
+  return KDSize(std::max(textSize1.width(), textSize2.width()), textSize1.height() + textSize2.height());
+}
+
+Escher::View * TestConclusionView::subviewAtIndex(int i) {
+  assert(i < numberOfSubviews());
+  Escher::View * subviews[] = {&m_textView1, &m_textView2};  // Draw icon above text
+  return subviews[i];
+}
+
+void TestConclusionView::layoutSubviews(bool force) {
+  KDSize textsSize = minimalSizeForOptimalDisplay();
+  KDCoordinate textHeight = textsSize.height() / 2;
+  KDCoordinate topOffset = (m_frame.height() - textsSize.height()) / 2;
+  m_textView1.setFrame(KDRect(KDPoint(0, topOffset), KDSize(bounds().width(), textHeight)), force);
+  m_textView2.setFrame(KDRect(KDPoint(0, topOffset + textHeight), KDSize(bounds().width(), textHeight)), force);
 }
 
 void TestConclusionView::generateConclusionTexts(bool isTestSuccessfull) {

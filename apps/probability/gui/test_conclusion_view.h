b252bcf2785b308e302f8343f48b60b63d8e5dc6
Emilie Feral <emilie.feral@numworks.com>
2022-01-25 15:35:11 +0100

    [apps/probability] Remove tick icon in TestConclusionView

--- apps/probability/gui/test_conclusion_view.h
+++ apps/probability/gui/test_conclusion_view.h
@@ -15,24 +15,19 @@ namespace Probability {
 class TestConclusionView : public Escher::View {
 public:
   TestConclusionView(Statistic * statistic);
-  int numberOfSubviews() const override { return 3; }
-  Escher::View * subviewAtIndex(int i) override;
-  void layoutSubviews(bool force) override;
-  KDSize minimalSizeForOptimalDisplay() const override;
-
   void drawRect(KDContext * ctx, KDRect rect) const override;
-
   void reload();
 
 private:
+  constexpr static int k_marginLeft = 20;
+
+  KDSize minimalSizeForOptimalDisplay() const override;
+  int numberOfSubviews() const override { return 2; }
+  Escher::View * subviewAtIndex(int i) override;
+  void layoutSubviews(bool force) override;
   void generateConclusionTexts(bool isTestSuccessfull);
 
-  constexpr static int k_marginBetween = 0;
-  constexpr static int k_marginLeft = 20;
-  constexpr static int k_iconSize = 14;
-
   Statistic * m_statistic;
-  HighlightImageCell m_icon;
   Escher::ExpressionView m_textView1;
   Escher::MessageTextView m_textView2;
 };

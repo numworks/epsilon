b20126591c09cb3fea4c4ba2e89ae924044924c6
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-02-03 18:17:29 +0100

    [ion/config/n0120] Define boundaries of Backup RAM

--- ion/src/device/include/n0120/config/ram_layout.h
+++ ion/src/device/include/n0120/config/ram_layout.h
@@ -12,6 +12,10 @@ namespace Board {
 constexpr uint32_t SRAMAddress = 0x24000000;
 constexpr uint32_t SRAMLength = 0x40000; // 256kB
 
+// Backup SRAM
+constexpr uint32_t BackupSRAMAddress = 0x38800000;
+constexpr uint32_t BackupSRAMLength = 0x1000; // 4kiB
+
 }
 }
 }

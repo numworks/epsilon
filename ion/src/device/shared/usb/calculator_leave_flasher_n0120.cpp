10000ffee4e4472b6a0660a5720926d3086cf7c8
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-02-10 14:47:22 +0100

    [ion/device/flasher] Forbid jumping from n0120 flasher

--- ion/src/device/shared/usb/calculator_leave_flasher_n0120.cpp
+++ ion/src/device/shared/usb/calculator_leave_flasher_n0120.cpp
@@ -7,15 +7,13 @@ namespace Ion {
 namespace Device {
 namespace USB {
 
-/* This implementation is used by the flasher, which does not need to handle
- * privilege escalation */
 void Calculator::leave(uint32_t leaveAddress) {
-  Board::shutdown();
-  Reset::jump(leaveAddress);
   /* On N0120, the flasher runs with the power supply configured by ST
    * bootloader. This configuration cannot be changed without cycling power to
    * the device. As such, the flasher should not be used to jump to the
-   * bootloader. */
+   * bootloader. Since this is the only use case, we do not allow the flasher
+   * to leave. */
+  assert(false);
 }
 
 }

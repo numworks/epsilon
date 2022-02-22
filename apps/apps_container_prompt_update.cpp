e5f805633dc0a89763ff95d86a87b29e64a196c2
Emilie Feral <emilie.feral@numworks.com>
2022-02-22 17:14:57 +0100

    [apps] Fix build on linux platform (constexpr unused in the compilation unit are discarded)

--- apps/apps_container_prompt_update.cpp
+++ apps/apps_container_prompt_update.cpp
@@ -1,6 +1,6 @@
 #include "apps_container.h"
 
-constexpr I18n::Message AppsContainer::k_promptMessages[] = {
+const I18n::Message AppsContainer::k_promptMessages[] = {
   I18n::Message::UpdateAvailable,
   I18n::Message::UpdateMessage1,
   I18n::Message::UpdateMessage2,
@@ -8,7 +8,7 @@ constexpr I18n::Message AppsContainer::k_promptMessages[] = {
   I18n::Message::UpdateMessage3,
   I18n::Message::UpdateMessage4};
 
-constexpr KDColor AppsContainer::k_promptColors[] = {
+const KDColor AppsContainer::k_promptColors[] = {
   KDColorBlack,
   KDColorBlack,
   KDColorBlack,
@@ -16,4 +16,4 @@ constexpr KDColor AppsContainer::k_promptColors[] = {
   KDColorBlack,
   Escher::Palette::YellowDark};
 
-constexpr int AppsContainer::k_promptNumberOfMessages = 6;
+const int AppsContainer::k_promptNumberOfMessages = 6;

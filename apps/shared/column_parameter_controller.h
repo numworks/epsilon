be56eb9397b5d3cf227e3ec10a6ac2dc943a2727
Félix Richart <felixrichart@felixs-macbook-air.home>
2022-02-22 11:10:27 +0100

    [apps/shared] new ColumnParametersController class
    This is the base class of ValuesParameterController and StoreParameterController.
    Factorized some code, like the title generation

---
+++ apps/shared/column_parameter_controller.h
@@ -0,0 +1,34 @@
+#ifndef SHARED_COLUMN_PARAMETER_CONTROLLER_H
+#define SHARED_COLUMN_PARAMETER_CONTROLLER_H
+
+#include <escher/message_table_cell.h>
+#include <escher/message_table_cell_with_chevron.h>
+#include <escher/selectable_list_view_controller.h>
+#include "interval_parameter_controller.h"
+
+namespace Shared {
+
+static constexpr int k_lengthOfColumnName = 4;
+
+class EditableCellTableViewController;
+
+class ColumnParameterController : public Escher::SelectableListViewController {
+public:
+  ColumnParameterController(Escher::Responder * parentResponder);
+  void didBecomeFirstResponder() override;
+  const char * title() override ;
+
+  virtual void initializeColumnParameters();
+protected:
+  static constexpr int k_titleBufferSize = Ion::Display::Width / 7; // KDFont::SmallFont->glyphSize().width() = 7
+  virtual EditableCellTableViewController * editableCellTableViewController() = 0;
+  Escher::StackViewController * stackView();
+  int m_columnIndex;
+  char m_columnNameBuffer[k_lengthOfColumnName];
+  char m_titleBuffer[k_titleBufferSize];
+
+};
+
+}
+
+#endif

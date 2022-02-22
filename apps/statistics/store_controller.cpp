84992ec1e1a4aa60c769b618360bb9a862294abe
Félix Richart <felixrichart@felixs-macbook-air.home>
2022-02-22 11:44:52 +0100

    [apps/regression+statistics] Factorized functions of store_controller and parameters Cleaned children classes mentionned in the previous commit.

--- apps/statistics/store_controller.cpp
+++ apps/statistics/store_controller.cpp
@@ -15,47 +15,13 @@ namespace Statistics {
 
 StoreController::StoreController(Responder * parentResponder, InputEventHandlerDelegate * inputEventHandlerDelegate, Store * store, ButtonRowController * header, Context * parentContext) :
   Shared::StoreController(parentResponder, inputEventHandlerDelegate, store, header),
-  m_titleCells{},
   m_store(store),
   m_statisticsContext(m_store, parentContext),
-  m_storeParameterController(this, store, this)
-{
-}
-
-void StoreController::setFormulaLabel() {
-  int series = selectedColumn() / Store::k_numberOfColumnsPerSeries;
-  int isValueColumn = selectedColumn() % Store::k_numberOfColumnsPerSeries == 0;
-  char text[] = {isValueColumn ? 'V' : 'N', static_cast<char>('1' + series), '=', 0};
-  static_cast<ContentView *>(view())->formulaInputView()->setBufferText(text);
-}
+  m_storeParameterController(this, this)
+  { }
 
 bool StoreController::fillColumnWithFormula(Expression formula) {
   return privateFillColumnWithFormula(formula, Symbol::isSeriesSymbol);
-}
-
-void StoreController::willDisplayCellAtLocation(HighlightCell * cell, int i, int j) {
-  Shared::StoreController::willDisplayCellAtLocation(cell, i, j);
-  if (typeAtLocation(i, j) != k_titleCellType) {
-    return;
-  }
-  Shared::StoreTitleCell * mytitleCell = static_cast<Shared::StoreTitleCell *>(cell);
-  bool isValuesColumn = i%Store::k_numberOfColumnsPerSeries == 0;
-  mytitleCell->setSeparatorLeft(i > 0 && isValuesColumn);
-  int seriesIndex = i/Store::k_numberOfColumnsPerSeries;
-  assert(seriesIndex >= 0 && seriesIndex < DoublePairStore::k_numberOfSeries);
-  if (isValuesColumn) {
-    I18n::Message valuesMessages[] = {I18n::Message::Values1, I18n::Message::Values2, I18n::Message::Values3};
-    mytitleCell->setText(I18n::translate(valuesMessages[seriesIndex]));
-  } else {
-    I18n::Message sizesMessages[] = {I18n::Message::Frequencies1, I18n::Message::Frequencies2, I18n::Message::Frequencies3};
-    mytitleCell->setText(I18n::translate(sizesMessages[seriesIndex]));
-  }
-  mytitleCell->setColor(m_store->numberOfPairsOfSeries(seriesIndex) == 0 ? Palette::GrayDark : Store::colorOfSeriesAtIndex(seriesIndex)); // TODO Share GrayDark with graph/list_controller
-}
-
-HighlightCell * StoreController::titleCells(int index) {
-  assert(index >= 0 && index < k_numberOfTitleCells);
-  return &m_titleCells[index];
 }
 
 bool StoreController::setDataAtLocation(double floatBody, int columnIndex, int rowIndex) {
@@ -68,6 +34,17 @@ bool StoreController::setDataAtLocation(double floatBody, int columnIndex, int r
     }
   }
   return Shared::StoreController::setDataAtLocation(floatBody, columnIndex, rowIndex);
+}
+
+void StoreController::fillColumnName(int columnIndex, char * buffer) {
+  int series = columnIndex / Store::k_numberOfColumnsPerSeries;
+  int isValueColumn = columnIndex % Store::k_numberOfColumnsPerSeries == 0;
+  buffer[0] = isValueColumn ? 'V' : 'N';
+  buffer[1] = static_cast<char>('1' + series);
+  for (int i = 2; i < Shared::k_lengthOfColumnName; i++) {
+    buffer[i] = 0;
+  }
+
 }
 
 }

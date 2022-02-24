298c2b57e529be5e61f29b6ffde3f17f81ff92cf
Emilie Feral <emilie.feral@numworks.com>
2022-02-24 11:10:59 +0100

    [ion] Simulator and device keyboard works similarly by queuing keyboard state on a queue and poping them one at a time

---
+++ ion/src/shared/keyboard_queue.h
@@ -0,0 +1,23 @@
+#ifndef ION_SHARED_KEYBOARD_QUEUE_H
+#define ION_SHARED_KEYBOARD_QUEUE_H
+
+#include <ion/keyboard.h>
+#include <ion/ring_buffer.h>
+
+namespace Ion {
+namespace Keyboard {
+
+static constexpr size_t k_maximalNumberOfStates = 5;
+
+class Queue : public RingBuffer<Ion::Keyboard::State, k_maximalNumberOfStates> {
+public:
+  static Queue * sharedQueue();
+  void flush(bool resetPending = true);
+private:
+  void didFlush(bool resetPending);
+};
+
+}
+}
+
+#endif

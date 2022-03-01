8062f738c241cda9a328fc93756ae342466382fd
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-03-01 15:59:00 +0100

    [apps/statistics/graph] Factorize PlotViews

--- apps/statistics/graph/normal_probability_view.cpp
+++ apps/statistics/graph/normal_probability_view.cpp
@@ -6,37 +6,23 @@
 namespace Statistics {
 
 NormalProbabilityView::NormalProbabilityView(Store * store) :
-    MultipleDataView(store),
+    PlotView(store),
     // No bannerView given to the curve view because the display is handled here
     m_curveView(&m_graphRange, &m_cursor, &m_cursorView, store) {
 }
 
-Escher::View * NormalProbabilityView::subviewAtIndex(int index) {
-  if (index == 0) {
-    return &m_curveView;
-  }
-  assert(index == 1);
-  return &m_bannerView;
-}
-
-void NormalProbabilityView::reload() {
+void NormalProbabilityView::computeYBounds(float * yMin, float *yMax) {
   int maxNumberOfPairs = 0;
   for (int i = 0; i < Store::k_numberOfSeries; i++) {
     maxNumberOfPairs = std::max(maxNumberOfPairs, m_store->numberOfPairsOfSeries(i));
   }
+  // Normal probability curve is bounded by the biggest series
+  /* TODO : Factorize with zScoreAtSortedIndex or
+   * NormalProbabilityCurveView::valueAtIndex */
   float yBound = Poincare::NormalDistribution::CumulativeDistributiveInverseForProbability<float>(0.5f/maxNumberOfPairs, 0.0f, 1.0f);
   assert(yBound <= 0.0f);
-
-  m_graphRange.calibrate(m_curveView.bounds().width(), m_curveView.bounds().height(), m_store->minValueForAllSeries(), m_store->maxValueForAllSeries(), yBound, -yBound);
-  layoutSubviews();
-  m_curveView.reload();
-}
-
-void NormalProbabilityView::layoutDataSubviews(bool force) {
-  KDCoordinate bannerHeight = bannerView()->minimalSizeForOptimalDisplay().height();
-  KDCoordinate subviewHeight = (bounds().height() - bannerHeight) + 1; // +1 to make sure that all pixel rows are drawn
-  KDRect frame = KDRect(0, 0, bounds().width(), subviewHeight);
-  m_curveView.setFrame(frame, force);
+  *yMin = yBound;
+  *yMax = -yBound;
 }
 
 }

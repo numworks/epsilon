9610496918ad3bf7233d3561eea00f853d3e4a76
Félix Richart <felix.richart@numworks.com>
2022-03-01 09:44:55 +0100

    [escher] Create MessagePopUp and BufferPopUp

--- escher/include/escher/pop_up_controller.h
+++ escher/include/escher/pop_up_controller.h
@@ -14,38 +14,69 @@ public:
   KDColor highlightedBackgroundColor() const override { return Palette::YellowDark; }
 };
 
+class PopUpViewDelegate {
+public:
+  PopUpViewDelegate(int numberOfLines) : m_numberOfLines(numberOfLines) {
+    assert(m_numberOfLines <= k_maxNumberOfLines && m_numberOfLines >= 0);
+  }
+  TextView * textViewAtIndex() = 0;
+  void layoutSubview(bool force, int width) = 0;
+  int numberOfLines() { return m_numberOfLines; }
+protected:
+  constexpr static int k_maxNumberOfLines = 4;
+  const int m_numberOfLines;
+};
+
+
 class PopUpController : public ViewController {
 public:
-  PopUpController(int numberOfLines, Invocation OkInvocation, I18n::Message warningMessage, I18n::Message okMessage, I18n::Message cancelMessage);
+  PopUpController(Invocation OkInvocation, I18n::Message warningMessage, I18n::Message okMessage, I18n::Message cancelMessage, PopUpViewDelegate * delegate);
   View * view() override;
   void didBecomeFirstResponder() override;
   bool handleEvent(Ion::Events::Event event) override;
-  void setContentMessage(int index, I18n::Message message);
 protected:
   class ContentView : public View, public Responder {
   public:
-    ContentView(Responder * parentResponder, int numberOfLines, Invocation okInvocation, I18n::Message warningMessage, I18n::Message okMessage, I18n::Message cancelMessage);
+    ContentView(Responder * parentResponder, Invocation okInvocation, I18n::Message warningMessage, I18n::Message okMessage, I18n::Message cancelMessage, PopUpViewDelegate * delegate);
     void drawRect(KDContext * ctx, KDRect rect) const override { ctx->fillRect(bounds(), KDColorBlack); }
     void setSelectedButton(int selectedButton);
     int selectedButton();
-    void setMessage(int index, I18n::Message message);
-  private:
     constexpr static KDCoordinate k_buttonMargin = 10;
     constexpr static KDCoordinate k_buttonHeight = 20;
     constexpr static KDCoordinate k_topMargin = 8;
     constexpr static KDCoordinate k_paragraphHeight = 20;
+  private:
     int numberOfSubviews() const override;
     View * subviewAtIndex(int index) override;
     void layoutSubviews(bool force = false) override;
     HighContrastButton m_cancelButton;
     HighContrastButton m_okButton;
     MessageTextView m_warningTextView;
-    const int m_numberOfLines;
-    constexpr static int k_maxNumberOfLines = 4;
-    MessageTextView m_messageTextViews[k_maxNumberOfLines];
+    PopUpViewDelegate * m_delegate;
   };
   ContentView m_contentView;
 };
 
+class MessagePopUpController : public PopUpController, public PopUpViewDelegate {
+public:
+  MessagePopUpController(int numberOfLines, Invocation OkInvocation, I18n::Message warningMessage, I18n::Message okMessage, I18n::Message cancelMessage);
+  TextView * textViewAtIndex() override { return m_messageTextViews[i]; }
+  void layoutSubviews(bool force) override;
+  void setContentMessage(int index, I18n::Message message);
+private:
+  MessageTextView m_messageTextViews[k_maxNumberOfLines];
 }
+
+class BufferPopUpController : public PopUpController, public PopUpViewDelegate {
+public:
+  MessagePopUpController(int numberOfLines, Invocation OkInvocation, I18n::Message warningMessage, I18n::Message okMessage, I18n::Message cancelMessage);
+  TextView * textViewAtIndex() override { return m_messageTextViews[i]; }
+  void layoutSubviews(bool force) override;
+  void setContentText(int index, I18n::Message message);
+  void setSimpleCustomContentText(int index, I18n::Message message, const char * string);
+private:
+  BufferTextView m_bufferTextViews[k_maxNumberOfLines];
+}
+
+
 #endif

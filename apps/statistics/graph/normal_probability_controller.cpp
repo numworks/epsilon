35f9ca693855db92fbf07de3e763fcdc0058374b
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-03-03 14:21:30 +0100

    [apps/statistics] Update and improve series colors

--- apps/statistics/graph/normal_probability_controller.cpp
+++ apps/statistics/graph/normal_probability_controller.cpp
@@ -25,21 +25,12 @@ void NormalProbabilityController::computeXBounds(float * xMin, float *xMax) cons
   *xMax = m_store->maxValueForAllSeries(false);
 }
 
-bool NormalProbabilityController::drawSeriesZScoreLine(int series, float * x, float * y, float * u, float * v, KDColor * color) const {
-  // TODO : Add these colors somewhere else
-  // Brighter colors for the curves
-  constexpr KDColor k_curveColors[Store::k_numberOfSeries] = {
-      KDColor::RGB888(255, 204, 204),   // #FFCCCC
-      KDColor::RGB888(220, 227, 253),   // #DCE3FD
-      KDColor::RGB888(220, 243, 204)};  // #DCF3CC
-  *color = k_curveColors[series];
-
+bool NormalProbabilityController::drawSeriesZScoreLine(int series, float * x, float * y, float * u, float * v) const {
   // Plot the y=(x-mean(X)/sigma(X)) line
-  // TODO : Handle frequencies different than 1
   float mean = m_store->mean(series);
   float sigma = m_store->standardDeviation(series);
-  float xMin = m_store->minValue(series);
-  float xMax = m_store->maxValue(series);
+  float xMin = m_store->minValue(series, false);
+  float xMax = m_store->maxValue(series, false);
   *x = xMin;
   *y = (xMin-mean)/sigma;
   *u = xMax;

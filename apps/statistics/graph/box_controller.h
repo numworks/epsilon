6f779241dc5f066f5d598b58479e477faeb0f5c5
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-03-04 10:57:29 +0100

    [apps/statistics] Add Box plot parameters

--- apps/statistics/graph/box_controller.h
+++ apps/statistics/graph/box_controller.h
@@ -5,6 +5,7 @@
 #include "../store.h"
 #include "multiple_boxes_view.h"
 #include "multiple_data_view_controller.h"
+#include "box_parameter_controller.h"
 
 namespace Statistics {
 
@@ -19,14 +20,30 @@ public:
                 BoxView::Quantile * selectedQuantile,
                 int * selectedSeriesIndex);
 
+  BoxParameterController * boxParameterController() { return &m_boxParameterController; }
+
+  // ButtonRowDelegate
+  int numberOfButtons(Escher::ButtonRowController::Position position) const override { return GraphButtonRowDelegate::numberOfButtons(position) + 1; }
+  Escher::Button * buttonAtIndex(int index, Escher::ButtonRowController::Position position) const override;
+
   // MultipleDataViewController
   MultipleDataView * multipleDataView() override { return &m_view; }
   bool moveSelectionHorizontally(int deltaIndex) override;
+
+  // ViewController
+  void viewWillAppear() override;
+
+  // Responder
+  bool handleEvent(Ion::Events::Event event) override;
+  void didEnterResponderChain(Responder * firstResponder) override;
+  void willExitResponderChain(Escher::Responder * nextFirstResponder) override;
 
   TELEMETRY_ID("Box");
 private:
   void reloadBannerView() override;
   MultipleBoxesView m_view;
+  BoxParameterController m_boxParameterController;
+  Escher::Button m_parameterButton;
 };
 
 }
---
+++ apps/statistics/graph/box_parameter_controller.cpp
@@ -0,0 +1,33 @@
+#include "box_parameter_controller.h"
+
+using namespace Escher;
+
+namespace Statistics {
+
+BoxParameterController::BoxParameterController(Responder * parentResponder, Store * store) :
+  SelectableCellListPage<MessageTableCellWithSwitch, k_numberOfParameterCells, Escher::RegularListViewDataSource>(parentResponder),
+  m_store(store)
+{
+  cellAtIndex(0)->setMessage(I18n::Message::DisplayOutliers);
+  // Being the only cell, it is always highlighted.
+  cellAtIndex(0)->setHighlighted(true);
+}
+
+void BoxParameterController::viewWillAppear() {
+  cellAtIndex(0)->setState(m_store->displayOutliers());
+}
+
+void BoxParameterController::willDisplayCellForIndex(HighlightCell * cell, int index) {
+}
+
+bool BoxParameterController::handleEvent(Ion::Events::Event event) {
+  // TODO : Improve navigation, handle UP and DOWN event from tab controller
+  if (event == Ion::Events::OK || event == Ion::Events::EXE) {
+    m_store->setDisplayOutliers(!m_store->displayOutliers());
+    cellAtIndex(0)->setState(m_store->displayOutliers());
+    return true;
+  }
+  return false;
+}
+
+}

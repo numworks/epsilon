61932cbbef3282e45dc4c4d090a625b5d1dc7bf7
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-03-08 10:58:23 +0100

    [quiz/assertion]Fix quiz_assert on PLATFORM_DEVICE

--- quiz/src/assertions.cpp
+++ quiz/src/assertions.cpp
@@ -3,12 +3,19 @@
 #include <quiz.h>
 
 void quiz_assert(bool condition) {
-#if PLATFORM_DEVICE
-  /* Freeze on device so that the error message can be read. */
-  while (!condition) {}
-#else
   if (!condition) {
+#if PLATFORM_DEVICE
+    /* Freeze on device so that the error message can be read.
+     * Note : "while (!condition) {}" could be omitted by the compiler because
+     * an iteration statement whose controlling expression is not a constant
+     * expression, that performs no input/output operations, does not access
+     * volatile objects, and performs no synchronization or atomic operations in
+     * its body, controlling expression, or (in the case of a for statement) its
+     * expression-3, may be assumed by the implementation to terminate.
+    */
+    while (true) {}
+#else
     abort();
-  }
 #endif
+  }
 }

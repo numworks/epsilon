b36075fdd3d6df03a8851653ad4f9dc86b454498
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-03-11 18:16:19 +0100

    [apps/probability] Fix parameter check

--- apps/probability/controllers/homogeneity_table_view_controller.cpp
+++ apps/probability/controllers/homogeneity_table_view_controller.cpp
@@ -51,7 +51,7 @@ bool HomogeneityTableViewController::textFieldDidFinishEditing(Escher::TextField
     return false;
   }
   int row = m_table.selectedRow(), column = m_table.selectedColumn();
-  if (!m_statistic->authorizedParameterAtPosition(p, row, column)) {
+  if (!m_statistic->authorizedParameterAtPosition(p, row - 1, column - 1)) {
     App::app()->displayWarning(I18n::Message::ForbiddenValue);
     return false;
   }

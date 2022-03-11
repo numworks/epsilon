14a017c66c81fe490776c9d2c9c9faff7f98dc02
Félix Richart <felix.richart@numworks.com>
2022-03-11 17:56:06 +0100

    [escher] Add buffer table cell with message

---
+++ escher/src/buffer_table_cell_with_message.cpp
@@ -0,0 +1,23 @@
+#include <escher/buffer_table_cell_with_message.h>
+#include <escher/palette.h>
+#include <assert.h>
+
+namespace Escher {
+
+BufferTableCellWithMessage::BufferTableCellWithMessage(I18n::Message subLabelMessage) :
+  BufferTableCell(),
+  m_subLabelView(KDFont::SmallFont, subLabelMessage, KDContext::k_alignLeft, KDContext::k_alignCenter, Palette::GrayDark, KDColorWhite)
+{
+}
+
+void BufferTableCellWithMessage::setHighlighted(bool highlight) {
+  BufferTableCell::setHighlighted(highlight);
+  KDColor backgroundColor = highlight? Palette::Select : KDColorWhite;
+  m_subLabelView.setBackgroundColor(backgroundColor);
+}
+
+void BufferTableCellWithMessage::setSubLabelMessage(I18n::Message text) {
+  m_subLabelView.setMessage(text);
+}
+
+}

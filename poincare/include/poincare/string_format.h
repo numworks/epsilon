1755418da3bb3d637a4f6967931a004608a71de4
Félix Richart <felix.richart@numworks.com>
2022-03-22 13:14:20 +0100

    [poincare] Add string format

---
+++ poincare/include/poincare/string_format.h
@@ -0,0 +1,30 @@
+#ifndef POINCARE_STRING_FORMAT_H
+#define POINCARE_STRING_FORMAT_H
+
+#include <kandinsky/font.h>
+#include <escher/metric.h>
+
+namespace Poincare {
+
+class StringFormat {
+public:
+  static constexpr const KDFont * k_defaultFont = KDFont::LargeFont;
+
+  StringFormat(const KDFont * font) {
+    setFont(font);
+  }
+
+  const KDFont * font() const { return m_font == StringFont::LargeFont ? KDFont::LargeFont : KDFont::SmallFont; }
+  void setFont(const KDFont * font) { font == KDFont::LargeFont ? m_font = StringFont::LargeFont : m_font = StringFont::SmallFont; }
+
+private:
+  enum class StringFont : uint8_t {
+    SmallFont,
+    LargeFont
+  };
+  StringFont m_font;
+};
+
+}
+
+#endif

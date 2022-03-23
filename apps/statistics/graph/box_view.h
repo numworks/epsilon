109a683616b09c6c46d465a5a5fac8bfdd3a6c86
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-03-23 16:57:56 +0100

    [apps/statistics/graph] Use constexpr static

--- apps/statistics/graph/box_view.h
+++ apps/statistics/graph/box_view.h
@@ -28,26 +28,26 @@ public:
   void drawRect(KDContext * ctx, KDRect rect) const override;
 private:
   /* Colors */
-  static constexpr KDColor k_selectedColor = Escher::Palette::YellowDark;
-  static constexpr KDColor k_unfocusedColor = Escher::Palette::GrayMiddle;
+  constexpr static KDColor k_selectedColor = Escher::Palette::YellowDark;
+  constexpr static KDColor k_unfocusedColor = Escher::Palette::GrayMiddle;
   /* Outlier size */
-  static constexpr Shared::CurveView::Size k_outlierDotSize = Shared::CurveView::Size::Small;
-  static constexpr KDCoordinate k_outlierSize = Shared::Dots::SmallDotDiameter;
+  constexpr static Shared::CurveView::Size k_outlierDotSize = Shared::CurveView::Size::Small;
+  constexpr static KDCoordinate k_outlierSize = Shared::Dots::SmallDotDiameter;
   /* Box and quantile dimensions */
-  static constexpr KDCoordinate k_twoBoxesHeight = 40;
-  static constexpr KDCoordinate k_threeBoxesHeight = 27;
-  static constexpr KDCoordinate k_quantileBarWidth = 2;
+  constexpr static KDCoordinate k_twoBoxesHeight = 40;
+  constexpr static KDCoordinate k_threeBoxesHeight = 27;
+  constexpr static KDCoordinate k_quantileBarWidth = 2;
   /* Margins to properly reload rects */
-  static constexpr KDCoordinate k_chevronMargin = 2;
+  constexpr static KDCoordinate k_chevronMargin = 2;
   // A calculation may be a quantile or an outlier. It has chevrons if selected.
-  static constexpr KDCoordinate k_biggestCalculationWidth = std::max({k_quantileBarWidth, Chevrons::k_chevronWidth, k_outlierSize});
+  constexpr static KDCoordinate k_biggestCalculationWidth = std::max({k_quantileBarWidth, Chevrons::k_chevronWidth, k_outlierSize});
   // Object placed at:              V        V
   // Object size in pixels:       12345    123456
   // Right pixels to be dirtied:  ##       ##
   // Left pixels to be dirtied:     ###      ####
-  static constexpr KDCoordinate k_leftMargin = (k_biggestCalculationWidth - 1)/2;
-  static constexpr KDCoordinate k_rightMargin = k_biggestCalculationWidth/2 + 1;
-  static constexpr KDCoordinate k_verticalMargin = k_chevronMargin + Chevrons::k_chevronHeight;
+  constexpr static KDCoordinate k_leftMargin = (k_biggestCalculationWidth - 1)/2;
+  constexpr static KDCoordinate k_rightMargin = k_biggestCalculationWidth/2 + 1;
+  constexpr static KDCoordinate k_verticalMargin = k_chevronMargin + Chevrons::k_chevronHeight;
 
   /* Draw */
   void drawBar(KDContext * ctx, KDRect rect, float calculation, float lowBound, float upBound, KDColor color, bool isSelected) const;

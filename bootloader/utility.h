a0caf67d7cd0ce102a2b06235d95a0dd42d307c4
devdl11 <dl11developer@gmail.com>
2022-04-08 18:33:27 +0200

    [review] revert flasher code

--- bootloader/utility.h
+++ bootloader/utility.h
@@ -2,7 +2,6 @@
 #define _BOOTLOADER_ITOA_H_
 
 namespace Utility {
-
     static char * itoa(int value, char * result, int base);
     static int versionSum(const char * version, int length);
 }
--- ion/src/device/flasher/main.cpp
+++ ion/src/device/flasher/main.cpp
@@ -11,6 +11,6 @@ void ion_main(int argc, const char * const argv[]) {
     Ion::USB::enable();
     while (!Ion::USB::isEnumerated()) {
     }
-    Ion::USB::DFU(false);
+    Ion::USB::DFU(false, false, 0);
   }
 }

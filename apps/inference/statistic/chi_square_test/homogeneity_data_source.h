a3f70a1563f7779d1ebcdf763bb40591831f0038
Emilie Feral <emilie.feral@numworks.com>
2022-04-21 16:00:19 +0200

    [apps/inference] Fix HomogeneityTest::k_numberOfReusableCells to take into account the total row

--- apps/inference/statistic/chi_square_test/homogeneity_data_source.h
+++ apps/inference/statistic/chi_square_test/homogeneity_data_source.h
@@ -37,8 +37,8 @@ public:
 
   constexpr static int k_columnWidth = 82;
   constexpr static int k_numberOfReusableColumns = std::min(Ion::Display::Width /k_columnWidth + 2, HomogeneityTest::k_maxNumberOfColumns); // 5
-  constexpr static int k_maxNumberOfReusableRows = std::min(CategoricalTableViewDataSource::k_maxNumberOfReusableRows, HomogeneityTest::k_maxNumberOfRows); // 9
-  constexpr static int k_numberOfReusableCells = k_maxNumberOfReusableRows * k_numberOfReusableColumns; // 5 * 9
+  constexpr static int k_maxNumberOfReusableRows = std::min(CategoricalTableViewDataSource::k_maxNumberOfReusableRows, HomogeneityTest::k_maxNumberOfRows + 1); // std::min(12, 9 + 1) - we take into account the potential "Total row"
+  constexpr static int k_numberOfReusableCells = k_maxNumberOfReusableRows * k_numberOfReusableColumns; // 5 * 10
 
 protected:
   virtual int innerNumberOfRows() const = 0;

870a22b663cce2a0cccff56684bf9aed08bb9ce8
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-04-26 18:00:04 +0200

    [apps/shared] Factorize StoreContext method

--- apps/shared/store_context.cpp
+++ apps/shared/store_context.cpp
@@ -5,6 +5,24 @@
 
 namespace Shared {
 
+const Poincare::Expression StoreContext::expressionForSymbolAbstract(const Poincare::SymbolAbstract & symbol, bool clone, float unknownSymbolValue ) {
+  if (symbol.type() == Poincare::ExpressionNode::Type::Symbol && isSymbol(symbol.name())) {
+    const char * seriesName = symbol.name();
+    assert(strlen(seriesName) == 2);
+
+    int series = (int)(seriesName[1] - '0') - 1;
+    assert(series >= 0 && series < DoublePairStore::k_numberOfSeries);
+
+    int storeI = getSymbolColumn(seriesName);
+
+    assert(m_seriesPairIndex >= 0);
+    assert(m_seriesPairIndex < m_store->numberOfPairsOfSeries(series));
+    return Poincare::Float<double>::Builder(m_store->get(series, storeI, m_seriesPairIndex));
+  } else {
+    return ContextWithParent::expressionForSymbolAbstract(symbol, clone);
+  }
+}
+
 Poincare::Context::SymbolAbstractType StoreContext::expressionTypeForIdentifier(const char * identifier, int length) {
   if (length == 2) {
     // Crop identifier

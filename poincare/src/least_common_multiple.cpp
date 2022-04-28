f36c7de41cdc2c68a8d482694611b16eab6133fb
Félix Richart <felix.richart@numworks.com>
2022-04-28 15:21:30 +0200

    [poincare] Distribute LCM over lists

--- poincare/src/least_common_multiple.cpp
+++ poincare/src/least_common_multiple.cpp
@@ -25,6 +25,7 @@ Expression LeastCommonMultipleNode::shallowBeautify(ReductionContext * reduction
 
 template<typename T>
 Evaluation<T> LeastCommonMultipleNode::templatedApproximate(ApproximationContext approximationContext) const {
+  /* TODO : distribute approx over list with Map */
   return Arithmetic::LCM<T>(*this, approximationContext);
 }
 
@@ -39,6 +40,14 @@ Expression LeastCommonMultiple::shallowBeautify(Context * context) {
 Expression LeastCommonMultiple::shallowReduce(ExpressionNode::ReductionContext reductionContext) {
   {
     Expression e = SimplificationHelper::defaultShallowReduce(*this);
+    if (!e.isUninitialized()) {
+      return e;
+    }
+    e = SimplificationHelper::undefinedOnMatrix(*this, reductionContext);
+    if (!e.isUninitialized()) {
+      return e;
+    }
+    e = SimplificationHelper::distributeReductionOverLists(*this, reductionContext);
     if (!e.isUninitialized()) {
       return e;
     }

4a877f7614f1a91cee128f955c6cf9839caca97b
Félix Richart <felix.richart@numworks.com>
2022-04-29 10:34:23 +0200

    [apps/global_context] Store only reduced expressions

--- apps/shared/global_context.h
+++ apps/shared/global_context.h
@@ -14,8 +14,8 @@ namespace Shared {
 
 class GlobalContext final : public Poincare::Context {
 public:
-  static constexpr int k_numberOfExtensions = 4;
-  static constexpr const char * k_extensions[] = {Ion::Storage::expExtension, Ion::Storage::matExtension, Ion::Storage::funcExtension, Ion::Storage::lisExtension, Ion::Storage::seqExtension};
+  constexpr static const char * k_extensions[] = {Ion::Storage::expExtension, Ion::Storage::matExtension, Ion::Storage::funcExtension, Ion::Storage::lisExtension, Ion::Storage::seqExtension};
+  constexpr static int k_numberOfExtensions = sizeof(k_extensions) / sizeof(char *);
 
   // Storage information
   static bool SymbolAbstractNameIsFree(const char * baseName);

887edc91c6cf0994e42272e98e6f5df23e3374ea
MarcNumworks <marc.coudriau@numworks.com>
2022-05-05 11:13:41 +0200

    [poincare] Reintroduce virtuality in distributions

--- poincare/include/poincare/geometric_distribution.h
+++ poincare/include/poincare/geometric_distribution.h
@@ -2,19 +2,34 @@
 #define POINCARE_GEOMETRIC_DISTRIBUTION_H
 
 #include <poincare/expression.h>
+#include <poincare/distribution.h>
 #include <poincare/preferences.h>
 
 namespace Poincare {
 
-class GeometricDistribution final {
+class GeometricDistribution final : public Distribution {
 public:
-  template<typename T> static T EvaluateAtAbscissa(T x, T p);
-  template<typename T> static T CumulativeDistributiveFunctionAtAbscissa(T x, T p);
-  template<typename T> static T CumulativeDistributiveInverseForProbability(T probability, T p);
+  template<typename T> static T EvaluateAtAbscissa(T x, const T p);
+  float EvaluateAtAbscissa(float x, const float * parameters) override { return EvaluateAtAbscissa<float>(x, parameters[0]); }
+  double EvaluateAtAbscissa(double x, const double * parameters) override { return EvaluateAtAbscissa<double>(x, parameters[0]); }
+
+  template<typename T> static T CumulativeDistributiveFunctionAtAbscissa(T x, const T lambda);
+  float CumulativeDistributiveFunctionAtAbscissa(float x, const float * parameters) override { return CumulativeDistributiveFunctionAtAbscissa<float>(x, parameters[0]); }
+  double CumulativeDistributiveFunctionAtAbscissa(double x, const double * parameters) override { return CumulativeDistributiveFunctionAtAbscissa<double>(x, parameters[0]); }
+
+  template<typename T> static T CumulativeDistributiveInverseForProbability(T probability, T lambda);
+  float CumulativeDistributiveInverseForProbability(float x, const float * parameters) override { return CumulativeDistributiveInverseForProbability<float>(x, parameters[0]); }
+  double CumulativeDistributiveInverseForProbability(double x, const double * parameters) override { return CumulativeDistributiveInverseForProbability<double>(x, parameters[0]); }
+
+  bool ParametersAreOK(const float * parameters) override { return PIsOK(parameters[0]); }
+  bool ParametersAreOK(const double * parameters) override { return PIsOK(parameters[0]); }
+
+  static bool ExpressionPIsOK(bool * result, const Expression &p, Context * context);
+  bool ExpressionParametersAreOK(bool * result, const Expression * parameters, Context * context) override { return ExpressionPIsOK(result, parameters[0], context); }
+
+private:
+  template<typename T> static T parameterP(T* parameters) { return parameters[0]; }
   template<typename T> static bool PIsOK(T p);
-  /* ExpressionParametersAreOK returns true if the expression could be verified.
-   * The result of the verification is *result. */
-  static bool ExpressionPIsOK(bool * result, const Expression & p, Context * context);
 };
 
 }
--- poincare/include/poincare/student_distribution.h
+++ poincare/include/poincare/student_distribution.h
@@ -1,20 +1,36 @@
 #ifndef POINCARE_STUDENT_DISTRIBUTION_H
 #define POINCARE_STUDENT_DISTRIBUTION_H
 
+#include <poincare/expression.h>
 #include <poincare/distribution.h>
+#include <poincare/preferences.h>
 
 namespace Poincare {
 
-class StudentDistribution final : Distribution {
+class StudentDistribution final : public Distribution {
 public:
-  template <typename T> static T EvaluateAtAbscissa(T x, T k);
-  template <typename T> static T CumulativeDistributiveFunctionAtAbscissa(T x, T k);
-  template <typename T> static T CumulativeDistributiveInverseForProbability(T probability, T k);
-  template<typename T> static bool KIsOK(T k);
-  /* ExpressionMuAndVarAreOK returns true if the expression could be verified.
-   * The result of the verification is *result. */
-  static bool ExpressionKIsOK(bool * result, const Expression & k, Context * context);
+  template<typename T> static T EvaluateAtAbscissa(T x, const T k);
+  float EvaluateAtAbscissa(float x, const float * parameters) override { return EvaluateAtAbscissa<float>(x, parameters[0]); }
+  double EvaluateAtAbscissa(double x, const double * parameters) override { return EvaluateAtAbscissa<double>(x, parameters[0]); }
+
+  template<typename T> static T CumulativeDistributiveFunctionAtAbscissa(T x, const T k);
+  float CumulativeDistributiveFunctionAtAbscissa(float x, const float * parameters) override { return CumulativeDistributiveFunctionAtAbscissa<float>(x, parameters[0]); }
+  double CumulativeDistributiveFunctionAtAbscissa(double x, const double * parameters) override { return CumulativeDistributiveFunctionAtAbscissa<double>(x, parameters[0]); }
+
+  template<typename T> static T CumulativeDistributiveInverseForProbability(T probability, T k);
+  float CumulativeDistributiveInverseForProbability(float x, const float * parameters) override { return CumulativeDistributiveInverseForProbability<float>(x, parameters[0]); }
+  double CumulativeDistributiveInverseForProbability(double x, const double * parameters) override { return CumulativeDistributiveInverseForProbability<double>(x, parameters[0]); }
+
+  bool ParametersAreOK(const float * parameters) override { return KIsOK(parameters[0]); }
+  bool ParametersAreOK(const double * parameters) override { return KIsOK(parameters[0]); }
+
+  static bool ExpressionKIsOK(bool * result, const Expression &k, Context * context);
+  bool ExpressionParametersAreOK(bool * result, const Expression * parameters, Context * context) override { return ExpressionKIsOK(result, parameters[0], context); }
+
   template <typename T> static T lnCoefficient(T k);
+private:
+  template<typename T> static T parameterK(T* parameters) { return parameters[0]; }
+  template<typename T> static bool KIsOK(T k);
 };
 
 }

9c2b43c4eff9b736d42f6bd4e9310413f857679a
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-05-09 10:09:22 +0200

    [apps/shared] Factorize CalculationControllers

--- apps/regression/calculation_controller.h
+++ apps/regression/calculation_controller.h
@@ -1,57 +1,41 @@
 #ifndef REGRESSION_CALCULATION_CONTROLLER_H
 #define REGRESSION_CALCULATION_CONTROLLER_H
 
-#include <escher/alternate_empty_view_controller.h>
-#include <escher/button_row_controller.h>
 #include <escher/even_odd_expression_cell.h>
 #include <escher/even_odd_message_text_cell.h>
 #include <poincare/preferences.h>
 #include "store.h"
 #include "column_title_cell.h"
 #include "even_odd_double_buffer_text_cell_with_separator.h"
-#include "../shared/hideable_even_odd_cell.h"
-#include "../shared/tab_table_controller.h"
-#include "../shared/separator_even_odd_buffer_text_cell.h"
-#include "../shared/store_cell.h"
+#include <apps/shared/hideable_even_odd_cell.h>
+#include <apps/shared/separator_even_odd_buffer_text_cell.h>
+#include <apps/shared/store_cell.h>
+#include <apps/shared/double_pair_table_controller.h>
 
 namespace Regression {
 
 constexpr static KDCoordinate maxCoordinate(KDCoordinate a, KDCoordinate b) { return a > b ? a : b; }
 
-class CalculationController : public Shared::TabTableController, public Escher::TableViewDataSource, public Escher::SelectableTableViewDelegate, public Escher::ButtonRowDelegate, public Escher::AlternateEmptyViewDefaultDelegate  {
+class CalculationController : public Shared::DoublePairTableController, public Escher::SelectableTableViewDelegate {
 
 public:
   CalculationController(Escher::Responder * parentResponder, Escher::ButtonRowController * header, Store * store);
 
   // View Controller
-  const char * title() override;
   TELEMETRY_ID("Calculation");
-
-  // Responder
-  bool handleEvent(Ion::Events::Event event) override;
-  void didBecomeFirstResponder() override;
 
   // SelectableTableViewDelegate
   void tableViewDidChangeSelection(Escher::SelectableTableView * t, int previousSelectedCellX, int previousSelectedCellY, bool withinTemporarySelection) override;
-
-  // AlternateEmptyViewDefaultDelegate
-  bool isEmpty() const override;
-  I18n::Message emptyMessage() override;
-  Escher::Responder * defaultController() override;
 
   // TableViewDataSource
   int numberOfRows() const override;
   int numberOfColumns() const override;
   void willDisplayCellAtLocation(Escher::HighlightCell * cell, int i, int j) override;
   KDCoordinate columnWidth(int i) override;
-  KDCoordinate rowHeight(int j) override;
-  KDCoordinate cumulatedHeightFromIndex(int j) override;
-  int indexFromCumulatedHeight(KDCoordinate offsetY) override;
   Escher::HighlightCell * reusableCell(int index, int type) override;
   int reusableCellCount(int type) override;
   int typeAtLocation(int i, int j) override;
 private:
-  constexpr static int k_maxNumberOfDisplayableRows = 11;
   constexpr static int k_totalNumberOfDoubleBufferRows = 5;
   constexpr static int k_numberOfDoubleCalculationCells = Store::k_numberOfSeries * k_totalNumberOfDoubleBufferRows;
   constexpr static int k_numberOfCalculationCells = Store::k_numberOfSeries * k_maxNumberOfDisplayableRows;
@@ -62,7 +46,6 @@ private:
   static constexpr int k_hideableCellType = 4;
   static constexpr int k_regressionCellIndex = 9;
 
-  static constexpr KDCoordinate k_cellHeight = 20;
   static constexpr KDCoordinate k_titleCalculationCellWidth = Ion::Display::Width/2 - Escher::Metric::CommonRightMargin/2 - Escher::Metric::CommonLeftMargin/2;
   /* Separator and margins from EvenOddCell::layoutSubviews (and derived classes
    * implementations) must be accounted for here.
@@ -74,13 +57,11 @@ private:
   static constexpr KDCoordinate k_cubicCalculationCellWidth = maxCoordinate(7*21+2*Escher::EvenOddCell::k_horizontalMargin+Escher::EvenOddCell::k_separatorWidth, k_minCalculationCellWidth);
   // To hold _y=a·x^4+b·x^3+c·x^2+d·x+e_
   static constexpr KDCoordinate k_quarticCalculationCellWidth = maxCoordinate(7*27+2*Escher::EvenOddCell::k_horizontalMargin+Escher::EvenOddCell::k_separatorWidth, k_minCalculationCellWidth);
-  static constexpr KDCoordinate k_margin = 8;
-  static constexpr KDCoordinate k_scrollBarMargin = Escher::Metric::CommonRightMargin;
-  Escher::Responder * tabController() const override;
-  Escher::SelectableTableView * selectableTableView() override { return &m_selectableTableView; }
+
+  Shared::DoublePairStore * store() const override { return m_store; }
   bool hasLinearRegression() const;
   int maxNumberOfCoefficients() const;
-  Escher::SelectableTableView m_selectableTableView;
+
   Escher::EvenOddMessageTextCell m_titleCells[k_maxNumberOfDisplayableRows];
   ColumnTitleCell m_columnTitleCells[Store::k_numberOfSeries];
   EvenOddDoubleBufferTextCellWithSeparator m_doubleCalculationCells[k_numberOfDoubleCalculationCells];
---
+++ apps/shared/double_pair_table_controller.h
@@ -0,0 +1,53 @@
+#ifndef SHARED_DOUBLE_PAIR_TABLE_CONTROLLER_H
+#define SHARED_DOUBLE_PAIR_TABLE_CONTROLLER_H
+
+#include "tab_table_controller.h"
+#include "double_pair_store.h"
+#include <escher/table_view_data_source.h>
+#include <escher/button_row_controller.h>
+#include <escher/alternate_empty_view_controller.h>
+#include <apps/i18n.h>
+
+namespace Shared {
+
+class DoublePairTableController : public TabTableController, public Escher::TableViewDataSource, public Escher::ButtonRowDelegate, public Escher::AlternateEmptyViewDefaultDelegate {
+public:
+  DoublePairTableController(Escher::Responder * parentResponder, Escher::ButtonRowController * header) :
+    TabTableController(parentResponder),
+    ButtonRowDelegate(header, nullptr),
+    m_selectableTableView(this, this, this)
+  {}
+
+  // TableViewDataSource
+  KDCoordinate rowHeight(int j) override { return k_cellHeight; }
+  KDCoordinate cumulatedHeightFromIndex(int j) override { return j*rowHeight(0); }
+  int indexFromCumulatedHeight(KDCoordinate offsetY) override { return (offsetY-1) / rowHeight(0); }
+
+  // AlternateEmptyViewDefaultDelegate
+  bool isEmpty() const override { return !store()->hasValidSeries(); }
+  I18n::Message emptyMessage() override { return I18n::Message::NoValueToCompute; }
+  Responder * defaultController() override { return tabController(); }
+
+  // ViewController
+  const char * title() override { return I18n::translate(I18n::Message::StatTab); }
+
+  // Responder
+  bool handleEvent(Ion::Events::Event event) override;
+  void didBecomeFirstResponder() override;
+
+protected:
+  constexpr static int k_maxNumberOfDisplayableRows = 11;
+  static constexpr KDCoordinate k_cellHeight = 20;
+  static constexpr KDCoordinate k_margin = 8;
+  static constexpr KDCoordinate k_scrollBarMargin = Escher::Metric::CommonRightMargin;
+
+  virtual DoublePairStore * store() const = 0;
+  Escher::Responder * tabController() const override { return (parentResponder()->parentResponder()->parentResponder()); }
+  Escher::SelectableTableView * selectableTableView() override { return &m_selectableTableView; }
+
+  Escher::SelectableTableView m_selectableTableView;
+};
+
+}
+
+#endif

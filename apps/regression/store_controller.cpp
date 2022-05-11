a0219c3961c4a87dac53bb2005257c323d0fb8a3
Félix Richart <felix.richart@numworks.com>
2022-05-11 13:57:09 +0200

    [apps] Remove store_context

--- apps/regression/store_controller.cpp
+++ apps/regression/store_controller.cpp
@@ -1,6 +1,5 @@
 #include "store_controller.h"
 #include "app.h"
-#include "regression_context.h"
 #include "../apps_container.h"
 #include "../constant.h"
 #include <assert.h>
@@ -13,13 +12,8 @@ namespace Regression {
 
 StoreController::StoreController(Responder * parentResponder, Escher::InputEventHandlerDelegate * inputEventHandlerDelegate, Store * store, ButtonRowController * header, Context * parentContext) :
   Shared::StoreController(parentResponder, inputEventHandlerDelegate, store, header),
-  m_regressionContext(store, parentContext),
   m_storeParameterController(this, this)
   { }
-
-bool StoreController::fillColumnWithFormula(Expression formula) {
-  return privateFillColumnWithFormula(formula, RegressionContext::IsSymbol);
-}
 
 void StoreController::clearSelectedColumn() {
   m_store->deleteColumn(m_store->seriesAtColumn(selectedColumn()), m_store->relativeColumnIndex(selectedColumn()));
--- apps/regression/store_controller.h
+++ apps/regression/store_controller.h
@@ -2,7 +2,6 @@
 #define REGRESSION_STORE_CONTROLLER_H
 
 #include "store.h"
-#include "regression_context.h"
 #include "store_parameter_controller.h"
 #include "../shared/store_controller.h"
 
@@ -11,15 +10,12 @@ namespace Regression {
 class StoreController : public Shared::StoreController {
 public:
   StoreController(Escher::Responder * parentResponder, Escher::InputEventHandlerDelegate * inputEventHandlerDelegate, Store * store, Escher::ButtonRowController * header, Poincare::Context * parentContext);
-  Shared::StoreContext * storeContext() override { return &m_regressionContext; }
-  bool fillColumnWithFormula(Poincare::Expression formula) override;
   Model * selectedModel() { return static_cast<Store *>(m_store)->modelForSeries(selectedSeries()); }
 
 private:
   Escher::InputViewController * inputViewController() override;
   Shared::ColumnParameterController * columnParameterController() override { return &m_storeParameterController; }
   void clearSelectedColumn() override;
-  RegressionContext m_regressionContext;
   StoreParameterController m_storeParameterController;
 };
 

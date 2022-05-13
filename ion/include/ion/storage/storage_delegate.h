2bb6f6da42ccfafeadaceb2c537bbaa12f7952bc
Félix Richart <felix.richart@numworks.com>
2022-05-13 17:52:25 +0200

    [ion/storage] Put storage_delegate and storage_helper in different files

---
+++ ion/include/ion/storage/storage_delegate.h
@@ -0,0 +1,27 @@
+#ifndef ION_STORAGE_DELEGATE_H
+#define ION_STORAGE_DELEGATE_H
+
+#include "record.h"
+
+namespace Ion {
+
+namespace Storage {
+
+/* Some apps memoize records and need to be notified when a record might have
+ * become invalid. For instance in the Graph app, if f(x) = A+x, and A changed,
+ * f(x) memoization which stores the reduced expression of f is outdated.
+ * We could have computed and compared the checksum of the storage to detect
+ * storage invalidity, but profiling showed that this slows down the execution
+ * (for example when scrolling the functions list).
+ * We thus decided to notify a delegate when the storage changes. */
+class StorageDelegate {
+public:
+  virtual void storageDidChangeForRecord(const Record record) = 0;
+  virtual void storageIsFull() = 0;
+};
+
+}
+
+}
+
+#endif
---
+++ ion/include/ion/storage/storage_helper.h
@@ -0,0 +1,34 @@
+#ifndef ION_STORAGE_HELPER_H
+#define ION_STORAGE_HELPER_H
+
+namespace Ion {
+
+namespace Storage {
+// emscripten read and writes must be aligned.
+class StorageHelper {
+public:
+  static uint16_t unalignedShort(char * address) {
+#if __EMSCRIPTEN__
+    uint8_t f1 = *(address);
+    uint8_t f2 = *(address+1);
+    uint16_t f = (uint16_t)f1 + (((uint16_t)f2)<<8);
+    return f;
+#else
+    return *(uint16_t *)address;
+#endif
+  }
+  static void writeUnalignedShort(uint16_t value, char * address) {
+#if __EMSCRIPTEN__
+    *((uint8_t *)address) = (uint8_t)(value & ((1 << 8) - 1));
+    *((uint8_t *)address+1) = (uint8_t)(value >> 8);
+#else
+    *((uint16_t *)address) = value;
+#endif
+  }
+};
+
+}
+
+}
+
+#endif

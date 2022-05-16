44fd5c558e3d8dcb827fe3f8153b5179019b9ea1
Emilie Feral <emilie.feral@numworks.com>
2022-05-16 16:53:11 +0200

    [apps/inference] Create model class Table extracted from Chi2 model

---
+++ apps/inference/models/statistic/table.cpp
@@ -0,0 +1,63 @@
+#include "table.h"
+
+#include <algorithm>
+
+namespace Inference {
+
+bool Table::deleteParameterAtPosition(int row, int column) {
+  if (std::isnan(parameterAtPosition(row, column))) {
+    // Param is already deleted
+    return false;
+  }
+  setParameterAtPosition(k_undefinedValue, row, column);
+  for (int i = 0; i < maxNumberOfColumns(); i++) {
+    if (i != column && !std::isnan(parameterAtPosition(row, i))) {
+      // There is another non deleted value in this row
+      return false;
+    }
+  }
+  return true;
+}
+
+Table::Index2D Table::computeDimensions() const {
+  Table::Index2D dim = computeInnerDimensions();
+  bool displayLastEmptyRow = dim.row < maxNumberOfRows();
+  bool displayLastEmptyColumn = dim.col < maxNumberOfColumns();
+  Table::Index2D initialDim = initialDimensions();
+  return Index2D{.row = std::max(initialDim.row, dim.row + displayLastEmptyRow), .col = std::max(initialDim.col, dim.col + displayLastEmptyColumn)};
+}
+
+Table::Index2D Table::computeInnerDimensions() const {
+  int maxCol = -1, maxRow = -1;
+  for (int row = 0; row < maxNumberOfRows(); row++) {
+    for (int col = 0; col < maxNumberOfColumns(); col++) {
+      double p = parameterAtPosition(row, col);
+      if (!std::isnan(p)) {
+        if (row > maxRow) {
+          maxRow = row;
+        }
+        if (col > maxCol) {
+          maxCol = col;
+        }
+      }
+    }
+  }
+  return Index2D{.row = maxRow + 1, .col = maxCol + 1};
+}
+
+Table::Index2D Table::indexToIndex2D(int index) const {
+  return Index2D{
+      .row = index / maxNumberOfColumns(),
+      .col = index % maxNumberOfColumns()
+    };
+}
+
+int Table::index2DToIndex(Index2D indexes) const {
+  return index2DToIndex(indexes.row, indexes.col);
+}
+
+int Table::index2DToIndex(int row, int column) const {
+  return column + row * maxNumberOfColumns();
+}
+
+}  // namespace Inference
---
+++ apps/inference/models/statistic/table.h
@@ -0,0 +1,50 @@
+#ifndef PROBABILITY_MODELS_STATISTIC_TABLE_H
+#define PROBABILITY_MODELS_STATISTIC_TABLE_H
+
+#include <assert.h>
+#include <cmath>
+
+namespace Inference {
+
+class Table {
+public:
+  // Matrix special functions
+  void setParameterAtPosition(double value, int row, int column) {
+    assert(index2DToIndex(row, column) < numberOfTableParameters());
+    tableSetParameterAtIndex(value, index2DToIndex(row, column));
+  }
+  double parameterAtPosition(int row, int column) const { return tableParameterAtIndex(index2DToIndex(row, column)); }
+  bool authorizedParameterAtPosition(double p, int row, int column) const { return tableAuthorizedParameterAtIndex(p, index2DToIndex(row, column)); }
+  /* Delete parameter at location, return true if the deleted param was the last
+   * non-deleted value of its row or column. */
+  virtual bool deleteParameterAtPosition(int row, int column);
+  virtual void recomputeData() = 0;
+  virtual int maxNumberOfColumns() const = 0;
+  virtual int maxNumberOfRows() const = 0;
+
+  struct Index2D {
+    int row;
+    int col;
+  };
+  Index2D computeDimensions() const;
+
+  constexpr static float k_undefinedValue = NAN;
+
+protected:
+  Index2D computeInnerDimensions() const;
+  virtual Index2D initialDimensions() const = 0;
+
+  Index2D indexToIndex2D(int index) const;
+  int index2DToIndex(Index2D indexes) const;
+  int index2DToIndex(int row, int column) const;
+
+private:
+  virtual void tableSetParameterAtIndex(double p, int index) = 0;
+  virtual double tableParameterAtIndex(int index) const = 0;
+  virtual bool tableAuthorizedParameterAtIndex(double p, int i) const = 0;
+  virtual int numberOfTableParameters() const = 0;
+};
+
+}  // namespace Inference
+
+#endif /* PROBABILITY_MODELS_STATISTIC_CHI2_TEST_H */

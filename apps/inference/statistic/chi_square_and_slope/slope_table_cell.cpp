c938a5e02156b51fd83f34fcb7406ef601d57f5a
Emilie Feral <emilie.feral@numworks.com>
2022-05-18 09:50:22 +0200

    [apps/inference] Fix SlopeTableCell look

--- apps/inference/statistic/chi_square_and_slope/slope_table_cell.cpp
+++ apps/inference/statistic/chi_square_and_slope/slope_table_cell.cpp
@@ -11,9 +11,10 @@ SlopeTableCell::SlopeTableCell(Responder * parentResponder, DynamicSizeTableView
 {
   // TODO: use Felix constexpr
   m_header[0].setText("X1");
-  m_header[0].setText("Y1");
+  m_header[1].setText("Y1");
   for (int i = 0; i < k_maxNumberOfColumns; i++) {
     m_header[i].setColor(Escher::Palette::Red);
+    m_header[i].setOrientation(Shared::FunctionTitleCell::Orientation::HorizontalIndicator);
     m_header[i].setEven(true);
     m_header[i].setFont(KDFont::SmallFont);
   }

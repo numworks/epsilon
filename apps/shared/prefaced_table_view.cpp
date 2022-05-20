b9b1fd5d3c0b3a7ee789f3169096b6e7dfa9d9f8
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-05-20 14:49:19 +0200

    [shared/prefaced_table_view] Freeze row instead of column

--- apps/shared/prefaced_table_view.cpp
+++ apps/shared/prefaced_table_view.cpp
@@ -4,9 +4,9 @@ using namespace Escher;
 
 namespace Shared {
 
-  PrefacedTableView::PrefacedTableView(int prefaceColumn, Responder * parentResponder, SelectableTableView * mainTableView, TableViewDataSource * cellsDataSource, SelectableTableViewDelegate * delegate) :
+  PrefacedTableView::PrefacedTableView(int prefaceRow, Responder * parentResponder, SelectableTableView * mainTableView, TableViewDataSource * cellsDataSource, SelectableTableViewDelegate * delegate) :
   Escher::Responder(parentResponder),
-  m_prefaceDataSource(prefaceColumn, cellsDataSource),
+  m_prefaceDataSource(prefaceRow, cellsDataSource),
   m_prefaceView(&m_prefaceDataSource, &m_prefaceDataSource),
   m_mainTableView(mainTableView),
   m_mainTableDelegate(delegate),
@@ -23,10 +23,10 @@ void PrefacedTableView::setMargins(KDCoordinate top, KDCoordinate right, KDCoord
   m_mainTableView->setRightMargin(right);
   m_mainTableView->setBottomMargin(bottom);
   m_mainTableView->setLeftMargin(left);
-  m_storedMargin = left;
+  m_storedMargin = top;
 
-  m_prefaceView.setTopMargin(top);
-  m_prefaceView.setBottomMargin(bottom);
+  m_prefaceView.setLeftMargin(left);
+  m_prefaceView.setRightMargin(right);
 }
 
 void PrefacedTableView::setBackgroundColor(KDColor color) {
@@ -50,11 +50,11 @@ void PrefacedTableView::tableViewDidChangeSelection(Escher::SelectableTableView 
 
 void PrefacedTableView::tableViewDidChangeSelectionAndDidScroll(Escher::SelectableTableView * t, int previousSelectedCellX, int previousSelectedCellY, bool withinTemporarySelection) {
   assert(t == m_mainTableView);
-  if (!m_prefaceDataSource.prefaceFullyInFrame(m_mainTableView->contentOffset().x())) {
+  if (!m_prefaceDataSource.prefaceFullyInFrame(m_mainTableView->contentOffset().y())) {
     /* Do not call setContentOffset if the preface is not going to appear.
      * Otherwise, as setContentOffset calls layoutSubviews, the preface would
      * "steal" its first cell from the table. */
-    m_prefaceView.setContentOffset(KDPoint(0, m_mainTableView->contentOffset().y()));
+    m_prefaceView.setContentOffset(KDPoint(m_mainTableView->contentOffset().x(), 0));
   }
   if (m_mainTableDelegate) {
     m_mainTableDelegate->tableViewDidChangeSelectionAndDidScroll(t, previousSelectedCellX, previousSelectedCellY, withinTemporarySelection);
@@ -64,22 +64,22 @@ void PrefacedTableView::tableViewDidChangeSelectionAndDidScroll(Escher::Selectab
 
 void PrefacedTableView::layoutSubviews(bool force) {
   KDPoint mainContentOffset = m_mainTableView->contentOffset();
-  bool hidePreface = m_prefaceDataSource.prefaceFullyInFrame(mainContentOffset.x()) || m_mainTableView->selectedRow() == -1;
+  bool hidePreface = m_prefaceDataSource.prefaceFullyInFrame(mainContentOffset.y()) || m_mainTableView->selectedRow() == -1;
   if (hidePreface) {
-    m_mainTableView->setLeftMargin(m_storedMargin);
+    m_mainTableView->setTopMargin(m_storedMargin);
     m_mainTableView->setFrame(bounds(), force);
-    m_prefaceView.setRightMargin(0);
+    m_prefaceView.setBottomMargin(0);
     m_prefaceView.setFrame(KDRectZero, force);
   } else {
-    m_prefaceView.setRightMargin(m_marginDelegate ? m_marginDelegate->prefaceMargin(&m_prefaceView) : 0);
-    KDCoordinate prefaceWidth = m_prefaceView.minimalSizeForOptimalDisplay().width();
+    m_prefaceView.setBottomMargin(m_marginDelegate ? m_marginDelegate->prefaceMargin(&m_prefaceView) : 0);
+    KDCoordinate prefaceHeight = m_prefaceView.minimalSizeForOptimalDisplay().height();
     if (m_prefaceView.bounds().isEmpty()) {
-      m_mainTableView->setLeftMargin(0);
+      m_mainTableView->setTopMargin(0);
     }
-    m_mainTableView->setFrame(KDRect(prefaceWidth, 0, bounds().width() - prefaceWidth, bounds().height()), force);
-    /* Scroll to reset the update the content offset with the new margins. */
+    m_mainTableView->setFrame(KDRect(0, prefaceHeight, bounds().width(), bounds().height() - prefaceHeight), force);
+    /* Scroll to update the content offset with the new margins. */
     m_mainTableView->scrollToCell(m_mainTableView->selectedColumn(), m_mainTableView->selectedRow());
-    m_prefaceView.setFrame(KDRect(0, 0, prefaceWidth, bounds().height()), force);
+    m_prefaceView.setFrame(KDRect(0, 0, bounds().width(), prefaceHeight), force);
   }
 }
 

d97bfdb688ac4901ad9a7e047cc9a25e7847fff4
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-05-20 13:57:52 +0200

    Improve some comments

--- ion/src/device/include/n0120/config/board.h
+++ ion/src/device/include/n0120/config/board.h
@@ -36,7 +36,28 @@ constexpr uint32_t BootloaderLength = InternalFlashLength - PseudoOTPLength - Tr
 constexpr uint32_t PseudoOTPOrigin = BootloaderOrigin + BootloaderLength;
 constexpr uint32_t TrampolineOrigin = PseudoOTPOrigin + PseudoOTPLength;
 
-/* External flash */
+/* External flash
+ *
+ * ||                                                    EXTERNAL fLASH (8mIb)                                                         ||
+ * ||         64K          |      64K or 0     |              61~62 * 64K               |       64K        ||         64 * 64K         ||
+ * ||                           SIGNED PAYLOAD A                        | EXTERNAL APPS | PERSISTING BYTES || ... Identical B Slot ... ||
+ * || SP HEADER | KERNEL A | BOOTLOADER UPDATE | USERLAND A | SIGNATURE |       "       |        "         ||                          ||
+ *
+ * - SP HEADER: || SECURITY LEVEL | PAYLOAD LENGTH (WITHOUT HEADER) ||
+ * - KERNEL:    || HEADER | INIT VECTOR | CODE | 0b111...1 ||
+ * - USERLAND:  || HEADER | INIT VECTOR | CODE ||
+ *
+ * Kernel and Userland are prefixed by:
+ * - a header listing various information that have to be retrieved from DFU
+ * - a initialisation vector starting with the entry point and the stack initialization
+ *
+ * The kernel initialisation vector table has specific requirements regarding:
+ * - its memory space: it must be in the range 0x00000080 to 0x3FFFFF80
+ * - its alignement: you must align the offset to the number of exception entries in the vector table. The minimum alignment is 128 words.
+ * [https://www.st.com/content/ccc/resource/technical/document/programming_manual/6c/3a/cb/e7/e4/ea/44/9b/DM00046982.pdf/files/DM00046982.pdf/jcr:content/translations/en.DM00046982.pdf]
+ * Therefore, its is also relocated in sRAM at booting.
+ */
+
 constexpr uint32_t ExternalFlashOrigin = 0x90000000;
 constexpr uint32_t ExternalFlashLength = 0x800000; // 8MiB
 constexpr uint32_t StandardExternalFlashSectorLength = 0x10000; // 64KiB
@@ -64,7 +85,28 @@ constexpr uint32_t UserlandVirtualOrigin = ExtraDataVirtualOrigin + ExtraDataLen
 
 constexpr uint32_t ExternalAppsSectorUnit = 0x10000;
 
-/* SRAM */
+/* SRAM
+ *
+ * |                                SRAM                                                 |
+ * | USERLAND DATA/BSS | HEAP | USERLAND STACK | KERNEL STACK | KERNEL DATA/BSS - canary |
+ *
+ * Bootloader SRAM is mapped like:
+ * |                                    STACK                                   | canary |
+ * Kernel SRAM is mapped like:
+ * | xxxxxxxxxxxxxxxxxxxxxxxx | USERLAND STACK | KERNEL STACK | KERNEL DATA/BSS - canary |
+ * Userland SRAM is mapped like:
+ * | USERLAND DATA/BSS | HEAP | USERLAND STACK | xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx |
+ *
+ * The few restrictions to layout the RAM memory are the following:
+ * - the begining of Kernel data/bss section should be aligned: the isr table
+ * is within this section and the VTOR registers can only be configure with
+ * some alignments constraints.
+ * - the kernel RAM should be aligned to be able to add MPU protection over
+ * this region: MPU region must be aligned to the size of the region.
+ * - Overflowing the userland stack should not impact the kernel (the MPU region
+ * should protect it anyway).
+ */
+
 constexpr uint32_t SRAMOrigin = 0x24000000;
 constexpr uint32_t SRAMLength = 0x40000; // 256KiB
 

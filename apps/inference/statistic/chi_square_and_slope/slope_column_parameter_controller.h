437fe587a65b80e399e145eb7d1269882ddafa1f
Emilie Feral <emilie.feral@numworks.com>
2022-05-24 17:40:16 +0200

    [apps/inference] Fix title and cells of SlopeTableCell::ColumnParameterController

---
+++ apps/inference/statistic/chi_square_and_slope/slope_column_parameter_controller.h
@@ -0,0 +1,23 @@
+#ifndef INFERENCE_SLOPE_COLUMN_PARAMETER_CONTROLLER_H
+#define INFERENCE_SLOPE_COLUMN_PARAMETER_CONTROLLER_H
+
+#include "shared/store_parameter_controller.h"
+
+namespace Inference {
+
+class SlopeColumnParameterController : public Shared::StoreParameterController {
+public:
+  using StoreParameterController::StoreParameterController;
+  Escher::ViewController::TitlesDisplay titlesDisplay() override { return Escher::ViewController::TitlesDisplay::DisplayLastTwoTitles; }
+  int numberOfRows() const override { return sizeof(k_typesOrder)/sizeof(int); }
+  int typeAtIndex(int index) override { return k_typesOrder[index]; }
+
+private:
+  constexpr static int k_typesOrder[] = {k_sortCellType, k_fillFormulaCellType, k_clearCellType};
+
+};
+
+}
+
+#endif
+

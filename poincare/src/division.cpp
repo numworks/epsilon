c8445b56066f5c6b9b4ea694ce7cc8720c8a7a99
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-05-24 15:34:32 +0200

    [poincare] Preserve % on denominator

--- poincare/src/division.cpp
+++ poincare/src/division.cpp
@@ -44,7 +44,7 @@ bool DivisionNode::childNeedsSystemParenthesesAtSerialization(const TreeNode * c
   if (static_cast<const ExpressionNode *>(child)->type() == Type::Rational && !static_cast<const RationalNode *>(child)->isInteger()) {
     return true;
   }
-  return static_cast<const ExpressionNode *>(child)->isOfType({Type::Subtraction, Type::Opposite, Type::Multiplication, Type::Division, Type::Addition});
+  return static_cast<const ExpressionNode *>(child)->isOfType({Type::Subtraction, Type::Opposite, Type::Multiplication, Type::Division, Type::Addition, Type::Percent});
 }
 
 int DivisionNode::serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const {

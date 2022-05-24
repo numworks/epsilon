8bffc30ec9109c972d35e1a15aaa719189465a12
Emilie Feral <emilie.feral@numworks.com>
2022-05-24 17:29:03 +0200

    [apps/inference] Add a ColumnParamaterController to InputSlopeController

--- apps/inference/statistic/chi_square_and_slope/input_slope_controller.cpp
+++ apps/inference/statistic/chi_square_and_slope/input_slope_controller.cpp
@@ -5,8 +5,19 @@ namespace Inference {
 
 InputSlopeController::InputSlopeController(StackViewController * parent, Escher::ViewController * resultsController, Statistic * statistic, Escher::InputEventHandlerDelegate * inputEventHandlerDelegate, Poincare::Context * context) :
   InputCategoricalController(parent, resultsController, statistic, inputEventHandlerDelegate),
-  m_slopeTableCell(&m_selectableTableView, this, this, statistic, context)
+  m_slopeTableCell(&m_selectableTableView, this, this, statistic, context),
+  m_storeParameterController(nullptr, &m_slopeTableCell)
 {
+}
+
+bool InputSlopeController::handleEvent(Ion::Events::Event event) {
+  if ((event == Ion::Events::OK || event == Ion::Events::EXE) && selectedRow() == 0) {
+    m_storeParameterController.initializeColumnParameters();
+    m_storeParameterController.selectRow(0);
+    stackController()->push(&m_storeParameterController);
+    return true;
+  }
+  return InputCategoricalController::handleEvent(event);
 }
 
 }

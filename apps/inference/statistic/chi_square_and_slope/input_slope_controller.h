99581626dc17be36fe8af141165c7ef81168f93c
Emilie Feral <emilie.feral@numworks.com>
2022-05-25 17:23:20 +0200

    [apps/inference] Fix InputSlopeController number of title banners

--- apps/inference/statistic/chi_square_and_slope/input_slope_controller.h
+++ apps/inference/statistic/chi_square_and_slope/input_slope_controller.h
@@ -12,6 +12,8 @@ using namespace Escher;
 
 namespace Inference {
 
+// TODO: add another StackViewController here to push the SlopeColumnParameterController on it and display it with Style::WhiteUniform
+
 class InputSlopeController : public InputCategoricalController, public Shared::LayoutFieldDelegate, public Shared::InputEventHandlerDelegate {
 public:
   InputSlopeController(StackViewController * parent, Escher::ViewController * resultsController, Statistic * statistic, Escher::InputEventHandlerDelegate * inputEventHandlerDelegate, Poincare::Context * parentContext);
@@ -25,7 +27,7 @@ public:
     return m_titleBuffer;
   }
   ViewController::TitlesDisplay titlesDisplay() override {
-    return ViewController::TitlesDisplay::DisplayLastTwoTitles;
+    return m_statistic->subApp() == Inference::SubApp::Interval ? ViewController::TitlesDisplay::DisplayLastTitle : ViewController::TitlesDisplay::DisplayLastTwoTitles;
   }
 
 private:
--- apps/inference/statistic/chi_square_and_slope/slope_column_parameter_controller.h
+++ apps/inference/statistic/chi_square_and_slope/slope_column_parameter_controller.h
@@ -8,13 +8,15 @@ namespace Inference {
 class SlopeColumnParameterController : public Shared::StoreParameterController {
 public:
   using StoreParameterController::StoreParameterController;
-  Escher::ViewController::TitlesDisplay titlesDisplay() override { return Escher::ViewController::TitlesDisplay::DisplayLastThreeTitles; }
+  Escher::ViewController::TitlesDisplay titlesDisplay() override { return m_titlesDisplay; }
+  void setTitlesDisplay(Escher::ViewController::TitlesDisplay titlesDisplay) { m_titlesDisplay = titlesDisplay; }
   int numberOfRows() const override { return sizeof(k_typesOrder)/sizeof(int); }
   int typeAtIndex(int index) override { return k_typesOrder[index]; }
 
 private:
   constexpr static int k_typesOrder[] = {k_sortCellType, k_fillFormulaCellType, k_clearCellType};
 
+  Escher::ViewController::TitlesDisplay m_titlesDisplay;
 };
 
 }

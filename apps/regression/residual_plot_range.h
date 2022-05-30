921451485451fad6b541906cb33f895f84138d0f
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-05-30 15:06:48 +0200

    [apps/regression] Cap ranges of ResidualPlot

---
+++ apps/regression/residual_plot_range.h
@@ -0,0 +1,19 @@
+#ifndef REGRESSION_RESIDUAL_PLOT_RANGE_H
+#define REGRESSION_RESIDUAL_PLOT_RANGE_H
+
+#include <apps/shared/memoized_curve_view_range.h>
+
+namespace Regression {
+
+class ResidualPlotRange : public Shared::MemoizedCurveViewRange {
+public:
+  using Shared::MemoizedCurveViewRange::MemoizedCurveViewRange;
+  // Calibrate the range to respect margin and display all data
+  void calibrate(double xMin, double xMax, double yMin, double yMax, KDCoordinate height, KDCoordinate bannerHeight);
+private:
+  static constexpr double k_relativeMargin = 0.1;
+};
+
+}
+
+#endif

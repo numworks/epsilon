b170eca8c02d0c272b91b38025ad6354554a6b23
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-05-31 15:56:15 +0200

    [apps/statistics] reset selectedBoxCalculation
    after changing outlier display status

--- apps/statistics/graph/box_parameter_controller.cpp
+++ apps/statistics/graph/box_parameter_controller.cpp
@@ -4,9 +4,10 @@ using namespace Escher;
 
 namespace Statistics {
 
-BoxParameterController::BoxParameterController(Responder * parentResponder, Store * store) :
+BoxParameterController::BoxParameterController(Responder * parentResponder, Store * store, int * selectedBoxCalculation) :
   SelectableCellListPage<MessageTableCellWithSwitch, k_numberOfParameterCells, Escher::RegularListViewDataSource>(parentResponder),
-  m_store(store)
+  m_store(store),
+  m_selectedBoxCalculation(selectedBoxCalculation)
 {
   cellAtIndex(0)->setMessage(I18n::Message::DisplayOutliers);
   // Being the only cell, it is always highlighted.
@@ -24,6 +25,8 @@ bool BoxParameterController::handleEvent(Ion::Events::Event event) {
   if (event == Ion::Events::OK || event == Ion::Events::EXE) {
     m_store->setDisplayOutliers(!m_store->displayOutliers());
     cellAtIndex(0)->setState(m_store->displayOutliers());
+    // Reset selected box calculation because the total number may have changed
+    *m_selectedBoxCalculation = 0;
     return true;
   }
   return false;
--- apps/statistics/graph/box_parameter_controller.h
+++ apps/statistics/graph/box_parameter_controller.h
@@ -12,13 +12,14 @@ constexpr static int k_numberOfParameterCells = 1;
 
 class BoxParameterController : public Escher::SelectableCellListPage<Escher::MessageTableCellWithSwitch, k_numberOfParameterCells, Escher::RegularListViewDataSource> {
 public:
-  BoxParameterController(Escher::Responder * parentResponder, Store * store);
+  BoxParameterController(Escher::Responder * parentResponder, Store * store, int * selectedBoxCalculation);
   void viewWillAppear() override;
   const char * title() override { return I18n::translate(I18n::Message::StatisticsGraphSettings); }
   void willDisplayCellForIndex(Escher::HighlightCell * cell, int index) override;
 private:
   bool handleEvent(Ion::Events::Event event) override;
   Store * m_store;
+  int * m_selectedBoxCalculation;
 };
 
 }

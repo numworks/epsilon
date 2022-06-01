715a17079299ee102e1743efba8d21dcd08b7ef4
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-06-01 15:44:17 +0200

    [inference/legend_view] Clean up class
    Legend is static and does not require a buffer, a message will suffice.

--- apps/inference/statistic/test/legend_view.cpp
+++ apps/inference/statistic/test/legend_view.cpp
@@ -1,29 +1,38 @@
 #include "legend_view.h"
-
 #include <escher/palette.h>
+
+using namespace Escher;
 
 namespace Inference {
 
 void LegendView::DotView::drawRect(KDContext * ctx, KDRect rect) const {
-  ctx->fillAntialiasedCircle(KDPointZero, k_diameter / 2, m_color, Escher::Palette::WallScreen);
+  ctx->fillAntialiasedCircle(KDPointZero, k_diameter / 2, m_color, Palette::WallScreen);
 }
 
-LegendView::LegendView(const char * label, KDColor color) : m_icon(color) {
-  m_labelView.setText(label);
-  m_labelView.setBackgroundColor(Escher::Palette::WallScreen);
-  m_labelView.setFont(KDFont::SmallFont);
-  m_labelView.setTextColor(Escher::Palette::GrayVeryDark);
+LegendView::LegendView() :
+  m_pValueLabel(KDFont::SmallFont, I18n::Message::PValue, 0.0, 0.0, Palette::GrayVeryDark, Palette::WallScreen),
+  m_pValueIcon(KDColorOrange)
+{}
+
+View * LegendView::subviewAtIndex(int i) {
+  switch (i) {
+  case 0:
+    return &m_pValueLabel;
+  default:
+    assert(i == 1);
+    return &m_pValueIcon;
+  }
 }
 
 KDSize LegendView::minimalSizeForOptimalDisplay() const {
-  KDSize labelSize = m_labelView.minimalSizeForOptimalDisplay();
+  KDSize labelSize = m_pValueLabel.minimalSizeForOptimalDisplay();
   return KDSize(k_diameter + k_marginBetween + labelSize.width(), labelSize.height() + k_offsetTop);
 }
 
 void LegendView::layoutSubviews(bool force) {
-  m_icon.setFrame(KDRect(KDPoint(0, k_offsetTop), k_diameter, k_diameter), force);
-  KDSize labelSize = m_labelView.minimalSizeForOptimalDisplay();
-  m_labelView.setFrame(KDRect(k_diameter + k_marginBetween, 0, labelSize), force);
+  m_pValueIcon.setFrame(KDRect(KDPoint(0, k_offsetTop), k_diameter, k_diameter), force);
+  KDSize labelSize = m_pValueLabel.minimalSizeForOptimalDisplay();
+  m_pValueLabel.setFrame(KDRect(k_diameter + k_marginBetween, 0, labelSize), force);
 }
 
 }  // namespace Inference

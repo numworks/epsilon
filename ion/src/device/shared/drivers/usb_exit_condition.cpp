966f8aaf0bc696066823b1fbba68b7908c0e24bb
Emilie Feral <emilie.feral@numworks.com>
2022-06-02 15:28:56 +0200

    [ion] Tiny optimization of USB::shouldInterruptDFU

--- ion/src/device/shared/drivers/usb_exit_condition.cpp
+++ ion/src/device/shared/drivers/usb_exit_condition.cpp
@@ -6,6 +6,12 @@ namespace Ion {
 namespace Device {
 namespace USB {
 
+void scanKey(Keyboard::Key key, Events::Event * event) {
+  if (*event == Events::None && Keyboard::columnIsActive(Keyboard::columnForKey(key))) {
+    *event = Events::Event::PlainKey(key);
+  }
+}
+
 // On N100 this function is called directly via the in-RAM relocated DFU
 Events::Event shouldInterruptDFU() {
   /* WARNING: we can't use hardware interruptions are their code is within the
@@ -13,14 +19,15 @@ Events::Event shouldInterruptDFU() {
    * Using Keyboard::scan here would be too slow, a mere
    * Ion::Timing::usleep(1000) is preventing the DFU device from being detected
    * and scan waits 100us for each row. */
-  Keyboard::Key interruptKeys[] = {Keyboard::Key::Back, Keyboard::Key::Home, Keyboard::Key::OnOff};
-  for (Keyboard::Key key : interruptKeys) {
-    Keyboard::activateRow(Keyboard::rowForKey(key));
-    if (Keyboard::columnIsActive(Keyboard::columnForKey(key))) {
-      return Events::Event::PlainKey(key);
-    }
-  }
-  return Events::None;
+  Events::Event event = Events::None;
+  Keyboard::activateRow(Keyboard::rowForKey(Keyboard::Key::Back));
+  scanKey(Keyboard::Key::Back, &event);
+  Keyboard::activateRow(Keyboard::rowForKey(Keyboard::Key::Home));
+  scanKey(Keyboard::Key::Home, &event);
+  // Avoid spending time re-activating the same row
+  assert(Keyboard::rowForKey(Keyboard::Key::OnOff) == Keyboard::rowForKey(Keyboard::Key::Home));
+  scanKey(Keyboard::Key::OnOff, &event);
+  return event;
 }
 
 }

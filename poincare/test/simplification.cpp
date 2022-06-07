4c274b8eb7eee24ca950b5e9c1f6204f13d62f83
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-06-07 14:39:47 +0200

    [poincare] Handle implicit multiplication after
    the percent.

--- poincare/test/simplification.cpp
+++ poincare/test/simplification.cpp
@@ -756,6 +756,7 @@ QUIZ_CASE(poincare_simplification_percent) {
   assert_parsed_expression_simplify_to("80+20-20%", "100×(1-20/100)");
   assert_parsed_expression_simplify_to("80+10*20%", "200/100+80");
   assert_parsed_expression_simplify_to("80+20%*10", "10×20/100+80");
+  assert_parsed_expression_simplify_to("80+20%π", "π×20/100+80");
 }
 
 QUIZ_CASE(poincare_simplification_logarithm) {

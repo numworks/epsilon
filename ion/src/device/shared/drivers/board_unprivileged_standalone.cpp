8670dce685810f8f5742ed80e512e5117e9bb944
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-06-16 16:03:16 +0200

    [ion/n0100/kernel] Fix address of kernel header

--- ion/src/device/shared/drivers/board_unprivileged_standalone.cpp
+++ ion/src/device/shared/drivers/board_unprivileged_standalone.cpp
@@ -1,13 +1,14 @@
 #include <config/board.h>
 #include <drivers/board.h>
 #include <drivers/kernel_header.h>
+#include <shared/boot/isr.h>
 
 namespace Ion {
 namespace Device {
 namespace Board {
 
 KernelHeader * kernelHeader() {
-  return reinterpret_cast<KernelHeader *>(Config::KernelVirtualOrigin + Config::SignedPayloadLength);
+  return reinterpret_cast<KernelHeader *>(Config::KernelVirtualOrigin + INITIALISATION_VECTOR_SIZE * sizeof(void *));
 }
 
 UserlandHeader * userlandHeader() {

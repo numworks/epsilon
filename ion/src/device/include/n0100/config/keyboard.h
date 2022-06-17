d0bddce93e1f5ed36c780f145e9f474357877107
Emilie Feral <emilie.feral@numworks.com>
2022-06-17 17:31:14 +0200

    [ion] Fix N0100 Keyboard by reverting to previous behaviour

--- ion/src/device/include/n0100/config/keyboard.h
+++ ion/src/device/include/n0100/config/keyboard.h
@@ -69,12 +69,14 @@ inline uint64_t ValidKeys(uint64_t state) {
   return state & 0x1F7DF7FFFFF0FF;
 }
 
-/* TODO: 100 us seems to work, but wasn't really calculated. On N0100 models,
- * it doesn't slow down USB::shouldInterruptDFU() enough to break the USB
- * protocole. However 10us would not be enough on N0100; it would generate
- * double keys for single user inputs. */
+/* TODO: 100 us seems to work, but wasn't really calculated. However, 10 us was
+ * not enough on N0100 models: the row activation was dodgy. Consequently, on
+ * n0100, we can interrupt the DFU application only with one key (Back) because
+ * activating other rows would take too much time and break the DFU synchrone
+ * protocole.
+ */
 constexpr int RowActivationDelay = 100; // us
-constexpr int DebouncingDelay = Clocks::Config::AHBLowFrequencyPrescaler * 10;
+constexpr int DebouncingDelay = 10; // Empirical value
 constexpr int PollDelay = Clocks::Config::AHBLowFrequencyPrescaler * 100;
 
 }

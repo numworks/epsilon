682acbd71c4ef69e35b6b9d1de4795d250693707
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-06-20 17:45:49 +0200

    [ion/persisting_bytes] Remove useless constant

--- ion/include/ion/persisting_bytes.h
+++ ion/include/ion/persisting_bytes.h
@@ -7,9 +7,8 @@ namespace Ion {
 namespace PersistingBytes {
 
 /* Warning : All persisting bytes are expected to be initialized to 0 in
- * shared_kernel_flash.ld which assumes k_numberOfPersistingBytes is 2. */
+ * shared_kernel_flash.ld which assumes PersistingBytesInt is 16 bits long */
 typedef uint16_t PersistingBytesInt;
-static constexpr uint8_t k_numberOfPersistingBytes = sizeof(PersistingBytesInt);
 
 void write(PersistingBytesInt value);
 PersistingBytesInt read();

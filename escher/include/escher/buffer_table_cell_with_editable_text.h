e30984b342905fb667c120b09252503b2f3d3fd9
MarcNumworks <marc.coudriau@numworks.com>
2022-06-22 17:53:14 +0200

    [escher] Add a table cell with a buffer and an editable text

---
+++ escher/include/escher/buffer_table_cell_with_editable_text.h
@@ -0,0 +1,43 @@
+#ifndef ESCHER_BUFFER_TABLE_CELL_WITH_EDITABLE_TEXT_H
+#define ESCHER_BUFFER_TABLE_CELL_WITH_EDITABLE_TEXT_H
+
+#include <escher/editable_text_cell.h>
+#include <escher/buffer_table_cell.h>
+#include <escher/responder.h>
+#include <poincare/print_float.h>
+#include <escher/container.h>
+
+namespace Escher {
+
+class BufferTableCellWithEditableText : public Responder, public BufferTableCell {
+public:
+  BufferTableCellWithEditableText(Responder * parentResponder = nullptr, InputEventHandlerDelegate * inputEventHandlerDelegate = nullptr, TextFieldDelegate * textFieldDelegate = nullptr);
+  const View * accessoryView() const override { return &m_textField; }
+  TextField * textField() { return &m_textField; }
+  const char * editedText() const { return m_textField.text(); }
+  void didBecomeFirstResponder() override { Container::activeApp()->setFirstResponder(&m_textField); }
+  bool isEditing() { return m_textField.isEditing(); }
+  void setEditing(bool isEditing) { m_textField.setEditing(isEditing); }
+  void setHighlighted(bool highlight) override;
+  Responder * responder() override { return this; }
+  const char * text() const override { return !m_textField.isEditing() ? m_textField.text() : nullptr; }
+  void setAccessoryText(const char * text);
+  bool shouldAlignSublabelRight() const override { return false; }
+  bool shouldAlignLabelAndAccessory() const override { return true; }
+  bool giveAccessoryAllWidth() const override { return true; }
+  KDCoordinate accessoryMinimalWidthOverridden() const override {
+    return Poincare::PrintFloat::glyphLengthForFloatWithPrecision(
+               Poincare::Preferences::ShortNumberOfSignificantDigits + 1) *
+           m_textField.font()->glyphSize().width() + TextCursorView::k_width;
+  }
+  virtual void setDelegates(InputEventHandlerDelegate * inputEventHandlerDelegate,
+                            TextFieldDelegate * textFieldDelegate);
+
+private:
+  TextField m_textField;
+  char m_textBody[Poincare::PrintFloat::k_maxFloatCharSize];
+};
+
+}
+
+#endif
---
+++ escher/src/buffer_table_cell_with_editable_text.cpp
@@ -0,0 +1,40 @@
+#include <escher/buffer_table_cell_with_editable_text.h>
+#include <escher/palette.h>
+
+namespace Escher {
+
+BufferTableCellWithEditableText::BufferTableCellWithEditableText(
+    Responder * parentResponder,
+    InputEventHandlerDelegate * inputEventHandlerDelegate,
+    TextFieldDelegate * textFieldDelegate) :
+    Responder(parentResponder),
+    BufferTableCell(),
+    m_textField(this,
+                m_textBody,
+                Poincare::PrintFloat::k_maxFloatCharSize,
+                TextField::maxBufferSize(),
+                inputEventHandlerDelegate,
+                textFieldDelegate,
+                KDFont::LargeFont,
+                KDContext::k_alignRight, KDContext::k_alignCenter,
+                KDColorBlack) {
+  m_textBody[0] = '\0';
+}
+
+void BufferTableCellWithEditableText::setHighlighted(bool highlight) {
+  BufferTableCell::setHighlighted(highlight);
+  KDColor backgroundColor = highlight? Palette::Select : KDColorWhite;
+  m_textField.setBackgroundColor(backgroundColor);
+}
+
+void BufferTableCellWithEditableText::setAccessoryText(const char * text) {
+  m_textField.setText(text);
+  layoutSubviews();
+}
+
+void BufferTableCellWithEditableText::setDelegates(InputEventHandlerDelegate * inputEventHandlerDelegate,
+                                                    TextFieldDelegate * textFieldDelegate) {
+  m_textField.setDelegates(inputEventHandlerDelegate, textFieldDelegate);
+}
+
+}

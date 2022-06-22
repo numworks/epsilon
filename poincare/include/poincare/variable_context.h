a3bf9bb9128d9fe5864ea3d0776f0dc10a876a3a
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-06-22 14:18:53 +0200

    [poincare/variable_context] Add constructor for self-referential layout

--- poincare/include/poincare/variable_context.h
+++ poincare/include/poincare/variable_context.h
@@ -3,15 +3,17 @@
 
 #include <poincare/context_with_parent.h>
 #include <poincare/float.h>
+#include <poincare/symbol_abstract.h>
 
 namespace Poincare {
 
 class VariableContext : public ContextWithParent {
 public:
-  VariableContext(const char * name, Context * parentContext) :
-    ContextWithParent(parentContext),
-    m_name(name)
-  {}
+  VariableContext(const char * name, Context * parentContext) : ContextWithParent(parentContext), m_name(name) {}
+  /* Building a self referential context is a trick for parsing parametered
+   * expression. */
+  VariableContext(SymbolAbstract symbol, Context * parentContext) : ContextWithParent(parentContext), m_name(symbol.name()), m_value(symbol) {}
+
   template<typename T>
   void setApproximationForVariable(T value);
 

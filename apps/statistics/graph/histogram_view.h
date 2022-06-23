5753b91e04f5dc037d31f92b58368eadfc73ca1a
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-06-23 11:23:44 +0200

    [statistics] Simplify HistogramView constructor

--- apps/statistics/graph/histogram_view.h
+++ apps/statistics/graph/histogram_view.h
@@ -12,7 +12,7 @@ class HistogramController;
 
 class HistogramView : public Shared::HorizontallyLabeledCurveView {
 public:
-  HistogramView(Store * store, int series, Shared::CurveViewRange * curveViewRange, KDColor selectedHistogramColor = Escher::Palette::Select, KDColor notSelectedHistogramColor = Escher::Palette::GrayMiddle, KDColor selectedBarColor = Escher::Palette::YellowDark);
+  HistogramView(Store * store, int series, Shared::CurveViewRange * curveViewRange);
   int series() const { return m_series; }
   void reload(bool resetInterrupted = false, bool force = false) override;
   void reloadSelectedBar();
@@ -20,14 +20,14 @@ public:
   void setHighlight(float start, float end);
   void setDisplayLabels(bool display) { m_displayLabels = display; }
 private:
+  constexpr static KDColor k_notSelectedHistogramColor = Escher::Palette::GrayMiddle;
+  constexpr static KDColor k_selectedBarColor = Escher::Palette::YellowDark;
+
   Store * m_store;
   static float EvaluateHistogramAtAbscissa(float abscissa, void * model, void * context);
   float m_highlightedBarStart;
   float m_highlightedBarEnd;
   int m_series;
-  KDColor m_selectedHistogramColor;
-  KDColor m_notSelectedHistogramColor;
-  KDColor m_selectedBarColor;
   bool m_displayLabels;
 };
 

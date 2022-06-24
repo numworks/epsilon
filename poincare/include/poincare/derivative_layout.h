7ce001580b86eeb8902086cfd560017b1817680e
Félix Richart <felix.richart@numworks.com>
2022-06-24 13:58:07 +0200

    [poincare] Split DerivativeLayout into FirstOrder and HigherOrder
    Doesn't work on its own.

--- poincare/include/poincare/derivative_layout.h
+++ poincare/include/poincare/derivative_layout.h
@@ -12,36 +12,27 @@ public:
   DerivativeLayoutNode() : LayoutNode(), m_variableChildInFractionSlot(true) {}
 
   // LayoutNode
-  Type type() const override { return Type::DerivativeLayout; }
   int serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode = Preferences::PrintFloatMode::Decimal, int numberOfSignificantDigits = 0) const override;
   void moveCursorLeft(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool forSelection = false) override;
   void moveCursorRight(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool forSelection = false) override;
   void moveCursorDown(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool equivalentPositionVisited = false, bool forSelection = false) override;
+  void moveCursorUp(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool equivalentPositionVisited = false, bool forSelection = false) override;
   void deleteBeforeCursor(LayoutCursor * cursor) override;
   Layout XNTLayout(int childIndex = -1) const override;
 
-  // TreeNode
-  size_t size() const override { return sizeof(DerivativeLayoutNode); }
-  int numberOfChildren() const override { return 3; }
-#if POINCARE_TREE_LOG
-  void logNodeName(std::ostream & stream) const override {
-    stream << "DerivativeLayout";
-  }
-#endif
-
 protected:
-  KDSize computeSize(KDFont::Size font) override;
-  KDCoordinate computeBaseline(KDFont::Size font) override;
-  KDPoint positionOfChild(LayoutNode * child, KDFont::Size font) override;
-
-private:
-  // diff(f(x), x, a)
+  // diff(f(x), x, a, ..)
   constexpr static int k_derivandLayoutIndex = 0;
   constexpr static int k_variableLayoutIndex = 1;
   constexpr static int k_abscissaLayoutIndex = 2;
+
   LayoutNode * derivandLayout() { return childAtIndex(k_derivandLayoutIndex); }
   LayoutNode * variableLayout() { return childAtIndex(k_variableLayoutIndex); }
   LayoutNode * abscissaLayout() { return childAtIndex(k_abscissaLayoutIndex); }
+
+  KDSize computeSize(KDFont::Size font) override;
+  KDCoordinate computeBaseline(KDFont::Size font) override;
+  KDPoint positionOfChild(LayoutNode * child, KDFont::Size font) override;
 
   KDPoint positionOfVariableInFractionSlot(KDFont::Size font);
   KDPoint positionOfVariableInAssignmentSlot(KDFont::Size font);
@@ -61,10 +52,72 @@ private:
   bool m_variableChildInFractionSlot;
 };
 
-class DerivativeLayout final : public LayoutThreeChildren<DerivativeLayout, DerivativeLayoutNode> {
+class FirstOrderDerivativeLayoutNode final : public DerivativeLayoutNode {
+  // diff(f(x), x, a)
 public:
-  DerivativeLayout() = delete;
+  // LayoutNode
+  Type type() const override { return Type::FirstOrderDerivativeLayout; }
+  void moveCursorLeft(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool forSelection = false) override;
+  void moveCursorRight(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool forSelection = false) override;
+  void moveCursorDown(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool equivalentPositionVisited = false, bool forSelection = false) override;
+  void moveCursorUp(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool equivalentPositionVisited = false, bool forSelection = false) override;
+
+  // TreeNode
+  size_t size() const override { return sizeof(FirstOrderDerivativeLayoutNode); }
+  int numberOfChildren() const override { return k_numberOfChildren; }
+#if POINCARE_TREE_LOG
+  void logNodeName(std::ostream & stream) const override {
+    stream << "FirstOrderDerivativeLayout";
+  }
+#endif
+
+private:
+  constexpr static int k_numberOfChildren = 3;
+
 };
+
+class HigherOrderDerivativeLayoutNode final : public DerivativeLayoutNode {
+  // diff(f(x), x, a, n)
+public:
+  HigherOrderDerivativeLayoutNode() : DerivativeLayoutNode(), m_orderInDenominator(true) {}
+  // LayoutNode
+  Type type() const override { return Type::HigherOrderDerivativeLayout; }
+  void moveCursorLeft(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool forSelection = false) override;
+  void moveCursorRight(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool forSelection = false) override;
+  void moveCursorDown(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool equivalentPositionVisited = false, bool forSelection = false) override;
+  void moveCursorUp(LayoutCursor * cursor, bool * shouldRecomputeLayout, bool equivalentPositionVisited = false, bool forSelection = false) override;
+
+    // TreeNode
+  size_t size() const override { return sizeof(HigherOrderDerivativeLayoutNode); }
+  int numberOfChildren() const override { return k_numberOfChildren; }
+#if POINCARE_TREE_LOG
+  void logNodeName(std::ostream & stream) const override {
+    stream << "HigherOrderDerivativeLayout";
+  }
+#endif
+
+private:
+  constexpr static int k_numberOfChildren = 4;
+  constexpr static int k_orderLayoutIndex = k_numberOfChildren - 1;
+  LayoutNode * orderLayout() { return childAtIndex(k_orderLayoutIndex); }
+
+  void setOrderSlot(bool denominator, bool * shouldRecomputeLayout);
+  /* There are two slots for the order of the derivative: the numerator and
+   * the denominator slots. This member is used to make the two copies of the
+   * order interactive while storing the order only once. */
+  bool m_orderInDenominator;
+};
+
+class FirstOrderDerivativeLayout final : public LayoutThreeChildren<FirstOrderDerivativeLayout, FirstOrderDerivativeLayoutNode> {
+public:
+  FirstOrderDerivativeLayout() = delete;
+};
+
+class HigherOrderDerivativeLayout final : public LayoutFourChildren<HigherOrderDerivativeLayout, HigherOrderDerivativeLayoutNode> {
+public:
+  HigherOrderDerivativeLayout() = delete;
+};
+
 
 }
 
--- poincare/src/derivative.cpp
+++ poincare/src/derivative.cpp
@@ -33,10 +33,17 @@ int DerivativeNode::polynomialDegree(Context * context, const char * symbolName)
 }
 
 Layout DerivativeNode::createLayout(Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const {
-  return DerivativeLayout::Builder(
+  if (Expression::IsOne(Expression(childAtIndex(numberOfChildren() - 1)))) {
+    return FirstOrderDerivativeLayout::Builder(
+        childAtIndex(0)->createLayout(floatDisplayMode, numberOfSignificantDigits),
+        childAtIndex(1)->createLayout(floatDisplayMode, numberOfSignificantDigits),
+        childAtIndex(2)->createLayout(floatDisplayMode, numberOfSignificantDigits));
+  }
+  return HigherOrderDerivativeLayout::Builder(
       childAtIndex(0)->createLayout(floatDisplayMode, numberOfSignificantDigits),
       childAtIndex(1)->createLayout(floatDisplayMode, numberOfSignificantDigits),
-      childAtIndex(2)->createLayout(floatDisplayMode, numberOfSignificantDigits));
+      childAtIndex(2)->createLayout(floatDisplayMode, numberOfSignificantDigits),
+      childAtIndex(3)->createLayout(floatDisplayMode, numberOfSignificantDigits));
 }
 
 int DerivativeNode::serialize(char * buffer, int bufferSize, Preferences::PrintFloatMode floatDisplayMode, int numberOfSignificantDigits) const {

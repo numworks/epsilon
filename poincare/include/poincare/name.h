039d3ff3bc2ad7e3bf421fa5d20d42681d7fd659
Félix Richart <felix.richart@numworks.com>
2022-07-01 17:36:57 +0200

    [poincare] Create Name class for aliases

---
+++ poincare/include/poincare/name.h
@@ -0,0 +1,24 @@
+#ifndef POINCARE_NAME_H
+#define POINCARE_NAME_H
+
+#include <assert.h>
+class Name {
+public:
+  constexpr Name(const char * formattedNamesList) : m_formattedNamesList(formattedNamesList) {}
+  constexpr operator const char *() const { return m_formattedNamesList; }
+
+  const char * mainName() const { return hasAliases() ? parseMainName() : m_formattedNamesList; }
+  bool isEquivalentTo(const char * name, int nameLen) const;
+
+  constexpr static char k_headerStart = '\01';
+  constexpr static char k_stringStart = '\02';
+
+private:
+  bool hasAliases() const { return m_formattedNamesList[0] == k_headerStart; }
+  const char * parseNextName(const char * currentPositionInNamesList) const;
+  const char * parseMainName() const { return parseNextName(m_formattedNamesList); }
+
+  const char * m_formattedNamesList;
+};
+
+#endif
---
+++ poincare/src/name.cpp
@@ -0,0 +1,32 @@
+#include <poincare/name.h>
+#include <string.h>
+#include <assert.h>
+
+bool Name::isEquivalentTo(const char * name, int nameLen) const {
+  if (!hasAliases()) {
+    return nameLen == strlen(m_formattedNamesList) && strncmp(name, m_formattedNamesList, nameLen) == 0;
+  }
+  const char * nameInList = parseNextName(m_formattedNamesList);
+  while (nameInList != nullptr) {
+    if (nameLen == strlen(nameInList) && strncmp(name, nameInList, nameLen) == 0) {
+      return true;
+    }
+    nameInList = nameInList + strlen(nameInList) + 1; // skip the terminating 0
+    nameInList = parseNextName(nameInList);
+  }
+  return false;
+}
+
+const char * Name::parseNextName(const char * currentPositionInNamesList) const {
+  if (currentPositionInNamesList[0] == 0) { // End of list
+    return nullptr;
+  }
+  assert(currentPositionInNamesList[0] == k_headerStart);
+  const char * result = currentPositionInNamesList;
+  while (result[0] != k_stringStart && result[0] != 0) { // Skip header for now
+    result++;
+  }
+  assert(result[0] == k_stringStart); // Wrong formatting of namesList
+  result++; // skip k_stringStart char
+  return result;
+}

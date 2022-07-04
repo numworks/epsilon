1d6561936d22b6243c57df83d8f3f246a79c8abc
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-07-04 18:11:34 +0200

    [periodic] Define color palette

---
+++ apps/periodic/palette.h
@@ -0,0 +1,39 @@
+#ifndef PERIODIC_PALETTE_H
+#define PERIODIC_PALETTE_H
+
+#include <kandinsky/color.h>
+
+namespace Periodic {
+namespace Palette {
+
+constexpr KDColor ElementBlueDark = KDColor::RGB24(0x0055bb);
+constexpr KDColor ElementBlueLight = KDColor::RGB24(0xcce3fd);
+constexpr KDColor ElementCeladonDark = KDColor::RGB24(0x157f6d);
+constexpr KDColor ElementCeladonLight = KDColor::RGB24(0xd6fdf6);
+constexpr KDColor ElementGrassDark = KDColor::RGB24(0x354a38);
+constexpr KDColor ElementGrassLight = KDColor::RGB24(0xc7ffc8);
+constexpr KDColor ElementGreenDark = KDColor::RGB24(0x405937);
+constexpr KDColor ElementGreenLight = KDColor::RGB24(0xd1f5d0);
+constexpr KDColor ElementGreyDark = KDColor::RGB24(0x5b5b5b);
+constexpr KDColor ElementGreyLight = KDColor::RGB24(0xe5e5e5);
+constexpr KDColor ElementOrangeDark = KDColor::RGB24(0xb06f00);
+constexpr KDColor ElementOrangeLight = KDColor::RGB24(0xffe4b6);
+constexpr KDColor ElementPinkDark = KDColor::RGB24(0x6e3c5d);
+constexpr KDColor ElementPinkLight = KDColor::RGB24(0xf9d1ec);
+constexpr KDColor ElementPurpleDark = KDColor::RGB24(0x402d81);
+constexpr KDColor ElementPurpleLight = KDColor::RGB24(0xecdcfd);
+constexpr KDColor ElementRedDark = KDColor::RGB24(0xb70000);
+constexpr KDColor ElementRedLight = KDColor::RGB24(0xffc9c9);
+constexpr KDColor ElementTurquoiseDark = KDColor::RGB24(0x006d80);
+constexpr KDColor ElementTurquoiseLight = KDColor::RGB24(0xc9f7ff);
+constexpr KDColor ElementYellowDark = KDColor::RGB24(0x907a02);
+constexpr KDColor ElementYellowLight = KDColor::RGB24(0xfff7cf);
+
+constexpr KDColor SystemGreyLight = KDColor::RGB24(0xf6f6f6);
+constexpr KDColor SystemGreyDark = KDColor::RGB24(0xdedade);
+constexpr KDColor SystemGreyBluish = KDColor::RGB24(0xd5d6e6);
+
+}
+}
+
+#endif

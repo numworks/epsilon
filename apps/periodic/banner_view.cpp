6e0cebf63bcfd8d85951704ba6f75c721a984a5e
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-07-07 17:01:49 +0200

    [periodic/banner_view] Add legend

--- apps/periodic/banner_view.cpp
+++ apps/periodic/banner_view.cpp
@@ -1,4 +1,5 @@
 #include "banner_view.h"
+#include <assert.h>
 
 namespace Periodic {
 
@@ -15,10 +16,25 @@ void BannerView::drawRect(KDContext * ctx, KDRect rect) const {
   ctx->fillRect(KDRect(0, k_borderHeight, bounds().width(), k_bannerHeight), k_backgroundColor);
 }
 
+Escher::View * BannerView::subviewAtIndex(int index) {
+  if (index == 0) {
+    return &m_textView;
+  }
+  assert(index == 1);
+  return &m_dotView;
+}
+
 void BannerView::layoutSubviews(bool force) {
   AtomicNumber z = m_dataSource->selectedElement();
   m_dotView.setColor(m_dataSource->coloring()->colorPairForElement(z).fg());
-  m_dotView.setFrame(KDRect(k_dotLeftMargin, (bounds().height() - k_dotDiameter) / 2, k_dotDiameter, k_dotDiameter), true);
+  KDRect dotRect = KDRect(k_dotLeftMargin, (bounds().height() - k_dotDiameter) / 2, k_dotDiameter, k_dotDiameter);
+  m_dotView.setFrame(dotRect, true);
+
+  m_textView.setFrame(KDRect(dotRect.x() + dotRect.width() + k_dotLegendMargin, k_borderHeight, bounds().width() - dotRect.width(), k_bannerHeight), force);
+  char buffer[Escher::BufferTextView::k_maxNumberOfChar];
+  m_dataSource->coloring()->setLegendForElement(z, buffer, Escher::BufferTextView::k_maxNumberOfChar);
+  m_textView.setText(buffer);
+
 }
 
 }
--- apps/periodic/banner_view.h
+++ apps/periodic/banner_view.h
@@ -2,6 +2,8 @@
 #define PERIODIC_BANNER_VIEW_H
 
 #include "elements_view_data_source.h"
+#include <escher/buffer_text_view.h>
+#include <escher/palette.h>
 #include <escher/view.h>
 #include <ion/display.h>
 
@@ -9,7 +11,7 @@ namespace Periodic {
 
 class BannerView : public Escher::View {
 public:
-  BannerView(ElementsViewDataSource * dataSource) : m_dataSource(dataSource) {}
+  BannerView(ElementsViewDataSource * dataSource) : m_textView(KDFont::Size::Small, KDContext::k_alignLeft, KDContext::k_alignCenter, k_legendColor, k_backgroundColor), m_dataSource(dataSource) {}
 
   // Escher::View
   void drawRect(KDContext * ctx, KDRect rect) const override;
@@ -18,11 +20,13 @@ public:
   void reload() { layoutSubviews(); }
 
 private:
+  constexpr static KDColor k_legendColor = Escher::Palette::GrayVeryDark;
   constexpr static KDColor k_backgroundColor = Palette::SystemGreyLight;
   constexpr static KDCoordinate k_dotLeftMargin = 16;
   constexpr static KDCoordinate k_dotDiameter = 8;
   constexpr static KDCoordinate k_bannerHeight = 25;
   constexpr static KDCoordinate k_borderHeight = 1;
+  constexpr static KDCoordinate k_dotLegendMargin = 12;
 
   class DotView : public Escher::View {
   public:
@@ -38,11 +42,12 @@ private:
     KDColor m_color;
   };
 
-  int numberOfSubviews() const override { return 1; }
-  Escher::View * subviewAtIndex(int index) override { return &m_dotView; }
+  int numberOfSubviews() const override { return 2; }
+  Escher::View * subviewAtIndex(int index) override;
   void layoutSubviews(bool force = false) override;
 
   DotView m_dotView;
+  Escher::BufferTextView m_textView;
   ElementsViewDataSource * m_dataSource;
 };
 

b439ce331fa83ca97ab9a82434b9c4b1f192d514
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-07-26 10:42:37 +0200

    [periodic/banner_view] Use ColonConvention

--- apps/periodic/banner_view.cpp
+++ apps/periodic/banner_view.cpp
@@ -88,7 +88,7 @@ void BannerView::layoutSubviews(bool force) {
   } else {
     m_textView.setText(I18n::translate(dataSource->field()->fieldLegend()));
     if (dataSource->field()->hasDouble(z)) {
-      m_textView.appendText(" : ");
+      m_textView.appendText(I18n::translate(I18n::Message::ColonConvention));
       char buffer[Escher::BufferTextView::k_maxNumberOfChar];
       dataSource->field()->getLayout(z).serializeForParsing(buffer, Escher::BufferTextView::k_maxNumberOfChar);
       m_textView.appendText(buffer);

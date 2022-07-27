3d402fed118e77b414918a9bd6b81e01635924a1
Félix Richart <felix.richart@numworks.com>
2022-07-27 17:00:28 +0200

    [poincare/tests] Add tests for randint on lists

--- poincare/test/approximation.cpp
+++ poincare/test/approximation.cpp
@@ -568,6 +568,8 @@ QUIZ_CASE(poincare_approximation_function) {
   assert_expression_approximation_is_bounded("randint(4,45)", 4.0f, 45.0f, true);
   assert_expression_approximation_is_bounded("randint(4,45)", 4.0, 45.0, true);
   assert_expression_approximation_is_bounded("randint(10)", 0.0, 10.0, true);
+  assert_expression_approximates_to<double>("randint({0,0})", "{0,0}");
+  assert_expression_approximates_to<double>("randint({1,2},{1,2})", "{1,2}");
   assert_expression_approximates_to<double>("1/randint(2,2)+1/2", "1");
   assert_expression_approximates_to<double>("randint(45,4)", Undefined::Name());
   assert_expression_approximates_to<double>("randint(1, inf)",Undefined::Name());
@@ -1366,4 +1368,3 @@ QUIZ_CASE(poincare_approximation_floor_ceil_integer) {
 
 template void assert_expression_approximates_to_scalar(const char * expression, float approximation, Preferences::AngleUnit angleUnit, Preferences::ComplexFormat complexFormat, Preferences::MixedFractions mixedFractionsParameter);
 template void assert_expression_approximates_to_scalar(const char * expression, double approximation, Preferences::AngleUnit angleUnit, Preferences::ComplexFormat complexFormat, Preferences::MixedFractions mixedFractionsParameter);
-

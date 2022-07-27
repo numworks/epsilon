31b1926d4e54e63619f5cdf3ab0cb09e79f20d54
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-07-27 17:08:01 +0200

    [escher/table_view_with_top_and_bottom_views] Refactor
    Use the contentOffset of the inner table view to allow cells reuse.

--- escher/include/escher/table_view_with_top_and_bottom_views.h
+++ escher/include/escher/table_view_with_top_and_bottom_views.h
@@ -14,62 +14,29 @@ namespace Escher {
  * Scroll is adjusted when selecting first and last row so that the top or
  * bottom view is fully visible. */
 
-class TableViewWithTopAndBottomViews : public ScrollView, public SelectableTableViewDelegate {
+class TableViewWithTopAndBottomViews : public View, public SelectableTableViewDelegate {
 public:
-  TableViewWithTopAndBottomViews(SelectableTableView * table, TableViewDataSource * tableDataSource, View * topView, View * bottomView = nullptr) :
-      ScrollView(&m_contentView, &m_scrollDataSource),
-      m_contentView(table, topView, bottomView),
-      m_tableDataSource(tableDataSource) {
-  setMargins(ContentView::k_margin, Escher::Metric::CommonMargin, ContentView::k_margin, Escher::Metric::CommonMargin);
-}
-  void reload();
-  void setTopView(View * view) { m_contentView.m_topView = view; }
-  void setBottomView(View * view) { m_contentView.m_bottomView = view; }
+  TableViewWithTopAndBottomViews(SelectableTableView * table, TableViewDataSource * tableDataSource, View * topView, View * bottomView = nullptr);
 
-  /* ScrollView */
+  /* View */
+  int numberOfSubviews() const override { return (m_topView != nullptr) + 1 + (m_bottomView != nullptr); }
+  View * subviewAtIndex(int i) override;
   void layoutSubviews(bool force = false) override;
 
   /* SelectableTableViewDelegate */
-  void tableViewDidChangeSelectionAndDidScroll(SelectableTableView * t,
-                                               int previousSelectedCellX,
-                                               int previousSelectedCellY,
-                                               bool withinTemporarySelection = false) override;
+  void tableViewDidChangeSelectionAndDidScroll(SelectableTableView * t, int previousSelectedCellX, int previousSelectedCellY, bool withinTemporarySelection = false) override;
+
+  void reload();
+  void setTopView(View * view) { m_topView = view; }
+  void setBottomView(View * view) { m_bottomView = view; }
+
 private:
-  /* Lays out top message, a table and bottom message. */
-  class ContentView : public VerticalLayout {
-  friend TableViewWithTopAndBottomViews;
-  public:
-    ContentView(SelectableTableView * table, View * topView, View * bottomView = nullptr) :
-        m_topView(topView),
-        m_table(table),
-        m_bottomView(bottomView) {
-      m_table->setMargins(topTableMargin(), 0, bottomTableMargin(), 0);
-    }
-    KDCoordinate tableOrigin() const { return topTableMargin() + (m_topView ? m_topView->minimalSizeForOptimalDisplay().height() : 0); }
-    KDCoordinate totalHeight() const { return minimalSizeForOptimalDisplay().height(); }
-    void reload();
+  KDRect tableFrame(KDCoordinate * yOffset) const;
 
-    /* View */
-    int numberOfSubviews() const override { return (m_topView != nullptr) + 1 + (m_bottomView != nullptr); }
-    View * subviewAtIndex(int i) override;
-    TableView * tableView() { return m_table; }
-  private:
-    constexpr static KDCoordinate k_emptyViewMargin = Escher::Metric::CommonMargin;
-    constexpr static KDCoordinate k_margin = Metric::TableSeparatorThickness;
-
-    /* If there is no top or bottom view, use a k_emptyViewMargin margin and
-     * account for TableViewWithTopAndBottomViews's margins. */
-    KDCoordinate topTableMargin() const { return m_topView ? k_margin : k_emptyViewMargin - k_margin; }
-    KDCoordinate bottomTableMargin() const { return m_bottomView ? k_margin : k_emptyViewMargin - k_margin; }
-
-    View * m_topView;
-    SelectableTableView * m_table;
-    View * m_bottomView;
-  };
-
-  ContentView m_contentView;
-  ScrollViewDataSource m_scrollDataSource;
   TableViewDataSource * m_tableDataSource;
+  View * m_topView;
+  SelectableTableView * m_table;
+  View * m_bottomView;
 };
 
 }  // namespace Escher
--- escher/src/table_view_with_top_and_bottom_views.cpp
+++ escher/src/table_view_with_top_and_bottom_views.cpp
@@ -1,54 +1,17 @@
 #include <escher/table_view_with_top_and_bottom_views.h>
 #include <escher/palette.h>
+#include <algorithm>
 
-using namespace Escher;
+namespace Escher {
 
-/* TableViewWithTopAndBottomViews */
+TableViewWithTopAndBottomViews::TableViewWithTopAndBottomViews(SelectableTableView * table, TableViewDataSource * tableDataSource, View * topView, View * bottomView) :
+  m_tableDataSource(tableDataSource),
+  m_topView(topView),
+  m_table(table),
+  m_bottomView(bottomView)
+{}
 
-void TableViewWithTopAndBottomViews::reload() {
-  m_contentView.reload();
-  layoutSubviews();
-}
-
-void TableViewWithTopAndBottomViews::layoutSubviews(bool force) {
-  /* SelectableTableView must be given a width, so that it can percole it to its
-   * cells. The cells might need their widths to know their heights. */
-  m_contentView.tableView()->initSize(visibleContentRect());
-  ScrollView::layoutSubviews(force);
-}
-
-void TableViewWithTopAndBottomViews::tableViewDidChangeSelectionAndDidScroll(
-    SelectableTableView * t,
-    int previousSelectedCellX,
-    int previousSelectedCellY,
-    bool withinTemporarySelection) {
-  // Scroll to correct location
-  int row = m_contentView.m_table->selectedRow(), col = m_contentView.m_table->selectedColumn();
-  if (row >= 0 && col >= 0) {
-    KDRect cellFrame = KDRect(m_tableDataSource->cumulatedWidthFromIndex(col),
-                              m_contentView.tableOrigin() + m_tableDataSource->cumulatedHeightFromIndex(row),
-                              m_tableDataSource->columnWidth(col) ? m_tableDataSource->columnWidth(col) : visibleContentRect().width(),
-                              m_tableDataSource->rowHeight(row));
-    /* Include the message in the first or last row cells to force scrolling
-     * enough to display it */
-    if (row == 0) {
-      cellFrame = cellFrame.unionedWith(KDRect(cellFrame.x(), 0, cellFrame.width(), cellFrame.height()));
-    } else if (row + 1 == m_contentView.m_table->numberOfDisplayableRows()) {
-      cellFrame = cellFrame.unionedWith(KDRect(cellFrame.x(), cellFrame.y(), cellFrame.width(), m_contentView.totalHeight() - cellFrame.y()));
-    }
-    scrollToContentRect(cellFrame);
-  }
-}
-
-/* TableViewWithTopAndBottomViews::ContentView */
-
-void TableViewWithTopAndBottomViews::ContentView::reload() {
-  // A subview may have (dis)appeared, recompute table's margins
-  m_table->setMargins(topTableMargin(), 0, bottomTableMargin(), 0);
-  m_table->reloadData();
-}
-
-View * TableViewWithTopAndBottomViews::ContentView::subviewAtIndex(int i) {
+View * TableViewWithTopAndBottomViews::subviewAtIndex(int i) {
   assert(i < numberOfSubviews());
   i += (m_topView == nullptr);
   switch (i) {
@@ -60,4 +23,79 @@ View * TableViewWithTopAndBottomViews::ContentView::subviewAtIndex(int i) {
     assert(i == 2);
     return m_bottomView;
   }
+}
+
+void TableViewWithTopAndBottomViews::layoutSubviews(bool force) {
+  if (bounds().isEmpty()) {
+    return;
+  }
+
+  KDPoint offset = m_table->contentOffset();
+  KDCoordinate yOffset = offset.y();
+  KDRect tableRect = tableFrame(&yOffset);
+  m_table->setFrame(tableRect, force);
+  m_table->setContentOffset(KDPoint(offset.x(), yOffset));
+  if (m_topView) {
+    KDCoordinate topHeight = m_topView->minimalSizeForOptimalDisplay().height();
+    m_topView->setFrame(KDRect(0, tableRect.y() - topHeight, bounds().width(), topHeight), force);
+  }
+  if (m_bottomView) {
+    m_bottomView->setFrame(KDRect(0, tableRect.y() + tableRect.height(), bounds().width(), m_bottomView->minimalSizeForOptimalDisplay().height()), force);
+  }
+}
+
+void TableViewWithTopAndBottomViews::tableViewDidChangeSelectionAndDidScroll(SelectableTableView * t, int previousSelectedCellX, int previousSelectedCellY, bool withinTemporarySelection) {
+  if (withinTemporarySelection) {
+    return;
+  }
+  assert(m_table->selectedColumn() == 0);
+  int row = m_table->selectedRow();
+
+  if (row == 0 && m_topView) {
+    m_table->setFrame(KDRectZero, false);
+    m_table->setContentOffset(KDPointZero);
+  } else if (row == m_tableDataSource->numberOfRows() - 1 && m_bottomView) {
+    m_table->setFrame(KDRectZero, false);
+    m_table->setContentOffset(KDPoint(0, m_table->minimalSizeForOptimalDisplay().height()));
+  }
+  layoutSubviews(false);
+}
+
+void TableViewWithTopAndBottomViews::reload() {
+  // A subview may have (dis)appeared, recompute table's margins
+  m_table->reloadData();
+  layoutSubviews();
+}
+
+KDRect TableViewWithTopAndBottomViews::tableFrame(KDCoordinate * yOffset) const {
+  assert(yOffset);
+  if (*yOffset + m_table->bounds().height() <= bounds().height()) {
+    /* Top of the table can fit on screen. Assume the table goes all the way to
+     * the bottom of the screen. */
+    KDCoordinate h = m_table->bounds().height() + *yOffset;
+    *yOffset = 0;
+    if (m_topView) {
+      h = std::max<KDCoordinate>(h, bounds().height() - m_topView->minimalSizeForOptimalDisplay().height());
+    }
+    return KDRect(0, bounds().height() - h, bounds().width(), h);
+  }
+
+  KDCoordinate bottom =  m_table->minimalSizeForOptimalDisplay().height() - *yOffset;
+  if (bottom <= bounds().height()) {
+    /* Bottom of the table can fit on screen. Assume the table starts from the
+     * top of the screen. */
+    if (m_bottomView) {
+      KDCoordinate bottomViewTop = bounds().height() - m_bottomView->minimalSizeForOptimalDisplay().height();
+      if (bottom < bottomViewTop) {
+        *yOffset -= bottomViewTop - bottom;
+        bottom = bottomViewTop;
+      }
+    }
+    return KDRect(0, 0, bounds().width(), bottom);
+  }
+
+  /* Neither top nor bottom view is visible. */
+  return bounds();
+}
+
 }

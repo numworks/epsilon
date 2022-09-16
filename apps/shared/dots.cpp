3caf48d915202e7ee0aadcbe6f8165ee6b1d518e
MarcNumworks <marc.coudriau@numworks.com>
2022-09-16 10:35:16 +0200

    [apps/shared] Change dots to pointers

--- apps/shared/dots.cpp
+++ apps/shared/dots.cpp
@@ -3,19 +3,22 @@
 
 namespace Shared {
 
-constexpr static const KDMask<Dots::TinyDotDiameter> tinyDot = KDMakeDot<Dots::TinyDotDiameter>(Dots::TinyDotRadius);
-const uint8_t (&Dots::TinyDotMask)[Dots::TinyDotDiameter][Dots::TinyDotDiameter] = tinyDot.m_mask;
+namespace Dots {
 
-constexpr static const KDMask<Dots::SmallDotDiameter> smallDot = KDMakeDot<Dots::SmallDotDiameter>(Dots::SmallDotRadius);
-const uint8_t (&Dots::SmallDotMask)[Dots::SmallDotDiameter][Dots::SmallDotDiameter] = smallDot.m_mask;
+constexpr static const KDMask<TinyDotDiameter> tinyDot = KDMakeDot<TinyDotDiameter>(TinyDotRadius);
+const uint8_t * TinyDotMask = &tinyDot.m_mask[0][0];
 
-constexpr static const KDMask<Dots::MediumDotDiameter> mediumDot = KDMakeDot<Dots::MediumDotDiameter>(Dots::MediumDotRadius);
-const uint8_t (&Dots::MediumDotMask)[Dots::MediumDotDiameter][Dots::MediumDotDiameter] = mediumDot.m_mask;
+constexpr static const KDMask<SmallDotDiameter> smallDot = KDMakeDot<SmallDotDiameter>(SmallDotRadius);
+const uint8_t * SmallDotMask = &smallDot.m_mask[0][0];
 
-constexpr static const KDMask<Dots::LargeDotDiameter> largeDot = KDMakeDot<Dots::LargeDotDiameter>(Dots::LargeDotRadius);
-const uint8_t (&Dots::LargeDotMask)[Dots::LargeDotDiameter][Dots::LargeDotDiameter] = largeDot.m_mask;
+constexpr static const KDMask<MediumDotDiameter> mediumDot = KDMakeDot<MediumDotDiameter>(MediumDotRadius);
+const uint8_t * MediumDotMask = &mediumDot.m_mask[0][0];
 
-constexpr static const KDMask<Dots::LargeRingDiameter> largeRing = KDMakeRing<Dots::LargeRingDiameter>(Dots::LargeRingInternalRadius, Dots::LargeRingExternalRadius);
-const uint8_t (&Dots::LargeRingMask)[Dots::LargeRingDiameter][Dots::LargeRingDiameter] = largeRing.m_mask;
+constexpr static const KDMask<LargeDotDiameter> largeDot = KDMakeDot<LargeDotDiameter>(LargeDotRadius);
+const uint8_t * LargeDotMask = &largeDot.m_mask[0][0];
 
+constexpr static const KDMask<LargeRingDiameter> largeRing = KDMakeRing<LargeRingDiameter>(LargeRingInternalRadius, LargeRingExternalRadius);
+const uint8_t * LargeRingMask = &largeRing.m_mask[0][0];
+
+}
 }
--- apps/shared/dots.h
+++ apps/shared/dots.h
@@ -5,28 +5,29 @@
 
 namespace Shared {
 
-class Dots {
-public:
-  constexpr static KDCoordinate TinyDotDiameter = 5;
-  constexpr static float TinyDotRadius = 2.4675f;
-  static const uint8_t (&TinyDotMask)[TinyDotDiameter][TinyDotDiameter];
+namespace Dots {
 
-  constexpr static KDCoordinate SmallDotDiameter = 6;
-  constexpr static float SmallDotRadius = 3.f;
-  static const uint8_t (&SmallDotMask)[SmallDotDiameter][SmallDotDiameter];
+constexpr static KDCoordinate TinyDotDiameter = 5;
+constexpr static float TinyDotRadius = 2.4675f;
+extern const uint8_t * TinyDotMask;
 
-  constexpr static KDCoordinate MediumDotDiameter = 7;
-  constexpr static float MediumDotRadius = 3.85f;
-  static const uint8_t (&MediumDotMask)[MediumDotDiameter][MediumDotDiameter];
+constexpr static KDCoordinate SmallDotDiameter = 6;
+constexpr static float SmallDotRadius = 3.f;
+extern const uint8_t * SmallDotMask;
 
-  constexpr static KDCoordinate LargeDotDiameter = 9;
-  constexpr static float LargeDotRadius = 4.f;
-  static const uint8_t (&LargeDotMask)[LargeDotDiameter][LargeDotDiameter];
+constexpr static KDCoordinate MediumDotDiameter = 7;
+constexpr static float MediumDotRadius = 3.85f;
+extern const uint8_t * MediumDotMask;
 
-  constexpr static KDCoordinate LargeRingDiameter = 11;
-  constexpr static float LargeRingInternalRadius = 4.25f;
-  constexpr static float LargeRingExternalRadius = 5.5f;
-  static const uint8_t (&LargeRingMask)[LargeRingDiameter][LargeRingDiameter];
+constexpr static KDCoordinate LargeDotDiameter = 9;
+constexpr static float LargeDotRadius = 4.f;
+extern const uint8_t * LargeDotMask;
+
+constexpr static KDCoordinate LargeRingDiameter = 11;
+constexpr static float LargeRingInternalRadius = 4.25f;
+constexpr static float LargeRingExternalRadius = 5.5f;
+extern const uint8_t * LargeRingMask;
+
 };
 
 }

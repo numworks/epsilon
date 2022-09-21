b3c6f802808f691fe9478fc39d944de62ae22f33
MarcNumworks <marc.coudriau@numworks.com>
2022-09-21 11:04:53 +0200

    [apps/shared] Add comment

--- apps/shared/round_cursor_view.cpp
+++ apps/shared/round_cursor_view.cpp
@@ -13,6 +13,9 @@ void AbstractRoundCursorView::setIsRing(bool isRing) {
 
 void AbstractRoundCursorView::drawCursor(KDContext * ctx, KDRect rect) const {
   KDColor cursorWorkingBuffer[k_maxSize * k_maxSize];
+  /* The bounding rect may be larger than the mask to be stamped. We thus need
+   * to compute a smaller, centered rect with the same width as the mask for the
+   * blend to be correct. */
   KDCoordinate diff = size() - Dots::LargeDotDiameter;
   KDRect r = bounds();
   KDRect dotRect = m_isRing ? r : KDRect(r.x() + diff/2, r.y() + diff/2, r.width() - diff, r.height() - diff);

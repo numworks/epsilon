d2e97f6187cf7654c537e96e8d7470921a3910f1
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-09-26 11:39:00 +0200

    [shared/plot_view] Create helper plot_view_policies.h

---
+++ apps/shared/plot_view_policies.h
@@ -0,0 +1,10 @@
+#ifndef SHARED_PLOT_VIEW_POLICIES_H
+#define SHARED_PLOT_VIEW_POLICIES_H
+
+#include "plot_view.h"
+#include "plot_view_axes.h"
+#include "plot_view_banners.h"
+#include "plot_view_cursors.h"
+#include "plot_view_plots.h"
+
+#endif

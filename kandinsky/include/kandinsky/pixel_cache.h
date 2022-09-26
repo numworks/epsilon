0f7f6c9b022ec4c610a1a38bcae15780d9f87e21
MarcNumworks <marc.coudriau@numworks.com>
2022-09-26 09:57:46 +0200

    [apps/sequence] Make pixel caches easier to use

--- kandinsky/include/kandinsky/pixel_cache.h
+++ kandinsky/include/kandinsky/pixel_cache.h
@@ -4,6 +4,7 @@
 #include <kandinsky/color.h>
 #include <kandinsky/context.h>
 #include <kandinsky/rect.h>
+#include "measuring_context.h"
 
 template <unsigned Size>
 class KDPixelCache {
@@ -19,6 +20,16 @@ public:
   void restore(KDContext * ctx) {
     ctx->fillRectWithPixels(m_rect, m_buffer, m_buffer);
     m_rect = KDRectZero;
+  }
+
+  /* Warning : the provided function is evaluated twice, the first time to
+   * compute the bounding rect of the actual drawings and the second time will
+   * real drawing calls. */
+  template<class T> void saveAndDraw(KDContext * ctx, T function) {
+    KDMeasuringContext measuringContext(*ctx);
+    function(&measuringContext);
+    save(ctx, measuringContext.writtenRect());
+    function(ctx);
   }
 private:
   KDRect m_rect;

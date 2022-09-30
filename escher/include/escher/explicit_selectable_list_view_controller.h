5547720660a4913e0f4692dca08d8ef00302c202
MarcNumworks <marc.coudriau@numworks.com>
2022-09-30 14:58:15 +0200

    [escher] Improve ExplicitSelectableListViewController

--- escher/include/escher/explicit_selectable_list_view_controller.h
+++ escher/include/escher/explicit_selectable_list_view_controller.h
@@ -5,12 +5,17 @@
 
 namespace Escher {
 
+/* This class is a MemoizedListViewDataSource where each cell has its own
+ * type. It basically means that the reusableCell mecanism is erased which
+ * results in easier manipulation of heterogeneous cells but should not be used
+ * in list with a long or dynamic cell count. */
+
 class ExplicitSelectableListViewController : public SelectableListViewController<MemoizedListViewDataSource> {
 public:
   using SelectableListViewController::SelectableListViewController;
-  int typeAtIndex(int index) override { return index; }
-  int reusableCellCount(int type) override { return 1; }
-  HighlightCell * reusableCell(int index, int type) override { return cell(type); }
+  int typeAtIndex(int index) override final { return index; }
+  int reusableCellCount(int type) override final { return 1; }
+  HighlightCell * reusableCell(int index, int type) override final { return cell(type); }
   HighlightCell * selectedCell() { return cell(selectedRow()); }
 
 protected:

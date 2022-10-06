9401bde0fa0a1415eaa34ec7ee6612a85711863e
Emilie Feral <emilie.feral@numworks.com>
2022-10-06 16:43:36 +0200

    [ion] Forbid userland DFU to write its own stack

--- ion/src/device/userland/drivers/board_ram_dfu_xip.cpp
+++ ion/src/device/userland/drivers/board_ram_dfu_xip.cpp
@@ -4,14 +4,15 @@ namespace Ion {
 namespace Device {
 namespace Board {
 
-/* N0110 userland DFU cannot write the SRAM allocated for the kernel. */
+/* N0110/N0120 userland DFU cannot write the SRAM allocated for the kernel or
+ * its own process stack. */
 
 uint32_t writableSRAMStartAddress() {
   return Config::UserlandSRAMOrigin;
 }
 
 uint32_t writableSRAMEndAddress() {
-  return Config::UserlandSRAMOrigin + Config::UserlandSRAMLength;
+  return Config::UserlandSRAMOrigin + Config::UserlandSRAMLength - Config::UserlandStackLength;
 }
 
 }

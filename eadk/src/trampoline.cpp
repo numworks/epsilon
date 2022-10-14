6a6d6cfaa4ae5755d47cd8dde9d77f519a3bcbd5
Romain Goyet <romain.goyet@numworks.com>
2022-10-14 10:27:29 -0400

    [eadk] Fix trampoline functions

--- eadk/src/trampoline.cpp
+++ eadk/src/trampoline.cpp
@@ -3,27 +3,24 @@ extern "C" {
 }
 
 static uint32_t trampolineFunctionsOrigin() {
-  static uint32_t origin = 0;
-  if (origin == 0) {
-    void * programCounter;
-    asm volatile ("mov %[pc], pc" : [pc] "=r" (programCounter) :);
-    // Find the running slot from the PC
-    uint32_t slotStart = (uint32_t)programCounter & (~(0x400000 - 1));
-    // Find the trampoline address start
-    uint32_t kernelSize = 0x10000;
-    uint32_t userlandHeaderSize = 4 + 8 + 4 + 4 + 4 + 4 + 4 + 4 + 4;
-    uint32_t isrOffset = 4 + 4;
-    origin = slotStart + kernelSize + userlandHeaderSize + isrOffset;
-  }
-  return origin;
+  void * programCounter;
+  asm volatile ("mov %[pc], pc" : [pc] "=r" (programCounter) :);
+  // Find the running slot from the PC
+  uint32_t slotStart = (uint32_t)programCounter & (~(0x400000 - 1));
+  // Find the trampoline address start
+  uint32_t kernelSize = 0x10000;
+  uint32_t userlandHeaderSize = 4 + 8 + 4 + 4 + 4 + 4 + 4 + 4 + 4;
+  uint32_t isrOffset = 4 + 4;
+  return slotStart + kernelSize + userlandHeaderSize + isrOffset;
 }
 
-static inline uint32_t trampolineFunctionAddress(int index) {
-  return trampolineFunctionsOrigin() + index * 4;
+static uint32_t trampolineFunctionAddress(int index) {
+  static uint32_t origin = trampolineFunctionsOrigin();
+  return origin + index * 4;
 }
 
 void eadk_display_draw_string(const char * text, eadk_point_t point, bool large_font, eadk_color_t text_color, eadk_color_t background_color) {
   typedef void (*Trampoline)(const char *, eadk_point_t, bool, eadk_color_t, eadk_color_t);
-  Trampoline trampoline = (Trampoline)trampolineFunctionAddress(0);
-  return trampoline(text, point, large_font, text_color, background_color);
+  Trampoline * trampoline = (Trampoline *)trampolineFunctionAddress(0);
+  return (*trampoline)(text, point, large_font, text_color, background_color);
 }

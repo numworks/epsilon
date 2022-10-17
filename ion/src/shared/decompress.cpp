faee4d69ac86e419d455e5acdbc54d93f4382234
Pia Chancerel <pia.chancerel@numworks.com>
2022-10-17 11:29:15 +0200

    [ion] Add comment because assert fails when decompressing

--- ion/src/shared/decompress.cpp
+++ ion/src/shared/decompress.cpp
@@ -4,5 +4,5 @@
 void Ion::decompress(const uint8_t * src, uint8_t * dst, int srcSize, int dstSize) {
   int outputSize = LZ4_decompress_safe(reinterpret_cast<const char *>(src), reinterpret_cast<char *>(dst), srcSize, dstSize);
   (void)outputSize; // Make the compiler happy if assertions are disabled
-  assert(outputSize == dstSize);
+  assert(outputSize == dstSize); // TODO: Fails when installing external app (voord)
 }

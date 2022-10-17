38bb59852bef49244ead3f67af22e385e22f994d
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-10-17 15:06:08 +0200

    [escher] Improve comment and assert in
    ListViewDataSource::nonMemoizedRowHeight, to catch more misuses
    of that method, and provide more details.

--- escher/src/list_view_data_source.cpp
+++ escher/src/list_view_data_source.cpp
@@ -16,30 +16,55 @@ void ListViewDataSource::initCellSize(TableView * view) {
   }
 }
 
-KDCoordinate ListViewDataSource::nonMemoizedRowHeight(int j) {
+// This ensures we can assume the reusable cell index j from the cell index
+bool CanReusableIndexBeAssumed(int index, int type, int reusableCellCount, ListViewDataSource * listView) {
+  if (reusableCellCount <= 0) {
+    // Cells of this type must be stored as reusable cells.
+    return false;
+  }
+  if (reusableCellCount == 1) {
+    // There is only one cell of this type, j = 0
+    return true;
+  }
+  /* TODO : It should be ensured here that the cell at index is visible.
+   * For example, we could have 7 rows and 7 reusable cells of a unique type
+   * but the first row is not visible because of a scroll. The accurate index
+   * would then have to be j = index - 1.
+   * It is assumed here that ListViewDataSource::nonMemoizedRowHeight is only
+   * used with lists and types for which the reusableCellCount is exactly the
+   * minimal number of displayable cells. */
+  if (listView->numberOfRows() == reusableCellCount) {
+    // All rows are reusable cells of the same type, j = index
+    return true;
+  }
+  for (int i = 0; i < index; i++) {
+    if (listView->typeAtIndex(i) != type) {
+      return false;
+    }
+  }
+  /* All previous cells are of the same type, j = index */
+  return true;
+}
+
+KDCoordinate ListViewDataSource::nonMemoizedRowHeight(int index) {
   /* Overridden cellHeight implementations boils down to instantiating
    * a temporary cell on which returning heightForCellAtIndex. As temporary cell
    * must be instantiated in the type expected in willDisplayCellAtIndex(),
-   * which is unknown here, we cannot factorize this logic. However, when cells
-   * are stored as reusable, there is no need for temporary cells, and behavior
-   * can be factorized as follows. */
-  assert(j < numberOfRows());
-  /* Row number (in nonMemoizedRowHeight() and typeAtIndex()) is usually
-   * different from cell index (in reusableCell()).
-   * This default implementation can be used in very specific situations : */
-  int type = typeAtIndex(j);
-  // 1 - Cells of this type must be stored as reusable cells.
-  assert(reusableCellCount(type) > 0);
-  // 2 - To ensure row number and cell index are equivalent, either :
-  assert(
-    // All rows are reusable cells of the same type
-    numberOfRows() == reusableCellCount(type) ||
-    // First rows are of this type (row number = cell index)
-    type == typeAtIndex(0) ||
-    // Row number does not matter in this reusableCell() implementation.
-    reusableCell(j, type) == reusableCell(0, type)
-  );
-  return heightForCellAtIndex(reusableCell(j, type), j);
+   * which is unknown here, we cannot factorize this logic.
+   * Generally, row index (in nonMemoizedRowHeight() and typeAtIndex()) is
+   * different from cell index (named j here, in reusableCell()).
+   * However, there are simple situations where we can assume the reusable cell
+   * index. We only assert that the list is simple enough for that trick.
+   * Selecting the wrong reusable cell here can lead to cells being corrupted or
+   * visually duplicated. */
+  assert(index < numberOfRows());
+  int type = typeAtIndex(index);
+  int thisReusableCellCount = reusableCellCount(type);
+  assert(CanReusableIndexBeAssumed(index, type, thisReusableCellCount, this));
+  // We can assume the reusable index
+  int j = (thisReusableCellCount == 1) ? 0 : index;
+  assert(j < thisReusableCellCount);
+  return heightForCellAtIndex(reusableCell(j, type), index);
 }
 
 KDCoordinate ListViewDataSource::heightForCellAtIndexWithWidthInit(HighlightCell * cell, int index) {

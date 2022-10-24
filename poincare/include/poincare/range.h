59f134a0456c2f006f3f8f4d6026799c56d797cd
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-10-24 14:58:23 +0200

    [points/range] Remove unused function

--- poincare/include/poincare/range.h
+++ poincare/include/poincare/range.h
@@ -31,7 +31,6 @@ public:
   float center() const { return 0.5f * (m_min + m_max); }
   void extend(float t);
   void zoom(float ratio, float center);
-  void nudgeToContain(float t);
 
 private:
   void privateSet(float t, float * bound, float limit);

355f343604baf89462fc3e476d5b4ad71bffab91
Félix Richart <felix.richart@numworks.com>
2022-10-24 10:27:01 +0200

    [explicit_float_parameter_controller] Fix crash where a hidden row would be selected

--- apps/shared/explicit_float_parameter_controller.cpp
+++ apps/shared/explicit_float_parameter_controller.cpp
@@ -87,7 +87,7 @@ bool ExplicitFloatParameterController::textFieldDidFinishEditing(AbstractTextFie
   m_selectableTableView.reloadCellAtLocation(0, selectedRow());
   m_selectableTableView.reloadData();
   if (event == Ion::Events::EXE || event == Ion::Events::OK) {
-    m_selectableTableView.selectCellAtLocation(selectedColumn(), row + 1);
+    m_selectableTableView.selectCellAtLocation(selectedColumn(), m_selectableTableView.indexOfNextSelectableRow(1));
   } else {
     m_selectableTableView.handleEvent(event);
   }

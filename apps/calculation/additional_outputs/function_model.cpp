67287c7ebc5e6900360250a417802a54baa5897c
MarcNumworks <marc.coudriau@numworks.com>
2022-10-26 10:36:03 +0200

    [apps/calculation] Cleanup FunctionModel

--- apps/calculation/additional_outputs/function_model.cpp
+++ apps/calculation/additional_outputs/function_model.cpp
@@ -13,6 +13,7 @@ void FunctionModel::setParameters(Poincare::Expression function, float abscissa)
 };
 
 void FunctionModel::recomputeViewRange() {
+  // TODO: to be replaced using the new zoom API
   const float x = m_abscissa * 2;
   const float y = m_ordinate * 2;
 
@@ -21,25 +22,13 @@ void FunctionModel::recomputeViewRange() {
   bool verticallyCapped = height * k_xyRatio > width;
   float ratio = verticallyCapped ? height/k_height : width/k_width;
 
-  /* When the function points downward, we need to keep space around the origin
-   * for the arc which extends on the other side of the function. */
-  float arcHorizontalMargin = 0;
-  float arcVerticalMargin = 0;
-
-  width += arcHorizontalMargin;
-  height += arcVerticalMargin;
-
-  /* Recompute ratio with updated size */
-  verticallyCapped = height * k_xyRatio > width;
-  ratio = verticallyCapped ? height/k_height : width/k_width;
-
   float xMargin = k_marginInPixels*ratio + (verticallyCapped ? (height * k_xyRatio - width) / 2.f : 0.f);
   float yMargin = k_marginInPixels*ratio + (verticallyCapped ? 0.f : (width / k_xyRatio - height) / 2.f);
 
-  setXMin(std::min(-arcHorizontalMargin, x) - xMargin);
+  setXMin(std::min(0.f, x) - xMargin);
   setXMax(std::max(0.f, x) + xMargin);
   setYMin(std::min(0.f, y) - yMargin);
-  setYMax(std::max(arcVerticalMargin, y) + yMargin);
+  setYMax(std::max(0.f, y) + yMargin);
 }
 
 }

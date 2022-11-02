b353503e36c1e27fb6ad934c28dfab4f37bc1e5e
Félix Richart <felix.richart@numworks.com>
2022-11-02 17:48:31 +0100

    [graph/test] Apply new way of computing number of subcurves to tests

--- apps/graph/test/function_properties.cpp
+++ apps/graph/test/function_properties.cpp
@@ -6,43 +6,33 @@ using namespace Shared;
 
 namespace Graph {
 
-constexpr static ContinuousFunctionProperties::Status k_defaultStatus = ContinuousFunctionProperties::k_defaultStatus;
-constexpr static I18n::Message k_defaultCaption = ContinuousFunctionProperties::k_defaultCaption;
-constexpr static Poincare::ComparisonNode::OperatorType k_defaultEquationType = ContinuousFunctionProperties::k_defaultEquationType;
-constexpr static ContinuousFunctionProperties::SymbolType k_defaultSymbolType = ContinuousFunctionProperties::k_defaultSymbolType;
-constexpr static ContinuousFunctionProperties::CurveParameterType k_defaultCurveParameterType = ContinuousFunctionProperties::k_defaultCurveParameterType;
-constexpr static Poincare::Conic::Shape k_defaultConicShape = ContinuousFunctionProperties::k_defaultConicShape;
-constexpr static bool k_defaultHasTwoSubCurves = ContinuousFunctionProperties::k_defaultHasTwoSubCurves;
-constexpr static bool k_defaultIsAlongY = ContinuousFunctionProperties::k_defaultIsAlongY;
-
 struct FunctionProperties {
-  ContinuousFunctionProperties::Status m_status = k_defaultStatus;
-  I18n::Message m_caption = k_defaultCaption;
-  ContinuousFunctionProperties::SymbolType m_symbolType = k_defaultSymbolType;
-  Poincare::ComparisonNode::OperatorType m_equationType = k_defaultEquationType;
-  ContinuousFunctionProperties::CurveParameterType m_curveParameterType = k_defaultCurveParameterType;
-  Poincare::Conic::Shape m_conicShape = k_defaultConicShape;
-  bool m_hasTwoSubCurves = k_defaultHasTwoSubCurves;
-  bool m_isAlongY = k_defaultIsAlongY;
+  ContinuousFunctionProperties::Status m_status = ContinuousFunctionProperties::k_defaultStatus;
+  I18n::Message m_caption = ContinuousFunctionProperties::k_defaultCaption;
+  ContinuousFunctionProperties::SymbolType m_symbolType = ContinuousFunctionProperties::k_defaultSymbolType;
+  Poincare::ComparisonNode::OperatorType m_equationType = ContinuousFunctionProperties::k_defaultEquationType;
+  ContinuousFunctionProperties::CurveParameterType m_curveParameterType = ContinuousFunctionProperties::k_defaultCurveParameterType;
+  Poincare::Conic::Shape m_conicShape = ContinuousFunctionProperties::k_defaultConicShape;
+  bool m_isOfDegreeTwo = ContinuousFunctionProperties::k_defaultIsOfDegreeTwo;
+  int m_numberOfSubCurves = 1;
+  bool m_isAlongY = ContinuousFunctionProperties::k_defaultIsAlongY;
 };
 
 void assert_check_function_properties(const char * expression, FunctionProperties expectedProperties, ContinuousFunctionStore * store, Context * context, ContinuousFunctionProperties::AreaType expectedAreaType = ContinuousFunctionProperties::AreaType::None) {
   Shared::ContinuousFunction * function = addFunction(expression, store, context);
-  // Memoize the reduced expression so that numberOfSubCurves() can be asserted
-  function->expressionReduced(context);
 
   ContinuousFunctionProperties fProperties = function->properties();
   quiz_assert(fProperties.caption() == expectedProperties.m_caption);
   quiz_assert(fProperties.status() == expectedProperties.m_status);
   quiz_assert(fProperties.symbolType() == expectedProperties.m_symbolType);
-  quiz_assert(fProperties.getCurveParameterType() == expectedProperties.m_curveParameterType);
-  quiz_assert(fProperties.conicShape() == expectedProperties.m_conicShape);
-  quiz_assert((fProperties.numberOfSubCurves() == 2) == expectedProperties.m_hasTwoSubCurves);
-  quiz_assert(fProperties.isAlongY() == expectedProperties.m_isAlongY);
   if (expectedProperties.m_status == ContinuousFunctionProperties::Status::Enabled) {
-    ContinuousFunctionProperties::AreaType observedAreaType = function->properties().areaType();
-    // Equation type is accurate on enabled functions only.
-    quiz_assert(fProperties.equationType() == expectedProperties.m_equationType);    quiz_assert(observedAreaType == expectedAreaType);
+    quiz_assert(fProperties.getCurveParameterType() == expectedProperties.m_curveParameterType);
+    quiz_assert(fProperties.conicShape() == expectedProperties.m_conicShape);
+    quiz_assert(fProperties.isOfDegreeTwo() == expectedProperties.m_isOfDegreeTwo);
+    quiz_assert(fProperties.isAlongY() == expectedProperties.m_isAlongY);
+    quiz_assert(function->numberOfSubCurves() == expectedProperties.m_numberOfSubCurves);
+    quiz_assert(fProperties.equationType() == expectedProperties.m_equationType);
+    quiz_assert(fProperties.areaType() == expectedAreaType);
   }
 }
 
@@ -56,6 +46,7 @@ void assert_check_function_properties(const char * expression, FunctionPropertie
 QUIZ_CASE(graph_function_plot_type) {
   // Test the plot type under different Press-to-test parameters :
   Preferences::PressToTestParams pressToTestParams = Preferences::k_inactivePressToTest;
+  Poincare::Preferences::sharedPreferences()->setComplexFormat(Preferences::ComplexFormat::Cartesian);
   for (size_t config = 0; config < 4; config++) {
     bool noInequations = (config == 1 || config == 3);
     bool noImplicitPlot = (config == 2 || config == 3);
@@ -126,23 +117,30 @@ QUIZ_CASE(graph_function_plot_type) {
     assert_check_function_properties("0=x", noImplicitPlot ? k_bannedProperties : k_verticalLineProperties);
     assert_check_function_properties("x=1-x", noImplicitPlot ? k_bannedProperties : k_verticalLineProperties);
 
-    constexpr static FunctionProperties k_alongYWithOneSubCurve = FunctionProperties{
+    constexpr static FunctionProperties k_alongY = FunctionProperties{
       .m_caption = I18n::Message::EquationType,
       .m_symbolType = ContinuousFunctionProperties::SymbolType::X,
       .m_isAlongY = true
     };
-    assert_check_function_properties("0=x+cos(y)+y^2", noImplicitPlot ? k_bannedProperties : k_alongYWithOneSubCurve);
-    assert_check_function_properties("y^3=x", noImplicitPlot ? k_bannedProperties : k_alongYWithOneSubCurve);
+    assert_check_function_properties("0=x+cos(y)+y^2", noImplicitPlot ? k_bannedProperties : k_alongY);
+    assert_check_function_properties("y^3=x", noImplicitPlot ? k_bannedProperties : k_alongY);
 
-    constexpr static FunctionProperties k_alongYWithTwoSubCurves = FunctionProperties{
+    constexpr static FunctionProperties k_alongYOfDegreeTwoWithTwoSubCurves = FunctionProperties{
       .m_caption = I18n::Message::EquationType,
-      .m_hasTwoSubCurves = true,
+      .m_isOfDegreeTwo = true,
+      .m_numberOfSubCurves = 2,
       .m_isAlongY = true
     };
-    assert_check_function_properties("0=x^2", noImplicitPlot ? k_bannedProperties : k_alongYWithTwoSubCurves);
-    assert_check_function_properties("1=x^2+x", noImplicitPlot ? k_bannedProperties : k_alongYWithTwoSubCurves);
-    assert_check_function_properties("1+x^2=0", noImplicitPlot ? k_bannedProperties : k_alongYWithTwoSubCurves);
-    assert_check_function_properties("x+x^2=cos(y)", noImplicitPlot ? k_bannedProperties : k_alongYWithTwoSubCurves);
+    constexpr static FunctionProperties k_alongYOfDegreeTwoWithOneSubCurve = FunctionProperties{
+      .m_caption = I18n::Message::EquationType,
+      .m_isOfDegreeTwo = true,
+      .m_numberOfSubCurves = 1,
+      .m_isAlongY = true
+    };
+    assert_check_function_properties("0=x^2", noImplicitPlot ? k_bannedProperties : k_alongYOfDegreeTwoWithOneSubCurve);
+    assert_check_function_properties("1=x^2+x", noImplicitPlot ? k_bannedProperties : k_alongYOfDegreeTwoWithTwoSubCurves);
+    assert_check_function_properties("1+x^2=0", noImplicitPlot ? k_bannedProperties : k_alongYOfDegreeTwoWithTwoSubCurves);
+    assert_check_function_properties("x+x^2=cos(y)", noImplicitPlot ? k_bannedProperties : k_alongYOfDegreeTwoWithTwoSubCurves);
 
     assert_check_function_properties(
       "x^2<0",
@@ -151,7 +149,8 @@ QUIZ_CASE(graph_function_plot_type) {
         FunctionProperties{
           .m_caption = I18n::Message::InequalityType,
           .m_equationType = Poincare::ComparisonNode::OperatorType::Inferior,
-          .m_hasTwoSubCurves = true,
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 1,
           .m_isAlongY = true
         },
       ContinuousFunctionProperties::AreaType::Inside
@@ -190,7 +189,8 @@ QUIZ_CASE(graph_function_plot_type) {
           .m_caption = I18n::Message::InequalityType,
           .m_equationType = Poincare::ComparisonNode::OperatorType::Inferior,
           .m_conicShape = Poincare::Conic::Shape::Circle,
-          .m_hasTwoSubCurves = true
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 2
         },
       ContinuousFunctionProperties::AreaType::Inside
     );
@@ -212,7 +212,8 @@ QUIZ_CASE(graph_function_plot_type) {
         FunctionProperties{
           .m_caption = I18n::Message::CircleType,
           .m_conicShape = Poincare::Conic::Shape::Circle,
-          .m_hasTwoSubCurves = true
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 2
         }
     );
     assert_check_function_properties(
@@ -222,7 +223,8 @@ QUIZ_CASE(graph_function_plot_type) {
         FunctionProperties{
           .m_caption = I18n::Message::EllipseType,
           .m_conicShape = Poincare::Conic::Shape::Ellipse,
-          .m_hasTwoSubCurves = true
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 2
         }
     );
     assert_check_function_properties(
@@ -232,7 +234,8 @@ QUIZ_CASE(graph_function_plot_type) {
         FunctionProperties{
           .m_caption = I18n::Message::ParabolaType,
           .m_conicShape = Poincare::Conic::Shape::Parabola,
-          .m_hasTwoSubCurves = true
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 2
         }
     );
     /* When implicit plots are disabled, these conics are no longer identified
@@ -248,7 +251,7 @@ QUIZ_CASE(graph_function_plot_type) {
           .m_caption = I18n::Message::ParabolaType,
           .m_curveParameterType = ContinuousFunctionProperties::CurveParameterType::CartesianFunction,
           .m_conicShape = Poincare::Conic::Shape::Parabola,
-          .m_hasTwoSubCurves = false
+          .m_isOfDegreeTwo = false
         }
     );
     assert_check_function_properties(
@@ -258,7 +261,8 @@ QUIZ_CASE(graph_function_plot_type) {
         FunctionProperties{
           .m_caption = I18n::Message::HyperbolaType,
           .m_conicShape = Poincare::Conic::Shape::Hyperbola,
-          .m_hasTwoSubCurves = true
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 2
         }
     );
     assert_check_function_properties(
@@ -269,13 +273,14 @@ QUIZ_CASE(graph_function_plot_type) {
           .m_caption = I18n::Message::HyperbolaType,
           .m_curveParameterType = ContinuousFunctionProperties::CurveParameterType::CartesianFunction,
           .m_conicShape = Poincare::Conic::Shape::Hyperbola,
-          .m_hasTwoSubCurves = false
+          .m_isOfDegreeTwo = false
         }
     );
 
     constexpr static FunctionProperties k_twoSubCurves = FunctionProperties{
       .m_caption = I18n::Message::EquationType,
-      .m_hasTwoSubCurves = true
+      .m_isOfDegreeTwo = true,
+      .m_numberOfSubCurves = 2
     };
     assert_check_function_properties("x^2-y^2=0", noImplicitPlot ? k_bannedProperties : k_twoSubCurves);
     assert_check_function_properties("x*y^2=1", noImplicitPlot ? k_bannedProperties : k_twoSubCurves);
@@ -288,7 +293,8 @@ QUIZ_CASE(graph_function_plot_type) {
         FunctionProperties{
           .m_caption = I18n::Message::InequalityType,
           .m_equationType = ComparisonNode::OperatorType::Superior,
-          .m_hasTwoSubCurves = true
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 2
         },
       ContinuousFunctionProperties::AreaType::Outside
     );
@@ -300,7 +306,8 @@ QUIZ_CASE(graph_function_plot_type) {
         FunctionProperties{
           .m_caption = I18n::Message::InequalityType,
           .m_equationType = ComparisonNode::OperatorType::SuperiorEqual,
-          .m_hasTwoSubCurves = true
+          .m_isOfDegreeTwo = true,
+          .m_numberOfSubCurves = 2
         },
       ContinuousFunctionProperties::AreaType::Outside
     );
@@ -360,7 +367,7 @@ QUIZ_CASE(graph_function_plot_type) {
     assert_check_function_properties("y=i*x+1", k_unhandledCartesian);
 
     // Updated complex format
-    Poincare::Preferences::sharedPreferences()->setComplexFormat(Preferences::ComplexFormat::Cartesian);
+    assert(Poincare::Preferences::sharedPreferences()->complexFormat() == Preferences::ComplexFormat::Cartesian);
     assert_check_function_properties("y=(√(-1))^2", k_horizontalLineProperties);
     assert_check_function_properties("y=(i)^2", k_horizontalLineProperties);
     assert_check_function_properties(
@@ -410,7 +417,7 @@ QUIZ_CASE(graph_function_plot_type_with_predefined_variables) {
     assert_check_function_properties("y=a*x+1", k_lineProperties, &store, &context);
     assert_check_function_properties(
       "a*y*y+y=x",
-      FunctionProperties{.m_caption = I18n::Message::EquationType, .m_hasTwoSubCurves = true},
+      FunctionProperties{.m_caption = I18n::Message::EquationType, .m_isOfDegreeTwo = true, .m_numberOfSubCurves = 2},
       &store,
       &context
     );
@@ -427,7 +434,8 @@ QUIZ_CASE(graph_function_plot_type_with_predefined_variables) {
       FunctionProperties{
         .m_caption = I18n::Message::ParabolaType,
         .m_conicShape = Poincare::Conic::Shape::Parabola,
-        .m_hasTwoSubCurves = true
+        .m_isOfDegreeTwo = true,
+        .m_numberOfSubCurves = 2
       },
       &store,
       &context

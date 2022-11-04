4afd3490af824007d16210ddebf0ca61f3c73805
Félix Richart <felix.richart@numworks.com>
2022-11-04 16:20:36 +0100

    [calculation/additional_result] Add margin around (0, 0) in function model graph

--- apps/calculation/additional_outputs/function_model.cpp
+++ apps/calculation/additional_outputs/function_model.cpp
@@ -27,20 +27,20 @@ void FunctionModel::recomputeViewRange() {
   zoom.fitPointsOfInterest(evaluator, static_cast<void *>(&m_function));
 
   zoom.fitPoint(Coordinate2D<float>(m_abscissa, m_ordinate));
-  zoom.fitPoint(Coordinate2D<float>(0.0, 0.0));
+  zoom.fitPoint(Coordinate2D<float>(0.0f, 0.0f));
 
   Range2D range = zoom.range(Range1D::k_maxFloat, false);
 
   float widthPixelRatio =  range.x()->length() / k_width;
-  float rightMargin = (range.xMax() - m_abscissa) / widthPixelRatio;
-  float leftMargin = (m_abscissa - range.xMin()) / widthPixelRatio;
+  float rightMargin = (range.xMax() - std::max(m_abscissa, 0.0f)) / widthPixelRatio;
+  float leftMargin = (std::min(m_abscissa, 0.0f) - range.xMin()) / widthPixelRatio;
 
   setXMin(range.xMin() - (leftMargin >= k_marginInPixels ? 0.0 : k_marginInPixels * widthPixelRatio));
   setXMax(range.xMax() + (rightMargin >= k_marginInPixels ? 0.0 : k_marginInPixels * widthPixelRatio));
 
   float heigthPixelRatio =  range.y()->length() / k_height;
-  float topMargin = (range.yMax() - m_ordinate) / heigthPixelRatio;
-  float bottomMargin = (m_ordinate - range.yMin()) / heigthPixelRatio;
+  float topMargin = (range.yMax() - std::max(m_ordinate, 0.0f)) / heigthPixelRatio;
+  float bottomMargin = (std::min(m_ordinate, 0.0f) - range.yMin()) / heigthPixelRatio;
 
   setYMin(range.yMin() - (bottomMargin >= k_marginInPixels ? 0.0 : k_marginInPixels * heigthPixelRatio));
   setYMax(range.yMax() + (topMargin >= k_marginInPixels ? 0.0 : k_marginInPixels * heigthPixelRatio));

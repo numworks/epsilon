a96b0173277dedc748e4a6a6ba8c5b89080ae43d
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-11-09 11:32:15 +0100

    [additional_outputs] ScientificLayout handles
    BasedInteger.
    For example, 400 was stored as a BasedInteger, which was
    layouted to "400" even in Scientific mode.
    We need to approximate it to a Float first.

--- apps/calculation/additional_outputs/scientific_notation_helper.cpp
+++ apps/calculation/additional_outputs/scientific_notation_helper.cpp
@@ -1,5 +1,6 @@
 #include "scientific_notation_helper.h"
 #include <apps/shared/poincare_helpers.h>
+#include <poincare/float.h>
 
 using namespace Poincare;
 
@@ -16,7 +17,12 @@ Layout ScientificLayout(Expression a, Context * context) {
   assert(!a.hasUnit());
   Preferences preferences = *Preferences::sharedPreferences();
   preferences.setDisplayMode(Preferences::PrintFloatMode::Scientific);
-  return Shared::PoincareHelpers::CreateLayout(a, context, &preferences);
+  if (a.type() != ExpressionNode::Type::BasedInteger) {
+    return Shared::PoincareHelpers::CreateLayout(a, context, &preferences);
+  }
+  // Based Integer must be approximated to be layouted in scientific mode
+  Expression floatRepr = Float<double>::Builder(a.approximateToScalar<double>(context, preferences.complexFormat(), preferences.angleUnit()));
+  return Shared::PoincareHelpers::CreateLayout(floatRepr, context, &preferences);
 }
 
 }

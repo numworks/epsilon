68874f1fd1f27349988b1a2845c864cf13ff79e4
Félix Richart <felix.richart@numworks.com>
2022-11-28 14:21:43 +0100

    [apps/inference] Replace MAXFLOAT with FLT_MAX

--- apps/inference/models/statistic/test.cpp
+++ apps/inference/models/statistic/test.cpp
@@ -136,8 +136,8 @@ void Test::computeCurveViewRange(float transition, bool zoomSide) {
   float height = std::exp(interpolate(std::log(computeYMax()), std::log(targetYMax), transition));
   float yMax = height;
   float yMin =  -k_displayBottomMarginRatio * height;
-  if (std::isnan(xMin)) { xMin = -MAXFLOAT; }
-  if (std::isnan(xMax)) { xMax = MAXFLOAT; }
+  if (std::isnan(xMin)) { xMin = -FLT_MAX; }
+  if (std::isnan(xMax)) { xMax = FLT_MAX; }
   protectedSetX(Poincare::Range1D(xMin, xMax));
   protectedSetY(Poincare::Range1D(yMin, yMax));
 }

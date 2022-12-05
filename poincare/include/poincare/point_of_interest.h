da304a145ce8e731936db2b3b12c79398c2338a7
Félix Richart <felix.richart@numworks.com>
2022-12-05 14:52:13 +0100

    [poincare/point_of_interest] Default arg in constructor

--- poincare/include/poincare/point_of_interest.h
+++ poincare/include/poincare/point_of_interest.h
@@ -30,8 +30,7 @@ class PointOfInterest : public TreeHandle {
 public:
   static PointOfInterest Builder(double x, double y, typename Solver<double>::Interest interest, uint32_t data);
 
-  PointOfInterest() : TreeHandle(nullptr) {}
-  PointOfInterest(PointOfInterestNode * node) : TreeHandle(node) {}
+  PointOfInterest(PointOfInterestNode * node = nullptr) : TreeHandle(node) {}
 
   double x() const { return node()->m_x; }
   double y() const { return node()->m_y; }

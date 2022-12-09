83b62d456d18cfb2b8017bead2651083a13ecd17
MarcNumworks <marc.coudriau@numworks.com>
2022-12-09 15:38:38 +0100

    [apps] Do not show help messages on Idle events

--- apps/shared/function_zoom_and_pan_curve_view_controller.cpp
+++ apps/shared/function_zoom_and_pan_curve_view_controller.cpp
@@ -32,7 +32,7 @@ void FunctionZoomAndPanCurveViewController::didBecomeFirstResponder() {
 }
 
 bool FunctionZoomAndPanCurveViewController::handleEvent(Ion::Events::Event event) {
-  if (event == Ion::Events::Back || event == Ion::Events::Home || event == Ion::Events::OK || event == Ion::Events::EXE) {
+  if (event == Ion::Events::Idle || event == Ion::Events::Back || event == Ion::Events::Home || event == Ion::Events::OK || event == Ion::Events::EXE) {
     setLegendVisible(false);
     adaptRangeForHeaders(false);
     return ZoomAndPanCurveViewController::handleEvent(event);

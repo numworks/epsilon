c706e3a0d5670cf2fa33fcce57774affb99eb007
Romain Goyet <romain.goyet@numworks.com>
2022-12-12 16:15:57 -0500

    [ion/simulator] Add Ion::Simulator::StateFile::loadMemory
    This function loads a .nws file from a memory buffer

--- ion/src/simulator/shared/state_file.cpp
+++ ion/src/simulator/shared/state_file.cpp
@@ -9,80 +9,76 @@ namespace Ion {
 namespace Simulator {
 namespace StateFile {
 
-constexpr static const char* sHeader = "NWSF";
-constexpr static int sHeaderLength = 4;
-constexpr static int sVersionLength = 8;
+constexpr static const char* sMagic = "NWSF";
+constexpr static size_t sMagicLength = 4;
+constexpr static size_t sVersionLength = 8;
 constexpr static const char* sWildcardVersion = "**.**.**";
-constexpr static int sFormatVersionLength = 1;
+constexpr static size_t sFormatVersionLength = 1;
 constexpr static uint8_t sLatestFormatVersion = 1;
-constexpr static int sLanguageLength = Ion::Events::Journal::k_languageSize - 1;
+constexpr static size_t sLanguageLength =
+    Ion::Events::Journal::k_languageSize - 1;
 constexpr static const char* sWildcardLanguage = "**";
+constexpr static size_t sHeaderLength =
+    sMagicLength + sVersionLength + sFormatVersionLength + sLanguageLength;
 
 /* File format:
  * Format version 0xFF (latest) :
- *   "NWSF" : Header
+ *   "NWSF" : Magic
  * + "XXXXXXXX" : Software version
  * + "0x01" : State file format version
  * + "XX" : Language code (en, fr, nl, pt, it, de, or es)
  * + EVENTS...
  */
 
-static inline bool load(FILE* f) {
-  char buffer[sVersionLength + 1];
+static inline bool loadFileHeader(const char* header) {
+  const char* magic = header;
+  const char* version = magic + sMagicLength;
+  const char* formatVersion = version + sVersionLength;
+  const char* language = formatVersion + sFormatVersionLength;
 
-  // Header
-  buffer[sHeaderLength] = 0;
-  if (fread(buffer, sHeaderLength, 1, f) != 1) {
+  if (strncmp(magic, sMagic, sMagicLength) != 0) {
     return false;
   }
-  if (strcmp(buffer, sHeader) != 0) {
+  if (strncmp(version, epsilonVersion(), sVersionLength) != 0 &&
+      strncmp(version, sWildcardVersion, sVersionLength) != 0) {
     return false;
   }
+  if (*formatVersion != sLatestFormatVersion) {
+    return false;
+  }
+  if (strncmp(language, sWildcardLanguage, sLanguageLength) != 0) {
+    Journal::replayJournal()->setStartingLanguage(language);
+  }
+  return true;
+}
 
-  // Software version
-  buffer[sVersionLength] = 0;
-  if (fread(buffer, sVersionLength, 1, f) != 1) {
-    return false;
+static inline void pushEvent(uint8_t c) {
+  Ion::Events::Event e = Ion::Events::Event(c);
+  if (Events::isDefined(static_cast<uint8_t>(e)) && e != Ion::Events::None &&
+      e != Ion::Events::Termination && e != Ion::Events::TimerFire &&
+      e != Ion::Events::ExternalText) {
+    /* Avoid pushing invalid events - useful when fuzzing.
+     * ExternalText is not handled by state files. */
+    Journal::replayJournal()->pushEvent(e);
   }
-  if (strcmp(buffer, epsilonVersion()) != 0 &&
-      strcmp(buffer, sWildcardVersion) != 0) {
-    return false;
-  }
+}
 
-  // Journal
-  Ion::Events::Journal* journal = Journal::replayJournal();
-
-  // Format version
-  int c = 0;
-  static_assert(sFormatVersionLength == 1, "sFormatVersionLength is incorrect");
-  if ((c = getc(f)) == EOF || c != sLatestFormatVersion) {
-    // Only the latest version is handled for now.
+static inline bool loadFile(FILE* f) {
+  char header[sHeaderLength];
+  if (fread(header, sHeaderLength, 1, f) != 1) {
     return false;
   }
-
-  // Language
-  static_assert(sVersionLength + 1 > sLanguageLength,
-                "Buffer isn't long enough for language");
-  buffer[sLanguageLength] = 0;
-  if (fread(buffer, sLanguageLength, 1, f) != 1) {
+  if (!loadFileHeader(header)) {
     return false;
-  }
-  if (strcmp(buffer, sWildcardLanguage) != 0) {
-    journal->setStartingLanguage(buffer);
   }
 
   // Events
+  int c = 0;
   while ((c = getc(f)) != EOF) {
-    Ion::Events::Event e = Ion::Events::Event(c);
-    if (Events::isDefined(static_cast<uint8_t>(e)) && e != Ion::Events::None &&
-        e != Ion::Events::Termination && e != Ion::Events::TimerFire &&
-        e != Ion::Events::ExternalText) {
-      /* Avoid pushing invalid events - useful when fuzzing.
-       * ExternalText is not handled by state files. */
-      journal->pushEvent(e);
-    }
+    pushEvent(c);
   }
-  Ion::Events::replayFrom(journal);
+
+  Ion::Events::replayFrom(Journal::replayJournal());
 
   return true;
 }
@@ -97,14 +93,29 @@ void load(const char* filename) {
   if (f == nullptr) {
     return;
   }
-  load(f);
+  loadFile(f);
   if (f != stdin) {
     fclose(f);
   }
 }
 
+void loadMemory(const char* buffer, size_t length) {
+  if (length < sHeaderLength) {
+    return;
+  }
+  if (!loadFileHeader(buffer)) {
+    return;
+  }
+  const uint8_t* bufferEnd = reinterpret_cast<const uint8_t*>(buffer + length);
+  const uint8_t* e = reinterpret_cast<const uint8_t*>(buffer + sHeaderLength);
+  while (e != bufferEnd) {
+    pushEvent(*e++);
+  }
+  Ion::Events::replayFrom(Journal::replayJournal());
+}
+
 static inline bool save(FILE* f) {
-  if (fwrite(sHeader, sHeaderLength, 1, f) != 1) {
+  if (fwrite(sMagic, sMagicLength, 1, f) != 1) {
     return false;
   }
 #if ASSERTIONS

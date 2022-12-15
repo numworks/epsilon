f0796ed978a819c4ea4bd2e0c444df0a9e0e4e72
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2022-12-15 15:50:00 +0100

    [poincare/test] Add exam mode tests

---
+++ poincare/test/exam_mode.cpp
@@ -0,0 +1,32 @@
+#include <quiz.h>
+#include <poincare/preferences.h>
+#include <ion/include/ion/persisting_bytes.h>
+
+using namespace Poincare;
+
+void assert_exam_mode_is(Ion::PersistingBytes::PersistingBytesInt bytes, Preferences::ExamMode mode) {
+  Preferences::ExamPersistingBytes exam(bytes);
+  quiz_assert(exam.mode() == mode);
+}
+
+void assert_exam_params_are(Ion::PersistingBytes::PersistingBytesInt bytes, Preferences::PressToTestParams params) {
+  Preferences::ExamPersistingBytes exam(bytes);
+  quiz_assert(params == Preferences::k_inactivePressToTest || exam.mode() == Preferences::ExamMode::PressToTest);
+  quiz_assert(exam.params() == params);
+}
+
+QUIZ_CASE(exam_mode) {
+  assert_exam_mode_is(0xffff, Preferences::ExamMode::Unknown);
+  Preferences::PressToTestParams params = Preferences::PressToTestParams({0});
+  params.m_equationSolverIsForbidden = 1;
+  params.m_inequalityGraphingIsForbidden = 1;
+  params.m_implicitPlotsAreForbidden = 1;
+  params.m_statsDiagnosticsAreForbidden = 1;
+  params.m_vectorsAreForbidden = 1;
+  params.m_basedLogarithmIsForbidden = 1;
+  params.m_sumIsForbidden = 1;
+  params.m_exactResultsAreForbidden = 1;
+  params.m_elementsAppIsForbidden = 1;
+  assert_exam_params_are(0x1ff4, params);
+  assert_exam_mode_is(0x001, Preferences::ExamMode::Standard);
+}

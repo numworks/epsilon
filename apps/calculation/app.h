4b8b57df9798cc77137ac439b5808314bf14916c
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2022-12-22 17:25:38 +0100

    [calculation/app] Whitespace

--- apps/calculation/app.h
+++ apps/calculation/app.h
@@ -18,36 +18,44 @@ public:
     I18n::Message upperName() const override;
     const Escher::Image * icon() const override;
   };
+
   class Snapshot : public Shared::SharedApp::Snapshot {
   public:
     Snapshot();
+
     App * unpack(Escher::Container * container) override;
     void reset() override;
     const Descriptor * descriptor() const override;
     CalculationStore * calculationStore() { return &m_calculationStore; }
     char * cacheBuffer() { return m_cacheBuffer; }
     size_t * cacheBufferInformationAddress() { return &m_cacheBufferInformation; }
+
   private:
-    CalculationStore m_calculationStore;
     // Set the size of the buffer needed to store the calculations
     constexpr static int k_calculationBufferSize = 10 * (sizeof(Calculation) + Calculation::k_numberOfExpressions * Constant::MaxSerializedExpressionSize + sizeof(Calculation *));
+
+    CalculationStore m_calculationStore;
     char m_calculationBuffer[k_calculationBufferSize];
     char m_cacheBuffer[EditExpressionController::k_cacheBufferSize];
     size_t m_cacheBufferInformation;
   };
-  static App * app() {
-    return static_cast<App *>(Escher::Container::activeApp());
-  }
+
+  static App * app() { return static_cast<App *>(Escher::Container::activeApp()); }
+
   TELEMETRY_ID("Calculation");
+
   bool textFieldDidReceiveEvent(Escher::AbstractTextField * textField, Ion::Events::Event event) override;
   bool layoutFieldDidReceiveEvent(Escher::LayoutField * layoutField, Ion::Events::Event event) override;
+
   // TextFieldDelegateApp
   bool isAcceptableExpression(const Poincare::Expression expression) override;
 
 private:
   App(Snapshot * snapshot);
-  HistoryController m_historyController;
+
   void didBecomeActive(Escher::Window * window) override;
+
+  HistoryController m_historyController;
   EditExpressionController m_editExpressionController;
 };
 

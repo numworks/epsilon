db40f92c47b7e7822fe00a82ebc971e7ad588151
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2023-01-03 16:43:26 +0100

    [on_boarding/prompt_controller] Idle does not dismiss popup

--- apps/on_boarding/prompt_controller.cpp
+++ apps/on_boarding/prompt_controller.cpp
@@ -50,7 +50,7 @@ PromptController::PromptController(const I18n::Message * messages, const KDColor
 }
 
 bool PromptController::handleEvent(Ion::Events::Event event) {
-  if (event != Ion::Events::Back && event != Ion::Events::OnOff && event != Ion::Events::USBPlug && event != Ion::Events::USBEnumeration) {
+  if (Ion::Events::EventCanDiscardPopUp(event) && event != Ion::Events::Back && event != Ion::Events::OnOff) {
     Container::activeApp()->modalViewController()->dismissModal();
     AppsContainer * appsContainer = AppsContainer::sharedAppsContainer();
     if (appsContainer->activeApp()->snapshot() == appsContainer->onBoardingAppSnapshot()) {

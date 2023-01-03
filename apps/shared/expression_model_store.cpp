f8878e5cd8d41d9a9bcd5a2162738678ffc99b1f
Félix Richart <felix.richart@numworks.com>
2023-01-03 10:06:36 +0100

    [shared/expression_model_store] Rework termination condition of while loops

--- apps/shared/expression_model_store.cpp
+++ apps/shared/expression_model_store.cpp
@@ -45,32 +45,34 @@ void ExpressionModelStore::tidyDownstreamPoolFrom(char * treePoolCursor) {
 int ExpressionModelStore::numberOfModelsSatisfyingTest(ModelTest test, void * context) const {
   int count = 0;
   int index = 0;
-  Ion::Storage::Record record;
-  do {
-    record = recordAtIndex(index++);
-    if (record.isNull()) {
-      break;
-    }
+  Ion::Storage::Record record = recordAtIndex(0);
+  while (!record.isNull()) {
     if (test(privateModelForRecord(record), context)) {
       count++;
     }
-  } while (true);
+    record = recordAtIndex(++index);
+  }
   return count;
 }
 
 Ion::Storage::Record ExpressionModelStore::recordSatisfyingTestAtIndex(int i, ModelTest test, void * context) const {
+  assert(i >= 0);
   int count = 0;
   int index = 0;
-  Ion::Storage::Record record;
-  do {
-    record = recordAtIndex(index++);
+  Ion::Storage::Record record = recordAtIndex(0);
+  while (!record.isNull()) {
     if (test(privateModelForRecord(record), context)) {
       if (i == count) {
         break;
       }
       count++;
     }
-  } while (true);
+    record = recordAtIndex(++index);
+  }
+  /* Returned record is null if i >= numberOfModelsSatisfyingTest.
+   * We'd like to assert that i < numberOfModelsStatisfyingTest at the
+   * beginning of this function but the method numberOfModelsStatisfyingTest
+   * is not const. */
   assert(!record.isNull());
   return record;
 }

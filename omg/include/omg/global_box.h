1c73874df5deeb198b0720a0537364bb7b3d2ee5
Romain Goyet <romain.goyet@numworks.com>
2023-01-09 12:32:40 -0500

    [omg/global_box] Add ASSERTIONS

--- omg/include/omg/global_box.h
+++ omg/include/omg/global_box.h
@@ -12,17 +12,42 @@ namespace OMG {
 
 template <typename T> class GlobalBox {
 public:
+#if ASSERTIONS
+  GlobalBox() : m_initialized(false) {}
+#endif
   void init() {
+#if ASSERTIONS
+    assert(!m_initialized);
+#endif
     new (m_buffer) T();
+#if ASSERTIONS
+    m_initialized = true;
+#endif
   }
   void shutdown() {
+#if ASSERTIONS
+    assert(m_initialized);
+#endif
     // TODO: Placement delete
+#if ASSERTIONS
+    m_initialized = false;
+#endif
   }
-  T* operator->() {
+  T * operator->() {
+#if ASSERTIONS
+    assert(m_initialized);
+#endif
     return reinterpret_cast<T *>(m_buffer);
   }
+  T * operator*() {
+    return reinterpret_cast<T *>(m_buffer);
+  }
+
 private:
   uint8_t m_buffer[sizeof(T)];
+#if ASSERTIONS
+  bool m_initialized;
+#endif
 };
 
 }

338af107f3a66b4f0ed4384837643a0f616101f7
MarcNumworks <marc.coudriau@numworks.com>
2023-01-19 15:59:36 +0100

    [python] Fix crash with hist(0,-1)

--- python/port/mod/matplotlib/pyplot/modpyplot.cpp
+++ python/port/mod/matplotlib/pyplot/modpyplot.cpp
@@ -301,13 +301,13 @@ mp_obj_t modpyplot_hist(size_t n_args, const mp_obj_t *args, mp_map_t* kw_args )
     }
 
     mp_float_t binWidth = (max-min)/nBins;
-    // Create a array of bins
-    edgeItems = m_new(mp_obj_t, nBins + 1);
     // Handle empty range case
     if (max - min <= FLT_EPSILON) {
       binWidth = 1.0;
       nBins = 1;
     }
+    // Create a array of bins
+    edgeItems = m_new(mp_obj_t, nBins + 1);
 
     // Fill the bin edges list
     for (size_t i = 0; i < nBins+1; i++) {

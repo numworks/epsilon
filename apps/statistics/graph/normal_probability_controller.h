9661a49f27d8ec366c2e5307b72e460450c7d46e
Pia Chancerel <pia.chancerel@numworks.com>
2023-01-26 17:57:45 +0100

    [apps/shared] Rename ValidSeriesAndValidTotalNormalProbabilities in ActiveSeriesAndActiveTotalNormalProbabilities

--- apps/statistics/graph/normal_probability_controller.h
+++ apps/statistics/graph/normal_probability_controller.h
@@ -36,7 +36,7 @@ public:
 private:
   // PlotController
   // Hide series having invalid total values.
-  Shared::DoublePairStore::ActiveSeriesTest validSerieMethod() const override { return Store::ValidSeriesAndValidTotalNormalProbabilities; };
+  Shared::DoublePairStore::ActiveSeriesTest validSerieMethod() const override { return Store::ActiveSeriesAndActiveTotalNormalProbabilities; };
   bool moveSelectionHorizontally(int deltaIndex) override;
   void computeYBounds(float * yMin, float *yMax) const override;
   bool handleNullFrequencies() const override { return false; }

af6e185525fb30dbfe2d99c9c0d342c625c4c80e
Pia Chancerel <pia.chancerel@numworks.com>
2023-02-08 17:17:52 +0100

    [apps/regression] Typo

--- apps/regression/store.h
+++ apps/regression/store.h
@@ -43,7 +43,7 @@ public:
 
   // Dots
   /* Return the closest dot to abscissa x above the regression curve if
-    * direction > 0, below otherwise */
+  * direction > 0, below otherwise */
   int closestVerticalDot(int direction, double x, double y, int currentSeries, int currentDot, int * nextSeries, Poincare::Context * globalContext);
   /* Return the closest dot to given dot, on the right if direction > 0,
    * on the left otherwise */

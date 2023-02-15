c623f65bcf8d77339c1799838efc5c4af02f8c9a
Félix Richart <felix.richart@numworks.com>
2023-02-15 11:23:22 +0100

    [poincare/empty_rectangle] Use simpler KDRect constructor

--- poincare/src/empty_rectangle.cpp
+++ poincare/src/empty_rectangle.cpp
@@ -11,7 +11,7 @@ void EmptyRectangle::DrawEmptyRectangle(KDContext * ctx, KDPoint p, KDFont::Size
   KDSize rectangleSize = RectangleSize(font, false);
 
   KDColor fillColor = rectangleColor == Color::Yellow ? Escher::Palette::YellowDark : Escher::Palette::GrayBright;
-  ctx->fillRect(KDRect(p.x() + k_marginWidth, p.y() + k_marginHeight, rectangleSize.width(), rectangleSize.height()), fillColor);
+  ctx->fillRect(KDRect(p.x() + k_marginWidth, p.y() + k_marginHeight, rectangleSize), fillColor);
 }
 
 }

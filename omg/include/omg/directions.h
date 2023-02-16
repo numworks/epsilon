4096d34bfbf26906551e4b45f2e8661b61bc5ae7
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2023-02-16 13:42:30 +0100

    [omg/directions] Prevent HorizontalDirection::Up and such

--- omg/include/omg/directions.h
+++ omg/include/omg/directions.h
@@ -7,7 +7,7 @@
 namespace OMG {
 
 class Direction {
-public:
+ public:
   Direction(Ion::Events::Event event);
 
   bool isLeft() const { return m_tag == Tag::Left; }
@@ -15,7 +15,9 @@ public:
   bool isUp() const { return m_tag == Tag::Up; }
   bool isDown() const { return m_tag == Tag::Down; }
 
-  bool isHorizontal() const { return m_tag == Tag::Left || m_tag == Tag::Right; }
+  bool isHorizontal() const {
+    return m_tag == Tag::Left || m_tag == Tag::Right;
+  }
   bool isVertical() const { return m_tag == Tag::Up || m_tag == Tag::Down; }
 
   operator class HorizontalDirection() const;
@@ -26,16 +28,11 @@ public:
   constexpr static Direction Up() { return Direction(Tag::Up); }
   constexpr static Direction Down() { return Direction(Tag::Down); }
 
-  bool operator ==(const Direction &other) const { return m_tag == other.m_tag; }
-  bool operator !=(const Direction &other) const { return m_tag != other.m_tag; }
+  bool operator==(const Direction &other) const { return m_tag == other.m_tag; }
+  bool operator!=(const Direction &other) const { return m_tag != other.m_tag; }
 
-private:
-  enum class Tag {
-    Left,
-    Right,
-    Up,
-    Down
-  };
+ private:
+  enum class Tag { Left, Right, Up, Down };
 
   constexpr Direction(Tag tag) : m_tag(tag) {}
 
@@ -43,11 +40,17 @@ private:
 };
 
 class HorizontalDirection : public Direction {
+ private:
+  using Direction::Down;
+  using Direction::Up;
 };
 
 class VerticalDirection : public Direction {
+ private:
+  using Direction::Left;
+  using Direction::Right;
 };
 
-}
+}  // namespace OMG
 
 #endif

f4ef6d6c4a8519e1b3006a324d4f2586de21f8a3
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2023-03-06 15:15:17 +0100

    [solver] Split up tests

--- apps/solver/test/equation_store.cpp
+++ apps/solver/test/equation_store.cpp
@@ -2,105 +2,85 @@
 
 #include "helpers.h"
 
-QUIZ_CASE(equation_solve) {
+QUIZ_CASE(equation_solver_error) {
+  assert_solves_to_error("cos(x)=0", RequireApproximateSolution);
   assert_solves_to_error("x+y+z+a+b+c+d=0", TooManyVariables);
   assert_solves_to_error("x^2+y=0", NonLinearSystem);
-  assert_solves_to_error("cos(x)=0", RequireApproximateSolution);
   assert_solves_to_error("x^3+x^2+1=int(1/t,t,0,1)", EquationUndefined);
   assert_solves_to_error("x×(x^2×int(1/t,t,0,1)+1)=0", EquationUndefined);
+}
 
+QUIZ_CASE(equation_solve_linear_system) {
+  assert_solves_to(
+      {"x+y=0", "3x+y+z=-5", "4z-π=0", "a+b+c=0", "a=3", "c=a+2"},
+      {"x=(-π-20)/8", "y=(π+20)/8", "z=π/4", "a=3", "b=-8", "c=5"});
+  assert_solves_to("2x+3=4", "x=1/2");
+  assert_solves_to({"x+y=0", "3x+y+z=-5", "4z-π=0"},
+                   {"x=(-π-20)/8", "y=(π+20)/8", "z=π/4"});
+  assert_solves_to({"x+y=0", "3x+y=-5"}, {"x=-5/2", "y=5/2"});
+  assert_solves_to_infinite_solutions("0=0");
+  assert_solves_to_infinite_solutions("x+y=0");
+  assert_solves_to_infinite_solutions("x-x=0");
+  assert_solves_to_infinite_solutions({"4y+(1-√(5))x=0", "x=(1+√(5))y"});
   assert_solves_to_no_solution("2=0");
   assert_solves_to_no_solution("e=1");
   assert_solves_to_no_solution("i=5");
   assert_solves_to_no_solution("x-x+2=0");
+}
 
-  assert_solves_to_infinite_solutions("0=0");
-  assert_solves_to_infinite_solutions("x-x=0");
-
-  assert_solves_to("2x+3=4", "x=1/2");
-  assert_solves_to("3×x^2-4x+4=2",
-                   {"x=2/3-(√(2)/3)i", "x=2/3+(√(2)/3)i", "delta=-8"});
+QUIZ_CASE(equation_solve_polynomials) {
+  assert_solves_to("(x-3)^2=0", {"x=3", "delta=0"});
+  assert_solves_to("(x-π)(x-ln(2))=0",
+                   {"x=ln(2)", "x=π", "delta=ln(2)^2+π^2-2×π×ln(2)"});
+  assert_solves_to("(x-√(2))(x-√(3))=0",
+                   {"x=√(2)", "x=√(3)", "delta=5-2×√(6)"});
+  assert_solves_to("2×x^2-4×x+2=0", {"x=1", "delta=0"});
   assert_solves_to("2×x^2-4×x+4=3",
                    {"x=(2-√(2))/2", "x=(2+√(2))/2", "delta=8"});
-  assert_solves_to("2×x^2-4×x+2=0", {"x=1", "delta=0"});
+  assert_solves_to("3×x^2-4x+4=2",
+                   {"x=2/3-(√(2)/3)i", "x=2/3+(√(2)/3)i", "delta=-8"});
   assert_solves_to("x^2+x+1=3×x^2+π×x-√(5)",
                    {"x=(-π+1-√(π^2-2π+9+8×√(5)))/4",
                     "x=(-π+1+√(π^2-2π+9+8√(5)))/4", "delta=π^2-2π+9+8√(5)"});
-  assert_solves_to("(x-3)^2=0", {"x=3", "delta=0"});
-  assert_solves_to("(x-√(2))(x-√(3))=0",
-                   {"x=√(2)", "x=√(3)", "delta=5-2×√(6)"});
-  assert_solves_to("(x-π)(x-ln(2))=0",
-                   {"x=ln(2)", "x=π", "delta=ln(2)^2+π^2-2×π×ln(2)"});
-
-  assert_solves_to("x^3-4x^2+6x-24=0",
-                   {"x=4", "x=-√(6)×i", "x=√(6)×i", "delta=-11616"});
-  assert_solves_to("x^3+x^2+1=0",
-                   {"x=-1.465571232", "x=0.2327856159-0.7925519925×i",
-                    "x=0.2327856159+0.7925519925×i", "delta=-31"});
-  assert_solves_to("x^3-3x-2=0", {"x=-1", "x=2", "delta=0"});
   assert_solves_to("x^3+x+1=0",
                    {"x=-0.6823278038", "x=0.3411639019-1.1615414×i",
                     "x=0.3411639019+1.1615414×i", "delta=-31"});
+  assert_solves_to("x^3+x^2+1=0",
+                   {"x=-1.465571232", "x=0.2327856159-0.7925519925×i",
+                    "x=0.2327856159+0.7925519925×i", "delta=-31"});
   assert_solves_to("x^3+x^2=10^200", {"delta=-27×10^400+4×10^200"});
+  assert_solves_to("x^3-3x-2=0", {"x=-1", "x=2", "delta=0"});
+  assert_solves_to("x^3-4x^2+6x-24=0",
+                   {"x=4", "x=-√(6)×i", "x=√(6)×i", "delta=-11616"});
+}
 
-  // Linear System
-  assert_solves_to_infinite_solutions("x+y=0");
-  assert_solves_to({"x+y=0", "3x+y=-5"}, {"x=-5/2", "y=5/2"});
-  assert_solves_to({"x+y=0", "3x+y+z=-5", "4z-π=0"},
-                   {"x=(-π-20)/8", "y=(π+20)/8", "z=π/4"});
-  assert_solves_to(
-      {"x+y=0", "3x+y+z=-5", "4z-π=0", "a+b+c=0", "a=3", "c=a+2"},
-      {"x=(-π-20)/8", "y=(π+20)/8", "z=π/4", "a=3", "b=-8", "c=5"});
-  assert_solves_to_infinite_solutions({"4y+(1-√(5))x=0", "x=(1+√(5))y"});
-
-  /* This test case needs the user defined variable. Indeed, in the equation
-   * store, m_variables is just before m_userVariables, so bad fetching in
-   * m_variables might fetch into m_userVariables and create problems. */
-  set("x", "0");
-  assert_solves_to_infinite_solutions({"b=0", "D=0", "c=0", "x+y+z+t=0"});
-  unset("x");
-
-  /* Without the user defined variable, this test has too many variables.
-   * With the user defined variable, it has no solutions. */
-  set("g", "0");
-  assert_solves_to_no_solution({"a=a+1", "a+b+c+d+f+g+h=0"});
-  unset("g");
-
-  // Monovariable non-polynomial equation
+QUIZ_CASE(equation_solve_approximate) {
   Poincare::Preferences::sharedPreferences->setAngleUnit(Degree);
+  assert_solves_numerically_to("(3x)^3/(0.1-3x)^3=10^(-8)", -10, 10,
+                               {0.000071660});
+  assert_solves_numerically_to("(x-1)/(2×(x-2)^2)=20.8", -10, 10,
+                               {1.856511, 2.167528});
+  assert_solves_numerically_to("-2/(x-4)=-x^2+2x-4", -10, 10, {4.154435});
+  assert_solves_numerically_to("1/cos(x)=0", 0, 10000, {});
+  assert_solves_numerically_to("4.4ᴇ-9/(0.12+x)^2=1.1ᴇ-9/x^2", -10, 10,
+                               {-0.04, 0.12});
+  assert_solves_numerically_to("8x^4-22x^2+15=0", -10, 10,
+                               {-1.224745, -1.118034, 1.118034, 1.224745});
   assert_solves_numerically_to("cos(x)=0", -100, 100, {-90.0, 90.0});
   assert_solves_numerically_to("cos(x)=0", -900, 1000,
                                {-810.0, -630.0, -450.0, -270.0, -90.0, 90.0,
                                 270.0, 450.0, 630.0, 810.0});
-  assert_solves_numerically_to("1/cos(x)=0", 0, 10000, {});
+  assert_solves_numerically_to("e^x/1000=0", -1000, 1000, {});
+  assert_solves_numerically_to("e^x=0", -1000, 1000, {});
   assert_solves_numerically_to("√(y)=0", -900, 1000, {0}, "y");
   assert_solves_numerically_to("√(y+1)=0", -900, 1000, {-1}, "y");
-  assert_solves_numerically_to("e^x=0", -1000, 1000, {});
-  assert_solves_numerically_to("e^x/1000=0", -1000, 1000, {});
-  assert_solves_numerically_to("(x-1)/(2×(x-2)^2)=20.8", -10, 10,
-                               {1.856511, 2.167528});
-  assert_solves_numerically_to("8x^4-22x^2+15=0", -10, 10,
-                               {-1.224745, -1.118034, 1.118034, 1.224745});
-  assert_solves_numerically_to("(3x)^3/(0.1-3x)^3=10^(-8)", -10, 10,
-                               {0.000071660});
-  assert_solves_numerically_to("4.4ᴇ-9/(0.12+x)^2=1.1ᴇ-9/x^2", -10, 10,
-                               {-0.04, 0.12});
-  assert_solves_numerically_to("-2/(x-4)=-x^2+2x-4", -10, 10, {4.154435});
-  // assert_solves_numerically_to("0^x=0", -10, 10, {0.2, 0.4, 0.6,
-  // 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0});
-
   // The ends of the interval are solutions
-  assert_solves_numerically_to("sin(x)=0", -180, 180, {-180, 0, 180});
-  assert_solves_numerically_to("(x-1)^2×(x+1)^2=0", -1, 1, {-1, 1});
-  assert_solves_numerically_to("(x-1.00001)^2×(x+1.00001)^2=0", -1, 1, {});
   assert_solves_numerically_to(
       "sin(x)=0", 0, 10000,
       {0, 180, 360, 540, 720, 900, 1080, 1260, 1440, 1620});
-
-  // Long variable names
-  assert_solves_to("2\"abcde\"+3=4", "\"abcde\"=1/2");
-  assert_solves_to({"\"Big1\"+\"Big2\"=0", "3\"Big1\"+\"Big2\"=-5"},
-                   {"\"Big1\"=-5/2", "\"Big2\"=5/2"});
+  assert_solves_numerically_to("(x-1)^2×(x+1)^2=0", -1, 1, {-1, 1});
+  assert_solves_numerically_to("(x-1.00001)^2×(x+1.00001)^2=0", -1, 1, {});
+  assert_solves_numerically_to("sin(x)=0", -180, 180, {-180, 0, 180});
 
   // conj(x)*x+1 = 0
   assert_solves_to_error("conj(x)*x+1=0", RequireApproximateSolution);
@@ -161,6 +141,24 @@ QUIZ_CASE(equation_solve_complex_polar) {
 }
 
 QUIZ_CASE(equation_and_symbolic_computation) {
+  /* This test case needs the user defined variable. Indeed, in the equation
+   * store, m_variables is just before m_userVariables, so bad fetching in
+   * m_variables might fetch into m_userVariables and create problems. */
+  set("x", "0");
+  assert_solves_to_infinite_solutions({"b=0", "D=0", "c=0", "x+y+z+t=0"});
+  unset("x");
+
+  // Long variable names
+  assert_solves_to("2\"abcde\"+3=4", "\"abcde\"=1/2");
+  assert_solves_to({"\"Big1\"+\"Big2\"=0", "3\"Big1\"+\"Big2\"=-5"},
+                   {"\"Big1\"=-5/2", "\"Big2\"=5/2"});
+
+  /* Without the user defined variable, this test has too many variables.
+   * With the user defined variable, it has no solutions. */
+  set("g", "0");
+  assert_solves_to_no_solution({"a=a+1", "a+b+c+d+f+g+h=0"});
+  unset("g");
+
   set("a", "x");
   // a is undef
   assert_solves_to({"a=0"}, {"a=0"});

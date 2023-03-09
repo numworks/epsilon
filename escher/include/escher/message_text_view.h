1bf9b2cd7c970fdb62c91909a34ffe9f6f1c280b
Félix Richart <felix.richart@numworks.com>
2023-03-09 17:13:50 +0100

    [escher/cell_widget] Make MessageTextView and SwitchView cell widgets

--- escher/include/escher/message_text_view.h
+++ escher/include/escher/message_text_view.h
@@ -1,12 +1,13 @@
 #ifndef ESCHER_MESSAGE_TEXT_VIEW_H
 #define ESCHER_MESSAGE_TEXT_VIEW_H
 
+#include <escher/cell_widget.h>
 #include <escher/i18n.h>
 #include <escher/text_view.h>
 
 namespace Escher {
 
-class MessageTextView : public TextView {
+class MessageTextView : public TextView, public CellWidget {
  public:
   MessageTextView(KDFont::Size font = KDFont::Size::Large,
                   I18n::Message message = (I18n::Message)0,
@@ -18,6 +19,12 @@ class MessageTextView : public TextView {
   void setMessage(I18n::Message message);
   const char* text() const override;
   KDSize minimalSizeForOptimalDisplay() const override;
+
+  // CellWidget
+  const View* view() const override { return this; }
+  void setWidgetBackgroundColor(KDColor color) override {
+    setBackgroundColor(color);
+  }
 
  private:
   I18n::Message m_message;
--- escher/include/escher/switch_view.h
+++ escher/include/escher/switch_view.h
@@ -1,12 +1,13 @@
 #ifndef ESCHER_SWITCH_VIEW_H
 #define ESCHER_SWITCH_VIEW_H
 
+#include <escher/cell_widget.h>
 #include <escher/palette.h>
 #include <escher/toggleable_view.h>
 
 namespace Escher {
 
-class SwitchView final : public ToggleableView {
+class SwitchView final : public ToggleableView, public CellWidget {
  public:
   using ToggleableView::ToggleableView;
   /* k_switchHeight and k_switchWidth are the dimensions of the switch
@@ -16,6 +17,14 @@ class SwitchView final : public ToggleableView {
   constexpr static KDCoordinate k_switchWidth = 22;
   KDSize minimalSizeForOptimalDisplay() const override {
     return KDSize(k_switchWidth, k_switchHeight);
+  }
+
+  // CellWidget
+  const View* view() const override { return this; }
+  bool enterOnEvent(Ion::Events::Event event) const override {
+    // TODO: This is not in the spec. Remove ?
+    return (event == Ion::Events::Right && !state()) ||
+           (event == Ion::Events::Left && state());
   }
 
  private:

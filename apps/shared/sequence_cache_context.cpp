28e3550e99871bfe30416dfd4dd060d8dcfe1738
Pia Chancerel <pia.chancerel@numworks.com>
2023-03-14 15:39:06 +0100

    [apps/shared/sequence_cache_context] Dedent inside method

--- apps/shared/sequence_cache_context.cpp
+++ apps/shared/sequence_cache_context.cpp
@@ -25,55 +25,55 @@ const Poincare::Expression
 SequenceCacheContext<T>::protectedExpressionForSymbolAbstract(
     const Poincare::SymbolAbstract &symbol, bool clone,
     ContextWithParent *lastDescendantContext) {
-  if (symbol.type() == Poincare::ExpressionNode::Type::Sequence) {
-    T result = NAN;
-    /* Do not use recordAtIndex : if the sequences have been reordered, the
-     * name index and the record index may not correspond. */
-    int index = nameIndexForSymbol(const_cast<Poincare::Symbol &>(
-        static_cast<const Poincare::Symbol &>(symbol)));
-    Ion::Storage::Record record =
-        m_sequenceContext->sequenceStore()->recordAtNameIndex(index);
-    if (record.isNull()) {
-      return Poincare::Float<T>::Builder(result);
-    }
-    assert(record.fullName()[0] == symbol.name()[0]);
-    Sequence *seq = m_sequenceContext->sequenceStore()->modelForRecord(record);
-    if (!seq->fullName()) {
-      return Poincare::Float<T>::Builder(result);
-    }
-    Poincare::Expression rank = symbol.childAtIndex(0).clone();
-    if (rank.isIdenticalTo(Poincare::Symbol::Builder(UCodePointUnknown))) {
-      // rank = n
-      result = m_values[index][0];
-    } else if (rank.isIdenticalTo(Poincare::Addition::Builder(
-                   Poincare::Symbol::Builder(UCodePointUnknown),
-                   Poincare::Rational::Builder(1)))) {
-      // rank = n+1
-      result = m_values[index][1];
-    } else if (rank.isIdenticalTo(Poincare::Addition::Builder(
-                   Poincare::Symbol::Builder(UCodePointUnknown),
-                   Poincare::Rational::Builder(2)))) {
-      // rank = n+2
-      result = m_values[index][2];
-    }
-    /* If the symbol was not in the two previous ranks, we try to approximate
-     * the sequence independently from the others at the required rank (this
-     * will solve u(n) = 5*n, v(n) = u(n+10) for instance). But we avoid doing
-     * so if the sequence referencing itself to avoid an infinite loop. */
-    if (std::isnan(result) && index != m_sequenceBeingComputed) {
-      /* The lastDesendantContext might contain informations on variables
-       * that are contained in the rank expression. */
-      T n = PoincareHelpers::ApproximateToScalar<T>(
-          rank, lastDescendantContext ? lastDescendantContext : this);
-      // If the rank is not an int, return NAN
-      if (std::floor(n) == n) {
-        result = seq->valueAtRank<T>(n, m_sequenceContext, true);
-      }
-    }
+  if (symbol.type() != Poincare::ExpressionNode::Type::Sequence) {
+    return ContextWithParent::protectedExpressionForSymbolAbstract(
+        symbol, clone, lastDescendantContext);
+  }
+  T result = NAN;
+  /* Do not use recordAtIndex : if the sequences have been reordered, the
+   * name index and the record index may not correspond. */
+  int index = nameIndexForSymbol(const_cast<Poincare::Symbol &>(
+      static_cast<const Poincare::Symbol &>(symbol)));
+  Ion::Storage::Record record =
+      m_sequenceContext->sequenceStore()->recordAtNameIndex(index);
+  if (record.isNull()) {
     return Poincare::Float<T>::Builder(result);
   }
-  return ContextWithParent::protectedExpressionForSymbolAbstract(
-      symbol, clone, lastDescendantContext);
+  assert(record.fullName()[0] == symbol.name()[0]);
+  Sequence *seq = m_sequenceContext->sequenceStore()->modelForRecord(record);
+  if (!seq->fullName()) {
+    return Poincare::Float<T>::Builder(result);
+  }
+  Poincare::Expression rank = symbol.childAtIndex(0).clone();
+  if (rank.isIdenticalTo(Poincare::Symbol::Builder(UCodePointUnknown))) {
+    // rank = n
+    result = m_values[index][0];
+  } else if (rank.isIdenticalTo(Poincare::Addition::Builder(
+                 Poincare::Symbol::Builder(UCodePointUnknown),
+                 Poincare::Rational::Builder(1)))) {
+    // rank = n+1
+    result = m_values[index][1];
+  } else if (rank.isIdenticalTo(Poincare::Addition::Builder(
+                 Poincare::Symbol::Builder(UCodePointUnknown),
+                 Poincare::Rational::Builder(2)))) {
+    // rank = n+2
+    result = m_values[index][2];
+  }
+  /* If the symbol was not in the two previous ranks, we try to approximate
+   * the sequence independently from the others at the required rank (this
+   * will solve u(n) = 5*n, v(n) = u(n+10) for instance). But we avoid doing
+   * so if the sequence referencing itself to avoid an infinite loop. */
+  if (std::isnan(result) && index != m_sequenceBeingComputed) {
+    /* The lastDesendantContext might contain informations on variables
+     * that are contained in the rank expression. */
+    T n = PoincareHelpers::ApproximateToScalar<T>(
+        rank, lastDescendantContext ? lastDescendantContext : this);
+    // If the rank is not an int, return NAN
+    if (std::floor(n) == n) {
+      result = seq->valueAtRank<T>(n, m_sequenceContext, true);
+    }
+  }
+  return Poincare::Float<T>::Builder(result);
 }
 
 template <typename T>

981a9e4dd899f264508710666c4a93aa7e971afe
Félix Richart <felix.richart@numworks.com>
2023-03-15 09:51:50 +0100

    [escher] Make GaugeView a CellWidget

--- escher/include/escher/gauge_view.h
+++ escher/include/escher/gauge_view.h
@@ -1,19 +1,23 @@
 #ifndef ESCHER_GAUGE_VIEW_H
 #define ESCHER_GAUGE_VIEW_H
 
+#include <escher/cell_widget.h>
 #include <escher/transparent_view.h>
 
 namespace Escher {
 
-class GaugeView : public TransparentView {
+class GaugeView : public TransparentView, public CellWidget {
  public:
   GaugeView();
   float level();
   void setLevel(float level);
-  void setBackgroundColor(KDColor color);
   void drawRect(KDContext* ctx, KDRect rect) const override;
   KDSize minimalSizeForOptimalDisplay() const override;
   constexpr static KDCoordinate k_indicatorDiameter = 10;
+
+  // CellWidget
+  const View* view() const override { return this; }
+  void setBackgroundColor(KDColor color) override;
 
  private:
   constexpr static KDCoordinate k_thickness = 2;

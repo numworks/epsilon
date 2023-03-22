851cd266e46e6e77e5f98fb58005bfb310374453
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2023-03-22 10:54:25 +0100

    [poincare/test] Add American exam mode tests

--- poincare/test/exam_mode.cpp
+++ poincare/test/exam_mode.cpp
@@ -112,6 +112,42 @@ QUIZ_CASE(poincare_exam_mode_english) {
   quiz_assert(!mode.forbidExactResults());
 }
 
+QUIZ_CASE(poincare_exam_mode_staar) {
+  ExamMode mode(ExamMode::Ruleset::STAAR);
+  quiz_assert(!mode.forbidSolverApp());
+  quiz_assert(!mode.forbidElementsApp());
+  quiz_assert(!mode.forbidCodeApp());
+  quiz_assert(!mode.forbidLineDetails());
+  quiz_assert(mode.forbidInequalityGraphing());
+  quiz_assert(mode.forbidImplicitPlots());
+  quiz_assert(!mode.forbidStatsDiagnostics());
+  quiz_assert(!mode.forbidVectorProduct());
+  quiz_assert(!mode.forbidVectorNorm());
+  quiz_assert(!mode.forbidBasedLogarithm());
+  quiz_assert(!mode.forbidSum());
+  quiz_assert(!mode.forbidUnits());
+  quiz_assert(!mode.forbidAdditionalResults());
+  quiz_assert(!mode.forbidExactResults());
+}
+
+QUIZ_CASE(poincare_exam_mode_keystone) {
+  ExamMode mode(ExamMode::Ruleset::Keystone);
+  quiz_assert(mode.forbidSolverApp());
+  quiz_assert(!mode.forbidElementsApp());
+  quiz_assert(!mode.forbidCodeApp());
+  quiz_assert(!mode.forbidLineDetails());
+  quiz_assert(!mode.forbidInequalityGraphing());
+  quiz_assert(mode.forbidImplicitPlots());
+  quiz_assert(!mode.forbidStatsDiagnostics());
+  quiz_assert(!mode.forbidVectorProduct());
+  quiz_assert(!mode.forbidVectorNorm());
+  quiz_assert(!mode.forbidBasedLogarithm());
+  quiz_assert(!mode.forbidSum());
+  quiz_assert(!mode.forbidUnits());
+  quiz_assert(!mode.forbidAdditionalResults());
+  quiz_assert(mode.forbidExactResults());
+}
+
 QUIZ_CASE(poincare_exam_mode_press_to_test) {
   {
     ExamMode mode(ExamMode::Ruleset::PressToTest,

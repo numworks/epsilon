a92c69bc1844c8383cb3344182e5c0b30a74b0a6
Pia Chancerel <pia.chancerel@numworks.com>
2023-03-28 13:48:53 +0200

    [omg] Tests of signaling NAN are not the same for web

--- omg/test/signaling_nan.cpp
+++ omg/test/signaling_nan.cpp
@@ -11,6 +11,11 @@ QUIZ_CASE(omg_signaling_nan) {
   quiz_assert(OMG::IsSignalingNan(OMG::SignalingNan<double>()));
   quiz_assert(!OMG::IsSignalingNan(static_cast<float>(NAN)));
   quiz_assert(!OMG::IsSignalingNan(static_cast<double>(NAN)));
+#if !__EMSCRIPTEN__
+  /* Emscripten cannot represent a NaN literal with custom bit pattern in
+   * NAN-canonicalizing JS engines. We use a magic number instead, which is
+   * not a NAN. */
   quiz_assert(std::isnan(OMG::SignalingNan<float>()));
   quiz_assert(std::isnan(OMG::SignalingNan<double>()));
+#endif
 }

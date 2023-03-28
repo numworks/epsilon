64b85723d56b23a72c253b3b821d7c69730ec081
Pia Chancerel <pia.chancerel@numworks.com>
2023-03-28 11:49:35 +0200

    [omg] Use template specialization for SignalingNan()

--- omg/src/signaling_nan.cpp
+++ omg/src/signaling_nan.cpp
@@ -2,13 +2,13 @@
 
 namespace OMG {
 
-template <typename T>
-T SignalingNan() {
-  return sizeof(T) == sizeof(float) ? static_cast<T>(k_sNanFloat.f)
-                                    : static_cast<T>(k_sNanDouble.d);
+template <>
+float SignalingNan<float>() {
+  return k_sNanFloat.f;
 }
-
-template float SignalingNan<float>();
-template double SignalingNan<double>();
+template <>
+double SignalingNan<double>() {
+  return k_sNanDouble.d;
+}
 
 }  // namespace OMG

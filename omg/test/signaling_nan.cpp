9d45079c4d85912c905b889d5cef7d520b6f4896
Pia Chancerel <pia.chancerel@numworks.com>
2023-03-28 11:24:38 +0200

    [omg/test] Add tests for isnan(SignalingNan)

--- omg/test/signaling_nan.cpp
+++ omg/test/signaling_nan.cpp
@@ -2,6 +2,8 @@
 #include <omg/signaling_nan.h>
 #include <quiz.h>
 
+#include <cmath>
+
 using namespace OMG;
 
 QUIZ_CASE(omg_signaling_nan) {
@@ -9,4 +11,6 @@ QUIZ_CASE(omg_signaling_nan) {
   quiz_assert(OMG::IsSignalingNan(OMG::SignalingNan<double>()));
   quiz_assert(!OMG::IsSignalingNan(static_cast<float>(NAN)));
   quiz_assert(!OMG::IsSignalingNan(static_cast<double>(NAN)));
+  quiz_assert(std::isnan(OMG::SignalingNan<float>()));
+  quiz_assert(std::isnan(OMG::SignalingNan<double>()));
 }

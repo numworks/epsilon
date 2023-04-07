6eb49b56adc0784e1dfbb218935d495ee42e93c5
Félix Richart <felix.richart@numworks.com>
2023-04-07 13:47:04 +0200

    [distributions] Use sharedPreferences significant digits for unknown parameter result

--- apps/distributions/probability/calculation_controller.cpp
+++ apps/distributions/probability/calculation_controller.cpp
@@ -266,7 +266,7 @@ void CalculationController::reload() {
         m_distribution->parameterAtIndex(
             m_distribution->uninitializedParameterIndex()),
         Poincare::Preferences::PrintFloatMode::Decimal,
-        Poincare::Preferences::ShortNumberOfSignificantDigits);
+        Poincare::Preferences::sharedPreferences->numberOfSignificantDigits());
     m_contentView.unknownParameterValue()->setText(buffer);
   }
   m_contentView.reload();

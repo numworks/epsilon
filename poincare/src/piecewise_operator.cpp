5603c8a9abc2e07e5f4ecd930560b87e388604c2
Félix Richart <felix.richart@numworks.com>
2023-04-11 11:20:24 +0200

    [poincare/piecewise] Fix piecewise returning the last condition when a condition can't be resolved

--- poincare/src/piecewise_operator.cpp
+++ poincare/src/piecewise_operator.cpp
@@ -65,8 +65,11 @@ int PiecewiseOperatorNode::indexOfFirstTrueCondition(
   while (i + 1 < n) {
     Evaluation<T> condition =
         childAtIndex(i + 1)->approximate(T(), approximationContext);
-    if (condition.type() == EvaluationNode<T>::Type::BooleanEvaluation &&
-        static_cast<BooleanEvaluation<T>&>(condition).value()) {
+    if (condition.type() != EvaluationNode<T>::Type::BooleanEvaluation) {
+      // Undef condition. It can be either true or false
+      return -1;
+    }
+    if (static_cast<BooleanEvaluation<T>&>(condition).value()) {
       return i;
     }
     i += 2;

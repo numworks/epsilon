917ef0bb139ee843ec9253ea62b46a44c37228bf
MarcNumworks <marc.coudriau@numworks.com>
2023-04-12 16:21:39 +0200

    [apps] Refactor handleRunException

--- apps/apps_container.cpp
+++ apps/apps_container.cpp
@@ -243,14 +243,16 @@ void AppsContainer::switchToExternalApp(Ion::ExternalApps::App app) {
 }
 
 void AppsContainer::handleRunException(bool resetSnapshot) {
-  if (s_activeApp != nullptr) {
-    /* The app models can reference layouts or expressions that have been
-     * destroyed from the pool. To avoid using them before packing the app
-     * (in App::willBecomeInactive for instance), we tidy them early on. */
-    App::Snapshot* activeSnapshot = s_activeApp->snapshot();
+  App::Snapshot* activeSnapshot =
+      s_activeApp != nullptr ? s_activeApp->snapshot() : nullptr;
+  if (activeSnapshot == homeAppSnapshot()) {
+    // Reset home selection if already selected
+    dispatchEvent(Ion::Events::Back);
+    return;
+  }
+  switchToBuiltinApp(homeAppSnapshot());
+  if (activeSnapshot != nullptr) {
     if (resetSnapshot) {
-      // Call App::willBecomeInactive before the snapshot is changed
-      switchToBuiltinApp(nullptr);
       /* When an app encountered an exception due to a full pool, the next time
        * the user enters the app, the same exception could happen again which
        * would prevent from reopening the app. To avoid being stuck outside the
@@ -262,12 +264,7 @@ void AppsContainer::handleRunException(bool resetSnapshot) {
     } else {
       activeSnapshot->tidy();
     }
-    if (activeSnapshot == homeAppSnapshot()) {
-      // Reset home selection if already selected
-      dispatchEvent(Ion::Events::Back);
-    }
   }
-  switchToBuiltinApp(appSnapshotAtIndex(0));
 }
 
 void AppsContainer::run() {

f88775ba14d3984d4fd1eb9942fb6462ca2fcc86
Félix Richart <felix.richart@numworks.com>
2023-04-14 13:51:16 +0200

    [apps/continuous_function] Comment

--- apps/shared/continuous_function.h
+++ apps/shared/continuous_function.h
@@ -344,6 +344,9 @@ class ContinuousFunction : public Function {
     // Return size of the record's expression
     size_t expressionSize(const Ion::Storage::Record *record) const override;
     mutable ContinuousFunctionProperties m_properties;
+    /* m_expression is used for values table and points of interest.
+     * m_expressionApproximated is used for plot and autozoom.
+     */
     mutable Poincare::Expression m_expressionApproximated;
     mutable Poincare::Expression m_expressionDerivate;
   };

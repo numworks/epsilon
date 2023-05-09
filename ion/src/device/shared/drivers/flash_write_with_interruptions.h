e4266336f10b5e099f68a03cb95417adb00cd250
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2023-05-09 14:53:06 +0200

    [epsilon-core] Update submodule

--- ion/src/device/shared/drivers/flash_write_with_interruptions.h
+++ ion/src/device/shared/drivers/flash_write_with_interruptions.h
@@ -8,10 +8,10 @@ namespace Ion {
 namespace Device {
 namespace Flash {
 
-void MassEraseWithInterruptions(bool handleInterruptsAndCache);
-bool EraseSectorWithInterruptions(int i, bool handleInterruptsAndCache);
+void MassEraseWithInterruptions(bool handleInterrupts);
+bool EraseSectorWithInterruptions(int i, bool handleInterrupts);
 bool WriteMemoryWithInterruptions(uint8_t* destination, const uint8_t* source,
-                                  size_t length, bool handleInterruptsAndCache);
+                                  size_t length, bool handleInterrupts);
 
 }  // namespace Flash
 }  // namespace Device

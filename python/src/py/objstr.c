09ea594a8624582f1e8e9b989fb760fe8341b51f
MarcNumworks <marc.coudriau@numworks.com>
2023-05-26 13:56:44 +0200

    [python] Fix memory corruption when n * lhs_len overflows

--- python/src/py/objstr.c
+++ python/src/py/objstr.c
@@ -341,6 +341,10 @@ mp_obj_t mp_obj_str_binary_op(mp_binary_op_t op, mp_obj_t lhs_in, mp_obj_t rhs_i
             }
         }
         vstr_t vstr;
+        /* Warning: this is a NumWorks change to MicroPython 1.17 */
+        if (n > UINT_MAX / lhs_len) {
+          m_malloc_fail(UINT_MAX);
+        }
         vstr_init_len(&vstr, lhs_len * n);
         mp_seq_multiply(lhs_data, sizeof(*lhs_data), lhs_len, n, vstr.buf);
         return mp_obj_new_str_from_vstr(lhs_type, &vstr);

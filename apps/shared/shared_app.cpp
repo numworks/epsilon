9b3b6a8899806a4b0f629e1cb69544ddcf6a97da
Félix Richart <felix.richart@numworks.com>
2023-06-01 09:58:27 +0200

    [shared] Comment

--- apps/shared/shared_app.cpp
+++ apps/shared/shared_app.cpp
@@ -6,6 +6,10 @@
 
 namespace Shared {
 
+/* TODO: SharedApp will be refactored in v22. This will be a good occasion to
+ * put ContinuousFunctionStore and SequenceStore out of the global context and
+ * in the MathApp instead. These two methods will then need to be also
+ * refactored. */
 void SharedApp::Snapshot::tidy() {
   /* Since the sequence store and the continuous function store is now
    * accessible from every app, when exiting any application, we need to tidy

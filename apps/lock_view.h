cabf87617c43f12929e05ca5f6293a652e75134d
Félix Richart <felix.richart@numworks.com>
2023-06-09 15:53:24 +0200

    test

--- apps/lock_view.h
+++ apps/lock_view.h
@@ -4,8 +4,8 @@
 #include <escher/transparent_view.h>
 
 class LockView : public Escher::TransparentView {
-public:
-  void drawRect(KDContext * ctx, KDRect rect) const override;
+ public:
+  void drawRect(KDContext* ctx, KDRect rect) const override;
   KDSize minimalSizeForOptimalDisplay() const override;
   constexpr static int k_lockHeight = 9;
   constexpr static int k_lockWidth = 7;

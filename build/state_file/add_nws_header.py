d7835509d70744de34310be6e87ab8c90065a20c
Félix Richart <felix.richart@numworks.com>
2023-06-12 10:11:13 +0200

    [build/state_file] Add scripts to add and remove header

---
+++ build/state_file/add_nws_header.py
@@ -0,0 +1,14 @@
+import sys
+
+src = sys.argv[1]
+if (len(sys.argv) > 2):
+  output = sys.argv[2]
+else:
+  output = src
+
+wildCardHeader = bytearray(b'\x4E\x57\x53\x46\x2A\x2A\x2E\x2A\x2A\x2E\x2A\x2A\x01\x2A\x2A')
+with open(src, "rb") as f:
+  srcBytes = bytearray(f.read());
+  newFile = open(output, "wb")
+  newFile.write(wildCardHeader)
+  newFile.write(srcBytes)

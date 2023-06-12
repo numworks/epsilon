018f0b498b904a6c6727631d601cbb3b08948dd0
Félix Richart <felix.richart@numworks.com>
2023-06-12 10:11:13 +0200

    [build/state_file] Add scripts to add and remove header

---
+++ build/state_file/remove_nws_header.py
@@ -0,0 +1,13 @@
+import sys
+
+src = sys.argv[1]
+if (len(sys.argv) > 2):
+  output = sys.argv[2]
+else:
+  output = src
+
+headerLength = 15
+with open(src, "rb") as f:
+  srcBytes = bytearray(f.read());
+  newFile = open(output, "wb")
+  newFile.write(srcBytes[headerLength:])

c9074e1cbabcc369e1d107ff19f138a2a2a25273
Félix Richart <felix.richart@numworks.com>
2023-06-12 11:43:29 +0200

    Comment

--- build/state_file/add_nws_header.py
+++ build/state_file/add_nws_header.py
@@ -6,6 +6,7 @@ if (len(sys.argv) > 2):
 else:
   output = src
 
+# NWSF**.**.**\x01**"
 wildCardHeader = bytearray(b'\x4E\x57\x53\x46\x2A\x2A\x2E\x2A\x2A\x2E\x2A\x2A\x01\x2A\x2A')
 with open(src, "rb") as f:
   srcBytes = bytearray(f.read());

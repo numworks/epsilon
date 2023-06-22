2c8a97d95adbbc6ebff280b28f1f8f9967a0c443
Félix Richart <felix.richart@numworks.com>
2023-06-22 15:09:49 +0200

    [kandinsky] Remove the overflow asserts in kandinsky and put them in some dangerous places in layout_node.cpp and view.cpp
    Python can overflow KDCoordinate when pushing a pixel

--- kandinsky/src/point.cpp
+++ kandinsky/src/point.cpp
@@ -2,16 +2,12 @@
 #include <kandinsky/point.h>
 
 KDPoint KDPoint::translatedBy(KDPoint other) const {
-  assert(!SumOverflowsKDCoordinate(m_x, other.x()));
-  assert(!SumOverflowsKDCoordinate(m_y, other.y()));
   return KDPoint(m_x + other.x(), m_y + other.y());
 }
 
 KDPoint KDPoint::opposite() const { return KDPoint(-m_x, -m_y); }
 
 uint16_t KDPoint::squareDistanceTo(KDPoint other) const {
-  assert(!SumOverflowsKDCoordinate(m_x, -other.x()));
-  assert(!SumOverflowsKDCoordinate(m_y, -other.y()));
   return (m_x - other.x()) * (m_x - other.x()) +
          (m_y - other.y()) * (m_y - other.y());
 }

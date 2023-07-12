449f4734026ddc087898bd90226f78f6667b3c29
Félix Richart <felix.richart@numworks.com>
2023-07-12 14:12:46 +0200

    [statistics/histogram_parameter_controller] Set a max barWidth and a max starting abscissa

--- apps/statistics/graph/histogram_parameter_controller.cpp
+++ apps/statistics/graph/histogram_parameter_controller.cpp
@@ -123,8 +123,12 @@ void HistogramParameterController::buttonAction() {
 
 bool HistogramParameterController::authorizedParameters(
     double barWidth, double firstDrawnBarAbscissa) {
-  if (barWidth < 0.0) {
+  if (barWidth < 0.0 || barWidth > Poincare::Range1D::k_maxFloat) {
     // The bar width cannot be negative
+    return false;
+  }
+  if (firstDrawnBarAbscissa > Poincare::Range1D::k_maxFloat ||
+      firstDrawnBarAbscissa < -Poincare::Range1D::k_maxFloat) {
     return false;
   }
   assert(DoublePairStore::k_numberOfSeries > 0);

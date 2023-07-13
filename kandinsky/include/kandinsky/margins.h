80ba6e46b8f1b1034b6355bdfb22bb6a25ea39f7
MarcNumworks <marc.coudriau@numworks.com>
2023-07-13 17:48:01 +0200

    [kandinsky] Inherit KDMargins from its horizontal and vertical parts

--- kandinsky/include/kandinsky/margins.h
+++ kandinsky/include/kandinsky/margins.h
@@ -68,17 +68,18 @@ class KDVerticalMargins : public KD1DMargins {
 // KDMargins(horizontal, vertical)
 // KDMargins(margin) -> all margins are equal
 // KDMargins() -> all margins are 0
-class KDMargins {
+class KDMargins : public KDHorizontalMargins, public KDVerticalMargins {
  public:
   constexpr KDMargins(KDCoordinate left, KDCoordinate right, KDCoordinate top,
                       KDCoordinate bottom)
-      : m_horizontal(left, right), m_vertical(top, bottom) {}
+      : KDHorizontalMargins(left, right), KDVerticalMargins(top, bottom) {}
   constexpr KDMargins(KDHorizontalMargins horizontal,
                       KDVerticalMargins vertical)
-      : m_horizontal(horizontal), m_vertical(vertical) {}
+      : KDHorizontalMargins(horizontal), KDVerticalMargins(vertical) {}
   constexpr explicit KDMargins(KDCoordinate margin)
-      : m_horizontal(margin, margin), m_vertical(margin, margin) {}
-  constexpr KDMargins() : m_horizontal(), m_vertical() {}
+      : KDHorizontalMargins(margin, margin),
+        KDVerticalMargins(margin, margin) {}
+  constexpr KDMargins() : KDHorizontalMargins(), KDVerticalMargins() {}
 
   constexpr bool operator==(const KDMargins& other) {
     return other.horizontal() == horizontal() && other.vertical() == vertical();
@@ -88,29 +89,17 @@ class KDMargins {
     return KDMargins(-left(), -right(), -top(), -bottom());
   }
 
-  constexpr KDHorizontalMargins horizontal() const { return m_horizontal; }
-  constexpr KDVerticalMargins vertical() const { return m_vertical; }
+  constexpr KDHorizontalMargins horizontal() const { return *this; }
+  constexpr KDVerticalMargins vertical() const { return *this; }
 
-  constexpr KDCoordinate left() const { return m_horizontal.left(); }
-  constexpr KDCoordinate right() const { return m_horizontal.right(); }
-  constexpr KDCoordinate top() const { return m_vertical.top(); }
-  constexpr KDCoordinate bottom() const { return m_vertical.bottom(); }
+  void setHorizontal(KDHorizontalMargins m) {
+    static_cast<KDHorizontalMargins&>(*this) = m;
+  }
+  void setVertical(KDVerticalMargins m) {
+    static_cast<KDVerticalMargins&>(*this) = m;
+  }
 
-  void setHorizontal(KDHorizontalMargins m) { m_horizontal = m; }
-  void setVertical(KDVerticalMargins m) { m_vertical = m; }
-
-  void setLeft(KDCoordinate m) { m_horizontal.setLeft(m); }
-  void setRight(KDCoordinate m) { m_horizontal.setRight(m); }
-  void setTop(KDCoordinate m) { m_vertical.setTop(m); }
-  void setBottom(KDCoordinate m) { m_vertical.setBottom(m); }
-
-  constexpr KDCoordinate width() const { return m_horizontal.width(); }
-  constexpr KDCoordinate height() const { return m_vertical.height(); }
   constexpr KDPoint topLeftPoint() const { return KDPoint(left(), top()); }
-
- private:
-  KDHorizontalMargins m_horizontal;
-  KDVerticalMargins m_vertical;
 };
 
 #endif

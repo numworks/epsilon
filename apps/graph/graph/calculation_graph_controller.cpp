3e899070dbaf8c2086e747b2161c3188fc94a99e
Félix Richart <felix.richart@numworks.com>
2023-07-19 15:11:31 +0200

    [graph] Duplicate call to viewWillAppear

--- apps/graph/graph/calculation_graph_controller.cpp
+++ apps/graph/graph/calculation_graph_controller.cpp
@@ -58,7 +58,6 @@ bool CalculationGraphController::handleEnter() {
 }
 
 void CalculationGraphController::viewWillAppear() {
-  Shared::SimpleInteractiveCurveViewController::viewWillAppear();
   curveView()->setFocus(true);
   assert(!m_record.isNull());
   Coordinate2D<double> pointOfInterest = computeNewPointOfInterestFromAbscissa(

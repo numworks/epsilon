9a8f494c53fc1435e370a0bb812c7d0a49470085
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2023-07-21 17:33:07 +0200

    Add SouthCarolina exam mode

--- apps/exam_pop_up_controller.cpp
+++ apps/exam_pop_up_controller.cpp
@@ -54,8 +54,8 @@ I18n::Message ExamPopUpController::activationWarningMessage() const {
       I18n::Message::ActiveDutchExamModeMessage,
       I18n::Message::ActiveDutchExamModeWithResetMessage,
       // IBTest
-      I18n::Message::ActiveKeystoneSTAAROrIBExamModeMessage,
-      I18n::Message::ActiveKeystoneSTAAROrIBExamModeWithResetMessage,
+      I18n::Message::ActiveTxPaScIbExamModeMessage,
+      I18n::Message::ActiveTxPaScIbExamModeWithResetMessage,
       // PressToTest
       I18n::Message::ActivePressToTestModeMessage,
       I18n::Message::ActivePressToTestWithResetMessage,
@@ -66,11 +66,14 @@ I18n::Message ExamPopUpController::activationWarningMessage() const {
       I18n::Message::ActiveEnglishExamModeMessage,
       I18n::Message::ActiveEnglishExamModeWithResetMessage,
       // STAAR
-      I18n::Message::ActiveKeystoneSTAAROrIBExamModeMessage,
-      I18n::Message::ActiveKeystoneSTAAROrIBExamModeWithResetMessage,
+      I18n::Message::ActiveTxPaScIbExamModeMessage,
+      I18n::Message::ActiveTxPaScIbExamModeWithResetMessage,
       // Keystone
-      I18n::Message::ActiveKeystoneSTAAROrIBExamModeMessage,
-      I18n::Message::ActiveKeystoneSTAAROrIBExamModeWithResetMessage,
+      I18n::Message::ActiveTxPaScIbExamModeMessage,
+      I18n::Message::ActiveTxPaScIbExamModeWithResetMessage,
+      // SouthCarolina
+      I18n::Message::ActiveTxPaScIbExamModeMessage,
+      I18n::Message::ActiveTxPaScIbExamModeWithResetMessage,
   };
   static_assert(std::size(messages) == numberOfModes * messagesPerMode,
                 "messages size is invalid");
--- ion/include/ion/exam_mode.h
+++ ion/include/ion/exam_mode.h
@@ -19,6 +19,7 @@ enum class Ruleset : Int {
   English,
   STAAR,
   Keystone,
+  SouthCarolina,
   NumberOfRulesets,
 };
 

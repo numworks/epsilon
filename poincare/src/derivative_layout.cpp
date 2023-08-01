c181813d929d56bff0d2767e780db3e9d6013044
Félix Richart <felix.richart@numworks.com>
2023-08-01 15:48:12 +0200

    [poincare/derivative_layout] Move check of overflow in computeSize

--- poincare/src/derivative_layout.cpp
+++ poincare/src/derivative_layout.cpp
@@ -191,12 +191,27 @@ Layout DerivativeLayoutNode::XNTLayout(int childIndex) const {
 }
 
 KDSize DerivativeLayoutNode::computeSize(KDFont::Size font) {
+  /* The derivative layout could overflow KDCoordinate if the variable or the
+   * order layouts are too large. Since they are duplicated, if there are nested
+   * derivative layouts, the size can be very large while the layout doesn't
+   * overflow the pool. This limit is to prevent this from happening. */
+  constexpr static KDCoordinate k_maxVariableAndOrderSize =
+      KDCOORDINATE_MAX / 4;
+  KDSize variableSize = variableLayout()->layoutSize(font);
+  KDSize orderSize = KDSize(orderWidth(font), orderHeightOffset(font));
+  if (variableSize.height() >= k_maxVariableAndOrderSize ||
+      variableSize.width() >= k_maxVariableAndOrderSize ||
+      orderSize.height() >= k_maxVariableAndOrderSize ||
+      orderSize.width() >= k_maxVariableAndOrderSize) {
+    ExceptionCheckpoint::Raise();
+  }
+
   KDPoint abscissaPosition = positionOfChild(abscissaLayout(), font);
   KDSize abscissaSize = abscissaLayout()->layoutSize(font);
   return KDSize(abscissaPosition.x() + abscissaSize.width(),
                 std::max(abscissaPosition.y() + abscissaSize.height(),
                          positionOfVariableInAssignmentSlot(font).y() +
-                             variableLayout()->layoutSize(font).height()));
+                             variableSize.height()));
 }
 
 KDCoordinate DerivativeLayoutNode::computeBaseline(KDFont::Size font) {
@@ -213,21 +228,6 @@ KDCoordinate DerivativeLayoutNode::computeBaseline(KDFont::Size font) {
 
 KDPoint DerivativeLayoutNode::positionOfChild(LayoutNode* child,
                                               KDFont::Size font) {
-  /* The derivative layout could overflow KDCoordinate if the variable or the
-   * order layouts are too large. Since they are duplicated, if there are nested
-   * derivative layouts, the size can be very large while the layout doesn't
-   * overflow the pool. This limit is to prevent this from happening. */
-  constexpr static KDCoordinate k_maxVariableAndOrderSize =
-      KDCOORDINATE_MAX / 4;
-  KDSize variableSize = variableLayout()->layoutSize(font);
-  KDSize orderSize = KDSize(orderWidth(font), orderHeightOffset(font));
-  if (variableSize.height() >= k_maxVariableAndOrderSize ||
-      variableSize.width() >= k_maxVariableAndOrderSize ||
-      orderSize.height() >= k_maxVariableAndOrderSize ||
-      orderSize.width() >= k_maxVariableAndOrderSize) {
-    ExceptionCheckpoint::Raise();
-  }
-
   if (child == variableLayout()) {
     return m_variableSlot == VariableSlot::Fraction
                ? positionOfVariableInFractionSlot(font)

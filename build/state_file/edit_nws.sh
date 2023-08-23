ee4b8e79f9397a4f53714121cf5e3e669a783c7b
Félix Richart <felix.richart@numworks.com>
2023-08-23 17:49:49 +0200

    [build/state_file] Enhance edit_nws to be able to see the changes and keep editing

--- build/state_file/edit_nws.sh
+++ build/state_file/edit_nws.sh
@@ -2,15 +2,42 @@
 
 if [ $# -lt 1 ]; then
   echo "Not enough parameters."
-  echo "Syntax: 'edit_nws.sh NWS_PATH' [NWS2TXT_PATH]"
+  echo "Syntax: 'edit_nws.sh NWS_PATH [EPSILON_BIN]'"
   exit 1
 fi
 
-NWS_FILE=$1
-TMP_FILE=nws_edit_$(date +%d.%m.%y-%H.%M.%S).txt
+ORIGINAL_NWS=$1
+DATE=$(date +%d.%m.%y-%H.%M.%S)
+FOLDER=nws_edit_$DATE
+TXT_FILE=$FOLDER/tmp.txt
+NWS_FILE=$FOLDER/tmp.nws
 
-python3 build/state_file/nws2txt.py $NWS_FILE > $TMP_FILE &&
-${EDITOR:-vim} $TMP_FILE &&
-python3 build/state_file/nws2txt.py $TMP_FILE > $NWS_FILE &&
-rm $TMP_FILE &&
-echo "$1 was successfully edited"
+EPSILON_BIN=${2:-}
+
+mkdir $FOLDER
+python3 build/state_file/nws2txt.py $ORIGINAL_NWS > $TXT_FILE
+
+while true
+do
+  ${EDITOR:-vim} $TXT_FILE
+  python3 build/state_file/nws2txt.py $TXT_FILE > $NWS_FILE
+  if [ $# -gt 1 ]; then
+    rm $FOLDER/*.png > /dev/null 2>&1
+    rm $FOLDER/*.gif > /dev/null 2>&1
+    ./build/screenshots/take_for_each_step.sh $EPSILON_BIN $NWS_FILE $FOLDER > /dev/null 2>&1
+    rm $FOLDER/*.png > /dev/null 2>&1
+    if [ -f "$FOLDER/scenario.gif" ]; then
+      echo "Gif of the new state file was saved in $FOLDER"
+    else
+      echo "Gif of the new state file could not be generated. Your state file could be badly formatted or your epsilon.bin path could be incorrect."
+    fi
+  fi
+  read -s -n 1 -p "Do you want to Continue edition / Save changes and quit / Quit without saving ? [c/S/q]" csq
+  echo
+  case $csq in
+    [Cc]* ) echo "Continue edition";;
+    [Qq]* ) echo "$ORIGINAL_NWS was not modified"; break;;
+    * ) mv $NWS_FILE $ORIGINAL_NWS && echo "$ORIGINAL_NWS was successfully edited"; break;;
+  esac
+done
+rm -rf $FOLDER

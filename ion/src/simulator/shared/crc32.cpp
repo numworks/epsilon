dea20d758805182a959d69d37e3d7a4ec3fc88a1
Pia Chancerel <pia.chancerel@numworks.com>
2023-08-24 10:20:21 +0200

    [ion/simulator] crc32Helper should take as length directly the length corresponding to the data type

--- ion/src/simulator/shared/crc32.cpp
+++ ion/src/simulator/shared/crc32.cpp
@@ -3,18 +3,16 @@
 
 namespace Ion {
 
-static uint32_t crc32Helper(const uint8_t *data, size_t length,
-                            bool wordAccess) {
+static uint32_t crc32Helper(const uint8_t *data, size_t length) {
   if (length == 0) {
     return 0;
   }
   assert(data != nullptr);
   size_t uint32ByteLength = sizeof(uint32_t) / sizeof(uint8_t);
   uint32_t crc = 0xFFFFFFFF;
-  size_t byteLength = (wordAccess ? length * uint32ByteLength : length);
-  size_t wordLength = byteLength / uint32ByteLength;
+  size_t lengthInDoubleWords = length / uint32ByteLength;
 
-  for (size_t i = 0; i < wordLength; i++) {
+  for (size_t i = 0; i < lengthInDoubleWords; i++) {
     // FIXME: Assumes little-endian byte order!
     for (int j = uint32ByteLength - 1; j >= 0; j--) {
       // scan byte by byte to avoid alignment issue when building for emscripten
@@ -22,18 +20,19 @@ static uint32_t crc32Helper(const uint8_t *data, size_t length,
       crc = crc32EatByte(crc, data[i * uint32ByteLength + j]);
     }
   }
-  for (size_t i = wordLength * uint32ByteLength; i < byteLength; i++) {
+  for (size_t i = lengthInDoubleWords * uint32ByteLength; i < length; i++) {
     crc = crc32EatByte(crc, data[i]);
   }
   return crc;
 }
 
 uint32_t crc32DoubleWord(const uint32_t *data, size_t length) {
-  return crc32Helper(reinterpret_cast<const uint8_t *>(data), length, true);
+  return crc32Helper(reinterpret_cast<const uint8_t *>(data),
+                     length * (sizeof(uint32_t) / sizeof(uint8_t)));
 }
 
 uint32_t crc32Byte(const uint8_t *data, size_t length) {
-  return crc32Helper(data, length, false);
+  return crc32Helper(data, length);
 }
 
 }  // namespace Ion

e5193ea380f94f92167ba8910ca40ba3f50b88eb
Pia Chancerel <pia.chancerel@numworks.com>
2023-08-28 17:44:09 +0200

    [build/screenshots] Add a script to easily add a scenario and its screenshot in screenshot dataset

---
+++ build/screenshots/add_scenario.py
@@ -0,0 +1,53 @@
+import sys, os, shutil
+import helper
+
+help_message = "Use command line: add_scenario.py <state_file.nws> <name>\nIf no name is given, we use the one of the state file."
+state_file_extension = '.nws'
+screenshot_extension = '.png'
+
+def get_args(argv):
+   if len(argv) < 1:
+      print("Error: missing parameter")
+      print(help_message)
+      sys.exit()
+   if len(argv) > 2:
+      print("Error: too many parameters")
+      print(help_message)
+      sys.exit()
+
+   # Get state file
+   state_file = argv[0]
+   if not os.path.exists(state_file) or os.path.splitext(state_file)[1] != state_file_extension:
+      print("Error:", state_file, "is not a state file")
+      sys.exit()
+
+   # Get name
+   name = os.path.splitext(state_file)[0]
+   if len(argv) == 2:
+      name = argv[1]
+
+   return state_file, name
+
+def main(argv):
+
+   # Get params
+   state_file, name = get_args(argv)
+
+   # Create new folder
+   new_dir = os.path.join("tests/screenshots_dataset", name)
+   if os.path.exists(new_dir):
+      print("Error:", new_dir, "already exists. Choose another name.")
+      sys.exit()
+   os.mkdir(new_dir)
+
+   # Add the state file
+   new_state_file_path = os.path.join(new_dir, "scenario" + state_file_extension)
+   shutil.copy(state_file, new_state_file_path)
+
+   # Generate the corresponding screenshot
+   screenshot = os.path.join(new_dir, "screenshot" + screenshot_extension)
+   executable = helper.executable_built_path()
+   helper.generate_screenshot(new_state_file_path, executable, screenshot)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])

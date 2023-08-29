44d4c4cc4a8a658914255de104cddda23c9dd53f
Pia Chancerel <pia.chancerel@numworks.com>
2023-08-29 10:56:40 +0200

    [ion/simulator] Make variable constexpr

--- ion/src/simulator/shared/crc32.cpp
+++ ion/src/simulator/shared/crc32.cpp
@@ -3,24 +3,25 @@
 
 namespace Ion {
 
+constexpr size_t k_uint32ByteLength = sizeof(uint32_t) / sizeof(uint8_t);
+
 uint32_t crc32Byte(const uint8_t *data, size_t length) {
   if (length == 0) {
     return 0;
   }
   assert(data != nullptr);
-  size_t uint32ByteLength = sizeof(uint32_t) / sizeof(uint8_t);
   uint32_t crc = 0xFFFFFFFF;
-  size_t lengthInDoubleWords = length / uint32ByteLength;
+  size_t lengthInDoubleWords = length / k_uint32ByteLength;
 
   for (size_t i = 0; i < lengthInDoubleWords; i++) {
     // FIXME: Assumes little-endian byte order!
-    for (int j = uint32ByteLength - 1; j >= 0; j--) {
+    for (int j = k_uint32ByteLength - 1; j >= 0; j--) {
       // scan byte by byte to avoid alignment issue when building for emscripten
       // platform
-      crc = crc32EatByte(crc, data[i * uint32ByteLength + j]);
+      crc = crc32EatByte(crc, data[i * k_uint32ByteLength + j]);
     }
   }
-  for (size_t i = lengthInDoubleWords * uint32ByteLength; i < length; i++) {
+  for (size_t i = lengthInDoubleWords * k_uint32ByteLength; i < length; i++) {
     crc = crc32EatByte(crc, data[i]);
   }
   return crc;
@@ -28,7 +29,7 @@ uint32_t crc32Byte(const uint8_t *data, size_t length) {
 
 uint32_t crc32DoubleWord(const uint32_t *data, size_t length) {
   return crc32Byte(reinterpret_cast<const uint8_t *>(data),
-                   length * (sizeof(uint32_t) / sizeof(uint8_t)));
+                   length * k_uint32ByteLength);
 }
 
 }  // namespace Ion

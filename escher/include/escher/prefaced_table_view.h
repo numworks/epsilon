numworks/epsilon

commit 04f98096a944c3874abafce6ac04865834d7d6b8

[escher] Restore offset before selecting the cell during table reload
The offset is set to zero by the selectable to the selected cell will always be
reached from the left side if we don't preserve the offset.

escher/src/selectable_table_view.cpp
@@ -239,8 +239,10 @@ void SelectableTableView::deselectTable(bool withinTemporarySelection) {
 void SelectableTableView::reloadData(bool setFirstResponder) {
   int col = selectedColumn();
   int row = selectedRow();
+  KDPoint previousOffset = contentOffset();
   deselectTable(true);
   SelectableTableView::layoutSubviews();
+  setContentOffset(previousOffset);
   selectCellAtLocation(col, row, setFirstResponder, true);
 }
 

commit 31243189c96adaaaa16e7f136e0fb154f38fed6a

[escher] Fix preface table bug
Introduced by https://github.com/numworks/epsilon-internal/commit/04f98096a944c3874abafce6ac04865834d7d6b8
Indeed, offset is not always absolute in prefaced table; somethimes the
bounds and margins of the main table view are different, we need to take
it into account.

escher/Makefile
@@ -73,6 +73,7 @@ escher_src += $(addprefix escher/src/,\
   scrollable_multiple_layouts_view.cpp \
   scrollable_two_layouts_cell.cpp \
   selectable_table_view.cpp \
+  selectable_table_view_delegate.cpp \
   solid_color_view.cpp \
   stack_view.cpp \
   stack_view_controller.cpp \

escher/include/escher/prefaced_table_view.h
@@ -38,6 +38,9 @@ class PrefacedTableView : public View,
   void tableViewDidChangeSelectionAndDidScroll(
       SelectableTableView* t, int previousSelectedCol, int previousSelectedRow,
       KDPoint previousOffset, bool withinTemporarySelection = false) override;
+  KDPoint actualOffset(const SelectableTableView* t) const override {
+    return m_actualOffsetWithMargins;
+  }
 
   SelectableTableView* selectableTableView() { return m_mainTableView; }
   virtual void setMargins(KDMargins m);
@@ -166,6 +169,7 @@ class PrefacedTableView : public View,
   }
 
   SelectableTableViewDelegate* m_mainTableDelegate;
+  KDPoint m_actualOffsetWithMargins;
 };
 
 }  // namespace Escher

escher/include/escher/selectable_table_view.h
@@ -88,6 +88,9 @@ class SelectableTableView : public TableView, public Responder {
                                             false);
   }
   int firstOrLastSelectableColumnOrRow(bool first, bool searchForRow);
+  KDPoint actualOffset() const {
+    return m_delegate ? m_delegate->actualOffset(this) : contentOffset();
+  }
 };
 
 }  // namespace Escher

escher/include/escher/selectable_table_view_delegate.h
@@ -17,6 +17,7 @@ class SelectableTableViewDelegate : public ContextProvider {
   virtual void tableViewDidChangeSelectionAndDidScroll(
       SelectableTableView* t, int previousSelectedCol, int previousSelectedRow,
       KDPoint previousOffset, bool withinTemporarySelection = false) {}
+  virtual KDPoint actualOffset(const SelectableTableView* t) const;
 };
 
 }  // namespace Escher

escher/src/prefaced_table_view.cpp
@@ -14,7 +14,8 @@ PrefacedTableView::PrefacedTableView(
       m_marginDelegate(nullptr),
       m_prefacedDelegate(prefacedTableViewDelegate),
       m_mainTableViewTopMargin(0),
-      m_mainTableDelegate(delegate) {
+      m_mainTableDelegate(delegate),
+      m_actualOffsetWithMargins(KDPointZero) {
   m_mainTableView->setParentResponder(parentResponder);
   m_mainTableView->setDelegate(this);
   m_rowPrefaceView.hideScrollBars();
@@ -67,6 +68,14 @@ void PrefacedTableView::tableViewDidChangeSelectionAndDidScroll(
     resetContentOffset();
   }
   layoutSubviews();
+
+  // Update actual offset
+  m_actualOffsetWithMargins =
+      m_mainTableView->contentOffset()
+          .relativeTo(relativeChildOrigin(m_mainTableView))
+          .translatedBy(marginToAddForVirtualOffset());
+  assert(m_actualOffsetWithMargins.x() >= 0 &&
+         m_actualOffsetWithMargins.y() >= 0);
 }
 
 View* PrefacedTableView::subviewAtIndex(int index) {

escher/src/selectable_table_view.cpp
@@ -238,10 +238,10 @@ void SelectableTableView::deselectTable(bool withinTemporarySelection) {
 void SelectableTableView::reloadData(bool setFirstResponder) {
   int col = selectedColumn();
   int row = selectedRow();
-  KDPoint previousOffset = contentOffset();
+  KDPoint offset = actualOffset();
   deselectTable(true);
   layoutSubviews();
-  setContentOffset(previousOffset);
+  setContentOffset(offset);
   selectCellAtLocation(col, row, setFirstResponder, true);
 }
 

escher/src/selectable_table_view_delegate.cpp
@@ -1,0 +1,11 @@
+#include <escher/selectable_table_view.h>
+#include <escher/selectable_table_view_delegate.h>
+
+namespace Escher {
+
+KDPoint SelectableTableViewDelegate::actualOffset(
+    const SelectableTableView* t) const {
+  return t->contentOffset();
+}
+
+}  // namespace Escher
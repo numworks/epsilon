7dcc7a07e7597fbbb6bfd7bf410ae8cdae4f4a01
Félix Richart <felix.richart@numworks.com>
2023-10-06 15:32:26 +0200

    [ion/exam_bytes] Remove dependency between flash.ld and ExamBytes::Int

--- ion/include/ion/exam_bytes.h
+++ ion/include/ion/exam_bytes.h
@@ -6,8 +6,6 @@
 namespace Ion {
 namespace ExamBytes {
 
-/* Warning : All persisting bytes are expected to be initialized to 0 in
- * shared_kernel_flash.ld which assumes ExamBytes::Int is 16 bits long */
 typedef uint16_t Int;
 
 Int read();

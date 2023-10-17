cae1afe076ff218dfb658e08b5da88621e8b7f4f
Félix Richart <felix.richart@numworks.com>
2023-10-17 15:11:16 +0200

    [build/device] Add a py script to print the layout of the sections of a dfu file

---
+++ build/device/dfu_layout.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+import argparse
+import dfu
+import os.path
+
+def print_layout(file):
+  if os.path.splitext(file)[1] != ".dfu":
+    raise ValueError("This file is not a .dfu")
+  dfu.read_dfu_file(file)
+
+parser = argparse.ArgumentParser(description="Print the layout of the sections of a DFU file.")
+parser.add_argument('input', metavar='DFU_FILE', help='Input DfuSe')
+
+args = parser.parse_args()
+print_layout(args.input)

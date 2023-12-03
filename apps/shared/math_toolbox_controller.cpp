a04aba25b1f72141f281ef953b295458bfecb8ea
Arthur-GYT <arthur.gayot@orange.fr>
2023-12-03 17:11:54 +0100

    Add Calorie Unit

--- apps/shared/math_toolbox_controller.cpp
+++ apps/shared/math_toolbox_controller.cpp
@@ -378,6 +378,13 @@ constexpr ToolboxMessageTree forceAndPressureChildren[] = {
                              unitPressureChildren),
 };
 
+constexpr ToolboxMessageTree unitEnergyCalorieChildren[] = {
+    ToolboxMessageTree::Leaf(I18n::Message::UnitEnergyCalorieSymbol,
+                             I18n::Message::UnitEnergyCalorie),
+    ToolboxMessageTree::Leaf(I18n::Message::UnitEnergyCalorieKiloSymbol,
+                             I18n::Message::UnitEnergyCalorieKilo),
+};
+
 constexpr ToolboxMessageTree unitEnergyJouleChildren[] = {
     ToolboxMessageTree::Leaf(I18n::Message::UnitEnergyJouleMilliSymbol,
                              I18n::Message::UnitEnergyJouleMilli),
@@ -399,7 +406,9 @@ constexpr ToolboxMessageTree unitEnergyElectronVoltChildren[] = {
 };
 
 constexpr ToolboxMessageTree unitEnergyChildren[] = {
-    ToolboxMessageTree::Node(I18n::Message::UnitEnergyJoule,
+    ToolboxMessageTree::Node(I18n::Message::UnitEnergyCalorieMenu,
+                             unitEnergyCalorieChildren),
+    ToolboxMessageTree::Node(I18n::Message::UnitEnergyJouleMenu,
                              unitEnergyJouleChildren),
     ToolboxMessageTree::Node(I18n::Message::UnitEnergyElectronVolt,
                              unitEnergyElectronVoltChildren),

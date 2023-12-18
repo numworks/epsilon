numworks/epsilon

commit 8483aff1e3a257c7bdaa0bc6f5b10e1870806d80

[poincare/range] Avoid as much as possible to use Range1D::setMin and Range1D::setMax. Prefer using Range1D::ValidRangeBetween
Calling setMin and then setMax isn't symetrical

Lets say we want to create the range [2E-4, 2.6E-4]
This range is not valid because it's length is below minLength = 1E-4

If we use setMin and then setMax:
- We first create a NAN range [NAN,NAN]
- Then we set the min to 2E-4 which turns the range into [2E-4, 3E-4].
- Then we set the max to 2.6E-4 which turns the range into [1.6E-4, 2.6E-4]
- The result given is [1.6E-4, 2.6E-4] which is not centered on the same
  value as [2E-4, 2.6E-4]

If we use ValidRangeBetween
- We first create a range [2E-4, 2.6E-4]
- It's then stretched by the same amount on both sides which gives as a
  result [1.8E-4, 2.8E-4].
> This is a symetrical result and do not make the range depend on which
method is called first between setMin and setMax

apps/shared/interactive_curve_view_range.cpp
@@ -52,28 +52,16 @@ float InteractiveCurveViewRange::roundLimit(float y, float range, bool isMin) {
   return magnitude * std::ceil(y / magnitude);
 }
 
-void InteractiveCurveViewRange::setXMin(float xMin) {
+void InteractiveCurveViewRange::setXRange(float min, float max) {
   assert(!xAuto() || m_delegate == nullptr);
-  MemoizedCurveViewRange::protectedSetXMin(xMin, true, k_maxFloat);
+  MemoizedCurveViewRange::protectedSetXRange(min, max, k_maxFloat);
   computeRanges();
 }
 
-void InteractiveCurveViewRange::setXMax(float xMax) {
-  assert(!xAuto() || m_delegate == nullptr);
-  MemoizedCurveViewRange::protectedSetXMax(xMax, true, k_maxFloat);
+void InteractiveCurveViewRange::setYRange(float min, float max) {
+  assert(!yAuto() || m_delegate == nullptr);
+  MemoizedCurveViewRange::protectedSetYRange(min, max, k_maxFloat);
   computeRanges();
-}
-
-void InteractiveCurveViewRange::setYMin(float yMin) {
-  assert(!yAuto() || m_delegate == nullptr);
-  MemoizedCurveViewRange::protectedSetYMin(yMin, true, k_maxFloat);
-  setZoomNormalize(isOrthonormal());
-}
-
-void InteractiveCurveViewRange::setYMax(float yMax) {
-  assert(!yAuto() || m_delegate == nullptr);
-  MemoizedCurveViewRange::protectedSetYMax(yMax, true, k_maxFloat);
-  setZoomNormalize(isOrthonormal());
 }
 
 void InteractiveCurveViewRange::setOffscreenYAxis(float f) {
@@ -82,7 +70,7 @@ void InteractiveCurveViewRange::setOffscreenYAxis(float f) {
   }
   float d = m_offscreenYAxis - f;
   m_offscreenYAxis = f;
-  MemoizedCurveViewRange::protectedSetYMax(yMax() + d, true, k_maxFloat);
+  MemoizedCurveViewRange::protectedSetYRange(yMin(), yMax() + d, k_maxFloat);
 }
 
 float InteractiveCurveViewRange::xGridUnit() const {
@@ -124,8 +112,8 @@ void InteractiveCurveViewRange::zoom(float ratio, float x, float y) {
   thisRange.zoom(ratio, center);
   assert(thisRange.x()->isValid() && thisRange.y()->isValid());
   setZoomAuto(false);
-  protectedSetX(*thisRange.x(), k_maxFloat);
-  protectedSetY(*thisRange.y(), k_maxFloat);
+  protectedSetXRange(*thisRange.x(), k_maxFloat);
+  protectedSetYRange(*thisRange.y(), k_maxFloat);
   /* The factor will typically be equal to ratio, unless yMax and yMin are
    * close to the maximal values. */
   float yRatio = memoizedRange().y()->length() / dy;
@@ -144,8 +132,7 @@ void InteractiveCurveViewRange::panWithVector(float x, float y) {
   if (x != 0.f) {
     x = OMG::WithGreatestAbs(vectorLengthForMove(x, xMin()),
                              vectorLengthForMove(x, xMax()));
-    xRange.setMin(xMin() + x, k_maxFloat);
-    xRange.setMax(xMax() + x, k_maxFloat);
+    xRange = Range1D::ValidRangeBetween(xMin() + x, xMax() + x, k_maxFloat);
     if (xRange.min() != xMin() + x || xRange.max() != xMax() + x) {
       return;
     }
@@ -155,8 +142,7 @@ void InteractiveCurveViewRange::panWithVector(float x, float y) {
   if (y != 0.f) {
     y = OMG::WithGreatestAbs(vectorLengthForMove(y, yMin()),
                              vectorLengthForMove(y, yMax()));
-    yRange.setMin(yMin() + y, k_maxFloat);
-    yRange.setMax(yMax() + y, k_maxFloat);
+    yRange = Range1D::ValidRangeBetween(yMin() + y, yMax() + y, k_maxFloat);
     if (yRange.min() != yMin() + y || yRange.max() != yMax() + y) {
       return;
     }
@@ -164,8 +150,8 @@ void InteractiveCurveViewRange::panWithVector(float x, float y) {
 
   if (x != 0.f || y != 0.f) {
     setZoomAuto(false);
-    protectedSetX(xRange, k_maxFloat);
-    protectedSetY(yRange, k_maxFloat);
+    protectedSetXRange(xRange, k_maxFloat);
+    protectedSetYRange(yRange, k_maxFloat);
   }
 }
 
@@ -190,7 +176,7 @@ void InteractiveCurveViewRange::centerAxisAround(Axis axis, float position) {
     float newXMax = position + range / 2.0f;
     if (xMax() != newXMax) {
       setZoomAuto(false);
-      protectedSetX(Range1D(newXMax - range, newXMax, k_maxFloat), k_maxFloat);
+      protectedSetXRange(newXMax - range, newXMax, k_maxFloat);
     }
   } else {
     float range = yMax() - yMin();
@@ -200,9 +186,8 @@ void InteractiveCurveViewRange::centerAxisAround(Axis axis, float position) {
     float newYMax = position + range / 2.0f;
     if (yMax() != newYMax) {
       setZoomAuto(false);
-      protectedSetY(
-          Range1D(position - 0.5f * range, position + 0.5f * range, k_maxFloat),
-          k_maxFloat);
+      protectedSetYRange(position - 0.5f * range, position + 0.5f * range,
+                         k_maxFloat);
     }
   }
 
@@ -226,7 +211,7 @@ bool InteractiveCurveViewRange::panToMakePointVisible(
           -k_maxFloat,
           std::floor((x - leftMargin - xMin()) / pixelWidth) * pixelWidth +
               xMin());
-      protectedSetX(Range1D(newXMin, newXMin + xRange, k_maxFloat), k_maxFloat);
+      protectedSetXRange(newXMin, newXMin + xRange, k_maxFloat);
     }
     const float rightMargin = rightMarginRatio * xRange;
     if (x > xMax() - rightMargin && xMax() < k_maxFloat) {
@@ -235,7 +220,7 @@ bool InteractiveCurveViewRange::panToMakePointVisible(
           k_maxFloat,
           std::ceil((x + rightMargin - xMax()) / pixelWidth) * pixelWidth +
               xMax());
-      protectedSetX(Range1D(newXMax - xRange, newXMax, k_maxFloat), k_maxFloat);
+      protectedSetXRange(newXMax - xRange, newXMax, k_maxFloat);
     }
   }
   if (std::isfinite(y)) {
@@ -244,13 +229,13 @@ bool InteractiveCurveViewRange::panToMakePointVisible(
     if (y < yMin() + bottomMargin && yMin() > -k_maxFloat) {
       moved = true;
       const float newYMin = std::max(-k_maxFloat, y - bottomMargin);
-      protectedSetY(Range1D(newYMin, newYMin + yRange, k_maxFloat), k_maxFloat);
+      protectedSetYRange(newYMin, newYMin + yRange, k_maxFloat);
     }
     const float topMargin = topMarginRatio * yRange;
     if (y > yMax() - topMargin && yMax() < k_maxFloat) {
       moved = true;
       const float newYMax = std::min(k_maxFloat, y + topMargin);
-      protectedSetY(Range1D(newYMax - yRange, newYMax, k_maxFloat), k_maxFloat);
+      protectedSetYRange(newYMax - yRange, newYMax, k_maxFloat);
     }
   }
 
@@ -276,8 +261,8 @@ bool InteractiveCurveViewRange::zoomOutToMakePointVisible(
   Range2D newRange = zoom.range(false, false);
 
   bool move = newRange != memoizedRange();
-  protectedSetX(*newRange.x(), k_maxFloat);
-  protectedSetY(*newRange.y(), k_maxFloat);
+  protectedSetXRange(*newRange.x(), k_maxFloat);
+  protectedSetYRange(*newRange.y(), k_maxFloat);
 
   if (move) {
     setZoomAuto(false);
@@ -312,8 +297,8 @@ void InteractiveCurveViewRange::protectedNormalize(bool canChangeX,
   if (!shrink || canShrink) {
     bool canSetRatio = thisRange.setRatio(NormalYXRatio(), shrink, k_maxFloat);
     if (canSetRatio) {
-      protectedSetX(*thisRange.x(), k_maxFloat);
-      protectedSetY(*thisRange.y(), k_maxFloat);
+      protectedSetXRange(*thisRange.x(), k_maxFloat);
+      protectedSetYRange(*thisRange.y(), k_maxFloat);
       assert(isOrthonormal());
     }
   }
@@ -365,10 +350,10 @@ void InteractiveCurveViewRange::privateComputeRanges(bool computeX,
     }
 
     if (computeX) {
-      protectedSetX(*newRange.x(), k_maxFloat);
+      protectedSetXRange(*newRange.x(), k_maxFloat);
     }
     if (computeY) {
-      protectedSetY(*newRange.y(), k_maxFloat);
+      protectedSetYRange(*newRange.y(), k_maxFloat);
     }
 
     /* We notify the delegate to refresh the cursor's position, which will
@@ -389,10 +374,10 @@ void InteractiveCurveViewRange::privateComputeRanges(bool computeX,
     }
 
     if (computeX) {
-      protectedSetX(*newRangeWithMargins.x(), k_maxFloat);
+      protectedSetXRange(*newRangeWithMargins.x(), k_maxFloat);
     }
     if (computeY) {
-      protectedSetY(*newRangeWithMargins.y(), k_maxFloat);
+      protectedSetYRange(*newRangeWithMargins.y(), k_maxFloat);
     }
   }
 

apps/calculation/additional_results/complex_model.cpp
@@ -23,7 +23,8 @@ float ComplexModel::rangeBound(float direction, bool horizontal) const {
   if (std::isnan(value) || std::isinf(value) || value == 0.0f) {
     return direction * maxFactor;
   }
-  Range1D range1D = Range1D::RangeBetween(minFactor * value, maxFactor * value);
+  Range1D range1D =
+      Range1D::ValidRangeBetween(minFactor * value, maxFactor * value);
   return direction >= 0.0f ? range1D.max() : range1D.min();
 }
 

apps/calculation/additional_results/function_model.cpp
@@ -58,14 +58,14 @@ void FunctionModel::recomputeViewRange() {
   float widthPixelRatio = range.x()->length() / k_width;
   float heigthPixelRatio = range.y()->length() / k_height;
 
-  setXMin(range.xMin() -
-          RangeMargin(false, range.xMin(), m_abscissa, widthPixelRatio));
-  setXMax(range.xMax() +
-          RangeMargin(true, range.xMax(), m_abscissa, widthPixelRatio));
-  setYMin(range.yMin() -
-          RangeMargin(false, range.yMin(), m_ordinate, heigthPixelRatio));
-  setYMax(range.yMax() +
-          RangeMargin(true, range.yMax(), m_ordinate, heigthPixelRatio));
+  setXRange(range.xMin() -
+                RangeMargin(false, range.xMin(), m_abscissa, widthPixelRatio),
+            range.xMax() +
+                RangeMargin(true, range.xMax(), m_abscissa, widthPixelRatio));
+  setYRange(range.yMin() -
+                RangeMargin(false, range.yMin(), m_ordinate, heigthPixelRatio),
+            range.yMax() +
+                RangeMargin(true, range.yMax(), m_ordinate, heigthPixelRatio));
 }
 
 }  // namespace Calculation

apps/calculation/additional_results/vector_model.cpp
@@ -41,10 +41,9 @@ void VectorModel::recomputeViewRange() {
   float yMargin = k_marginInPixels * ratio +
                   (verticallyCapped ? 0.f : (width / k_xyRatio - height) / 2.f);
 
-  setXMin(std::min(-arcLeftMargin, x) - xMargin);
-  setXMax(std::max(arcRightMargin, x) + xMargin);
-  setYMin(std::min(0.f, y) - yMargin);
-  setYMax(std::max(arcTopMargin, y) + yMargin);
+  setXRange(std::min(-arcLeftMargin, x) - xMargin,
+            std::max(arcRightMargin, x) + xMargin);
+  setYRange(std::min(0.f, y) - yMargin, std::max(arcTopMargin, y) + yMargin);
 }
 
 }  // namespace Calculation

apps/graph/shared/domain_parameter_controller.cpp
@@ -77,12 +77,10 @@ bool DomainParameterController::parametersAreDifferent() {
 }
 
 void DomainParameterController::extractParameters() {
-  setParameterAtIndex(1, function()->tMin());
-  setParameterAtIndex(2, function()->tMax());
+  setRange(function()->tMin(), function()->tMax());
   m_autoParam = function()->tAuto();
-  /* Setting m_rangeParam tMin might affect m_rangeParam.max(), but setting tMax
-   * right after will not affect m_rangeParam.min() because Function's Range1D
-   * parameters are valid (tMax>tMin), and final tMin value is already set.
+  /* Setting m_rangeParam should affect tMin and tMax because Function's Range1D
+   * parameters are valid (tMax>tMin).
    * Same happens in confirmParameters when setting function's parameters from
    * valid m_rangeParam parameters. */
   assert(!parametersAreDifferent());
@@ -90,8 +88,7 @@ void DomainParameterController::extractParameters() {
 
 void DomainParameterController::setAutoRange() {
   assert(m_autoParam);
-  setParameterAtIndex(1, function()->autoTMin());
-  setParameterAtIndex(2, function()->autoTMax());
+  setRange(function()->autoTMin(), function()->autoTMax());
 }
 
 void DomainParameterController::confirmParameters() {

apps/graph/test/caching.cpp
@@ -109,10 +109,8 @@ void assert_cache_stays_valid(const char* definition, float rangeXMin = -5,
   functionStore.setCachesContainer(&cachesContainer);
 
   InteractiveCurveViewRange graphRange;
-  graphRange.setXMin(rangeXMin);
-  graphRange.setXMax(rangeXMax);
-  graphRange.setYMin(-3.f);
-  graphRange.setYMax(3.f);
+  graphRange.setXRange(rangeXMin, rangeXMax);
+  graphRange.setYRange(-3.f, 3.f);
 
   CurveViewCursor cursor;
   ContinuousFunction* function =

apps/inference/models/statistic/test.cpp
@@ -177,8 +177,8 @@ bool Test::computeCurveViewRange(float transition, bool zoomSide) {
     xMax = FLT_MAX;
   }
   assert(std::isfinite(yMin) && std::isfinite(yMax));
-  protectedSetX(Poincare::Range1D(xMin, xMax));
-  protectedSetY(Poincare::Range1D(yMin, yMax));
+  protectedSetXRange(xMin, xMax);
+  protectedSetYRange(yMin, yMax);
   return true;
 }
 

apps/regression/graph/residual_plot_range.cpp
@@ -33,10 +33,9 @@ void ResidualPlotRange::calibrateXorY(bool isX, double min, double max,
         static_cast<double>(bannerHeight) / static_cast<double>(height);
     double rangeMin =
         (range.min() - range.max() * heightRatio) / (1 - heightRatio);
-    range.setMin(rangeMin);
-    protectedSetY(range);
+    protectedSetYRange(rangeMin, range.max());
   } else {
-    protectedSetX(range);
+    protectedSetXRange(range);
   }
 }
 

commit 302ed89adc24f52fb0c5c32e1798870b41e3b325

[shared/interactive_curve_view_range] We used to check only if it was not NaN
Crash introduced in 8483aff

apps/shared/interactive_curve_view_range.cpp
@@ -110,7 +110,7 @@ void InteractiveCurveViewRange::zoom(float ratio, float x, float y) {
   Coordinate2D<float> center(std::isfinite(x) ? x : thisRange.x()->center(),
                              std::isfinite(y) ? y : thisRange.y()->center());
   thisRange.zoom(ratio, center);
-  assert(thisRange.x()->isValid() && thisRange.y()->isValid());
+  assert(!thisRange.x()->isNan() && !thisRange.y()->isNan());
   setZoomAuto(false);
   protectedSetXRange(*thisRange.x(), k_maxFloat);
   protectedSetYRange(*thisRange.y(), k_maxFloat);
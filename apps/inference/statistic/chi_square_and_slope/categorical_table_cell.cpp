a87ad826b63a1827c0826fe8b32c9673ef5119fe
Pia Chancerel <pia.chancerel@numworks.com>
2023-12-22 15:47:00 +0100

    [inference/chi_square_and_slope] Fix reload when clearing column

--- apps/inference/statistic/chi_square_and_slope/categorical_table_cell.cpp
+++ apps/inference/statistic/chi_square_and_slope/categorical_table_cell.cpp
@@ -184,6 +184,7 @@ void InputCategoricalTableCell::clearSelectedColumn() {
   tableModel()->recomputeData();
   m_selectableTableView.deselectTable();
   m_selectableTableView.resetScroll();
+  categoricalController()->selectableListView()->reloadData(false);
   recomputeDimensionsAndReload(true, true);
   m_selectableTableView.selectCellAtClippedLocation(column, 1, false);
 }

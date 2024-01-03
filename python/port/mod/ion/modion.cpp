1b47415abef773915046d38865b6a7e5a4c2c56f
Félix Richart <felix.richart@numworks.com>
2024-01-03 10:35:21 +0100

    [python] Slow down keydown

--- python/port/mod/ion/modion.cpp
+++ python/port/mod/ion/modion.cpp
@@ -14,7 +14,13 @@ mp_obj_t modion_keyboard_keydown(mp_obj_t key_o) {
   if (key >= Ion::Keyboard::Key::None) {
     return mp_obj_new_bool(false);
   }
+  /* In version 22, scan was greatly sped up. We wait 1 ms after the scan to
+   * keep the keydown function consistent with older versions.
+   * WARNING: Do not use Timing::msleep to avoid putting the device to sleep,
+   * which alters timings (mostly on N120) */
+  uint64_t currentTime = Ion::Timing::millis();
   Ion::Keyboard::State state = Ion::Keyboard::scan();
+  while (currentTime + 1 > Ion::Timing::millis()) {}
   micropython_port_interrupt_if_needed();
   return mp_obj_new_bool(state.keyDown(key));
 }

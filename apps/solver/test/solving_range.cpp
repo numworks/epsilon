e79fd4c4a77c83aea3a6fd2827c14be8661dfb8d
Pia Chancerel <pia.chancerel@numworks.com>
2024-03-25 13:41:05 +0100

    [apps/solver/test] Use assert_solves_with_auto_solving_range instead of assert_auto_solving_range_is

--- apps/solver/test/solving_range.cpp
+++ apps/solver/test/solving_range.cpp
@@ -5,8 +5,13 @@
 QUIZ_CASE(solver_solving_range) {
   setComplexFormatAndAngleUnit(Cartesian, Radian);
   assert_auto_solving_range_is("cos(x)=0", -15.5654296875, 15.5654296875);
-  assert_auto_solving_range_is("ln(x)=11", 54120.2578125, 66269.203125);
-  assert_auto_solving_range_is("(x+24543)^2+cos(x)-0.5^x=123456789123457890",
-                               318647616, 390680384);
-  assert_auto_solving_range_is("x^(1/3)=23456", 11642386513920, 14351806234624);
+  assert_solves_with_auto_solving_range(
+      "cos(x)=0", {-14.137166941154073, -10.995574287564276,
+                   -7.8539816324381064, -4.7123889787100008,
+                   -1.5707963267886629, 1.5707963267957366, 4.7123889818623867,
+                   7.8539816549519345, 10.995574295553068, 14.137166941154067});
+  assert_solves_with_auto_solving_range("ln(x)=11", {59874.141715133868});
+  assert_solves_with_auto_solving_range(
+      "(x+24543)^2+cos(x)-0.5^x=123456789123457890", {351339639.64352596});
+  assert_solves_with_auto_solving_range("x^(1/3)=23456", {12905114402814.777});
 }

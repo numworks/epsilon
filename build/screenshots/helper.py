4a46b74bbaf8a11a1c22c3f046f5fb32d7a09809
MarcNumworks <marc.coudriau@numworks.com>
2024-04-02 14:28:27 +0200

    Revert "[build/screenshot] Add --skip-idle flag to take_for_each_step.py"
    This reverts commit 46dc4f3a846f38211cdca6b75506b89eb7230cb6.

--- build/screenshots/helper.py
+++ build/screenshots/helper.py
@@ -46,9 +46,7 @@ def print_error(stderr, exit=True):
             sys.exit(1)
 
 
-def generate_all_screenshots(
-    state_file, executable, folder, skip_idle, exit_if_error=True
-):
+def generate_all_screenshots(state_file, executable, folder, exit_if_error=True):
     print("Generating all screenshots of", state_file)
     clean_or_create_folder(folder)
     p = Popen(
@@ -56,7 +54,6 @@ def generate_all_screenshots(
         + executable
         + " --headless --load-state-file "
         + state_file
-        + (" --skip-idle-screenshots " if skip_idle else "")
         + " --take-all-screenshots "
         + folder,
         shell=True,
@@ -99,11 +96,11 @@ def folder_images(folder):
 
 
 def generate_all_screenshots_and_create_gif(
-    state_file, executable, folder, skip_idle=False, exit_if_error=True
+    state_file, executable, folder, exit_if_error=True
 ):
     clean_or_create_folder(folder)
     list_images = generate_all_screenshots(
-        state_file, executable, folder_images(folder), skip_idle, exit_if_error
+        state_file, executable, folder_images(folder), exit_if_error
     )
     create_gif(list_images, folder)
     return list_images

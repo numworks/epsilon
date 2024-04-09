211fceb8da9e2d7e1c49a7a6903198eebf78b72b
Gabriel Ozouf <gabriel.ozouf@numworks.com>
2024-04-09 11:18:06 +0200

    [omg/code_guard] Make CODE_GUARD consistent across builds

--- omg/include/omg/code_guard.h
+++ omg/include/omg/code_guard.h
@@ -5,16 +5,17 @@
 
 namespace OMG {
 
-#define CODE_GUARD_STRINGIFY(...) #__VA_ARGS__
-
 /* Place a CODE_GUARD macro around code that shouldn't be changed lightly. The
  * program will refuse to compile if the guarded code is changed without also
- * updating the CRC, making it more obvious than a simple comment. */
-#define CODE_GUARD(id, reference_crc, ...)                                  \
-  static_assert(                                                            \
-      Ion::crc32String(CODE_GUARD_STRINGIFY(__VA_ARGS__)) == reference_crc, \
-      CODE_GUARD_STRINGIFY(                                                 \
-          id) ": modifying this code might break an external API!");        \
+ * updating the CRC, making it more obvious than a simple comment.
+ *
+ * Note: Purposefully use #__VA_ARGS__ instead of a classic STRINGIFY macro, to
+ * avoid expanding macros in the computation of the CRC. Indeed, some macros
+ * definitions depend on the compilation target or flags (e.g. assert) and would
+ * lead to inconsistent CRC across builds. */
+#define CODE_GUARD(id, reference_crc, ...)                                 \
+  static_assert(Ion::crc32String(#__VA_ARGS__) == reference_crc,           \
+                #id ": modifying this code might break an external API!"); \
   __VA_ARGS__
 
 }  // namespace OMG

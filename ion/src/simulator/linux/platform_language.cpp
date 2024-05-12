ac71576accd4fef74d74e417c3ff94507d78be79
Leviathan3DPrinting <18338698+Leviathan3DPrinting@users.noreply.github.com>
2024-05-12 06:35:12 -0500

    [Ion] Prevent crash if locale is null (#365)
    * fix a compilation issue
    
    * revert file
    
    ---------
    
    Co-authored-by: Leviathan3DPrinting <leviathan3dprinting@gmail.com>

--- ion/src/simulator/linux/platform_language.cpp
+++ ion/src/simulator/linux/platform_language.cpp
@@ -8,7 +8,7 @@ namespace Platform {
 const char * languageCode() {
   static char buffer[3] = {0};
   char * locale = setlocale(LC_ALL, "");
-  if (locale[2] == '_') {
+  if (locale && locale[2] == '_') { // Check if locale is not null before accessing its elements
     buffer[0] = locale[0];
     buffer[1] = locale[1];
     return buffer;

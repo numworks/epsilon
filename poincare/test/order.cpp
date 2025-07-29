28d3844a02cb185d8e60ef64036d9c2b0f028bd8
PaulineNumWorks <pauline.garelli@numworks.com>
2025-07-29 18:03:33 +0200

    test(poincare): add default argument to clarify order tests

--- poincare/test/order.cpp
+++ poincare/test/order.cpp
@@ -7,13 +7,13 @@
 using namespace Poincare::Internal;
 using enum Order::OrderType;
 
-void assert_order_is(const Tree* e1, const Tree* e2, int result,
+void assert_order_is(const Tree* e1, const Tree* e2, int result = -1,
                      Order::OrderType order = System) {
   int comparison = Order::Compare(e1, e2, order);
   quiz_assert(comparison == result);
 }
 
-void assert_order_is(const char* t1, const char* t2, int result,
+void assert_order_is(const char* t1, const char* t2, int result = -1,
                      Order::OrderType order = System) {
   Tree* e1 = parse(t1);
   Tree* e2 = parse(t2);
@@ -24,64 +24,65 @@ void assert_order_is(const char* t1, const char* t2, int result,
 
 QUIZ_CASE(pcj_order) {
   // Numbers
-  assert_order_is(10.0_fe, 2.0_fe, 1);
-  assert_order_is(1.0_fe, 2.0_fe, -1);
-  assert_order_is(0.234_de, -0.2392_de, 1);
-  assert_order_is(0.234_fe, 0.123_fe, 1);
-  assert_order_is(234_de, 123_de, 1);
-  assert_order_is(2_e, 3_e, -1);
-  assert_order_is(9_e / 10_e, -9_e / 10_e, 1);
-  assert_order_is(3_e / 4_e, 2_e / 3_e, 1);
-  assert_order_is(KOpposite(KDecimal(1_e, 3_e)), KOpposite(KDecimal(1_e, 2_e)),
-                  1);
-  assert_order_is(KDecimal(123_e, 3_e), KDecimal(12_e, 3_e), 1);
-  assert_order_is(KDecimal(1_e, 3_e), KOpposite(KDecimal(1_e, 3_e)),
-                  -1);                                          // Should be 1?
-  assert_order_is(KDecimal(1_e, 3_e), KDecimal(1_e, 4_e), -1);  // Should be 1?
+  assert_order_is(2.0_fe, 10.0_fe);
+  assert_order_is(1.0_fe, 2.0_fe);
+  assert_order_is(0.123_fe, 0.234_fe);
+  assert_order_is(-0.2392_de, 0.234_de);
+  assert_order_is(123_de, 234_de);
+  assert_order_is(1_e, 1_e, 0);
+  assert_order_is(2_e, 3_e);
+  assert_order_is(-9_e / 10_e, 9_e / 10_e);
+  assert_order_is(2_e / 3_e, 3_e / 4_e);
+  assert_order_is(KOpposite(KDecimal(1_e, 2_e)), KOpposite(KDecimal(1_e, 3_e)));
+  assert_order_is(KDecimal(12_e, 3_e), KDecimal(123_e, 3_e));
+  assert_order_is(KDecimal(1_e, 3_e),
+                  KOpposite(KDecimal(1_e, 3_e)));  // Should be reversed?
+  assert_order_is(KDecimal(1_e, 3_e),
+                  KDecimal(1_e, 4_e));  // Should be reversed?
 
   // Powers
-  assert_order_is(KPow(1_e / 2_e, 5_e), KPow(2_e, 3_e), -1);
-  assert_order_is("2^2", "2^3", -1);
-  assert_order_is("1", "2^3", -1);
-  assert_order_is("2", "2^3", -1);
+  assert_order_is(KPow(1_e / 2_e, 5_e), KPow(2_e, 3_e));
+  assert_order_is("2^2", "2^3");
+  assert_order_is("1", "2^3");
+  assert_order_is("2", "2^3");
 
   // Constants
-  assert_order_is(π_e, e_e, -1);
-  assert_order_is(π_e, i_e, -1);
-  assert_order_is(e_e, i_e, -1);
-  assert_order_is("e", "_c", -1);
-  assert_order_is("pi", "G", -1);
-  assert_order_is(KAdd(2_e, π_e), π_e, 1);
+  assert_order_is(π_e, e_e);
+  assert_order_is(π_e, i_e);
+  assert_order_is(e_e, i_e);
+  assert_order_is("e", "_c");
+  assert_order_is("pi", "G");
+  assert_order_is(π_e, KAdd(2_e, π_e));
 
   // Variables
   assert_order_is("a"_e, "a"_e, 0);
-  assert_order_is("a"_e, "b"_e, -1);
+  assert_order_is("a"_e, "b"_e);
   assert_order_is("b"_e, "a"_e, 1);
-  assert_order_is(KAdd(3_e, "a"_e), KAdd(2_e, "a"_e), 1);
-  assert_order_is("x"_e, KPow("x"_e, -1_e), 1);
-  assert_order_is("y", "x", 1);
-  assert_order_is("x^2", "x", 1);
-  assert_order_is("x", "w^2", 1);
-  assert_order_is("y^2", "x", 1);
-  assert_order_is("1+y", "x", 1);
-  assert_order_is("z", "1+y", 1);
-  assert_order_is("x*z", "y", 1);
+  assert_order_is(KAdd(2_e, "a"_e), KAdd(3_e, "a"_e));
+  assert_order_is(KPow("x"_e, -1_e), "x"_e);
+  assert_order_is("x", "y");
+  assert_order_is("x", "x^2");
+  assert_order_is("w^2", "x");
+  assert_order_is("x", "y^2");
+  assert_order_is("x", "1+y");
+  assert_order_is("1+y", "z");
+  assert_order_is("y", "x*z");
 
   // Units
   assert_order_is("_yd", "_in", -1, Order::OrderType::Beautification);
   assert_order_is("_yd", "_au", 1, Order::OrderType::Beautification);
 
   // Functions
-  assert_order_is(KFun<"g">(1_e), KFun<"f">(9_e), 1);
-  assert_order_is("b(y)", "a(x)", 1);
-  assert_order_is("a(2)", "a(1)", 1);
-  assert_order_is("gcd(0, 2, 3)", "gcd(0, 1, 4)", 1);
-  assert_order_is("1 + 4", "2 + 3", 1);
-  assert_order_is(KTrig("x"_e, 4_e), KTrig(5_e, 0_e), 1);
+  assert_order_is(KFun<"f">(9_e), KFun<"g">(1_e));
+  assert_order_is("a(x)", "b(y)");
+  assert_order_is("a(1)", "a(2)");
+  assert_order_is("gcd(0, 1, 4)", "gcd(0, 2, 3)");
+  assert_order_is("2 + 3", "1 + 4");
+  assert_order_is(KTrig(5_e, 0_e), KTrig("x"_e, 4_e));
 
   // Others
-  assert_order_is(KInf, "x"_e, 1);
-  assert_order_is("x"_e, 2_e, 1);
+  assert_order_is("x"_e, KInf);
+  assert_order_is(2_e, "x"_e);
 }
 
 void assert_sorts_to(const char* input, const char* output,

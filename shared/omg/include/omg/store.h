91bf4d80f55bb2ec6ad74f66ee89218b3de4eaff
MarcNumworks <marc.coudriau@numworks.com>
2025-07-29 11:45:38 +0200

    feat(omg): copy calculation store

---
+++ shared/omg/include/omg/store.h
@@ -0,0 +1,150 @@
+#ifndef CALCULATION_CALCULATION_STORE_H
+#define CALCULATION_CALCULATION_STORE_H
+
+#include <apps/constant.h>
+#include <apps/math_preferences.h>
+#include <omg/expiring_pointer.h>
+#include <poincare/old/pool_variable_context.h>
+#include <stddef.h>
+
+#include "calculation.h"
+
+namespace Calculation {
+
+struct OutputExpressions {
+  Poincare::UserExpression exact;
+  Poincare::UserExpression approximate;
+};
+
+// clang-format off
+/*
+  To optimize the storage space, we use one big buffer for all calculations.
+  The calculations are stored one after another while pointers to the end of each
+  calculation are stored at the end of the buffer, in the opposite direction.
+  By doing so, we can memoize every calculation entered while not limiting
+  the number of calculation stored in the buffer.
+
+  If the remaining space is too small for storing a new calculation, we
+  delete the oldest one.
+
+ Memory layout :
+                                                                <- Available space for new calculations ->
++--------------------------------------------------------------------------------------------------------------------+
+|               |               |               |               |                                        |  |  |  |  |
+| Calculation 3 | Calculation 2 | Calculation 1 | Calculation O |                                        |p0|p1|p2|p3|
+|     Oldest    |               |               |               |                                        |  |  |  |  |
++--------------------------------------------------------------------------------------------------------------------+
+^               ^               ^               ^               ^                                        ^
+m_buffer        p3              p2              p1              p0                                       a
+
+a = pointerArea()
+
+Each calculation in the buffer is composed of a Calculation instance directly
+followed by some Poincare expressions, one for each element of the calculation
+(in order: input, exact output, approximate output). See the m_trees member
+variable of Calculation for more details on this memory layout.
+*/
+// clang-format on
+
+class CalculationStore {
+ public:
+  CalculationStore(char* buffer, size_t bufferSize);
+
+  /* A Calculation does not count toward the number while it is being built and
+   * filled. */
+  int numberOfCalculations() const { return m_numberOfCalculations; }
+  OMG::ExpiringPointer<Calculation> calculationAtIndex(int index) const;
+  Poincare::UserExpression ansExpression(Poincare::Context* context) const;
+
+  void replaceAnsInExpression(Poincare::UserExpression& expression,
+                              Poincare::Context* context) const;
+  size_t bufferSize() const { return m_bufferSize; }
+  size_t remainingBufferSize() const {
+    return spaceForNewCalculations(endOfCalculations()) + sizeof(Calculation*);
+  }
+
+  OMG::ExpiringPointer<Calculation> push(Poincare::Layout input,
+                                         Poincare::Context* context);
+  void deleteCalculationAtIndex(int index);
+  void deleteAll() { m_numberOfCalculations = 0; }
+  bool preferencesHaveChanged();
+
+  Poincare::PoolVariableContext createAnsContext(Poincare::Context* context);
+
+ private:
+  constexpr static size_t k_pushErrorSize = 0;
+
+  constexpr static size_t neededSizeForCalculation(size_t sizeOfExpressions) {
+    /* See the "memory layout" section in the description of the
+     * CalculationStore class for more details on how calculations are stored.
+     */
+    return sizeof(Calculation) + sizeOfExpressions + sizeof(Calculation*);
+  }
+
+  char* pointerArea() const {
+    return m_buffer + m_bufferSize -
+           m_numberOfCalculations * sizeof(Calculation*);
+  }
+  char** pointerArray() const {
+    return reinterpret_cast<char**>(pointerArea());
+  }
+  char* endOfCalculations() const {
+    return numberOfCalculations() == 0 ? m_buffer : endOfCalculationAtIndex(0);
+  }
+  char* endOfCalculationAtIndex(int index) const;
+  /* Account for the size of an additional pointer at the end of the buffer. */
+  size_t spaceForNewCalculations(const char* currentEndOfCalculations) const;
+
+  void deleteOldestCalculation() {
+    assert(numberOfCalculations() > 0);
+    deleteCalculationAtIndex(numberOfCalculations() - 1);
+  }
+
+  /* Make space for calculation by clearing some older calculations if needed.
+   * neededSize must be smaller than m_bufferSize */
+  void getEmptySpace(size_t neededSize);
+
+  struct CalculationElements {
+    Poincare::UserExpression input;
+    OutputExpressions outputs;
+    bool hasReductionFailure;
+    Poincare::Preferences::ComplexFormat complexFormat;
+
+    size_t sizeOfTrees() const {
+      return input.tree()->treeSize() + outputs.exact.tree()->treeSize() +
+             outputs.approximate.tree()->treeSize();
+    }
+  };
+
+  Poincare::UserExpression parseInput(Poincare::Layout inputLayout,
+                                      Poincare::Context* context);
+
+  CalculationElements computeAndProcess(Poincare::Expression inputExpression,
+                                        Poincare::Context* context);
+
+  /* Push an empty calculation at a certain location. Assumes there is enough
+   * space to push an empty calculation. Returns a pointer to the new
+   * Calculation. */
+  Calculation* pushEmptyCalculation(char** location);
+
+  /* Push helper method that takes the current location and updates it to the
+   * end of the pushed content. Assumes there is enough space to push the
+   * expression tree. Returns the size of the pushed content. */
+  size_t pushExpressionTree(char** location, Poincare::UserExpression e);
+
+  /* Push a Calculation with its expressions (input, exact and approximate
+   * output), and updates the calculation buffer to hold this new calculation
+   * (see the memory layout in the CalculationStore class description).
+   * Note: this method assumes that enough space was freed to contain the new
+   * calculation. */
+  Calculation* pushCalculation(const CalculationElements& calculationToPush);
+
+  char* const m_buffer;
+  const size_t m_bufferSize;
+  int m_numberOfCalculations;
+  MathPreferences m_inUsePreferences;
+};
+
+}  // namespace Calculation
+
+#endif

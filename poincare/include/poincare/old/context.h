d16a5670507422a9844990643c0e56b459a98c3b
LoreneNumworks <lorene.imperor@numworks.com>
2025-08-05 15:44:44 +0200

    refactor(poincare): cleanup and add todos

--- poincare/include/poincare/old/context.h
+++ poincare/include/poincare/old/context.h
@@ -1,26 +1,27 @@
 #ifndef POINCARE_CONTEXT_H
 #define POINCARE_CONTEXT_H
 
-#include <assert.h>
 #include <stdint.h>
 
 #include <cmath>
 
+// TODO: this file should be moved to the "new" Poincare.
+
+// TODO: no need to forward-declare Tree once moved to the new Poincare
 namespace Poincare::Internal {
 class Tree;
 }
 
 namespace Poincare {
 
-class PoolObject;
-
-// TODO: Rename SymbolContext
+// TODO: Rename to a more specific name
 class Context {
  public:
   enum class UserNamedType : uint8_t { None, Function, Sequence, Symbol, List };
   virtual UserNamedType expressionTypeForIdentifier(const char* identifier,
                                                     int length) const = 0;
 
+  // TODO: return an Expression type
   /* The returned Tree* may live in the Pool or in the Storage. */
   virtual const Internal::Tree* expressionForUserNamed(
       const Internal::Tree* symbol) const = 0;
@@ -38,6 +39,7 @@ class Context {
   virtual ~Context() = default;
 #endif
 
+  // TODO: remove, only a static SequenceContext may still be needed
   static Context* GlobalContext;
 };
 

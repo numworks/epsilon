c5c79c7847f0ef5e3ee8dafee82909a4fbec4cfb
MarcNumworks <marc.coudriau@numworks.com>
2025-08-21 09:54:00 +0200

    refactor(quiz): move constant to uncomment assert

--- shared/quiz/include/quiz.h
+++ shared/quiz/include/quiz.h
@@ -5,20 +5,23 @@
 #include <quiz/src/symbols.h>
 #include <stdbool.h>
 
+constexpr int k_maxNumberOfQuizCases = 1024;
+
 /* Each quiz case has a associated static instance of RegisterQuizCase that adds
  * quiz_case to a static array. */
 struct RegisterQuizCase {
   RegisterQuizCase(const char* name, QuizCase function) {
-    // Note that the array is initialized in fiasco order
-    // assert(quiz_number_of_cases < k_maxNumberOfCases);
+    // Note that the array is initialized in an arbitrary order, we need to sort
+    // it afterwise. Cf Static Initialization Order Fiasco in C++.
+    assert(quiz_number_of_cases < k_maxNumberOfQuizCases);
     quiz_case_names[quiz_number_of_cases] = name;
     quiz_cases[quiz_number_of_cases++] = function;
   };
 };
 
-#define QUIZ_CASE(name)                                                \
-  void quiz_case_##name();                                             \
-  RegisterQuizCase register_quiz_case_##name(#name, quiz_case_##name); \
+#define QUIZ_CASE(name)                                                      \
+  void quiz_case_##name();                                                   \
+  const RegisterQuizCase register_quiz_case_##name(#name, quiz_case_##name); \
   void quiz_case_##name()
 
 void quiz_assert(bool condition);
--- shared/quiz/src/symbols.cpp
+++ shared/quiz/src/symbols.cpp
@@ -1,6 +1,7 @@
 #include "symbols.h"
 
-constexpr int k_maxNumberOfCases = 1024;
+#include "quiz.h"
+
 int quiz_number_of_cases = 0;
-QuizCase quiz_cases[k_maxNumberOfCases];
-const char* quiz_case_names[k_maxNumberOfCases];
+QuizCase quiz_cases[k_maxNumberOfQuizCases];
+const char* quiz_case_names[k_maxNumberOfQuizCases];

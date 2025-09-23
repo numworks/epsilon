aa8135071487af9e8ee6e8ccd0536ba332ba2e66
Mateo <mateo.dugas@numworks.com>
2025-09-23 11:10:01 +0200

    feat(clipboard): hijack journal to change system clipboard paste into externaltext

--- shared/ion/src/simulator/shared/journal/queue_journal.h
+++ shared/ion/src/simulator/shared/journal/queue_journal.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ion/clipboard.h>
 #include <ion/events.h>
 
 #include <queue>
@@ -13,6 +14,31 @@ namespace Journal {
 class QueueJournal : public Ion::Events::Journal {
  public:
   void pushEvent(Ion::Events::Event e) override {
+#if ION_KEYBOARD_RICH
+    if (e == Ion::Events::Paste) {
+      const char* systemBuffer = Ion::Clipboard::read();
+      if (systemBuffer) {
+        // Changed a Paste that uses the system clipboard into an ExternalText
+        // to preserve pasted content
+        size_t len = strlen(systemBuffer);
+        // 1 <=len<= 9 = 1 event
+        // 10<=len<=18 = 2 events
+        // 19<=len<=27 = 3 events
+        for (int i = 0;
+             i < (len + Ion::Events::sharedExternalTextBufferSize - 2) /
+                     (Ion::Events::sharedExternalTextBufferSize - 1);
+             ++i) {
+          m_eventStorage.push(Ion::Events::ExternalText);
+          AdditionalData data;
+          strlcpy(data.text, systemBuffer,
+                  Ion::Events::sharedExternalTextBufferSize);
+          m_externalTextStorage.push(data);
+          systemBuffer += Ion::Events::sharedExternalTextBufferSize - 1;
+        }
+        return;
+      }
+    }
+#endif
     if (e != Ion::Events::None) {
       m_eventStorage.push(e);
     }

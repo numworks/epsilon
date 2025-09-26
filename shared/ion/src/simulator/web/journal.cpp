c79368a4239416204c3efe42d45d534d1d3a4629
Mateo <mateo.dugas@numworks.com>
2025-09-26 13:49:21 +0200

    feat(web): send ExternalText and Paste data to website

--- shared/ion/src/simulator/web/journal.cpp
+++ shared/ion/src/simulator/web/journal.cpp
@@ -11,28 +11,72 @@ namespace Journal {
 using Ion::Events::Event;
 using Ion::Events::None;
 
+static inline void callOnEpsilonIdle() {
+  // clang-format off
+  EM_ASM({
+    if (typeof Module.onEpsilonIdle === "function") {
+      Module.onEpsilonIdle();
+    }
+  });
+  // clang-format on
+}
+
+static inline void sendEventToJS(uint8_t event) {
+  // clang-format off
+  EM_ASM({
+    if (typeof Module.onIonEvent === "function") {
+      Module.onIonEvent($0, true);
+    }
+  }, event);
+  // clang-format on
+}
+
+static inline void sendRawDataToJS(uint8_t data) {
+  // clang-format off
+  EM_ASM({
+    if (typeof Module.onIonEvent === "function") {
+      Module.onIonEvent($0, false);
+    }
+  }, data);
+  // clang-format on
+}
+
+static inline void sendRawDataToJS(const char* data) {
+  while (true) {
+    sendRawDataToJS(*data);
+    if (*data == 0) {
+      break;
+    }
+    ++data;
+  }
+}
+
 class LogJournal : public Ion::Events::Journal {
  public:
   void pushEvent(Event e) override {
     static bool lastEventWasNone = false;
+#if ION_KEYBOARD_RICH
+    if (e == Ion::Events::Paste) {
+      const char* systemBuffer = Ion::Clipboard::read();
+      if (systemBuffer) {
+        sendEventToJS(Events::Event(Events::ExternalText));
+        sendRawDataToJS(systemBuffer);
+      } else {
+        sendEventToJS(e);
+      }
+    } else
+#endif
+        if (e == Ion::Events::ExternalText) {
+      sendEventToJS(e);
+      sendRawDataToJS(Ion::Events::sharedExternalTextBuffer());
+    } else if (e != Ion::Events::None) {
+      sendEventToJS(e);
+    }
     if (e != None) {
-      // clang-format off
-      EM_ASM({
-        if (typeof Module.onIonEvent === "function") {
-          Module.onIonEvent($0);
-        }
-      }, static_cast<uint8_t>(e));
-      // clang-format on
       lastEventWasNone = false;
     } else {
       if (!lastEventWasNone) {
-        // clang-format off
-        EM_ASM({
-          if (typeof Module.onEpsilonIdle === "function") {
-            Module.onEpsilonIdle();
-          }
-        });
-        // clang-format on
+        callOnEpsilonIdle();
         lastEventWasNone = true;
       }
     }

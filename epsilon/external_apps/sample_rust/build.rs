2315326ea5424f5331bc6358f05777a303e49160
Mateo <mateo.dugas@numworks.com>
2025-09-29 16:30:18 +0200

    fix(external_apps): fix rust app

--- epsilon/external_apps/sample_rust/build.rs
+++ epsilon/external_apps/sample_rust/build.rs
@@ -3,8 +3,14 @@ use std::process::Command;
 fn main() {
     // Turn icon.png into icon.nwi
     println!("cargo:rerun-if-changed=src/icon.png");
-    let output = Command::new("nwlink")
-        .args(&["png-nwi", "src/icon.png", "target/icon.nwi"])
-        .output().expect("Failure to launch process");
-    assert!(output.status.success(), "{}", String::from_utf8_lossy(&output.stderr));
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("npx --yes -- nwlink@0.0.19 png-nwi src/icon.png target/icon.nwi")
+        .output()
+        .expect("Icon creation failed.");
+    assert!(
+        output.status.success(),
+        "{}",
+        String::from_utf8_lossy(&output.stderr)
+    );
 }

5cf3b6e3d6eec3c87eb649b82cdbdee236f56b9c
LoreneNumworks <lorene.imperor@numworks.com>
2025-10-09 10:20:01 +0200

    refactor(poincare): expose VectorDatasetColumn

--- poincare/include/poincare/statistics/dataset_column.h
+++ poincare/include/poincare/statistics/dataset_column.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <omg/vector.h>
+
 namespace Poincare {
 
 template <typename T>
@@ -7,6 +9,22 @@ class DatasetColumn {
  public:
   virtual T valueAtIndex(int index) const = 0;
   virtual int length() const = 0;
+};
+
+template <typename T>
+class VectorDatasetColumn : public DatasetColumn<T> {
+ public:
+  static constexpr size_t k_capacity = 100;
+
+  VectorDatasetColumn(std::initializer_list<T> values) : m_vector{values} {}
+  T valueAtIndex(int index) const override {
+    assert(index >= 0);
+    return m_vector[index];
+  }
+  int length() const override { return m_vector.size(); }
+
+ private:
+  OMG::StaticVector<T, k_capacity> m_vector;
 };
 
 }  // namespace Poincare
--- poincare/test/statistics.cpp
+++ poincare/test/statistics.cpp
@@ -1,27 +1,7 @@
-#include <omg/vector.h>
 #include <poincare/statistics/statistics_dataset.h>
 #include <quiz.h>
 
 #include "float_helper.h"
-
-namespace Poincare {
-
-template <typename T>
-class VectorDatasetColumn : public DatasetColumn<T> {
- public:
-  VectorDatasetColumn(std::initializer_list<T> values) : m_vector{values} {}
-  T valueAtIndex(int index) const override {
-    assert(index >= 0);
-    return m_vector[index];
-  }
-  int length() const override { return m_vector.size(); }
-
- private:
-  static constexpr size_t k_capacity = 100;
-  OMG::StaticVector<T, k_capacity> m_vector;
-};
-
-}  // namespace Poincare
 
 QUIZ_CASE(pcj_statistics_dataset) {
   Poincare::VectorDatasetColumn values{1.0, 2.0, 3.0, 4.0};

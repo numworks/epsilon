11a003a3f9e4554265c79bdcf00d7688fe6a166e
LoreneNumworks <lorene.imperor@numworks.com>
2025-10-15 13:52:00 +0200

    feat(poincare): add hypothesis symbol for anova

--- poincare/src/statistics/inference/significance_test.cpp
+++ poincare/src/statistics/inference/significance_test.cpp
@@ -35,6 +35,7 @@ const char* HypothesisSymbol(TestType testType) {
     case TestType::TwoProportions:
       return "p1-p2";
     case TestType::ANOVA:
+      return "μi≠μj";
     case TestType::OneMean:
       return "μ";
     case TestType::TwoMeans:
@@ -53,6 +54,9 @@ Poincare::Layout HypothesisLayout(TestType testType) {
   }
   if (testType == TestType::TwoMeans) {
     return "μ"_l ^ KSubscriptL("1"_l) ^ "-"_l ^ "μ"_l ^ KSubscriptL("2"_l);
+  }
+  if (testType == TestType::ANOVA) {
+    return "μ"_l ^ KSubscriptL("i"_l) ^ "≠"_l ^ "μ"_l ^ KSubscriptL("j"_l);
   }
   return Layout::String(HypothesisSymbol(testType));
 }

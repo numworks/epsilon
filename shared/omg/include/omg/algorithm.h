de36c44880dc8d35ddf24fa5bafc2177a3b20179
LoreneNumworks <lorene.imperor@numworks.com>
2025-12-10 16:48:05 +0100

    feat(omg): add AllEqual algorithm

---
+++ shared/omg/include/omg/algorithm.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/* Equivalent to <algorithm> but for custom algorithms that do not exist in the
+ * STL */
+
+namespace OMG {
+
+template <class Iterator>
+constexpr bool AllEqual(Iterator begin, Iterator end) {
+  if (begin == end) {
+    // Empty container
+    return true;
+  }
+  Iterator it = begin + 1;
+  while (it != end) {
+    if (*it != *begin) {
+      return false;
+    }
+    it++;
+  }
+  return true;
+}
+
+}  // namespace OMG

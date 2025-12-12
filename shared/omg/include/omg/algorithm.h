20df33ab61544cb33a6f4cf5e39b6daeb43dcf38
LoreneNumworks <lorene.imperor@numworks.com>
2025-12-12 17:27:42 +0100

    refactor(omg): add assertion

--- shared/omg/include/omg/algorithm.h
+++ shared/omg/include/omg/algorithm.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <assert.h>
+
 /* Equivalent to <algorithm> but for custom algorithms that do not exist in the
  * STL */
 
@@ -7,6 +9,7 @@ namespace OMG {
 
 template <class Iterator>
 constexpr bool AllEqual(Iterator begin, Iterator end) {
+  assert(begin <= end);
   if (begin == end) {
     // Empty container
     return true;

23b918f79dc4edbeb385cfe375a2132ed07f4766
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2026-01-06 17:14:43 +0100

    docs(omg): fix typo

--- shared/omg/include/omg/unreachable.h
+++ shared/omg/include/omg/unreachable.h
@@ -4,7 +4,7 @@
 
 /* Raise an undefined behavior, equivalent to C++23 std::unreachable.
  * Use it instead of assert(false) to mark places that should not be reached.
- * This could be a OMG::unreachable() method, but we use a macro to shorten
+ * This could be an OMG::unreachable() method, but we use a macro to shorten
  * backtrace during fuzzer debugging. */
 #define OMG_UNREACHABLE      \
   {                          \

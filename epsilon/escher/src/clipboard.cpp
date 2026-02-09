c4466bd774380c2bdd788326a2e37dee32120df6
PaulineNumWorks <pauline.garelli@numworks.com>
2026-02-09 16:11:28 +0100

    refactor(epsilon/clipboard): remove unused code

--- epsilon/escher/src/clipboard.cpp
+++ epsilon/escher/src/clipboard.cpp
@@ -64,31 +64,6 @@ const char* Clipboard::storedText() {
   if (m_bufferState == TextOutdated) {
     updateTextFromTree();
   }
-#if 0
-  // TODO remove if nothing is broken
-  /* In order to allow copy/paste of empty formulas, we need to add empty
-   * layouts between empty system parenthesis. This way, when the expression
-   * is parsed, it is recognized as a proper formula and appears with the
-   * correct visual layout. Without this process, copying an empty integral
-   * then pasting it gives : int((), x, (), ()) instead of drawing an empty
-   * integral.
-   *
-   * Furthermore, in case the user switches from linear to natural writing
-   * mode we need to add an empty layout between parenthesis to allow proper
-   * layout construction. */
-  constexpr int numberOfPairs = 6;
-  constexpr UTF8Helper::TextPair textPairs[numberOfPairs] = {
-      UTF8Helper::TextPair("()", "(\x11)"),
-      UTF8Helper::TextPair("[]", "[\x11]"),
-      UTF8Helper::TextPair("[,", "[\x11,"),
-      UTF8Helper::TextPair(",,", ",\x11,"),
-      UTF8Helper::TextPair(",]", ",\x11]"),
-      UTF8Helper::TextPair("\x12\x13", "\x12\x11\x13"),
-  };
-
-  UTF8Helper::TryAndReplacePatternsInStringByPatterns(
-      m_textBuffer, TextField::MaxBufferSize(), textPairs, numberOfPairs, true);
-#endif
   return m_textBuffer;
 }
 
